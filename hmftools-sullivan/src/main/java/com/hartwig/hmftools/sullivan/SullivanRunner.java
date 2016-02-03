4a207a97df6d45c992abc69c34d953a5a0906dbb
kduyvesteyn <korneel.duyvesteyn@gmail.com>
2016-02-03 14:01:50 +0100

    Add command line parameters and make sullivan deployable

--- hmftools-sullivan/src/main/java/com/hartwig/hmftools/sullivan/SullivanRunner.java
+++ hmftools-sullivan/src/main/java/com/hartwig/hmftools/sullivan/SullivanRunner.java
@@ -3,11 +3,10 @@ package com.hartwig.hmftools.sullivan;
 public class SullivanRunner {
 
     public static void main(String[] args) {
-        String originalFastqPath =
-                "/Users/kduyvesteyn/hmf/data/100k_reads_hiseq/Sample_CONTROLP25_H7YRLADXX/CONTROLP25_H7YRLADXX_ATCACG_L001_R1_001.subset_100k.fastq";
-        String recreatedFastqPath =
-                "/Users/kduyvesteyn/hmf/out/fastq/CONTROLP25_H7YRLADXX_ATCACG_L001_1.fastq";
-
-        SullivanAlgo.runSullivanAlgo(originalFastqPath, recreatedFastqPath);
+        if (args.length != 2) {
+            System.out.println("Usage <originalFastqPath> <recreatedFastqPath>");
+        } else {
+            SullivanAlgo.runSullivanAlgo(args[0], args[1]);
+        }
     }
 }
