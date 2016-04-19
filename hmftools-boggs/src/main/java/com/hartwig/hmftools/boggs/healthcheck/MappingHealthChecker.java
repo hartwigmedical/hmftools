7ec07b618ff146be9106802e735a31d9b17bef6f
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-04-19 13:46:56 +0200

    Added log4j for logging

--- hmftools-boggs/src/main/java/com/hartwig/hmftools/boggs/healthcheck/MappingHealthChecker.java
+++ hmftools-boggs/src/main/java/com/hartwig/hmftools/boggs/healthcheck/MappingHealthChecker.java
@@ -4,9 +4,13 @@ import com.hartwig.hmftools.boggs.PatientData;
 import com.hartwig.hmftools.boggs.SampleData;
 import com.hartwig.hmftools.boggs.flagstatreader.FlagStatData;
 import com.hartwig.hmftools.boggs.flagstatreader.FlagStats;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class MappingHealthChecker implements HealthChecker {
+
+    private static final Logger LOGGER = LogManager.getLogger(MappingHealthChecker.class);
 
     private static final double MIN_MAPPED_PERCENTAGE = 0.992;
     private static final double MIN_PROPERLY_PAIRED_PERCENTAGE = 0.99;
@@ -20,9 +24,9 @@ public class MappingHealthChecker implements HealthChecker {
     }
 
     private void checkSample(@NotNull SampleData sample) {
-        System.out.println("Checking mapping health for " + sample.externalID());
+        LOGGER.info("Checking mapping health for " + sample.externalID());
         for (FlagStatData flagstatData : sample.rawMappingFlagstats()) {
-            System.out.println(" Verifying " + flagstatData.path());
+            LOGGER.info(" Verifying " + flagstatData.path());
             FlagStats passed = flagstatData.qcPassedReads();
             double mappedPercentage = passed.mapped() / (double) passed.total();
             double properlyPairedPercentage = passed.properlyPaired() / (double) passed.total();
@@ -30,34 +34,34 @@ public class MappingHealthChecker implements HealthChecker {
             double mateMappedToDifferentChrPercentage = passed.mateMappedToDifferentChr() / (double) passed.total();
 
             if (mappedPercentage < MIN_MAPPED_PERCENTAGE) {
-                System.out.println("  WARN: Low mapped percentage: " +
+                LOGGER.info("  WARN: Low mapped percentage: " +
                         toPercentage(mappedPercentage));
             } else {
-                System.out.println("  OK: Acceptable mapped percentage: " +
+                LOGGER.info("  OK: Acceptable mapped percentage: " +
                         toPercentage(mappedPercentage));
             }
 
             if (properlyPairedPercentage < MIN_PROPERLY_PAIRED_PERCENTAGE) {
-                System.out.println("  WARN: Low properly paired percentage: " +
+                LOGGER.info("  WARN: Low properly paired percentage: " +
                         toPercentage(properlyPairedPercentage));
             } else {
-                System.out.println("  OK: Acceptable properly paired percentage: " +
+                LOGGER.info("  OK: Acceptable properly paired percentage: " +
                         toPercentage(properlyPairedPercentage));
             }
 
             if (singletonPercentage > MAX_SINGLETONS) {
-                System.out.println("  WARN: High singleton percentage: " +
+                LOGGER.info("  WARN: High singleton percentage: " +
                         toPercentage(singletonPercentage));
             } else {
-                System.out.println("  OK: Acceptable singleton percentage: " +
+                LOGGER.info("  OK: Acceptable singleton percentage: " +
                         toPercentage(singletonPercentage));
             }
 
             if (mateMappedToDifferentChrPercentage > MAX_MATE_MAPPED_TO_DIFFERENT_CHR) {
-                System.out.println("  WARN: High mate mapped to different chr percentage: " +
+                LOGGER.info("  WARN: High mate mapped to different chr percentage: " +
                         toPercentage(mateMappedToDifferentChrPercentage));
             } else {
-                System.out.println("  OK: Acceptable mate mapped to different chr percentage: " +
+                LOGGER.info("  OK: Acceptable mate mapped to different chr percentage: " +
                         toPercentage(mateMappedToDifferentChrPercentage));
             }
         }
