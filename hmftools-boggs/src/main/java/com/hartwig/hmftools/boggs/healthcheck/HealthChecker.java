f2e9cf5c6bd0d6bfad38526450c895bf23f5942f
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-09-09 15:56:44 +0200

    Remove obsolete module

--- hmftools-boggs/src/main/java/com/hartwig/hmftools/boggs/healthcheck/HealthChecker.java
+++
@@ -1,9 +0,0 @@
-package com.hartwig.hmftools.boggs.healthcheck;
-
-import com.hartwig.hmftools.boggs.PatientData;
-import org.jetbrains.annotations.NotNull;
-
-public interface HealthChecker {
-
-    boolean isHealthy(@NotNull PatientData patient);
-}
