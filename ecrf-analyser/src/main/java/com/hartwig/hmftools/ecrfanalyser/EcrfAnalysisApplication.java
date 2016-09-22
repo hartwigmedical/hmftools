ed73a8d03d0d474d679423fb2d44e71f6f9cce65
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-09-22 14:04:33 +0200

    Added skeleton structure for ecrf analyser

---
+++ ecrf-analyser/src/main/java/com/hartwig/hmftools/ecrfanalyser/EcrfAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.hartwig.hmftools.ecrfanalyser;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EcrfAnalysisApplication {
+
+    private static final Logger LOGGER = LogManager.getLogger(EcrfAnalysisApplication.class);
+
+    public static void main(final String... args) {
+        LOGGER.info("Hello World");
+    }
+}
