9d4625ae12327158e04302895c75178bc060fffe
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-09-28 16:29:55 +0200

    Some updates but still trying things out with ECRF

--- ecrf-analyser/src/main/java/com/hartwig/hmftools/ecrfanalyser/EcrfAnalysisApplication.java
+++ ecrf-analyser/src/main/java/com/hartwig/hmftools/ecrfanalyser/EcrfAnalysisApplication.java
@@ -2,10 +2,14 @@ package com.hartwig.hmftools.ecrfanalyser;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.List;
 
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,9 +23,82 @@ public class EcrfAnalysisApplication {
         LOGGER.info("Hello World");
     }
 
-    void run(@NotNull String path) throws FileNotFoundException, XMLStreamException {
+    @NotNull
+    private final String ecrfXmlPath;
+    @NotNull
+    private final String csvOutPath;
+
+    EcrfAnalysisApplication(@NotNull final String ecrfXmlPath, @NotNull final String csvOutPath) {
+        this.ecrfXmlPath = ecrfXmlPath;
+        this.csvOutPath = csvOutPath;
+    }
+
+    void generateCsv(@NotNull List<String> patients, @NotNull List<String> fields)
+            throws FileNotFoundException, XMLStreamException {
         XMLInputFactory factory = XMLInputFactory.newInstance();
-        XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(path));
+        XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(ecrfXmlPath));
+        int count = 0;
+        while (reader.hasNext() && count < 10) {
+            reader.next();
+            LOGGER.info("ElementType " + count + ": " + getEventTypeString(reader.getEventType()));
+            if (reader.getEventType() != XMLEvent.CHARACTERS) {
+                LOGGER.info("ElementName " + count + ": " + reader.getName());
+            }
+
+            if (reader.getEventType() == XMLEvent.START_ELEMENT) {
+                if (reader.hasText()) {
+                    LOGGER.info(reader.getElementText());
+                }
+                LOGGER.info(reader.getAttributeCount());
+            }
+
+            reader.next();
+            count++;
+        }
+        LOGGER.info(count);
+
+//        int namespaces = reader.getNamespaceCount();
+//        for (int i = 0; i < namespaces; i++) {
+//            LOGGER.info("NamespacePrefix " + i + ": " + reader.getNamespacePrefix(i));
+//            LOGGER.info("NamespaceURI " + i + ": " + reader.getNamespaceURI(i));
+//        }
         reader.close();
+
+        //        XMLEventReader eventReader = factory.createXMLEventReader(new FileInputStream(ecrfXmlPath));
+        //        while (eventReader.hasNext()) {
+        //            XMLEvent e = eventReader.nextEvent();
+        //            LOGGER.info(e.toString());
+        //        }
+    }
+
+    @NotNull
+    private static String getEventTypeString(int eventType) {
+        switch (eventType) {
+            case XMLEvent.START_ELEMENT:
+                return "START_ELEMENT";
+            case XMLEvent.END_ELEMENT:
+                return "END_ELEMENT";
+            case XMLEvent.PROCESSING_INSTRUCTION:
+                return "PROCESSING_INSTRUCTION";
+            case XMLEvent.CHARACTERS:
+                return "CHARACTERS";
+            case XMLEvent.COMMENT:
+                return "COMMENT";
+            case XMLEvent.START_DOCUMENT:
+                return "START_DOCUMENT";
+            case XMLEvent.END_DOCUMENT:
+                return "END_DOCUMENT";
+            case XMLEvent.ENTITY_REFERENCE:
+                return "ENTITY_REFERENCE";
+            case XMLEvent.ATTRIBUTE:
+                return "ATTRIBUTE";
+            case XMLEvent.DTD:
+                return "DTD";
+            case XMLEvent.CDATA:
+                return "CDATA";
+            case XMLEvent.SPACE:
+                return "SPACE";
+        }
+        return "UNKNOWN_EVENT_TYPE , " + eventType;
     }
 }
--- ecrf-analyser/src/test/java/com/hartwig/hmftools/ecrfanalyser/EcrfAnalysisApplicationTest.java
+++ ecrf-analyser/src/test/java/com/hartwig/hmftools/ecrfanalyser/EcrfAnalysisApplicationTest.java
@@ -3,9 +3,11 @@ package com.hartwig.hmftools.ecrfanalyser;
 import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
+import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 
 import org.junit.Test;
@@ -13,11 +15,14 @@ import org.junit.Test;
 public class EcrfAnalysisApplicationTest {
 
     private static final String TEST_ECRF = Resources.getResource("example/ecrf.xml").getPath();
+    private static final String CSV_OUT = "/Users/kduyvesteyn/hmf/tmp/ecrf.csv";
 
     @Test
     public void tryIt() throws FileNotFoundException, XMLStreamException {
-        EcrfAnalysisApplication app = new EcrfAnalysisApplication();
-        app.run(TEST_ECRF);
-        assertTrue(true);
+        EcrfAnalysisApplication app = new EcrfAnalysisApplication(TEST_ECRF, CSV_OUT);
+
+        List<String> patients = Lists.newArrayList();
+        List<String> fields = Lists.newArrayList();
+        app.generateCsv(patients, fields);
     }
 }
