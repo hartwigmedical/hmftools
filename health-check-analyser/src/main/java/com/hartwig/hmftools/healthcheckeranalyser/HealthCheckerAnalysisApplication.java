786d000caa1c22c069c211901a4a5260fe93ffb3
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-12-18 13:24:26 +0100

    Add support for printing a single json health checker output

--- health-check-analyser/src/main/java/com/hartwig/hmftools/healthcheckeranalyser/HealthCheckerAnalysisApplication.java
+++ health-check-analyser/src/main/java/com/hartwig/hmftools/healthcheckeranalyser/HealthCheckerAnalysisApplication.java
@@ -2,9 +2,11 @@ package com.hartwig.hmftools.healthcheckeranalyser;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.collect.Lists;
 import com.hartwig.hmftools.healthcheckeranalyser.model.HealthCheckReport;
@@ -19,43 +21,51 @@ import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class HealthCheckerAnalysisApplication {
 
     private static final Logger LOGGER = LogManager.getLogger(HealthCheckerAnalysisApplication.class);
 
-    private static final String REPORTS_PATH_ARGS_DESC = "The path where health check report can be found.";
-    private static final String REPORTS_PATH = "reports";
+    private static final String REPORT_PATH_ARGS_DESC = "A path towards a single report to print in STDOUT.";
+    private static final String REPORT_PATH = "report";
+
+    private static final String DATA_PATH_ARGS_DESC = "A path which contains multiple health check runs.";
+    private static final String DATA_PATH = "data";
 
     private static final String CSV_OUT_ARGS_DESC = "The file to write csv results to.";
     private static final String CSV_OUT = "csvout";
 
-    @NotNull
-    private final String reportsPath;
-    @NotNull
+    @Nullable
+    private final String reportPath;
+    @Nullable
+    private final String dataPath;
+    @Nullable
     private final String csvOut;
 
     public static void main(final String... args) throws ParseException, IOException {
         Options options = createOptions();
         CommandLine cmd = createCommandLine(options, args);
 
-        String reportsPath = cmd.getOptionValue(REPORTS_PATH);
+        String reportPath = cmd.getOptionValue(REPORT_PATH);
+        String dataPath = cmd.getOptionValue(DATA_PATH);
         String csvOut = cmd.getOptionValue(CSV_OUT);
 
-        if (reportsPath == null || csvOut == null) {
+        if (reportPath == null && dataPath == null || (dataPath != null && csvOut == null)) {
             final HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("Health-Checks-Analyser", options);
             System.exit(1);
         }
 
-        new HealthCheckerAnalysisApplication(reportsPath, csvOut).runAnalysis();
+        new HealthCheckerAnalysisApplication(reportPath, dataPath, csvOut).runAnalysis();
     }
 
     @NotNull
     private static Options createOptions() {
         Options options = new Options();
 
-        options.addOption(REPORTS_PATH, true, REPORTS_PATH_ARGS_DESC);
+        options.addOption(REPORT_PATH, true, REPORT_PATH_ARGS_DESC);
+        options.addOption(DATA_PATH, true, DATA_PATH_ARGS_DESC);
         options.addOption(CSV_OUT, true, CSV_OUT_ARGS_DESC);
 
         return options;
@@ -68,14 +78,26 @@ public class HealthCheckerAnalysisApplication {
         return parser.parse(options, args);
     }
 
-    HealthCheckerAnalysisApplication(@NotNull final String reportsPath, @NotNull final String csvOut) {
-        this.reportsPath = reportsPath;
+    HealthCheckerAnalysisApplication(@Nullable final String reportPath, @Nullable final String dataPath,
+            @Nullable final String csvOut) {
+        this.reportPath = reportPath;
+        this.dataPath = dataPath;
         this.csvOut = csvOut;
     }
 
     void runAnalysis() throws IOException {
+        if (reportPath == null && dataPath != null && csvOut != null) {
+            runInBatchMode(dataPath, csvOut);
+        } else if (reportPath != null) {
+            runInSingleMode(reportPath);
+        } else {
+            LOGGER.warn("Could not run, invalid input parameters!");
+        }
+    }
+
+    private static void runInBatchMode(@NotNull String dataPath, @NotNull String csvOut) throws IOException {
         List<HealthCheckReport> reports = Lists.newArrayList();
-        File[] runs = new File(reportsPath).listFiles();
+        File[] runs = new File(dataPath).listFiles();
         if (runs != null) {
             for (File runDirectory : runs) {
                 LOGGER.info("Processing " + runDirectory.getPath());
@@ -83,7 +105,7 @@ public class HealthCheckerAnalysisApplication {
                 reports.add(report);
             }
         } else {
-            LOGGER.warn(String.format("%s contains no runs!", reportsPath));
+            LOGGER.warn(String.format("%s contains no runs!", dataPath));
         }
 
         BufferedWriter writer = new BufferedWriter(new FileWriter(csvOut, false));
@@ -96,5 +118,26 @@ public class HealthCheckerAnalysisApplication {
             writer.write(HealthCheckDataToCSV.tumorSample(report));
         }
         writer.close();
+    }
+
+    private static void runInSingleMode(@NotNull String reportPath) throws FileNotFoundException {
+        HealthCheckReport report = HealthCheckReportFactory.fromHealthCheckReport(reportPath);
+
+        LOGGER.info("REF SAMPLE CHECKS - " + report.refSample());
+        printChecks(report.refChecks());
+
+        LOGGER.info("");
+        LOGGER.info("TUMOR SAMPLE CHECKS - " + report.tumorSample());
+        printChecks(report.tumorChecks());
+
+        LOGGER.info("");
+        LOGGER.info("PATIENT CHECKS");
+        printChecks(report.patientChecks());
+    }
+
+    private static void printChecks(@NotNull Map<String, String> checks) {
+        for (Map.Entry<String, String> check : checks.entrySet()) {
+            LOGGER.info(check.getKey() + "\t" + check.getValue());
+        }
     }
 }
--- health-check-analyser/src/test/java/com/hartwig/hmftools/healthcheckeranalyser/HealthCheckerAnalysisApplicationTest.java
+++ health-check-analyser/src/test/java/com/hartwig/hmftools/healthcheckeranalyser/HealthCheckerAnalysisApplicationTest.java
@@ -1,5 +1,6 @@
 package com.hartwig.hmftools.healthcheckeranalyser;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.google.common.io.Resources;
@@ -8,11 +9,18 @@ import org.junit.Test;
 
 public class HealthCheckerAnalysisApplicationTest {
 
-    private static final String EXAMPLE_REPORT_PATH = Resources.getResource("checks").getPath();
+    private static final String EXAMPLE_REPORT_PATH =
+            Resources.getResource("checks").getPath() + File.separator + "example.json";
+    private static final String EXAMPLE_DATA_PATH = Resources.getResource("checks").getPath();
 
     @Test
-    public void canRunAnalysis() throws IOException {
+    public void canRunAnalysisOnData() throws IOException {
         String csvOut = "/Users/kduyvesteyn/hmf/tmp/checks.csv";
-        new HealthCheckerAnalysisApplication(EXAMPLE_REPORT_PATH, csvOut).runAnalysis();
+        new HealthCheckerAnalysisApplication(null, EXAMPLE_DATA_PATH, csvOut).runAnalysis();
+    }
+
+    @Test
+    public void canRunAnalysisOnReport() throws IOException {
+        new HealthCheckerAnalysisApplication(EXAMPLE_REPORT_PATH, null, null).runAnalysis();
     }
 }
