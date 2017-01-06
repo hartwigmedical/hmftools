365e948b26cd1bbe51bd8e57c2deb3e5405d07dc
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-01-06 10:23:11 +0100

    Added actual filter bed

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/slicing/SlicerFactoryTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/slicing/SlicerFactoryTest.java
@@ -13,13 +13,22 @@ import org.junit.Test;
 public class SlicerFactoryTest {
 
     private static final String BED_FILE_BASE_PATH = Resources.getResource("bed").getPath();
+    private static final String FILTER_BED = "filter.bed";
     private static final String VALID_BED = "valid.bed";
     private static final String UNSORTED_BED = "unsorted.bed";
     private static final String INVALID_BED = "invalid.bed";
 
     @Test
+    public void canCreateFilterBed() throws IOException, EmptyFileException {
+        final String bedFile = BED_FILE_BASE_PATH + File.separator + FILTER_BED;
+        final Slicer slicer = SlicerFactory.fromBedFile(bedFile);
+        assertEquals(118, slicer.numberOfRegions());
+        assertEquals(15593430, slicer.numberOfBases());
+    }
+
+    @Test
     public void handleTrivialBed() throws IOException, EmptyFileException {
-        String bedFile = BED_FILE_BASE_PATH + File.separator + VALID_BED;
+        final String bedFile = BED_FILE_BASE_PATH + File.separator + VALID_BED;
         final Slicer slicer = SlicerFactory.fromBedFile(bedFile);
         assertEquals(1, slicer.numberOfRegions());
         assertEquals(1, slicer.numberOfBases());
@@ -27,14 +36,14 @@ public class SlicerFactoryTest {
 
     @Test
     public void handleUnsortedBed() throws IOException, EmptyFileException {
-        String bedFile = BED_FILE_BASE_PATH + File.separator + UNSORTED_BED;
+        final String bedFile = BED_FILE_BASE_PATH + File.separator + UNSORTED_BED;
         final Slicer slicer = SlicerFactory.fromBedFile(bedFile);
         assertEquals(2, slicer.numberOfRegions());
     }
 
     @Test
     public void handleInvalidBedRegion() throws IOException, EmptyFileException {
-        String bedFile = BED_FILE_BASE_PATH + File.separator + INVALID_BED;
+        final String bedFile = BED_FILE_BASE_PATH + File.separator + INVALID_BED;
         final Slicer slicer = SlicerFactory.fromBedFile(bedFile);
         assertEquals(2, slicer.numberOfRegions());
     }
