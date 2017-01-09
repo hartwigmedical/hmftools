bce98e42af5a2e06b47edd00b5f5c37d20486f1c
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-01-09 16:15:39 +0100

    Add support for DRUP in health checker

--- health-checker/src/main/java/com/hartwig/hmftools/healthchecker/context/ProductionRunContextFactory.java
+++ health-checker/src/main/java/com/hartwig/hmftools/healthchecker/context/ProductionRunContextFactory.java
@@ -8,60 +8,79 @@ import org.jetbrains.annotations.NotNull;
 
 public final class ProductionRunContextFactory {
 
-    private static final int CPCT_PATIENT_NAME_LENGTH = 12;
     private static final String CPCT_PATIENT_IDENTIFIER = "_CPCT";
+    private static final int CPCT_PATIENT_NAME_LENGTH = 12;
     private static final String CPCT_REF_SAMPLE_SUFFIX = "R";
     private static final String CPCT_TUMOR_SAMPLE_SUFFIX = "T";
 
-    //    private static final int DRUP_PATIENT_NAME_LENGTH = 12;
-    //    private static final String DRUP_PATIENT_IDENTIFIER = "_DRUP";
-    //    private static final String DRUP_REF_SAMPLE_SUFFIX = "R";
-    //    private static final String DRUP_TUMOR_SAMPLE_SUFFIX = "T";
+    private static final String DRUP_PATIENT_IDENTIFIER = "_DRUP";
+    private static final int DRUP_PATIENT_NAME_LENGTH = 12;
+    private static final String DRUP_REF_SAMPLE_SUFFIX = "R";
+    private static final String DRUP_TUMOR_SAMPLE_SUFFIX = "T";
 
     private ProductionRunContextFactory() {
     }
 
     @NotNull
     public static RunContext fromRunDirectory(@NotNull final String runDirectory) throws HartwigException {
-        final String fullRunName = removePath(runDirectory);
+        final String runName = removePath(runDirectory);
 
-        if (isCPCTRun(fullRunName)) {
-            final int patientPosition = fullRunName.indexOf(CPCT_PATIENT_IDENTIFIER) + 1;
-            if (fullRunName.length() < (patientPosition + CPCT_PATIENT_NAME_LENGTH)) {
-                throw new MalformedRunDirException(fullRunName);
-            }
-
-            final String patient = fullRunName.substring(patientPosition, patientPosition + CPCT_PATIENT_NAME_LENGTH);
-            final String runName = fullRunName.substring(0, patientPosition + CPCT_PATIENT_NAME_LENGTH);
-
-            final boolean hasPassedTests = fullRunName.length() == runName.length();
-
-            final File[] runContents = new File(runDirectory).listFiles();
-            assert runContents != null;
-
-            String refSample = null;
-            String tumorSample = null;
-            for (final File content : runContents) {
-                if (content.isDirectory() && content.getName().contains(patient)) {
-                    if (content.getName().contains(CPCT_REF_SAMPLE_SUFFIX)) {
-                        refSample = content.getName();
-                    } else if (content.getName().contains(CPCT_TUMOR_SAMPLE_SUFFIX)) {
-                        tumorSample = content.getName();
-                    }
-                }
-            }
-
-            if (refSample == null || tumorSample == null) {
-                throw new MalformedRunDirException(fullRunName);
-            }
-            return new RunContextImpl(runDirectory, runName, refSample, tumorSample, hasPassedTests);
+        if (isCPCTRun(runName)) {
+            return somatic(runName, runDirectory, CPCT_PATIENT_IDENTIFIER, CPCT_PATIENT_NAME_LENGTH,
+                    CPCT_REF_SAMPLE_SUFFIX, CPCT_TUMOR_SAMPLE_SUFFIX);
+        } else if (isDRUPRun(runName)) {
+            return somatic(runName, runDirectory, DRUP_PATIENT_IDENTIFIER, DRUP_PATIENT_NAME_LENGTH,
+                    DRUP_REF_SAMPLE_SUFFIX, DRUP_TUMOR_SAMPLE_SUFFIX);
         } else {
-            throw new HartwigException("CPCT is the only supported type of analysis for the health checker, sorry!");
+            throw new HartwigException("Currently only somatic pipelines from CPCT or DRUP are supported!");
         }
     }
 
-    private static boolean isCPCTRun(@NotNull final String fullRunName) {
-        return fullRunName.contains(CPCT_PATIENT_IDENTIFIER);
+    private static boolean isCPCTRun(@NotNull final String runName) {
+        return runName.contains(CPCT_PATIENT_IDENTIFIER);
+    }
+
+    private static boolean isDRUPRun(@NotNull final String runName) {
+        return runName.contains(DRUP_PATIENT_IDENTIFIER);
+    }
+
+    @NotNull
+    private static RunContext somatic(@NotNull final String runName, @NotNull final String runDirectory,
+            @NotNull final String patientIdentifier, final int patientNameLength,
+            @NotNull final String refSampleSuffix, @NotNull final String tumorSampleSuffix)
+            throws MalformedRunDirException {
+        final int patientPosition = runName.indexOf(patientIdentifier) + 1;
+        assert patientPosition > 0;
+
+        if (runName.length() < (patientPosition + patientNameLength)) {
+            throw new MalformedRunDirException(runName);
+        }
+
+        final String patient = runName.substring(patientPosition, patientPosition + patientNameLength);
+        final String runNameUntilPatient = runName.substring(0, patientPosition + patientNameLength);
+
+        final boolean hasPassedTests = runName.length() == runNameUntilPatient.length();
+
+        final File[] runContents = new File(runDirectory).listFiles();
+        assert runContents != null;
+
+        String refSample = null;
+        String tumorSample = null;
+        for (final File content : runContents) {
+            if (content.isDirectory() && content.getName().contains(patient)) {
+                if (content.getName().substring(patientNameLength).contains(refSampleSuffix)) {
+                    refSample = content.getName();
+                } else if (content.getName().substring(patientNameLength).contains(tumorSampleSuffix)) {
+                    tumorSample = content.getName();
+                }
+            }
+        }
+
+        if (refSample == null || tumorSample == null) {
+            throw new MalformedRunDirException(runName);
+        }
+
+        return new RunContextImpl(runDirectory, runNameUntilPatient, refSample, tumorSample, hasPassedTests);
     }
 
     @NotNull
--- health-checker/src/test/java/com/hartwig/hmftools/healthchecker/context/ProductionRunContextFactoryTest.java
+++ health-checker/src/test/java/com/hartwig/hmftools/healthchecker/context/ProductionRunContextFactoryTest.java
@@ -12,47 +12,62 @@ import org.junit.Test;
 
 public class ProductionRunContextFactoryTest {
 
-    private static final String RESOURCE_DIR = "CPCTRunContextFactory";
+    private static final String RESOURCE_DIR = "ProductionRunContextFactory";
 
-    private static final String VALID_RUNDIR = "160101_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
-    private static final String VALID_RUN_NAME = VALID_RUNDIR;
-    private static final String VALID_REF = "CPCT12345678R";
-    private static final String VALID_TUMOR = "CPCT12345678TII";
+    private static final String VALID_CPCT_RUNDIR = "160101_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
+    private static final String VALID_CPCT_RUN_NAME = VALID_CPCT_RUNDIR;
+    private static final String VALID_CPCT_REF = "CPCT12345678R";
+    private static final String VALID_CPCT_TUMOR = "CPCT12345678TII";
 
-    private static final String LOW_QUAL_RUNDIR = "160102_HMFregCPCT_FR10002000_FR20003000_CPCT12345678_LowQual";
-    private static final String LOW_QUAL_RUN_NAME = "160102_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
-    private static final String LOW_QUAL_REF = "CPCT12345678R";
-    private static final String LOW_QUAL_TUMOR = "CPCT12345678T";
+    private static final String VALID_DRUP_RUNDIR = "170101_HMFregDRUP_FR10002000_FR10012001_DRUP01020005";
+    private static final String VALID_DRUP_RUN_NAME = VALID_DRUP_RUNDIR;
+    private static final String VALID_DRUP_REF = "DRUP01020005R";
+    private static final String VALID_DRUP_TUMOR = "DRUP01020005T";
 
-    private static final String INVALID_PATIENT_RUNDIR = "160103_HMFregCPCT_FR10002000_FR20003000_CPCT1234";
-    private static final String MISSING_REF_RUNDIR = "160104_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
+    private static final String LOW_QUAL_CPCT_RUNDIR = "160102_HMFregCPCT_FR10002000_FR20003000_CPCT12345678_LowQual";
+    private static final String LOW_QUAL_CPCT_RUN_NAME = "160102_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
+    private static final String LOW_QUAL_CPCT_REF = "CPCT12345678R";
+    private static final String LOW_QUAL_CPCT_TUMOR = "CPCT12345678T";
+
+    private static final String INVALID_CPCT_PATIENT_RUNDIR = "160103_HMFregCPCT_FR10002000_FR20003000_CPCT1234";
+    private static final String MISSING_REF_CPCT_RUNDIR = "160104_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
 
     @Test
-    public void resolveCorrectlyForValidRunWithTII() throws HartwigException {
-        final RunContext runContext = ProductionRunContextFactory.fromRunDirectory(toPath(VALID_RUNDIR));
-        assertEquals(VALID_REF, runContext.refSample());
-        assertEquals(VALID_TUMOR, runContext.tumorSample());
-        assertEquals(VALID_RUN_NAME, runContext.runName());
+    public void alsoWorksForDRUP() throws HartwigException {
+        final RunContext runContext = ProductionRunContextFactory.fromRunDirectory(toPath(VALID_DRUP_RUNDIR));
+        assertEquals(VALID_DRUP_REF, runContext.refSample());
+        assertEquals(VALID_DRUP_TUMOR, runContext.tumorSample());
+        assertEquals(VALID_DRUP_RUN_NAME, runContext.runName());
         assertEquals(true, runContext.hasPassedTests());
     }
 
     @Test
-    public void resolveCorrectlyForLowQualRun() throws HartwigException {
-        final RunContext runContextLowQual = ProductionRunContextFactory.fromRunDirectory(toPath(LOW_QUAL_RUNDIR));
-        assertEquals(LOW_QUAL_REF, runContextLowQual.refSample());
-        assertEquals(LOW_QUAL_TUMOR, runContextLowQual.tumorSample());
-        assertEquals(LOW_QUAL_RUN_NAME, runContextLowQual.runName());
+    public void resolveCorrectlyForValidCPCTRunWithTII() throws HartwigException {
+        final RunContext runContext = ProductionRunContextFactory.fromRunDirectory(toPath(VALID_CPCT_RUNDIR));
+        assertEquals(VALID_CPCT_REF, runContext.refSample());
+        assertEquals(VALID_CPCT_TUMOR, runContext.tumorSample());
+        assertEquals(VALID_CPCT_RUN_NAME, runContext.runName());
+        assertEquals(true, runContext.hasPassedTests());
+    }
+
+    @Test
+    public void resolveCorrectlyForLowQualCPCTRun() throws HartwigException {
+        final RunContext runContextLowQual = ProductionRunContextFactory.fromRunDirectory(
+                toPath(LOW_QUAL_CPCT_RUNDIR));
+        assertEquals(LOW_QUAL_CPCT_REF, runContextLowQual.refSample());
+        assertEquals(LOW_QUAL_CPCT_TUMOR, runContextLowQual.tumorSample());
+        assertEquals(LOW_QUAL_CPCT_RUN_NAME, runContextLowQual.runName());
         assertEquals(false, runContextLowQual.hasPassedTests());
     }
 
     @Test(expected = MalformedRunDirException.class)
-    public void exceptionOnRunDirWithTooShortPatientName() throws HartwigException {
-        ProductionRunContextFactory.fromRunDirectory(toPath(INVALID_PATIENT_RUNDIR));
+    public void exceptionOnCPCTRunDirWithTooShortPatientName() throws HartwigException {
+        ProductionRunContextFactory.fromRunDirectory(toPath(INVALID_CPCT_PATIENT_RUNDIR));
     }
 
     @Test(expected = MalformedRunDirException.class)
-    public void exceptionOnRunDirWithMissingRef() throws HartwigException {
-        ProductionRunContextFactory.fromRunDirectory(toPath(MISSING_REF_RUNDIR));
+    public void exceptionOnCPCTRunDirWithMissingRef() throws HartwigException {
+        ProductionRunContextFactory.fromRunDirectory(toPath(MISSING_REF_CPCT_RUNDIR));
     }
 
     @NotNull
