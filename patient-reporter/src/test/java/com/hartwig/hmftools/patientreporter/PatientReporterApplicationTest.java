bfef2c6453cea47feb1855ebe4641610fa28f726
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-01-19 11:51:17 +0100

    Added importing the slicing annotations from HMF bed file

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/PatientReporterApplicationTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/PatientReporterApplicationTest.java
@@ -1,10 +1,14 @@
 package com.hartwig.hmftools.patientreporter;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
 import java.io.IOException;
 
 import com.google.common.io.Resources;
 import com.hartwig.hmftools.common.exception.HartwigException;
+import com.hartwig.hmftools.patientreporter.slicing.GenomeRegion;
+import com.hartwig.hmftools.patientreporter.slicing.HMFSlicingAnnotation;
 import com.hartwig.hmftools.patientreporter.slicing.Slicer;
 import com.hartwig.hmftools.patientreporter.slicing.SlicerFactory;
 
@@ -17,8 +21,13 @@ public class PatientReporterApplicationTest {
 
     @Test
     public void canRunOnRunDirectory() throws IOException, HartwigException {
-        final String bedFile = BED_DIRECTORY + File.separator + "valid.bed";
+        final String bedFile = BED_DIRECTORY + File.separator + "HMF_Slicing.bed";
         final Slicer slicer = SlicerFactory.fromBedFile(bedFile);
+        // KODU: Every region in the HMF slicing bed should be convertable to a slicing annotation!
+        for (final GenomeRegion region : slicer.regions()) {
+            assertNotNull(HMFSlicingAnnotation.fromGenomeRegion(region));
+        }
+
         final ConsensusRule consensusRule = new ConsensusRule(slicer, slicer);
 
         new PatientReporterApplication(RUN_DIRECTORY, consensusRule, slicer, null, false).run();
---
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/slicing/HMFSlicingAnnotationTest.java
@@ -0,0 +1,31 @@
+package com.hartwig.hmftools.patientreporter.slicing;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.apache.logging.log4j.util.Strings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.junit.Test;
+
+public class HMFSlicingAnnotationTest {
+
+    @Test
+    public void canCreateFromGenomeRegion() {
+        assertNull(HMFSlicingAnnotation.fromGenomeRegion(toRegion(null)));
+        assertNull(HMFSlicingAnnotation.fromGenomeRegion(toRegion("korneel")));
+        assertNull(HMFSlicingAnnotation.fromGenomeRegion(toRegion("hello world")));
+
+        HMFSlicingAnnotation annotation = HMFSlicingAnnotation.fromGenomeRegion(toRegion("ENST00000361445.4 (MTOR)"));
+        assertNotNull(annotation);
+        assertEquals("ENST00000361445", annotation.transcriptID());
+        assertEquals(4, annotation.transcriptVersion());
+        assertEquals("MTOR", annotation.gene());
+    }
+
+    @NotNull
+    private static GenomeRegion toRegion(@Nullable final String annotation) {
+        return new GenomeRegion(Strings.EMPTY, 0L, 0L, annotation);
+    }
+}
