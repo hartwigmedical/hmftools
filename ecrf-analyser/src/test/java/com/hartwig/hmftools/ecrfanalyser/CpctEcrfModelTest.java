bff2f8bb7dd6ae40dcd368f5a4aba1a5348c5691
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-01-28 12:10:22 +0100

    Refactored test

---
+++ ecrf-analyser/src/test/java/com/hartwig/hmftools/ecrfanalyser/CpctEcrfModelTest.java
@@ -0,0 +1,53 @@
+package com.hartwig.hmftools.ecrfanalyser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import javax.xml.stream.XMLStreamException;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
+import com.hartwig.hmftools.ecrfanalyser.datamodel.EcrfPatient;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CpctEcrfModelTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(CpctEcrfModel.class);
+
+    // KODU: This file does not exist in github for privacy reasons.
+    private static final String TEST_ECRF = Resources.getResource("example").getPath() + File.separator + "ecrf.xml";
+
+    @Test
+    public void loadDataFromRealXML() throws IOException, XMLStreamException {
+        if (Files.exists(new File(TEST_ECRF).toPath())) {
+            final CpctEcrfModel model = CpctEcrfModel.loadFromXML(TEST_ECRF);
+            assertTrue(hasField(model, "BASELINE.CARCINOMA.CARCINOMA.PTUMLOC"));
+            assertFalse(hasField(model, "Does Not Exist"));
+
+            assertTrue(hasPatient(model, "CPCT02252500"));
+            assertFalse(hasPatient(model, "Does Not Exist"));
+        } else {
+            LOGGER.warn("Could not run CpctEcrfModelTest since test xml is not present: " + TEST_ECRF);
+        }
+    }
+
+    private static boolean hasField(@NotNull final CpctEcrfModel model, @NotNull final String fieldId) {
+        return Lists.newArrayList(model.findFieldsById(Lists.newArrayList(fieldId))).size() > 0;
+    }
+
+    private static boolean hasPatient(@NotNull final CpctEcrfModel model, @NotNull final String patientId) {
+        final List<EcrfPatient> patients =  Lists.newArrayList(model.findPatientsById(Lists.newArrayList(patientId)));
+        return !patients.get(0).fields().isEmpty();
+    }
+}
