452c8a6ccd2818c359558fc18352ad9b9ec3f5ce
Mircea Voda <mircearaul.voda@gmail.com>
2017-03-10 14:32:32 +0100

    cleanup fastq-stats tests

--- fastq-stats/src/test/java/com/hartwig/hmftools/fastqstats/FastqStatsTest.java
+++ fastq-stats/src/test/java/com/hartwig/hmftools/fastqstats/FastqStatsTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+
+import com.google.common.io.Resources;
 
 import org.junit.Test;
 
@@ -11,13 +14,14 @@ public class FastqStatsTest {
     // 100% of baseCalls have a quality score >30
     @Test
     public void computesCorrectStatsFor100PercentDir() throws IOException {
-        File dir = new File("./src/test/resources/DeterminedBaseCalls-100");
-        FastqTracker tracker = FastqStats.processDir(dir);
+        final URL path = Resources.getResource("DeterminedBaseCalls-100");
+        final File dir = new File(path.getPath());
+        final FastqTracker tracker = FastqStats.processDir(dir);
         assertEquals(300, tracker.getFlowcellData().getYield());
-        double q30Percentage = tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield();
+        final double q30Percentage = tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield();
         assertEquals(100.0, q30Percentage, 0.00001);
         assertEquals(300, tracker.getLaneData("L001").getYield());
-        double laneQ30Percentage =
+        final double laneQ30Percentage =
                 tracker.getLaneData("L001").getQ30() * 100.0 / tracker.getLaneData("L001").getYield();
         assertEquals(100.0, laneQ30Percentage, 0.00001);
     }
@@ -25,20 +29,21 @@ public class FastqStatsTest {
     // Sample Z has 50% Q30, others 100%
     @Test
     public void computesCorrectStatsForIndividualSamples() throws IOException {
-        File dir = new File("./src/test/resources/DeterminedBaseCalls-100&50");
-        FastqTracker tracker = FastqStats.processDir(dir);
+        final URL path = Resources.getResource("DeterminedBaseCalls-100&50");
+        final File dir = new File(path.getPath());
+        final FastqTracker tracker = FastqStats.processDir(dir);
         assertEquals(300, tracker.getFlowcellData().getYield());
-        double q30Percentage = tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield();
+        final double q30Percentage = tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield();
         assertEquals(83.33, q30Percentage, 0.01);
         assertEquals(300, tracker.getLaneData("L001").getYield());
-        double laneQ30Percentage =
+        final double laneQ30Percentage =
                 tracker.getLaneData("L001").getQ30() * 100.0 / tracker.getLaneData("L001").getYield();
         assertEquals(83.33, laneQ30Percentage, 0.01);
-        double sampleXQ30 =
+        final double sampleXQ30 =
                 tracker.getSampleData("TESTX").getQ30() * 100.0 / tracker.getSampleData("TESTX").getYield();
-        double sampleYQ30 =
+        final double sampleYQ30 =
                 tracker.getSampleData("TESTY").getQ30() * 100.0 / tracker.getSampleData("TESTY").getYield();
-        double sampleZQ30 =
+        final double sampleZQ30 =
                 tracker.getSampleData("TESTZ").getQ30() * 100.0 / tracker.getSampleData("TESTZ").getYield();
         assertEquals(100.0, sampleXQ30, 0.00001);
         assertEquals(100.0, sampleYQ30, 0.00001);
@@ -48,48 +53,47 @@ public class FastqStatsTest {
     // 50% of baseCalls belong to undetermined sample
     @Test
     public void computesCorrectPercentageOfUndetermined50() throws IOException {
-        File dir = new File("./src/test/resources/50UndeterminedBaseCalls");
-        FastqTracker tracker = FastqStats.processDir(dir);
+        final URL path = Resources.getResource("50UndeterminedBaseCalls");
+        final File dir = new File(path.getPath());
+        final FastqTracker tracker = FastqStats.processDir(dir);
         assertEquals(600, tracker.getFlowcellData().getYield());
-        double q30Percentage = tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield();
+        final double q30Percentage = tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield();
         assertEquals(100.0, q30Percentage, 0.00001);
-        double undeterminedPercentage =
+        final double undeterminedPercentage =
                 tracker.getUndeterminedData().getYield() * 100.0 / tracker.getFlowcellData().getYield();
         assertEquals(50.0, undeterminedPercentage, 0.00001);
         assertEquals(600, tracker.getLaneData("L001").getYield());
-        FastqStatsRunner.printOutput(tracker);
     }
 
     // 100% of baseCalls belong to undetermined sample
     @Test
     public void computesCorrectPercentageOfUndetermined100() throws IOException {
-        File dir = new File("./src/test/resources/100UndeterminedBaseCalls");
-        FastqTracker tracker = FastqStats.processDir(dir);
+        final URL path = Resources.getResource("100UndeterminedBaseCalls");
+        final File dir = new File(path.getPath());
+        final FastqTracker tracker = FastqStats.processDir(dir);
         assertEquals(300, tracker.getFlowcellData().getYield());
         assertEquals(100.0, tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield(),
                 0.00001);
-        double undeterminedPercentage =
+        final double undeterminedPercentage =
                 tracker.getUndeterminedData().getYield() * 100.0 / tracker.getFlowcellData().getYield();
         assertEquals(100.0, undeterminedPercentage, 0.00001);
         assertEquals(300, tracker.getLaneData("L002").getYield());
-        FastqStatsRunner.printOutput(tracker);
     }
 
     // 50% of baseCalls are undetermined and all undetermined have Q30 of 0
     @Test
     public void computesCorrectStatsOfUndeterminedLane() throws IOException {
-        File dir = new File("./src/test/resources/UndeterminedBaseCalls0Q30");
-        FastqTracker tracker = FastqStats.processDir(dir);
+        final URL path = Resources.getResource("UndeterminedBaseCalls0Q30");
+        final File dir = new File(path.getPath());
+        final FastqTracker tracker = FastqStats.processDir(dir);
         assertEquals(600, tracker.getFlowcellData().getYield());
         assertEquals(50.0, tracker.getFlowcellData().getQ30() * 100.0 / tracker.getFlowcellData().getYield(), 0.00001);
-        double undeterminedPercentage =
+        final double undeterminedPercentage =
                 tracker.getUndeterminedData().getYield() * 100.0 / tracker.getFlowcellData().getYield();
         assertEquals(50.0, undeterminedPercentage, 0.00001);
-        double undeterminedQ30 =
+        final double undeterminedQ30 =
                 tracker.getUndeterminedData().getQ30() * 100.0 / tracker.getUndeterminedData().getYield();
         assertEquals(0.0, undeterminedQ30, 0.00001);
         assertEquals(300, tracker.getLaneData("L002").getYield());
-        FastqStatsRunner.printOutput(tracker);
     }
-
 }
--- fastq-stats/src/test/java/com/hartwig/hmftools/fastqstats/FastqTrackerTest.java
+++ fastq-stats/src/test/java/com/hartwig/hmftools/fastqstats/FastqTrackerTest.java
@@ -5,36 +5,42 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+
+import com.google.common.io.Resources;
 
 import org.junit.Test;
 
 public class FastqTrackerTest {
     @Test
     public void computesCorrectPercentageFor100PercentFile() throws IOException {
-        FileInputStream fin = new FileInputStream(new File("./src/test/resources/fastq/q30-100.fastq"));
-        FastqReader fr = new FastqReader(fin);
-        FastqData data = fr.read();
-        double q30Percentage = data.getQ30() * 100.0 / data.getYield();
+        final URL path = Resources.getResource("fastq/q30-100.fastq");
+        final FileInputStream fin = new FileInputStream(new File(path.getPath()));
+        final FastqReader fr = new FastqReader(fin);
+        final FastqData data = fr.read();
+        final double q30Percentage = data.getQ30() * 100.0 / data.getYield();
         assertEquals(100, data.getYield());
         assertEquals(100.0, q30Percentage, 0.00001);
     }
 
     @Test
     public void computesCorrectPercentageFor0PercentFile() throws IOException {
-        FileInputStream fin = new FileInputStream(new File("./src/test/resources/fastq/q30-0.fastq"));
-        FastqReader fr = new FastqReader(fin);
-        FastqData data = fr.read();
-        double q30Percentage = data.getQ30() * 100.0 / data.getYield();
+        final URL path = Resources.getResource("fastq/q30-0.fastq");
+        final FileInputStream fin = new FileInputStream(new File(path.getPath()));
+        final FastqReader fr = new FastqReader(fin);
+        final FastqData data = fr.read();
+        final double q30Percentage = data.getQ30() * 100.0 / data.getYield();
         assertEquals(100, data.getYield());
         assertEquals(0.0, q30Percentage, 0.00001);
     }
 
     @Test
     public void computesCorrectPercentageFor10PercentFile() throws IOException {
-        FileInputStream fin = new FileInputStream(new File("./src/test/resources/fastq/q30-10.fastq"));
-        FastqReader fr = new FastqReader(fin);
-        FastqData data = fr.read();
-        double q30Percentage = data.getQ30() * 100.0 / data.getYield();
+        final URL path = Resources.getResource("fastq/q30-10.fastq");
+        final FileInputStream fin = new FileInputStream(new File(path.getPath()));
+        final FastqReader fr = new FastqReader(fin);
+        final FastqData data = fr.read();
+        final double q30Percentage = data.getQ30() * 100.0 / data.getYield();
         assertEquals(100, data.getYield());
         assertEquals(10.0, q30Percentage, 0.00001);
     }
