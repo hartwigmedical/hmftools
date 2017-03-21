42afe309aad48a1ae06bcde00fe2a2a9cef6db10
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-03-21 11:17:17 +0100

    Remove retention checker, not used anymore

--- retention-checker/src/main/java/com/hartwig/hmftools/retentionchecker/FastqHeaderNormalizer.java
+++
@@ -1,6 +0,0 @@
-package com.hartwig.hmftools.retentionchecker;
-
-import java.util.function.Function;
-
-interface FastqHeaderNormalizer extends Function<String, String> {
-}
--- retention-checker/src/main/java/com/hartwig/hmftools/retentionchecker/FileNameConverter.java
+++
@@ -1,7 +0,0 @@
-package com.hartwig.hmftools.retentionchecker;
-
-import java.util.function.Function;
-
-interface FileNameConverter extends Function<String, String> {
-
-}
