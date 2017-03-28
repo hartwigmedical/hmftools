fb2d60444449274ada0b412e6f496623a3f50d56
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-03-28 10:24:32 +0200

    Added implementation for resolving run context on meta data

--- health-checker/src/main/java/com/hartwig/hmftools/healthchecker/context/MetaDataResolver.java
+++ health-checker/src/main/java/com/hartwig/hmftools/healthchecker/context/MetaDataResolver.java
@@ -1,13 +1,43 @@
 package com.hartwig.hmftools.healthchecker.context;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 final class MetaDataResolver {
+
+    private static final String METADATA_FILE = "metadata";
+
+    private static final String REF_SAMPLE_FIELD = "ref_sample";
+    private static final String TUMOR_SAMPLE_FIELD = "tumor_sample";
+    private static final String SET_NAME_FIELD = "set_name";
+
+    private static final String NO_TUMOR_SAMPLE = "NA";
+
+    private static final Gson GSON = new GsonBuilder().create();
 
     private MetaDataResolver() {
     }
 
-    static RunContext fromMetaDataFile(@NotNull final String metaDataFilePath) {
+    @Nullable
+    static RunContext fromMetaDataFile(@NotNull final String runDirectory) throws FileNotFoundException {
+        final String metaDataFilePath = runDirectory + File.separator + METADATA_FILE;
+        if (new File(metaDataFilePath).exists()) {
+            final JsonObject json = GSON.fromJson(new FileReader(metaDataFilePath), JsonObject.class);
+            final String refSample = json.get(REF_SAMPLE_FIELD).getAsString();
+            final String tumorSample = json.get(TUMOR_SAMPLE_FIELD).getAsString();
+            final String setName = json.get(SET_NAME_FIELD).getAsString();
+
+            final boolean isSomaticRun = !tumorSample.equals(NO_TUMOR_SAMPLE);
+            return new RunContextImpl(runDirectory, setName, refSample, tumorSample, true, isSomaticRun);
+        }
         return null;
     }
 }
---
+++ health-checker/src/test/java/com/hartwig/hmftools/healthchecker/context/MetaDataResolverTest.java
@@ -0,0 +1,52 @@
+package com.hartwig.hmftools.healthchecker.context;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.google.common.io.Resources;
+
+import org.junit.Test;
+
+public class MetaDataResolverTest {
+
+    private static final String RESOURCE_DIR = Resources.getResource("MetaDataResolver").getPath();
+
+    @Test
+    public void noMetaDataReturnsNull() throws FileNotFoundException {
+        final String noMetaDataRunDir = RESOURCE_DIR + File.separator + "RunDirNoMetaData";
+        assertNull(MetaDataResolver.fromMetaDataFile(noMetaDataRunDir));
+    }
+
+    @Test
+    public void canResolveSingleSampleMetaData() throws FileNotFoundException {
+        final String runName = "RunDirSingleSample";
+        final String singleSampleRunDir = RESOURCE_DIR + File.separator + runName;
+        final RunContext runContext = MetaDataResolver.fromMetaDataFile(singleSampleRunDir);
+
+        assertNotNull(runContext);
+        assertFalse(runContext.isSomaticRun());
+        assertEquals("GIAB", runContext.refSample());
+        assertEquals(runName, runContext.runName());
+        assertEquals(singleSampleRunDir, runContext.runDirectory());
+    }
+
+    @Test
+    public void canResolveSomaticMetaData() throws FileNotFoundException {
+        final String runName = "RunDirSomatic";
+        final String somaticRunDir = RESOURCE_DIR + File.separator + runName;
+        final RunContext runContext = MetaDataResolver.fromMetaDataFile(somaticRunDir);
+
+        assertNotNull(runContext);
+        assertTrue(runContext.isSomaticRun());
+        assertEquals("CPCT12345678R", runContext.refSample());
+        assertEquals("CPCT12345678T", runContext.tumorSample());
+        assertEquals(runName, runContext.runName());
+        assertEquals(somaticRunDir, runContext.runDirectory());
+    }
+}
