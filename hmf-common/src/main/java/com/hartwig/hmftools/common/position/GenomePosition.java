b9106778269d7ec9f24ab6056f4c053000891cb6
Jonathan Baber <jonbaber@gmail.com>
2017-05-05 14:42:06 +1000

    DEV-11: GenomePosition Sorting

--- hmf-common/src/main/java/com/hartwig/hmftools/common/position/GenomePosition.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/position/GenomePosition.java
@@ -1,11 +1,21 @@
 package com.hartwig.hmftools.common.position;
 
+import com.hartwig.hmftools.common.chromosome.Chromosomes;
 import org.jetbrains.annotations.NotNull;
 
-public interface GenomePosition {
+public interface GenomePosition extends Comparable<GenomePosition> {
 
     @NotNull
     String chromosome();
 
     long position();
+
+    @Override
+    default int compareTo(@NotNull GenomePosition other) {
+        if (chromosome().equals(other.chromosome())) {
+            return Long.compare(position(), other.position());
+        }
+
+        return Integer.compare(Chromosomes.asInt(chromosome()), Chromosomes.asInt(other.chromosome()));
+    }
 }
