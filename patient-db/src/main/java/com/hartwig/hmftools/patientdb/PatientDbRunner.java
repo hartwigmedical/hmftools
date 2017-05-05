42a948f01a760feff16f1c2c667824dcff0b63fa
Mircea Voda <mircearaul.voda@gmail.com>
2017-05-05 17:17:25 +0200

    update patientDbRunner

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/PatientDbRunner.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/PatientDbRunner.java
@@ -4,17 +4,22 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.xml.stream.XMLStreamException;
 
+import com.google.common.collect.Maps;
 import com.hartwig.hmftools.common.ecrf.CpctEcrfModel;
 import com.hartwig.hmftools.common.ecrf.datamodel.EcrfPatient;
+import com.hartwig.hmftools.common.exception.EmptyFileException;
 import com.hartwig.hmftools.common.exception.HartwigException;
+import com.hartwig.hmftools.common.io.reader.FileReader;
 import com.hartwig.hmftools.common.slicing.Slicer;
 import com.hartwig.hmftools.common.slicing.SlicerFactory;
 import com.hartwig.hmftools.common.variant.consensus.ConsensusRule;
-import com.hartwig.hmftools.patientdb.data.CpctRunData;
+import com.hartwig.hmftools.healthchecker.context.RunContext;
 import com.hartwig.hmftools.patientdb.data.Patient;
 import com.hartwig.hmftools.patientdb.readers.CpctPatientReader;
 import com.hartwig.hmftools.patientdb.readers.RunsFolderReader;
@@ -41,6 +46,8 @@ public final class PatientDbRunner {
     private static final String HIGH_CONFIDENCE_BED = "high_confidence_bed";
     private static final String EXTREME_CONFIDENCE_BED = "extreme_confidence_bed";
     private static final String TREATMENT_TYPES_CSV = "treatment_types_csv";
+    private static final String LIMS_CSV = "lims_csv";
+    private static final String LIMS_OLD_CSV = "lims_old_csv";
 
     public static void main(@NotNull final String[] args)
             throws ParseException, IOException, InterruptedException, java.text.ParseException, XMLStreamException,
@@ -56,18 +63,18 @@ public final class PatientDbRunner {
         final String highConfidenceBed = cmd.getOptionValue(HIGH_CONFIDENCE_BED);
         final String extremeConfidenceBed = cmd.getOptionValue(EXTREME_CONFIDENCE_BED);
         final String treatmentMappingCsv = cmd.getOptionValue(TREATMENT_TYPES_CSV);
+        final String limsCsv = cmd.getOptionValue(LIMS_CSV);
+        final String limsOldCsv = cmd.getOptionValue(LIMS_OLD_CSV);
 
         ThreadContext.put("cpctHospitalCode", "default");
         if (Utils.anyNull(runsFolderPath, ecrfFilePath, userName, password, databaseUrl, highConfidenceBed,
-                extremeConfidenceBed, treatmentMappingCsv)) {
+                extremeConfidenceBed, treatmentMappingCsv, limsCsv, limsOldCsv)) {
             final HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("Patient-Db", options);
         } else {
             final File runDirectory = new File(runsFolderPath);
             if (runDirectory.isDirectory()) {
-                final List<CpctRunData> runDatas = RunsFolderReader.getPatientRunsData(runDirectory);
-                LOGGER.info("Listing data for " + runDatas.size() + " patients.");
-                runDatas.stream().map(CpctRunData::patientId).forEach(LOGGER::info);
+                final List<RunContext> runsContexts = RunsFolderReader.getRunContexts(runDirectory);
                 LOGGER.info("Loading ecrf model...");
                 final CpctEcrfModel model = CpctEcrfModel.loadFromXML(ecrfFilePath);
                 final Slicer highConfidenceSlicer = SlicerFactory.fromBedFile(highConfidenceBed);
@@ -75,19 +82,23 @@ public final class PatientDbRunner {
                 final ConsensusRule consensusRule = ConsensusRule.fromSlicers(highConfidenceSlicer,
                         extremeConfidenceSlicer);
                 final CpctPatientReader cpctPatientReader = new CpctPatientReader(model, consensusRule,
-                        treatmentMappingCsv);
+                        readTreatmentMappingFile(treatmentMappingCsv), limsCsv, limsOldCsv);
                 final DatabaseWriter dbWriter = new DatabaseWriter(userName, password, jdbcUrl);
                 dbWriter.clearTables();
-                final List<CpctRunData> cpctRunDatas = runDatas.stream().filter(
-                        cpctRunData -> cpctRunData.patientId().startsWith("CPCT")).collect(Collectors.toList());
-                LOGGER.info("Reading CPCT patient data...");
-                for (final CpctRunData cpctRunData : cpctRunDatas) {
-                    final EcrfPatient patient = model.findPatientById(cpctRunData.patientId());
+                final Set<String> cpctPatientIds = runsContexts.stream().map(
+                        runContext -> getPatientId(runContext.setName())).filter(
+                        setName -> setName.startsWith("CPCT")).collect(Collectors.toSet());
+                LOGGER.info("Reading CPCT patient data for " + cpctPatientIds.size() + " patients.");
+                for (final String patientId : cpctPatientIds) {
+                    final EcrfPatient patient = model.findPatientById(patientId);
                     if (patient == null) {
-                        LOGGER.warn("Could not find patient with id: " + cpctRunData.patientId() + " in ecrf file.");
+                        LOGGER.warn("Could not find patient with id: " + patientId + " in ecrf file.");
                     } else {
-                        final Patient cpctPatient = cpctPatientReader.read(patient,
-                                runsFolderPath + File.separator + cpctRunData.folderName());
+                        final List<String> sampleIdsForPatient = runsContexts.stream().map(
+                                RunContext::tumorSample).filter(
+                                sampleId -> sampleId.startsWith(patient.patientId())).collect(Collectors.toList());
+                        LOGGER.info("Samples for patient " + patient.patientId() + ": " + sampleIdsForPatient);
+                        final Patient cpctPatient = cpctPatientReader.read(patient, sampleIdsForPatient);
                         dbWriter.writePatient(cpctPatient);
                     }
                 }
@@ -113,6 +124,8 @@ public final class PatientDbRunner {
         options.addOption(EXTREME_CONFIDENCE_BED, true, "The full path towards the extreme confidence bed");
         options.addOption(TREATMENT_TYPES_CSV, true,
                 "Path towards the .csv file that maps treatment names to treatment types");
+        options.addOption(LIMS_CSV, true, "Path towards the LIMS .csv file");
+        options.addOption(LIMS_OLD_CSV, true, "Path towards the LIMS-old .csv file");
         return options;
     }
 
@@ -121,5 +134,26 @@ public final class PatientDbRunner {
             throws ParseException {
         final CommandLineParser parser = new DefaultParser();
         return parser.parse(options, args);
+    }
+
+    @NotNull
+    private static String getPatientId(@NotNull final String runName) {
+        final String[] names = runName.split("_");
+        return names[4];
+    }
+
+    @NotNull
+    private static Map<String, String> readTreatmentMappingFile(@NotNull final String treatmentMappingCsv)
+            throws IOException, EmptyFileException {
+        final Map<String, String> treatmentMapping = Maps.newHashMap();
+        FileReader.build().readLines(new File(treatmentMappingCsv).toPath()).forEach(line -> {
+            final String[] parts = line.split(",");
+            if (parts.length == 2) {
+                treatmentMapping.put(parts[0].toLowerCase().trim(), parts[1].toLowerCase().trim());
+            } else {
+                LOGGER.warn("Invalid row found in treatment mapping csv: " + line);
+            }
+        });
+        return treatmentMapping;
     }
 }
