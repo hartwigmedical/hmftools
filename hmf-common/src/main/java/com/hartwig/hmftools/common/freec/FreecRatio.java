7845414be48ea108a1ae4b27b39720b301770398
Jonathan Baber <jonbaber@gmail.com>
2017-05-19 09:51:38 +1000

    DEV-11: Add estimatedBAF to FreecRatio

--- hmf-common/src/main/java/com/hartwig/hmftools/common/freec/FreecRatio.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/freec/FreecRatio.java
@@ -1,11 +1,14 @@
 package com.hartwig.hmftools.common.freec;
 
 import com.hartwig.hmftools.common.position.GenomePosition;
+
 import org.immutables.value.Value;
 
 @Value.Style(allParameters = true)
 @Value.Immutable
 public interface FreecRatio extends GenomePosition {
+
+    double estimatedBAF();
 
     double ratio();
 
--- hmf-common/src/main/java/com/hartwig/hmftools/common/freec/FreecRatioFactory.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/freec/FreecRatioFactory.java
@@ -1,12 +1,13 @@
 package com.hartwig.hmftools.common.freec;
 
-import com.google.common.collect.Lists;
-import com.hartwig.hmftools.common.exception.HartwigException;
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.hartwig.hmftools.common.exception.HartwigException;
+
+import org.jetbrains.annotations.NotNull;
 
 public enum FreecRatioFactory {
     ;
@@ -16,6 +17,7 @@ public enum FreecRatioFactory {
     private static final int START_FIELD_COLUMN = 1;
     private static final int RATIO_COLUMN = 2;
     private static final int MEDIAN_RATIO_COLUMN = 3;
+    private static final int ESTIMATED_BAF_COLUMN = 6;
 
     @NotNull
     public static List<FreecRatio> loadNormalRatios(@NotNull final String basePath, @NotNull final String sample) throws IOException, HartwigException {
@@ -35,8 +37,9 @@ public enum FreecRatioFactory {
         final long position = Long.valueOf(values[START_FIELD_COLUMN].trim());
         final double ratio = Double.valueOf(values[RATIO_COLUMN].trim());
         final double medianRatio = Double.valueOf(values[MEDIAN_RATIO_COLUMN].trim());
+        final double estimatedBAF = Double.valueOf(values[ESTIMATED_BAF_COLUMN].trim());
 
-        return ImmutableFreecRatio.of(ratio, medianRatio, chromosome, position);
+        return ImmutableFreecRatio.of(estimatedBAF, ratio, medianRatio, chromosome, position);
     }
 
     @NotNull
