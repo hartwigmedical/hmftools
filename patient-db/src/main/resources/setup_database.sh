9f794c3029aa1415137320c2758a16f60f56c5e4
Hessel Keegstra <hessel.keegstra@chello.nl>
2017-06-14 11:44:21 +0200

    Make auto DB rebuild script work on older OS-X versions

--- patient-db/src/main/resources/setup_database.sh
+++ patient-db/src/main/resources/setup_database.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+OS=$(uname)
+if [ $OS = "Darwin" ];
+then
+DARWIN_VERSION=$(uname -r | cut -d. -f1)
+if [ $DARWIN_VERSION -gt 15 ];
+then
+SCRIPT_EPOCH=$(date -r $1 '+%s')
+else
+SCRIPT_EPOCH=$(stat -t '%s' $1 | cut -d\" -f4)
+fi 
+else
+#Assume Linux with GNU date syntax
 SCRIPT_EPOCH=$(date -r $1 '+%s') 
+fi
 
 DB_EPOCH=$(mysql --defaults-file=~/mysql.login << HERE | sed -n '2p'
 SELECT UNIX_TIMESTAMP(MAX(create_time)) db_creation FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = "hmfpatients";
