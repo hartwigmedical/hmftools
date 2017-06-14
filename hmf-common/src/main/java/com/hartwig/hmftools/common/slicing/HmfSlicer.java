aac1c7cd44794bee4d5f67ef56c87c381e8d59db
Mircea Voda <mircearaul.voda@gmail.com>
2017-06-14 13:14:40 +0200

    add hmfSlicer

---
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/slicing/HmfSlicer.java
@@ -0,0 +1,25 @@
+package com.hartwig.hmftools.common.slicing;
+
+import java.util.Collection;
+
+import com.google.common.collect.SortedSetMultimap;
+import com.google.common.collect.TreeMultimap;
+import com.hartwig.hmftools.common.region.HmfGenomeRegion;
+
+import org.jetbrains.annotations.NotNull;
+
+public class HmfSlicer extends BidirectionalSlicer {
+
+    @NotNull
+    private final SortedSetMultimap<String, HmfGenomeRegion> regions;
+
+    HmfSlicer(@NotNull final SortedSetMultimap<String, HmfGenomeRegion> regions) {
+        super(TreeMultimap.create(regions));
+        this.regions = regions;
+    }
+
+    @NotNull
+    public Collection<HmfGenomeRegion> hmfRegions() {
+        return regions.values();
+    }
+}
