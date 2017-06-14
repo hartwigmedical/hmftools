a2fcdf09e49bc311c9643553fdee481b5e9089d8
Mircea Voda <mircearaul.voda@gmail.com>
2017-06-14 12:26:24 +0200

    remove copyNumberAnalyzer dependency on hmfSlicingAnnotation

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/copynumber/CopyNumberAnalyzerTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/copynumber/CopyNumberAnalyzerTest.java
@@ -1,40 +1,34 @@
 package com.hartwig.hmftools.patientreporter.copynumber;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.hartwig.hmftools.common.copynumber.CopyNumber;
-import com.hartwig.hmftools.common.copynumber.freec.ImmutableFreecCopyNumber;
-import com.hartwig.hmftools.common.region.GenomeRegion;
-import com.hartwig.hmftools.common.region.bed.ImmutableBEDGenomeRegion;
-import com.hartwig.hmftools.patientreporter.slicing.HMFSlicingAnnotation;
-import com.hartwig.hmftools.patientreporter.slicing.HMFSlicingAnnotationFactory;
-import org.jetbrains.annotations.NotNull;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.hartwig.hmftools.common.copynumber.CopyNumber;
+import com.hartwig.hmftools.common.copynumber.freec.ImmutableFreecCopyNumber;
+import com.hartwig.hmftools.common.region.HmfGenomeRegion;
+import com.hartwig.hmftools.common.region.hmfslicer.ImmutableHmfSlicerGenomeRegion;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
 
 public class CopyNumberAnalyzerTest {
 
     private static final String CHROMOSOME = "X";
+    private static final String ENTREZ_ID = "11";
+    private static final int TRANSCRIPT_VERSION = 1;
     private static final double EPSILON = 1.0e-10;
 
     @Test
     public void worksAsExpected() {
-        final GenomeRegion first = region(101, 200);
-        final GenomeRegion second = region(301, 400);
-        final GenomeRegion third = region(601, 700);
+        final HmfGenomeRegion first = region(101, 200, "TRANS1", "GENE1", "p1");
+        final HmfGenomeRegion second = region(301, 400, "TRANS2", "GENE2", "p2");
+        final HmfGenomeRegion third = region(601, 700, "TRANS3", "GENE3", "p3");
 
-        final HMFSlicingAnnotation firstAnnotation = HMFSlicingAnnotationFactory.create("TRANS1", 1, "GENE1");
-        final HMFSlicingAnnotation secondAnnotation = HMFSlicingAnnotationFactory.create("TRANS2", 1, "GENE2");
-        final HMFSlicingAnnotation thirdAnnotation = HMFSlicingAnnotationFactory.create("TRANS3", 1, "GENE3");
-
-        final Map<GenomeRegion, HMFSlicingAnnotation> annotations = Maps.newHashMap();
-        annotations.put(first, firstAnnotation);
-        annotations.put(second, secondAnnotation);
-        annotations.put(third, thirdAnnotation);
+        final Set<HmfGenomeRegion> regions = Sets.newHashSet(first, second, third);
 
         final List<CopyNumber> copyNumbers = Lists.newArrayList();
         copyNumbers.add(copyNumber(11, 20, 1));
@@ -42,14 +36,15 @@ public class CopyNumberAnalyzerTest {
         copyNumbers.add(copyNumber(191, 260, 5));
         copyNumbers.add(copyNumber(291, 500, 0));
 
-        final CopyNumberAnalyzer copyNumberAnalyzer = new CopyNumberAnalyzer(annotations);
+        final CopyNumberAnalyzer copyNumberAnalyzer = new CopyNumberAnalyzer(regions);
         final CopyNumberAnalysis analysis = copyNumberAnalyzer.run(copyNumbers);
 
         final List<CopyNumberReport> findings = analysis.findings();
         assertEquals(1, findings.size());
         assertEquals(CHROMOSOME, findings.get(0).chromosome());
-        assertEquals(secondAnnotation.gene(), findings.get(0).gene());
-        assertEquals(secondAnnotation.transcript(), findings.get(0).transcript());
+        assertEquals(second.gene(), findings.get(0).gene());
+        assertEquals(second.transcript(), findings.get(0).transcript());
+        assertEquals(second.chromosomeBand(), findings.get(0).chromosomeBand());
         assertEquals(0, findings.get(0).copyNumber());
 
         final CopyNumberStats firstStat = analysis.stats().get(first);
@@ -69,8 +64,10 @@ public class CopyNumberAnalyzerTest {
     }
 
     @NotNull
-    private static GenomeRegion region(final long start, final long end) {
-        return ImmutableBEDGenomeRegion.of(CHROMOSOME, start, end, null);
+    private static HmfGenomeRegion region(final long start, final long end, @NotNull final String transcriptId,
+            @NotNull final String gene, @NotNull final String chromosomeBand) {
+        return new ImmutableHmfSlicerGenomeRegion(CHROMOSOME, start, end, transcriptId, TRANSCRIPT_VERSION, gene,
+                chromosomeBand, ENTREZ_ID);
     }
 
     @NotNull
