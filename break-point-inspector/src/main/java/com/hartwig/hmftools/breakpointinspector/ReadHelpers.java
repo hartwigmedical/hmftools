d08c2f2460a13f8384716ec5cae9d6e0534785f0
Nevhan Ramadan <me@binaryminary.tech>
2017-06-14 12:00:27 +0200

    cleanup

--- break-point-inspector/src/main/java/com/hartwig/hmftools/breakpointinspector/ReadHelpers.java
+++ break-point-inspector/src/main/java/com/hartwig/hmftools/breakpointinspector/ReadHelpers.java
@@ -1,6 +1,5 @@
 package com.hartwig.hmftools.breakpointinspector;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -9,15 +8,17 @@ import java.util.stream.Stream;
 import htsjdk.samtools.Cigar;
 import htsjdk.samtools.SAMRecord;
 
+import static com.hartwig.hmftools.breakpointinspector.Util.*;
+
 class ReadHelpers {
 
-    static boolean readIntersectsLocation(final SAMRecord read, final Util.Location location) {
+    static boolean readIntersectsLocation(final SAMRecord read, final Location location) {
         assert !read.getReadUnmappedFlag();
         return read.getReferenceIndex() == location.ReferenceIndex && read.getAlignmentStart() <= location.Position
                 && read.getAlignmentEnd() >= location.Position;
     }
 
-    static boolean pairStraddlesLocation(final SAMRecord read, final Util.Location location) {
+    static boolean pairStraddlesLocation(final SAMRecord read, final Location location) {
         assert !read.getReadUnmappedFlag() && !read.getMateUnmappedFlag();
         if (read.getInferredInsertSize() > 0)
             return read.getAlignmentStart() <= location.Position
@@ -31,20 +32,20 @@ class ReadHelpers {
         return read.getCigar().isClipped();
     }
 
-    private static Util.ClipInfo getLeftClip(final SAMRecord read) {
-        final Util.ClipInfo result = new Util.ClipInfo();
+    private static ClipInfo getLeftClip(final SAMRecord read) {
+        final ClipInfo result = new ClipInfo();
         result.Left = true;
         final Cigar cigar = read.getCigar();
         if (cigar.isEmpty())
             return null;
         switch (cigar.getFirstCigarElement().getOperator()) {
             case H:
-                result.Alignment = Util.Location.fromSAMRecord(read, true);
+                result.Alignment = Location.fromSAMRecord(read, true);
                 result.Length = cigar.getFirstCigarElement().getLength();
                 result.HardClipped = true;
                 return result;
             case S:
-                result.Alignment = Util.Location.fromSAMRecord(read, true);
+                result.Alignment = Location.fromSAMRecord(read, true);
                 result.Length = cigar.getFirstCigarElement().getLength();
                 result.Sequence = read.getReadString().substring(0, result.Length);
                 return result;
@@ -52,20 +53,20 @@ class ReadHelpers {
         return null;
     }
 
-    private static Util.ClipInfo getRightClip(final SAMRecord read) {
-        final Util.ClipInfo result = new Util.ClipInfo();
+    private static ClipInfo getRightClip(final SAMRecord read) {
+        final ClipInfo result = new ClipInfo();
         result.Right = true;
         final Cigar cigar = read.getCigar();
         if (cigar.isEmpty())
             return null;
         switch (cigar.getLastCigarElement().getOperator()) {
             case H:
-                result.Alignment = Util.Location.fromSAMRecord(read, false);
+                result.Alignment = Location.fromSAMRecord(read, false);
                 result.Length = cigar.getLastCigarElement().getLength();
                 result.HardClipped = true;
                 return result;
             case S:
-                result.Alignment = Util.Location.fromSAMRecord(read, false);
+                result.Alignment = Location.fromSAMRecord(read, false);
                 result.Length = cigar.getLastCigarElement().getLength();
                 result.Sequence = read.getReadString().substring(read.getReadLength() - result.Length);
                 return result;
@@ -73,28 +74,31 @@ class ReadHelpers {
         return null;
     }
 
-    static List<Util.ClipInfo> getClips(final SAMRecord read) {
+    static List<ClipInfo> getClips(final SAMRecord read) {
         return Stream.of(getLeftClip(read), getRightClip(read)).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
-    static Util.Region determineRegion(final SAMRecord read, final Util.Location location1,
-            final Util.Location location2) {
+    static Region determineRegion(final SAMRecord read, final Location location1,
+            final Location location2) {
         if (location1.ReferenceIndex != location2.ReferenceIndex) {
             if (read.getReferenceIndex() == location1.ReferenceIndex)
-                return Util.Region.BP1;
+                return Region.BP1;
             else if (read.getReferenceIndex() == location2.ReferenceIndex)
-                return Util.Region.BP2;
+                return Region.BP2;
         } else if (read.getReferenceIndex() == location1.ReferenceIndex) {
             final int distance1 = Math.min(Math.abs(read.getAlignmentStart() - location1.Position),
                     Math.abs(read.getAlignmentEnd() - location1.Position));
             final int distance2 = Math.min(Math.abs(read.getAlignmentStart() - location2.Position),
                     Math.abs(read.getAlignmentEnd() - location2.Position));
-            if (distance1 < distance2)
-                return Util.Region.BP1;
+
+            if (distance1 < 500 && distance2 < 500) {
+                // TODO: handle uncertainty
+            } else if (distance1 < distance2)
+                return Region.BP1;
             else
-                return Util.Region.BP2;
+                return Region.BP2;
         }
-        return Util.Region.OTHER;
+        return Region.OTHER;
     }
 
     static boolean isMate(final SAMRecord read, final SAMRecord mate) {
