c11f1aed73f35bec62455da2fde09347567150de
mvoda <mircearaul.voda@gmail.com>
2017-06-15 02:28:55 +0200

    check drup and cosmic command line parameters

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
@@ -8,8 +8,6 @@ import java.time.format.DateTimeFormatter;
 
 import javax.xml.stream.XMLStreamException;
 
-import com.hartwig.hmftools.common.cosmic.Cosmic;
-import com.hartwig.hmftools.common.cosmic.CosmicModel;
 import com.hartwig.hmftools.common.ecrf.CpctEcrfModel;
 import com.hartwig.hmftools.common.exception.EmptyFileException;
 import com.hartwig.hmftools.common.exception.HartwigException;
@@ -22,8 +20,6 @@ import com.hartwig.hmftools.patientreporter.algo.NotSequenceableReporter;
 import com.hartwig.hmftools.patientreporter.algo.SinglePatientReporter;
 import com.hartwig.hmftools.patientreporter.batch.BatchReportAnalyser;
 import com.hartwig.hmftools.patientreporter.copynumber.CopyNumberAnalyzer;
-import com.hartwig.hmftools.patientreporter.filters.DrupFilter;
-import com.hartwig.hmftools.patientreporter.genePanel.GenePanelModel;
 import com.hartwig.hmftools.patientreporter.report.PDFWriter;
 import com.hartwig.hmftools.patientreporter.report.ReportWriter;
 import com.hartwig.hmftools.patientreporter.variants.VariantAnalyzer;
@@ -62,7 +58,7 @@ public class PatientReporterApplication {
     private static final String NOT_SEQUENCEABLE_REASON = "not_sequenceable_reason";
     private static final String NOT_SEQUENCEABLE_SAMPLE = "not_sequenceable_sample";
     private static final String DRUP_GENES_CSV = "drup_genes_csv";
-    private static final String GENE_PANEL_CSV = "gene_panel_csv";
+    private static final String COSMIC_CSV = "cosmic_csv";
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
@@ -103,31 +99,10 @@ public class PatientReporterApplication {
         }
     }
 
-    @NotNull
-    private static HmfSlicer buildHmfSlicingRegion(@NotNull final CommandLine cmd)
-            throws IOException, EmptyFileException {
-        return SlicerFactory.fromHmfSlicerFile(cmd.getOptionValue(HMF_SLICING_BED));
-    }
-
-    @NotNull
-    private static DrupFilter buildDrupFilter(@NotNull final CommandLine cmd) throws IOException, HartwigException {
-        return new DrupFilter(cmd.getOptionValue(DRUP_GENES_CSV));
-    }
-
-    @NotNull
-    private static GenePanelModel buildGenePanel(@NotNull final CommandLine cmd) throws IOException, HartwigException {
-        return new GenePanelModel(cmd.getOptionValue(GENE_PANEL_CSV));
-    }
-
-    @NotNull
-    private static CosmicModel buildCosmicModel(@NotNull final CommandLine cmd) throws IOException, HartwigException {
-        return Cosmic.buildModelFromCsv(cmd.getOptionValue(GENE_PANEL_CSV));
-    }
-
     private static HmfReporterData buildReporterData(@NotNull final CommandLine cmd)
             throws IOException, HartwigException {
         return HmfReporterDataLoader.buildFromFiles(cmd.getOptionValue(HMF_SLICING_BED),
-                cmd.getOptionValue(DRUP_GENES_CSV), cmd.getOptionValue(GENE_PANEL_CSV));
+                cmd.getOptionValue(DRUP_GENES_CSV), cmd.getOptionValue(COSMIC_CSV));
     }
 
     @NotNull
@@ -204,6 +179,8 @@ public class PatientReporterApplication {
             final String highConfidenceBed = cmd.getOptionValue(HIGH_CONFIDENCE_BED);
             final String hmfSlicingBed = cmd.getOptionValue(HMF_SLICING_BED);
             final String tmpDirectory = cmd.getOptionValue(TMP_DIRECTORY);
+            final String drupGenesCsv = cmd.getOptionValue(DRUP_GENES_CSV);
+            final String cosmicCsv = cmd.getOptionValue(COSMIC_CSV);
 
             if (cpctSlicingBed == null || !exists(cpctSlicingBed)) {
                 LOGGER.warn(CPCT_SLICING_BED + " has to be an existing file: " + cpctSlicingBed);
@@ -211,6 +188,10 @@ public class PatientReporterApplication {
                 LOGGER.warn(HIGH_CONFIDENCE_BED + " has to be an existing file: " + highConfidenceBed);
             } else if (hmfSlicingBed == null || !exists(hmfSlicingBed)) {
                 LOGGER.warn(HMF_SLICING_BED + " has to be an existing file: " + hmfSlicingBed);
+            } else if (drupGenesCsv == null || !exists(drupGenesCsv)) {
+                LOGGER.warn(DRUP_GENES_CSV + " has to be an existing file: " + drupGenesCsv);
+            } else if (cosmicCsv == null || !exists(cosmicCsv)) {
+                LOGGER.warn(COSMIC_CSV + " has to be an existing file: " + cosmicCsv);
             } else if (tmpDirectory != null && (!exists(tmpDirectory) || !isDirectory(tmpDirectory))) {
                 LOGGER.warn(TMP_DIRECTORY + " has to be an existing directory: " + highConfidenceBed);
             } else {
@@ -306,7 +287,7 @@ public class PatientReporterApplication {
                 "In case of non-sequenceable reports, the name of the sample used.");
         options.addOption(DRUP_GENES_CSV, true,
                 "Path towards a CSV containing genes that could potentially indicate inclusion in DRUP.");
-        options.addOption(GENE_PANEL_CSV, true, "Path towards a CSV containing gene panel data.");
+        options.addOption(COSMIC_CSV, true, "Path towards a CSV containing COSMIC census data.");
         return options;
     }
 
