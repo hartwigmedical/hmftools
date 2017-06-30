0813525d3cc0daba9f0cfbbdc751a47a003c863d
Mircea Voda <mircearaul.voda@gmail.com>
2017-06-30 16:53:55 +0200

    remove logging from patient-db readers

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/BiopsyReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/BiopsyReader.java
@@ -3,8 +3,6 @@ package com.hartwig.hmftools.patientdb.readers;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.ecrf.datamodel.EcrfForm;
@@ -13,20 +11,16 @@ import com.hartwig.hmftools.common.ecrf.datamodel.EcrfPatient;
 import com.hartwig.hmftools.common.ecrf.datamodel.EcrfStudyEvent;
 import com.hartwig.hmftools.patientdb.data.BiopsyData;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-final class BiopsyReader {
-    private static final Logger LOGGER = LogManager.getLogger(BiopsyReader.class);
-
+public final class BiopsyReader {
     private static final String STUDY_BIOPSY = "SE.BIOPSY";
-    private static final String FORM_BIOPS = "FRM.BIOPS";
+    public static final String FORM_BIOPS = "FRM.BIOPS";
     private static final String ITEMGROUP_BIOPSIES = "GRP.BIOPS.BIOPSIES";
 
-    private static final String FIELD_DATE = "FLD.BIOPS.BIOPTDT";
-    private static final String FIELD_LOCATION = "FLD.BIOPS.BILESSITE";
-    private static final String FIELD_LOCATION_OTHER = "FLD.BIOPS.BIOTHLESSITE";
+    public static final String FIELD_BIOPSY_DATE = "FLD.BIOPS.BIOPTDT";
+    public static final String FIELD_LOCATION = "FLD.BIOPS.BILESSITE";
+    public static final String FIELD_LOCATION_OTHER = "FLD.BIOPS.BIOTHLESSITE";
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
@@ -39,7 +33,7 @@ final class BiopsyReader {
         for (final EcrfStudyEvent studyEvent : patient.studyEventsPerOID(STUDY_BIOPSY)) {
             for (final EcrfForm form : studyEvent.nonEmptyFormsPerOID(FORM_BIOPS, true)) {
                 for (final EcrfItemGroup itemGroup : form.nonEmptyItemGroupsPerOID(ITEMGROUP_BIOPSIES, true)) {
-                    final LocalDate date = itemGroup.readItemDate(FIELD_DATE, 0, DATE_FORMATTER, true);
+                    final LocalDate date = itemGroup.readItemDate(FIELD_BIOPSY_DATE, 0, DATE_FORMATTER, true);
                     final String location = itemGroup.readItemString(FIELD_LOCATION, 0, true);
                     if (location != null && location.trim().toLowerCase().startsWith("other")) {
                         final String location_other = itemGroup.readItemString(FIELD_LOCATION_OTHER, 0, true);
@@ -48,14 +42,6 @@ final class BiopsyReader {
                         biopsies.add(new BiopsyData(date, location));
                     }
                 }
-            }
-        }
-        final Map<LocalDate, List<BiopsyData>> groups = biopsies.stream().filter(
-                biopsy -> biopsy.date() != null).collect(Collectors.groupingBy(BiopsyData::date));
-        for (final LocalDate biopsyDate : groups.keySet()) {
-            final int biopsiesPerDate = groups.get(biopsyDate).size();
-            if (biopsiesPerDate > 1) {
-                LOGGER.warn(patient.patientId() + ": " + biopsiesPerDate + " biopsies with same date: " + biopsyDate);
             }
         }
         return biopsies;
--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/BiopsyTreatmentResponseReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/BiopsyTreatmentResponseReader.java
@@ -11,21 +11,18 @@ import com.hartwig.hmftools.common.ecrf.datamodel.EcrfPatient;
 import com.hartwig.hmftools.common.ecrf.datamodel.EcrfStudyEvent;
 import com.hartwig.hmftools.patientdb.data.BiopsyTreatmentResponseData;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-final class BiopsyTreatmentResponseReader {
-    private static final Logger LOGGER = LogManager.getLogger(BiopsyTreatmentResponseReader.class);
+public final class BiopsyTreatmentResponseReader {
 
     private static final String STUDY_TREATMENT = "SE.TREATMENT";
-    private static final String FORM_TUMOR_MEASUREMENT = "FRM.TUMORMEASUREMENT";
+    public static final String FORM_TUMOR_MEASUREMENT = "FRM.TUMORMEASUREMENT";
     private static final String ITEMGROUP_MEASUREMENT = "GRP.TUMORMEASUREMENT.MEASUREMENT";
-    private static final String FIELD_ASSESSMENT_DATE = "FLD.TUMORMEASUREMENT.ASSDTC";
+    public static final String FIELD_ASSESSMENT_DATE = "FLD.TUMORMEASUREMENT.ASSDTC";
     private static final String ITEMGROUP_TUMOR_MEASUREMENT = "GRP.TUMORMEASUREMENT.TUMORMEASUREMENT";
-    private static final String FIELD_RESPONSE_DATE = "FLD.TUMORMEASUREMENT.RESPONSEDTC";
-    private static final String FIELD_MEASUREMENT_YN = "FLD.TUMORMEASUREMENT.TMYN";
-    private static final String FIELD_RESPONSE = "FLD.TUMORMEASUREMENT.BESTRESPON";
+    public static final String FIELD_RESPONSE_DATE = "FLD.TUMORMEASUREMENT.RESPONSEDTC";
+    public static final String FIELD_MEASUREMENT_YN = "FLD.TUMORMEASUREMENT.TMYN";
+    public static final String FIELD_RESPONSE = "FLD.TUMORMEASUREMENT.BESTRESPON";
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
@@ -48,8 +45,7 @@ final class BiopsyTreatmentResponseReader {
                 LocalDate responseDate = null;
                 String measurementDone = null;
                 String response = null;
-                for (final EcrfItemGroup itemGroup : form.nonEmptyItemGroupsPerOID(ITEMGROUP_TUMOR_MEASUREMENT,
-                        true)) {
+                for (final EcrfItemGroup itemGroup : form.nonEmptyItemGroupsPerOID(ITEMGROUP_TUMOR_MEASUREMENT, true)) {
                     final LocalDate date = itemGroup.readItemDate(FIELD_RESPONSE_DATE, 0, DATE_FORMATTER, true);
                     if (date != null) {
                         responseDate = date;
@@ -63,16 +59,7 @@ final class BiopsyTreatmentResponseReader {
                         response = responseValue;
                     }
                 }
-                if (assessmentDate == null && responseDate == null) {
-                    LOGGER.warn(patient.patientId() + ": both assessment date and treatment response date missing.");
-                } else {
-                    if (assessmentDate != null && responseDate != null && !assessmentDate.isEqual(responseDate)) {
-                        LOGGER.warn(patient.patientId() + ": assessment date(" + assessmentDate
-                                + ") differs from treatment response date(" + responseDate + ")");
-                    }
-                    treatmentResponses.add(
-                            new BiopsyTreatmentResponseData(assessmentDate, responseDate, response, measurementDone));
-                }
+                treatmentResponses.add(new BiopsyTreatmentResponseData(assessmentDate, responseDate, response, measurementDone));
             }
         }
         return treatmentResponses;
