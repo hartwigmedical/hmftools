2921cd611b36f5cece8c90ff6fe27d315715f5d6
Mircea Voda <mircearaul.voda@gmail.com>
2017-06-30 16:45:14 +0200

    add validator constants

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/Config.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/Config.java
@@ -9,4 +9,6 @@ public final class Config {
     public static final int ARRIVAL_DATE_THRESHOLD = 180;
     public static final int MAX_DAYS_BETWEEN_TREATMENT_AND_BIOPSY = 90;
     public static final int BATCH_INSERT_SIZE = 1000;
+    public static final int START_DATE_RESPONSE_THRESHOLD = 112;
+    public static final int IMMEDIATE_TREATMENT_END_THRESHOLD = 7;
 }
