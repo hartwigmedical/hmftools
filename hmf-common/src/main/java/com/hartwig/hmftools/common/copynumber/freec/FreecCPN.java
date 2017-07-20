6ede725c084d32a781d700a89dd029e8ff1358a9
Jonathan Baber <jonbaber@gmail.com>
2017-07-20 12:21:43 +1000

    FreeCPN File Loader

---
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/copynumber/freec/FreecCPN.java
@@ -0,0 +1,14 @@
+package com.hartwig.hmftools.common.copynumber.freec;
+
+import com.hartwig.hmftools.common.position.GenomePosition;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public interface FreecCPN extends GenomePosition {
+
+    int readCount();
+}
---
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/copynumber/freec/FreecCPNFileLoader.java
@@ -0,0 +1,53 @@
+package com.hartwig.hmftools.common.copynumber.freec;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.hartwig.hmftools.common.exception.HartwigException;
+import com.hartwig.hmftools.common.io.reader.LineReader;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum FreecCPNFileLoader {
+    ;
+
+    private static final String RATIO_COLUMN_SEPARATOR = "\t";
+    private static final int CHROMOSOME_COLUMN = 0;
+    private static final int START_FIELD_COLUMN = 1;
+    private static final int READ_COLUMN = 2;
+
+    @NotNull
+    public static Multimap<String, FreecCPN> loadCPNContent(@NotNull final String filePath) throws IOException, HartwigException {
+        final Path path = new File(filePath).toPath();
+        return loadCPNContent(LineReader.build().readLines(path, x -> true));
+    }
+
+    @NotNull
+    private static Multimap<String, FreecCPN> loadCPNContent(@NotNull final List<String> lines) {
+
+        final Multimap<String, FreecCPN> result = ArrayListMultimap.create();
+        for (String line : lines) {
+            final FreecCPN gcContent = fromLine(line);
+            result.put(gcContent.chromosome(), gcContent);
+        }
+
+        return result;
+    }
+
+    @NotNull
+    @VisibleForTesting
+    static FreecCPN fromLine(@NotNull final String ratioLine) {
+        final String[] values = ratioLine.split(RATIO_COLUMN_SEPARATOR);
+
+        final String chromosome = values[CHROMOSOME_COLUMN].trim();
+        final long position = Long.valueOf(values[START_FIELD_COLUMN].trim());
+        final int readCount = Integer.valueOf(values[READ_COLUMN].trim());
+
+        return ImmutableFreecCPN.builder().chromosome(chromosome).position(position).readCount(readCount).build();
+    }
+}
