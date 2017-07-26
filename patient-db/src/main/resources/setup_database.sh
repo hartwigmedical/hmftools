050547b8ad4c33e676b7f61aa6e45adc325b3689
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-07-26 09:44:47 +0200

    Fixed some warnings in database setup script

--- patient-db/src/main/resources/setup_database.sh
+++ patient-db/src/main/resources/setup_database.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 OS=$(uname)
-if [ $OS = "Darwin" ];
+if [ ${OS} = "Darwin" ];
 then
-DARWIN_VERSION=$(uname -r | cut -d. -f1)
-if [ $DARWIN_VERSION -gt 15 ];
+    DARWIN_VERSION=$(uname -r | cut -d. -f1)
+if [ ${DARWIN_VERSION} -gt 15 ];
 then
-SCRIPT_EPOCH=$(date -r $1 '+%s')
+    SCRIPT_EPOCH=$(date -r $1 '+%s')
 else
-SCRIPT_EPOCH=$(stat -t '%s' $1 | cut -d\" -f4)
+    SCRIPT_EPOCH=$(stat -t '%s' $1 | cut -d\" -f4)
 fi 
 else
-#Assume Linux with GNU date syntax
-SCRIPT_EPOCH=$(date -r $1 '+%s') 
+    #Assume Linux with GNU date syntax
+    SCRIPT_EPOCH=$(date -r $1 '+%s')
 fi
 
 DB_EPOCH=$(mysql --defaults-file=~/mysql.login << HERE | sed -n '2p'
@@ -20,9 +20,9 @@ SELECT UNIX_TIMESTAMP(MAX(create_time)) db_creation FROM INFORMATION_SCHEMA.TABL
 HERE
 )
 
-if [ $SCRIPT_EPOCH -gt $DB_EPOCH ];  
+if [ ${SCRIPT_EPOCH} -gt ${DB_EPOCH} ];
 then
-echo REBUILDING DATABASE
-mysql --defaults-file=~/mysql.login < $1
+    echo REBUILDING DATABASE
+    mysql --defaults-file=~/mysql.login < $1
 fi
 
