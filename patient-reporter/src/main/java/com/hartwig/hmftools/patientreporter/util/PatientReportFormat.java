d5d301f45e90453620c82460a3591da04cd8cbcb
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-07-26 11:25:19 +0200

    Added missing annotations

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/util/PatientReportFormat.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/util/PatientReportFormat.java
@@ -1,14 +1,17 @@
 package com.hartwig.hmftools.patientreporter.util;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public enum PatientReportFormat {
     ;
 
+    @NotNull
     public static String formatNullablePercent(final @Nullable Double percentage) {
         return percentage == null ? "Na" : formatPercent(percentage);
     }
 
+    @NotNull
     public static String formatPercent(final double percentage) {
         return Long.toString(Math.round(percentage * 100D)) + "%";
     }
