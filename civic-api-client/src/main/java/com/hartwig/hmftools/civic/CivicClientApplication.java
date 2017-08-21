f318d87635fd6712ac3e106c82a49d2132d2372c
Mircea Voda <mircearaul.voda@gmail.com>
2017-08-21 17:03:18 +0200

    update civic-api-client main

--- civic-api-client/src/main/java/com/hartwig/hmftools/civic/CivicClientApplication.java
+++ civic-api-client/src/main/java/com/hartwig/hmftools/civic/CivicClientApplication.java
@@ -1,10 +1,10 @@
 package com.hartwig.hmftools.civic;
 
-import java.io.IOException;
-
-import javax.xml.stream.XMLStreamException;
-
-import com.hartwig.hmftools.common.exception.HartwigException;
+import com.google.gson.Gson;
+import com.hartwig.hmftools.civic.api.CivicApi;
+import com.hartwig.hmftools.civic.data.CivicApiDataGson;
+import com.hartwig.hmftools.civic.data.CivicGene;
+import com.hartwig.hmftools.civic.data.CivicVariant;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -15,20 +15,40 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CivicClientApplication {
 
     private static final Logger LOGGER = LogManager.getLogger(CivicClientApplication.class);
 
-    public static void main(final String... args) throws ParseException, IOException, XMLStreamException, HartwigException {
+    private static final String CIVIC_API_ENDPOINT = "https://civic.genome.wustl.edu/api/";
+
+    public static void main(final String... args) throws Exception {
         final Options options = createOptions();
         final CommandLine cmd = createCommandLine(options, args);
 
-        final Retrofit retrofit = new Retrofit.Builder().baseUrl("https://civic.genome.wustl.edu/api/")
-                //                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build();
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        final Gson gson = CivicApiDataGson.buildGson();
+
+        final Retrofit retrofit = new Retrofit.Builder().baseUrl(CIVIC_API_ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
+                .build();
+
+        final CivicApi civic = retrofit.create(CivicApi.class);
+        civic.getGene(1956)
+                .flatMapIterable(CivicGene::variants)
+                .filter(variantKey -> variantKey.acceptedEvidenceItems() > 0)
+                .flatMap(variantKey -> civic.getVariant(variantKey.id()))
+                .flatMapIterable(CivicVariant::evidenceItems)
+                .filter(evidenceItem -> !evidenceItem.drugs().isEmpty())
+                .filter(evidenceItem -> evidenceItem.level() <= 'C')
+                .subscribe(LOGGER::info, Throwable::printStackTrace, () -> LOGGER.info("completed"));
     }
 
     @NotNull
