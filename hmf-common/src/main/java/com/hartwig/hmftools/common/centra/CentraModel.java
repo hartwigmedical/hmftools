56b4a5b0a4e99788fa9484d908895c416d09ea65
Mircea Voda <mircearaul.voda@gmail.com>
2017-08-29 11:54:42 +0200

    add getAddressForSample to centraModel

--- hmf-common/src/main/java/com/hartwig/hmftools/common/centra/CentraModel.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/centra/CentraModel.java
@@ -1,9 +1,13 @@
 package com.hartwig.hmftools.common.centra;
 
+import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.Lists;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.Strings;
 import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -29,7 +33,7 @@ public abstract class CentraModel {
 
     @Nullable
     public String getCpctRecipientsFromSample(@NotNull final String sample) {
-        return getCpctRecipients(getCentraFromSample(sample));
+        return getCpctRecipients(getCentraIdFromSample(sample));
     }
 
     @Nullable
@@ -44,7 +48,7 @@ public abstract class CentraModel {
 
     @Nullable
     public String getDrupRecipientsFromSample(@NotNull final String sample) {
-        return getDrupRecipients(getCentraFromSample(sample));
+        return getDrupRecipients(getCentraIdFromSample(sample));
     }
 
     @Nullable
@@ -54,11 +58,41 @@ public abstract class CentraModel {
 
     // MIVO: expects sample in DRUP/CPCT format
     @NotNull
-    private static String getCentraFromSample(@NotNull final String sample) {
+    private static String getCentraIdFromSample(@NotNull final String sample) {
         final String ucSample = sample.toUpperCase();
         if ((ucSample.startsWith("DRUP") || ucSample.startsWith("CPCT")) && sample.length() >= 12) {
             return sample.substring(6, 8);
         }
         throw new IllegalArgumentException("Sample parameter: " + sample + "was not in CPCT/DRUP format");
+    }
+
+    @Nullable
+    public String getAddressStringForSample(@NotNull final String sample) {
+        final String centraId = getCentraIdFromSample(sample);
+        final CentraData centra = centraPerId(centraId);
+        if (centra == null) {
+            LOGGER.error("Centra model does not contain id " + centraId);
+            return null;
+        }
+        checkAddressFields(centra);
+        return centra.addressName() + ", " + centra.addressZip() + " " + centra.addressCity();
+    }
+
+    private void checkAddressFields(@NotNull final CentraData centra) {
+        final List<String> missingFields = Lists.newArrayList();
+        if (centra.addressName().isEmpty()) {
+            missingFields.add("name");
+        }
+
+        if (centra.addressZip().isEmpty()) {
+            missingFields.add("zip");
+        }
+
+        if (centra.addressCity().isEmpty()) {
+            missingFields.add("city");
+        }
+        if (!missingFields.isEmpty()) {
+            LOGGER.warn("Some address fields (" + Strings.join(missingFields, ',') + ") are missing.");
+        }
     }
 }
