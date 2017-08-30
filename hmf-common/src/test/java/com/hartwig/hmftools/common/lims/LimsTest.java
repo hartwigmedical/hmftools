a74d093f0ec2464a0a3ec566a1b13136d1da1630
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-08-30 12:20:08 +0200

    Move test into own class

--- hmf-common/src/test/java/com/hartwig/hmftools/common/lims/LimsTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/lims/LimsTest.java
@@ -80,25 +80,4 @@ public class LimsTest {
         assertEquals(0.3, sample5TumorPercentage, EPSILON);
         assertEquals(sample5TumorPercentage, limsModelTumorPercentage5, EPSILON);
     }
-
-    @Test
-    public void canLoadJsonModel() throws IOException, EmptyFileException {
-        final String limsJson = Resources.getResource("lims").getPath() + File.separator + "lims.json";
-        final LimsJsonModel jsonLims = LimsJsonModel.readModelFromFile(limsJson);
-        assertEquals(LocalDate.parse("2016-01-02"), jsonLims.samplingDateForSample("SAMP01010003R"));
-        assertEquals(LocalDate.parse("2016-01-03"), jsonLims.arrivalDateForSample("SAMP01010003R"));
-        assertNull(jsonLims.tumorPercentageForSample("SAMP01010003R"));
-        assertEquals("CSB000000", jsonLims.bloodBarcodeForSample("SAMP01010003R"));
-        assertEquals("CSB000000", jsonLims.barcodeForSample("SAMP01010003R"));
-        assertEquals(LocalDate.parse("2016-01-03"), jsonLims.bloodArrivalDateForSample("SAMP01010003R"));
-
-        assertEquals("CSB000000", jsonLims.bloodBarcodeForSample("SAMP01010003T"));
-        assertEquals("FC0000001", jsonLims.barcodeForSample("SAMP01010003T"));
-        final Double tumorPercentage = jsonLims.tumorPercentageForSample("SAMP01010003T");
-        assertNotNull(tumorPercentage);
-        assertEquals(.3, tumorPercentage, 0.001);
-        assertEquals(LocalDate.parse("2016-02-05"), jsonLims.arrivalDateForSample("SAMP01010003T"));
-        assertEquals(LocalDate.parse("2016-01-04"), jsonLims.samplingDateForSample("SAMP01010003T"));
-        assertEquals(LocalDate.parse("2016-01-03"), jsonLims.bloodArrivalDateForSample("SAMP01010003T"));
-    }
 }
