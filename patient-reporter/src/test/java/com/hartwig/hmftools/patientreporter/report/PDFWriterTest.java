b0c559ce753a4eeb55da8814b431bb9ed3371ac1
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-08-30 14:38:37 +0200

    Refactored patient reporter test case

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/report/PDFWriterTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/report/PDFWriterTest.java
@@ -28,6 +28,7 @@ import com.hartwig.hmftools.patientreporter.copynumber.ImmutableCopyNumberReport
 import com.hartwig.hmftools.patientreporter.variants.ImmutableVariantReport;
 import com.hartwig.hmftools.patientreporter.variants.VariantReport;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
@@ -43,10 +44,45 @@ public class PDFWriterTest {
     @Test
     public void canGeneratePatientReport() throws DRException, IOException, HartwigException {
         final String sample = "CPCT11111111T";
+        final String tumorType = "Melanoma";
+        final Double pathologyTumorPercentage = 0.6;
+
         final FittedPurity fittedPurity =
                 ImmutableFittedPurity.builder().purity(0.58).diploidProportion(0).normFactor(0).score(0).ploidy(2).build();
-        final PurityAdjuster purityAdjuster = new PurityAdjuster(Gender.MALE, fittedPurity);
 
+        final List<VariantReport> variants = createTestVariants(new PurityAdjuster(Gender.MALE, fittedPurity));
+        final int mutationalLoad = 361;
+        final List<CopyNumberReport> copyNumbers = createTestCopyNumbers();
+
+        final PatientReport patientReport =
+                ImmutablePatientReport.of(sample, variants, copyNumbers, mutationalLoad, tumorType, pathologyTumorPercentage, "58%",
+                        "FC000001", "CSB000000", LocalDate.parse("2016-01-05"), LocalDate.parse("2016-01-01"));
+
+        final String genePanelPath = Resources.getResource("bed").getPath() + File.separator + "hmf_gene_panel.tsv";
+        final String drupFilterPath = Resources.getResource("csv").getPath() + File.separator + "drup_genes.csv";
+        final String cosmicPath = Resources.getResource("csv").getPath() + File.separator + "cosmic_slice.csv";
+        final String centerPath = Resources.getResource("center").getPath() + File.separator + "centers.csv";
+        final String signaturePath = Resources.getResource("signature").getPath() + File.separator + "signature.png";
+
+        final HmfReporterData reporterData =
+                HmfReporterDataLoader.buildFromFiles(genePanelPath, drupFilterPath, cosmicPath, centerPath, signaturePath);
+
+        final InputStream logoStream = Resources.asByteSource(Resources.getResource(PDFWriter.REPORT_LOGO_PATH)).openStream();
+        final JasperReportBuilder report = PDFWriter.generatePatientReport(patientReport, logoStream, reporterData);
+        assertNotNull(report);
+
+        if (SHOW_AND_PRINT) {
+            report.show().print();
+        }
+
+        if (WRITE_TO_PDF) {
+            report.toPdf(new FileOutputStream(REPORT_BASE_DIR + "/hmf/tmp/test_report.pdf"));
+        }
+        logoStream.close();
+    }
+
+    @NotNull
+    private static List<VariantReport> createTestVariants(@NotNull final PurityAdjuster purityAdjuster) {
         final VariantReport variant1 = ImmutableVariantReport.builder()
                 .gene("BRAF")
                 .chromosome("7")
@@ -63,6 +99,7 @@ public class PDFWriterTest {
                 .baf("AAAB")
                 .impliedVAF(purityAdjuster.purityAdjustedVAF(4, 0.18 / 0.99))
                 .build();
+
         final VariantReport variant2 = ImmutableVariantReport.builder()
                 .gene("MYC")
                 .chromosome("8")
@@ -79,6 +116,7 @@ public class PDFWriterTest {
                 .impliedVAF(purityAdjuster.purityAdjustedVAF(2, 0.2 / 0.88))
                 .baf("AB")
                 .build();
+
         final VariantReport variant3 = ImmutableVariantReport.builder()
                 .gene("TP53")
                 .chromosome("17")
@@ -94,8 +132,11 @@ public class PDFWriterTest {
                 .impliedVAF(purityAdjuster.purityAdjustedVAF(3, 0.20 / 0.87))
                 .baf("AAA")
                 .build();
-        final List<VariantReport> variants = Lists.newArrayList(variant1, variant2, variant3);
+        return Lists.newArrayList(variant1, variant2, variant3);
+    }
 
+    @Test
+    private static List<CopyNumberReport> createTestCopyNumbers() {
         final CopyNumberReport copyNumber1 = ImmutableCopyNumberReport.builder()
                 .chromosome("2")
                 .chromosomeBand("p23.1-p23.2")
@@ -110,39 +151,7 @@ public class PDFWriterTest {
                 .copyNumber(9)
                 .type(CopyNumberReportType.GAIN)
                 .build();
-        final List<CopyNumberReport> copyNumbers = Lists.newArrayList(copyNumber1, copyNumber2);
-
-        final int mutationalLoad = 361;
-        final String tumorType = "Melanoma";
-        final Double pathologyTumorPercentage = 0.6;
-
-        final PatientReport patientReport =
-                ImmutablePatientReport.of(sample, variants, copyNumbers, mutationalLoad, tumorType, pathologyTumorPercentage, "58%",
-                        "FC000001", "CSB000000", LocalDate.parse("2016-01-05"), LocalDate.parse("2016-01-01"));
-
-        final String genePanelPath = Resources.getResource("bed").getPath() + File.separator + "hmf_gene_panel.tsv";
-        final String drupFilterPath = Resources.getResource("csv").getPath() + File.separator + "drup_genes.csv";
-        final String cosmicPath = Resources.getResource("csv").getPath() + File.separator + "cosmic_slice.csv";
-        final String centerPath = Resources.getResource("center").getPath() + File.separator + "centers.csv";
-        final String signaturePath = Resources.getResource("signature").getPath() + File.separator + "signature.png";
-
-        // KODU: Refers to the actual cosmic path on datastore:
-        // final String cosmicPath = REPORT_BASE_DIR + "/hmf/tmp/170529_grch37_cosmic_census.csv";
-        final HmfReporterData reporterData =
-                HmfReporterDataLoader.buildFromFiles(genePanelPath, drupFilterPath, cosmicPath, centerPath, signaturePath);
-
-        final InputStream logoStream = Resources.asByteSource(Resources.getResource(PDFWriter.REPORT_LOGO_PATH)).openStream();
-        final JasperReportBuilder report = PDFWriter.generatePatientReport(patientReport, logoStream, reporterData);
-        assertNotNull(report);
-
-        if (SHOW_AND_PRINT) {
-            report.show().print();
-        }
-
-        if (WRITE_TO_PDF) {
-            report.toPdf(new FileOutputStream(REPORT_BASE_DIR + "/hmf/tmp/test_report.pdf"));
-        }
-        logoStream.close();
+        return Lists.newArrayList(copyNumber1, copyNumber2);
     }
 
     @Test
