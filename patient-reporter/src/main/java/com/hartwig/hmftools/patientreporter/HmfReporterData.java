5c377f353d19b835e3233da08dec6897d53893dd
Mircea Voda <mircearaul.voda@gmail.com>
2017-09-20 10:51:42 +0200

    add tumorLocationDoid mapping to hmfReporterData

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/HmfReporterData.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/HmfReporterData.java
@@ -2,6 +2,7 @@ package com.hartwig.hmftools.patientreporter;
 
 import com.hartwig.hmftools.common.center.CenterModel;
 import com.hartwig.hmftools.common.cosmic.CosmicModel;
+import com.hartwig.hmftools.common.ecrf.doid.TumorLocationDoidMapping;
 import com.hartwig.hmftools.common.gene.GeneModel;
 import com.hartwig.hmftools.patientreporter.data.COSMICGeneFusionModel;
 import com.hartwig.hmftools.patientreporter.filters.DrupFilter;
@@ -33,4 +34,7 @@ public abstract class HmfReporterData {
 
     @NotNull
     public abstract COSMICGeneFusionModel fusionModel();
+
+    @NotNull
+    public abstract TumorLocationDoidMapping doidMapping();
 }
