3443f8e8ec47cb2c820ac2b708bfb485898e4e05
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-09-22 14:10:09 +0200

    Bump version of patient report

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
@@ -44,7 +44,7 @@ public class PatientReporterApplication {
 
     private static final Logger LOGGER = LogManager.getLogger(PatientReporterApplication.class);
 
-    public static final String VERSION = "3.12";
+    public static final String VERSION = "3.13";
 
     // TODO (KODU): Make version read from POM
     //    public static final String VERSION = PatientReporterApplication.class.getPackage().getImplementationVersion();
