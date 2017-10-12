3e4e454a2e92fd6cc51e829733cdb4e9b84ad9a9
Mircea Voda <mircearaul.voda@gmail.com>
2017-10-12 18:12:03 +0200

    update notSequenceableReporter

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/algo/NotSequenceableReporter.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/algo/NotSequenceableReporter.java
@@ -1,13 +1,19 @@
 package com.hartwig.hmftools.patientreporter.algo;
 
 import java.io.IOException;
+import java.util.Optional;
 
+import com.hartwig.hmftools.common.center.CenterModel;
 import com.hartwig.hmftools.common.ecrf.CpctEcrfModel;
 import com.hartwig.hmftools.common.lims.LimsJsonModel;
+import com.hartwig.hmftools.patientreporter.ImmutableNotSequencedPatientReport;
+import com.hartwig.hmftools.patientreporter.ImmutableSampleReport;
+import com.hartwig.hmftools.patientreporter.NotSequencedPatientReport;
+import com.hartwig.hmftools.patientreporter.SampleReport;
 import com.hartwig.hmftools.patientreporter.report.ReportWriter;
-import com.hartwig.hmftools.patientreporter.util.PatientReportFormat;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import net.sf.dynamicreports.report.exception.DRException;
 
@@ -19,18 +25,27 @@ public class NotSequenceableReporter {
     private final LimsJsonModel limsModel;
     @NotNull
     private final ReportWriter reportWriter;
+    @NotNull
+    private final CenterModel centerModel;
 
     public NotSequenceableReporter(@NotNull final CpctEcrfModel cpctEcrfModel, @NotNull final LimsJsonModel limsModel,
-            @NotNull final ReportWriter reportWriter) {
+            @NotNull final CenterModel centerModel, @NotNull final ReportWriter reportWriter) {
         this.cpctEcrfModel = cpctEcrfModel;
         this.limsModel = limsModel;
+        this.centerModel = centerModel;
         this.reportWriter = reportWriter;
     }
 
-    public void run(@NotNull final String sample, @NotNull final NotSequenceableReason reason, @NotNull final NotSequenceableStudy study)
-            throws IOException, DRException {
+    public void run(@NotNull final String sample, @NotNull final NotSequenceableReason reason, @NotNull final NotSequenceableStudy study,
+            @NotNull final String signaturePath, @Nullable final String comments) throws IOException, DRException {
         final String tumorType = PatientReporterHelper.extractTumorType(cpctEcrfModel, sample);
-        final String tumorPercentageString = PatientReportFormat.formatNullablePercent(limsModel.tumorPercentageForSample(sample));
-        reportWriter.writeNonSequenceableReport(sample, tumorType, tumorPercentageString, reason, study);
+        final Double tumorPercentage = limsModel.tumorPercentageForSample(sample);
+        final String sampleRecipient = centerModel.getAddresseeStringForSample(sample);
+        final SampleReport sampleReport = ImmutableSampleReport.of(sample, tumorType, tumorPercentage, limsModel.barcodeForSample(sample),
+                limsModel.bloodBarcodeForSample(sample), limsModel.arrivalDateForSample(sample),
+                limsModel.bloodArrivalDateForSample(sample), limsModel.labProceduresForSample(sample), sampleRecipient);
+        final NotSequencedPatientReport report =
+                ImmutableNotSequencedPatientReport.of(sampleReport, reason, study, Optional.ofNullable(comments), signaturePath);
+        reportWriter.writeNonSequenceableReport(report);
     }
 }
