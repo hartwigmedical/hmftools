c3dae7b8d9e01b14563a66e8f50e28ae5368ca22
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-10-14 12:30:55 +0200

    Corrected mistake in output path for test reports

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/report/PDFWriterTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/report/PDFWriterTest.java
@@ -57,7 +57,7 @@ public class PDFWriterTest {
     private static final boolean SHOW_AND_PRINT = false;
     private static final boolean WRITE_TO_PDF = false;
 
-    private static final String REPORT_BASE_DIR = System.getProperty("user.home") + File.separator + "hmf " + File.separator + "tmp";
+    private static final String REPORT_BASE_DIR = System.getProperty("user.home") + File.separator + "hmf" + File.separator + "tmp";
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
 
