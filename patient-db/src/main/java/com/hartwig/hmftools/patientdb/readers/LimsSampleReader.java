32c57a506064ec0103193eb6329d2377dc35db8d
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-10-20 17:24:02 +0200

    Remove duplicate warning

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/LimsSampleReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/LimsSampleReader.java
@@ -7,13 +7,9 @@ import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.lims.Lims;
 import com.hartwig.hmftools.patientdb.data.SampleData;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 class LimsSampleReader {
-    private static final Logger LOGGER = LogManager.getLogger(LimsSampleReader.class);
-
     @NotNull
     private final Lims lims;
 
@@ -29,8 +25,6 @@ class LimsSampleReader {
             final LocalDate arrivalDate = lims.arrivalDateForSample(sampleId);
             if (arrivalDate != null) {
                 limsBiopsies.add(new SampleData(sampleId, arrivalDate, lims.samplingDateForSample(sampleId)));
-            } else {
-                LOGGER.warn("Could not find arrival date for sample: " + sampleId);
             }
         });
         return limsBiopsies;
