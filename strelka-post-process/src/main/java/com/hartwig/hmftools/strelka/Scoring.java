0a338059e0d7141d6109f10954b93dc7699d1475
Mircea Voda <mircearaul.voda@gmail.com>
2017-10-24 17:01:40 +0200

    refactor computing mnv frequency

--- strelka-post-process/src/main/java/com/hartwig/hmftools/strelka/Scoring.java
+++ strelka-post-process/src/main/java/com/hartwig/hmftools/strelka/Scoring.java
@@ -3,15 +3,12 @@ package com.hartwig.hmftools.strelka;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.hartwig.hmftools.strelka.scores.ImmutableReadScore;
 import com.hartwig.hmftools.strelka.scores.ReadScore;
 import com.hartwig.hmftools.strelka.scores.ReadType;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import htsjdk.samtools.SAMRecord;
@@ -19,8 +16,6 @@ import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.VariantContext;
 
 final class Scoring {
-    private static final Logger LOGGER = LogManager.getLogger(Scoring.class);
-
     private Scoring() {
     }
 
@@ -117,52 +112,5 @@ final class Scoring {
                 .stream()
                 .map(variant -> ImmutablePair.of(variant, getReadScore(record, variant)))
                 .collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
-    }
-
-    @NotNull
-    @VisibleForTesting
-    static Map<SAMRecord, Map<VariantContext, ReadScore>> correctMissingScores(
-            @NotNull final Map<SAMRecord, Map<VariantContext, ReadScore>> scores) {
-        final Map<VariantContext, Integer> averageScoresPerPosition = scores.entrySet()
-                .stream()
-                .flatMap(readEntry -> readEntry.getValue().entrySet().stream())
-                .filter(entry -> refOrAlt(entry.getValue().type()))
-                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(readScore -> readScore.getValue().score(),
-                        Collectors.collectingAndThen(Collectors.toList(),
-                                list -> (int) (list.stream().mapToLong(value -> (long) value).sum() / list.size())))));
-        return scores.entrySet()
-                .stream()
-                .map(readEntry -> correctEntry(readEntry, averageScoresPerPosition))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    @NotNull
-    private static Map.Entry<SAMRecord, Map<VariantContext, ReadScore>> correctEntry(
-            @NotNull final Map.Entry<SAMRecord, Map<VariantContext, ReadScore>> entry,
-            @NotNull final Map<VariantContext, Integer> averageScoresPerPosition) {
-        final Map.Entry<SAMRecord, Map<VariantContext, ReadScore>> correctedEntry = ImmutablePair.of(entry.getKey(), entry.getValue()
-                .entrySet()
-                .stream()
-                .map(variantEntry -> ImmutablePair.of(variantEntry.getKey(),
-                        correctRead(variantEntry.getValue(), averageScoresPerPosition.get(variantEntry.getKey()))))
-                .filter(variantEntry -> refOrAlt(variantEntry.getRight().type()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
-        final int readsWithOther = correctedEntry.getValue().entrySet().size() - entry.getValue().entrySet().size();
-        if (readsWithOther > 0) {
-            LOGGER.warn("Filtered out {} reads where read type was OTHER", readsWithOther);
-        }
-        return correctedEntry;
-    }
-
-    @NotNull
-    private static ReadScore correctRead(@NotNull final ReadScore score, final int averageScoreAtPosition) {
-        if (score.type() == ReadType.MISSING) {
-            return ImmutableReadScore.of(ReadType.REF, averageScoreAtPosition);
-        }
-        return score;
-    }
-
-    private static boolean refOrAlt(@NotNull final ReadType readType) {
-        return readType == ReadType.ALT || readType == ReadType.REF;
     }
 }
