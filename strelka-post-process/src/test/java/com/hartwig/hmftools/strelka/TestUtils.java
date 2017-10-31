b2a2c1239ddd01203ef8638b5059bddbf9673c00
Mircea Voda <mircearaul.voda@gmail.com>
2017-10-31 12:22:04 +0100

    cleanup strelka post process test utils

--- strelka-post-process/src/test/java/com/hartwig/hmftools/strelka/TestUtils.java
+++ strelka-post-process/src/test/java/com/hartwig/hmftools/strelka/TestUtils.java
@@ -1,13 +1,9 @@
 package com.hartwig.hmftools.strelka;
 
-import java.io.File;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.google.common.collect.Maps;
-import com.google.common.collect.Streams;
-import com.google.common.io.Resources;
 import com.hartwig.hmftools.strelka.scores.VariantScore;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -15,13 +11,8 @@ import org.jetbrains.annotations.NotNull;
 
 import htsjdk.samtools.SAMRecord;
 import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.vcf.VCFFileReader;
 
-public class TestUtils {
-    private static final File VCF_FILE = new File(Resources.getResource("mnvs.vcf").getPath());
-    private static final VCFFileReader VCF_FILE_READER = new VCFFileReader(VCF_FILE, false);
-    private static final List<VariantContext> VARIANTS = Streams.stream(VCF_FILE_READER).collect(Collectors.toList());
-
+class TestUtils {
     @NotNull
     static SAMRecord buildSamRecord(final int alignmentStart, @NotNull final String cigar, @NotNull final String readString,
             final boolean negativeStrand) {
