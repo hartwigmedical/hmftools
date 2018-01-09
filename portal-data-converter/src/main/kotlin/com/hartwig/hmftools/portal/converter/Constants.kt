6eaf00cefd6bb6b1e9f81df2ad4df935c53c1180
Mircea Voda <mircearaul.voda@gmail.com>
2018-01-09 16:54:19 +0100

    add portal data converter application

--- portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/Constants.kt
+++ portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/Constants.kt
@@ -1,3 +1,17 @@
 package com.hartwig.hmftools.portal.converter
 
 const val DEFAULT_VALUE = "-888"
+
+// CSV header fields
+const val SAMPLE_ID_FIELD = "sampleId"
+const val GENDER_FIELD = "gender"
+const val BIRTH_YEAR_FIELD = "birthYear"
+const val REGISTRATION_DATE_FIELD = "registrationDate"
+const val CANCER_TYPE_FIELD = "cancerType"
+const val BIOPSY_SITE_FIELD = "biopsySite"
+
+// CLI options
+
+const val RUNS_FILE = "runs_file"
+const val CLINICAL_DATA_CSV = "clinical_data_csv"
+const val OUTPUT_DIR = "out_dir"
---
+++ portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/PortalDataConverterApplication.kt
@@ -0,0 +1,70 @@
+package com.hartwig.hmftools.portal.converter
+
+import com.google.common.collect.ArrayListMultimap
+import com.google.common.collect.Multimap
+import com.hartwig.hmftools.common.context.ProductionRunContextFactory
+import com.hartwig.hmftools.common.context.RunContext
+import com.hartwig.hmftools.common.extensions.cli.createCommandLine
+import com.hartwig.hmftools.portal.converter.extensions.readCpctSamples
+import com.hartwig.hmftools.portal.converter.extensions.somaticVcfPath
+import com.hartwig.hmftools.portal.converter.records.SampleRecords
+import com.hartwig.hmftools.portal.converter.records.ssm.SimpleSomaticMutation
+import com.hartwig.hmftools.portal.converter.records.ssm.SimpleSomaticMutationMetadata
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.logging.log4j.LogManager
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors
+
+private val logger = LogManager.getLogger("PortalDataConverterApplication")
+
+fun main(args: Array<String>) {
+    val options = createOptions()
+    val cmd = options.createCommandLine("hmf-id-create", args)
+    val patientData = File(cmd.getOptionValue(CLINICAL_DATA_CSV)).readCpctSamples()
+    val runs = Files.lines(Paths.get(cmd.getOptionValue(RUNS_FILE))).map { it.trim() }.filter { it.isNotEmpty() }
+            .collect(Collectors.toList()).map { ProductionRunContextFactory.fromRunDirectory(it) }
+    convertSamples(runs, cmd.getOptionValue(OUTPUT_DIR), patientData)
+}
+
+private fun createOptions(): Options {
+    val options = Options()
+    options.addOption(Option.builder(RUNS_FILE).required().hasArg().desc("path to file containing runs to convert").build())
+    options.addOption(Option.builder(CLINICAL_DATA_CSV).required().hasArg()
+            .desc("path to csv file containing clinical data").build())
+    options.addOption(Option.builder(OUTPUT_DIR).required().hasArg().desc("output directory").build())
+    return options
+}
+
+private fun convertSamples(runContexts: List<RunContext>, outputDirectory: String, patientData: Map<String, SampleClinicalData>) {
+    val clinicalRecords: Multimap<String, SampleRecords> = ArrayListMultimap.create()
+
+    runContexts.forEach { it ->
+        val (clinicalData, somaticVcfPath) = getClinicalDataAndVcf(it, patientData) ?: return
+        val projectFolder = "$outputDirectory/HMF-${clinicalData.cancerType}"
+        clinicalRecords.put(projectFolder, SampleRecords(clinicalData))
+        TsvWriter.writeSomaticMutationMetadata(projectFolder, clinicalData.sampleId,
+                listOf(SimpleSomaticMutationMetadata(clinicalData.sampleId)))
+        TsvWriter.writeSimpleSomaticMutation(projectFolder, clinicalData.sampleId, SimpleSomaticMutation(somaticVcfPath))
+    }
+    clinicalRecords.keySet().forEach { TsvWriter.writeSampleRecords(it, clinicalRecords[it]) }
+}
+
+private fun getClinicalDataAndVcf(runContext: RunContext,
+                                  patientData: Map<String, SampleClinicalData>): Pair<SampleClinicalData, String>? {
+    val clinicalData = patientData[runContext.tumorSample()]
+    val somaticVcfPath = runContext.somaticVcfPath()
+    return when {
+        clinicalData == null -> {
+            logger.warn("Missing clinical data for sample id: ${runContext.tumorSample()}")
+            null
+        }
+        somaticVcfPath == null -> {
+            logger.warn("Could not locate somatic vcf file for set: ${runContext.setName()}")
+            null
+        }
+        else -> Pair(clinicalData, somaticVcfPath)
+    }
+}
