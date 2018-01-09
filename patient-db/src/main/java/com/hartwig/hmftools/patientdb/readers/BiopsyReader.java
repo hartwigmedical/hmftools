cfeb495fbbac18d3ebaa0493c6accb4611f11560
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-01-09 17:20:38 +0100

    DEV-270: Fixed biopsy taken present in different item group

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/BiopsyReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/BiopsyReader.java
@@ -22,6 +22,7 @@ public final class BiopsyReader {
 
     static final String STUDY_BIOPSY = "SE.BIOPSY";
     public static final String FORM_BIOPS = "FRM.BIOPS";
+    static final String ITEMGROUP_BIOPSY = "GRP.BIOPS.BIOPS";
     static final String ITEMGROUP_BIOPSIES = "GRP.BIOPS.BIOPSIES";
 
     public static final String FIELD_BIOPSY_DATE = "FLD.BIOPS.BIOPTDT";
@@ -40,13 +41,17 @@ public final class BiopsyReader {
         final List<BiopsyData> biopsies = Lists.newArrayList();
         for (final EcrfStudyEvent studyEvent : patient.studyEventsPerOID(STUDY_BIOPSY)) {
             for (final EcrfForm form : studyEvent.nonEmptyFormsPerOID(FORM_BIOPS, false)) {
-                for (final EcrfItemGroup itemGroup : form.nonEmptyItemGroupsPerOID(ITEMGROUP_BIOPSIES, false)) {
-                    final LocalDate date = itemGroup.readItemDate(FIELD_BIOPSY_DATE, 0, DATE_FORMATTER, false);
-                    final String biopsyTaken = itemGroup.readItemString(FIELD_BIOPSY_TAKEN, 0, false);
-                    final String location = itemGroup.readItemString(FIELD_LOCATION, 0, false);
+                String biopsyTaken = null;
+                for (final EcrfItemGroup biopsyGroup : form.nonEmptyItemGroupsPerOID(ITEMGROUP_BIOPSY, false)) {
+                    biopsyTaken = biopsyGroup.readItemString(FIELD_BIOPSY_TAKEN, 0, false);
+                }
+                for (final EcrfItemGroup biopsiesGroup : form.nonEmptyItemGroupsPerOID(ITEMGROUP_BIOPSIES, false)) {
+                    final LocalDate date = biopsiesGroup.readItemDate(FIELD_BIOPSY_DATE, 0, DATE_FORMATTER, false);
 
-                    final String site = itemGroup.readItemString(FIELD_SITE, 0, false);
-                    final String siteOther = itemGroup.readItemString(FIELD_SITE_OTHER, 0, false);
+                    final String location = biopsiesGroup.readItemString(FIELD_LOCATION, 0, false);
+
+                    final String site = biopsiesGroup.readItemString(FIELD_SITE, 0, false);
+                    final String siteOther = biopsiesGroup.readItemString(FIELD_SITE_OTHER, 0, false);
                     final String finalSite = (site == null || site.trim().toLowerCase().startsWith("other")) ? siteOther : site;
 
                     BiopsyData biopsy = ImmutableBiopsyData.of(date, biopsyTaken, finalSite, location, form.status(), form.locked());
--- patient-db/src/test/java/com/hartwig/hmftools/patientdb/readers/BiopsyReaderTest.java
+++ patient-db/src/test/java/com/hartwig/hmftools/patientdb/readers/BiopsyReaderTest.java
@@ -25,21 +25,25 @@ public class BiopsyReaderTest {
         List<BiopsyData> biopsies = BiopsyReader.read(buildTestPatient());
         assertEquals(1, biopsies.size());
         assertNotNull(biopsies.get(0).date());
+        assertNotNull(biopsies.get(0).biopsyTaken());
     }
 
     @NotNull
     private static EcrfPatient buildTestPatient() {
         final String patient = "dummy";
 
+        EcrfItemGroup biopsies1 = new EcrfItemGroup(patient);
+        biopsies1.addItem(BiopsyReader.FIELD_BIOPSY_DATE, "2017-11-10");
+        biopsies1.addItem(BiopsyReader.FIELD_LOCATION, "liver");
+        biopsies1.addItem(BiopsyReader.FIELD_SITE, "other");
+        biopsies1.addItem(BiopsyReader.FIELD_SITE_OTHER, "body");
+
         EcrfItemGroup biopsy1 = new EcrfItemGroup(patient);
         biopsy1.addItem(BiopsyReader.FIELD_BIOPSY_TAKEN, "Yes");
-        biopsy1.addItem(BiopsyReader.FIELD_BIOPSY_DATE, "2017-11-10");
-        biopsy1.addItem(BiopsyReader.FIELD_LOCATION, "liver");
-        biopsy1.addItem(BiopsyReader.FIELD_SITE, "other");
-        biopsy1.addItem(BiopsyReader.FIELD_SITE_OTHER, "body");
 
         EcrfForm form1 = new EcrfForm(patient, FormStatusState.SAVED, true);
-        form1.addItemGroup(BiopsyReader.ITEMGROUP_BIOPSIES, biopsy1);
+        form1.addItemGroup(BiopsyReader.ITEMGROUP_BIOPSIES, biopsies1);
+        form1.addItemGroup(BiopsyReader.ITEMGROUP_BIOPSY, biopsy1);
 
         EcrfItemGroup biopsy2 = new EcrfItemGroup(patient);
         EcrfForm form2 = new EcrfForm(patient, FormStatusState.SAVED, true);
