965e0dcbc72d5338fb4cd141ca553faa4c40bf92
Mircea Voda <mircearaul.voda@gmail.com>
2018-01-10 13:19:27 +0100

    refactor portal coverter sample records

--- portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/records/SampleRecords.kt
+++ portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/records/SampleRecords.kt
@@ -5,8 +5,8 @@ import com.hartwig.hmftools.portal.converter.records.donor.Donor
 import com.hartwig.hmftools.portal.converter.records.sample.Sample
 import com.hartwig.hmftools.portal.converter.records.specimen.Specimen
 
-data class SampleRecords(private val patientId: String, private val clinicalData: SampleClinicalData) {
+data class SampleRecords(private val patientId: String, private val sampleId: String, private val clinicalData: SampleClinicalData) {
     val donor = Donor(patientId, clinicalData.gender, clinicalData.ageAtEnrollment)
-    val sample = Sample(clinicalData.sampleId)
-    val specimen = Specimen(clinicalData.sampleId, clinicalData.specimenType, clinicalData.specimenTypeOther, patientId)
+    val sample = Sample(sampleId)
+    val specimen = Specimen(sampleId, clinicalData.specimenType, clinicalData.specimenTypeOther, patientId)
 }
