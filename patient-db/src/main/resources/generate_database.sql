5787b46e570b6f17846b4f24bb8e6c8a875c8a47
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-01-19 15:09:03 +0100

    Update database schema

--- patient-db/src/main/resources/generate_database.sql
+++ patient-db/src/main/resources/generate_database.sql
@@ -81,214 +81,6 @@ CREATE TABLE treatmentResponse
     FOREIGN KEY (patientId) REFERENCES patient(id)
 );
 
-DROP TABLE IF EXISTS comprehensiveSomaticVariant;
-DROP TABLE IF EXISTS somaticVariant;
-CREATE TABLE somaticVariant
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    chromosome varchar(255) NOT NULL,
-    position int not null,
-    filter varchar(255) NOT NULL,
-    type varchar(255) NOT NULL,
-    ref varchar(255) NOT NULL,
-    alt varchar(255) NOT NULL,
-    gene varchar(255) NOT NULL,
-    cosmicId varchar(255) NOT NULL,
-    dbsnpId varchar(255) NOT NULL,
-    effect varchar(255) NOT NULL,
-    microhomology varchar(255) NOT NULL,
-    repeatSequence varchar(255) NOT NULL,
-    repeatCount int NOT NULL,
-    alleleReadCount int NOT NULL,
-    totalReadCount int NOT NULL,
-    adjustedVaf DOUBLE PRECISION NOT NULL,
-    adjustedCopyNumber DOUBLE PRECISION NOT NULL,
-    highConfidence BOOLEAN NOT NULL,
-    trinucleotideContext varchar(3) NOT NULL,
-    clonality varchar(455) NOT NULL,
-    loh BOOLEAN NOT NULL,
-    mappability DOUBLE PRECISION NOT NULL,
-    germlineStatus varchar(255) NOT NULL,
-    minorAllelePloidy DOUBLE PRECISION NOT NULL,
-    PRIMARY KEY (id),
-    INDEX(sampleId),
-    INDEX(filter),
-    INDEX(type),
-    INDEX(gene)
-);
-
-DROP TABLE IF EXISTS copyNumber;
-CREATE TABLE copyNumber
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    chromosome varchar(255) NOT NULL,
-    start int not null,
-    end int not null,
-    segmentStartSupport varchar(255) NOT NULL,
-    segmentEndSupport varchar(255) NOT NULL,
-    bafCount int not null,
-    observedBaf DOUBLE PRECISION not null,
-    actualBaf DOUBLE PRECISION not null,
-    copyNumber DOUBLE PRECISION not null,
-    copyNumberMethod varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX(sampleId)
-);
-
-DROP TABLE IF EXISTS copyNumberGermline;
-CREATE TABLE copyNumberGermline
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    chromosome varchar(255) NOT NULL,
-    start int not null,
-    end int not null,
-    segmentStartSupport varchar(255) NOT NULL,
-    segmentEndSupport varchar(255) NOT NULL,
-    bafCount int not null,
-    observedBaf DOUBLE PRECISION not null,
-    actualBaf DOUBLE PRECISION not null,
-    copyNumber DOUBLE PRECISION not null,
-    copyNumberMethod varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX(sampleId)
-);
-
-DROP TABLE IF EXISTS purityRange;
-CREATE TABLE purityRange
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    purity DOUBLE PRECISION not null,
-    normFactor DOUBLE PRECISION not null,
-    score DOUBLE PRECISION not null,
-    ploidy DOUBLE PRECISION not null,
-    diploidProportion DOUBLE PRECISION not null,
-    PRIMARY KEY (id),
-    INDEX(sampleId)
-);
-
-DROP TABLE IF EXISTS purityScore;
-DROP VIEW IF EXISTS purity;
-DROP TABLE IF EXISTS purity;
-CREATE TABLE purity
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    version VARCHAR(255) NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    gender varchar(255) NOT NULL,
-    status varchar(255) NOT NULL,
-    qcStatus varchar(255) NOT NULL,
-    purity DOUBLE PRECISION not null,
-    normFactor DOUBLE PRECISION not null,
-    score DOUBLE PRECISION not null,
-    ploidy DOUBLE PRECISION not null,
-    diploidProportion DOUBLE PRECISION not null,
-    polyclonalProportion DOUBLE PRECISION not null,
-    minPurity DOUBLE PRECISION not null,
-    maxPurity DOUBLE PRECISION not null,
-    minPloidy DOUBLE PRECISION not null,
-    maxPloidy DOUBLE PRECISION not null,
-    minDiploidProportion DOUBLE PRECISION not null,
-    maxDiploidProportion DOUBLE PRECISION not null,
-    PRIMARY KEY (id),
-    INDEX(sampleId)
-);
-
-
-DROP TABLE IF EXISTS copyNumberRegion;
-CREATE TABLE copyNumberRegion
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    chromosome varchar(255) NOT NULL,
-    start int not null,
-    end int not null,
-    germlineStatus varchar(255) NOT NULL,
-    svCluster BOOLEAN NOT NULL,
-    ratioSupport BOOLEAN NOT NULL,
-    segmentStartSupport varchar(255) NOT NULL,
-    bafCount int not null,
-    observedBaf DOUBLE PRECISION not null,
-    observedTumorRatio DOUBLE PRECISION not null,
-    observedNormalRatio DOUBLE PRECISION not null,
-    observedTumorRatioCount int not null,
-    gcContent DOUBLE PRECISION not null,
-    modelPloidy int not null,
-    modelBaf DOUBLE PRECISION not null,
-    modelTumorRatio DOUBLE PRECISION not null,
-    actualTumorBaf DOUBLE PRECISION not null,
-    actualTumorCopyNumber DOUBLE PRECISION not null,
-    refNormalisedTumorCopyNumber DOUBLE PRECISION not null,
-    cnvDeviation DOUBLE PRECISION not null,
-    bafDeviation DOUBLE PRECISION not null,
-    totalDeviation DOUBLE PRECISION not null,
-    ploidyPenalty DOUBLE PRECISION not null,
-    fittedBaf DOUBLE PRECISION not null,
-    fittedCopyNumber DOUBLE PRECISION not null,
-    PRIMARY KEY (id),
-    INDEX(sampleId)
-);
-
-DROP TABLE IF EXISTS structuralVariant;
-CREATE TABLE structuralVariant
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    startChromosome varchar(255) NOT NULL,
-    endChromosome varchar(255) NOT NULL,
-    startPosition int not null,
-    endPosition int not null,
-    startOrientation tinyint not null,
-    endOrientation tinyint not null,
-    startHomologySequence varchar(255) not null,
-    endHomologySequence varchar(255) not null,
-    startAF DOUBLE PRECISION,
-    endAF DOUBLE PRECISION,
-    ploidy DOUBLE PRECISION,
-    adjustedStartAF DOUBLE PRECISION,
-    adjustedEndAF DOUBLE PRECISION,
-    adjustedStartCopyNumber DOUBLE PRECISION,
-    adjustedEndCopyNumber DOUBLE PRECISION,
-    adjustedStartCopyNumberChange DOUBLE PRECISION,
-    adjustedEndCopyNumberChange DOUBLE PRECISION,
-    insertSequence varchar(255) not null,
-    type varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX(sampleId)
-);
-
-DROP TABLE IF EXISTS geneCopyNumber;
-CREATE TABLE geneCopyNumber
-(   id int NOT NULL AUTO_INCREMENT,
-    modified DATETIME NOT NULL,
-    sampleId varchar(255) NOT NULL,
-    chromosome varchar(255) NOT NULL,
-    start int not null,
-    end int not null,
-    gene varchar(255) NOT NULL,
-    chromosomeBand varchar(255) NOT NULL,
-    transcriptId varchar(255) NOT NULL,
-    transcriptVersion int not null,
-    minCopyNumber DOUBLE PRECISION not null,
-    maxCopyNumber DOUBLE PRECISION not null,
-    meanCopyNumber DOUBLE PRECISION not null,
-    somaticRegions int not null,
-    germlineHomRegions int not null,
-    germlineHetRegions int not null,
-    minRegions int not null,
-    minRegionStart int not null,
-    minRegionEnd int not null,
-    minRegionStartSupport varchar(255) NOT NULL,
-    minRegionEndSupport varchar(255) NOT NULL,
-    minRegionMethod varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX(sampleId),
-    INDEX(gene)
-);
-
 DROP TABLE IF EXISTS clinicalFindings;
 CREATE TABLE clinicalFindings
 (   id int NOT NULL AUTO_INCREMENT,
@@ -341,7 +133,6 @@ CREATE TABLE ecrfDatamodel
     relevant varchar(5)
 );
 
-
 DROP TABLE IF EXISTS formsMetadata;
 CREATE TABLE formsMetadata
 (   id int NOT NULL,
@@ -389,10 +180,211 @@ CREATE TABLE drupEcrfDatamodel
     relevant varchar(5)
 );
 
-DROP TABLE IF EXISTS structuralVariantDisruption;
-DROP TABLE IF EXISTS structuralVariantFusion;
-DROP TABLE IF EXISTS structuralVariantBreakend;
+DROP TABLE IF EXISTS somaticVariant;
+CREATE TABLE somaticVariant
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    chromosome varchar(255) NOT NULL,
+    position int not null,
+    filter varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    ref varchar(255) NOT NULL,
+    alt varchar(255) NOT NULL,
+    gene varchar(255) NOT NULL,
+    cosmicId varchar(255) NOT NULL,
+    dbsnpId varchar(255) NOT NULL,
+    effect varchar(255) NOT NULL,
+    microhomology varchar(255) NOT NULL,
+    repeatSequence varchar(255) NOT NULL,
+    repeatCount int NOT NULL,
+    alleleReadCount int NOT NULL,
+    totalReadCount int NOT NULL,
+    adjustedVaf DOUBLE PRECISION NOT NULL,
+    adjustedCopyNumber DOUBLE PRECISION NOT NULL,
+    highConfidence BOOLEAN NOT NULL,
+    trinucleotideContext varchar(3) NOT NULL,
+    clonality varchar(455) NOT NULL,
+    loh BOOLEAN NOT NULL,
+    mappability DOUBLE PRECISION NOT NULL,
+    germlineStatus varchar(255) NOT NULL,
+    minorAllelePloidy DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (id),
+    INDEX(sampleId),
+    INDEX(filter),
+    INDEX(type),
+    INDEX(gene)
+);
 
+DROP TABLE IF EXISTS purity;
+CREATE TABLE purity
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    version VARCHAR(255) NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    gender varchar(255) NOT NULL,
+    status varchar(255) NOT NULL,
+    qcStatus varchar(255) NOT NULL,
+    purity DOUBLE PRECISION not null,
+    normFactor DOUBLE PRECISION not null,
+    score DOUBLE PRECISION not null,
+    ploidy DOUBLE PRECISION not null,
+    diploidProportion DOUBLE PRECISION not null,
+    polyclonalProportion DOUBLE PRECISION not null,
+    minPurity DOUBLE PRECISION not null,
+    maxPurity DOUBLE PRECISION not null,
+    minPloidy DOUBLE PRECISION not null,
+    maxPloidy DOUBLE PRECISION not null,
+    minDiploidProportion DOUBLE PRECISION not null,
+    maxDiploidProportion DOUBLE PRECISION not null,
+    PRIMARY KEY (id),
+    INDEX(sampleId)
+);
+
+DROP TABLE IF EXISTS purityRange;
+CREATE TABLE purityRange
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    purity DOUBLE PRECISION not null,
+    normFactor DOUBLE PRECISION not null,
+    score DOUBLE PRECISION not null,
+    ploidy DOUBLE PRECISION not null,
+    diploidProportion DOUBLE PRECISION not null,
+    PRIMARY KEY (id),
+    INDEX(sampleId)
+);
+
+DROP TABLE IF EXISTS copyNumber;
+CREATE TABLE copyNumber
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    chromosome varchar(255) NOT NULL,
+    start int not null,
+    end int not null,
+    segmentStartSupport varchar(255) NOT NULL,
+    segmentEndSupport varchar(255) NOT NULL,
+    bafCount int not null,
+    observedBaf DOUBLE PRECISION not null,
+    actualBaf DOUBLE PRECISION not null,
+    copyNumber DOUBLE PRECISION not null,
+    copyNumberMethod varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX(sampleId)
+);
+
+DROP TABLE IF EXISTS copyNumberGermline;
+CREATE TABLE copyNumberGermline
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    chromosome varchar(255) NOT NULL,
+    start int not null,
+    end int not null,
+    segmentStartSupport varchar(255) NOT NULL,
+    segmentEndSupport varchar(255) NOT NULL,
+    bafCount int not null,
+    observedBaf DOUBLE PRECISION not null,
+    actualBaf DOUBLE PRECISION not null,
+    copyNumber DOUBLE PRECISION not null,
+    copyNumberMethod varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX(sampleId)
+);
+
+DROP TABLE IF EXISTS copyNumberRegion;
+CREATE TABLE copyNumberRegion
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    chromosome varchar(255) NOT NULL,
+    start int not null,
+    end int not null,
+    germlineStatus varchar(255) NOT NULL,
+    svCluster BOOLEAN NOT NULL,
+    ratioSupport BOOLEAN NOT NULL,
+    segmentStartSupport varchar(255) NOT NULL,
+    bafCount int not null,
+    observedBaf DOUBLE PRECISION not null,
+    observedTumorRatio DOUBLE PRECISION not null,
+    observedNormalRatio DOUBLE PRECISION not null,
+    observedTumorRatioCount int not null,
+    gcContent DOUBLE PRECISION not null,
+    modelPloidy int not null,
+    modelBaf DOUBLE PRECISION not null,
+    modelTumorRatio DOUBLE PRECISION not null,
+    actualTumorBaf DOUBLE PRECISION not null,
+    actualTumorCopyNumber DOUBLE PRECISION not null,
+    refNormalisedTumorCopyNumber DOUBLE PRECISION not null,
+    cnvDeviation DOUBLE PRECISION not null,
+    bafDeviation DOUBLE PRECISION not null,
+    totalDeviation DOUBLE PRECISION not null,
+    ploidyPenalty DOUBLE PRECISION not null,
+    fittedBaf DOUBLE PRECISION not null,
+    fittedCopyNumber DOUBLE PRECISION not null,
+    PRIMARY KEY (id),
+    INDEX(sampleId)
+);
+
+DROP TABLE IF EXISTS geneCopyNumber;
+CREATE TABLE geneCopyNumber
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    chromosome varchar(255) NOT NULL,
+    start int not null,
+    end int not null,
+    gene varchar(255) NOT NULL,
+    chromosomeBand varchar(255) NOT NULL,
+    transcriptId varchar(255) NOT NULL,
+    transcriptVersion int not null,
+    minCopyNumber DOUBLE PRECISION not null,
+    maxCopyNumber DOUBLE PRECISION not null,
+    meanCopyNumber DOUBLE PRECISION not null,
+    somaticRegions int not null,
+    germlineHomRegions int not null,
+    germlineHetRegions int not null,
+    minRegions int not null,
+    minRegionStart int not null,
+    minRegionEnd int not null,
+    minRegionStartSupport varchar(255) NOT NULL,
+    minRegionEndSupport varchar(255) NOT NULL,
+    minRegionMethod varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX(sampleId),
+    INDEX(gene)
+);
+
+DROP TABLE IF EXISTS structuralVariant;
+CREATE TABLE structuralVariant
+(   id int NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId varchar(255) NOT NULL,
+    startChromosome varchar(255) NOT NULL,
+    endChromosome varchar(255) NOT NULL,
+    startPosition int not null,
+    endPosition int not null,
+    startOrientation tinyint not null,
+    endOrientation tinyint not null,
+    startHomologySequence varchar(255) not null,
+    endHomologySequence varchar(255) not null,
+    startAF DOUBLE PRECISION,
+    endAF DOUBLE PRECISION,
+    ploidy DOUBLE PRECISION,
+    adjustedStartAF DOUBLE PRECISION,
+    adjustedEndAF DOUBLE PRECISION,
+    adjustedStartCopyNumber DOUBLE PRECISION,
+    adjustedEndCopyNumber DOUBLE PRECISION,
+    adjustedStartCopyNumberChange DOUBLE PRECISION,
+    adjustedEndCopyNumberChange DOUBLE PRECISION,
+    insertSequence varchar(255) not null,
+    type varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX(sampleId)
+);
+
+DROP TABLE IF EXISTS structuralVariantBreakend;
 CREATE TABLE structuralVariantBreakend
 (   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     modified DATETIME NOT NULL,
@@ -415,18 +407,18 @@ CREATE TABLE structuralVariantBreakend
     INDEX(transcriptId)
 );
 
+DROP TABLE IF EXISTS structuralVariantDisruption;
 CREATE TABLE structuralVariantDisruption
-(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+(   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     breakendId INT UNSIGNED NOT NULL,
     isReported BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     INDEX(breakendId)
 );
 
+DROP TABLE IF EXISTS structuralVariantFusion;
 CREATE TABLE structuralVariantFusion
-(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+(   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fivePrimeBreakendId INT UNSIGNED NOT NULL,
     threePrimeBreakendId INT UNSIGNED NOT NULL,
     isReported BOOLEAN NOT NULL,
