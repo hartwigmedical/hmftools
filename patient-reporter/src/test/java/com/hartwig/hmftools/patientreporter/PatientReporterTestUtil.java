03d82f5b381f9d768bbec32efeadc30e8fbae72f
Mircea Voda <mircearaul.voda@gmail.com>
2018-01-22 11:51:10 +0100

    update patient reporter tests

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/PatientReporterTestUtil.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/PatientReporterTestUtil.java
@@ -1,27 +1,40 @@
 package com.hartwig.hmftools.patientreporter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 import com.hartwig.hmftools.common.center.Center;
 import com.hartwig.hmftools.common.center.CenterModel;
+import com.hartwig.hmftools.common.cosmic.fusions.CosmicFusionModel;
+import com.hartwig.hmftools.common.cosmic.fusions.CosmicFusions;
+import com.hartwig.hmftools.common.cosmic.genes.CosmicGeneModel;
+import com.hartwig.hmftools.common.cosmic.genes.CosmicGenes;
 import com.hartwig.hmftools.common.ecrf.CpctEcrfModel;
 import com.hartwig.hmftools.common.ecrf.reader.ImmutableXMLEcrfDatamodel;
 import com.hartwig.hmftools.common.exception.EmptyFileException;
 import com.hartwig.hmftools.common.exception.HartwigException;
+import com.hartwig.hmftools.common.gene.GeneModel;
 import com.hartwig.hmftools.common.lims.Lims;
 import com.hartwig.hmftools.common.lims.LimsFactory;
+import com.hartwig.hmftools.common.variant.SomaticVariant;
+import com.hartwig.hmftools.hmfslicer.HmfGenePanelSupplier;
+import com.hartwig.hmftools.patientreporter.algo.MSIAnalyzer;
+import com.hartwig.hmftools.patientreporter.filters.DrupFilter;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import htsjdk.samtools.reference.IndexedFastaSequenceFile;
 
 public final class PatientReporterTestUtil {
 
     public static final String SIGNATURE_PATH = Resources.getResource("signature").getPath() + File.separator + "signature.png";
 
     private PatientReporterTestUtil() {
-
     }
 
     @NotNull
@@ -29,7 +42,28 @@ public final class PatientReporterTestUtil {
         final String drupFilterPath = Resources.getResource("csv").getPath() + File.separator + "drup_genes.csv";
         final String cosmicPath = Resources.getResource("csv").getPath() + File.separator + "cosmic_slice.csv";
         final String fusionPath = Resources.getResource("csv").getPath() + File.separator + "cosmic_gene_fusions.csv";
-        return HmfReporterDataLoader.buildFromFiles(drupFilterPath, cosmicPath, fusionPath);
+        final GeneModel geneModel = new GeneModel(HmfGenePanelSupplier.hmfGeneMap());
+        final DrupFilter drupFilter = new DrupFilter(drupFilterPath);
+        final CosmicGeneModel cosmicGeneModel = CosmicGenes.buildModelFromCsv(cosmicPath);
+        final CosmicFusionModel fusionModel = CosmicFusions.readFromCSV(fusionPath);
+        final MSIAnalyzer msiAnalyzer = testMSIAnalyzer();
+        return ImmutableHmfReporterData.of(geneModel, cosmicGeneModel, drupFilter, fusionModel, msiAnalyzer);
+    }
+
+    @NotNull
+    private static MSIAnalyzer testMSIAnalyzer() throws FileNotFoundException {
+        return new MSIAnalyzer() {
+            @Override
+            @Nullable
+            public IndexedFastaSequenceFile reference() {
+                return null;
+            }
+
+            @Override
+            public double analyzeVariants(@NotNull final List<SomaticVariant> variants) throws FileNotFoundException {
+                return 0.91;
+            }
+        };
     }
 
     @NotNull
