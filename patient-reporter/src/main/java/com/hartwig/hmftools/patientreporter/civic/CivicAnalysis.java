5162269d2e82b25c2f7ebc5fddf800c437b9981c
Mircea Voda <mircearaul.voda@gmail.com>
2018-02-05 17:25:30 +0100

    add wild type variants to civic evidence report

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/civic/CivicAnalysis.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/civic/CivicAnalysis.java
@@ -1,7 +1,11 @@
 package com.hartwig.hmftools.patientreporter.civic;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -18,6 +22,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import io.reactivex.Observable;
+
 public class CivicAnalysis {
     private static final Logger LOGGER = LogManager.getLogger(CivicAnalysis.class);
 
@@ -29,8 +35,9 @@ public class CivicAnalysis {
         if (relevantDoids.isEmpty()) {
             LOGGER.warn("  Disease-ontology id set for this tumor is empty!");
         }
-        final List<Alteration> alterations = civicVariantAlterations(reportedVariants, geneModel, relevantDoids);
-        alterations.addAll(civicCopyNumberAlterations(copyNumbers, geneModel, relevantDoids));
+        final Collection<HmfGenomeRegion> geneRegions = geneModel.regions().stream().filter(distinctByGene()).collect(Collectors.toList());
+        final List<Alteration> alterations = civicVariantAlterations(reportedVariants, geneRegions, relevantDoids);
+        alterations.addAll(civicCopyNumberAlterations(copyNumbers, geneRegions, relevantDoids));
         return alterations;
     }
 
@@ -57,58 +64,79 @@ public class CivicAnalysis {
 
     @NotNull
     private static List<Alteration> civicVariantAlterations(@NotNull final List<VariantReport> reportedVariants,
-            @NotNull final GeneModel geneModel, @NotNull final Set<String> relevantDoids) {
+            @NotNull final Collection<HmfGenomeRegion> geneRegions, @NotNull final Set<String> relevantDoids) {
         LOGGER.info("  Fetching civic variant alterations...");
         final List<Alteration> alterations = Lists.newArrayList();
         final CivicApiWrapper civicApi = new CivicApiWrapper();
-        for (final VariantReport variantReport : reportedVariants) {
-            for (final HmfGenomeRegion region : geneModel.regions()) {
-                if (region.gene().equals(variantReport.gene())) {
-                    for (final int entrezId : region.entrezId()) {
-                        try {
-                            final List<CivicVariant> civicVariants = civicApi.getVariantsForGene(entrezId).toList().blockingGet();
-                            final Alteration alteration = Alteration.from(variantReport, civicVariants, relevantDoids);
-                            if (alteration.getMatches().size() > 0) {
-                                alterations.add(alteration);
-                            }
-                        } catch (final Throwable throwable) {
-                            LOGGER.error("  Failed to get civic variants for variant: " + variantReport.variant().chromosomePosition()
-                                    + ". error message: " + throwable.getMessage());
-                        }
-                    }
+        final List<HmfGenomeRegion> wildTypeGenes = Lists.newArrayList();
+        for (final HmfGenomeRegion region : geneRegions) {
+            final List<VariantReport> reportedVariantsInGene = reportedVariants.stream()
+                    .filter(variantReport -> region.gene().equals(variantReport.gene()))
+                    .collect(Collectors.toList());
+            if (!reportedVariantsInGene.isEmpty()) {
+                for (final VariantReport variantReport : reportedVariantsInGene) {
+                    alterations.addAll(queryCivicAlteration(region,
+                            variantList -> Alteration.from(variantReport, variantList, relevantDoids),
+                            "  Failed to get civic variants for variant: " + variantReport.variant().chromosomePosition()));
                 }
+            } else {
+                wildTypeGenes.add(region);
             }
         }
+        final List<Alteration> wildTypeAlterations = Observable.fromIterable(wildTypeGenes)
+                .flatMap(hmfGenomeRegion -> civicApi.getWildTypeVariantsForGeneIds(hmfGenomeRegion.entrezId())
+                        .toList()
+                        .map(variantList -> Alteration.fromWildType(hmfGenomeRegion.gene(), variantList, relevantDoids))
+                        .filter(alteration -> !alteration.getMatches().isEmpty())
+                        .toObservable())
+                .toList()
+                .blockingGet();
+        alterations.addAll(wildTypeAlterations);
         civicApi.releaseResources();
         return alterations;
     }
 
     @NotNull
     private static List<Alteration> civicCopyNumberAlterations(@NotNull final List<GeneCopyNumber> copyNumbers,
-            @NotNull final GeneModel geneModel, @NotNull final Set<String> relevantDoids) {
+            @NotNull final Collection<HmfGenomeRegion> geneRegions, @NotNull final Set<String> relevantDoids) {
         LOGGER.info("  Fetching civic copy number alterations...");
         final List<Alteration> alterations = Lists.newArrayList();
-        final CivicApiWrapper civicApi = new CivicApiWrapper();
         for (final GeneCopyNumber copyNumberReport : copyNumbers) {
-            for (final HmfGenomeRegion region : geneModel.regions()) {
+            for (final HmfGenomeRegion region : geneRegions) {
                 if (region.gene().equals(copyNumberReport.gene())) {
-                    for (final int entrezId : region.entrezId()) {
-                        try {
-                            final List<CivicVariant> civicVariants = civicApi.getVariantsForGene(entrezId).toList().blockingGet();
-                            final Alteration alteration = Alteration.from(copyNumberReport, civicVariants, relevantDoids);
-                            if (alteration.getMatches().size() > 0) {
-                                alterations.add(alteration);
-                            }
-                        } catch (final Throwable throwable) {
-                            LOGGER.error("  Failed to get civic variants for copy number: {}. error message: {}",
-                                    copyNumberReport.gene(),
-                                    throwable.getMessage());
-                        }
-                    }
+                    alterations.addAll(queryCivicAlteration(region,
+                            variantList -> Alteration.from(copyNumberReport, variantList, relevantDoids),
+                            "  Failed to get civic variants for copy number: " + copyNumberReport.gene()));
                 }
             }
         }
-        civicApi.releaseResources();
         return alterations;
+    }
+
+    @NotNull
+    private static List<Alteration> queryCivicAlteration(@NotNull final HmfGenomeRegion region,
+            io.reactivex.functions.Function<List<CivicVariant>, Alteration> alterationBuilder, @NotNull final String error) {
+        final CivicApiWrapper civicApi = new CivicApiWrapper();
+        final List<Alteration> result = Lists.newArrayList();
+        try {
+            result.addAll(Observable.fromIterable(region.entrezId())
+                    .flatMap(civicApi::getVariantsForGene)
+                    .toList()
+                    .map(alterationBuilder)
+                    .filter(alteration -> !alteration.getMatches().isEmpty())
+                    .toObservable()
+                    .toList()
+                    .blockingGet());
+        } catch (final Throwable throwable) {
+            LOGGER.error("{}. Error message: {}", error, throwable.getMessage());
+        }
+        civicApi.releaseResources();
+        return result;
+    }
+
+    @NotNull
+    private static Predicate<HmfGenomeRegion> distinctByGene() {
+        final Set<String> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(t.geneID());
     }
 }
