ff36635b274bea645fa03ae04f75e82ecc311b6b
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-02-09 14:06:01 +0100

    Bump patient reporter version

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
@@ -47,7 +47,7 @@ public class PatientReporterApplication {
     public static final String VERSION = PatientReporterApplication.class.getPackage().getImplementationVersion();
 
     // KODU: For testing
-    //    public static final String VERSION = "4.4";
+    //    public static final String VERSION = "4.5";
 
     private static final String CANCER_TYPES_CSV = "cancer_types_csv";
     private static final String LIMS_JSON = "lims_json";
