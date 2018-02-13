18cece0a63971970fa81b9da2c7a524973ce29f3
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-02-13 15:49:30 +0100

    Fix ploidy formatting in test

--- purity-ploidy-estimator/src/test/java/com/hartwig/hmftools/purple/plot/ChartWriterTest.java
+++ purity-ploidy-estimator/src/test/java/com/hartwig/hmftools/purple/plot/ChartWriterTest.java
@@ -2,14 +2,23 @@ package com.hartwig.hmftools.purple.plot;
 
 import static org.junit.Assert.assertEquals;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class ChartWriterTest {
 
     @Test
     public void testSubtitle() {
-        assertEquals("COLO829 PUR:100% PLE:2.01", ChartWriter.subtitle("COLO829", 1.0, 2.01));
-        assertEquals("CPCT012345 PUR:83% PLE:1.10", ChartWriter.subtitle("CPCT012345", 0.83, 1.10));
-        assertEquals("CPCT012345 PUR:83% PLE:1.10", ChartWriter.subtitle("CPCT012345", 0.83, 1.10));
+        assertEquals("COLO829 PUR:100% PLE:" + formattedPloidy(2.01), ChartWriter.subtitle("COLO829", 1.0, 2.01));
+        assertEquals("CPCT012345 PUR:83% PLE:" + formattedPloidy(1.10), ChartWriter.subtitle("CPCT012345", 0.83, 1.10));
+        assertEquals("CPCT012345 PUR:83% PLE:" + formattedPloidy(1.10), ChartWriter.subtitle("CPCT012345", 0.83, 1.10));
+    }
+
+    @NotNull
+    private static String formattedPloidy(double ploidy) {
+        return String.format("%.2f", ploidy);
     }
 }
