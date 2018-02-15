8f6aa950c15263838180dc8d6d19762969eedd40
Jonathan Baber <jonbaber@gmail.com>
2018-02-15 13:28:29 +1000

    Added licensing info to KernelEstimator

--- hmf-common/src/main/java/com/hartwig/hmftools/common/kde/KernelEstimator.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/kde/KernelEstimator.java
@@ -1,6 +1,26 @@
 package com.hartwig.hmftools.common.kde;
 
-// JOBA: This is a modified version of the WEKA KernelEstimator
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    KernelEstimator.java
+ *    Copyright (C) 1999 University of Waikato, Hamilton, New Zealand
+ *
+ */
 public class KernelEstimator   {
     private double[] m_Values = new double[50];
     private double[] m_Weights = new double[50];
