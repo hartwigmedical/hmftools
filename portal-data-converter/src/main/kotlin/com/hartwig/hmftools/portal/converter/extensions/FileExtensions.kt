7d06124dfd5dbec80ded8825a8b191a9d6eb95a4
Mircea Voda <mircearaul.voda@gmail.com>
2018-03-06 12:18:08 +0100

    warn on csv entries with null cpct id

--- portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/extensions/FileExtensions.kt
+++ portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/extensions/FileExtensions.kt
@@ -3,10 +3,19 @@ package com.hartwig.hmftools.portal.converter.extensions
 import com.hartwig.hmftools.portal.converter.SampleClinicalData
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
+import org.apache.logging.log4j.LogManager
 import java.io.File
 import java.nio.charset.Charset
 
+private val logger = LogManager.getLogger("FileExtensions")
+
 fun File.readSamplesData(): List<SampleClinicalData> {
     val parser = CSVParser.parse(this, Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader())
-    return parser.map { SampleClinicalData(it) }
+    return parser.map { csvRecord ->
+        val sampleData = SampleClinicalData(csvRecord)
+        if (sampleData.cpctId == null) {
+            logger.warn("Read a CSV record with null CPCT ID")
+        }
+        sampleData
+    }
 }
