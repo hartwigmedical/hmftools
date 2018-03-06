22a1beeae61b319e47637b9b230e2eec4d4e6cd4
Mircea Voda <mircearaul.voda@gmail.com>
2018-03-06 10:11:22 +0100

    log variants with unknown mutation type

--- portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/records/ssm/SimpleSomaticMutation.kt
+++ portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/records/ssm/SimpleSomaticMutation.kt
@@ -8,11 +8,13 @@ import com.hartwig.hmftools.portal.converter.extensions.toRecord
 import com.hartwig.hmftools.portal.converter.records.Record
 import htsjdk.variant.variantcontext.VariantContext
 import htsjdk.variant.vcf.VCFFileReader
+import org.apache.logging.log4j.LogManager
 import java.io.File
 import kotlin.reflect.KClass
 
 data class SimpleSomaticMutation(private val fields: Map<SimpleSomaticMutationHeader, String>) : Record {
     companion object Factory {
+        private val logger = LogManager.getLogger(SimpleSomaticMutation::class)
         val header: KClass<SimpleSomaticMutationHeader> = SimpleSomaticMutationHeader::class
         private val CHROMOSOME_STRAND = "1"                             //MIVO: 1 = forward
         private val VERIFICATION_STATUS = "2"                           //MIVO: 2 = not tested
@@ -20,21 +22,19 @@ data class SimpleSomaticMutation(private val fields: Map<SimpleSomaticMutationHe
                                     ref: String, alt: String): SimpleSomaticMutation {
             val controlGenotype = "$ref/$ref"
             val tumorGenotype = "$alt/$alt"
-            return SimpleSomaticMutation(mapOf(
-                    SimpleSomaticMutationHeader.analysis_id to analysisId,
-                    SimpleSomaticMutationHeader.analyzed_sample_id to sampleId,
-                    SimpleSomaticMutationHeader.mutation_type to mutationType,
-                    SimpleSomaticMutationHeader.chromosome to chromosome,
-                    SimpleSomaticMutationHeader.chromosome_start to start.toString(),
-                    SimpleSomaticMutationHeader.chromosome_end to end.toString(),
-                    SimpleSomaticMutationHeader.control_genotype to controlGenotype,
-                    SimpleSomaticMutationHeader.tumour_genotype to tumorGenotype,
-                    SimpleSomaticMutationHeader.reference_genome_allele to ref,
-                    SimpleSomaticMutationHeader.mutated_from_allele to ref,
-                    SimpleSomaticMutationHeader.mutated_to_allele to alt,
-                    SimpleSomaticMutationHeader.chromosome_strand to CHROMOSOME_STRAND,
-                    SimpleSomaticMutationHeader.verification_status to VERIFICATION_STATUS
-            ))
+            return SimpleSomaticMutation(mapOf(SimpleSomaticMutationHeader.analysis_id to analysisId,
+                                               SimpleSomaticMutationHeader.analyzed_sample_id to sampleId,
+                                               SimpleSomaticMutationHeader.mutation_type to mutationType,
+                                               SimpleSomaticMutationHeader.chromosome to chromosome,
+                                               SimpleSomaticMutationHeader.chromosome_start to start.toString(),
+                                               SimpleSomaticMutationHeader.chromosome_end to end.toString(),
+                                               SimpleSomaticMutationHeader.control_genotype to controlGenotype,
+                                               SimpleSomaticMutationHeader.tumour_genotype to tumorGenotype,
+                                               SimpleSomaticMutationHeader.reference_genome_allele to ref,
+                                               SimpleSomaticMutationHeader.mutated_from_allele to ref,
+                                               SimpleSomaticMutationHeader.mutated_to_allele to alt,
+                                               SimpleSomaticMutationHeader.chromosome_strand to CHROMOSOME_STRAND,
+                                               SimpleSomaticMutationHeader.verification_status to VERIFICATION_STATUS))
         }
 
         private operator fun invoke(variantContext: VariantContext): List<SimpleSomaticMutation> {
@@ -44,15 +44,33 @@ data class SimpleSomaticMutation(private val fields: Map<SimpleSomaticMutationHe
             } else {
                 Lists.newArrayList<VariantContext>(variantContext)
             }
-            return variants.filter { it.mutationType() != "0" }.map { it ->
-                SimpleSomaticMutation(sampleId, sampleId, it.mutationType(), it.contig, it.start, it.end,
-                        it.reference.baseString, it.alternateAlleles[0].baseString)
+            return variants.filter(this::filterVariants).map { it ->
+                SimpleSomaticMutation(sampleId,
+                                      sampleId,
+                                      it.mutationType(),
+                                      it.contig,
+                                      it.start,
+                                      it.end,
+                                      it.reference.baseString,
+                                      it.alternateAlleles[0].baseString)
             }
         }
 
         operator fun invoke(vcfPath: String): List<SimpleSomaticMutation> {
             val vcfReader = VCFFileReader(File(vcfPath), false)
             return vcfReader.flatMap { it -> SimpleSomaticMutation(it) }
+        }
+
+        private fun filterVariants(variant: VariantContext): Boolean {
+            if (variant.mutationType() == "0") {
+                logger.warn("[{}] could not determine mutation type for variant {}:{}\t{} -> {}",
+                            variant.sampleNamesOrderedByName[0],
+                            variant.contig,
+                            variant.start,
+                            variant.reference.baseString,
+                            variant.alternateAlleles.first().baseString)
+            }
+            return variant.mutationType() != "0"
         }
     }
 
