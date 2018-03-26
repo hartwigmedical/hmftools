525e663df563c39898a5e5f342f20610d4f9dbd1
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-03-26 17:32:44 +0200

    Remove some unused code

--- hmf-common/src/main/java/com/hartwig/hmftools/common/variant/VariantType.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/variant/VariantType.java
@@ -1,26 +1,8 @@
 package com.hartwig.hmftools.common.variant;
 
-import org.jetbrains.annotations.NotNull;
-
 public enum VariantType {
     MNP,
     SNP,
     INDEL,
-    UNDEFINED;
-
-    private static final String MULTIPLE_ALTS_IDENTIFIER = ",";
-
-    @NotNull
-    public static VariantType fromRefAlt(@NotNull final String refValue, @NotNull final String altValue) {
-        final String[] allAlts = altValue.split(MULTIPLE_ALTS_IDENTIFIER);
-
-        VariantType type = VariantType.SNP;
-
-        for (final String alt : allAlts) {
-            if (refValue.length() != alt.length()) {
-                type = VariantType.INDEL;
-            }
-        }
-        return type;
-    }
+    UNDEFINED
 }
