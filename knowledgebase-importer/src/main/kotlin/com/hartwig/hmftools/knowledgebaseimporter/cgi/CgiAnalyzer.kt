594d8a5b4e37bc07d424e1f1ebdb963853c34a37
Mircea Voda <mircearaul.voda@gmail.com>
2018-04-24 10:57:56 +0200

    refactor cgi importer

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cgi/CgiAnalyzer.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cgi/CgiAnalyzer.kt
@@ -1,11 +1,9 @@
 package com.hartwig.hmftools.knowledgebaseimporter.cgi
 
+import com.hartwig.hmftools.common.variant.SomaticVariant
 import com.hartwig.hmftools.knowledgebaseimporter.output.KnownVariantOutput
-import com.hartwig.hmftools.knowledgebaseimporter.transvar.TransvarProteinAnalyzer
+import com.hartwig.hmftools.knowledgebaseimporter.transvar.*
 import com.hartwig.hmftools.knowledgebaseimporter.transvar.annotations.ProteinAnnotation
-import com.hartwig.hmftools.knowledgebaseimporter.transvar.extract
-import com.hartwig.hmftools.knowledgebaseimporter.transvar.extractChromosome
-import com.hartwig.hmftools.knowledgebaseimporter.transvar.extractVariants
 import htsjdk.samtools.reference.IndexedFastaSequenceFile
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
@@ -13,20 +11,30 @@ import java.io.File
 import java.nio.charset.Charset
 
 fun analyzeCgi(transvarLocation: String, fileLocation: String, reference: IndexedFastaSequenceFile): List<KnownVariantOutput> {
-    val parser = CSVParser.parse(File(fileLocation), Charset.defaultCharset(), CSVFormat.TDF.withFirstRecordAsHeader().withNullString(""))
-    val analyzer = TransvarProteinAnalyzer(transvarLocation)
-    val records = parser.asSequence().map { CgiRecord(it) }.toList().filter { it.context == "somatic" }
-    val transvarOutput = analyzer.analyze(records.map { ProteinAnnotation(it.transcript, it.impact) })
+    val records = readCgiRecords(fileLocation)
+    val transvarOutput = annotateCgiRecords(transvarLocation, records)
     return records.zip(transvarOutput)
             .flatMap { (cgiRecord, transvarOutput) ->
-                val cgiVariantGdnas = cgiRecord.gdna.split("__").filterNot { it.isBlank() }
-                val inferredVariants = extractVariants(transvarOutput, reference)
-                val chromosome = extractChromosome(cgiVariantGdnas.first())
-                val cgiVariants = extract(chromosome, cgiVariantGdnas, reference)
+                val cgiVariants = extractCgiVariants(cgiRecord, reference)
+                val inferredVariants = extractVariants(transvarOutput, reference).filterNot { variant -> cgiVariants.any { it == variant } }
                 val cgiVariantOutputs = cgiVariants.map { KnownVariantOutput(cgiRecord.gene, cgiRecord.transcript, "CGI", it) }
-                val inferredVariantOutputs = inferredVariants.filterNot { variant -> cgiVariants.any { it == variant } }.map {
-                    KnownVariantOutput(cgiRecord.gene, cgiRecord.transcript, "", it)
-                }
+                val inferredVariantOutputs = inferredVariants.map { KnownVariantOutput(cgiRecord.gene, cgiRecord.transcript, "", it) }
                 cgiVariantOutputs + inferredVariantOutputs
             }
+}
+
+fun readCgiRecords(fileLocation: String): List<CgiRecord> {
+    val parser = CSVParser.parse(File(fileLocation), Charset.defaultCharset(), CSVFormat.TDF.withFirstRecordAsHeader().withNullString(""))
+    return parser.asSequence().map { CgiRecord(it) }.toList().filter { it.context == "somatic" }
+}
+
+fun annotateCgiRecords(transvarLocation: String, records: List<CgiRecord>): List<TransvarOutput> {
+    val analyzer = TransvarProteinAnalyzer(transvarLocation)
+    return analyzer.analyze(records.map { ProteinAnnotation(it.transcript, it.impact) })
+}
+
+fun extractCgiVariants(cgiRecord: CgiRecord, reference: IndexedFastaSequenceFile): List<SomaticVariant> {
+    val cgiVariantGdnas = cgiRecord.gdna.split("__").filterNot { it.isBlank() }
+    val chromosome = extractChromosome(cgiVariantGdnas.first())
+    return extract(chromosome, cgiVariantGdnas, reference)
 }
