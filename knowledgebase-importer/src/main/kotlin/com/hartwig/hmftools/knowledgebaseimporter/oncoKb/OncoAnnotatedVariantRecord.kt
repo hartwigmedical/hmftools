d381393b8185202232eafa3813164284b656ca3d
Mircea Voda <mircearaul.voda@gmail.com>
2018-04-25 17:29:00 +0200

    refactor onco annotated variant record

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoAnnotatedVariantRecord.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoAnnotatedVariantRecord.kt
@@ -2,9 +2,10 @@ package com.hartwig.hmftools.knowledgebaseimporter.oncoKb
 
 import org.apache.commons.csv.CSVRecord
 
-data class OncoAnnotatedVariantRecord(private val csvRecord: CSVRecord) {
-    val transcript: String = csvRecord["Isoform"]
-    val gene: String = csvRecord["Gene"]
-    val alteration: String = csvRecord["Alteration"]
-    val oncogenicity: String = csvRecord["Oncogenicity"]
+data class OncoAnnotatedVariantRecord(val transcript: String, val gene: String, val alteration: String, val oncogenicity: String) {
+    companion object {
+        operator fun invoke(csvRecord: CSVRecord): OncoAnnotatedVariantRecord {
+            return OncoAnnotatedVariantRecord(csvRecord["Isoform"], csvRecord["Gene"], csvRecord["Alteration"], csvRecord["Oncogenicity"])
+        }
+    }
 }
