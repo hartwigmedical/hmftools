e625f0f1bf2911eda9b441258d2a1180296cbb58
Mircea Voda <mircearaul.voda@gmail.com>
2018-04-25 12:47:59 +0200

    refactor civic record

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/CivicRecord.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/CivicRecord.kt
@@ -4,38 +4,24 @@ import com.google.common.collect.Multimap
 import org.apache.commons.csv.CSVRecord
 import java.util.regex.Pattern
 
-data class CivicRecord(val transcript: String, val gene: String, val chromosome: String, val start: String, val stop: String,
-                       val ref: String, val alt: String, val evidence: Collection<CivicEvidence>, val hgvs: String) {
-    companion object Factory {
-        private val hgvsCDnaPattern = Pattern.compile("(ENST[0-9]+\\.[0-9+]:c\\.[0-9][^,\\t\\s\\n]+)")
+class CivicRecord(variantCSVRecord: CSVRecord, variantEvidenceMap: Multimap<String, CivicEvidence>) {
+    val transcript: String = if (variantCSVRecord["representative_transcript"].isBlank()) "-" else variantCSVRecord["representative_transcript"]
+    val variant: String = variantCSVRecord["variant"]
+    val gene: String = variantCSVRecord["gene"]
+    val chromosome: String = variantCSVRecord["chromosome"]
+    val start: String = variantCSVRecord["start"]
+    val stop: String = variantCSVRecord["stop"]
+    val ref: String = variantCSVRecord["reference_bases"]
+    val alt: String = variantCSVRecord["variant_bases"]
+    val evidence: Collection<CivicEvidence> = variantEvidenceMap[variantCSVRecord["variant_id"]]
+    val hgvs: String = extractHgvs(variantCSVRecord["hgvs_expressions"])
 
-        operator fun invoke(variantCSVRecord: CSVRecord, variantEvidenceMap: Multimap<String, CivicEvidence>): CivicRecord {
-            val hgvs = extractHgvs(variantCSVRecord.get("hgvs_expressions"))
-            val evidence = variantEvidenceMap[variantCSVRecord.get("variant_id")]
-            val representativeTranscript = variantCSVRecord.get("representative_transcript")
-            val transcript = if (representativeTranscript.isEmpty()) {
-                "-"
-            } else {
-                representativeTranscript
-            }
-            return CivicRecord(transcript,
-                               variantCSVRecord.get("gene"),
-                               variantCSVRecord.get("chromosome"),
-                               variantCSVRecord.get("start"),
-                               variantCSVRecord.get("stop"),
-                               variantCSVRecord.get("reference_bases"),
-                               variantCSVRecord.get("variant_bases"),
-                               evidence,
-                               hgvs)
-        }
-
-        private fun extractHgvs(hgvsField: String): String {
-            val matcher = hgvsCDnaPattern.matcher(hgvsField)
-            return if (matcher.find()) {
-                matcher.group(1)
-            } else {
-                ""
-            }
+    private fun extractHgvs(hgvsField: String): String {
+        val matcher = Pattern.compile("(ENST[0-9]+\\.[0-9+]:c\\.[0-9][^,\\t\\s\\n]+)").matcher(hgvsField)
+        return if (matcher.find()) {
+            matcher.group(1)
+        } else {
+            ""
         }
     }
 }
