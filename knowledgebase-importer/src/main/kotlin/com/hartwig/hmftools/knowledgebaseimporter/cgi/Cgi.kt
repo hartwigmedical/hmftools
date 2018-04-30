f96a0a6f08b0f446814a250c61a1dc3d6fa111a1
Mircea Voda <mircearaul.voda@gmail.com>
2018-04-30 10:44:03 +0200

    refactor cgi knowledgebase importer

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cgi/Cgi.kt
@@ -0,0 +1,94 @@
+package com.hartwig.hmftools.knowledgebaseimporter.cgi
+
+import com.hartwig.hmftools.common.variant.SomaticVariant
+import com.hartwig.hmftools.knowledgebaseimporter.Knowledgebase
+import com.hartwig.hmftools.knowledgebaseimporter.output.*
+import com.hartwig.hmftools.knowledgebaseimporter.readCSVRecords
+import com.hartwig.hmftools.knowledgebaseimporter.transvar.*
+import com.hartwig.hmftools.knowledgebaseimporter.transvar.annotations.CDnaAnnotation
+import com.hartwig.hmftools.knowledgebaseimporter.transvar.annotations.ProteinAnnotation
+import htsjdk.samtools.reference.IndexedFastaSequenceFile
+
+class Cgi(variantsLocation: String, biomarkersLocation: String, transvarLocation: String, private val reference: IndexedFastaSequenceFile) :
+        Knowledgebase {
+    companion object {
+        private const val SOURCE: String = "cgi"
+    }
+
+    private val proteinAnalyzer = TransvarProteinAnalyzer(transvarLocation)
+    private val cdnaAnalyzer = TransvarCdnaAnalyzer(transvarLocation)
+    private val somaticVariantRecords = readCSVRecords(variantsLocation) { CgiKnownVariantRecord(it) }.filter { it.context == "somatic" }
+    private val biomarkersRecords = readCSVRecords(biomarkersLocation) { CgiBiomarkersRecord(it) }
+
+    override val knownVariants: List<KnownVariantOutput> by lazy { knownVariants() }
+    override val knownFusionPairs: List<Pair<String, String>>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val promiscuousGenes: List<String>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val actionableVariants: List<ActionableVariantOutput> by lazy { actionableVariants() }
+    override val actionableCNVs: List<ActionableCNVOutput> by lazy { actionableCNVs() }
+    override val actionableFusions: List<ActionableFusionOutput>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    private fun knownVariants(): List<KnownVariantOutput> {
+        val transvarOutput = proteinAnalyzer.analyze(somaticVariantRecords.map { ProteinAnnotation(it.transcript, it.impact) })
+        return somaticVariantRecords.zip(transvarOutput)
+                .flatMap { (cgiRecord, transvarOutput) ->
+                    val cgiVariants = extractCgiVariants(cgiRecord.gdna, reference)
+                    val inferredKnownVariants = extractVariants(transvarOutput, reference)
+                            .filterNot { variant -> cgiVariants.any { it == variant } }
+                            .map { KnownVariantOutput(cgiRecord.gene, cgiRecord.transcript, "", it) }
+                    val cgiKnownVariants = cgiVariants.map { KnownVariantOutput(cgiRecord.gene, cgiRecord.transcript, "CGI", it) }
+                    cgiKnownVariants + inferredKnownVariants
+                }
+    }
+
+    private fun actionableVariants(): List<ActionableVariantOutput> {
+        val transvarOutput = annotateCgiBiomarkers()
+        return biomarkersRecords.zip(transvarOutput)
+                .flatMap { (cgiRecord, transvarOutput) ->
+                    val cgiVariants = extractCgiVariants(cgiRecord.gdna, reference)
+                    val inferredProteinVariants = extractVariants(transvarOutput.first, reference)
+                            .filterNot { variant -> cgiVariants.any { it == variant } }
+                    val inferredCdnaVariants = extractVariants(transvarOutput.second, reference)
+                            .filterNot { variant -> cgiVariants.any { it == variant } || inferredProteinVariants.any { it == variant } }
+                    actionableVariantOutput(cgiRecord, cgiVariants + inferredProteinVariants + inferredCdnaVariants)
+                }
+    }
+
+    private fun actionableCNVs(): List<ActionableCNVOutput> {
+        val cnaRecords = biomarkersRecords.filter { it.alterationType == "CNA" }
+        return cnaRecords.flatMap { record ->
+            record.cancerTypes.map { cancerType ->
+                ActionableCNVOutput(record.gene, extractAmpOrDel(record.alteration), actionability(cancerType, record))
+            }
+        }
+    }
+
+    private fun extractCgiVariants(gdna: String, reference: IndexedFastaSequenceFile): List<SomaticVariant> {
+        val cgiVariantGdnas = gdna.split("__").filterNot { it.isBlank() }
+        val chromosome = extractChromosome(cgiVariantGdnas.first())
+        return extract(chromosome, cgiVariantGdnas, reference)
+    }
+
+    private fun annotateCgiBiomarkers(): List<Pair<TransvarOutput, TransvarOutput>> {
+        val proteinOutput = proteinAnalyzer.analyze(biomarkersRecords.map { ProteinAnnotation(it.transcript, it.protein) })
+        val cdnaOutput = cdnaAnalyzer.analyze(biomarkersRecords.map { CDnaAnnotation(it.transcript, it.cdna) })
+        return proteinOutput.zip(cdnaOutput)
+    }
+
+    private fun actionableVariantOutput(cgiRecord: CgiBiomarkersRecord,
+                                        somaticVariants: List<SomaticVariant>): List<ActionableVariantOutput> {
+        return cgiRecord.cancerTypes.flatMap { cancerType ->
+            somaticVariants.map { somaticVariant ->
+                ActionableVariantOutput(cgiRecord.gene, somaticVariant, actionability(cancerType, cgiRecord))
+            }
+        }
+    }
+
+    private fun extractAmpOrDel(alteration: String): String = if (alteration.contains("amp")) "Amplification" else "Deletion"
+
+    private fun actionability(cancerType: String, cgiBiomarker: CgiBiomarkersRecord): Actionability {
+        return Actionability(SOURCE, cancerType, cgiBiomarker.drug, cgiBiomarker.level, cgiBiomarker.association, "")
+    }
+}
