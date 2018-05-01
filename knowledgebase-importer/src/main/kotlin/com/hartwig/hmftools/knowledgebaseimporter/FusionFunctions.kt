5c789923452b71aa5224afff02748387546ae03f
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-01 11:05:32 +0200

    refactor fusion fusion functions

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/FusionFunctions.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/FusionFunctions.kt
@@ -4,42 +4,68 @@ import org.apache.logging.log4j.LogManager
 
 private val logger = LogManager.getLogger("FusionFunctions")
 
-fun extractFusion(gene: String, fusion: String, separators: List<String>): Pair<String, String>? {
-    val fusionPair = separators.mapNotNull { extractFusion(gene, fusion, it) }.firstOrNull()
-    if (fusionPair == null) {
-        logger.warn("Could not extract fusion pair from $fusion. Searching for gene $gene with separators $separators")
+private const val GENE_PATTERN = "[A-Za-z0-9-]"
+private const val GENE_GROUP = "($GENE_PATTERN+)"
+
+fun extractFusion(gene: String, fusionString: String, separators: List<String>): Fusion? {
+    val fusion = separators.mapNotNull { extractFusion(gene, fusionString, it) }.sortedBy {
+        when (it) {
+            is FusionPair      -> 0
+            is PromiscuousGene -> 1
+        }
+    }.firstOrNull()
+    if (fusion == null) {
+        logger.warn("Could not extract fusion pair from $fusionString. Searching for gene $gene with separators $separators")
+    } else if (fusion is PromiscuousGene) {
     }
-    return fusionPair
+    return fusion
 }
 
-fun extractFusion(gene: String, fusion: String, separator: String): Pair<String, String>? {
-    return if (!isThreeGene(gene, fusion, separator) && !isFiveGene(gene, fusion, separator)) {
-        null
+fun extractFusion(gene: String, fusionString: String, separator: String): Fusion? {
+    val fiveGene = fiveGene(gene, fusionString, separator)
+    val threeGene = threeGene(gene, fusionString, separator)
+    return if (fiveGene == null || threeGene == null) {
+        PromiscuousGene(gene)
     } else {
-        Pair(fiveGene(gene, fusion, separator), threeGene(gene, fusion, separator))
+        FusionPair(fiveGene, threeGene)
     }
 }
 
-fun fiveGene(gene: String, fusion: String, separator: String): String {
-    return if (isFiveGene(gene, fusion, separator)) gene else fusion.substringBefore("$separator$gene")
+fun fiveGene(gene: String, fusion: String, separator: String): String? {
+    return if (isFiveGene(gene, fusion, separator)) gene else extractFiveGene(gene, fusion, separator)
 }
 
-fun threeGene(gene: String, fusion: String, separator: String): String {
-    return if (isThreeGene(gene, fusion, separator)) gene else fusion.substringAfter("$gene$separator")
+fun threeGene(gene: String, fusion: String, separator: String): String? {
+    return if (isThreeGene(gene, fusion, separator)) gene else extractThreeGene(gene, fusion, separator)
 }
 
-fun isThreeGene(gene: String, fusion: String, separator: String): Boolean {
-    return fusion.contains("$separator$gene")
+fun extractFiveGene(gene: String, fusion: String, separator: String): String? {
+    return extractGene(fusion, "$GENE_GROUP${Regex.escape(separator)}${geneStartLetters(gene)}")
+}
+
+fun extractThreeGene(gene: String, fusion: String, separator: String): String? {
+    return extractGene(fusion, "${geneStartLetters(gene)}$GENE_PATTERN*${Regex.escape(separator)}$GENE_GROUP")
+}
+
+private fun extractGene(fusion: String, patternString: String): String? {
+    val matchResult = patternString.toRegex().find(fusion)
+    return matchResult?.groupValues?.get(1)
 }
 
 fun isFiveGene(gene: String, fusion: String, separator: String): Boolean {
-    return fusion.contains("$gene$separator")
+    return !isThreeGene(gene, fusion, separator) && fusion.contains(geneStartLetters(gene))
 }
 
-fun flipGenePair(pair: Pair<String, String>, pairsToFlip: Set<Pair<String, String>>): Pair<String, String> {
-    return if (pairsToFlip.contains(pair)) {
-        Pair(pair.second, pair.first)
+fun isThreeGene(gene: String, fusion: String, separator: String): Boolean {
+    return fusion.contains("$separator${geneStartLetters(gene)}")
+}
+
+private fun geneStartLetters(gene: String) = gene.substring(0, 3)
+
+fun flipFusion(fusion: Fusion, pairsToFlip: Set<FusionPair>): Fusion {
+    return if (fusion is FusionPair && pairsToFlip.contains(fusion)) {
+        FusionPair(fusion.threeGene, fusion.fiveGene)
     } else {
-        pair
+        fusion
     }
 }
