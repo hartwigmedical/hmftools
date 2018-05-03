bfdd5333dcf855e5cb6d75fa9afc846ab2a8c04e
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-03 14:43:38 +0200

    trim and convert disease ontology names to lowercase

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/diseaseOntology/DiseaseOntology.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/diseaseOntology/DiseaseOntology.kt
@@ -18,8 +18,8 @@ class DiseaseOntology(fileLocation: String) {
             val logger = Logger.getLogger("org.obolibrary.oboformat.parser.OBOFormatParser")
             logger.level = Level.SEVERE
             val ontologyManager = OWLManager.createOWLOntologyManager()
-            val config = OWLOntologyLoaderConfiguration().setFollowRedirects(true).setMissingImportHandlingStrategy(
-                    MissingImportHandlingStrategy.SILENT)
+            val config = OWLOntologyLoaderConfiguration().setFollowRedirects(true)
+                    .setMissingImportHandlingStrategy(MissingImportHandlingStrategy.SILENT)
             return ontologyManager.loadOntologyFromOntologyDocument(FileDocumentSource(File(fileLocation)), config)
         }
 
@@ -32,20 +32,23 @@ class DiseaseOntology(fileLocation: String) {
         private fun createDiseaseMapping(ontology: OWLOntology, reasoner: OWLReasoner): Map<String, OWLClass> {
             val diseaseClass = ontology.owlOntologyManager.owlDataFactory.getOWLClass(diseaseIRI)
             val diseases = setOf(diseaseClass) + reasoner.getSubClasses(diseaseClass, false).flattened.filterNot { it.isOWLNothing }
-            val synonyms = diseases.flatMap { disease -> getSynonyms(disease, ontology).map { Pair(it.toLowerCase(), disease) } }
-            return diseases.associateBy { getLabel(it, ontology).toLowerCase() } + synonyms.toMap()
+            val synonyms = diseases.flatMap { disease -> getSynonyms(disease, ontology).map { Pair(it, disease) } }
+            return diseases.associateBy { getLabel(it, ontology) } + synonyms.toMap()
         }
 
         private fun getLabel(owlClass: OWLClass, ontology: OWLOntology): String {
             val dataFactory = ontology.owlOntologyManager.owlDataFactory
-            return owlClass.getAnnotations(ontology, dataFactory.rdfsLabel).map { it.value as OWLLiteral }.map { it.literal }.first()
+            return owlClass.getAnnotations(ontology, dataFactory.rdfsLabel)
+                    .map { it.value as OWLLiteral }
+                    .map { it.literal.toLowerCase().trim() }
+                    .first()
         }
 
         private fun getSynonyms(owlClass: OWLClass, ontology: OWLOntology): Set<String> {
             return owlClass.getAnnotations(ontology)
                     .filter { it.property.toString().contains("hasExactSynonym") }
                     .map { it.value as OWLLiteral }
-                    .map { it.literal }
+                    .map { it.literal.toLowerCase().trim() }
                     .toSet()
         }
     }
@@ -55,6 +58,6 @@ class DiseaseOntology(fileLocation: String) {
     private val diseaseNameToClass = createDiseaseMapping(ontology, reasoner)
 
     fun isPresent(cancerType: String): Boolean {
-        return diseaseNameToClass.containsKey(cancerType.toLowerCase())
+        return diseaseNameToClass.containsKey(cancerType.toLowerCase().trim())
     }
 }
