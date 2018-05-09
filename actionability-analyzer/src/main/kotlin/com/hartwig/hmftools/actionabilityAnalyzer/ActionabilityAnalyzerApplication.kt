2b3807b6b00bf46015a373c1263591463e36ac97
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-09 14:52:57 +0200

    add actionability analyzer module

---
+++ actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
@@ -0,0 +1,9 @@
+package com.hartwig.hmftools.actionabilityAnalyzer
+
+import org.apache.logging.log4j.LogManager
+
+private val logger = LogManager.getLogger("ActionabilityAnalyzerApplication")
+
+fun main(args: Array<String>) {
+    
+}
