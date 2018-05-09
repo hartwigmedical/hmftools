873496f42672216797d6dd88ecb7258b1f07864c
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-09 14:58:57 +0200

    create csv printer for actionability output

--- actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
+++ actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
@@ -1,9 +1,17 @@
 package com.hartwig.hmftools.actionabilityAnalyzer
 
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
 import org.apache.logging.log4j.LogManager
+import java.io.FileWriter
 
 private val logger = LogManager.getLogger("ActionabilityAnalyzerApplication")
 
 fun main(args: Array<String>) {
     
+}
+
+private fun createPrinter(): CSVPrinter {
+    val format = CSVFormat.TDF.withHeader(*ActionabilityOutput.header.toTypedArray()).withNullString("NULL")
+    return CSVPrinter(FileWriter("actionableVariantsPerSample.tsv"), format)
 }
