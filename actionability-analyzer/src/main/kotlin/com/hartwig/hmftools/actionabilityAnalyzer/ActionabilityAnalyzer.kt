92984acea246b95481ffed2ef481e17c3081de19
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-09 16:36:45 +0200

    read actionability items for fusions and CNVs

--- actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzer.kt
+++ actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzer.kt
@@ -1,13 +1,12 @@
 package com.hartwig.hmftools.actionabilityAnalyzer
 
-import com.hartwig.hmftools.knowledgebaseimporter.output.Actionability
-import com.hartwig.hmftools.knowledgebaseimporter.output.ActionableItem
-import com.hartwig.hmftools.knowledgebaseimporter.output.ActionableVariantOutput
-import com.hartwig.hmftools.knowledgebaseimporter.output.SomaticVariantEvent
+import com.hartwig.hmftools.knowledgebaseimporter.output.*
 import com.hartwig.hmftools.knowledgebaseimporter.readTSVRecords
 import com.hartwig.hmftools.patientdb.data.PotentialActionableVariant
+import org.apache.commons.csv.CSVRecord
 
-class ActionabilityAnalyzer(actionableVariantsLocation: String) {
+class ActionabilityAnalyzer(actionableVariantsLocation: String, fustionPairsLocation: String, promiscuousFiveLocation: String,
+                            promiscuousThreeLocation: String, cnvsLocation: String) {
     companion object {
         private fun <T> createActionabilityMap(items: List<ActionableItem<T>>): Map<T, List<ActionableTreatment>> {
             return items.groupBy { it.event }.mapValues { (_, actionableOutputs) ->
@@ -15,21 +14,62 @@ class ActionabilityAnalyzer(actionableVariantsLocation: String) {
             }
         }
 
-        private fun readActionableVariants(actionableVariantsLocation: String): List<ActionableVariantOutput> {
-            return readTSVRecords(actionableVariantsLocation) {
+        private fun readActionableVariants(fileLocation: String): List<ActionableVariantOutput> {
+            return readTSVRecords(fileLocation) {
                 ActionableVariantOutput(it["gene"], SomaticVariantEvent(it["chromosome"], it["position"], it["ref"], it["alt"]),
-                                        Actionability(it["source"], it["cancerType"], it["drug"] ?: "", it["level"],
-                                                      it["significance"] ?: "", it["evidenceType"] ?: ""))
+                                        readActionability(it))
             }
+        }
+
+        private fun readActionableFusionPairs(fileLocation: String): List<ActionableFusionOutput> {
+            return readTSVRecords(fileLocation) {
+                ActionableFusionOutput(FusionPair(it["fiveGene"], it["threeGene"]), readActionability(it))
+            }
+        }
+
+        private fun readActionablePromiscuousGenes(fileLocation: String): List<ActionableFusionOutput> {
+            return readTSVRecords(fileLocation) {
+                ActionableFusionOutput(PromiscuousGene(it["gene"]), readActionability(it))
+            }
+        }
+
+        private fun readActionableCNVs(fileLocation: String): List<ActionableCNVOutput> {
+            return readTSVRecords(fileLocation) {
+                ActionableCNVOutput(CnvEvent(it["gene"], it["type"]), readActionability(it))
+            }
+        }
+
+        private fun readActionability(record: CSVRecord): Actionability {
+            return Actionability(record["source"], record["cancerType"], record["drug"].orEmpty(), record["level"],
+                                 record["significance"].orEmpty(), record["evidenceType"].orEmpty())
+        }
+
+        private fun <T> getActionability(actionabilityMap: Map<T, List<ActionableTreatment>>, event: T,
+                                         sampleId: String): List<ActionabilityOutput> {
+            return actionabilityMap[event].orEmpty().map { ActionabilityOutput(sampleId, it) }
         }
     }
 
     private val variantActionabilityMap = createActionabilityMap(readActionableVariants(actionableVariantsLocation))
-    //    private val fusionActionabilityMap: String = TODO("not implemented")
-    //    private val cnvActionabilityMap: String = TODO("not implemented")
+    private val fusionActionabilityMap = createActionabilityMap(readActionableFusionPairs(fustionPairsLocation))
+    private val promiscuousFiveActionabilityMap = createActionabilityMap(readActionablePromiscuousGenes(promiscuousFiveLocation))
+    private val promiscuousThreeActionabilityMap = createActionabilityMap(readActionablePromiscuousGenes(promiscuousThreeLocation))
+    private val cnvActionabilityMap = createActionabilityMap(readActionableCNVs(cnvsLocation))
 
-    fun actionabilityForVariant(variant: PotentialActionableVariant): List<ActionabilityOutput> {
+    fun actionabilityForVariant(variant: PotentialActionableVariant): Set<ActionabilityOutput> {
         val variantKey = SomaticVariantEvent(variant.chromosome(), variant.position().toString(), variant.ref(), variant.alt())
-        return variantActionabilityMap[variantKey]?.map { ActionabilityOutput(variant.sampleId(), it) } ?: listOf()
+        return getActionability(variantActionabilityMap, variantKey, variant.sampleId()).toSet()
+    }
+
+    fun actionabilityForFusion(sampleId: String, fiveGene: String, threeGene: String): Set<ActionabilityOutput> {
+        val fusionPairActionability = getActionability(fusionActionabilityMap, FusionPair(fiveGene, threeGene), sampleId)
+        val promiscuousFiveActionability = getActionability(promiscuousFiveActionabilityMap, PromiscuousGene(fiveGene), sampleId)
+        val promiscuousThreeActionability = getActionability(promiscuousThreeActionabilityMap, PromiscuousGene(threeGene), sampleId)
+        return (fusionPairActionability + promiscuousFiveActionability + promiscuousThreeActionability).toSet()
+    }
+
+    fun actionabilityForCNV(sampleId: String, gene: String, cnvType: String): Set<ActionabilityOutput> {
+        val cnvEvent = CnvEvent(gene, cnvType)
+        return getActionability(cnvActionabilityMap, cnvEvent, sampleId).toSet()
     }
 }
