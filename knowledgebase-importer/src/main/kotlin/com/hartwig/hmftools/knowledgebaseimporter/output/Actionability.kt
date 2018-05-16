68964646e7e3a77456f00d4aedc1b1a8576ec116
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-16 16:24:13 +0200

    add hmfLevel field to actionability info

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/output/Actionability.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/output/Actionability.kt
@@ -1,10 +1,19 @@
 package com.hartwig.hmftools.knowledgebaseimporter.output
 
 data class Actionability(val source: String, val cancerType: String, val drug: String, val level: String, val significance: String,
-                         val evidenceType: String) {
+                         val evidenceType: String, val hmfLevel: HmfLevel) {
     companion object {
-        val header = listOf("source", "drug", "cancerType", "level", "evidenceType", "significance")
+        val header = listOf("source", "drug", "cancerType", "level", "hmfLevel", "evidenceType", "significance")
+
+        operator fun invoke(source: String, cancerTypes: List<String>, drugs: List<String>, level: String, significance: String,
+                            evidenceType: String, hmfLevel: HmfLevel): List<Actionability> {
+            return cancerTypes.flatMap { cancerType ->
+                drugs.map { drug ->
+                    Actionability(source, cancerType, drug, level, significance, evidenceType, hmfLevel)
+                }
+            }
+        }
     }
 
-    val record: List<String> = listOf(source, drug, cancerType, level, evidenceType, significance)
+    val record: List<String> = listOf(source, drug, cancerType, level, hmfLevel.name, evidenceType, significance)
 }
