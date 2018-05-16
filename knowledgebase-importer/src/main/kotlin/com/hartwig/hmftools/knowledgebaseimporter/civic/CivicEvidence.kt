a9cb897362e3bea643f0f6573e43aafd46f0a42f
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-16 16:25:56 +0200

    determine hmf evidence level for actionable records

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/CivicEvidence.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/CivicEvidence.kt
@@ -1,5 +1,7 @@
 package com.hartwig.hmftools.knowledgebaseimporter.civic
 
+import com.hartwig.hmftools.knowledgebaseimporter.output.Actionability
+import com.hartwig.hmftools.knowledgebaseimporter.output.HmfLevel
 import org.apache.commons.csv.CSVRecord
 
 data class CivicEvidence(private val csvRecord: CSVRecord) {
@@ -10,4 +12,6 @@ data class CivicEvidence(private val csvRecord: CSVRecord) {
     val direction: String = csvRecord["evidence_direction"].orEmpty()
     val level: String = csvRecord["evidence_level"].orEmpty()
     val significance: String = csvRecord["clinical_significance"].orEmpty()
+    val actionabilityItems: List<Actionability> = Actionability("civic", listOf(cancerType), drugs, level, significance, type,
+                                                                HmfLevel(level))
 }
--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoActionableVariantRecord.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoActionableVariantRecord.kt
@@ -1,5 +1,7 @@
 package com.hartwig.hmftools.knowledgebaseimporter.oncoKb
 
+import com.hartwig.hmftools.knowledgebaseimporter.output.Actionability
+import com.hartwig.hmftools.knowledgebaseimporter.output.HmfLevel
 import org.apache.commons.csv.CSVRecord
 
 data class OncoActionableVariantRecord(private val csvRecord: CSVRecord) {
@@ -8,14 +10,14 @@ data class OncoActionableVariantRecord(private val csvRecord: CSVRecord) {
     val alteration: String = csvRecord["Alteration"]
     val cancerType: String = csvRecord["Cancer Type"]
     val drugs: List<String> = csvRecord["Drugs(s)"].split(",").map { it.trim() }
-    val level: String = if (csvRecord["Level"].startsWith("R")) {
-        csvRecord["Level"].substring(1)
-    } else {
-        csvRecord["Level"]
-    }
-    val significance = if (csvRecord["Level"].startsWith("R")) {
-        "resistance"
-    } else {
-        "sensitivity"
+    val level: String = readLevel(csvRecord["Level"])
+    val significance = if (csvRecord["Level"].startsWith("R")) "resistance" else "sensitivity"
+    val actionabilityItems: List<Actionability> = Actionability("civic", listOf(cancerType), drugs, level, significance,
+                                                                "Predictive", HmfLevel(csvRecord["Level"]))
+
+    companion object {
+        private fun readLevel(levelField: String): String {
+            return if (levelField.startsWith("R")) levelField.substring(1) else levelField
+        }
     }
 }
