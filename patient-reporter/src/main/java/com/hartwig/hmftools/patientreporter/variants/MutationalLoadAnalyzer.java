77306a467723c994ceb6344bafecdcc5b2c7dfe6
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-05-17 21:27:10 +0200

    DEV-299: Count mutational load of all wordtCodingEffect missense variants

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/MutationalLoadAnalyzer.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/MutationalLoadAnalyzer.java
@@ -1,5 +1,6 @@
 package com.hartwig.hmftools.patientreporter.variants;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.hartwig.hmftools.common.variant.SomaticVariant;
@@ -9,13 +10,15 @@ import org.jetbrains.annotations.NotNull;
 public class MutationalLoadAnalyzer {
 
     static int analyzeVariants(@NotNull final List<SomaticVariant> variants) {
+        final List<Boolean> mutationalLoadList = new ArrayList<>();
         int mutationalLoadSize = 0;
-
         for (final SomaticVariant variant : variants) {
-            mutationalLoad(variant);
+            final boolean mutationLoadValue = mutationalLoad(variant);
+            mutationalLoadList.add(mutationLoadValue);
             mutationalLoadSize++;
         }
-        return mutationalLoadSize;
+
+        return mutationalLoadList.size();
     }
 
     static boolean mutationalLoad(@NotNull final SomaticVariant variant) {
