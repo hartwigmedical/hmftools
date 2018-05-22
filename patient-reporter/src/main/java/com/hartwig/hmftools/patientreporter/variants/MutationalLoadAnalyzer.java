4c78a6e52979af0235c43ef7ec48deaeacc542a6
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-05-22 15:19:58 +0200

    Add test for mutation load

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/MutationalLoadAnalyzer.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/MutationalLoadAnalyzer.java
@@ -1,6 +1,5 @@
 package com.hartwig.hmftools.patientreporter.variants;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.hartwig.hmftools.common.variant.SomaticVariant;
@@ -10,19 +9,16 @@ import org.jetbrains.annotations.NotNull;
 public class MutationalLoadAnalyzer {
 
     static int analyzeVariants(@NotNull final List<SomaticVariant> variants) {
-        final List<Boolean> mutationalLoad = new ArrayList<>();
         int mutationalLoadSize = 0;
         for (final SomaticVariant variant : variants) {
             if (mutationalLoadCheck(variant)) {
-                mutationalLoad.add(mutationalLoadCheck(variant));
+                mutationalLoadSize++;
             }
-            mutationalLoadSize++;
         }
-
-        return mutationalLoad.size();
+        return mutationalLoadSize;
     }
 
-    static boolean mutationalLoadCheck(@NotNull final SomaticVariant variant) {
+    private static boolean mutationalLoadCheck(@NotNull final SomaticVariant variant) {
         return variant.worstCodingEffect().contains("MISSENSE") && variant.filter().equals("PASS");
     }
 }
