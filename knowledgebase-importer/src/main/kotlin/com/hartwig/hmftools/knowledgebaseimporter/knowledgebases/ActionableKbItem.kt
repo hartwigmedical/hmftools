a9622a5045df4af50055d0f34e7cd5645cf87e64
Mircea Voda <mircearaul.voda@gmail.com>
2018-05-23 15:48:44 +0200

    add actionable knowledgebase item

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/knowledgebases/ActionableKbItem.kt
@@ -0,0 +1,14 @@
+package com.hartwig.hmftools.knowledgebaseimporter.knowledgebases
+
+import com.hartwig.hmftools.knowledgebaseimporter.output.*
+
+data class ActionableKbItem(val event: ActionableEvent, val actionability: Actionability) {
+    fun toActionableOutput(): ActionableItem<*>? {
+        return when (event) {
+            is CnvEvent            -> ActionableCNVOutput(event, actionability)
+            is SomaticVariantEvent -> ActionableVariantOutput("", event, actionability)
+            is FusionEvent         -> ActionableFusionOutput(event, actionability)
+            else                   -> null
+        }
+    }
+}
