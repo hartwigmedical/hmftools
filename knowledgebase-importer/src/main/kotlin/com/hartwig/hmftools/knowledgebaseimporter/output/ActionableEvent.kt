34ee6f5b2598ea8d59b4b0bd8f416947f6f01614
Mircea Voda <mircearaul.voda@gmail.com>
2018-06-01 17:49:09 +0200

    refactor actionable events

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/output/ActionableEvent.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/output/ActionableEvent.kt
@@ -1,61 +1,42 @@
 package com.hartwig.hmftools.knowledgebaseimporter.output
 
 import com.hartwig.hmftools.common.variant.SomaticVariant
+import com.hartwig.hmftools.extensions.csv.CsvData
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.SomaticEvent
 
-sealed class ActionableEvent : SomaticEvent
+sealed class ActionableEvent : SomaticEvent {
+    abstract fun eventString(): String
+}
 
 sealed class FusionEvent : ActionableEvent()
 
-data class FusionPair(val fiveGene: String, val threeGene: String) : FusionEvent() {
-    companion object {
-        val header = listOf("fiveGene", "threeGene")
-    }
-
-    val record: List<String> = listOf(fiveGene, threeGene)
-
-    override fun toString(): String {
+data class FusionPair(val fiveGene: String, val threeGene: String) : FusionEvent(), CsvData {
+    override fun eventString(): String {
         return "$fiveGene - $threeGene fusion"
     }
 }
 
-data class PromiscuousGene(val gene: String) : FusionEvent() {
-    companion object {
-        val header = listOf("gene")
-    }
-
-    val record: List<String> = listOf(gene)
-
-    override fun toString(): String {
+data class PromiscuousGene(val gene: String) : FusionEvent(), CsvData {
+    override fun eventString(): String {
         return "$gene fusions"
     }
 }
 
-data class CnvEvent(val gene: String, val cnvType: String) : ActionableEvent() {
-    companion object {
-        val header = listOf("gene", "cnvType")
-    }
-
-    val record: List<String> = listOf(gene, cnvType)
-
-    override fun toString(): String {
+data class CnvEvent(val gene: String, val cnvType: String) : ActionableEvent(), CsvData {
+    override fun eventString(): String {
         return "$gene $cnvType"
     }
 }
 
 data class SomaticVariantEvent(val gene: String, val chromosome: String, val position: String, val ref: String, val alt: String) :
-        ActionableEvent() {
+        ActionableEvent(), CsvData {
     companion object {
-        val header = listOf("chromosome", "position", "ref", "alt")
-
         operator fun invoke(gene: String, variant: SomaticVariant): SomaticVariantEvent {
             return SomaticVariantEvent(gene, variant.chromosome(), variant.position().toString(), variant.ref(), variant.alt())
         }
     }
 
-    val record: List<String> = listOf(chromosome, position, ref, alt)
-
-    override fun toString(): String {
+    override fun eventString(): String {
         return "$gene $chromosome:$position $ref->$alt"
     }
 }
