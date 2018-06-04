e610ef0ca6acea58dfd5acfa0c1fff97d0413fe0
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-06-04 15:39:05 +0200

    DEV-407: Expand treatment names

--- patient-db/src/main/resources/patches/patientdb3.6_to_3.7_migration.sql
+++ patient-db/src/main/resources/patches/patientdb3.6_to_3.7_migration.sql
@@ -12,3 +12,19 @@ ALTER TABLE baseline
 
 ALTER TABLE treatment
     ADD COLUMN mechanism varchar(255) after type;
+
+ALTER TABLE baseline DROP COLUMN preTreatments;
+ALTER TABLE baseline
+    ADD COLUMN preTreatments varchar(800) after hasRadiotherapyPreTreatment;
+
+ALTER TABLE preTreatmentDrug DROP COLUMN name;
+ALTER TABLE preTreatmentDrug
+    ADD COLUMN name varchar(800) after endDate;
+
+ALTER TABLE treatment DROP COLUMN name;
+ALTER TABLE treatment
+    ADD COLUMN name varchar(800) after endDate;
+
+ALTER TABLE drug DROP COLUMN name;
+ALTER TABLE drug
+    ADD COLUMN name varchar(800)after endDate;
