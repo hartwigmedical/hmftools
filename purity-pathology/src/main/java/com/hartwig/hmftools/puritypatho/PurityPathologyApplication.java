446ecfecbf43d74bb0a22eb61a8f7f8b33557206
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-06-12 13:53:22 +0200

    add purity pathology class

---
+++ purity-pathology/src/main/java/com/hartwig/hmftools/puritypatho/PurityPathologyApplication.java
@@ -0,0 +1,10 @@
+package com.hartwig.hmftools.puritypatho;
+
+public class PurityPathologyApplication {
+
+    public static void main(final String... args) {
+
+        System.out.print("hoi lieke");
+    }
+
+}
