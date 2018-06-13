2065904c03d44572308bfc92036cf4efa0ed3dbb
Mircea Voda <mircearaul.voda@gmail.com>
2018-06-13 14:13:33 +0200

    read oncoKb gene and exon level mutations

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/knowledgebases/SomaticEvent.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/knowledgebases/SomaticEvent.kt
@@ -2,15 +2,21 @@ package com.hartwig.hmftools.knowledgebaseimporter.knowledgebases
 
 interface SomaticEvent
 
-sealed class HgvsAnnotation : SomaticEvent {
-    abstract val transcript: String
-    abstract val alteration: String
-}
-
 data class GDnaVariant(val gDnaImpact: String) : SomaticEvent
 
 data class KnowledgebaseVariant(val gene: String, val chromosome: String, val position: Long, val ref: String?, val alt: String?) :
         SomaticEvent
+
+sealed class GenericMutation : SomaticEvent
+
+data class ExonMutations(val gene: String, val transcript: String, val exonNumber: Int) : GenericMutation()
+
+data class GeneMutations(val gene: String, val transcript: String) : GenericMutation()
+
+sealed class HgvsAnnotation : SomaticEvent {
+    abstract val transcript: String
+    abstract val alteration: String
+}
 
 data class CDnaAnnotation(override val transcript: String, override val alteration: String) : HgvsAnnotation()
 
--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoSomaticEventReader.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoSomaticEventReader.kt
@@ -1,14 +1,14 @@
 package com.hartwig.hmftools.knowledgebaseimporter.oncoKb
 
 import com.hartwig.hmftools.knowledgebaseimporter.FusionReader
-import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.ProteinAnnotation
-import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.SomaticEvent
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.*
 import com.hartwig.hmftools.knowledgebaseimporter.output.CnvEvent
 import com.hartwig.hmftools.knowledgebaseimporter.output.FusionEvent
 import com.hartwig.hmftools.knowledgebaseimporter.output.FusionPair
 
 class OncoSomaticEventReader {
     companion object {
+        private const val EXON_PATTERN = "Exon\\s?([0-9]+)\\s+"
         private val FUSION_SEPARATORS = setOf("-", " - ", "?")
         private val FUSIONS_TO_FLIP = setOf(FusionPair("ROS1", "CD74"),
                                             FusionPair("EP300", "MLL"),
@@ -19,14 +19,17 @@ class OncoSomaticEventReader {
 
     fun read(gene: String, transcript: String, alteration: String, effect: String = ""): List<SomaticEvent> {
         return when {
-            isFusion(alteration) -> listOfNotNull(readFusions(gene, alteration, effect))
-            isCnv(alteration)    -> listOfNotNull(readCnv(gene, alteration))
-            else                 -> listOf(ProteinAnnotation(transcript, alteration))
+            isFusion(alteration)          -> listOfNotNull(readFusions(gene, alteration, effect))
+            isCnv(alteration)             -> listOfNotNull(readCnv(gene, alteration))
+            isGenericMutation(alteration) -> listOfNotNull(readGenericMutation(gene, transcript, alteration))
+            else                          -> listOf(ProteinAnnotation(transcript, alteration))
         }
     }
 
-    private fun isFusion(alteration: String): Boolean = alteration.contains(Regex("Fusion"))
+    private fun isFusion(alteration: String): Boolean = alteration.contains("Fusion".toRegex())
     private fun isCnv(alteration: String): Boolean = alteration == "Amplification" || alteration == "Deletion"
+    private fun isGenericMutation(alteration: String): Boolean = alteration == "Oncogenic Mutations" || isExonMutation(alteration)
+    private fun isExonMutation(alteration: String) = alteration.contains("${EXON_PATTERN}Mutations".toRegex(RegexOption.IGNORE_CASE))
 
     private fun readFusions(gene: String, alteration: String, effect: String): FusionEvent? {
         return when {
@@ -41,5 +44,18 @@ class OncoSomaticEventReader {
             "Deletion"      -> CnvEvent(gene, "Deletion")
             else            -> null
         }
+    }
+
+    private fun readGenericMutation(gene: String, transcript: String, alteration: String): GenericMutation? {
+        return when {
+            alteration == "Oncogenic Mutations" -> GeneMutations(gene, transcript)
+            isExonMutation(alteration)          -> ExonMutations(gene, transcript, extractExon(alteration))
+            else                                -> null
+        }
+    }
+
+    private fun extractExon(alteration: String): Int {
+        val matchResult = EXON_PATTERN.toRegex(RegexOption.IGNORE_CASE).find(alteration)
+        return matchResult!!.groupValues[1].toInt()
     }
 }
