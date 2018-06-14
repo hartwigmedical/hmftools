d9201df63b854f11c8a47eec80bffb29fd403b09
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-06-14 17:40:17 +0200

    DEV-412: get parameter for set name

--- purity-pathology/src/main/java/com/hartwig/hmftools/puritypatho/PurityPathologyApplication.java
+++ purity-pathology/src/main/java/com/hartwig/hmftools/puritypatho/PurityPathologyApplication.java
@@ -1,10 +1,43 @@
 package com.hartwig.hmftools.puritypatho;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+
+import com.hartwig.hmftools.common.version.VersionInfo;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
 public class PurityPathologyApplication {
+    private static final String RUNS_DIR = "runs_dir";
 
-    public static void main(final String... args) {
+    private static final Logger LOGGER = LogManager.getLogger(PurityPathologyApplication.class);
 
-        System.out.print("hoi lieke");
+    public static void main(final String... args) throws ParseException, IOException, SQLException, ExecutionException, InterruptedException {
+        final Options options = createOptions();
+        final CommandLine cmd = createCommandLine(args, options);
+        final String runsFolderPath = cmd.getOptionValue(RUNS_DIR);
+        LOGGER.info("Set name: " + runsFolderPath);
     }
 
+    @NotNull
+    private static Options createOptions() {
+        final Options options = new Options();
+        options.addOption(RUNS_DIR, true, "Path towards the folder containing patient runs.");
+        return options;
+    }
+
+    @NotNull
+    private static CommandLine createCommandLine(@NotNull final String[] args, @NotNull final Options options) throws ParseException {
+        final CommandLineParser parser = new DefaultParser();
+        return parser.parse(options, args);
+    }
 }
+
