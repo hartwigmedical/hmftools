e9127b6a6f53ce413168ff745d9b17df3acf7f3e
Mircea Voda <mircearaul.voda@gmail.com>
2018-06-18 13:58:58 +0200

    add gene and transcript to genericMutation class

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/knowledgebases/SomaticEvent.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/knowledgebases/SomaticEvent.kt
@@ -7,17 +7,19 @@ data class GDnaVariant(val gDnaImpact: String) : SomaticEvent
 data class KnowledgebaseVariant(val gene: String, val chromosome: String, val position: Long, val ref: String?, val alt: String?) :
         SomaticEvent
 
-sealed class GenericMutation : SomaticEvent
+sealed class GenericMutation : SomaticEvent {
+    abstract val transcript: String?
+    abstract val gene: String
+}
 
-data class CodonMutations(val gene: String, val transcript: String?, val codonNumber: Int) : GenericMutation()
+data class CodonMutations(override val gene: String, override val transcript: String?, val codonNumber: Int) : GenericMutation()
 
-data class CodonRangeMutations(val gene: String, val transcript: String?, val startCodon: Int, val endCodon: Int) : GenericMutation()
+data class CodonRangeMutations(override val gene: String, override val transcript: String?, val startCodon: Int,
+                               val endCodon: Int) : GenericMutation()
 
-data class ExonMutations(val gene: String, val transcript: String?, val exonNumber: Int) : GenericMutation()
+data class ExonMutations(override val gene: String, override val transcript: String?, val exonNumber: Int) : GenericMutation()
 
-data class GeneMutations(val gene: String, val transcript: String?) : GenericMutation()
-
-data class GenomicRangeMutations(val gene: String, val chromosome: String, val start: Long, val stop: Long) : GenericMutation()
+data class GeneMutations(override val gene: String, override val transcript: String?) : GenericMutation()
 
 sealed class HgvsAnnotation : SomaticEvent {
     abstract val transcript: String
