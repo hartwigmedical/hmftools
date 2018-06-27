cbf1ddb16216e53d066b35725eb0d6a6b6626f53
Mircea Voda <mircearaul.voda@gmail.com>
2018-06-27 14:51:23 +0200

    add ensembl queries for gene start and end exons

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/dao/EnsemblGeneDAO.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/dao/EnsemblGeneDAO.kt
@@ -21,6 +21,14 @@ class EnsemblGeneDAO(url: String, user: String, password: String) {
                     .join(SEQ_REGION).on(EXON.SEQ_REGION_ID.eq(SEQ_REGION.SEQ_REGION_ID))
         }
 
+        private fun startExonQuery(context: DSLContext): SelectOnConditionStep<ExonRecord> {
+            return baseExonQuery(context).join(TRANSLATION).on(TRANSLATION.START_EXON_ID.eq(EXON.EXON_ID))
+        }
+
+        private fun endExonQuery(context: DSLContext): SelectOnConditionStep<ExonRecord> {
+            return baseExonQuery(context).join(TRANSLATION).on(TRANSLATION.END_EXON_ID.eq(EXON.EXON_ID))
+        }
+
         private fun baseTranslationQuery(context: DSLContext): SelectJoinStep<TranslationRecord> {
             return context.select(TRANSLATION.SEQ_START, TRANSLATION.SEQ_END).from(TRANSLATION)
         }
@@ -50,46 +58,78 @@ class EnsemblGeneDAO(url: String, user: String, password: String) {
 
     fun canonicalGeneModel(geneName: String): Gene? {
         val exons = canonicalExons(geneName)
-        return canonicalSequenceStartEnd(geneName)?.run { Gene(exons, first, second) }
+        return canonicalSequenceStartEnd(geneName)?.run {
+            Gene(exons, canonicalStartExon(geneName), canonicalEndExon(geneName), first, second)
+        }
     }
 
     fun transcriptGeneModel(transcript: String): Gene? {
         val exons = transcriptExons(transcript)
-        return transcriptSequenceStartEnd(transcript)?.run { Gene(exons, first, second) }
+        return transcriptSequenceStartEnd(transcript)?.run { Gene(exons, startExon(transcript), endExon(transcript), first, second) }
     }
 
     private fun canonicalExons(geneName: String): List<Exon> {
-        return baseExonQuery(context).run {
-            this.join(GENE).on(EXON_TRANSCRIPT.TRANSCRIPT_ID.eq(GENE.CANONICAL_TRANSCRIPT_ID))
-                    .join(XREF).on(XREF.XREF_ID.eq(GENE.DISPLAY_XREF_ID))
-                    .where(XREF.DISPLAY_LABEL.eq(geneName))
-                    .stream()
-        }.run(createExonsLambda)
+        return baseExonQuery(context).join(GENE).on(EXON_TRANSCRIPT.TRANSCRIPT_ID.eq(GENE.CANONICAL_TRANSCRIPT_ID))
+                .join(XREF).on(XREF.XREF_ID.eq(GENE.DISPLAY_XREF_ID))
+                .where(XREF.DISPLAY_LABEL.eq(geneName))
+                .stream()
+                .run(createExonsLambda)
     }
 
     private fun canonicalSequenceStartEnd(geneName: String): Pair<Long, Long>? {
-        return baseTranslationQuery(context).run {
-            this.join(GENE).on(TRANSLATION.TRANSCRIPT_ID.eq(GENE.CANONICAL_TRANSCRIPT_ID))
-                    .join(XREF).on(XREF.XREF_ID.eq(GENE.DISPLAY_XREF_ID))
-                    .where(XREF.DISPLAY_LABEL.eq(geneName))
-                    .stream()
-        }.run(createGeneStartEndLambda)
+        return baseTranslationQuery(context).join(GENE).on(TRANSLATION.TRANSCRIPT_ID.eq(GENE.CANONICAL_TRANSCRIPT_ID))
+                .join(XREF).on(XREF.XREF_ID.eq(GENE.DISPLAY_XREF_ID))
+                .where(XREF.DISPLAY_LABEL.eq(geneName))
+                .stream()
+                .run(createGeneStartEndLambda)
+    }
+
+    private fun canonicalStartExon(geneName: String): Exon {
+        return startExonQuery(context).join(GENE).on(EXON_TRANSCRIPT.TRANSCRIPT_ID.eq(GENE.CANONICAL_TRANSCRIPT_ID))
+                .join(XREF).on(XREF.XREF_ID.eq(GENE.DISPLAY_XREF_ID))
+                .where(XREF.DISPLAY_LABEL.eq(geneName))
+                .stream()
+                .run(createExonsLambda)
+                .first()
+    }
+
+    private fun canonicalEndExon(geneName: String): Exon {
+        return endExonQuery(context).join(GENE).on(EXON_TRANSCRIPT.TRANSCRIPT_ID.eq(GENE.CANONICAL_TRANSCRIPT_ID))
+                .join(XREF).on(XREF.XREF_ID.eq(GENE.DISPLAY_XREF_ID))
+                .where(XREF.DISPLAY_LABEL.eq(geneName))
+                .stream()
+                .run(createExonsLambda)
+                .first()
     }
 
 
     private fun transcriptExons(transcript: String): List<Exon> {
-        return baseExonQuery(context).run {
-            this.join(TRANSCRIPT).on(EXON_TRANSCRIPT.TRANSCRIPT_ID.eq(TRANSCRIPT.TRANSCRIPT_ID))
-                    .where(TRANSCRIPT.STABLE_ID.eq(transcript))
-                    .stream()
-        }.run(createExonsLambda)
+        return baseExonQuery(context).join(TRANSCRIPT).on(EXON_TRANSCRIPT.TRANSCRIPT_ID.eq(TRANSCRIPT.TRANSCRIPT_ID))
+                .where(TRANSCRIPT.STABLE_ID.eq(transcript))
+                .stream()
+                .run(createExonsLambda)
     }
 
     private fun transcriptSequenceStartEnd(transcript: String): Pair<Long, Long>? {
-        return baseTranslationQuery(context).run {
-            this.join(TRANSCRIPT).on(TRANSLATION.TRANSCRIPT_ID.eq(TRANSCRIPT.TRANSCRIPT_ID))
-                    .where(TRANSCRIPT.STABLE_ID.eq(transcript))
-                    .stream()
-        }.run(createGeneStartEndLambda)
+        return baseTranslationQuery(context).join(TRANSCRIPT).on(TRANSLATION.TRANSCRIPT_ID.eq(TRANSCRIPT.TRANSCRIPT_ID))
+                .where(TRANSCRIPT.STABLE_ID.eq(transcript))
+                .stream()
+                .run(createGeneStartEndLambda)
+    }
+
+    private fun startExon(transcript: String): Exon {
+        return startExonQuery(context)
+                .join(TRANSCRIPT).on(TRANSLATION.TRANSCRIPT_ID.eq(TRANSCRIPT.TRANSCRIPT_ID)).where(TRANSCRIPT.STABLE_ID.eq(transcript))
+                .stream()
+                .run(createExonsLambda)
+                .first()
+    }
+
+    private fun endExon(transcript: String): Exon {
+        return endExonQuery(context).join(TRANSCRIPT).on(TRANSLATION.TRANSCRIPT_ID.eq(TRANSCRIPT.TRANSCRIPT_ID))
+                .where(TRANSCRIPT.STABLE_ID.eq(transcript))
+                .stream()
+                .run(createExonsLambda)
+                .first()
     }
 }
