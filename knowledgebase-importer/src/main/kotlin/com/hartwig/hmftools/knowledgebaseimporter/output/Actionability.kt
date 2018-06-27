488408f967187c62c0514b361268ba998f307b0f
Mircea Voda <mircearaul.voda@gmail.com>
2018-06-27 15:38:19 +0200

    add reference field to actionability

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/output/Actionability.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/output/Actionability.kt
@@ -2,14 +2,15 @@ package com.hartwig.hmftools.knowledgebaseimporter.output
 
 import com.hartwig.hmftools.extensions.csv.CsvData
 
-data class Actionability(val source: String, val drug: HmfDrug, val cancerType: String, val level: String, val hmfLevel: String,
-                         val evidenceType: String, val significance: String, val hmfResponse: String) : CsvData {
+data class Actionability(val source: String, val reference: String, val drug: HmfDrug, val cancerType: String, val level: String,
+                         val hmfLevel: String, val evidenceType: String, val significance: String, val hmfResponse: String) : CsvData {
     companion object {
-        operator fun invoke(source: String, cancerTypes: List<String>, drugs: List<HmfDrug>, level: String, significance: String,
-                            evidenceType: String, hmfLevel: HmfLevel, hmfResponse: HmfResponse): List<Actionability> {
+        operator fun invoke(source: String, reference: String, cancerTypes: List<String>, drugs: List<HmfDrug>, level: String,
+                            significance: String, evidenceType: String, hmfLevel: HmfLevel, hmfResponse: HmfResponse): List<Actionability> {
             return cancerTypes.flatMap { cancerType ->
                 drugs.map { drug ->
-                    Actionability(source, drug, cancerType, level, hmfLevel.toString(), evidenceType, significance, hmfResponse.toString())
+                    Actionability(source, reference, drug, cancerType, level, hmfLevel.toString(), evidenceType, significance,
+                                  hmfResponse.toString())
                 }
             }
         }
