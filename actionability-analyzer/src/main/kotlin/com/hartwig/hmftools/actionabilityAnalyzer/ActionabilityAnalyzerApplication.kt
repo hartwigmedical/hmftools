ac0afc675ee564a3a55ab16eaa871399445d4cf8
Mircea Voda <mircearaul.voda@gmail.com>
2018-07-03 16:47:14 +0200

    read cohort mutations from file instead of database

--- actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
+++ actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
@@ -1,11 +1,11 @@
 package com.hartwig.hmftools.actionabilityAnalyzer
 
+import com.hartwig.hmftools.extensions.csv.CsvReader
 import com.hartwig.hmftools.extensions.csv.CsvWriter
 import com.hartwig.hmftools.knowledgebaseimporter.readCSVRecords
 import com.hartwig.hmftools.patientdb.dao.DatabaseAccess
 import com.hartwig.hmftools.patientdb.data.PotentialActionableCNV
 import com.hartwig.hmftools.patientdb.data.PotentialActionableFusion
-import com.hartwig.hmftools.patientdb.data.PotentialActionableVariant
 import org.apache.logging.log4j.LogManager
 import java.util.stream.Stream
 import kotlin.streams.asSequence
@@ -24,6 +24,7 @@ private val actionableGenomicRanges = "actionableRanges"
 private val cancerTypes = "knowledgebaseCancerTypes"
 private val allDbSamples = false
 private val cohortCsvLocation = "cohort.csv"
+private val cohortMutations = "cohortMutations.tsv"
 
 fun main(args: Array<String>) {
     val dbAccess = DatabaseAccess(user, password, databaseUrl)
@@ -37,21 +38,16 @@ fun main(args: Array<String>) {
 }
 
 private fun queryDatabase(dbAccess: DatabaseAccess, samplesToAnalyze: Map<String, String>, actionabilityAnalyzer: ActionabilityAnalyzer) {
-    logger.info("Querying actionable variants.")
     val records = mutableListOf<ActionabilityOutput>()
-    potentiallyActionableVariants(dbAccess, samplesToAnalyze).use {
-        val variantRecords = it.asSequence().mapIndexed { index, variant ->
-            logProgress(index)
-            actionabilityAnalyzer.actionabilityForVariant(variant) + actionabilityAnalyzer.rangeActionabilityForVariant(variant)
-        }.flatten().toList()
+    cohortMutations(samplesToAnalyze).forEach { mutation ->
+        val variantRecords = actionabilityAnalyzer.actionabilityForVariant(mutation) +
+                actionabilityAnalyzer.rangeActionabilityForVariant(mutation)
         records.addAll(variantRecords)
     }
-    logger.info("Querying actionable cnvs.")
     potentiallyActionableCNVs(dbAccess, samplesToAnalyze).use {
         val cnvRecords = it.asSequence().flatMap { actionabilityAnalyzer.actionabilityForCNV(it).asSequence() }.toList()
         records.addAll(cnvRecords)
     }
-    logger.info("Querying actionable fusions.")
     potentiallyActionableFusions(dbAccess, samplesToAnalyze).use {
         val fusionRecords = it.asSequence().flatMap { actionabilityAnalyzer.actionabilityForFusion(it).asSequence() }.toList()
         records.addAll(fusionRecords)
@@ -61,27 +57,25 @@ private fun queryDatabase(dbAccess: DatabaseAccess, samplesToAnalyze: Map<String
     logger.info("Done.")
 }
 
-private fun logProgress(index: Int) {
-    if (index % 1000000 == 0) {
-        logger.info("Processed $index records...")
-    }
-}
-
 private fun readSamples(dbAccess: DatabaseAccess): Map<String, String> =
         if (allDbSamples) dbAccess.allSamplesAndTumorLocations().toList().associate { Pair(it.key, it.value) }
         else readCSVRecords(cohortCsvLocation) { Pair(it[0], it[1]) }.toMap()
 
-private fun potentiallyActionableVariants(dbAccess: DatabaseAccess,
-                                          samplesToAnalyze: Map<String, String>): Stream<PotentialActionableVariant> =
-        if (allDbSamples) dbAccess.allPotentiallyActionableVariants()
-        else dbAccess.potentiallyActionableVariants(samplesToAnalyze.keys)
+private fun cohortMutations(samplesToAnalyze: Map<String, String>): List<CohortMutation> {
+    logger.info("Looking up cohort mutations")
+    return CsvReader.readTSV<CohortMutation>(cohortMutations).filter { samplesToAnalyze.containsKey(it.sampleId) }
+            .flatMap { mutation -> mutation.alt.split(",").map { mutation.copy(alt = it) } }
+}
 
-private fun potentiallyActionableCNVs(dbAccess: DatabaseAccess,
-                                      samplesToAnalyze: Map<String, String>): Stream<PotentialActionableCNV> =
-        if (allDbSamples) dbAccess.allPotentiallyActionableCNVs()
-        else dbAccess.potentiallyActionableCNVs(samplesToAnalyze.keys)
+private fun potentiallyActionableCNVs(dbAccess: DatabaseAccess, samplesToAnalyze: Map<String, String>): Stream<PotentialActionableCNV> {
+    logger.info("Querying actionable cnvs.")
+    return if (allDbSamples) dbAccess.allPotentiallyActionableCNVs()
+    else dbAccess.potentiallyActionableCNVs(samplesToAnalyze.keys)
+}
 
 private fun potentiallyActionableFusions(dbAccess: DatabaseAccess,
-                                         samplesToAnalyze: Map<String, String>): Stream<PotentialActionableFusion> =
-        if (allDbSamples) dbAccess.allPotentiallyActionableFusions()
-        else dbAccess.potentiallyActionableFusions(samplesToAnalyze.keys)
+                                         samplesToAnalyze: Map<String, String>): Stream<PotentialActionableFusion> {
+    logger.info("Querying actionable fusions.")
+    return if (allDbSamples) dbAccess.allPotentiallyActionableFusions()
+    else dbAccess.potentiallyActionableFusions(samplesToAnalyze.keys)
+}
