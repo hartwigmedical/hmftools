5ffcb3ad1057f5eb098a40bc4cc65ff5a4e1dd09
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-07-03 11:26:10 +0200

    Refactoring in repeat context construction

--- hmf-common/src/test/java/com/hartwig/hmftools/common/purple/repeat/RepeatContextTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/purple/repeat/RepeatContextTest.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
-@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class RepeatContextTest {
 
     @Test
@@ -26,41 +25,15 @@ public class RepeatContextTest {
         assertRepeats("B", 4, RepeatContextFactory.repeats(11, sequence));
     }
 
-    private void assertRepeats(final String expectedSequence, final int expectedCount, @NotNull final String victim) {
+    private static void assertRepeats(final String expectedSequence, final int expectedCount, @NotNull final String victim) {
         for (int i = 0; i < victim.length(); i++) {
             assertRepeats(expectedSequence, expectedCount, RepeatContextFactory.repeats(i, victim));
         }
     }
 
-    private void assertRepeats(final String expectedSequence, final int expectedCount, @NotNull final Optional<RepeatContext> victim) {
+    private static void assertRepeats(final String expectedSequence, final int expectedCount, @NotNull final Optional<RepeatContext> victim) {
         assertTrue(victim.isPresent());
         assertEquals(expectedSequence, victim.get().sequence());
         assertEquals(expectedCount, victim.get().count());
-    }
-
-    @Test
-    public void testForwardsRepeat() {
-        assertEquals(1, RepeatContextFactory.forwardRepeats("A", "A"));
-        assertEquals(2, RepeatContextFactory.forwardRepeats("A", "AA"));
-        assertEquals(3, RepeatContextFactory.forwardRepeats("A", "AAA"));
-
-        assertEquals(0, RepeatContextFactory.forwardRepeats("AC", "A"));
-        assertEquals(1, RepeatContextFactory.forwardRepeats("AC", "AC"));
-        assertEquals(1, RepeatContextFactory.forwardRepeats("AC", "ACA"));
-        assertEquals(2, RepeatContextFactory.forwardRepeats("AC", "ACAC"));
-        assertEquals(2, RepeatContextFactory.forwardRepeats("AC", "ACACA"));
-    }
-
-    @Test
-    public void testBackwardsRepeat() {
-        assertEquals(1, RepeatContextFactory.backwardRepeats("A", "A"));
-        assertEquals(2, RepeatContextFactory.backwardRepeats("A", "AA"));
-        assertEquals(3, RepeatContextFactory.backwardRepeats("A", "AAA"));
-
-        assertEquals(0, RepeatContextFactory.backwardRepeats("AC", "A"));
-        assertEquals(1, RepeatContextFactory.backwardRepeats("AC", "AC"));
-        assertEquals(1, RepeatContextFactory.backwardRepeats("AC", "AAC"));
-        assertEquals(2, RepeatContextFactory.backwardRepeats("AC", "ACAC"));
-        assertEquals(2, RepeatContextFactory.backwardRepeats("AC", "AACAC"));
     }
 }
