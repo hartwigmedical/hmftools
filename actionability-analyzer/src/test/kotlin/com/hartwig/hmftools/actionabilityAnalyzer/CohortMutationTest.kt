769f40b0248e8a46974bf3fb30b015cb5bb4ce7c
Mircea Voda <mircearaul.voda@gmail.com>
2018-07-09 11:42:15 +0200

    add impact field to cohort mutations tests

--- actionability-analyzer/src/test/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/CohortMutationTest.kt
+++ actionability-analyzer/src/test/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/CohortMutationTest.kt
@@ -6,49 +6,61 @@ import io.kotlintest.specs.StringSpec
 class CohortMutationTest : StringSpec() {
     init {
         "splice oncogene is not potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+            val variant = CohortMutation("", "", "", "", "", "", "", "Splice", "",
                                          "SPLICE", "", "TRUE", "")
             variant.potentiallyActionable shouldBe false
         }
 
         "splice non-oncogene is potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+            val variant = CohortMutation("", "", "", "", "", "", "", "Splice", "",
                                          "SPLICE", "", "", "")
             variant.potentiallyActionable shouldBe true
         }
 
-        "nonsense or frameshift oncogene not potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+        "nonsense oncogene not potentially actionable" {
+            val variant = CohortMutation("", "", "", "", "", "", "", "Nonsense", "",
                                          "NONSENSE_OR_FRAMESHIFT", "", "TRUE", "")
             variant.potentiallyActionable shouldBe false
         }
 
-        "nonsense or frameshift non-oncogene is potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+        "frameshift oncogene not potentially actionable" {
+            val variant = CohortMutation("", "", "", "", "", "", "", "Frameshift", "",
+                                         "NONSENSE_OR_FRAMESHIFT", "", "TRUE", "")
+            variant.potentiallyActionable shouldBe false
+        }
+
+        "nonsense non-oncogene is potentially actionable" {
+            val variant = CohortMutation("", "", "", "", "", "", "", "Nonsense", "",
+                                         "NONSENSE_OR_FRAMESHIFT", "", "", "")
+            variant.potentiallyActionable shouldBe true
+        }
+
+        "frameshift non-oncogene is potentially actionable" {
+            val variant = CohortMutation("", "", "", "", "", "", "", "Frameshift", "",
                                          "NONSENSE_OR_FRAMESHIFT", "", "", "")
             variant.potentiallyActionable shouldBe true
         }
 
         "synonymous effect is not potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+            val variant = CohortMutation("", "", "", "", "", "", "", "Synonymous", "",
                                          "SYNONYMOUS", "", "", "")
             variant.potentiallyActionable shouldBe false
         }
 
-        "none effect is not potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
-                                         "NONE", "", "", "")
-            variant.potentiallyActionable shouldBe false
-        }
-
         "missense is potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+            val variant = CohortMutation("", "", "", "", "", "", "", "Missense", "",
                                          "MISSENSE", "", "", "")
             variant.potentiallyActionable shouldBe true
         }
 
+        "inframe is potentially actionable" {
+            val variant = CohortMutation("", "", "", "", "", "", "", "Inframe", "",
+                                         "", "", "", "")
+            variant.potentiallyActionable shouldBe true
+        }
+
         "missense oncogene is potentially actionable" {
-            val variant = CohortMutation("", "", "", "", "", "", "", "", "",
+            val variant = CohortMutation("", "", "", "", "", "", "", "Missense", "",
                                          "MISSENSE", "", "TRUE", "")
             variant.potentiallyActionable shouldBe true
         }
