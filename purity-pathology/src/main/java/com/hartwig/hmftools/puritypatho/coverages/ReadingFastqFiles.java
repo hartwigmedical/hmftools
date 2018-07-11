c069b1af2cb5597017935c0fb09ac7f184f094da
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-07-11 15:44:24 +0200

    DEV-445: reading fastq.gz file

--- purity-pathology/src/main/java/com/hartwig/hmftools/puritypatho/coverages/ReadingFastqFiles.java
+++ purity-pathology/src/main/java/com/hartwig/hmftools/puritypatho/coverages/ReadingFastqFiles.java
@@ -1,26 +1,27 @@
 package com.hartwig.hmftools.puritypatho.coverages;
 
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-import htsjdk.samtools.fastq.FastqReader;
-import htsjdk.samtools.fastq.FastqRecord;
-
 public class ReadingFastqFiles {
     private static final Logger LOGGER = LogManager.getLogger(ReadingFastqFiles.class);
 
     @NotNull
-    public static void readingFiles(@NotNull String fastqDir) {
+    public static void readingFiles(@NotNull String fastqDir) throws IOException {
         LOGGER.info("fastqDir: " + fastqDir);
         File folder = new File(fastqDir);
         String [] fileList = folder.list();
-        for (String fileName:fileList) {
-            LOGGER.info("fileName: " + fileName);
-
+        for (String file:fileList) {
+            if (file.endsWith(".fastq.gz")) {
+                LOGGER.info(".fastq.gz file");
+            } else if (file.endsWith(".fastq")) {
+                LOGGER.info(".fastq file");
+            }
+            LOGGER.info("Processing file: " + file);
         }
     }
 }
