bd97259dece74dd05aafc03eddf5b6836e6c9041
Mircea Voda <mircearaul.voda@gmail.com>
2018-07-19 13:38:03 +0200

    add logging to transvar analyzer

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/transvar/TransvarAnalyzer.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/transvar/TransvarAnalyzer.kt
@@ -1,7 +1,10 @@
 package com.hartwig.hmftools.knowledgebaseimporter.transvar
 
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.HgvsAnnotation
+import org.apache.logging.log4j.LogManager
 import java.io.*
+
+private val logger = LogManager.getLogger("TransvarAnalyzer")
 
 interface TransvarAnalyzer<in T : HgvsAnnotation> {
     val analysisMode: String
@@ -18,8 +21,10 @@ interface TransvarAnalyzer<in T : HgvsAnnotation> {
 
     fun analyze(variants: List<T>): List<TransvarOutput> {
         val variantFile = createVariantFile(variants)
-        val args = arrayOf("--noheader", "--ensembl", "--max-candidates", "1000", "-g", "1", "-m", "2", "-l")
+        val args = arrayOf("--noheader", "--ensembl", "--refversion", "hg19", "--max-candidates", "1000", "-g", "1", "-m", "2", "-l")
+        logger.info("Creating transvar analyzer with command: $transvarLocation $analysisMode ${args.joinToString(" ")}")
         val process = ProcessBuilder(transvarLocation, analysisMode, *args, variantFile.absolutePath).start()
+        InputStreamReader(process.errorStream).readLines().forEach { throw Exception("Transvar error: $it") }
         val inputStream = BufferedReader(InputStreamReader(process.inputStream))
         return inputStream.lineSequence().map { TransvarOutput(it) }.toList()
     }
