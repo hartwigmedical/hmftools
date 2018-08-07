9174faeb3693fd25cb270f50b1c6aca488a6a90c
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-07 15:52:53 +0200

    add iclusion indication missing fields

--- api-clients/src/main/kotlin/com/hartwig/hmftools/apiclients/iclusion/data/Indication.kt
+++ api-clients/src/main/kotlin/com/hartwig/hmftools/apiclients/iclusion/data/Indication.kt
@@ -1,3 +1,3 @@
 package com.hartwig.hmftools.apiclients.iclusion.data
 
-data class Indication(val id: String, val doid: String, val indication_name: String)
+data class Indication(val id: String, val doid: String, val doid2: String?, val indication_name: String, val node_ids: List<String>)
