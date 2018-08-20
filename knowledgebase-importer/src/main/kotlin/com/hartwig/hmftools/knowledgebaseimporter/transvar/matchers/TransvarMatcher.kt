d715cd20e14043fa9670d75e37ca1d88d82d50d9
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-20 17:34:09 +0200

    add transvar cdna and gdna matchers

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/transvar/matchers/TransvarMatcher.kt
@@ -0,0 +1,12 @@
+package com.hartwig.hmftools.knowledgebaseimporter.transvar.matchers
+
+object TransvarMatcher : Matcher {
+    private val nestedMatchers = listOf(TransvarCDnaMatcher, TransvarGDnaMatcher, TransvarProteinMatcher)
+    override fun matches(string: String): Boolean {
+        return nestedMatchers.any { it.matches(string) }
+    }
+
+    override fun contains(string: String): Boolean {
+        return nestedMatchers.any { it.contains(string) }
+    }
+}
