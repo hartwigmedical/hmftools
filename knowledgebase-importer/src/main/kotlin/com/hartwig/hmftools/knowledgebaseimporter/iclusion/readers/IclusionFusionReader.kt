16f50c037f09cb7a94a1683527054fd2001f99de
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-21 09:55:47 +0200

    add iclusion fusion reader

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/iclusion/readers/IclusionFusionReader.kt
@@ -0,0 +1,23 @@
+package com.hartwig.hmftools.knowledgebaseimporter.iclusion.readers
+
+import com.hartwig.hmftools.knowledgebaseimporter.FusionReader
+import com.hartwig.hmftools.knowledgebaseimporter.iclusion.IclusionEvent
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.EventType
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.SomaticEventReader
+import com.hartwig.hmftools.knowledgebaseimporter.output.FusionEvent
+
+object IclusionFusionReader : SomaticEventReader<IclusionEvent, FusionEvent> {
+    const val FUSION_SEPARATOR = "-"
+    private val fusionReader = FusionReader(separators = setOf(FUSION_SEPARATOR))
+
+    private fun match(event: IclusionEvent): Boolean {
+        val mutation = event.variant.toLowerCase()
+        return (event.types.size == 1 && event.types.contains(EventType.FUS)) &&
+                (mutation.contains(FUSION_SEPARATOR) && mutation.contains(event.gene.toLowerCase().substring(0, 3)))
+    }
+
+    override fun read(event: IclusionEvent): List<FusionEvent> {
+        if (match(event)) return listOfNotNull(fusionReader.read(event.gene, event.variant))
+        return emptyList()
+    }
+}
