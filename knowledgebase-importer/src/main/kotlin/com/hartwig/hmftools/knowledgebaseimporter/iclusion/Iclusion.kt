3d52f8e1a858310ae5832e550a8996df6da67ff0
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-22 15:21:28 +0200

    add iclusion knowledgebase

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/iclusion/Iclusion.kt
@@ -0,0 +1,37 @@
+package com.hartwig.hmftools.knowledgebaseimporter.iclusion
+
+import com.hartwig.hmftools.apiclients.iclusion.api.IclusionApiWrapper
+import com.hartwig.hmftools.knowledgebaseimporter.Knowledgebase
+import com.hartwig.hmftools.knowledgebaseimporter.dao.GeneDAO
+import com.hartwig.hmftools.knowledgebaseimporter.diseaseOntology.DiseaseOntology
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.ActionableRecord
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.KnowledgebaseSource
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.RecordAnalyzer
+import com.hartwig.hmftools.knowledgebaseimporter.output.*
+
+class Iclusion(iclusionApiWrapper: IclusionApiWrapper, diseaseOntology: DiseaseOntology, recordAnalyzer: RecordAnalyzer, geneDAO: GeneDAO) :
+        Knowledgebase, KnowledgebaseSource<IclusionRecord, ActionableRecord> {
+    override val source: String = "iclusion"
+
+    private val iclusionStudies = iclusionApiWrapper.studyDetails()
+    private val canonicalTranscripts = iclusionStudies.flatMap { it.mutations.map { it.geneName } }.distinct().map {
+        Pair(it, geneDAO.hmfCanonicalTranscript(it).getOrNull(0))
+    }.toMap()
+    override val knownVariants by lazy { recordAnalyzer.knownVariants(listOf(this)).distinct() }
+    override val knownFusionPairs by lazy { knownKbRecords.flatMap { it.events }.filterIsInstance<FusionPair>().distinct() }
+    override val promiscuousGenes by lazy { knownKbRecords.flatMap { it.events }.filterIsInstance<PromiscuousGene>().distinct() }
+    override val actionableVariants by lazy { actionableKbItems.filterIsInstance<ActionableVariantOutput>() }
+    override val actionableCNVs by lazy { actionableKbItems.filterIsInstance<ActionableCNVOutput>() }
+    override val actionableFusionPairs by lazy { actionableKbItems.filterIsInstance<ActionableFusionPairOutput>() }
+    override val actionablePromiscuousGenes by lazy { actionableKbItems.filterIsInstance<ActionablePromiscuousGeneOutput>() }
+    override val actionableRanges by lazy { actionableKbItems.filterIsInstance<ActionableGenomicRangeOutput>() }
+    override val cancerTypes by lazy {
+        actionableKbRecords.flatMap { it.doids.entries }
+                .associateBy({ it.key }, { it.value.flatMap { diseaseOntology.findDoids(it) }.toSet() })
+    }
+
+    override val knownKbRecords: List<IclusionRecord> by lazy { iclusionStudies.flatMap { IclusionRecord(it, canonicalTranscripts) } }
+
+    override val actionableKbRecords = knownKbRecords
+    private val actionableKbItems by lazy { recordAnalyzer.actionableItems(listOf(this)).distinct() }
+}
