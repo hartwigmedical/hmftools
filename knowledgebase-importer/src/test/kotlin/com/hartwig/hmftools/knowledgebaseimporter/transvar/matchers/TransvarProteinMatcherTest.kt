b769bffaed9d1d868965c945be171fa797a36601
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-23 16:28:35 +0200

    make transvar protein matcher more readable + update tests

--- knowledgebase-importer/src/test/kotlin/com/hartwig/hmftools/knowledgebaseimporter/transvar/matchers/TransvarProteinMatcherTest.kt
+++ knowledgebase-importer/src/test/kotlin/com/hartwig/hmftools/knowledgebaseimporter/transvar/matchers/TransvarProteinMatcherTest.kt
@@ -6,31 +6,83 @@ import io.kotlintest.specs.StringSpec
 class TransvarProteinMatcherTest : StringSpec() {
 
     init {
-        "matches protein mutation" {
-            TransvarProteinMatcher.matches("V600E") shouldBe true
-            TransvarProteinMatcher.matches("Val600Glu") shouldBe true
+        "matches substitutions"{
+            TransvarProteinMatcher.matches("p.Trp24Cys") shouldBe true
+            TransvarProteinMatcher.matches("Trp24Cys") shouldBe true
+            TransvarProteinMatcher.matches("p.Trp24Ter") shouldBe true
+            TransvarProteinMatcher.matches("Trp24Ter") shouldBe true
+            TransvarProteinMatcher.matches("p.Trp24*") shouldBe true
+            TransvarProteinMatcher.matches("Trp24*") shouldBe true
+            TransvarProteinMatcher.matches("p.*757L") shouldBe true
+            TransvarProteinMatcher.matches("*757L") shouldBe true
         }
 
-        "complex variant does not match"{
+        "matches substitutions with mixed AA lengths"{
+            TransvarProteinMatcher.matches("p.V600Glu") shouldBe true
+            TransvarProteinMatcher.matches("V600Glu") shouldBe true
+            TransvarProteinMatcher.matches("p.Val600E") shouldBe true
+            TransvarProteinMatcher.matches("Val600E") shouldBe true
+        }
+
+        "matches deletions"{
+            TransvarProteinMatcher.matches("p.Gln8del") shouldBe true
+            TransvarProteinMatcher.matches("Gln8del") shouldBe true
+            TransvarProteinMatcher.matches("p.*4del") shouldBe true
+            TransvarProteinMatcher.matches("*4del") shouldBe true
+            TransvarProteinMatcher.matches("p.Cys28_Met30del") shouldBe true
+            TransvarProteinMatcher.matches("Cys28_Met30del") shouldBe true
+        }
+
+        "matches duplications"{
+            TransvarProteinMatcher.matches("p.G10dup") shouldBe true
+            TransvarProteinMatcher.matches("G10dup") shouldBe true
+            TransvarProteinMatcher.matches("p.Gly4_Gln6dup") shouldBe true
+            TransvarProteinMatcher.matches("Gly4_Gln6dup") shouldBe true
+        }
+
+        "matches insertions"{
+            TransvarProteinMatcher.matches("p.His4_Gln5insAla") shouldBe true
+            TransvarProteinMatcher.matches("His4_Gln5insAla") shouldBe true
+            TransvarProteinMatcher.matches("p.Lys2_Met3insGlnSerLys") shouldBe true
+            TransvarProteinMatcher.matches("Lys2_Met3insGlnSerLys") shouldBe true
+            TransvarProteinMatcher.matches("p.W603_E604insDREYEYDLKW") shouldBe true
+            TransvarProteinMatcher.matches("W603_E604insDREYEYDLKW") shouldBe true
+        }
+
+        "matches deletion-insertions"{
+            TransvarProteinMatcher.matches("p.Cys28delinsTrp") shouldBe true
+            TransvarProteinMatcher.matches("Cys28delinsTrp") shouldBe true
+            TransvarProteinMatcher.matches("p.Cys28delinsTrpVal") shouldBe true
+            TransvarProteinMatcher.matches("Cys28delinsTrpVal") shouldBe true
+            TransvarProteinMatcher.matches("p.Cys28_Lys29delinsTrp") shouldBe true
+            TransvarProteinMatcher.matches("Cys28_Lys29delinsTrp") shouldBe true
+            TransvarProteinMatcher.matches("p.C420_A423delinsW") shouldBe true
+            TransvarProteinMatcher.matches("C420_A423delinsW") shouldBe true
+            TransvarProteinMatcher.matches("p.Pro578_Lys579delinsLeuTer") shouldBe true
+            TransvarProteinMatcher.matches("Pro578_Lys579delinsLeuTer") shouldBe true
+        }
+
+        "matches frameshifts" {
+            TransvarProteinMatcher.matches("p.Glu5fs") shouldBe true
+            TransvarProteinMatcher.matches("Glu5fs") shouldBe true
+            TransvarProteinMatcher.matches("p.Glu5ValfsTer5") shouldBe true
+            TransvarProteinMatcher.matches("Glu5ValfsTer5") shouldBe true
+            TransvarProteinMatcher.matches("p.Glu5Valfs*5") shouldBe true
+            TransvarProteinMatcher.matches("Glu5Valfs*5") shouldBe true
+            TransvarProteinMatcher.matches("p.Ile327Argfs*?") shouldBe true
+            TransvarProteinMatcher.matches("Ile327Argfs*?") shouldBe true
+        }
+
+        "does not match codon mutations" {
+            TransvarProteinMatcher.matches("V600") shouldBe false
+            TransvarProteinMatcher.matches("Val600") shouldBe false
+        }
+
+        "does not match complex variants"{
             TransvarProteinMatcher.matches("Fusion + V600E") shouldBe false
             TransvarProteinMatcher.matches("Fusion + Val600E") shouldBe false
             TransvarProteinMatcher.matches("Fusion + V600") shouldBe false
             TransvarProteinMatcher.matches("Fusion + Val600") shouldBe false
-        }
-
-        "matches codon mutation" {
-            TransvarProteinMatcher.matches("V600") shouldBe true
-            TransvarProteinMatcher.matches("Val600") shouldBe true
-        }
-
-        "finds protein mutation in complex variant"{
-            TransvarProteinMatcher.contains("Fusion + V600E") shouldBe true
-            TransvarProteinMatcher.contains("Fusion + Val600Glu") shouldBe true
-            TransvarProteinMatcher.contains("Fusion + V600") shouldBe true
-            TransvarProteinMatcher.contains("Fusion + Val600") shouldBe true
-            TransvarProteinMatcher.contains("Y175fs (c.526delA)") shouldBe true
-            TransvarProteinMatcher.contains("F76del (c.226_228delTTC)") shouldBe true
-            TransvarProteinMatcher.contains("G12/G13") shouldBe true
         }
 
         "does not match other variants" {
@@ -45,85 +97,20 @@ class TransvarProteinMatcherTest : StringSpec() {
             TransvarProteinMatcher.contains("Splicing alteration (c.463+2C>T)") shouldBe false
         }
 
-        "codon mutation does not match gene fusion"{
+        "does not match gene fusion"{
             TransvarProteinMatcher.contains("RANBP2-ALK") shouldBe false
-        }
-
-        "matches hgvs protein mutation"{
-            TransvarProteinMatcher.matches("Y175fs") shouldBe true
-            TransvarProteinMatcher.matches("F76del") shouldBe true
-            TransvarProteinMatcher.matches("Cys28delinsTrpVal") shouldBe true
-            TransvarProteinMatcher.matches("Arg97GlyfsTer26") shouldBe true
-            TransvarProteinMatcher.matches("Arg97Glyfs*26") shouldBe true
-            TransvarProteinMatcher.matches("Arg97_Leu833delinsGlyfsTer26") shouldBe true
-            TransvarProteinMatcher.matches("Glu5Valfs*5") shouldBe true
-            TransvarProteinMatcher.matches("Tyr4*") shouldBe true
-            TransvarProteinMatcher.matches("Asp2Metfs*4") shouldBe true
-            TransvarProteinMatcher.matches("Glu5Valfs*5") shouldBe true
-            TransvarProteinMatcher.matches("Ile327fs") shouldBe true
-            TransvarProteinMatcher.matches("Gln151Thrfs*9") shouldBe true
-        }
-
-        "matches hgvs protein substitutions"{
-            TransvarProteinMatcher.matches("*757L") shouldBe true
-            TransvarProteinMatcher.matches("Trp26Cys") shouldBe true
-            TransvarProteinMatcher.matches("Phe2_Met46del") shouldBe true
-            TransvarProteinMatcher.matches("Met1?") shouldBe true
-            TransvarProteinMatcher.matches("Trp26*") shouldBe true
-            TransvarProteinMatcher.matches("Trp26_Leu833del") shouldBe true
-        }
-
-        "matches hgvs protein deletions"{
-            TransvarProteinMatcher.matches("Gln8del") shouldBe true
-            TransvarProteinMatcher.matches("Cys28_Met30del") shouldBe true
-            TransvarProteinMatcher.matches("Met1_Leu833del") shouldBe true
-            TransvarProteinMatcher.matches("Met1_Lys45del") shouldBe true
-            TransvarProteinMatcher.matches("W26*") shouldBe true
-        }
-
-        "matches hgvs protein duplications"{
-            TransvarProteinMatcher.matches("G10dup") shouldBe true
-            TransvarProteinMatcher.matches("T1151dup") shouldBe true
-            TransvarProteinMatcher.matches("Gly4_Gln6dup") shouldBe true
-            TransvarProteinMatcher.matches("His7_Gln8dup") shouldBe true
-            TransvarProteinMatcher.matches("A502_Y503dup") shouldBe true
-        }
-
-        "matches hgvs protein insertions"{
-            TransvarProteinMatcher.matches("Lys2_Met3insGlnSerLys") shouldBe true
-            TransvarProteinMatcher.matches("Pro2_Ile3insGlyTer") shouldBe true
-            TransvarProteinMatcher.matches("Ile3_Ile3418delinsGly") shouldBe true
-            TransvarProteinMatcher.matches("W603_E604insDREYEYDLKW") shouldBe true
-        }
-
-        "matches hgvs protein indels"{
-            TransvarProteinMatcher.matches("Cys28_Lys29delinsTrp") shouldBe true
-            TransvarProteinMatcher.matches("Cys28delinsTrpVal") shouldBe true
-            TransvarProteinMatcher.matches("Pro578_Lys579delinsLeuTer") shouldBe true
-            TransvarProteinMatcher.matches("Pro578_Gln598del") shouldBe true
-            TransvarProteinMatcher.matches("Arg97Glyfs*26") shouldBe true
-            TransvarProteinMatcher.matches("Arg97GlyfsTer26") shouldBe true
-            TransvarProteinMatcher.matches("Arg97fs") shouldBe true
-            TransvarProteinMatcher.matches("Arg97_Leu833delinsGlyfsTer26") shouldBe true
-            TransvarProteinMatcher.matches("p.C420_A423delinsW") shouldBe true
-            TransvarProteinMatcher.matches("Arg97fs") shouldBe true
-            TransvarProteinMatcher.matches("Arg97Gly") shouldBe true
-            TransvarProteinMatcher.matches("Arg97ProfsTer23") shouldBe true
-            TransvarProteinMatcher.matches("Arg97Profs*23") shouldBe true
-            TransvarProteinMatcher.matches("Glu5Valfs*5") shouldBe true
-            TransvarProteinMatcher.matches("Tyr4*") shouldBe true
-            TransvarProteinMatcher.matches("Asp2Metfs*4") shouldBe true
-            TransvarProteinMatcher.matches("Asp2fs") shouldBe true
-            TransvarProteinMatcher.matches("Glu5Valfs*5") shouldBe true
-            TransvarProteinMatcher.matches("Glu5fs") shouldBe true
-            TransvarProteinMatcher.matches("Ile327Argfs*?") shouldBe true
-            TransvarProteinMatcher.matches("Ile327fs") shouldBe true
-            TransvarProteinMatcher.matches("Gln151Thrfs*9") shouldBe true
         }
 
         "does not match invalid frameshift"{
             TransvarProteinMatcher.matches("K442Nfs*") shouldBe false
             TransvarProteinMatcher.matches("N1333Gfs*") shouldBe false
+        }
+
+        "finds protein mutation in complex variant"{
+            TransvarProteinMatcher.contains("Fusion + V600E") shouldBe true
+            TransvarProteinMatcher.contains("Fusion + Val600Glu") shouldBe true
+            TransvarProteinMatcher.contains("Y175fs (c.526delA)") shouldBe true
+            TransvarProteinMatcher.contains("F76del (c.226_228delTTC)") shouldBe true
         }
     }
 }
