838b8a81c0e2cf5ec1d99be9ca522669d4eaa855
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-24 13:34:02 +0200

    move correcting onco fusion pairs into oncoKnownInput

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/input/OncoKnownInput.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/input/OncoKnownInput.kt
@@ -11,8 +11,12 @@ data class OncoKnownInput(private val Isoform: String?, val Gene: String, val Al
 
     override fun correct(): OncoKnownInput {
         return when {
-            Alteration.contains(Regex("IGH-NKX2")) && Gene == "NKX2-1" -> copy(Alteration = Alteration.replace("IGH-NKX2", "IGH-NKX2-1"))
-            else                                                       -> this
+            Alteration.contains("IGH-NKX2") && Gene == "NKX2-1" -> copy(Alteration = Alteration.replace("IGH-NKX2", "IGH-NKX2-1"))
+            Alteration.contains("ROS1-CD74")                    -> copy(Alteration = Alteration.replace("ROS1-CD74", "CD74-ROS1"))
+            Alteration.contains("EP300-MLL")                    -> copy(Alteration = Alteration.replace("EP300-MLL", "MLL-EP300"))
+            Alteration.contains("EP300-MOZ")                    -> copy(Alteration = Alteration.replace("EP300-MOZ", "MOZ-EP300"))
+            Alteration.contains("RET-CCDC6")                    -> copy(Alteration = Alteration.replace("RET-CCDC6", "CCDC6-RET"))
+            else                                                -> this
         }
     }
 }
