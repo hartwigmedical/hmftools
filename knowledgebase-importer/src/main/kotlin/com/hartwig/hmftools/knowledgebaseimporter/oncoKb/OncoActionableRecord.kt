320da1ec136f46e265601954d4ef236b3526ca0c
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-29 13:51:15 +0200

    update onco actionable and known records to use new readers

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoActionableRecord.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoActionableRecord.kt
@@ -1,29 +1,29 @@
 package com.hartwig.hmftools.knowledgebaseimporter.oncoKb
 
+import com.hartwig.hmftools.knowledgebaseimporter.iclusion.readers.IclusionProteinAnnotationReader
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.ActionableRecord
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.RecordMetadata
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.SomaticEvent
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.KnowledgebaseEventReader
 import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoActionableInput
+import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.readers.*
 import com.hartwig.hmftools.knowledgebaseimporter.output.Actionability
 import com.hartwig.hmftools.knowledgebaseimporter.output.HmfDrug
-import org.apache.logging.log4j.LogManager
 
 data class OncoActionableRecord(private val metadata: RecordMetadata, override val events: List<SomaticEvent>,
-                                override val actionability: List<Actionability>) : RecordMetadata by metadata, ActionableRecord {
+                                override val actionability: List<Actionability>) :
+        RecordMetadata by metadata, ActionableRecord {
     companion object {
-        private val logger = LogManager.getLogger("OncoActionableRecord")
-        private val somaticEventReader = OncoSomaticEventReader()
+        private val multiSnvReader = OncoAnyActionableEventReader(KnowledgebaseEventReader("oncoKb", IclusionProteinAnnotationReader))
+        private val reader = KnowledgebaseEventReader("oncoKb", OncoCnvReader, OncoExonMutationReader, OncoFusionReader,
+                                                      OncoGeneMutationReader, IclusionProteinAnnotationReader, multiSnvReader)
 
         operator fun invoke(input: OncoActionableInput, treatmentTypeMap: Map<String, String>): OncoActionableRecord {
             val drugs = readDrugEntries(input, treatmentTypeMap)
             val actionability = Actionability("oncoKb", input.reference, listOf(input.`Cancer Type`), drugs, input.level,
                                               input.significance.name, "Predictive", input.hmfLevel, input.significance)
-            val metadata = OncoMetadata(input.Gene, input.transcript)
-            val events = somaticEventReader.read(input.Gene, input.transcript, input.Alteration)
-            if (events.isEmpty()) {
-                val aOrBLevelCount = actionability.filter { it.hmfLevel == "A" || it.hmfLevel == "B" }.size
-                logger.warn("Could not extract somatic event from:\toncoKb\t${input.Gene}\t${input.Alteration}\t\t$aOrBLevelCount")
-            }
+            val metadata = OncoMetadata(input.gene, input.transcript)
+            val events = reader.read(input)
             return OncoActionableRecord(metadata, events, actionability)
         }
 
--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoKnownRecord.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoKnownRecord.kt
@@ -1,19 +1,31 @@
 package com.hartwig.hmftools.knowledgebaseimporter.oncoKb
 
+import com.hartwig.hmftools.knowledgebaseimporter.iclusion.readers.IclusionProteinAnnotationReader
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.KnownRecord
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.RecordMetadata
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.SomaticEvent
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.KnowledgebaseEventReader
 import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoKnownInput
+import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.readers.*
+import com.hartwig.hmftools.knowledgebaseimporter.output.FusionEvent
 
 data class OncoKnownRecord(private val metadata: RecordMetadata, override val additionalInfo: String,
-                           override val events: List<SomaticEvent>) : KnownRecord, RecordMetadata by metadata {
+                           override val events: List<SomaticEvent>) : KnownRecord,
+        RecordMetadata by metadata {
     companion object {
-        private val somaticEventReader = OncoSomaticEventReader()
+        private val multiSnvReader = OncoAnyKnownEventReader(KnowledgebaseEventReader("oncoKb", IclusionProteinAnnotationReader))
+        private val reader = KnowledgebaseEventReader("oncoKb", OncoCnvReader, OncoExonMutationReader, OncoFusionReader,
+                                                      OncoGeneMutationReader, IclusionProteinAnnotationReader, multiSnvReader)
 
         operator fun invoke(input: OncoKnownInput): OncoKnownRecord {
-            val metadata = OncoMetadata(input.Gene, input.transcript)
-            return OncoKnownRecord(metadata, input.Oncogenicity, somaticEventReader.read(input.Gene, input.transcript, input.Alteration,
-                                                                                         input.`Mutation Effect`))
+            val metadata = OncoMetadata(input.gene, input.transcript!!)
+            val events = readEvents(input)
+            return OncoKnownRecord(metadata, input.Oncogenicity, events)
+        }
+
+        private fun readEvents(input: OncoKnownInput): List<SomaticEvent> {
+            val events = reader.read(input)
+            return events.filterNot { it is FusionEvent && input.`Mutation Effect`.contains("Loss-of-function") }
         }
     }
 }
