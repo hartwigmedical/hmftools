3aee9aa5c2e0a3d7fc2b0e73e048b939e5b2ce92
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-29 12:50:48 +0200

    add extra onco fusion reader test case

--- knowledgebase-importer/src/test/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/readers/OncoFusionReaderTest.kt
+++ knowledgebase-importer/src/test/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/readers/OncoFusionReaderTest.kt
@@ -1,6 +1,7 @@
 package com.hartwig.hmftools.knowledgebaseimporter.oncoKb.readers
 
 import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoActionableInput
+import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoKnownInput
 import com.hartwig.hmftools.knowledgebaseimporter.output.FusionEvent
 import com.hartwig.hmftools.knowledgebaseimporter.output.FusionPair
 import com.hartwig.hmftools.knowledgebaseimporter.output.PromiscuousGene
@@ -9,30 +10,35 @@ import io.kotlintest.specs.StringSpec
 
 class OncoFusionReaderTest : StringSpec() {
     private val actionable = OncoActionableInput("ENST00000000", "BRAF", "", "", "", "")
+    private val known = OncoKnownInput("ENST00000000", "KIT", "", "Loss-of-function", "")
 
     init {
-        "can read actionable fusion pair" {
-            OncoFusionReader.read(actionable.copy(Alteration = "BRAF-PIK3 Fusion")) shouldBe
-                    listOf(FusionPair(actionable.gene, "PIK3"))
+        "can read fusion pair" {
+            OncoFusionReader.read(actionable.copy(Alteration = "BRAF-PIK3 Fusion")) shouldBe listOf(FusionPair(actionable.gene, "PIK3"))
+            OncoFusionReader.read(known.copy(Alteration = "KIT-PIK3 Fusion")) shouldBe listOf(FusionPair(known.gene, "PIK3"))
         }
 
         "can read promiscuous gene" {
-            OncoFusionReader.read(actionable.copy(Alteration = "BRAF Fusions")) shouldBe
-                    listOf(PromiscuousGene(actionable.gene))
+            OncoFusionReader.read(actionable.copy(Alteration = "BRAF Fusions")) shouldBe listOf(PromiscuousGene(actionable.gene))
+            OncoFusionReader.read(known.copy(Alteration = "KIT Fusions")) shouldBe listOf(PromiscuousGene(known.gene))
         }
 
         "can read fusion with ? separator" {
-            OncoFusionReader.read(actionable.copy(Alteration = "BRAF?PIK3 Fusion")) shouldBe
-                    listOf(FusionPair(actionable.gene, "PIK3"))
+            OncoFusionReader.read(actionable.copy(Alteration = "BRAF?PIK3 Fusion")) shouldBe listOf(FusionPair(actionable.gene, "PIK3"))
+            OncoFusionReader.read(known.copy(Alteration = "KIT?PIK3 Fusion")) shouldBe listOf(FusionPair(known.gene, "PIK3"))
         }
 
         "can read fusion with ' - ' separator" {
-            OncoFusionReader.read(actionable.copy(Alteration = "BRAF - PIK3 Fusion")) shouldBe
-                    listOf(FusionPair(actionable.gene, "PIK3"))
+            OncoFusionReader.read(actionable.copy(Alteration = "BRAF - PIK3 Fusion")) shouldBe listOf(FusionPair(actionable.gene, "PIK3"))
+            OncoFusionReader.read(known.copy(Alteration = "KIT - PIK3 Fusion")) shouldBe listOf(FusionPair(known.gene, "PIK3"))
         }
 
         "does not match fusions without 'Fusion' keyword" {
             OncoFusionReader.read(actionable.copy(Alteration = "BRAF-PIK3")) shouldBe emptyList<FusionEvent>()
+        }
+
+        "reads known fusion when effect is loss-of-function" {
+            OncoFusionReader.read(known.copy(Alteration = "KIT-PIK3 Fusion")) shouldBe listOf(FusionPair(known.gene, "PIK3"))
         }
     }
 }
