2367f740c6695ac5647acc86553cc43d1a7af949
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-29 16:34:43 +0200

    simplify oncoFusionReader

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/readers/OncoFusionReader.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/readers/OncoFusionReader.kt
@@ -4,8 +4,6 @@ import com.hartwig.hmftools.knowledgebaseimporter.FusionReader
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.SomaticEventReader
 import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoKbInput
 import com.hartwig.hmftools.knowledgebaseimporter.output.FusionEvent
-import com.hartwig.hmftools.knowledgebaseimporter.output.FusionPair
-import com.hartwig.hmftools.knowledgebaseimporter.output.PromiscuousGene
 
 object OncoFusionReader : SomaticEventReader<OncoKbInput, FusionEvent> {
     private val FUSION_SEPARATORS = setOf("-", " - ", "?")
@@ -14,14 +12,7 @@ object OncoFusionReader : SomaticEventReader<OncoKbInput, FusionEvent> {
     private fun matches(event: OncoKbInput) = event.variant.contains("Fusion".toRegex())
 
     override fun read(event: OncoKbInput): List<FusionEvent> {
-        if (matches(event)) {
-            val fusion = fusionReader.read(event.gene, event.variant)
-            return when (fusion) {
-                is FusionPair      -> listOf(FusionPair(fusion.fiveGene, fusion.threeGene))
-                is PromiscuousGene -> listOf(PromiscuousGene(event.gene))
-                null               -> emptyList()
-            }
-        }
+        if (matches(event)) return listOfNotNull(fusionReader.read(event.gene, event.variant))
         return emptyList()
     }
 }
