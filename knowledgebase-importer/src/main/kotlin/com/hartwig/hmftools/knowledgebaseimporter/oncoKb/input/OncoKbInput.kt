2d0111bce0283d61bdd478640ba1ed4ca68e3729
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-29 13:49:47 +0200

    update oncoKb actionable and known input classes

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/input/OncoKbInput.kt
@@ -0,0 +1,5 @@
+package com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input
+
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.events.KnowledgebaseEvent
+
+interface OncoKbInput : KnowledgebaseEvent
