77c56b69d78ed06b1eb8e4bb4bee8b15ddd02502
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-31 14:00:41 +0200

    add civic knowledgebase variant reader

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/readers/CivicKnowledgebaseVariantReader.kt
@@ -0,0 +1,21 @@
+package com.hartwig.hmftools.knowledgebaseimporter.civic.readers
+
+import com.hartwig.hmftools.knowledgebaseimporter.civic.input.CivicVariantInput
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.KnowledgebaseVariant
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.SomaticEventReader
+
+object CivicKnowledgebaseVariantReader : SomaticEventReader<CivicVariantInput, KnowledgebaseVariant> {
+    // MIVO: allow ref or alt to be empty since civic annotates insertions as ['' -> 'GATC'] and deletions as ['GATC' -> '']
+    private fun match(event: CivicVariantInput) = event.chromosome.isNotBlank() && event.start.isNotBlank() && event.stop.isNotBlank() &&
+            (event.reference_bases.isNotBlank() || event.variant_bases.isNotBlank())
+
+    override fun read(event: CivicVariantInput): List<KnowledgebaseVariant> {
+        return if (match(event)) {
+            val ref = if (event.reference_bases.isBlank()) null else event.reference_bases
+            val alt = if (event.variant_bases.isBlank()) null else event.variant_bases
+            listOf(KnowledgebaseVariant(event.gene, event.chromosome, event.start.toLong(), ref, alt))
+        } else {
+            emptyList()
+        }
+    }
+}
