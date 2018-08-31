77acf567f750852c6cf6d6b56eff32b507b953f2
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-31 17:39:32 +0200

    make rangeMutation reader more precise + update tests

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/readers/CivicRangeMutationReader.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/readers/CivicRangeMutationReader.kt
@@ -3,23 +3,23 @@ package com.hartwig.hmftools.knowledgebaseimporter.civic.readers
 import com.hartwig.hmftools.knowledgebaseimporter.civic.input.CivicVariantInput
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.GenericRangeMutations
 import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.SomaticEventReader
+import com.hartwig.hmftools.knowledgebaseimporter.transvar.matchers.CodonMatcher
 
 object CivicRangeMutationReader : SomaticEventReader<CivicVariantInput, GenericRangeMutations> {
-    private const val RANGE_VARIANTS = "gene_variant|transcript_variant|exon_variant|coding_sequence_variant|protein_altering_variant"
-    private val AND_PATTERN = "(\\+|and)".toRegex(RegexOption.IGNORE_CASE)
+    private val MUTATION = "Mutation".toRegex(RegexOption.IGNORE_CASE)
+    private val EXON_MUTATION = "Exon\\s[\\d]+(?:-[\\d]+)?\\sMutation".toRegex(RegexOption.IGNORE_CASE)
+    private val DOMAIN_MUTATION = ".+\\sDomain\\sMutation".toRegex(RegexOption.IGNORE_CASE)
+    private val G12G13 = "G12/G13".toRegex(RegexOption.IGNORE_CASE)
+    private val variantPatterns = listOf(MUTATION, EXON_MUTATION, DOMAIN_MUTATION, G12G13)
 
     private fun match(event: CivicVariantInput) = event.hasPosition && !event.hasRefOrAlt &&
-            (isGenericMutation(event) || isGenericMissense(event))
-
-    private fun isGenericMutation(event: CivicVariantInput) = event.variant.toLowerCase() == "mutation" ||
-            event.variantTypes.any { it.contains(RANGE_VARIANTS.toRegex()) }
-
-    private fun isGenericMissense(event: CivicVariantInput) = !event.variant.contains(AND_PATTERN) && event.variantTypes.size == 1
-            && event.variantTypes.first() == "missense_variant"
+            (CodonMatcher.matches(event.variant) || variantPatterns.any { event.variant.matches(it) })
 
     override fun read(event: CivicVariantInput): List<GenericRangeMutations> {
-        if (match(event)) return listOf(GenericRangeMutations(event.gene, event.transcript.substringBefore("."), event.start.toInt(),
-                                                              event.stop.toInt()))
+        if (match(event)) {
+            return listOf(GenericRangeMutations(event.gene, event.transcript.substringBefore("."), event.start.toInt(),
+                                                event.stop.toInt()))
+        }
         return emptyList()
     }
 }
--- knowledgebase-importer/src/test/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/readers/CivicRangeMutationReaderTest.kt
+++ knowledgebase-importer/src/test/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/readers/CivicRangeMutationReaderTest.kt
@@ -13,18 +13,47 @@ class CivicRangeMutationReaderTest : StringSpec() {
                                                   "25457241", "25457243", "", "", "", "missense_variant")
 
     init {
-        "can read range mutation from exon_variant record" {
+        "can read range mutation from exon variant record" {
             CivicRangeMutationReader.read(exonMutation) shouldBe
                     listOf(GenericRangeMutations("PIK3CA", "ENST00000263967", 178951882, 178952495))
         }
 
-        "can read range mutation from generic missense record" {
+        "can read range mutation from codon variant record" {
             CivicRangeMutationReader.read(codonMutation) shouldBe
                     listOf(GenericRangeMutations("DNMT3A", "ENST00000264709", 25457241, 25457243))
         }
 
-        "does not read range mutation from record without variant types" {
-            CivicRangeMutationReader.read(codonMutation.copy(variant_types = "")) shouldBe emptyList<SomaticEvent>()
+        "can read range mutation from domain mutations record" {
+            CivicRangeMutationReader.read(codonMutation.copy(variant = "B2 DOMAIN MUTATION")) shouldBe
+                    listOf(GenericRangeMutations("DNMT3A", "ENST00000264709", 25457241, 25457243))
+        }
+
+        "can read range mutation from g12/13 record" {
+            CivicRangeMutationReader.read(codonMutation.copy(variant = "G12/G13")) shouldBe
+                    listOf(GenericRangeMutations("DNMT3A", "ENST00000264709", 25457241, 25457243))
+        }
+
+        "can read range mutation from mutation record" {
+            CivicRangeMutationReader.read(codonMutation.copy(variant = "MUTATION")) shouldBe
+                    listOf(GenericRangeMutations("DNMT3A", "ENST00000264709", 25457241, 25457243))
+        }
+
+        "does not read range mutation from record without chromosome" {
+            CivicRangeMutationReader.read(codonMutation.copy(chromosome = "")) shouldBe emptyList<SomaticEvent>()
+        }
+
+        "does not read range mutation from record without start" {
+            CivicRangeMutationReader.read(codonMutation.copy(start = "")) shouldBe emptyList<SomaticEvent>()
+        }
+
+        "does not read range mutation from record without stop" {
+            CivicRangeMutationReader.read(codonMutation.copy(stop = "")) shouldBe emptyList<SomaticEvent>()
+        }
+
+        "does not read range mutation from record with position and ref/alt" {
+            CivicRangeMutationReader.read(codonMutation.copy(reference_bases = "A")) shouldBe emptyList<SomaticEvent>()
+            CivicRangeMutationReader.read(codonMutation.copy(variant_bases = "A")) shouldBe emptyList<SomaticEvent>()
+
         }
     }
 }
