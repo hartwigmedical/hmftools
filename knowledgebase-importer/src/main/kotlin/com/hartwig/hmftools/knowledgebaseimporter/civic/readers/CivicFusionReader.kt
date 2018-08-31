58211e7954c8593a11bcba1ad38048268bf680d1
Mircea Voda <mircearaul.voda@gmail.com>
2018-08-31 14:44:38 +0200

    add civic fusion reader + tests

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/readers/CivicFusionReader.kt
@@ -0,0 +1,18 @@
+package com.hartwig.hmftools.knowledgebaseimporter.civic.readers
+
+import com.hartwig.hmftools.knowledgebaseimporter.FusionReader
+import com.hartwig.hmftools.knowledgebaseimporter.civic.input.CivicVariantInput
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.SomaticEventReader
+import com.hartwig.hmftools.knowledgebaseimporter.output.FusionEvent
+
+object CivicFusionReader : SomaticEventReader<CivicVariantInput, FusionEvent> {
+    private val FUSION_SEPARATORS = setOf("-")
+    private val fusionReader = FusionReader(separators = FUSION_SEPARATORS)
+
+    private fun matches(event: CivicVariantInput) = event.variantTypes.isNotEmpty() && event.variantTypes.all { it.contains("fusion") }
+
+    override fun read(event: CivicVariantInput): List<FusionEvent> {
+        if (matches(event)) return listOfNotNull(fusionReader.read(event.gene, event.variant))
+        return emptyList()
+    }
+}
