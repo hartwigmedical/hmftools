1990802d2915979eef024e8287ba8e419c1ef3d6
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-09-03 14:56:24 +0200

    DEV-504: Add arguments for connecting database and one sample

--- actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
+++ actionability-analyzer/src/main/kotlin/com/hartwig/hmftools/actionabilityAnalyzer/ActionabilityAnalyzerApplication.kt
@@ -1,20 +1,26 @@
 package com.hartwig.hmftools.actionabilityAnalyzer
 
+import com.hartwig.hmftools.extensions.cli.createCommandLine
+import com.hartwig.hmftools.extensions.cli.options.HmfOptions
+import com.hartwig.hmftools.extensions.cli.options.strings.RequiredInputOption
+import com.hartwig.hmftools.extensions.cli.options.strings.RequiredOutputOption
 import com.hartwig.hmftools.extensions.csv.CsvReader
 import com.hartwig.hmftools.extensions.csv.CsvWriter
+import com.hartwig.hmftools.knowledgebaseimporter.DB_PASSWORD
+import com.hartwig.hmftools.knowledgebaseimporter.DB_USER
+import com.hartwig.hmftools.knowledgebaseimporter.HMFPATIENTS_DB
 import com.hartwig.hmftools.knowledgebaseimporter.readCSVRecords
 import com.hartwig.hmftools.patientdb.dao.DatabaseAccess
 import com.hartwig.hmftools.patientdb.data.PotentialActionableCNV
 import com.hartwig.hmftools.patientdb.data.PotentialActionableFusion
 import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.io.File
 import java.util.stream.Stream
 import kotlin.streams.asSequence
 import kotlin.streams.toList
 
 private val logger = LogManager.getLogger("ActionabilityAnalyzerApplication")
-private val user = ""
-private val password = ""
-private val databaseUrl = "jdbc:mysql://"
 private val actionableVariants = "actionableVariants"
 private val actionableFusionPairs = "actionableFusionPairs"
 private val actionablePromiscuousFive = "actionablePromiscuousFive"
@@ -25,19 +31,38 @@ private val cancerTypes = "knowledgebaseCancerTypes"
 private val allDbSamples = false
 private val cohortCsvLocation = "cohort.csv"
 private val cohortMutations = "cohortMutations.tsv"
+const val SAMPLE_ID = "sample_ID"
+const val OUTPUT_DIRECTORY = "/data/common/dbs/knowledgebases/actionability"
 
 fun main(args: Array<String>) {
-    val dbAccess = DatabaseAccess(user, password, databaseUrl)
-    val samplesToAnalyze = readSamples(dbAccess)
-    val actionabilityAnalyzer = ActionabilityAnalyzer(samplesToAnalyze, actionableVariants, actionableFusionPairs,
-                                                      actionablePromiscuousFive, actionablePromiscuousThree, actionableCNVs, cancerTypes,
-                                                      actionableGenomicRanges)
     logger.info("Start")
-    queryDatabase(dbAccess, samplesToAnalyze, actionabilityAnalyzer)
+    val cmd = createOptions().createCommandLine("actionability-analyzer", args)
+    val sampleId = cmd.getOptionValue(SAMPLE_ID)
+    logger.info(sampleId)
+    val user = cmd.getOptionValue(DB_USER)
+    val password = cmd.getOptionValue(DB_PASSWORD)
+    val databaseUrl = cmd.getOptionValue(HMFPATIENTS_DB)
+
+    //val dbAccess = DatabaseAccess(user, password, databaseUrl)
+    //val samplesToAnalyze = readSamples(sampleId, dbAccess)
+    //  val actionabilityAnalyzer = ActionabilityAnalyzer(samplesToAnalyze, actionableVariants, actionableFusionPairs,
+    //                                                  actionablePromiscuousFive, actionablePromiscuousThree, actionableCNVs, cancerTypes,
+    //                                                actionableGenomicRanges)
+
+    //   queryDatabase(outputDir, dbAccess, samplesToAnalyze, actionabilityAnalyzer)
+    //logger.info("sampleToAnalyse" + samplesToAnalyze)
     logger.info("Done.")
 }
 
-private fun queryDatabase(dbAccess: DatabaseAccess, samplesToAnalyze: Map<String, String>, actionabilityAnalyzer: ActionabilityAnalyzer) {
+private fun createOptions(): HmfOptions {
+    val options = HmfOptions()
+    options.add(RequiredInputOption(SAMPLE_ID, "sample_ID"))
+    options.add(RequiredInputOption(HMFPATIENTS_DB, "hmfpatients db url"))
+    options.add(RequiredInputOption(DB_USER, "db user"))
+    options.add(RequiredInputOption(DB_PASSWORD, "db password"))
+    return options
+}
+private fun queryDatabase(outputDir: String, dbAccess: DatabaseAccess, samplesToAnalyze: Map<String, String>, actionabilityAnalyzer: ActionabilityAnalyzer) {
     val records = mutableListOf<ActionabilityOutput>()
     cohortMutations(samplesToAnalyze).forEach { mutation ->
         val variantRecords = actionabilityAnalyzer.actionabilityForVariant(mutation) +
@@ -53,13 +78,13 @@ private fun queryDatabase(dbAccess: DatabaseAccess, samplesToAnalyze: Map<String
         records.addAll(fusionRecords)
     }
     logger.info("Done. Writing results to file...")
-    CsvWriter.writeTSV(records, "actionableVariantsPerSample.tsv")
+    CsvWriter.writeTSV(records, "$outputDir${File.separator}actionableVariantsPerSample.tsv")
     logger.info("Done.")
 }
 
-private fun readSamples(dbAccess: DatabaseAccess): Map<String, String> =
-        if (allDbSamples) dbAccess.allSamplesAndTumorLocations().toList().associate { Pair(it.key, it.value) }
-        else readCSVRecords(cohortCsvLocation) { Pair(it[0], it[1]) }.toMap()
+private fun readSamples(sampleId: String, dbAccess: DatabaseAccess) =
+        if (sampleId.isNotEmpty() && sampleId.isNotBlank()) dbAccess.allSamplesAndTumorLocations().toList().associate { Pair(it.key, it.value) }
+        else logger.info("no sampleId")
 
 private fun cohortMutations(samplesToAnalyze: Map<String, String>): List<CohortMutation> {
     logger.info("Looking up cohort mutations")
