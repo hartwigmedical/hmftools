05585d8ec7b2476edd8a0fda69dc3b0225a009f6
Mircea Voda <mircearaul.voda@gmail.com>
2018-09-04 16:55:53 +0200

    add cgi cdna annotation reader + tests

---
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cgi/readers/CgiActionableCDnaAnnotationReader.kt
@@ -0,0 +1,14 @@
+package com.hartwig.hmftools.knowledgebaseimporter.cgi.readers
+
+import com.hartwig.hmftools.knowledgebaseimporter.cgi.input.CgiActionableInput
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.CDnaAnnotation
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.CDnaAnnotationReader
+import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.readers.SomaticEventReader
+
+object CgiActionableCDnaAnnotationReader : SomaticEventReader<CgiActionableInput, CDnaAnnotation> {
+    override fun read(event: CgiActionableInput): List<CDnaAnnotation> {
+        if (event.`Alteration type` == "MUT")
+            return CDnaAnnotationReader.read(event.copy(variant = event.cDNA.orEmpty()))
+        return emptyList()
+    }
+}
