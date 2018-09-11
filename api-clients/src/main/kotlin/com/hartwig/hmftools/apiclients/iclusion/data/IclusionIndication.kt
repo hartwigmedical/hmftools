6655f08f5ad65e72ce70640360c526d742f609f5
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-09-11 15:59:27 +0200

    Remove parent_id from iclusion indication

--- api-clients/src/main/kotlin/com/hartwig/hmftools/apiclients/iclusion/data/IclusionIndication.kt
+++ api-clients/src/main/kotlin/com/hartwig/hmftools/apiclients/iclusion/data/IclusionIndication.kt
@@ -1,7 +1,7 @@
 package com.hartwig.hmftools.apiclients.iclusion.data
 
 data class IclusionIndication(val id: String, val doid: String?, val doid2: String?, val indication_name: String,
-                              val indication_name_full: String, val parent_id: String, val node_ids: List<String>) {
+                              val indication_name_full: String, val node_ids: List<String>) {
 
     val doidSet: Set<String> = listOfNotNull(doid, doid2).toSet()
 }
