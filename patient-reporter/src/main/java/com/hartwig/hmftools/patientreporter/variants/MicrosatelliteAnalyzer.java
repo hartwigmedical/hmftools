9dc1ba45f5de631a259a9ddacaca52a3f36c735c
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-09-14 12:36:27 +0200

    Minor refactoring

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/MicrosatelliteAnalyzer.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/MicrosatelliteAnalyzer.java
@@ -11,17 +11,25 @@ import org.jetbrains.annotations.NotNull;
 
 public final class MicrosatelliteAnalyzer {
 
+    private static final double NUMBER_OF_MB_PER_GENOME = 3095D;
+
+    private static final int MIN_SEQUENCE_LENGTH_FOR_LONG_REPEATS = 2;
+    private static final int MAX_SEQUENCE_LENGTH_FOR_LONG_REPEATS = 4;
+    private static final int MIN_REPEAT_COUNT_FOR_LONG_REPEATS = 4;
+
+    private static final int MIN_REPEAT_COUNT_FOR_SHORT_REPEATS = 5;
+
     private MicrosatelliteAnalyzer() {
     }
 
-    static double determineMicrosatelliteIndels(@NotNull final List<EnrichedSomaticVariant> variants) {
+    static double determineMicrosatelliteIndelsPerMb(@NotNull final List<EnrichedSomaticVariant> variants) {
         int indelCount = 0;
         for (EnrichedSomaticVariant variant : variants) {
             if (isPassIndel(variant) && repeatContextIsRelevant(variant.repeatCount(), variant.repeatSequence())) {
                 indelCount++;
             }
         }
-        return (double) indelCount / 3095D;
+        return (double) indelCount / NUMBER_OF_MB_PER_GENOME;
     }
 
     private static boolean isPassIndel(@NotNull final SomaticVariant variant) {
@@ -31,8 +39,10 @@ public final class MicrosatelliteAnalyzer {
     @VisibleForTesting
     static boolean repeatContextIsRelevant(int repeatCount, @NotNull String sequence) {
         final int repeatSequenceLength = sequence.length();
-        final boolean longRepeatRelevant = repeatSequenceLength >= 2 && repeatSequenceLength <= 4 && repeatCount >= 4;
-        final boolean shortRepeatRelevant = repeatSequenceLength == 1 && repeatCount >= 5;
+        final boolean longRepeatRelevant =
+                repeatSequenceLength >= MIN_SEQUENCE_LENGTH_FOR_LONG_REPEATS && repeatSequenceLength <= MAX_SEQUENCE_LENGTH_FOR_LONG_REPEATS
+                        && repeatCount >= MIN_REPEAT_COUNT_FOR_LONG_REPEATS;
+        final boolean shortRepeatRelevant = repeatSequenceLength == 1 && repeatCount >= MIN_REPEAT_COUNT_FOR_SHORT_REPEATS;
         return longRepeatRelevant | shortRepeatRelevant;
     }
 }
