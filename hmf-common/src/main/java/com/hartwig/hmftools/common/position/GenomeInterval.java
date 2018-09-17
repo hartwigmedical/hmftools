ce059370a35640389597ad59609bf085cbdfa40c
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-09-17 17:09:54 +0200

    Various cleanups

--- hmf-common/src/main/java/com/hartwig/hmftools/common/position/GenomeInterval.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/position/GenomeInterval.java
@@ -1,30 +1,22 @@
 package com.hartwig.hmftools.common.position;
 
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface GenomeInterval extends GenomePosition {
 
-    default long startPosition() {
-        return startOffset() == null ? position() : position() + startOffset();
-    }
-
-    default long endPosition() {
-        return endOffset() == null ? position() : position() + endOffset();
-    }
-
     @Nullable
     Integer startOffset();
+
+    default long startPosition() {
+        Integer startOffset = startOffset();
+        return startOffset == null ? position() : position() + startOffset;
+    }
 
     @Nullable
     Integer endOffset();
 
-    default long intervalWidth() {
-        return endPosition() - startPosition() + 1;
-    }
-
-    @NotNull
-    default String chromosomeInterval() {
-        return chromosome() + ":" + startPosition() + "-" + endPosition();
+    default long endPosition() {
+        Integer endOffset = endOffset();
+        return endOffset == null ? position() : position() + endOffset;
     }
 }
