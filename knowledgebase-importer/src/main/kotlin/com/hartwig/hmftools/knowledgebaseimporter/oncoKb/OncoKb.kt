7d8ea8abecf79eb6046a5e7450e1847f50cb363b
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-09-19 17:23:55 +0200

    Added more logging to oncokb reader

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoKb.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/OncoKb.kt
@@ -9,15 +9,23 @@ import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.RecordAnalyzer
 import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoActionableInput
 import com.hartwig.hmftools.knowledgebaseimporter.oncoKb.input.OncoKnownInput
 import com.hartwig.hmftools.knowledgebaseimporter.output.*
+import org.apache.logging.log4j.LogManager
 
 
 class OncoKb(annotatedVariantsLocation: String, actionableVariantsLocation: String, diseaseOntology: DiseaseOntology,
              private val recordAnalyzer: RecordAnalyzer, treatmentTypeMap: Map<String, String>) : Knowledgebase,
         KnowledgebaseSource<OncoKnownRecord, ActionableRecord> {
+    private val logger = LogManager.getLogger("OncoKb")
+
     override val source = "oncoKb"
-    override val knownVariants by lazy { recordAnalyzer.knownVariants(listOf(this)).distinct() }
+    override val knownVariants by lazy {
+        logger.info("Extracting oncoKb known variants.")
+        recordAnalyzer.knownVariants(listOf(this)).distinct()
+    }
+
     override val knownFusionPairs by lazy { knownKbRecords.flatMap { it.events }.filterIsInstance<FusionPair>().distinct() }
     override val promiscuousGenes by lazy { knownKbRecords.flatMap { it.events }.filterIsInstance<PromiscuousGene>().distinct() }
+
     override val actionableVariants by lazy { actionableKbItems.filterIsInstance<ActionableVariantOutput>() }
     override val actionableCNVs by lazy { actionableKbItems.filterIsInstance<ActionableCNVOutput>() }
     override val actionableFusionPairs by lazy { actionableKbItems.filterIsInstance<ActionableFusionPairOutput>() }
@@ -28,9 +36,11 @@ class OncoKb(annotatedVariantsLocation: String, actionableVariantsLocation: Stri
                 .associateBy({ it }, { diseaseOntology.findDoids(it) })
     }
     override val knownKbRecords by lazy {
+        logger.info("Reading oncoKb known records.")
         CsvReader.readTSVByName<OncoKnownInput>(annotatedVariantsLocation).mapNotNull { it.corrected() }.map { OncoKnownRecord(it) }
     }
     override val actionableKbRecords by lazy {
+        logger.info("Reading oncoKb actionable records.")
         CsvReader.readTSVByName<OncoActionableInput>(actionableVariantsLocation, nullString = "null").mapNotNull { it.corrected() }
                 .map { OncoActionableRecord(it, treatmentTypeMap) }
     }
