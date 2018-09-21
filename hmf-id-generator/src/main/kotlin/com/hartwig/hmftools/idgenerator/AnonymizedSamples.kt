3cc4b474a7395f121f71d007b7081faa7aa719e1
Mircea Voda <mircearaul.voda@gmail.com>
2018-09-21 13:34:57 +0200

    small refactor to anonymized samples

--- hmf-id-generator/src/main/kotlin/com/hartwig/hmftools/idgenerator/AnonymizedSamples.kt
+++ hmf-id-generator/src/main/kotlin/com/hartwig/hmftools/idgenerator/AnonymizedSamples.kt
@@ -4,12 +4,20 @@ import com.hartwig.hmftools.idgenerator.anonymizedIds.HmfSampleId
 import com.hartwig.hmftools.idgenerator.ids.CanonicalPatientId
 import com.hartwig.hmftools.idgenerator.ids.SampleId
 
-class AnonymizedSamples(val password: String, private val hmfSampleIds: Collection<HmfSampleId>, private val samplesInput: SamplesInput) :
-        Collection<HmfSampleId> by hmfSampleIds {
+class AnonymizedSamples private constructor(val password: String, val sampleIds: Collection<HmfSampleId>,
+                                            private val samplesInput: SamplesInput) :
+        Collection<HmfSampleId> by sampleIds {
+
+    companion object {
+        operator fun invoke(password: String, hmfSampleIds: Collection<HmfSampleId>, samplesInput: SamplesInput): AnonymizedSamples {
+            val sampleIds = hmfSampleIds.toSet().sorted()
+            return AnonymizedSamples(password, sampleIds, samplesInput)
+        }
+    }
+
     private val generator = IdGenerator(password)
-    val sampleIds = hmfSampleIds.toSet().sorted()
-    private val hmfSampleIdPerHash = hmfSampleIds.groupBy { it.hash }
-    private val hmfSampleIdPerPatientHash = hmfSampleIds.groupBy { it.hmfPatientId.hash }
+    private val hmfSampleIdPerHash = sampleIds.groupBy { it.hash }
+    private val hmfSampleIdPerPatientHash = sampleIds.groupBy { it.hmfPatientId.hash }
 
     operator fun get(sampleId: SampleId): HmfSampleId? = hmfSampleIdPerHash[generator.hash(sampleId.id)]
             ?.find { it.hmfPatientId.hash == generator.hash(samplesInput.canonicalId(sampleId.patientId).patientId.id) }
