081f68a5e7d07671f858113c376ab055dbe2cc69
Mircea Voda <mircearaul.voda@gmail.com>
2018-09-21 12:27:23 +0200

    add utility methods to hmfSampleId and hmfPatientId

--- hmf-id-generator/src/main/kotlin/com/hartwig/hmftools/idgenerator/anonymizedIds/HmfPatientId.kt
+++ hmf-id-generator/src/main/kotlin/com/hartwig/hmftools/idgenerator/anonymizedIds/HmfPatientId.kt
@@ -7,5 +7,7 @@ data class HmfPatientId(val hashId: HashId) : AnonymizedId by hashId {
         operator fun invoke(hashString: String, id: Int) = HmfPatientId(HashId(Hash(hashString), id))
     }
 
+    fun updateHash(hash: Hash) = copy(hashId = hashId.copy(hash = hash))
+
     val plaintext = "HMF" + id.toString().padStart(6, '0')
 }
--- hmf-id-generator/src/main/kotlin/com/hartwig/hmftools/idgenerator/anonymizedIds/HmfSampleId.kt
+++ hmf-id-generator/src/main/kotlin/com/hartwig/hmftools/idgenerator/anonymizedIds/HmfSampleId.kt
@@ -1,5 +1,9 @@
 package com.hartwig.hmftools.idgenerator.anonymizedIds
 
+import com.hartwig.hmftools.idgenerator.Hash
+
 data class HmfSampleId(val hashId: HashId, val hmfPatientId: HmfPatientId) : AnonymizedId by hashId {
     val plaintext = hmfPatientId.plaintext + (64 + hashId.id).toChar()
+    fun updatePatientHash(hash: Hash) = copy(hmfPatientId = hmfPatientId.updateHash(hash))
+    fun updateSampleHash(hash: Hash) = copy(hashId = hashId.copy(hash = hash))
 }
