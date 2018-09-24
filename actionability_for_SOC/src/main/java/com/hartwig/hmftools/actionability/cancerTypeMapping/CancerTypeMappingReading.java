1e90e3586367c42cec7359129ebd0a654186109c
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-09-24 15:13:21 +0200

    DEV-506: fix formatting

--- actionability_for_SOC/src/main/java/com/hartwig/hmftools/actionability/cancerTypeMapping/CancerTypeMappingReading.java
+++ actionability_for_SOC/src/main/java/com/hartwig/hmftools/actionability/cancerTypeMapping/CancerTypeMappingReading.java
@@ -3,13 +3,10 @@ package com.hartwig.hmftools.actionability.cancerTypeMapping;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.List;
+import java.security.Key;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import com.google.common.collect.Lists;
-import com.hartwig.hmftools.patientdb.data.CuratedTumorLocation;
+import com.google.common.collect.Maps;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -24,21 +21,22 @@ public class CancerTypeMappingReading {
     private static final InputStream TUMOR_LOCATION_MAPPING_RESOURCE =
             CancerTypeMappingReading.class.getResourceAsStream("/primary_tumor_locations_mapping.csv");
 
+    @NotNull
+    private final Map<String, CancerTypeMapping> tumorLocationMap = Maps.newHashMap();
 
     @NotNull
     public static CancerTypeMappingReading readingFile() throws IOException {
-
         return new CancerTypeMappingReading(TUMOR_LOCATION_MAPPING_RESOURCE);
     }
 
     public CancerTypeMappingReading(@NotNull final InputStream mappingInputStream) throws IOException {
-        final List<CancerTypeMapping> cancerTypeMapping = Lists.newArrayList();
-
         final CSVParser parser = CSVParser.parse(mappingInputStream, Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader());
         for (final CSVRecord record : parser) {
             final String primaryTumorLocation = record.get("primaryTumorLocation");
             final String doids = record.get("doids");
-            cancerTypeMapping.add(ImmutableCancerTypeMapping.of(primaryTumorLocation, doids));
+
+            tumorLocationMap.put(null,
+                    ImmutableCancerTypeMapping.of(primaryTumorLocation, doids));
         }
     }
 }
