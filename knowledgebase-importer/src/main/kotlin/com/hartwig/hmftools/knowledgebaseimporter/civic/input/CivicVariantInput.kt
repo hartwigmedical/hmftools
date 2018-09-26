50f42fb8ef649654722bfb9fdf060293e8ff76b0
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-09-26 15:56:12 +0200

    Fix typo in ensembl transcript for MLH1 in civic

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/input/CivicVariantInput.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/input/CivicVariantInput.kt
@@ -20,6 +20,7 @@ data class CivicVariantInput(override val gene: String, private val representati
     override fun correct(): CivicVariantInput? = when {
         variant.contains(Regex("MLL-MLLT3")) && gene == "KMT2A" -> copy(variant = variant.replace("MLL-MLLT3", "KMT2A-MLLT3"))
         variant == "BRAF-CUL1"                                  -> null
+        transcript == "ENST0000023170.2"                        -> copy(representative_transcript = "ENST00000231790.2")
         else                                                    -> this
     }
 }
