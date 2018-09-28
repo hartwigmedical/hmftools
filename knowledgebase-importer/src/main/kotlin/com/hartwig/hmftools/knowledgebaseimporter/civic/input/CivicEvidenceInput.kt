59c89b4b5bb89e302673a5235701d3242b76dfe8
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-09-28 13:14:02 +0200

    Add comment for mapping melanoma DOID

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/input/CivicEvidenceInput.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/civic/input/CivicEvidenceInput.kt
@@ -7,9 +7,10 @@ data class CivicEvidenceInput(val evidence_id: String, val variant_id: String, v
                               val evidence_level: String, val drugs: String, val disease: String, val doid: String,
                               val clinical_significance: String) : CsvData, CorrectedInput<CivicEvidenceInput> {
 
-    //MIVO: map melanoma to skin melanoma
     override fun correct(): CivicEvidenceInput? {
+        // MIVO: map melanoma to skin melanoma
         val disease = if (disease == "Melanoma") "Skin Melanoma" else disease
+        // KODU: Also map melanoma DOID to skin melanoma DOID
         val doid = if (doid == "1909") "8923" else doid
         return copy(disease = disease, doid = doid)
     }
