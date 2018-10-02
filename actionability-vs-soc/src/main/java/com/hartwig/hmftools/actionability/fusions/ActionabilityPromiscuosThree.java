af7ccfeda456c5714e5771c56791270666f66e9b
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-10-02 15:32:01 +0200

    Renamed actionability module

---
+++ actionability-vs-soc/src/main/java/com/hartwig/hmftools/actionability/fusions/ActionabilityPromiscuosThree.java
@@ -0,0 +1,44 @@
+package com.hartwig.hmftools.actionability.fusions;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(allParameters = true,
+             passAnnotations = { NotNull.class, Nullable.class })
+abstract class ActionabilityPromiscuosThree {
+
+    @NotNull
+    abstract String gene();
+
+    @NotNull
+    abstract String source();
+
+    @NotNull
+    abstract String reference();
+
+    @NotNull
+    abstract String drugsName();
+
+    @NotNull
+    abstract String drugsType();
+
+    @NotNull
+    abstract String cancerType();
+
+    @NotNull
+    abstract String level();
+
+    @NotNull
+    abstract String hmfLevel();
+
+    @NotNull
+    abstract String evidenceType();
+
+    @NotNull
+    abstract String significanceSource();
+
+    @NotNull
+    abstract String hmfResponse();
+}
---
+++ actionability-vs-soc/src/test/java/com/hartwig/hmftools/actionability/CNVs/ActionabilityCNVsAnalyzerTest.java
@@ -0,0 +1,79 @@
+package com.hartwig.hmftools.actionability.CNVs;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.Lists;
+import com.hartwig.hmftools.actionability.cancerTypeMapping.CancerTypeAnalyzer;
+import com.hartwig.hmftools.actionability.cancerTypeMapping.CancerTypeReading;
+import com.hartwig.hmftools.actionability.cancerTypeMapping.ImmutableCancerTypeReading;
+import com.hartwig.hmftools.common.purple.copynumber.CopyNumberMethod;
+import com.hartwig.hmftools.common.purple.gene.GeneCopyNumber;
+import com.hartwig.hmftools.common.purple.gene.ImmutableGeneCopyNumber;
+import com.hartwig.hmftools.common.purple.segment.SegmentSupport;
+
+import org.junit.Test;
+
+public class ActionabilityCNVsAnalyzerTest {
+
+
+    @Test
+    public void ActionabilityWorksCNVs() {
+        ActionabilityCNVs actionanilityCNV = ImmutableActionabilityCNVs.builder()
+                .gene("ERBB2")
+                .cnvType("Amplification")
+                .source("oncoKb")
+                .reference("ERBB2:amp")
+                .drugsName("Trastuzumab")
+                .drugsType("EGFR inhibitor")
+                .cancerType("Breast Cancer")
+                .levelSource("1")
+                .hmfLevel("A")
+                .evidenceType("Predictive")
+                .significanceSource("Responsive")
+                .hmfResponse("Responsive")
+                .build();
+
+        CancerTypeReading reading = ImmutableCancerTypeReading.builder()
+                .doidSet("1612")
+                .cancerType("Breast")
+                .build();
+
+        ActionabilityCNVsAnalyzer cnvAnalyzer = new ActionabilityCNVsAnalyzer(Lists.newArrayList(actionanilityCNV));
+        CancerTypeAnalyzer cancerType = new CancerTypeAnalyzer(Lists.newArrayList(reading));
+
+        GeneCopyNumber geneCopyNumber = ImmutableGeneCopyNumber.builder()
+                .gene("ERBB2")
+                .maxCopyNumber(45)
+                .minCopyNumber(30.001)
+                .somaticRegions(1)
+                .germlineHet2HomRegions(0)
+                .germlineHomRegions(0)
+                .minRegions(1)
+                .minRegionStart(1)
+                .minRegionEnd(10)
+                .minRegionStartSupport(SegmentSupport.NONE)
+                .minRegionEndSupport(SegmentSupport.NONE)
+                .minRegionMethod(CopyNumberMethod.UNKNOWN)
+                .nonsenseBiallelicCount(0)
+                .nonsenseNonBiallelicCount(0)
+                .nonsenseNonBiallelicPloidy(0)
+                .spliceBiallelicCount(0)
+                .spliceNonBiallelicCount(0)
+                .missenseNonBiallelicPloidy(0)
+                .minMinorAllelePloidy(0)
+                .transcriptID("trans")
+                .transcriptVersion(1)
+                .chromosomeBand("12.1")
+                .chromosome("1")
+                .start(1)
+                .end(2)
+                .spliceNonBiallelicPloidy(0)
+                .missenseBiallelicCount(0)
+                .missenseNonBiallelicCount(0)
+                .build();
+
+        assertEquals(true, cnvAnalyzer.actionableCNVs(geneCopyNumber, cancerType, "1612", "Breast"));
+        assertEquals(false, cnvAnalyzer.actionableCNVs(geneCopyNumber, cancerType, "1612", "Skin"));
+
+    }
+}
