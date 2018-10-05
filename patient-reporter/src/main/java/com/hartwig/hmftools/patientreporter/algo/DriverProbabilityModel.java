602755465dfefac131a708eb36caa677f910d6e2
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-10-05 11:09:00 +0200

    Formatting

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/algo/DriverProbabilityModel.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/algo/DriverProbabilityModel.java
@@ -29,12 +29,11 @@ public class DriverProbabilityModel {
 
     public DriverProbabilityModel(@NotNull List<DriverCatalog> driverCatalogList) {
         this.driverCatalogList = driverCatalogList;
-
     }
 
     @Nullable
     public DriverCatalog catalogForVariant(@NotNull SomaticVariant variant) {
-        // TODO (KODU): Filter on snv-like catalog entries only.
+        // TODO (KODU): Filter on snv-like catalog entries only?
         for (DriverCatalog entry : driverCatalogList) {
             if (entry.gene().equals(variant.gene())) {
                 return entry;
