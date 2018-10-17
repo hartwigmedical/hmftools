81435184110641dd68d644f6e194f2ffbdf0fdf7
Jonathan Baber <jonbaber@gmail.com>
2018-10-17 10:26:12 +1000

    HG38 support in baf segmentation

--- amber/src/main/resources/r/bafSegmentation.R
+++ amber/src/main/resources/r/bafSegmentation.R
@@ -7,4 +7,5 @@ pcfFile1  <- paste(args[2], "1", sep="")
 library(copynumber)
 baf <- read.table(bafFile, header=TRUE)
 baf <- baf[,c("Chromosome","Position","TumorModifiedBAF")]
+baf$Chromosome <- gsub("chr", "", baf$Chromosome, ignore.case = T)
 baf.seg<-pcf(baf,verbose=FALSE,gamma=100,kmin=1,save.res = TRUE, file.names = c(pcfFile1, pcfFile))
