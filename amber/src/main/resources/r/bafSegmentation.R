5b67ad3e9e705b6eeada46588759ba5604d7db61
Jonathan Baber <jonbaber@gmail.com>
2018-10-18 11:15:28 +1000

    pcf segmentation to support hg38

--- amber/src/main/resources/r/bafSegmentation.R
+++ amber/src/main/resources/r/bafSegmentation.R
@@ -2,10 +2,17 @@
 args <- commandArgs(trailing=T)
 bafFile <- args[1]
 pcfFile   <- args[2]
-pcfFile1  <- paste(args[2], "1", sep="")
 
 library(copynumber)
 baf <- read.table(bafFile, header=TRUE)
+chromosomeLevels = levels(baf$Chromosome)
+chromosomePrefix = ""
+if (any(grepl("chr", chromosomeLevels, ignore.case = T))) {
+    chromosomePrefix = substr(chromosomeLevels[1], 1, 3)
+}
+
 baf <- baf[,c("Chromosome","Position","TumorModifiedBAF")]
-baf$Chromosome <- gsub("chr", "", baf$Chromosome, ignore.case = T)
-baf.seg<-pcf(baf,verbose=FALSE,gamma=100,kmin=1,save.res = TRUE, file.names = c(pcfFile1, pcfFile))
+baf$Chromosome <- gsub(chromosomePrefix, "", baf$Chromosome, ignore.case = T)
+baf.seg<-pcf(baf,verbose=FALSE,gamma=100,kmin=1)
+baf.seg$chrom = paste0(chromosomePrefix, baf.seg$chrom)
+write.table(baf.seg, file = pcfFile, row.names = F, sep = "\t", quote = F)
