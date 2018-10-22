8d270286166ab75b2ae359d23dea91c4efe8b518
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-10-22 11:12:28 +0200

    Refactor

--- hmf-common/src/test/java/com/hartwig/hmftools/common/center/CenterTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/center/CenterTest.java
@@ -3,7 +3,6 @@ package com.hartwig.hmftools.common.center;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.io.File;
 import java.io.IOException;
 
 import com.google.common.io.Resources;
@@ -12,18 +11,17 @@ import org.junit.Test;
 
 public class CenterTest {
 
-    private static final String BASE_RESOURCE_DIR = Resources.getResource("center").getPath();
-    private static final String TEST_FILE = BASE_RESOURCE_DIR + File.separator + "centers.csv";
+    private static final String CENTER_RESOURCE = Resources.getResource("center/centers.csv").getPath();
 
     @Test
     public void canReadCPCTRecipients() throws IOException {
-        final CenterModel centerModel = Center.readFromCSV(TEST_FILE);
+        final CenterModel centerModel = Center.readFromCSV(CENTER_RESOURCE);
         assertEquals("my@email.com; my2@email.com", centerModel.getCpctRecipients("01"));
     }
 
     @Test
     public void canReadPIs() throws IOException {
-        final CenterModel centerModel = Center.readFromCSV(TEST_FILE);
+        final CenterModel centerModel = Center.readFromCSV(CENTER_RESOURCE);
         final CenterData center = centerModel.centerPerId("01");
         assertNotNull(center);
         assertEquals("Someone", CenterModel.getPI("CPCT02010001", center));
@@ -38,7 +36,7 @@ public class CenterTest {
 
     @Test
     public void canReadDRUPRecipients() throws IOException {
-        final CenterModel centerModel = Center.readFromCSV(TEST_FILE);
+        final CenterModel centerModel = Center.readFromCSV(CENTER_RESOURCE);
         assertEquals("my3@email.com", centerModel.getDrupRecipients("01"));
         //MIVO: drup recipient field with '*'
         assertEquals("my@email.com; my2@email.com", centerModel.getDrupRecipients("02"));
@@ -46,7 +44,7 @@ public class CenterTest {
 
     @Test
     public void canReadAddress() throws IOException {
-        final CenterModel centerModel = Center.readFromCSV(TEST_FILE);
+        final CenterModel centerModel = Center.readFromCSV(CENTER_RESOURCE);
         final CenterData center = centerModel.centerPerId("01");
         assertNotNull(center);
         assertEquals("Address-AVL", center.addressName());
