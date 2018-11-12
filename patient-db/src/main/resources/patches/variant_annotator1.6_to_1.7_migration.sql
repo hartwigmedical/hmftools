30cac16540ba882ed3c43ffa619b6c04fbf4bd26
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-11-12 15:01:00 +0100

    Fix bug for databases that contain data

--- patient-db/src/main/resources/patches/variant_annotator1.6_to_1.7_migration.sql
+++ patient-db/src/main/resources/patches/variant_annotator1.6_to_1.7_migration.sql
@@ -2,9 +2,9 @@ ALTER TABLE structuralVariantBreakend
     ADD sampleId varchar(255) NOT NULL AFTER modified;
 
 ALTER TABLE structuralVariantFusion
-    ADD modified DATETIME NOT NULL AFTER id,
+    ADD modified DATETIME AFTER id,
     ADD sampleId varchar(255) NOT NULL AFTER modified;
 
 ALTER TABLE structuralVariantDisruption
-    ADD modified DATETIME NOT NULL AFTER id,
+    ADD modified DATETIME AFTER id,
     ADD sampleId varchar(255) NOT NULL AFTER modified;
