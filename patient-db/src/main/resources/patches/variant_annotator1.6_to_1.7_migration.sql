9d3f94360518dbcbefe52e6f8638b504975bf928
Charles Shale <shalecharles@gmail.com>
2018-11-14 19:14:28 +1100

    Fixed patch for SV annotations tables

--- patient-db/src/main/resources/patches/variant_annotator1.6_to_1.7_migration.sql
+++ patient-db/src/main/resources/patches/variant_annotator1.6_to_1.7_migration.sql
@@ -1,15 +1,6 @@
 ALTER TABLE structuralVariantBreakend
     ADD sampleId varchar(255) NOT NULL AFTER modified;
 
-<<<<<<< HEAD
-ALTER TABLE structuralVariantDisruption
-    ADD modified DATETIME NOT NULL DEFAULT '2018-01-01' AFTER id,
-    ADD sampleId varchar(255) NOT NULL AFTER modified;
-
-ALTER TABLE structuralVariantFusion
-    ADD modified DATETIME NOT NULL DEFAULT '2018-01-01' AFTER id,
-    ADD sampleId varchar(255) NOT NULL AFTER modified;
-=======
 ALTER TABLE structuralVariantFusion
     ADD modified DATETIME AFTER id,
     ADD sampleId varchar(255) NOT NULL AFTER modified;
@@ -17,4 +8,3 @@ ALTER TABLE structuralVariantFusion
 ALTER TABLE structuralVariantDisruption
     ADD modified DATETIME AFTER id,
     ADD sampleId varchar(255) NOT NULL AFTER modified;
->>>>>>> 96fb392a1136c412b96107186d1735be6e5ecb55
