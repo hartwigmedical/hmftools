50ead1ae21823578cffe2e1a91ef069c19658801
Jonathan Baber <jonbaber@gmail.com>
2018-11-19 16:47:52 +1000

    Add germline indel filter

--- hmf-common/src/main/java/com/hartwig/hmftools/common/hotspot/HotspotEvidenceVCF.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/hotspot/HotspotEvidenceVCF.java
@@ -36,6 +36,9 @@ public class HotspotEvidenceVCF {
 
     private final static String PASS = "PASS";
     private final static String HOTSPOT_FLAG = "HOTSPOT";
+    private final static String GERMLINE_INDEL = "GERMLINE_INDEL";
+    private final static String GERMLINE_INDEL_DESCRIPTION = "Set if inframe indel has any indels at that site.";
+
     private final static String LOW_CONFIDENCE = "LOW_CONFIDENCE";
     private final static String LOW_CONFIDENCE_DESCRIPTION =
             "Set if not true: AD[NormalAlt] = 0 && AD[TumorAlt] >= " + MIN_TUMOR_EVIDENCE + " && QUAL[Known|Inframe] >= "
@@ -56,6 +59,7 @@ public class HotspotEvidenceVCF {
         header.addMetaDataLine(new VCFInfoHeaderLine(HOTSPOT_FLAG, 1, VCFHeaderLineType.String, "Hotspot Type: known, inframe"));
         header.addMetaDataLine(new VCFFilterHeaderLine(PASS, "All filters passed"));
         header.addMetaDataLine(new VCFFilterHeaderLine(LOW_CONFIDENCE, LOW_CONFIDENCE_DESCRIPTION));
+        header.addMetaDataLine(new VCFFilterHeaderLine(GERMLINE_INDEL, GERMLINE_INDEL_DESCRIPTION));
     }
 
     public void write(@NotNull final String filename, @NotNull final List<HotspotEvidence> evidence) {
@@ -75,11 +79,14 @@ public class HotspotEvidenceVCF {
     }
 
     private static boolean lowConfidence(@NotNull HotspotEvidence hotspotEvidence) {
-        return hotspotEvidence.isIndel() && hotspotEvidence.normalIndelCount() > 0
-                || hotspotEvidence.type() == HotspotEvidenceType.INFRAME && hotspotEvidence.qualityScore() < MIN_INFRAME_QUALITY
+        return hotspotEvidence.type() == HotspotEvidenceType.INFRAME && hotspotEvidence.qualityScore() < MIN_INFRAME_QUALITY
                 || hotspotEvidence.type() == HotspotEvidenceType.KNOWN && hotspotEvidence.qualityScore() < MIN_KNOWN_QUALITY
                 || hotspotEvidence.normalAltCount() > 0
                 || hotspotEvidence.tumorAltCount() < MIN_TUMOR_EVIDENCE;
+    }
+
+    private static boolean germlineIndel(@NotNull HotspotEvidence hotspotEvidence) {
+        return hotspotEvidence.isIndel() && hotspotEvidence.normalIndelCount() > 0;
     }
 
     @VisibleForTesting
@@ -116,6 +123,8 @@ public class HotspotEvidenceVCF {
 
         if (lowConfidence(hotspotEvidence)) {
             builder.filter(LOW_CONFIDENCE);
+        } else if (germlineIndel(hotspotEvidence)) {
+            builder.filter(GERMLINE_INDEL);
         } else {
             builder.filter(PASS);
         }
