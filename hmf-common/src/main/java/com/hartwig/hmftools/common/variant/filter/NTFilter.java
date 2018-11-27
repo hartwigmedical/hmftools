3df840603c952b3d1860ca447f7d137104ffa8bc
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-11-27 15:21:01 +0100

    Make NT filter mandatory in all factories

--- hmf-common/src/main/java/com/hartwig/hmftools/common/variant/filter/NTFilter.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/variant/filter/NTFilter.java
@@ -7,6 +7,7 @@ public class NTFilter implements VariantContextFilter {
 
     @Override
     public boolean test(final VariantContext record) {
+        // KODU: If NT attribute is present we want to only include ref calls (no het/hom/conflict).
         return !record.getCommonInfo().hasAttribute("NT") || record.getCommonInfo().getAttribute("NT").equals("ref");
     }
 }
