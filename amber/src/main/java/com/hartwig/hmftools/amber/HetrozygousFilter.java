e6afae7abb77bb7d2de58f8db74cfdf004fa85fb
Jonathan Baber <jonbaber@gmail.com>
2018-11-28 10:58:22 +1000

    should be using read depth

--- amber/src/main/java/com/hartwig/hmftools/amber/HetrozygousFilter.java
+++ amber/src/main/java/com/hartwig/hmftools/amber/HetrozygousFilter.java
@@ -18,22 +18,22 @@ public class HetrozygousFilter implements Predicate<NormalBAF> {
     public boolean test(final NormalBAF bafEvidence) {
         if (bafEvidence.isValid() && isHeterozygousRef(bafEvidence.refCount(), bafEvidence.readDepth())) {
             final NormalBAF.Base alt = bafEvidence.alt();
-            return isHeterozygousAlt(bafEvidence.baseMap().get(alt), bafEvidence.refCount());
+            return isHeterozygousAlt(bafEvidence.baseMap().get(alt), bafEvidence.readDepth());
         }
 
         return false;
     }
 
-    private boolean isHeterozygousRef(int refCount, int totalCount) {
-        final int minCount = (int) Math.round((1 - maxHetAFPercentage) * totalCount);
-        final int maxCount = (int) Math.round((1 - minHetAFPercentage) * totalCount);
-        return between(refCount, minCount, maxCount);
+    private boolean isHeterozygousRef(int refSupport, int readDepth) {
+        final int minCount = (int) Math.round((1 - maxHetAFPercentage) * readDepth);
+        final int maxCount = (int) Math.round((1 - minHetAFPercentage) * readDepth);
+        return between(refSupport, minCount, maxCount);
     }
 
-    private boolean isHeterozygousAlt(int altCount, int totalCount) {
-        final int minCount = (int) Math.round(minHetAFPercentage * totalCount);
-        final int maxCount = (int) Math.round(maxHetAFPercentage * totalCount);
-        return between(altCount, minCount, maxCount);
+    private boolean isHeterozygousAlt(int altSupport, int readDepth) {
+        final int minCount = (int) Math.round(minHetAFPercentage * readDepth);
+        final int maxCount = (int) Math.round(maxHetAFPercentage * readDepth);
+        return between(altSupport, minCount, maxCount);
     }
 
     private static boolean between(int totalCount, int min, int max) {
