733451ed67a04f847ac81e8f222e184acab1220a
Charles Shale <shalecharles@gmail.com>
2018-11-30 19:43:29 +1100

    Fixed test build

--- sv-analyser/src/test/java/com/hartwig/hmftools/svanalysis/analyser/SvTestHelper.java
+++ sv-analyser/src/test/java/com/hartwig/hmftools/svanalysis/analyser/SvTestHelper.java
@@ -171,6 +171,8 @@ public class SvTestHelper
                         .inexactHomologyOffsetEnd(0)
                         .startLinkedBy("")
                         .endLinkedBy("")
+                        .startRefContext("")
+                        .endRefContext("")
                         .build();
 
         SvVarData var = new SvVarData(svData);
