cc1318d390fd8deca13d296daf2247d4eecefa7d
Jonathan Baber <jonbaber@gmail.com>
2018-11-30 10:22:00 +1000

    read sam file using query intervals

--- hmf-common/src/main/java/com/hartwig/hmftools/common/hotspot/SAMSupplier.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/hotspot/SAMSupplier.java
@@ -1,10 +1,12 @@
 package com.hartwig.hmftools.common.hotspot;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.hartwig.hmftools.common.chromosome.Chromosome;
 import com.hartwig.hmftools.common.chromosome.HumanChromosome;
@@ -14,6 +16,8 @@ import com.hartwig.hmftools.common.region.GenomeRegion;
 
 import org.jetbrains.annotations.NotNull;
 
+import htsjdk.samtools.QueryInterval;
+import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SAMRecordIterator;
 import htsjdk.samtools.SamReader;
@@ -23,12 +27,58 @@ public class SAMSupplier {
     private static final int MIN_MAPPING_QUALITY = 1;
 
     private final ListMultimap<Chromosome, GenomeRegion> codingRegions;
+    private final Collection<GenomeRegion> regions;
 
     public SAMSupplier(@NotNull final Collection<GenomeRegion> regions) {
         this.codingRegions = Multimaps.fromRegions(regions);
+        this.regions = regions;
     }
 
+
     public void readOnce(@NotNull final SamReader samReader, @NotNull final Consumer<SAMRecord> consumer) {
+
+        final Set<String> processed = Sets.newHashSet();
+        final QueryInterval[] queryIntervals = createIntervals(regions, samReader.getFileHeader());
+
+        try (final SAMRecordIterator iterator = samReader.queryOverlapping(queryIntervals)) {
+            while (iterator.hasNext()) {
+                final SAMRecord record = iterator.next();
+                if (samRecordMeetsQualityRequirements(record)) {
+                    if (processed.add(record.toString())) {
+                        consumer.accept(record);
+                    }
+                }
+            }
+        }
+    }
+
+    @NotNull
+    private static QueryInterval[] createIntervals(@NotNull final Collection<GenomeRegion> regions, @NotNull final SAMFileHeader header) {
+        final List<QueryInterval> queryIntervals = Lists.newArrayList();
+        for (final GenomeRegion region : regions) {
+            int sequenceIndex = header.getSequenceIndex(region.chromosome());
+            if (sequenceIndex > -1) {
+                queryIntervals.add(new QueryInterval(sequenceIndex, (int) region.start(), (int) region.end()));
+            }
+        }
+        return QueryInterval.optimizeIntervals(queryIntervals.toArray(new QueryInterval[queryIntervals.size()]));
+    }
+
+    public boolean isInCodingRegions(@NotNull final GenomePosition hotspot) {
+        return codingRegions.get(HumanChromosome.fromString(hotspot.chromosome())).stream().anyMatch(x -> x.contains(hotspot));
+    }
+
+    private boolean old(@NotNull final SAMRecord record) {
+        return record.getMappingQuality() >= MIN_MAPPING_QUALITY && !record.getDuplicateReadFlag();
+    }
+
+    private boolean samRecordMeetsQualityRequirements(@NotNull SAMRecord record) {
+        return record.getMappingQuality() >= MIN_MAPPING_QUALITY && !record.getReadUnmappedFlag() && !record.getDuplicateReadFlag()
+                && !record.isSecondaryOrSupplementary();
+    }
+
+    public void readOnceWithoutQueryIntervals(@NotNull final SamReader samReader, @NotNull final Consumer<SAMRecord> consumer) {
+
         final Set<String> processed = Sets.newHashSet();
         for (GenomeRegion codingRegion : codingRegions.values()) {
             try (final SAMRecordIterator iterator = samReader.queryOverlapping(codingRegion.chromosome(),
@@ -44,14 +94,6 @@ public class SAMSupplier {
                 }
             }
         }
-    }
-
-    public boolean isInCodingRegions(@NotNull final GenomePosition hotspot) {
-        return codingRegions.get(HumanChromosome.fromString(hotspot.chromosome())).stream().anyMatch(x -> x.contains(hotspot));
-    }
-
-    private boolean samRecordMeetsQualityRequirements(@NotNull final SAMRecord record) {
-        return record.getMappingQuality() >= MIN_MAPPING_QUALITY && !record.getDuplicateReadFlag();
     }
 
 }
