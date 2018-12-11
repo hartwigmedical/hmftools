af8e0fe3d88cbb4a55ee7d66167707222531ab52
Charles Shale <shalecharles@gmail.com>
2018-12-12 08:37:56 +1100

    SV Annotator: cap homology sequence at 255 char DB field length

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/dao/StructuralVariantDAO.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/dao/StructuralVariantDAO.java
@@ -274,6 +274,8 @@ class StructuralVariantDAO {
         }
     }
 
+    private static int MAX_HOM_SEQUENCE = 255;
+
     private static void addRecord(@NotNull Timestamp timestamp, @NotNull InsertValuesStepN inserter, @NotNull String sample,
             @NotNull EnrichedStructuralVariant variant) {
         //noinspection unchecked
@@ -284,8 +286,8 @@ class StructuralVariantDAO {
                 variant.end() == null ? null : variant.end().position(),
                 variant.start().orientation(),
                 variant.end() == null ? null : variant.end().orientation(),
-                variant.start().homology(),
-                variant.end() == null ? null : variant.end().homology(),
+                variant.start().homology().length() > MAX_HOM_SEQUENCE ? variant.start().homology().substring(0, MAX_HOM_SEQUENCE) : variant.start().homology(),
+                variant.end() == null ? null : (variant.end().homology().length() > MAX_HOM_SEQUENCE ? variant.end().homology().substring(0, MAX_HOM_SEQUENCE) : variant.end().homology()),
                 variant.insertSequence(),
                 variant.type(),
                 DatabaseUtil.decimal(variant.start().alleleFrequency()),
