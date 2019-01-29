4adf52c9481e1bb4a2cea9718f0c0e998e47098d
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-01-29 09:55:52 +0100

    DEV-645: update test for gene fusions

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/structural/ReportableGeneFusionFactoryTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/structural/ReportableGeneFusionFactoryTest.java
@@ -5,17 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import com.google.common.collect.Lists;
-import com.hartwig.hmftools.common.variant.structural.EnrichedStructuralVariant;
-import com.hartwig.hmftools.common.variant.structural.ImmutableEnrichedStructuralVariant;
-import com.hartwig.hmftools.common.variant.structural.ImmutableEnrichedStructuralVariantLeg;
-import com.hartwig.hmftools.common.variant.structural.StructuralVariantType;
-import com.hartwig.hmftools.common.variant.structural.annotation.GeneAnnotation;
-import com.hartwig.hmftools.common.variant.structural.annotation.GeneFusion;
-import com.hartwig.hmftools.common.variant.structural.annotation.Transcript;
 import com.hartwig.hmftools.patientreporter.loadStructuralVariants.Fusion;
 import com.hartwig.hmftools.patientreporter.loadStructuralVariants.ImmutableFusion;
 
-import org.apache.logging.log4j.util.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
@@ -91,57 +83,14 @@ public class ReportableGeneFusionFactoryTest {
 
     @Test
     public void canConvertGeneFusions() {
-      //  GeneFusion fusion1 =
-       //         createFusion("TMPRSS2", "ENST00000398585", 4, 5, "PNPLA7", "ENST00000406427", 2, 3, KnownFusionsModel.CIVIC, 0.4);
-       // GeneFusion fusion2 = createFusion("CLCN6", "ENST00000346436", 1, 2, "BRAF", "ENST00000288602", 8, 9, KnownFusionsModel.ONCOKB, 1D);
-
         List<ReportableGeneFusion> reportableFusions =
                 ReportableGeneFusionFactory.fusionConvertToReportable(Lists.newArrayList(fusionTestData()));
 
         assertEquals(1, reportableFusions.size());
 
-        ReportableGeneFusion reportableFusion1 = reportableFusions.get(0);
-        assertEquals("PRKAR2B", reportableFusion1.geneStart());
-        assertEquals("MET", reportableFusion1.geneEnd());
-        assertEquals(1.65, reportableFusion1.ploidy(), EPSILON);
-    }
-
-    @NotNull
-    private static GeneFusion createFusion(@NotNull String startGene, @NotNull String startTranscript, int startExonUpstream,
-            int startExonDownstream, @NotNull String endGene, @NotNull String endTranscript, int endExonUpstream, int endExonDownstream,
-            @NotNull String source, double ploidy) {
-        Transcript upstreamTranscript = createFusionLeg(true, startGene, startTranscript, startExonUpstream, startExonDownstream, ploidy);
-        Transcript downstreamTranscript = createFusionLeg(false, endGene, endTranscript, endExonUpstream, endExonDownstream, ploidy);
-
-        return new GeneFusion(upstreamTranscript, downstreamTranscript, source, true);
-    }
-
-    @NotNull
-    private static Transcript createFusionLeg(boolean isUpstream, @NotNull String gene, @NotNull String transcript, int exonUpstream,
-            int exonDownstream, double ploidy) {
-        EnrichedStructuralVariant variant = createEnrichedStructuralVariantBuilder().type(StructuralVariantType.BND)
-                .primaryKey(1)
-                .start(createEnrichedStructuralVariantLegBuilder().orientation((byte) 1).chromosome("any").build())
-                .end(createEnrichedStructuralVariantLegBuilder().orientation((byte) 1).chromosome("any").build())
-                .ploidy(ploidy)
-                .build();
-        GeneAnnotation upstreamGene =
-                new GeneAnnotation(variant, isUpstream, gene, Strings.EMPTY, 1, Lists.newArrayList(), Lists.newArrayList(), Strings.EMPTY);
-        return new Transcript(upstreamGene, 0, transcript, exonUpstream, -1, exonDownstream, -1, 0, 0, 10, true, 0, 0, null, null);
-    }
-
-    @NotNull
-    private static ImmutableEnrichedStructuralVariantLeg.Builder createEnrichedStructuralVariantLegBuilder() {
-        return ImmutableEnrichedStructuralVariantLeg.builder().homology(Strings.EMPTY).position(1);
-    }
-
-    @NotNull
-    private static ImmutableEnrichedStructuralVariant.Builder createEnrichedStructuralVariantBuilder() {
-        return ImmutableEnrichedStructuralVariant.builder()
-                .id(Strings.EMPTY)
-                .insertSequence(Strings.EMPTY)
-                .insertSequenceAlignments(Strings.EMPTY)
-                .qualityScore(0)
-                .recovered(false);
+        ReportableGeneFusion reportableFusion = reportableFusions.get(0);
+        assertEquals("PRKAR2B", reportableFusion.geneStart());
+        assertEquals("MET", reportableFusion.geneEnd());
+        assertEquals(1.65, reportableFusion.ploidy(), EPSILON);
     }
 }
