d50ac3f3e129b1e957c6914d0492e184be3eaa51
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-02-11 17:30:53 +0100

    DEV-652: fix some tests

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/report/PDFWriterTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/report/PDFWriterTest.java
@@ -106,7 +106,7 @@ public class PDFWriterTest {
                 LocalDate.parse("05-Jan-2018", DATE_FORMATTER),
                 LocalDate.parse("01-Jan-2018", DATE_FORMATTER),
                 "PREP013V23-QC037V20-SEQ008V25",
-                "HMF Testing Center", "COLO", "", "", "", "");
+                "HMF Testing Center", "COLO", "", "", "", "", true);
 
         NotAnalysedPatientReport patientReport = ImmutableNotAnalysedPatientReport.of(sampleReport,
                 reason,
