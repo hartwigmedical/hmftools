15e2236737d63d03917343cc3223cd8f318bab25
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-03-14 11:52:17 +0100

    DEV-642: read first object from json

--- hmf-common/src/main/java/com/hartwig/hmftools/common/vicc/ViccFactory.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/vicc/ViccFactory.java
@@ -5,9 +5,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,30 +25,24 @@ public abstract class ViccFactory {
     public static void extractBRCAFile(@NotNull String brcaJsonPath) throws IOException {
         final String csvFileName = "/data/experiments/knowledgebase_vicckb/brca.csv";
         PrintWriter writer = new PrintWriter(new File(csvFileName));
-        writer.append("Variant_frequency_LOVD;ClinVarAccession_ENIGMA");
-        final Object object = new JsonParser().parse(new FileReader(brcaJsonPath));
-        JsonArray jsonObject = (JsonArray) object;
+        writer.append("Variant_frequency_LOVD;ClinVarAccession_ENIGMA\n");
 
-        for (Object dataJson : jsonObject) {
-            JsonObject brca = (JsonObject) dataJson;
+        JsonParser parser = new JsonParser();
+        JsonReader reader = new JsonReader(new FileReader(brcaJsonPath));
+        reader.setLenient(true);
 
-            LOGGER.info(brca.getAsJsonObject("brca").get("Variant_frequency_LOVD"));
-            LOGGER.info(brca.getAsJsonObject("brca").get("ClinVarAccession_ENIGMA"));
-            LOGGER.info(brca.getAsJsonObject("gene_identifiers").get("symbol"));
-            LOGGER.info(brca.getAsJsonObject("gene_identifiers").get("entrez_id"));
-            LOGGER.info(brca.getAsJsonObject("gene_identifiers").get("ensembl_gene_id"));
-            BRCA brca1 =  ImmutableBRCA.builder()
-                    .variantFrequencyLOVD(brca.getAsJsonObject("brca").get("Variant_frequency_LOVD").toString())
-                    .clinVarAccessionENIGMA(brca.getAsJsonObject("brca").get("ClinVarAccession_ENIGMA").toString()).build();
-            LOGGER.info(brca1.toString());
-            writeToCsvFile(brca1.toString(), writer);
-        }
+        JsonObject object = parser.parse(reader).getAsJsonObject();
+
+        BRCA brca1 =  ImmutableBRCA.builder()
+                .variantFrequencyLOVD(object.getAsJsonObject("brca").get("Variant_frequency_LOVD").toString())
+               .clinVarAccessionENIGMA(object.getAsJsonObject("brca").get("ClinVarAccession_ENIGMA").toString()).build();
+        writer.append(brca1.toString());
+        writer.append("\n");
+        writer.close();
     }
 
     private static void writeToCsvFile(@NotNull String csvFormatString, @NotNull PrintWriter writer) throws IOException {
         writer.append(csvFormatString);
-
-
 
     }
 
