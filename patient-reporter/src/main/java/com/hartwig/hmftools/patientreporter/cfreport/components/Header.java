e3e97f58f2a24ef9130f5b8a25e0dbd7fa7d636f
Wilco Tomassen <wilco@cleverfranke.com>
2019-03-21 12:17:28 +0100

    Added extra text to header

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/components/Header.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/components/Header.java
@@ -6,35 +6,48 @@ import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.kernel.pdf.xobject.PdfImageXObject;
 import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
 
 public final class Header {
 
-    private static final String HMF_LOGO_PATH = "pdf/hartwig_logo.jpg"; // "src/main/resources/pdf/hartwig_logo.jpg";
+    private static final String HMF_LOGO_PATH = "pdf/hartwig_logo.jpg";
     private static PdfImageXObject hmfLogoObj = null;
 
     public static void addHeader(String chapterTitle, PdfPage page) {
 
         final PdfCanvas pdfCanvas = new PdfCanvas(page.getLastContentStream(), page.getResources(), page.getDocument());
+        Canvas cv = new Canvas(pdfCanvas, page.getDocument(), page.getPageSize());
 
         // Attempt to load image object
         if (hmfLogoObj == null) {
             hmfLogoObj = new PdfImageXObject(ReportResources.loadImageData(HMF_LOGO_PATH));
         }
 
-        // Add image object to page
+        // Add HMF logo image object to page
         if (hmfLogoObj != null) {
             pdfCanvas.addXObject(hmfLogoObj, 52, 772, 44, false);
         }
 
-        // @TODO: add "Hartwig Medical OncoAct"
+        // Add "Hartwig Medical OncoAct"
+        cv.add(new Paragraph()
+                .add(new Text("Hartwig Medical")
+                        .setFont(ReportResources.getFontBold())
+                        .setFontSize(11)
+                        .setFontColor(ReportResources.PALETTE_BLUE))
+                .add(new Text(" Onco")
+                        .setFont(ReportResources.getFontRegular())
+                        .setFontSize(11)
+                        .setFontColor(ReportResources.PALETTE_BLUE))
+                .add(new Text("Act")
+                        .setFont(ReportResources.getFontRegular())
+                        .setFontSize(11)
+                        .setFontColor(ReportResources.PALETTE_RED))
+                .setFixedPosition(230, 791, 300));
 
         // Add chapter title
-        Paragraph chapterTitleParagraph = new Paragraph(chapterTitle)
+        cv.add(new Paragraph(chapterTitle)
                 .addStyle(ReportResources.chapterTitleStyle())
-                .setFixedPosition(ReportResources.PAGE_MARGIN_LEFT, 721, 500);
-
-        Canvas cv = new Canvas(pdfCanvas, page.getDocument(), page.getPageSize());
-        cv.add(chapterTitleParagraph);
+                .setFixedPosition(ReportResources.PAGE_MARGIN_LEFT, 721, 500));
 
         pdfCanvas.release();
 
