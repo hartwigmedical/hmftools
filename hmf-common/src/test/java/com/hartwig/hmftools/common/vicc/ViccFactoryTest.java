efbfbd2da6a02b6b3d2cf79ae18eed72e544c16e
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-03-21 14:46:59 +0100

    Ignore test

--- hmf-common/src/test/java/com/hartwig/hmftools/common/vicc/ViccFactoryTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/vicc/ViccFactoryTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.google.common.io.Resources;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ViccFactoryTest {
@@ -11,6 +12,7 @@ public class ViccFactoryTest {
     private static final String BRCA_FILE_PATH = Resources.getResource("vicc_knowledgebase").getPath();
 
     @Test
+    @Ignore
     public void convertBRCA() throws IOException {
         ViccFactory.extractBRCAFile(BRCA_FILE_PATH + "/brca.json");
     }
