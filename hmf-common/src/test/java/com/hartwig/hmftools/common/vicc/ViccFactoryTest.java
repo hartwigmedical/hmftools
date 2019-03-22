09f8117b0d32df255b248d84a144fda8a73c9195
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-03-22 17:21:06 +0100

    DEV-642: test ignore

--- hmf-common/src/test/java/com/hartwig/hmftools/common/vicc/ViccFactoryTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/vicc/ViccFactoryTest.java
@@ -1,20 +1,19 @@
 package com.hartwig.hmftools.common.vicc;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
+import com.google.common.io.Resources;
+
 import org.junit.Test;
+import org.junit.Ignore;
 
 public class ViccFactoryTest {
-    
-//    @Test
-//    public void convertBRCA() throws IOException {
-//        ViccFactory.extractBRCAFile("hmf/tmp/brca.json");
-//    }
-//
-//    @Test
-//    public void convertAll() throws IOException {
-//        ViccFactory.extractBRCAFile("hmf/tmp/all.json");
-//    }
+
+    private static final String BRCA_FILE_PATH = Resources.getResource("vicc_knowledgebase").getPath();
+
+    @Test
+    @Ignore
+    public void convertBRCA() throws IOException {
+        ViccFactory.extractBRCAFile(BRCA_FILE_PATH + "/brca.json");
+    }
 }
