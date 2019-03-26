d8f0c09a0addae272e7d3b79371c91c0a255d5bb
Charles Shale <shalecharles@gmail.com>
2019-03-26 21:45:07 +1100

    Bachelor: removing config tests

--- bachelor/src/test/java/com/hartwig/hmftools/bachelor/BachelorTest.java
+++ bachelor/src/test/java/com/hartwig/hmftools/bachelor/BachelorTest.java
@@ -16,28 +16,11 @@ import org.xml.sax.SAXException;
 
 public class BachelorTest {
 
-    private final static String TEST_XML = Resources.getResource("valid.xml").getPath();
-    private final static String TEST_INVALID_XML = Resources.getResource("missing_namespace.xml").getPath();
-    private final static String TEST_DBSNP_XML = Resources.getResource("invalid_dbsnp.xml").getPath();
-
-    @Nullable
-    private static Program testFile(final String path) throws SAXException {
-        final BachelorSchema schema = BachelorSchema.make();
-        return schema.processXML(Paths.get(path));
-    }
+    // private final static String TEST_XML = Resources.getResource("test_config.xml").getPath();
 
     @Test
-    public void TestValid() throws SAXException {
-        assertNotNull(testFile(TEST_XML));
-    }
+    public void testStandardMatch()
+    {
 
-    @Test
-    public void TestMissingNamespace() throws SAXException {
-        assertNull(testFile(TEST_INVALID_XML));
-    }
-
-    @Test
-    public void TestInvalidDBSNP() throws SAXException {
-        assertNull(testFile(TEST_DBSNP_XML));
     }
 }
