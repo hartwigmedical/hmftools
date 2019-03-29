25b7b96ce95a56d5eb8922b92d0fc94ad2dfe3a7
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-03-29 16:37:43 +0100

    DEV-642: read civic

--- hmf-common/src/main/java/com/hartwig/hmftools/common/vicc/ViccFactory.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/vicc/ViccFactory.java
@@ -58,9 +58,204 @@ public abstract class ViccFactory {
         return stringToCSVCGI;
     }
 
+    private static void commandCIVIC(@NotNull JsonObject civicObject, @NotNull List<String> keysOfLifeCycleActions, @NotNull String i,
+            @NotNull StringBuilder stringToCSVCIVIC) {
+        JsonObject objectLastCommened = civicObject.get(i).getAsJsonObject();
+        List<String> keysLastCommened = new ArrayList<>(objectLastCommened.keySet());
+        for (int p = 0; p < keysLastCommened.size(); p++) {
+            if (keysLastCommened.get(p).equals("timestamp")) {
+                stringToCSVCIVIC.append(objectLastCommened.get(i)).append(";");
+            } else if (keysLastCommened.get(p).equals("user")) {
+                JsonObject objectUser = objectLastCommened.get(keysLastCommened.get(p)).getAsJsonObject();
+                List<String> keysUser = new ArrayList<>(objectUser.keySet());
+
+                for (int g = 0; g < keysUser.size(); g++) {
+                    if (keysUser.get(g).equals("organization")) {
+                        JsonObject objectOrganization = objectUser.get(keysUser.get(g)).getAsJsonObject();
+                        List<String> keysOrganization = new ArrayList<>(objectOrganization.keySet());
+                        for (int d = 0; d < keysOrganization.size(); d++) {
+                            if (keysOrganization.get(d).equals("profile_image")) {
+                                JsonObject objectProfile_image = objectOrganization.get(keysOrganization.get(d)).getAsJsonObject();
+                                List<String> keysProfileImage = new ArrayList<>(objectProfile_image.keySet());
+                                for (int t = 0; t < keysProfileImage.size(); t++) {
+                                    stringToCSVCIVIC.append(objectProfile_image.get(keysProfileImage.get(t))).append(";");
+                                }
+                            } else {
+                                stringToCSVCIVIC.append(objectOrganization.get(keysOrganization.get(d))).append(";");
+                            }
+                        }
+                    } else if (keysUser.get(g).equals("avatars")) {
+                        JsonObject objectAvatars = objectUser.get(keysUser.get(g)).getAsJsonObject();
+                        List<String> keysAvatars = new ArrayList<>(objectAvatars.keySet());
+                        for (int u = 0; u < keysAvatars.size(); u++) {
+                            stringToCSVCIVIC.append(objectAvatars.get(keysAvatars.get(u))).append(";");
+
+                        }
+                    } else {
+                        stringToCSVCIVIC.append(objectUser.get(keysUser.get(g))).append(";");
+                    }
+                }
+            }
+        }
+    }
+
     private static StringBuilder readObjectCIVIC(@NotNull JsonObject object, @NotNull StringBuilder headerCSV) {
         //CIVIC object
         StringBuilder stringToCSVCIVIC = new StringBuilder();
+        StringBuilder stringDisplayName = new StringBuilder();
+        StringBuilder stringDescription = new StringBuilder();
+        StringBuilder stringUrl = new StringBuilder();
+        StringBuilder stringSoId = new StringBuilder();
+        StringBuilder stringId = new StringBuilder();
+        StringBuilder stringName = new StringBuilder();
+
+        if (object.getAsJsonObject("civic") != null) {
+            List<String> keysOfCivic = new ArrayList<>(object.getAsJsonObject("civic").keySet());
+            LOGGER.info(keysOfCivic);
+            for (int x = 0; x < keysOfCivic.size(); x++) {
+                if (keysOfCivic.get(x).equals("variant_types")) {
+                    JsonArray civicVariantTypesArray = object.getAsJsonObject("civic").get(keysOfCivic.get(x)).getAsJsonArray();
+                    for (int z = 0; z < civicVariantTypesArray.size(); z++) {
+                        JsonObject objectVariantTypes = (JsonObject) civicVariantTypesArray.get(z);
+                        List<String> keysVariantTypes = new ArrayList<>(objectVariantTypes.keySet());
+                        for (int i = 0; i < keysVariantTypes.size(); i++) {
+                            if (keysVariantTypes.get(i).equals("display_name")) {
+                                JsonElement displayName = objectVariantTypes.get(keysVariantTypes.get(i));
+                                stringDisplayName.append(displayName).append(",");
+                            } else if (keysVariantTypes.get(i).equals("description")) {
+                                JsonElement discription = objectVariantTypes.get(keysVariantTypes.get(i));
+                                stringDescription.append(discription).append(",");
+                            } else if (keysVariantTypes.get(i).equals("url")) {
+                                JsonElement url = objectVariantTypes.get(keysVariantTypes.get(i));
+                                stringUrl.append(url).append(",");
+                            } else if (keysVariantTypes.get(i).equals("so_id")) {
+                                JsonElement soId = objectVariantTypes.get(keysVariantTypes.get(i));
+                                stringSoId.append(soId).append(",");
+                            } else if (keysVariantTypes.get(i).equals("id")) {
+                                JsonElement id = objectVariantTypes.get(keysVariantTypes.get(i));
+                                stringId.append(id).append(",");
+                            } else if (keysVariantTypes.get(i).equals("name")) {
+                                JsonElement name = objectVariantTypes.get(keysVariantTypes.get(i));
+                                stringName.append(name).append(",");
+                            }
+                        }
+                    }
+                    stringToCSVCIVIC.append(stringDisplayName)
+                            .append(";")
+                            .append(stringDescription)
+                            .append(";")
+                            .append(stringUrl)
+                            .append(";")
+                            .append(stringSoId)
+                            .append(";")
+                            .append(stringId)
+                            .append(";")
+                            .append(stringName)
+                            .append(";");
+                } else if (keysOfCivic.get(x).equals("lifecycle_actions")) {
+                    JsonObject civicObject = object.getAsJsonObject("civic").get(keysOfCivic.get(x)).getAsJsonObject();
+                    List<String> keysOfLifeCycleActions = new ArrayList<>(civicObject.keySet());
+                    for (int i = 0; i < keysOfLifeCycleActions.size(); i++) {
+                        if (keysOfLifeCycleActions.get(i).equals("last_commented_on")) {
+                            LOGGER.info("last_commented_on");
+                            commandCIVIC(civicObject, keysOfLifeCycleActions, "last_commented_on", stringToCSVCIVIC);
+                        }
+                        if (keysOfLifeCycleActions.get(i).equals("last_modified")) {
+                            LOGGER.info("last_modified");
+                            commandCIVIC(civicObject, keysOfLifeCycleActions, "last_modified", stringToCSVCIVIC);
+                        }
+                        if (keysOfLifeCycleActions.get(i).equals("last_reviewed")) {
+                            LOGGER.info("last_reviewed");
+                            commandCIVIC(civicObject, keysOfLifeCycleActions, "last_reviewed", stringToCSVCIVIC);
+                        }
+                    }
+
+                } else if (keysOfCivic.get(x).equals("evidence_items")) {
+                    JsonArray civicEvidenceItemsArray = object.getAsJsonObject("civic").get(keysOfCivic.get(x)).getAsJsonArray();
+                    for (int z = 0; z < civicEvidenceItemsArray.size(); z++) {
+                        JsonObject objectEvidenceitems = (JsonObject) civicEvidenceItemsArray.get(z);
+                        List<String> keysEvidenceItems = new ArrayList<>(objectEvidenceitems.keySet());
+                        for (int h = 0; h < keysEvidenceItems.size(); h++) {
+                            if (keysEvidenceItems.get(h).equals("drugs")) {
+                                JsonArray ArrayDrugs = objectEvidenceitems.get(keysEvidenceItems.get(h)).getAsJsonArray();
+                                for (int r = 0; r < ArrayDrugs.size(); r++) {
+                                    JsonObject objectDrugs = (JsonObject) ArrayDrugs.get(r);
+                                    List<String> keysDrugs = new ArrayList<>(objectDrugs.keySet());
+                                    for (int f = 0; f < keysDrugs.size(); f++) {
+                                        stringToCSVCIVIC.append(objectDrugs.get(keysDrugs.get(f))).append(";");
+                                    }
+                                }
+                            } else if (keysEvidenceItems.get(h).equals("disease")) {
+                                JsonObject objectDiseases = objectEvidenceitems.get(keysEvidenceItems.get(h)).getAsJsonObject();
+                                List<String> keysDiseases = new ArrayList<>(objectDiseases.keySet());
+                                for (int a = 0; a < keysDiseases.size(); a++) {
+                                    stringToCSVCIVIC.append(objectDiseases.get(keysDiseases.get(a))).append(";");
+                                }
+                            } else if (keysEvidenceItems.get(h).equals("source")) {
+                                JsonObject objectSource = objectEvidenceitems.get(keysEvidenceItems.get(h)).getAsJsonObject();
+                                List<String> keysSource = new ArrayList<>(objectSource.keySet());
+                                for (int o = 0; o < keysSource.size(); o++) {
+                                    if (keysSource.get(o).equals("publication_date")) {
+                                        JsonObject objectPublication = objectSource.get(keysSource.get(o)).getAsJsonObject();
+                                        List<String> keysPublication = new ArrayList<>(objectPublication.keySet());
+                                        for (int d = 0; d < keysPublication.size(); d++) {
+                                            stringToCSVCIVIC.append(objectPublication.get(keysPublication.get(d))).append(";");
+                                        }
+                                    } else {
+                                        stringToCSVCIVIC.append(objectSource.get(keysSource.get(o))).append(";");
+                                    }
+                                }
+                            } else {
+                                stringToCSVCIVIC.append(objectEvidenceitems.get(keysEvidenceItems.get(h))).append(";");
+                            }
+                        }
+                    }
+                } else if (keysOfCivic.get(x).equals("coordinates")) {
+                    JsonObject civicObject = object.getAsJsonObject("civic").get(keysOfCivic.get(x)).getAsJsonObject();
+                    List<String> keysCoordinates = new ArrayList<>(civicObject.keySet());
+                    for (int w = 0; w < keysCoordinates.size(); w++) {
+                        stringToCSVCIVIC.append(civicObject.get(keysCoordinates.get(w))).append(";");
+
+                    }
+                } else {
+                    stringToCSVCIVIC.append(object.getAsJsonObject("civic").get(keysOfCivic.get(x))).append(";");
+                }
+            }
+        } else {
+            stringToCSVCIVIC.append(";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; "
+                    + ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
+        }
+        headerCSV.append("variant_groups; entrez_name; display_name;description;url;so_id;id;name; civic_actionability_score; "
+                + "clinvar_entries; last_commented_on.timestamp;last_commented_on.username;last_commented_on.area_of_expertise;"
+                + "last_commented_on.url;last_commented_on.id;last_commented_on.x32;last_commented_on.x256;last_commented_on.x14;"
+                + "last_commented_on.x64;last_commented_on.x128; last_commented_on.description; last_commented_on.name;  "
+                + "last_commented_on.twitter_handle;  last_commented_on.name;   last_commented_on.bio;  "
+                + "last_commented_on.url; last_commented_on.created_at;  last_commented_on.x32;  last_commented_on.x14; "
+                + "last_commented_on.x64; last_commented_on.x128; last_commented_on.accepted_license;last_commented_on.affiliation;"
+                + "last_commented_on.avatar_url;last_commented_on.role;last_commented_on.facebook_profile;"
+                + "last_commented_on.linkedin_profile; last_commented_on.orcid; last_commented_on.display_name; "
+                + "last_commented_on.last_seen_at; last_commented_on.featured_expert; last_commented_on.id;"
+                + " last_commented_on.signup_complete; last_modified.timestamp;last_modified.username;last_modified.area_of_expertise;"
+                + "last_modified.url;last_modified.id;last_modified.x32;last_modified.x256;last_modified.x14;last_modified.x64;"
+                + "last_modified.x128; last_modified.description; last_modified.name;  last_modified.twitter_handle;  last_modified.name;  "
+                + " last_modified.bio;  last_modified.url; last_modified.created_at;  last_modified.x32;  last_modified.x14; "
+                + "last_modified.x64; last_modified.x128; last_modified.accepted_license;last_modified.affiliation;last_modified.avatar_url;"
+                + "last_modified.role;last_modified.facebook_profile;last_modified.linkedin_profile; last_modified.orcid; "
+                + "last_modified.display_name; last_modified.last_seen_at; last_modified.featured_expert; last_modified.id; "
+                + "last_modified.signup_complete;last_reviewed.timestamp;last_reviewed.username;last_reviewed.area_of_expertise;"
+                + "last_reviewed.url;last_reviewed.id;last_reviewed.x32;last_reviewed.x256;last_reviewed.x14;last_reviewed.x64;"
+                + "last_reviewed.x128; last_reviewed.description; last_reviewed.name;  last_reviewed.twitter_handle;  last_reviewed.name; "
+                + "  last_reviewed.bio;  last_reviewed.url; last_reviewed.created_at;  last_reviewed.x32;  last_reviewed.x14; "
+                + "last_reviewed.x64; last_reviewed.x128; last_reviewed.accepted_license;last_reviewed.affiliation;last_reviewed.avatar_url;"
+                + "last_reviewed.role;last_reviewed.facebook_profile;last_reviewed.linkedin_profile; last_reviewed.orcid; "
+                + "last_reviewed.display_name; last_reviewed.last_seen_at; last_reviewed.featured_expert; last_reviewed.id; "
+                + "last_reviewed.signup_complete; variant_aliases; allele_registry_id; provisional_values; gene_id; name; status; "
+                + "rating; drug_interaction_type; description; open_change_count; evidence_type; pubchem_id; id; name; variant_origin;"
+                + " doid;url;display_name;id;name;status;open_access;name;journal;citation;pmc_id;full_journal_title;source_url;"
+                + "clinical_trials;pubmed_id;is_review;year;day;month;id;evidence_direction;variant_id;clinical_significance;evidence_level;"
+                + "type;id;name; sources; entrez_id; assertions; hgvs_expressions; errors; chromosome2;reference_bases;start2;variant_bases;"
+                + "stop;stop2;representative_transcript2;start;representative_transcript;ensembl_version;chromosome;reference_build; type;"
+                + " id; description");
         return stringToCSVCIVIC;
     }
 
@@ -698,7 +893,7 @@ public abstract class ViccFactory {
         JsonReader reader = new JsonReader(new FileReader(allJsonPath));
         reader.setLenient(true);
         int index = 1;
-        while (reader.peek() != JsonToken.END_DOCUMENT && index < 3100) {
+        while (reader.peek() != JsonToken.END_DOCUMENT && index < 10) {
             LOGGER.info(index);
             JsonObject object = parser.parse(reader).getAsJsonObject();
 
@@ -717,7 +912,8 @@ public abstract class ViccFactory {
             // StringBuilder StringToCSVJax = readObjectJax(object, headerCSV);
             // StringBuilder StringToCSVJaxTrials = readObjectJaxTrials(object, headerCSV);
             //StringBuilder StringToCSVCGI = readObjectCGI(object, headerCSV);
-            StringBuilder StringToCSVOncokb = readObjectOncokb(object, headerCSV);
+            //  StringBuilder StringToCSVOncokb = readObjectOncokb(object, headerCSV);
+            StringBuilder StringToCSVCivic = readObjectCIVIC(object, headerCSV);
 
             stringToCSVAll.append(index).append(";");
             stringToCSVAll.append(StringToCSVSource);
@@ -725,7 +921,8 @@ public abstract class ViccFactory {
             //            stringToCSVAll.append(StringToCSVTags);
             //            stringToCSVAll.append(StringToCSVDevTags);
             //            stringToCSVAll.append(StringToCSVGeneIdentifiers);
-            stringToCSVAll.append(StringToCSVOncokb);
+            //  stringToCSVAll.append(StringToCSVOncokb);
+            stringToCSVAll.append(StringToCSVCivic);
 
             writer.append(headerCSV);
             writer.append("\n");
