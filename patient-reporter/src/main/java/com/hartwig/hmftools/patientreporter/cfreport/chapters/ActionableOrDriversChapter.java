37259f75ee9e52b5c87f04b5d5442a6e1955bac9
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-05-09 12:08:40 +0200

    DEV-730: update legend tumor specific table

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/chapters/ActionableOrDriversChapter.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/chapters/ActionableOrDriversChapter.java
@@ -100,7 +100,7 @@ public class ActionableOrDriversChapter implements ReportChapter {
 
         if (SomaticVariants.hasNotifiableGermlineVariant(reportableVariants)) {
             contentTable.addCell(TableUtil.createLayoutCell(1, contentTable.getNumberOfColumns())
-                    .add(new Paragraph("Marked variant(s) (#) are also present in the germline of the patient. "
+                    .add(new Paragraph("# Marked variant(s) are also present in the germline of the patient. "
                             + "Referral to a genetic specialist should be advised.").addStyle(ReportResources.subTextStyle())));
         }
 
