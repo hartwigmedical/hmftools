8f40f6447daf36e7dac7b5bf065681d6b97ed7bf
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-05-10 15:14:34 +0200

    DEV-730: Some very minor textual changes to explanation chapter

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/chapters/ExplanationChapter.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/chapters/ExplanationChapter.java
@@ -56,9 +56,9 @@ public class ExplanationChapter implements ReportChapter {
                         "The 'Read Depth' displays the raw number of reads supporting the variant versus the total "
                                 + "number of reads on the mutated position.",
                         "The 'Ploidy (VAF)' field displays the tumor ploidy for the observed position. The ploidy has "
-                                + "been adjusted for the implied tumor purity (see above) and is shown as a proportion of "
+                                + "been adjusted for the implied tumor purity and is shown as a proportion of "
                                 + "A’s and B’s (e.g. AAABB for 3 copies of A, and 2 copies of B). The copy number is the "
-                                + "sum of A’s and B’s. The VAF value is to the alternative allele frequency after "
+                                + "sum of A’s and B’s. The VAF value is the alternative allele frequency after "
                                 + "correction for tumor purity.",
                         "The 'Biallelic' field indicates whether the variant is present across all alleles in the tumor "
                                 + "(and is including variants with loss-of-heterozygosity).",
@@ -77,8 +77,8 @@ public class ExplanationChapter implements ReportChapter {
         table.addCell(TableUtil.createLayoutCell()
                 .add(createContentDiv(new String[] { "The lowest copy number value along the exonic regions of the canonical transcript is "
                         + "determined as a measure for the gene's copy number.",
-                        "Copy numbers are corrected for the implied tumor purity and represent the number of copies " + "in the tumor DNA.",
-                        "Any gene with less than 0.5 copies along the entire canonical transcript is reported as a " + "full loss.",
+                        "Copy numbers are corrected for the implied tumor purity and represent the number of copies in the tumor DNA.",
+                        "Any gene with less than 0.5 copies along the entire canonical transcript is reported as a full loss.",
                         "Any gene where only a part along the canonical transcript has less than 0.5 copies is reported "
                                 + "as a partial loss.",
                         "Any gene with more copies than 3 times the average tumor ploidy is reported as a gain." })));
@@ -92,7 +92,7 @@ public class ExplanationChapter implements ReportChapter {
                 .add(createContentDiv(new String[] {
                         "Genes are reported as being disrupted if their canonical transcript has been disrupted",
                         "The range of the disruption is indicated by the intron/exon/promoter region of the break point "
-                                + "occurred and the direction the disruption faces.",
+                                + "and the direction the disruption faces.",
                         "The type of disruption can be INV (inversion), DEL (deletion), DUP (duplication), INS "
                                 + "(insertion), SGL (single) or BND (translocation)." })));
 
