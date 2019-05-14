389433bdc35924c5967c61ac2cf839ae0d47b782
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-05-14 10:40:28 +0200

    set write to pdf to false

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/cfreport/CFReportWriterTest.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/cfreport/CFReportWriterTest.java
@@ -23,7 +23,7 @@ import static com.hartwig.hmftools.patientreporter.PatientReporterTestUtil.testB
 
 public class CFReportWriterTest {
 
-    private static final boolean WRITE_TO_PDF = true;
+    private static final boolean WRITE_TO_PDF = false;
     private static final boolean TIMESTAMP_FILES = false;
 
     private static final String REPORT_BASE_DIR = System.getProperty("user.home") + File.separator + "hmf" + File.separator + "tmp";
