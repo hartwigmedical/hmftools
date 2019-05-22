41458419750a6b572a13720dacf6b3006fcd7b33
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-05-22 16:11:59 +0200

    DEV-642: fix test

--- hmf-common/src/main/java/com/hartwig/hmftools/common/vicc/sage.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/vicc/sage.java
@@ -1,13 +1,20 @@
 package com.hartwig.hmftools.common.vicc;
 
+import java.security.acl.LastOwnerException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class sage {
+
+    private static final Logger LOGGER = LogManager.getLogger(sage.class);
 
     public static StringBuilder readObjectSage(@NotNull JsonObject object) {
         //SAGE object
@@ -26,20 +33,41 @@ public class sage {
         return stringToCSVSage;
     }
 
-    public static StringBuilder readObjectSageSpecificFields(@NotNull JsonObject object) {
+    public static void readObjectSageSpecificFields(@NotNull JsonObject object) {
         //SAGE object
-        StringBuilder stringToCSVSage = new StringBuilder();
+        StringBuffer stringToCSVSage = new StringBuffer();
         List<String> keysOfSage;
+        List<String> listSage = Lists.newArrayList();
+
 
         if (object.getAsJsonObject("sage") != null) {
             keysOfSage = new ArrayList<>(object.getAsJsonObject("sage").keySet());
 
             for (int j = 0; j < keysOfSage.size(); j++) {
-                stringToCSVSage.append(object.getAsJsonObject("sage").get(keysOfSage.get(j))).append(";");
+                if (keysOfSage.get(j).equals("publication_url")) {
+                    listSage.add(0, object.getAsJsonObject("sage").get(keysOfSage.get(j)).toString());
+                    LOGGER.info(listSage.size());
+                    LOGGER.info(listSage);
+                }
+                if (keysOfSage.get(j).equals("clinical_manifestation")) {
+                    LOGGER.info(listSage.size());
+
+                    listSage.add(0, object.getAsJsonObject("sage").get(keysOfSage.get(j)).toString());
+                }
+                if (keysOfSage.get(j).equals("drug_labels")) {
+                    listSage.add(0, object.getAsJsonObject("sage").get(keysOfSage.get(j)).toString());
+                }
+                if (keysOfSage.get(j).equals("evidence_label")) {
+                    listSage.add(0, object.getAsJsonObject("sage").get(keysOfSage.get(j)).toString());
+                }
+                if (keysOfSage.get(j).equals("response_type")) {
+                    listSage.add(0, object.getAsJsonObject("sage").get(keysOfSage.get(j)).toString());
+                }
+                if (keysOfSage.get(j).equals("gene")) {
+                    listSage.add(0, object.getAsJsonObject("sage").get(keysOfSage.get(j)).toString());
+                }
             }
-        } else {
-            stringToCSVSage.append(";;;;;;;;");
+         //   LOGGER.info(String.join(";" , listSage));
         }
-        return stringToCSVSage;
     }
 }
--- hmf-common/src/test/java/com/hartwig/hmftools/common/vicc/ViccFactoryTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/vicc/ViccFactoryTest.java
@@ -9,14 +9,6 @@ import org.junit.Ignore;
 
 public class ViccFactoryTest {
 
-    private static final String BRCA_FILE_PATH = Resources.getResource("vicc_knowledgebase").getPath();
-
-    @Test
-    @Ignore
-    public void convertBRCA() throws IOException {
-        ViccFactory.extractBRCAFile(BRCA_FILE_PATH + "/brca.json");
-    }
-
     @Test
     @Ignore
     public void convertAll() throws IOException {
