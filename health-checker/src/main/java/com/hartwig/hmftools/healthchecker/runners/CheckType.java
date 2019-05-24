aaa8e13092a157af111297b366d8d54e34c8e69a
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-05-24 11:04:36 +0200

    DEV-750: Major clean up

--- health-checker/src/main/java/com/hartwig/hmftools/healthchecker/runners/CheckType.java
+++
@@ -1,7 +0,0 @@
-package com.hartwig.hmftools.healthchecker.runners;
-
-public enum CheckType {
-    COVERAGE,
-    PURPLE,
-    AMBER
-}
--- health-checker/src/main/java/com/hartwig/hmftools/healthchecker/runners/CoverageChecker.java
+++ health-checker/src/main/java/com/hartwig/hmftools/healthchecker/runners/CoverageChecker.java
@@ -3,25 +3,17 @@ package com.hartwig.hmftools.healthchecker.runners;
 import java.io.IOException;
 import java.util.List;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.metrics.WGSMetrics;
 import com.hartwig.hmftools.common.metrics.WGSMetricsFile;
-import com.hartwig.hmftools.healthchecker.result.BaseResult;
 import com.hartwig.hmftools.healthchecker.result.ImmutableQCValue;
-import com.hartwig.hmftools.healthchecker.result.MultiValueResult;
-import com.hartwig.hmftools.healthchecker.result.PatientResult;
 import com.hartwig.hmftools.healthchecker.result.QCValue;
 import com.hartwig.hmftools.healthchecker.result.QCValueType;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class CoverageChecker implements HealthChecker {
-
-    private static final Logger LOGGER = LogManager.getLogger(CoverageChecker.class);
 
     @NotNull
     private final String refSample;
@@ -57,30 +49,6 @@ public class CoverageChecker implements HealthChecker {
     }
 
     @NotNull
-    @VisibleForTesting
-    static BaseResult toCheckResult(@NotNull WGSMetrics metrics, @NotNull String refSample, @Nullable String tumorSample) {
-        final List<HealthCheck> refChecks = Lists.newArrayList(new HealthCheck(refSample,
-                        CoverageCheck.COVERAGE_10X.toString(),
-                        String.valueOf(metrics.ref10xCoveragePercentage())),
-                new HealthCheck(refSample, CoverageCheck.COVERAGE_20X.toString(), String.valueOf(metrics.ref20xCoveragePercentage())));
-
-        if (tumorSample != null) {
-            assert metrics.tumor30xCoveragePercentage() != null;
-            assert metrics.tumor60xCoveragePercentage() != null;
-
-            final List<HealthCheck> tumorChecks = Lists.newArrayList(new HealthCheck(tumorSample,
-                            CoverageCheck.COVERAGE_30X.toString(),
-                            String.valueOf(metrics.tumor30xCoveragePercentage())),
-                    new HealthCheck(tumorSample,
-                            CoverageCheck.COVERAGE_60X.toString(),
-                            String.valueOf(metrics.tumor60xCoveragePercentage())));
-            return toPatientResult(refChecks, tumorChecks);
-        } else {
-            return toMultiValueResult(refChecks);
-        }
-    }
-
-    @NotNull
     private WGSMetrics extractMetrics() throws IOException {
         String refFile = WGSMetricsFile.generateFilenamePv5(metricsDirectory, refSample);
         if (tumorSample != null) {
@@ -89,20 +57,5 @@ public class CoverageChecker implements HealthChecker {
         } else {
             return WGSMetricsFile.read(refFile);
         }
-    }
-
-    @NotNull
-    private static BaseResult toPatientResult(@NotNull final List<HealthCheck> refChecks, @NotNull final List<HealthCheck> tumorChecks) {
-        HealthCheck.log(LOGGER, refChecks);
-        HealthCheck.log(LOGGER, tumorChecks);
-
-        return new PatientResult(CheckType.COVERAGE, refChecks, tumorChecks);
-    }
-
-    @NotNull
-    private static BaseResult toMultiValueResult(@NotNull final List<HealthCheck> checks) {
-        HealthCheck.log(LOGGER, checks);
-
-        return new MultiValueResult(CheckType.COVERAGE, checks);
     }
 }
