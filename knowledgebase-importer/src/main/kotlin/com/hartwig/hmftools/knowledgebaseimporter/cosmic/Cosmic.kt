970c73cbb2779420897fdf352b818018ab1bb42d
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-05-28 15:21:57 +0200

    DEV-714: Correct gene names for cosmis

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cosmic/Cosmic.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cosmic/Cosmic.kt
@@ -5,12 +5,11 @@ import com.hartwig.hmftools.knowledgebaseimporter.diseaseOntology.Doid
 import com.hartwig.hmftools.knowledgebaseimporter.output.*
 import com.hartwig.hmftools.knowledgebaseimporter.readCSVRecords
 import org.apache.commons.csv.CSVRecord
-import org.apache.logging.log4j.LogManager
 
 class Cosmic(fusionsLocation: String) : Knowledgebase {
     override val source = "cosmic"
     override val knownVariants: List<KnownVariantOutput> = listOf()
-    override val knownFusionPairs by lazy { readCSVRecords(fusionsLocation) { readFusion(it) }.distinct() }
+    override val knownFusionPairs by lazy { readCSVRecords(fusionsLocation) { readAndCorrectFusion(it) }.distinct() }
     override val promiscuousGenes: List<PromiscuousGene> = listOf()
     override val actionableVariants: List<ActionableVariantOutput> = listOf()
     override val actionableCNVs: List<ActionableCNVOutput> = listOf()
@@ -18,16 +17,25 @@ class Cosmic(fusionsLocation: String) : Knowledgebase {
     override val actionablePromiscuousGenes: List<ActionablePromiscuousGeneOutput> = listOf()
     override val actionableRanges: List<ActionableGenomicRangeOutput> = listOf()
     override val cancerTypes: Map<String, Set<Doid>> = mapOf()
-    private val logger = LogManager.getLogger("cosmic")
 
-    private fun readFusion(csvRecord: CSVRecord): FusionPair {
+    private fun readAndCorrectFusion(csvRecord: CSVRecord): FusionPair {
         val fiveGene = csvRecord["5' Partner"].split("_").first()
         val threeGene = csvRecord["3' Partner"].split("_").first()
+        if (threeGene.equals("DUX4L1")) {
+            threeGene.replace("DUX4L1", "DUX4")
+        }
+        if (threeGene.equals("SIP1")) {
+            threeGene.replace("SIP1", "GEMIN2")
+        }
+        if (threeGene.equals("KIAA0284")) {
+            threeGene.replace("KIAA0284", "CEP170B")
+        }
+        if (threeGene.equals("ACCN1")) {
+            threeGene.replace("ACCN1", "ASIC2")
+        }
+        if (threeGene.equals("FAM22A")) {
+            threeGene.replace("FAM22A", "NUTM2A")
+        }
         return FusionPair(fiveGene, threeGene)
     }
-
-//    override val actionableKbRecords by lazy {
-//        logger.info("Reading cosmic actionable records.")
-//        CsvReader.readTSVByName<CosmicKnownInput>(fusionsLocation).mapNotNull { it.corrected() }.map { }
-//    }
 }
--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/cosmic/input/ComicInput.kt
+++
@@ -1,6 +0,0 @@
-package com.hartwig.hmftools.knowledgebaseimporter.cosmic.input
-
-import com.hartwig.hmftools.knowledgebaseimporter.knowledgebases.events.KnowledgebaseEvent
-
-interface ComicInput: KnowledgebaseEvent {
-}
