0a17899d923edebf33ac023b2f2a97d61f9dff3b
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-05-28 12:36:06 +0200

    DEV-750: Add health check evaluation

---
+++ health-checker/src/test/java/com/hartwig/hmftools/healthchecker/HealthChecksApplicationTest.java
@@ -0,0 +1,35 @@
+package com.hartwig.hmftools.healthchecker;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.common.io.Resources;
+
+import org.junit.Test;
+
+public class HealthChecksApplicationTest {
+
+    private static final String BASE_DIR = Resources.getResource("").getPath();
+
+    private static final String OUTPUT_DIR = System.getProperty("user.home") + File.separator + "hmf" + File.separator + "tmp";
+    private static final boolean WRITE_OUTPUT = false;
+
+    @Test
+    public void runHealthCheckerInSomaticMode() throws IOException {
+        HealthChecksApplication app = new HealthChecksApplication("reference",
+                "tumor",
+                BASE_DIR + "metrics",
+                BASE_DIR + "amber",
+                BASE_DIR + "purple",
+                OUTPUT_DIR);
+
+        app.run(WRITE_OUTPUT);
+    }
+
+    @Test
+    public void runHealthCheckerInSingleSampleMode() throws IOException {
+        HealthChecksApplication app = new HealthChecksApplication("reference", null, BASE_DIR + "metrics", null, null, OUTPUT_DIR);
+
+        app.run(WRITE_OUTPUT);
+    }
+}
