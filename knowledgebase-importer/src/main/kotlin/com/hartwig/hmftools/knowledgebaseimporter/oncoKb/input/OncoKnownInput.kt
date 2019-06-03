0a1c33214f09ebddf2846df5b890417e8ecdde93
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-06-03 11:55:08 +0200

    DEV-714: removing to genes of oncoKB

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/input/OncoKnownInput.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/oncoKb/input/OncoKnownInput.kt
@@ -17,7 +17,6 @@ data class OncoKnownInput(private val Isoform: String?, @get:JvmName("getGene_")
             Alteration.contains("IGH-NKX2") && Gene == "NKX2-1" -> copy(Alteration = Alteration.replace("IGH-NKX2", "IGH-NKX2-1"))
             Alteration.contains("ROS1-CD74")                    -> copy(Alteration = Alteration.replace("ROS1-CD74", "CD74-ROS1"))
             Alteration.contains("RET-CCDC6")                    -> copy(Alteration = Alteration.replace("RET-CCDC6", "CCDC6-RET"))
-            Alteration.contains("FGFR1OP1-FGFR1")               -> copy(Alteration = Alteration.replace("FGFR1OP-FGFR1", "FGFR1OP1-FGFR1"))
             Alteration.contains("EP300-MOZ")                    -> copy(Alteration = Alteration.replace("EP300-MOZ", "KAT6A-EP300"))
             Alteration.contains("EP300-MLL")                    -> copy(Alteration = Alteration.replace("EP300-MLL", "KMT2A-EP300"))
             Alteration.contains("BRD4-NUT")                     -> copy(Alteration = Alteration.replace("BRD4-NUT", "BRD4-NUTM1"))
@@ -36,8 +35,8 @@ data class OncoKnownInput(private val Isoform: String?, @get:JvmName("getGene_")
             Alteration.contains("FGFR1OP1-FGFR1")               -> copy(Alteration = Alteration.replace("FGFR1OP1-FGFR1", "FGFR1OP-FGFR1"))
             Alteration.contains("ZNF198-FGFR1")                 -> copy(Alteration = Alteration.replace("ZNF198-FGFR1", "ZMYM2-FGFR1"))
             Alteration == "p61BRAF-V600E"                       -> copy(Alteration = "V600E/V600K")
-            Alteration == "Delta"                               -> null
-            Alteration == "RARA"                                -> null
+            Gene.contains("Delta")                              -> null
+            gene.contains("GTF2I")                              -> null
             else                                                -> this
         }
     }
