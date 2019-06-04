64cc18a3a867034a2840eb6a8043d37997f53beb
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-06-04 13:20:55 +0200

    DEV-714: Fix issue for removing fusion genes

--- knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/FusionsAnalysis.kt
+++ knowledgebase-importer/src/main/kotlin/com/hartwig/hmftools/knowledgebaseimporter/FusionsAnalysis.kt
@@ -8,30 +8,10 @@ import com.hartwig.hmftools.knowledgebaseimporter.output.PromiscuousGene
 private const val MIN_PARTNER_COUNT = 2
 
 // Fusions that appear more than MIN_PARTNER_COUNT on the fiveGene side of known fusion pairs
-fun inferredPromiscuousFiveGenes(knowledgebases: Knowledgebase): List<PromiscuousGene> {
-    return knowledgebases.knownFusionPairs
-            .distinct()
-            .groupBy { it.fiveGene }
-            .filter { it.value.size > MIN_PARTNER_COUNT }
-            .map { PromiscuousGene(it.key) }
-            .toList()
-}
-
-// Fusions that appear more than MIN_PARTNER_COUNT on the fiveGene side of known fusion pairs
 fun inferredPromiscuousFiveGenes(knowledgebases: Collection<Knowledgebase>): List<PromiscuousGene> {
     return knowledgebases.flatMap { it.knownFusionPairs }
             .distinct()
             .groupBy { it.fiveGene }
-            .filter { it.value.size > MIN_PARTNER_COUNT }
-            .map { PromiscuousGene(it.key) }
-            .toList()
-}
-
-// Fusions that appear more than MIN_PARTNER_COUNT on the threeGene side of known fusion pairs
-fun inferredPromiscuousThreeGenes(knowledgebases: Knowledgebase): List<PromiscuousGene> {
-    return knowledgebases.knownFusionPairs
-            .distinct()
-            .groupBy { it.threeGene }
             .filter { it.value.size > MIN_PARTNER_COUNT }
             .map { PromiscuousGene(it.key) }
             .toList()
@@ -48,24 +28,10 @@ fun inferredPromiscuousThreeGenes(knowledgebases: Collection<Knowledgebase>): Li
 }
 
 // Genes that appear as promiscuous in knowledgebases and also appear at least once (as fiveGene) with a specific partner in known fusion pairs
-fun externalPromiscuousFiveGenes(knowledgebases: Knowledgebase): List<PromiscuousGene> {
-    return knowledgebases.promiscuousGenes
-            .distinct()
-            .filter { knowledgebases.knownFusionPairs.distinct().map { it.fiveGene }.toSet().contains(it.gene) }
-}
-
-// Genes that appear as promiscuous in knowledgebases and also appear at least once (as fiveGene) with a specific partner in known fusion pairs
 fun externalPromiscuousFiveGenes(knowledgebases: Collection<Knowledgebase>): List<PromiscuousGene> {
     return knowledgebases.flatMap { it.promiscuousGenes }
             .distinct()
             .filter { knowledgebases.flatMap { it.knownFusionPairs }.distinct().map { it.fiveGene }.toSet().contains(it.gene) }
-}
-
-// Genes that appear as promiscuous in knowledgebases and also appear at least once (as threeGene) with a specific partner in known fusion pairs
-fun externalPromiscuousThreeGenes(knowledgebases: Knowledgebase): List<PromiscuousGene> {
-    return knowledgebases.promiscuousGenes
-            .distinct()
-            .filter { knowledgebases.knownFusionPairs.distinct().map { it.threeGene }.toSet().contains(it.gene) }
 }
 
 // Genes that appear as promiscuous in knowledgebases and also appear at least once (as threeGene) with a specific partner in known fusion pairs
@@ -75,20 +41,12 @@ fun externalPromiscuousThreeGenes(knowledgebases: Collection<Knowledgebase>): Li
             .filter { knowledgebases.flatMap { it.knownFusionPairs }.distinct().map { it.threeGene }.toSet().contains(it.gene) }
 }
 
-fun knownFusionPairs(knowledgebases: Knowledgebase): List<FusionPair> {
-    return knowledgebases.knownFusionPairs.distinct()
-}
-
-fun knownPromiscuousFive(knowledgebases: Knowledgebase): List<PromiscuousGene> {
-    return (inferredPromiscuousFiveGenes(knowledgebases) + externalPromiscuousFiveGenes(knowledgebases)).distinct()
+fun knownFusionPairs(knowledgebases: Collection<Knowledgebase>): List<FusionPair> {
+    return knowledgebases.flatMap { it.knownFusionPairs }.distinct()
 }
 
 fun knownPromiscuousFive(knowledgebases: Collection<Knowledgebase>): List<PromiscuousGene> {
     return (inferredPromiscuousFiveGenes(knowledgebases) + externalPromiscuousFiveGenes(knowledgebases)).distinct()
-}
-
-fun knownPromiscuousThree(knowledgebases: Knowledgebase): List<PromiscuousGene> {
-    return (inferredPromiscuousThreeGenes(knowledgebases) + externalPromiscuousThreeGenes(knowledgebases)).distinct()
 }
 
 fun knownPromiscuousThree(knowledgebases: Collection<Knowledgebase>): List<PromiscuousGene> {
