0d9722247fba75c64fbb3bb84ea52410bffed14c
Jonathan Baber <jonbaber@gmail.com>
2019-06-13 15:39:11 +1000

    Make comparable

--- hmf-common/src/main/java/com/hartwig/hmftools/common/actionability/panel/ActionablePanel.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/actionability/panel/ActionablePanel.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.Nullable;
 @Value.Immutable
 @Value.Style(allParameters = true,
              passAnnotations = { NotNull.class, Nullable.class })
-public abstract class ActionablePanel {
+public abstract class ActionablePanel implements Comparable<ActionablePanel> {
 
     @NotNull
     public abstract String gene();
@@ -20,4 +20,8 @@ public abstract class ActionablePanel {
 
     public abstract boolean variant();
 
+    @Override
+    public int compareTo(@NotNull final ActionablePanel o) {
+        return gene().compareTo(o.gene());
+    }
 }
