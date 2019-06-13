66c6634b93ea010a6273a042973290f18cf82a30
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-06-13 10:08:48 +0200

    DEV-759: start with changing database tables

--- vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/dao/ViccDAO.java
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/dao/ViccDAO.java
@@ -1,15 +1,10 @@
 package com.hartwig.hmftools.vicc.dao;
 
-import static com.hartwig.hmftools.vicc.database.Tables.ASSOCIATION;
 import static com.hartwig.hmftools.vicc.database.Tables.DEVTAG;
-import static com.hartwig.hmftools.vicc.database.Tables.ENVIRONMENTALCONTEXT;
-import static com.hartwig.hmftools.vicc.database.Tables.EVIDENCE;
-import static com.hartwig.hmftools.vicc.database.Tables.FEATURE;
 import static com.hartwig.hmftools.vicc.database.Tables.FEATURENAME;
+import static com.hartwig.hmftools.vicc.database.Tables.FEATURES;
 import static com.hartwig.hmftools.vicc.database.Tables.GENE;
 import static com.hartwig.hmftools.vicc.database.Tables.GENEIDENTIFIER;
-import static com.hartwig.hmftools.vicc.database.Tables.PHENOTYPE;
-import static com.hartwig.hmftools.vicc.database.Tables.SEQUENCEONTOLOGY;
 import static com.hartwig.hmftools.vicc.database.Tables.TAG;
 import static com.hartwig.hmftools.vicc.database.Tables.VICCENTRY;
 
@@ -19,17 +14,12 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.hartwig.hmftools.vicc.ViccJsonToSQLImporter;
-import com.hartwig.hmftools.vicc.datamodel.Association;
-import com.hartwig.hmftools.vicc.datamodel.EnvironmentalContext;
-import com.hartwig.hmftools.vicc.datamodel.Evidence;
 import com.hartwig.hmftools.vicc.datamodel.Feature;
 import com.hartwig.hmftools.vicc.datamodel.GeneIdentifier;
-import com.hartwig.hmftools.vicc.datamodel.SequenceOntology;
 import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jooq.DSLContext;
@@ -68,161 +58,15 @@ public class ViccDAO {
         writeGeneIdentifiers(id, viccEntry.geneIdentifiers());
         writeGenes(id, viccEntry.genes());
         writeFeatureNames(id, viccEntry.featureNames());
-        writeFeature(id, viccEntry.features());
-        writeAssociation(id, viccEntry.association());
+        writeFeatures(id, viccEntry.features());
     }
 
-    private void writeAssociation(int viccEntryId, @NotNull Association association) {
-        int id = context.insertInto(ASSOCIATION,
-                ASSOCIATION.VARIANTNAME,
-                ASSOCIATION.EVIDENCELEVEL,
-                ASSOCIATION.EVIDENCELABEL,
-                ASSOCIATION.RESPONSETYPE,
-                ASSOCIATION.DRUGLABELS,
-                ASSOCIATION.SOURCELINK,
-                ASSOCIATION.PUBLICATIONURLS,
-                ASSOCIATION.DESCRIPTION,
-                ASSOCIATION.ONCOGENIC,
-                ASSOCIATION.VICCENTRYID)
-                .values(association.variantName(),
-                        association.evidenceLevel(),
-                        association.evidenceLabel(),
-                        association.responseType(),
-                        association.drugLabels(),
-                        association.sourceLink(),
-                        "",
-                        association.description(),
-                        association.oncogenic(),
-                        viccEntryId)
-                .returning(ASSOCIATION.IDASSOCIATION)
-                .fetchOne()
-                .getValue(ASSOCIATION.IDASSOCIATION);
-        writeEvidence(id, association);
-        writePhenotype(id, association);
-        writeEnvironmentalContext(id, association);
-    }
+    private void writeFeatures(int viccEntryId, @NotNull List<Feature> features) {
+        for (Feature feature: features) {
+            context.insertInto(FEATURES, FEATURES.NAME, FEATURES.VICCENTRYID).values(feature.name(), viccEntryId).execute();
 
-    private void writeEvidence(int associationId, @Nullable Association association) {
-        for (Evidence evidence : association.evidence()) {
-            context.insertInto(EVIDENCE,
-                    EVIDENCE.EVIDENCEINFOPUBLICATIONS,
-                    EVIDENCE.EVIDENCETYPESOURCENAME,
-                    EVIDENCE.EVIDENCETYPEID,
-                    EVIDENCE.DESCRIPTION,
-                    EVIDENCE.ASSOCIATIONENTRYID)
-                    .values("", evidence.evidenceType().sourceName(), evidence.evidenceType().id(), evidence.description(), associationId)
-                    .execute();
-        }
-
-    }
-
-    private void writePhenotype(int associationId, @Nullable Association association) {
-        context.insertInto(PHENOTYPE,
-                PHENOTYPE.PHENOTYPETYPESOURCE,
-                PHENOTYPE.PHENOTYPETYPETERM,
-                PHENOTYPE.PHENOTYPETYPEID,
-                PHENOTYPE.DESCRIPTION,
-                PHENOTYPE.FAMILY,
-                PHENOTYPE.ID,
-                PHENOTYPE.ASSOCIATIONENTRYID)
-                .values(association.phenotype().type().source(),
-                        association.phenotype().type().term(),
-                        association.phenotype().type().id(),
-                        association.phenotype().description(),
-                        association.phenotype().family(),
-                        association.phenotype().id(),
-                        associationId)
-                .execute();
-    }
-
-    private void writeEnvironmentalContext(int associationId, @Nullable Association association) {
-        for (EnvironmentalContext environmentalContext : association.environmentalContexts()) {
-            context.insertInto(ENVIRONMENTALCONTEXT,
-                    ENVIRONMENTALCONTEXT.TERM,
-                    ENVIRONMENTALCONTEXT.DESCRIPTION,
-                    ENVIRONMENTALCONTEXT.TAXONOMYKINGDOM,
-                    ENVIRONMENTALCONTEXT.TAXONOMYDIRECTPARENT,
-                    ENVIRONMENTALCONTEXT.TAXONOMYCLASS,
-                    ENVIRONMENTALCONTEXT.TAXONOMYSUBCLASS,
-                    ENVIRONMENTALCONTEXT.TAXONOMYSUPERCLASS,
-                    ENVIRONMENTALCONTEXT.SOURCE,
-                    ENVIRONMENTALCONTEXT.USANSTEM,
-                    ENVIRONMENTALCONTEXT.APPROVEDCOUNTRIES,
-                    ENVIRONMENTALCONTEXT.ID,
-                    ENVIRONMENTALCONTEXT.ASSOCIATIONENTRYID)
-                    .values(environmentalContext.term(),
-                            environmentalContext.description(),
-                            environmentalContext.taxonomy().kingdom(),
-                            environmentalContext.taxonomy().directParent(),
-                            environmentalContext.taxonomy().classs(),
-                            environmentalContext.taxonomy().subClass(),
-                            environmentalContext.taxonomy().superClass(),
-                            environmentalContext.source(),
-                            environmentalContext.usanStem(),
-                            "",
-                            environmentalContext.id(),
-                            associationId)
-                    .execute();
         }
     }
-
-    private void writeSequenceOntology(int featureId, @Nullable SequenceOntology sequenceOntologies) {
-        String hierarchy = Strings.EMPTY;
-        context.insertInto(SEQUENCEONTOLOGY,
-                SEQUENCEONTOLOGY.HIERARCHY,
-                SEQUENCEONTOLOGY.SOID,
-                SEQUENCEONTOLOGY.PARENTSOID,
-                SEQUENCEONTOLOGY.NAME,
-                SEQUENCEONTOLOGY.PARENTNAME,
-                SEQUENCEONTOLOGY.FEATUREENTRYID).values(hierarchy, "", "", "", "", featureId).execute();
-    }
-
-    private void writeFeature(int viccEntryId, @NotNull List<Feature> features) {
-        int idFeature = 1;
-        for (Feature feature : features) {
-            String linksString = Strings.EMPTY;
-            String provenanceString = Strings.EMPTY;
-            String synonymsString = Strings.EMPTY;
-
-            context.insertInto(FEATURE,
-                    FEATURE.NAME,
-                    FEATURE.BIOMARKERTYPE,
-                    FEATURE.REFERENCENAME,
-                    FEATURE.CHROMOSOME,
-                    FEATURE.START,
-                    FEATURE.END,
-                    FEATURE.REF,
-                    FEATURE.ALT,
-                    FEATURE.PROVENANCE,
-                    FEATURE.PROVENANCERULE,
-                    FEATURE.GENESYMBOL,
-                    FEATURE.SYNONYMS,
-                    FEATURE.ENTREZID,
-                    FEATURE.LINKS,
-                    FEATURE.DESCRIPTION,
-                    FEATURE.VICCENTRYID)
-                    .values(feature.name(),
-                            feature.biomarkerType(),
-                            feature.referenceName(),
-                            feature.chromosome(),
-                            feature.start(),
-                            feature.end(),
-                            feature.ref(),
-                            feature.alt(),
-                            provenanceString,
-                            feature.provenanceRule(),
-                            feature.geneSymbol(),
-                            synonymsString,
-                            feature.entrezId(),
-                            linksString,
-                            feature.description(),
-                            viccEntryId)
-                    .execute();
-            writeSequenceOntology(idFeature, feature.sequenceOntology());
-            idFeature++;
-        }
-    }
-
     private void writeTags(int viccEntryId, @NotNull List<String> tags) {
         for (String tag : tags) {
             context.insertInto(TAG, TAG.TAGNAME, TAG.VICCENTRYID).values(tag, viccEntryId).execute();
@@ -271,12 +115,6 @@ public class ViccDAO {
         context.deleteFrom(GENEIDENTIFIER).execute();
         context.deleteFrom(GENE).execute();
         context.deleteFrom(FEATURENAME).execute();
-        context.deleteFrom(FEATURE).execute();
-        context.deleteFrom(SEQUENCEONTOLOGY).execute();
-        context.deleteFrom(ASSOCIATION).execute();
-        context.deleteFrom(EVIDENCE).execute();
-        context.deleteFrom(PHENOTYPE).execute();
-        context.deleteFrom(ENVIRONMENTALCONTEXT).execute();
         context.deleteFrom(VICCENTRY).execute();
     }
 }
--- vicc-knowledgebase-importer/src/main/resources/database/create_vicc_database.sql
+++ vicc-knowledgebase-importer/src/main/resources/database/create_vicc_database.sql
@@ -54,102 +54,13 @@ CREATE TABLE devTag
     FOREIGN KEY (viccEntryId) REFERENCES viccEntry(id)
 );
 
-DROP TABLE IF EXISTS feature;
-CREATE TABLE feature
-(   idFeature int NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS features;
+CREATE TABLE features
+(   id int NOT NULL AUTO_INCREMENT,
     viccEntryId int NOT NULL,
     name varchar(255),
-    biomarkerType varchar(255),
-    referenceName varchar(255),
-    chromosome varchar(255),
-    start varchar(255),
-    end varchar(255),
-    ref varchar(255),
-    alt varchar(255),
-    provenance varchar(255),
-    provenanceRule varchar(255),
-    geneSymbol varchar(255),
-    synonyms varchar(255),
-    entrezId varchar(255),
-    links varchar(255),
-    description varchar(255),
-    PRIMARY KEY (idFeature),
-    FOREIGN KEY (viccEntryId) REFERENCES viccEntry(id)
-);
-
-DROP TABLE IF EXISTS sequenceOntology;
-CREATE TABLE sequenceOntology
-(   id int NOT NULL AUTO_INCREMENT,
-    featureEntryId int NOT NULL,
-    hierarchy varchar(255),
-    soid varchar(255) NOT NULL,
-    parentSoid varchar(255) NOT NULL,
-    name varchar(255) NOT NULL,
-    parentName varchar(255) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (featureEntryId) REFERENCES feature(idFeature)
-);
-
-DROP TABLE IF EXISTS association;
-CREATE TABLE association
-(   idAssociation int NOT NULL AUTO_INCREMENT,
-    viccEntryId int NOT NULL,
-    variantName varchar(255),
-    evidenceLevel varchar(255),
-    evidenceLabel varchar(255),
-    responseType varchar(255),
-    drugLabels varchar(255),
-    sourceLink varchar(255),
-    publicationUrls varchar(255),
-    description varchar(255) NOT NULL,
-    oncogenic varchar(255),
-    PRIMARY KEY (idAssociation),
     FOREIGN KEY (viccEntryId) REFERENCES viccEntry(id)
-);
-
-DROP TABLE IF EXISTS evidence;
-CREATE TABLE evidence
-(   idEvidence int NOT NULL AUTO_INCREMENT,
-    associationEntryId int NOT NULL,
-    evidenceInfoPublications varchar(255),
-    evidenceTypeSourceName varchar(255),
-    evidenceTypeId varchar(255),
-    description varchar(255),
-    PRIMARY KEY (idEvidence),
-    FOREIGN KEY (associationEntryId) REFERENCES association(idAssociation)
-);
-
-DROP TABLE IF EXISTS phenotype;
-CREATE TABLE phenotype
-(   idPhenotype int NOT NULL AUTO_INCREMENT,
-    associationEntryId int NOT NULL,
-    phenotypeTypeSource varchar(255),
-    phenotypeTypeterm varchar(255),
-    phenotypeTypeid varchar(255),
-    description varchar(255),
-    family varchar(255),
-    id varchar(255),
-    PRIMARY KEY (idPhenotype),
-    FOREIGN KEY (associationEntryId) REFERENCES association(idAssociation)
-);
-
-DROP TABLE IF EXISTS environmentalContext;
-CREATE TABLE environmentalContext
-(   idEnvironmentalContext int NOT NULL AUTO_INCREMENT,
-    associationEntryId int NOT NULL,
-    term varchar(255),
-    description varchar(255),
-    taxonomyKingdom varchar(255),
-    taxonomyDirectParent varchar(255),
-    taxonomyClass varchar(255),
-    taxonomySubClass varchar(255),
-    taxonomySuperClass varchar(255),
-    source varchar(255),
-    usanStem varchar(255),
-    approvedCountries varchar(255),
-    id varchar(255),
-    PRIMARY KEY (idEnvironmentalContext),
-    FOREIGN KEY (associationEntryId) REFERENCES association(idAssociation)
 );
 
 
