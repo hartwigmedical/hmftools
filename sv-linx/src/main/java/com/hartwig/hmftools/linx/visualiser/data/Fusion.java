79aa4ad48d62c0cc2f740e0bde3e153923d45e5a
Jonathan Baber <jonbaber@gmail.com>
2019-07-03 10:46:43 +1000

    Read details fusions file

---
+++ sv-linx/src/main/java/com/hartwig/hmftools/linx/visualiser/data/Fusion.java
@@ -0,0 +1,39 @@
+package com.hartwig.hmftools.linx.visualiser.data;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+abstract class Fusion
+{
+    public abstract String sampleId();
+
+    public abstract int clusterId();
+
+    public abstract String geneUp();
+
+    public abstract String chromosomeUp();
+
+    public abstract long positionUp();
+
+    public abstract String regionTypeUp();
+
+    public abstract int strandUp();
+
+    public abstract int exonUp();
+
+    public abstract String geneDown();
+
+    public abstract String chromosomeDown();
+
+    public abstract long positionDown();
+
+    public abstract String regionTypeDown();
+
+    public abstract int strandDown();
+
+    public abstract int exonDown();
+
+}
---
+++ sv-linx/src/main/java/com/hartwig/hmftools/linx/visualiser/data/Fusions.java
@@ -0,0 +1,74 @@
+package com.hartwig.hmftools.linx.visualiser.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import org.jetbrains.annotations.NotNull;
+
+class Fusions
+{
+    private static final String SEPARATOR = ",";
+
+    @NotNull
+    public static List<Fusion> fromFile(@NotNull final String fileName) throws IOException
+    {
+        return fromLines(Files.readAllLines(new File(fileName).toPath()));
+    }
+
+    @NotNull
+    private static List<Fusion> fromLines(@NotNull final List<String> lines)
+    {
+        final Map<String, Integer> map = columnMap(lines.get(0));
+
+        final List<Fusion> result = Lists.newArrayList();
+        for (int i = 1; i < lines.size(); i++)
+        {
+            result.add(fromLine(map, lines.get(i)));
+        }
+
+        return result;
+    }
+
+    @NotNull
+    private static Map<String, Integer> columnMap(@NotNull final String header)
+    {
+        final Map<String, Integer> result = Maps.newHashMap();
+        final String[] values = header.split(SEPARATOR);
+
+        for (int i = 0; i < values.length; i++)
+        {
+            result.put(values[i], i);
+        }
+
+        return result;
+
+    }
+
+    private static Fusion fromLine(@NotNull final Map<String, Integer> map, @NotNull final String line)
+    {
+
+        String[] values = line.split(SEPARATOR);
+        return ImmutableFusion.builder()
+                .sampleId(values[map.get("SampleId")])
+                .clusterId(Integer.valueOf(values[map.get("ClusterId")]))
+                .chromosomeUp(values[map.get("ChrUp")])
+                .positionUp(Long.valueOf(values[map.get("PosUp")]))
+                .geneUp(values[map.get("GeneNameUp")])
+                .strandUp(Integer.valueOf(values[map.get("StrandUp")]))
+                .regionTypeUp(values[map.get("RegionTypeUp")])
+                .exonUp(Integer.valueOf(values[map.get("ExonUp")]))
+                .chromosomeDown(values[map.get("ChrDown")])
+                .positionDown(Long.valueOf(values[map.get("PosDown")]))
+                .geneDown(values[map.get("GeneNameDown")])
+                .strandDown(Integer.valueOf(values[map.get("StrandDown")]))
+                .regionTypeDown(values[map.get("RegionTypeDown")])
+                .exonDown(Integer.valueOf(values[map.get("ExonDown")]))
+                .build();
+    }
+}
