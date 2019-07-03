23d3a42f24f772451d23b3cbc1aabf6ace4a27b1
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-07-03 15:59:47 +0200

    DEV-801: Remove dependency on metadata for loading metrics data

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/LoadMetricsData.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/LoadMetricsData.java
@@ -4,14 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import com.hartwig.hmftools.common.context.ProductionRunContextFactory;
-import com.hartwig.hmftools.common.context.RunContext;
 import com.hartwig.hmftools.common.metrics.WGSMetrics;
 import com.hartwig.hmftools.common.metrics.WGSMetricsFile;
 import com.hartwig.hmftools.patientdb.dao.DatabaseAccess;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
@@ -24,41 +21,44 @@ public final class LoadMetricsData {
 
     private static final Logger LOGGER = LogManager.getLogger(LoadCanonicalTranscripts.class);
 
+    private static final String METRICS_DIR = "metrics_dir";
+    private static final String REF_SAMPLE = "ref_sample";
+    private static final String TUMOR_SAMPLE = "tumor_sample";
+
     private static final String DB_USER = "db_user";
     private static final String DB_PASS = "db_pass";
     private static final String DB_URL = "db_url";
 
-    private static final String RUN_DIR = "run_dir";
-
     public static void main(@NotNull final String[] args) throws ParseException, SQLException, IOException {
-        final Options options = createOptions();
-        final CommandLine cmd = createCommandLine(args, options);
-        final String userName = cmd.getOptionValue(DB_USER);
-        final String password = cmd.getOptionValue(DB_PASS);
-        final String databaseUrl = cmd.getOptionValue(DB_URL);
+        Options options = createOptions();
+        CommandLine cmd = new DefaultParser().parse(options, args);
 
-        final String runDirectoryPath = cmd.getOptionValue(RUN_DIR);
+        String userName = cmd.getOptionValue(DB_USER);
+        String password = cmd.getOptionValue(DB_PASS);
+        String databaseUrl = cmd.getOptionValue(DB_URL);
 
-        if (Utils.anyNull(userName, password, databaseUrl, runDirectoryPath)) {
+        String metricsDirectory = cmd.getOptionValue(METRICS_DIR);
+        String refSample = cmd.getOptionValue(REF_SAMPLE);
+        String tumorSample = cmd.getOptionValue(TUMOR_SAMPLE);
+
+        if (Utils.anyNull(userName, password, databaseUrl, metricsDirectory, refSample, tumorSample)) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("patient-db - load metrics data", options);
         } else {
-            final File runDirectory = new File(runDirectoryPath);
-            if (runDirectory.isDirectory()) {
+            final File metricsDir = new File(metricsDirectory);
+            if (metricsDir.isDirectory()) {
                 final String jdbcUrl = "jdbc:" + databaseUrl;
                 final DatabaseAccess dbWriter = new DatabaseAccess(userName, password, jdbcUrl);
 
-                RunContext runContext = ProductionRunContextFactory.fromRunDirectory(runDirectory.toPath().toString());
-                LOGGER.info(String.format("Extracting and writing metrics for %s", runContext.runDirectory()));
-                try {
-                    WGSMetrics metrics = generateMetricsForRun(runContext);
-                    dbWriter.writeMetrics(runContext.tumorSample(), metrics);
-                } catch (IOException e) {
-                    LOGGER.warn(String.format("Cannot extract metrics for %s.", runContext.runDirectory()));
-                }
+                LOGGER.info(String.format("Extracting and writing metrics for %s", tumorSample));
+                String refFile = WGSMetricsFile.generateFilename(metricsDirectory, refSample);
+                String tumorFile = WGSMetricsFile.generateFilename(metricsDirectory, tumorSample);
+
+                WGSMetrics metrics = WGSMetricsFile.read(refFile, tumorFile);
+                dbWriter.writeMetrics(tumorSample, metrics);
             } else {
-                if (!runDirectory.exists()) {
-                    LOGGER.warn("dir " + runDirectory + " does not exist.");
+                if (!metricsDir.exists()) {
+                    LOGGER.warn("Dir " + metricsDir + " does not exist.");
                 }
                 HelpFormatter formatter = new HelpFormatter();
                 formatter.printHelp("patient-db - load metrics data", options);
@@ -67,26 +67,16 @@ public final class LoadMetricsData {
     }
 
     @NotNull
-    private static WGSMetrics generateMetricsForRun(@NotNull RunContext context) throws IOException {
-        assert context.isSomaticRun();
-        String refFile = WGSMetricsFile.generateFilename(context.runDirectory(), context.refSample());
-        String tumorFile = WGSMetricsFile.generateFilename(context.runDirectory(), context.tumorSample());
-        return WGSMetricsFile.read(refFile, tumorFile);
-    }
-
-    @NotNull
     private static Options createOptions() {
-        final Options options = new Options();
+        Options options = new Options();
+        options.addOption(METRICS_DIR, true, "Path towards the folder containing the metrics files.");
+        options.addOption(REF_SAMPLE, true, "ID of the reference sample of set.");
+        options.addOption(TUMOR_SAMPLE, true, "ID of the tumor sample of set.");
+
         options.addOption(DB_USER, true, "Database user name.");
         options.addOption(DB_PASS, true, "Database password.");
         options.addOption(DB_URL, true, "Database url.");
-        options.addOption(RUN_DIR, true, "Path towards the folder containing patient runs.");
-        return options;
-    }
 
-    @NotNull
-    private static CommandLine createCommandLine(@NotNull final String[] args, @NotNull final Options options) throws ParseException {
-        final CommandLineParser parser = new DefaultParser();
-        return parser.parse(options, args);
+        return options;
     }
 }
