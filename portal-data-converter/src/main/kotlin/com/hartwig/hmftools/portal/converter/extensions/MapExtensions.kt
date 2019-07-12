a4fb4962fa06d74130c197e8a0cd5bab30ff6380
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-07-12 10:52:29 +0200

    DEV-813: Remove portal data converter project

--- portal-data-converter/src/main/kotlin/com/hartwig/hmftools/portal/converter/extensions/MapExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.hartwig.hmftools.portal.converter.extensions
-
-import com.hartwig.hmftools.portal.converter.DEFAULT_VALUE
-
-inline fun <reified T : Enum<T>> Map<T, String>.toRecord(): List<String> {
-    return enumValues<T>().map { this[it] ?: DEFAULT_VALUE }
-}
