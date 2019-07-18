cc69f879e356e2bc99df1e1d9074ff0bb6288e01
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-07-18 16:23:18 +0200

    Bump patient reporter version

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
@@ -37,7 +37,7 @@ public class PatientReporterApplication {
     public static final String VERSION = PatientReporterApplication.class.getPackage().getImplementationVersion();
 
     // Uncomment this line when generating an example report using PDFWriterTest
-    //        public static final String VERSION = "7.1";
+    //        public static final String VERSION = "7.2";
 
     // General params needed for every report
     private static final String TUMOR_SAMPLE = "tumor_sample";
