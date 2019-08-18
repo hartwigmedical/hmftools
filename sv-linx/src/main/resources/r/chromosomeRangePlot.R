420942f1ad19adf069bc552025b87c5cbb3d7acc
Jonathan Baber <jonbaber@gmail.com>
2019-08-19 09:14:54 +1000

    Add chromosomal ranges

--- sv-linx/src/main/resources/r/chromosomeRangePlot.R
+++ sv-linx/src/main/resources/r/chromosomeRangePlot.R
@@ -5,17 +5,19 @@ library(ggplot2)
 library(cowplot)
 
 #chromosomeHeight = 150
-#circosFontsize = 43
-#circosPicturePath = "~/hmf/analysis/fusions/plot/JONTEST.png"
-#bandsPath = "/Users/jon/hmf/analysis/fusions/data/cytoBand.txt"
-#chromosomePath = "/Users/jon/hmf/analysis/fusions/data/COLO829T.cluster27.COMPLEX.sv8.chromosome.circos"
+#chromosomeFontsize = 70
+#circosPicturePath = "~/hmf/analysis/fusions/plot/COLO829T.cluster27.COMPLEX.sv8.png"
+#bandsPath = "/Users/jon/hmf/analysis/fusions/data/COLO829T.cluster27.COMPLEX.sv8.cytoBand.txt"
+#chromosomeRangePath = "/Users/jon/hmf/analysis/fusions/data/COLO829T.cluster27.COMPLEX.sv8.chromosome.circos"
 
 args <- commandArgs(trailing=T)
-chromosomePath <- args[1]
+chromosomeRangePath <- args[1]
 bandsPath <- args[2]
 circosPicturePath <- args[3]
-circosFontsize <- as.numeric(args[4])
+chromosomeFontsize <- as.numeric(args[4])
 chromosomeHeight <- as.numeric(args[5])
+chromosomeColumns <- as.numeric(args[6])
+circosWidth = 3000
   
 doPlot <- function(contig) {
   start = chromosomeRanges[chromosomeRanges$chromosome == contig, "start"]
@@ -24,14 +26,14 @@ doPlot <- function(contig) {
   label = chromosomeRanges[chromosomeRanges$chromosome == contig, "label"]
   chrColor = chromosomeRanges[chromosomeRanges$chromosome == contig, "chrColor"]
   
-  itrack <- IdeogramTrack(genome="X",chromosome=chr, name=label,bands=bands, ucscChromosomeNames=FALSE, 
-                          fontsize = circosFontsize, fontcolor = "black", outline = T, min.height = 1, min.width = 1, fill = chrColor, col = chrColor, lwd = 10)
-  plotTracks(itrack, from = start, to = end, add=TRUE, panel.only = T)
+  itrack <- IdeogramTrack(genome="X",chromosome=chr, name=label, bands=bands, ucscChromosomeNames=FALSE,
+                          fontsize = chromosomeFontsize, fontcolor = "black",  fill = chrColor, col = chrColor, lwd = 10)
+  plotTracks(itrack, from = start, to = end, add=TRUE, panel.only = F)
 }
 
 pCircos <- ggdraw() + draw_image(circosPicturePath)
 bands <- read.table(bandsPath, h = T)
-chromosomeRanges <- read.table(chromosomePath, h = F, sep = "\t", comment = "!", stringsAsFactors = F)
+chromosomeRanges <- read.table(chromosomeRangePath, h = F, sep = "\t", comment = "!", stringsAsFactors = F)
 names(chromosomeRanges) <- c("chromosome", "start", "end", "chrColor")
 chromosomeRanges = chromosomeRanges %>% mutate(label = paste0("chr ", chromosome))
 
@@ -43,7 +45,7 @@ chromosomeLengths = bands %>%
   ungroup() %>%
   mutate(relLength = length / max(length)) %>%
   arrange(-relLength) %>%
-  mutate(row = (row_number() - 1) %/% 4 + 1) %>%
+  mutate(row = (row_number() - 1) %/% chromosomeColumns + 1) %>%
   group_by(row) %>%
   mutate(rowLength = sum(relLength)) %>%
   ungroup() %>%
@@ -56,9 +58,13 @@ chromosomeLengths = bands %>%
   ungroup() %>%
   mutate(height = 1 / max(row), y = (max(row) - row) * height) %>%
   select(chromosome, row, length, x, y, width, height)
-chromosomeLengths = data.frame(chromosomeLengths)  
+chromosomeLengths = data.frame(chromosomeLengths)
 
-png(file = circosPicturePath, width = 3000, height = chromosomeHeight, units = "px")
+# Height is per row
+chromosomeHeight = chromosomeHeight * max(chromosomeLengths$row)
+
+#circosPicturePath = "~/hmf/analysis/fusions/plot/COLO829T.cluster27.COMPLEX.sv8.out.png"
+png(file = circosPicturePath, width = circosWidth, height = chromosomeHeight, units = "px")
 for (i in 1:nrow(chromosomeLengths)) {
   x = chromosomeLengths[i, "x"]
   y = chromosomeLengths[i, "y"]
@@ -66,7 +72,7 @@ for (i in 1:nrow(chromosomeLengths)) {
   height = chromosomeLengths[i, "height"]
   contig = chromosomeLengths[i, "chromosome"]
    
-  pushViewport(viewport(just = c("left","bottom"), x = x + 0.01, y = y, width = width - 0.02, height = height))
+  pushViewport(viewport(just = c("left","bottom"), x = x, y = y, width = width, height = height))
   doPlot(contig)
   popViewport(1)
 }
@@ -74,5 +80,5 @@ dev.off()
 
 
 pChr <- ggdraw() + draw_image(circosPicturePath)
-pCombined = plot_grid(pChr, pCircos, ncol = 1, rel_heights = c(chromosomeHeight, 3000))
-ggsave(circosPicturePath, pCombined, width = 3000/300.0, height = (3000 + chromosomeHeight)/300.0, units = "in", dpi = 300)
+pCombined = plot_grid(pChr, pCircos, ncol = 1, rel_heights = c(chromosomeHeight - 0.05 * chromosomeHeight, circosWidth))
+ggsave(circosPicturePath, pCombined, width = circosWidth/300.0, height = (circosWidth + chromosomeHeight)/300.0, units = "in", dpi = 300)
