d5b1d61b68a5c87b987704ed711fe72109d3f42f
Charles Shale <shalecharles@gmail.com>
2019-09-11 14:07:46 +1000

    Linx: switched to read vis_fusion file instead of old detailed fusions file

--- sv-linx/src/main/java/com/hartwig/hmftools/linx/visualiser/data/Fusions.java
+++ sv-linx/src/main/java/com/hartwig/hmftools/linx/visualiser/data/Fusions.java
@@ -5,77 +5,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.hartwig.hmftools.linx.visualiser.file.VisFusionFile;
 
 import org.jetbrains.annotations.NotNull;
 
 public class Fusions
 {
-    private static final String SEPARATOR = ",";
-
     @NotNull
     public static List<Fusion> fromFile(@NotNull final String fileName) throws IOException
     {
-        return fromLines(Files.readAllLines(new File(fileName).toPath()));
-    }
+        final List<VisFusionFile> visFusions = VisFusionFile.read(fileName);
 
-    @NotNull
-    private static List<Fusion> fromLines(@NotNull final List<String> lines)
-    {
-        final List<Fusion> result = Lists.newArrayList();
-
-        if (!lines.isEmpty()) {
-            final Map<String, Integer> map = columnMap(lines.get(0));
-
-            for (int i = 1; i < lines.size(); i++) {
-                Fusion fusion = fromLine(map, lines.get(i));
-                if (fusion.reportable()) {
-                    result.add(fusion);
-                }
-            }
-        }
-
-        return result;
-    }
-
-    @NotNull
-    private static Map<String, Integer> columnMap(@NotNull final String header)
-    {
-        final Map<String, Integer> result = Maps.newHashMap();
-        final String[] values = header.split(SEPARATOR);
-
-        for (int i = 0; i < values.length; i++)
-        {
-            result.put(values[i], i);
-        }
-
-        return result;
-
-    }
-
-    private static Fusion fromLine(@NotNull final Map<String, Integer> map, @NotNull final String line)
-    {
-        String[] values = line.split(SEPARATOR);
-        return ImmutableFusion.builder()
-                .sampleId(values[map.get("SampleId")])
-                .reportable(Boolean.valueOf(values[map.get("Reportable")]))
-                .clusterId(Integer.valueOf(values[map.get("ClusterId")]))
-                .chromosomeUp(values[map.get("ChrUp")])
-                .positionUp(Long.valueOf(values[map.get("PosUp")]))
-                .geneUp(values[map.get("GeneNameUp")])
-                .strandUp(Integer.valueOf(values[map.get("StrandUp")]))
-                .regionTypeUp(values[map.get("RegionTypeUp")])
-                .fusedExonUp(Integer.valueOf(values[map.get("FusedExonUp")]))
-                .chromosomeDown(values[map.get("ChrDown")])
-                .positionDown(Long.valueOf(values[map.get("PosDown")]))
-                .geneDown(values[map.get("GeneNameDown")])
-                .strandDown(Integer.valueOf(values[map.get("StrandDown")]))
-                .regionTypeDown(values[map.get("RegionTypeDown")])
-                .fusedExonDown(Integer.valueOf(values[map.get("FusedExonDown")]))
-                .transcriptUp(values[map.get("TranscriptUp")])
-                .transcriptDown(values[map.get("TranscriptDown")])
-                .build();
+        return visFusions.stream().map(x -> ImmutableFusion.builder()
+                .sampleId(x.SampleId)
+                .reportable(x.Reportable)
+                .clusterId(x.ClusterId)
+                .geneUp(x.GeneNameUp)
+                .transcriptUp(x.TranscriptUp)
+                .chromosomeUp(x.ChrUp)
+                .positionUp(x.PosUp)
+                .strandUp(x.StrandUp)
+                .regionTypeUp(x.RegionTypeUp)
+                .fusedExonUp(x.FusedExonUp)
+                .geneDown(x.GeneNameDown)
+                .transcriptDown(x.TranscriptDown)
+                .chromosomeDown(x.ChrDown)
+                .positionDown(x.PosDown)
+                .strandDown(x.StrandDown)
+                .regionTypeDown(x.RegionTypeDown)
+                .fusedExonDown(x.FusedExonDown)
+                .build()).collect(Collectors.toList());
     }
 }
