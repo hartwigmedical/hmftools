50418e9b8583a29aaf5349cd4e4de37a2d5168e0
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-10-10 11:43:28 +0200

    Formatting changes to chord test

--- hmf-common/src/test/java/com/hartwig/hmftools/common/chord/ChordFileReaderTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/chord/ChordFileReaderTest.java
@@ -13,21 +13,19 @@ public class ChordFileReaderTest {
     private static final double EPSILON = 1.0E-10;
 
     private static final double NONE = 0.850;
-
     private static final double BRCA1 = 0.230;
     private static final double BRCA2 = 0.400;
     private static final double HRD = 0.630;
-    private static final boolean PREDICTEDRESPONSE = true;
+    private static final boolean PREDICTED_RESPONSE = true;
 
     @Test
     public void loadChordFile() throws IOException {
         ChordAnalysis chordAnalysis = ChordFileReader.read(FILE);
 
-        assertEquals(BRCA1, chordAnalysis.BRCA1Value(), EPSILON);
         assertEquals(NONE, chordAnalysis.noneValue(), EPSILON);
+        assertEquals(BRCA1, chordAnalysis.BRCA1Value(), EPSILON);
         assertEquals(BRCA2, chordAnalysis.BRCA2Value(), EPSILON);
         assertEquals(HRD, chordAnalysis.hrdValue(), EPSILON);
-        assertEquals(PREDICTEDRESPONSE, chordAnalysis.predictedResponseValue());
-
+        assertEquals(PREDICTED_RESPONSE, chordAnalysis.predictedResponseValue());
     }
 }
