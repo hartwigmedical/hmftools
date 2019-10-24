c367f2caab95825c7dc1f687c817c60e3e62d8bd
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-10-24 17:37:14 +0200

    DEV-950: update gene disruption for colo

--- patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/ExampleAnalysisTestFactory.java
+++ patient-reporter/src/test/java/com/hartwig/hmftools/patientreporter/ExampleAnalysisTestFactory.java
@@ -724,7 +724,7 @@ public final class ExampleAnalysisTestFactory {
                 .range("Intron 5 -> Intron 6")
                 .type("DEL")
                 .ploidy(2D)
-                .undisruptedCopyNumber(0.5)
+                .undisruptedCopyNumber(0)
                 .build();
 
         return Lists.newArrayList(disruption1);
