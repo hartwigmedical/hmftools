d2aac186c11f7c5e1253646df54660d0499dbd7e
Jonathan Baber <jonbaber@gmail.com>
2019-10-28 09:42:47 +1000

    Read amber sites from VCF

---
+++ amber/src/main/java/com/hartwig/hmftools/amber/site/AmberSite.java
@@ -0,0 +1,17 @@
+package com.hartwig.hmftools.amber.site;
+
+import com.hartwig.hmftools.common.position.GenomePosition;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public interface AmberSite extends GenomePosition {
+    @NotNull
+    String ref();
+
+    @NotNull
+    String alt();
+}
