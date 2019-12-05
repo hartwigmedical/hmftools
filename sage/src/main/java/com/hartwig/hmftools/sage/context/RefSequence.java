8f4cfc88c6dd68a1de8b930d9a329f85c5a41b07
Jonathan Baber <jonbaber@gmail.com>
2019-12-05 10:53:05 +1000

    Get additional ref genome

--- sage/src/main/java/com/hartwig/hmftools/sage/context/RefSequence.java
+++ sage/src/main/java/com/hartwig/hmftools/sage/context/RefSequence.java
@@ -1,17 +1,22 @@
 package com.hartwig.hmftools.sage.context;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 
 import com.hartwig.hmftools.common.genome.region.GenomeRegion;
 import com.hartwig.hmftools.sage.read.IndexedBases;
 
 import org.jetbrains.annotations.NotNull;
 
+import htsjdk.samtools.CigarElement;
+import htsjdk.samtools.CigarOperator;
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.reference.IndexedFastaSequenceFile;
 import htsjdk.samtools.reference.ReferenceSequence;
 
 public class RefSequence {
+
+    private static final EnumSet<CigarOperator> EXTEND_START = EnumSet.of(CigarOperator.I, CigarOperator.S);
 
     private static final int BUFFER = 1000;
     private final int sequenceLength;
@@ -36,12 +41,19 @@ public class RefSequence {
     public IndexedBases alignment(final SAMRecord record) {
 
         int alignmentStart = record.getAlignmentStart();
+        int additionalBases = 0;
+        int cigarLength = 0;
 
-        int refPositionStart = record.getCigar().isLeftClipped()
-                ? Math.max(1, alignmentStart - record.getCigar().getCigarElement(0).getLength())
-                : alignmentStart;
+        for (final CigarElement cigarElement : record.getCigar().getCigarElements()) {
+            if (EXTEND_START.contains(cigarElement.getOperator())) {
+                additionalBases += cigarElement.getLength();
+            }
+            cigarLength += cigarElement.getLength();
+        }
 
-        int alignmentEnd = Math.max(record.getAlignmentEnd(), alignmentStart + record.getReadLength());
+        int refPositionStart = Math.max(1, alignmentStart - additionalBases);
+
+        int alignmentEnd = Math.max(alignmentStart + cigarLength, alignmentStart + record.getReadLength());
         int alignmentStartIndex = alignmentStart - refPositionStart;
 
         return new IndexedBases(alignmentStart, alignmentStartIndex, alignment(refPositionStart, Math.min(alignmentEnd, sequenceLength)));
