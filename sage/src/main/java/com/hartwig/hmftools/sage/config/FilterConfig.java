ac8e7f3144f376a212e04857bf0f3e575dbb9a55
Jonathan Baber <jonbaber@gmail.com>
2019-12-11 07:14:27 +1000

    min core distance

--- sage/src/main/java/com/hartwig/hmftools/sage/config/FilterConfig.java
+++ sage/src/main/java/com/hartwig/hmftools/sage/config/FilterConfig.java
@@ -16,6 +16,7 @@ public interface FilterConfig {
     String HARD_FILTER = "hard_filter";
     String HARD_MIN_TUMOR_QUAL = "hard_min_tumor_qual";
     String HARD_MIN_TUMOR_ALT_SUPPORT = "hard_min_tumor_alt_support";
+    String MIN_GERMLINE_VAF = "min_germline_vaf";
 
     int DEFAULT_HARD_MIN_TUMOR_QUAL = 1;
     int DEFAULT_HARD_MIN_TUMOR_ALT_SUPPORT = 2;
@@ -70,6 +71,10 @@ public interface FilterConfig {
 
     default int hotspotMinTumorReadContextSupportToSkipQualCheck() {
         return 5;
+    }
+
+    default double minGermlineVaf() {
+        return 0.1;
     }
 
     @NotNull
