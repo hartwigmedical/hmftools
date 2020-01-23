12e0bed59591bb628ab3b2161d60cd431debbb05
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2020-01-23 11:43:47 +0100

    Formatting

--- hmf-common/src/main/java/com/hartwig/hmftools/common/actionability/EvidenceLevel.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/actionability/EvidenceLevel.java
@@ -17,7 +17,6 @@ public enum EvidenceLevel {
     EvidenceLevel(@NotNull final String readableString, final boolean includeForReporting) {
         this.readableString = readableString;
         this.includeForReporting = includeForReporting;
-
     }
 
     @NotNull
