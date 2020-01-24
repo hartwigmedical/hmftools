dbfebcc7723a970f6627cc3bb6be994f67c909f1
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2020-01-24 17:27:02 +0100

    DEV-1113: Add writing to file

--- iclusion-importer/src/main/java/com/hartwig/hmftools/iclusion/io/IclusionTrialFile.java
+++ iclusion-importer/src/main/java/com/hartwig/hmftools/iclusion/io/IclusionTrialFile.java
@@ -7,18 +7,27 @@ import java.util.List;
 import java.util.StringJoiner;
 
 import com.google.common.collect.Lists;
+import com.hartwig.hmftools.iclusion.data.IclusionMutation;
 import com.hartwig.hmftools.iclusion.data.IclusionTrial;
+import com.hartwig.hmftools.iclusion.data.IclusionTumorLocation;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public final class IclusionTrialFile {
 
+    private static final Logger LOGGER = LogManager.getLogger(IclusionTrialFile.class);
+
+    private static final String MAIN_FIELD_DELIMITER = "\t";
+    private static final String SUB_FIELD_DELIMITER = "|";
+    private static final String SUB_FIELD_SEPARATOR = " - ";
+    private static final String DOID_DELIMITER = ",";
+
     private IclusionTrialFile() {
     }
 
-    private static final String DELIMITER = "\t";
-
-    public void write(@NotNull String iClusionTrialsTsv, @NotNull List<IclusionTrial> trials) throws IOException {
+    public static void write(@NotNull String iClusionTrialsTsv, @NotNull List<IclusionTrial> trials) throws IOException {
         Files.write(new File(iClusionTrialsTsv).toPath(), toLines(trials));
     }
 
@@ -32,7 +41,7 @@ public final class IclusionTrialFile {
 
     @NotNull
     private static String header() {
-        return new StringJoiner(DELIMITER, "", "").add("id")
+        return new StringJoiner(MAIN_FIELD_DELIMITER, "", "").add("id")
                 .add("acronym")
                 .add("title")
                 .add("eudra")
@@ -46,15 +55,56 @@ public final class IclusionTrialFile {
 
     @NotNull
     private static String toString(@NotNull IclusionTrial trial) {
-        return new StringJoiner(DELIMITER).add(trial.id())
+        return new StringJoiner(MAIN_FIELD_DELIMITER).add(trial.id())
                 .add(trial.acronym())
                 .add(trial.title())
                 .add(trial.eudra())
                 .add(trial.nct())
                 .add(trial.ipn())
                 .add(trial.ccmo())
-                .add("locations")
-                .add("mutations")
+                .add(tumorLocationsToString(trial.tumorLocations()))
+                .add(mutationsToString(trial.mutations()))
                 .toString();
+    }
+
+    @NotNull
+    private static String tumorLocationsToString(@NotNull List<IclusionTumorLocation> tumorLocations) {
+        StringJoiner tumorLocationString = new StringJoiner(SUB_FIELD_DELIMITER);
+
+        for (IclusionTumorLocation tumorLocation : tumorLocations) {
+            StringJoiner doidString = new StringJoiner(DOID_DELIMITER);
+            for (String doid : tumorLocation.doids()) {
+                if (!containsInvalidString(doid, DOID_DELIMITER)) {
+                    doidString.add(doid);
+                }
+            }
+
+            if (!containsInvalidString(tumorLocation.primaryTumorLocation(), SUB_FIELD_DELIMITER)
+                    && !containsInvalidString(tumorLocation.primaryTumorLocation(), SUB_FIELD_SEPARATOR)) {
+                tumorLocationString.add(tumorLocation.primaryTumorLocation() + SUB_FIELD_SEPARATOR + doidString.toString());
+            }
+        }
+        return tumorLocationString.toString();
+    }
+
+    @NotNull
+    private static String mutationsToString(@NotNull List<IclusionMutation> mutations) {
+        StringJoiner mutationString = new StringJoiner(SUB_FIELD_DELIMITER);
+
+        for (IclusionMutation mutation : mutations) {
+            if (!containsInvalidString(mutation.gene(), SUB_FIELD_SEPARATOR) && !containsInvalidString(mutation.name(), SUB_FIELD_SEPARATOR)) {
+                mutationString.add(mutation.gene() + SUB_FIELD_SEPARATOR + mutation.name());
+            }
+        }
+        return mutationString.toString();
+    }
+
+    private static boolean containsInvalidString(@NotNull String stringToCheck, @NotNull String invalidString) {
+        if (stringToCheck.contains(invalidString)) {
+            LOGGER.error("{} found in primary tumor location '{}'. Cannot serialize", invalidString, stringToCheck);
+            return true;
+        }
+
+        return false;
     }
 }
