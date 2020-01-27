a56bb5f09ffe25022c4dbaeae023a1d8a64953da
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-01-27 16:10:49 +0100

    DEV-1118: add conclusion for amplifications and deletions

--- protect/src/main/java/com/hartwig/hmftools/protect/conclusion/ConclusionFactory.java
+++ protect/src/main/java/com/hartwig/hmftools/protect/conclusion/ConclusionFactory.java
@@ -4,6 +4,7 @@ import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 
+import com.hartwig.hmftools.common.purple.copynumber.CopyNumberInterpretation;
 import com.hartwig.hmftools.common.variant.msi.MicrosatelliteStatus;
 import com.hartwig.hmftools.common.variant.structural.annotation.ReportableGeneFusion;
 import com.hartwig.hmftools.common.variant.tml.TumorMutationalStatus;
@@ -77,6 +78,31 @@ public class ConclusionFactory {
                 }
             }
         } else if (geneCopyNumbers.size() >= 1) {
+            for (ReportableGainLoss gainLoss : geneCopyNumbers) {
+                String interpetation = Strings.EMPTY;
+                if (gainLoss.interpretation() == CopyNumberInterpretation.FULL_LOSS
+                        || gainLoss.interpretation() == CopyNumberInterpretation.PARTIAL_LOSS) {
+                    interpetation = "loss";
+                } else if (gainLoss.interpretation() == CopyNumberInterpretation.GAIN) {
+                    interpetation = "amplification";
+                }
+
+                for (Map.Entry<String, TemplateConclusion> entry : MapTemplateConclusion.entrySet()) {
+                    String keyTemplate = entry.getKey().toLowerCase();
+                    TemplateConclusion templateConclusion = entry.getValue();
+
+                    if (interpetation.equals("loss")) {
+                        if (keyTemplate.contains(gainLoss.gene()) && keyTemplate.contains(interpetation)) {
+                            String sentenceConclusion = templateConclusion.summaryTextStatement();
+                            conclusion.append(startRow).append(sentenceConclusion).append(enter);
+                        }
+                    } else if (interpetation.equals("amplification") && keyTemplate.contains(interpetation)) {
+                        String sentenceConclusion = templateConclusion.summaryTextStatement();
+                        conclusion.append(startRow).append(sentenceConclusion).append(enter);
+
+                    }
+                }
+            }
 
         } else if (reportableHomozygousDisruptions.size() >= 1) {
 
