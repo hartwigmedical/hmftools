4ebabd9a554e3a2da1212eca3b2f9d9e75a47791
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-01-31 13:38:34 +0100

    DEV-1126: refactor

--- iclusion-importer/src/main/java/com/hartwig/hmftools/iclusion/api/IclusionApiObjectMapper.java
+++ iclusion-importer/src/main/java/com/hartwig/hmftools/iclusion/api/IclusionApiObjectMapper.java
@@ -15,7 +15,6 @@ import com.hartwig.hmftools.iclusion.io.IclusionTrialFile;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
