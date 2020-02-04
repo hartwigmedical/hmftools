85843552aeb74b5802a6c590421fc1a56e571955
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2020-02-04 13:54:30 +0100

    DEV-1112: New project for knowledgebase generator

---
+++ knowledgebase-generator/src/main/java/com/hartwig/hmftools/knowledgebasegenerator/KnowledgebaseGeneratorApplication.java
@@ -0,0 +1,8 @@
+package com.hartwig.hmftools.knowledgebasegenerator;
+
+public class KnowledgebaseGeneratorApplication {
+
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
