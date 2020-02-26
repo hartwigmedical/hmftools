84eca785283a1ed3630d4a83df28db462491f031
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-02-26 13:17:39 +0100

    DEV-1150: fix knowledgebase specific object

--- knowledgebase-generator/src/main/java/com/hartwig/hmftools/knowledgebasegenerator/eventtype/EventTypeAnalyzer.java
+++ knowledgebase-generator/src/main/java/com/hartwig/hmftools/knowledgebasegenerator/eventtype/EventTypeAnalyzer.java
@@ -46,45 +46,58 @@ public class EventTypeAnalyzer {
                         }
                     }
                     LOGGER.info("event oncokb: " + event);
+                    break;
                 case "cgi": // extract info for cgi
                     Cgi kbCgi = (Cgi) kbSpecificObject;
                     event = feature.biomarkerType();
                     LOGGER.info("event cgi: " + event);
+                    break;
                 case "brca": // extract info for brca
                     Brca kbBrca = (Brca) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 case "civic": // extract info for civic
                     Civic kbCivic = (Civic) kbSpecificObject;
                     event = feature.biomarkerType();
                     LOGGER.info("event civic: " + event);
                     // TODO: extract event type
+                    break;
                 case "jax": // extract info for jax
                     Jax kbJax = (Jax) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 case "jax_trials": // extract info for jax trials
                     JaxTrials kbJaxTrials = (JaxTrials) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 case "molecularmatch": // extract info for molecular match
                     MolecularMatch kbMolecularMatch = (MolecularMatch) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 case "molecularmatch_trials": // extract info for molecular match trials
                     MolecularMatchTrials kbMolecularMatchTrials = (MolecularMatchTrials) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 case "pmkb": // extract info for pmkb
                     Pmkb kbPmkb = (Pmkb) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 case "sage": // extract info for sage
                     Sage kbSage = (Sage) kbSpecificObject;
                     event = Strings.EMPTY;
                     // TODO: extract event type
+                    break;
                 default:
                     LOGGER.info("Unknown knowledgebase");
+            }
+            if (event == null) {
+                event = Strings.EMPTY;
             }
             eventType.add(ImmutableEventType.builder().gene("").eventType(event).build());
         }
