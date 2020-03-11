94d2056b9a4dd4265c0a4b2661c482fc7a38fa5f
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2020-03-11 16:08:23 +0100

    DEV-1136: Add test for trinucleotide lookup

--- knowledgebase-generator/src/test/java/com/hartwig/hmftools/knowledgebasegenerator/util/AminoAcidLookupTest.java
+++ knowledgebase-generator/src/test/java/com/hartwig/hmftools/knowledgebasegenerator/util/AminoAcidLookupTest.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
+import com.hartwig.hmftools.common.genome.region.Strand;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
@@ -31,7 +32,7 @@ public class AminoAcidLookupTest {
         }
     }
 
-    private int aminoAcidCount(@NotNull Map<String, Set<String>> aminoAcidToTrinucleotideMap, @NotNull String trinucleotide) {
+    private static int aminoAcidCount(@NotNull Map<String, Set<String>> aminoAcidToTrinucleotideMap, @NotNull String trinucleotide) {
         int count = 0;
         for (Map.Entry<String, Set<String>> entry : aminoAcidToTrinucleotideMap.entrySet()) {
             if (entry.getValue().contains(trinucleotide)) {
@@ -39,5 +40,24 @@ public class AminoAcidLookupTest {
             }
         }
         return count;
+    }
+
+    @Test
+    public void canLookupTrinucleotides() {
+        // Serine (S)
+        assertEquals(6, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("TCT", Strand.FORWARD).size());
+        assertEquals(6, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("TCT", Strand.REVERSE).size());
+
+        // Valine (V)
+        assertEquals(4, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("GTC", Strand.FORWARD).size());
+        assertEquals(4, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("CTG", Strand.REVERSE).size());
+
+        // Tyrosine (Y)
+        assertEquals(2, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("TAC", Strand.FORWARD).size());
+        assertEquals(2, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("CAT", Strand.REVERSE).size());
+
+        // Does not exist -> no trinucleotides found!
+        assertEquals(0, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("???", Strand.FORWARD).size());
+        assertEquals(0, AminoAcidLookup.allTrinucleotidesForSameAminoAcid("???", Strand.REVERSE).size());
     }
 }
