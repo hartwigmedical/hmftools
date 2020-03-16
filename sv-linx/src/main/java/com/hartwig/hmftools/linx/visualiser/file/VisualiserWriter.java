9deb874386689453497e6fbc90fb01b8a64dd86f
Charles Shale <shalecharles@gmail.com>
2020-03-16 16:36:37 +1100

    Removed import

--- sv-linx/src/main/java/com/hartwig/hmftools/linx/visualiser/file/VisualiserWriter.java
+++ sv-linx/src/main/java/com/hartwig/hmftools/linx/visualiser/file/VisualiserWriter.java
@@ -3,7 +3,6 @@ package com.hartwig.hmftools.linx.visualiser.file;
 import static com.hartwig.hmftools.common.utils.io.FileWriterUtils.closeBufferedWriter;
 import static com.hartwig.hmftools.common.utils.io.FileWriterUtils.createBufferedWriter;
 import static com.hartwig.hmftools.linx.analysis.SvClassification.isFilteredResolvedType;
-import static com.hartwig.hmftools.linx.types.ChromosomeArm.CHROMOSOME_ARM_P;
 import static com.hartwig.hmftools.linx.types.ChromosomeArm.P_ARM;
 import static com.hartwig.hmftools.linx.types.ChromosomeArm.Q_ARM;
 import static com.hartwig.hmftools.linx.types.SvBreakend.DIRECTION_CENTROMERE;
