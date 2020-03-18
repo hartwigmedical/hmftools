d9103ac42fe2ff5c0eae7d1f7ce9df65e0142ec3
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-03-18 15:09:14 +0100

    DEV-1112: Create test for source and genomic events

---
+++ knowledgebase-generator/src/test/java/com/hartwig/hmftools/knowledgebasegenerator/eventtype/EventTypeAnalyzerTest.java
@@ -0,0 +1,24 @@
+package com.hartwig.hmftools.knowledgebasegenerator.eventtype;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EventTypeAnalyzerTest {
+
+    @Test
+    public void canDetermineEventsOfCGI() {
+
+    }
+
+    @Test
+    public void canDetermineEventsOfCIVIC() {
+
+    }
+
+    @Test
+    public void canDetermineEventsOfONCOKB() {
+
+    }
+
+}
