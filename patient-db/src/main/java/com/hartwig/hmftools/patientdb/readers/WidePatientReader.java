4f66290319458d1b09911f3df40466efda5c2b40
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-03-27 16:19:50 +0100

    DEV-1174: Create empty patient object voor WIDE timeline

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/WidePatientReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/WidePatientReader.java
@@ -2,8 +2,15 @@ package com.hartwig.hmftools.patientdb.readers;
 
 import java.util.List;
 
+import com.google.common.collect.Lists;
+import com.hartwig.hmftools.common.ecrf.formstatus.FormStatus;
 import com.hartwig.hmftools.patientdb.curators.TumorLocationCurator;
+import com.hartwig.hmftools.patientdb.data.BaselineData;
+import com.hartwig.hmftools.patientdb.data.CuratedTumorLocation;
+import com.hartwig.hmftools.patientdb.data.ImmutableBaselineData;
+import com.hartwig.hmftools.patientdb.data.ImmutablePreTreatmentData;
 import com.hartwig.hmftools.patientdb.data.Patient;
+import com.hartwig.hmftools.patientdb.data.PreTreatmentData;
 import com.hartwig.hmftools.patientdb.data.SampleData;
 import com.hartwig.hmftools.patientdb.readers.wide.WideEcrfModel;
 
@@ -25,9 +32,39 @@ public class WidePatientReader {
     @NotNull
     public Patient read(@NotNull String patientIdentifier, @Nullable String primaryTumorLocation,
             @NotNull List<SampleData> sequencedSamples) {
-        // TODO: Curate tumor location
         // TODO: Create the timeline based on the wideEcrfModel
         // TODO: Create patient object.
-        return null;
+        return new Patient(patientIdentifier,
+                toBaselineData(tumorLocationCurator.search(primaryTumorLocation)),
+                noPreTreatmentData(),
+                sequencedSamples,
+                Lists.newArrayList(),
+                Lists.newArrayList(),
+                Lists.newArrayList(),
+                Lists.newArrayList(),
+                Lists.newArrayList(),
+                Lists.newArrayList());
+    }
+
+    @NotNull
+    private static BaselineData toBaselineData(@NotNull CuratedTumorLocation curatedTumorLocation) {
+        return ImmutableBaselineData.of(null,
+                null,
+                null,
+                null,
+                null,
+                curatedTumorLocation,
+                null,
+                FormStatus.undefined(),
+                FormStatus.undefined(),
+                FormStatus.undefined(),
+                FormStatus.undefined(),
+                FormStatus.undefined(),
+                FormStatus.undefined());
+    }
+
+    @NotNull
+    private static PreTreatmentData noPreTreatmentData() {
+        return ImmutablePreTreatmentData.of(null, null, Lists.newArrayList(), FormStatus.undefined());
     }
 }
