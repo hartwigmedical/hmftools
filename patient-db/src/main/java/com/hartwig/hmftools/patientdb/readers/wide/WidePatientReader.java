1727aa1439c6704e98d7d2b7fac1c8112195d7c9
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-04-01 17:44:57 +0200

    DEV-1174: refactor

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/wide/WidePatientReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/readers/wide/WidePatientReader.java
@@ -144,20 +144,20 @@ public class WidePatientReader {
                     drugs.add(ImmutableDrugData.of(preTreatment.drug1(), null, drugsEndDate, null, curatedDrugs1));
                 }
 
-//                if (!preTreatment.drug2().isEmpty() || drugsEndDate != null) {
-//                    final List<CuratedDrug> curatedDrugs2 = treatmentCurator.search(preTreatment.drug2());
-//                    drugs.add(ImmutableDrugData.of(preTreatment.drug2(), null, drugsEndDate, null, curatedDrugs2));
-//                }
-//
-//                if (!preTreatment.drug3().isEmpty() || drugsEndDate != null) {
-//                    final List<CuratedDrug> curatedDrugs3 = treatmentCurator.search(preTreatment.drug3());
-//                    drugs.add(ImmutableDrugData.of(preTreatment.drug3(), null, drugsEndDate, null, curatedDrugs3));
-//                }
-//
-//                if (!preTreatment.drug4().isEmpty() || drugsEndDate != null) {
-//                    final List<CuratedDrug> curatedDrugs4 = treatmentCurator.search(preTreatment.drug4());
-//                    drugs.add(ImmutableDrugData.of(preTreatment.drug4(), null, drugsEndDate, null, curatedDrugs4));
-//                }
+                if (!preTreatment.drug2().isEmpty() || drugsEndDate != null) {
+                    final List<CuratedDrug> curatedDrugs2 = treatmentCurator.search(preTreatment.drug2());
+                    drugs.add(ImmutableDrugData.of(preTreatment.drug2(), null, drugsEndDate, null, curatedDrugs2));
+                }
+
+                if (!preTreatment.drug3().isEmpty() || drugsEndDate != null) {
+                    final List<CuratedDrug> curatedDrugs3 = treatmentCurator.search(preTreatment.drug3());
+                    drugs.add(ImmutableDrugData.of(preTreatment.drug3(), null, drugsEndDate, null, curatedDrugs3));
+                }
+
+                if (!preTreatment.drug4().isEmpty() || drugsEndDate != null) {
+                    final List<CuratedDrug> curatedDrugs4 = treatmentCurator.search(preTreatment.drug4());
+                    drugs.add(ImmutableDrugData.of(preTreatment.drug4(), null, drugsEndDate, null, curatedDrugs4));
+                }
             }
         }
         return drugs;
@@ -215,8 +215,8 @@ public class WidePatientReader {
         LocalDate drugsStartDate = treatmentData.startDate().isEmpty() ? null : createInterpretDate(treatmentData.startDate());
         LocalDate drugsEndDate = treatmentData.endDate().isEmpty() ? null : createInterpretDate(treatmentData.endDate());
 
-        if (drugName != null || drugsStartDate != null || drugsEndDate != null) {
-            final List<CuratedDrug> curatedDrugs = drugName == null ? Lists.newArrayList() : treatmentCurator.search(drugName);
+        if (!drugName.isEmpty() || drugsStartDate != null || drugsEndDate != null) {
+            final List<CuratedDrug> curatedDrugs = drugName.isEmpty() ? Lists.newArrayList() : treatmentCurator.search(drugName);
             drugs.add(ImmutableDrugData.of(drugName, drugsStartDate, drugsEndDate, null, curatedDrugs));
         }
         return drugs;
