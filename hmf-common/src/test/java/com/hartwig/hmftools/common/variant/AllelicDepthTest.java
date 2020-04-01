7589a6a8d12f4e236359b05a51ac7b04fca04bdc
Jonathan Baber <jonbaber@gmail.com>
2020-04-01 10:06:38 +1000

    Allelic Depth should use DP when available

---
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/variant/AllelicDepthTest.java
@@ -0,0 +1,28 @@
+package com.hartwig.hmftools.common.variant;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import htsjdk.variant.variantcontext.Genotype;
+import htsjdk.variant.variantcontext.GenotypeBuilder;
+
+public class AllelicDepthTest {
+
+    @Test
+    public void allelicDepthFromAd() {
+        final Genotype genotype = new GenotypeBuilder("SAMPLE").AD(new int[] { 6, 4 }).make();
+        final AllelicDepth victim = AllelicDepth.fromGenotype(genotype);
+        assertEquals(10, victim.totalReadCount());
+        assertEquals(0.4, victim.alleleFrequency(), 0.01);
+    }
+
+    @Test
+    public void allelicDepthFromDpIfAvailable() {
+        final Genotype genotype = new GenotypeBuilder("SAMPLE").AD(new int[] { 6, 4 }).DP(20).make();
+        final AllelicDepth victim = AllelicDepth.fromGenotype(genotype);
+        assertEquals(20, victim.totalReadCount());
+        assertEquals(0.2, victim.alleleFrequency(), 0.01);
+    }
+
+}
