ee89e6cedfb650bfb3f1d4e1ca3b0f1b53f11e7b
Jonathan Baber <jonbaber@gmail.com>
2020-04-06 14:11:15 +1000

    Remove logging

--- sage/src/main/java/com/hartwig/hmftools/sage/quality/QualityCounterFactory.java
+++ sage/src/main/java/com/hartwig/hmftools/sage/quality/QualityCounterFactory.java
@@ -8,8 +8,6 @@ import com.hartwig.hmftools.common.genome.region.GenomeRegion;
 import com.hartwig.hmftools.sage.ref.RefSequence;
 import com.hartwig.hmftools.sage.sam.SamSlicer;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import htsjdk.samtools.SamReader;
@@ -18,8 +16,6 @@ import htsjdk.samtools.cram.ref.ReferenceSource;
 import htsjdk.samtools.reference.ReferenceSequenceFile;
 
 class QualityCounterFactory {
-
-    private static final Logger LOGGER = LogManager.getLogger(QualityCounterFactory.class);
 
     private final String bamFile;
     private final ReferenceSequenceFile refGenome;
@@ -33,8 +29,6 @@ class QualityCounterFactory {
 
     @NotNull
     public Collection<QualityCounter> regionCount(@NotNull final GenomeRegion bounds) {
-
-        LOGGER.info("Processing region {}", bounds);
 
         final RefSequence refSequence = new RefSequence(bounds, refGenome);
         final QualityCounterCigarHandler counter = new QualityCounterCigarHandler(refSequence, bounds, maxAltCount);
