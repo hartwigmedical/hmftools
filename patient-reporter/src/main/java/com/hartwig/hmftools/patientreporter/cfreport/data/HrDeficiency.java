282fd25e0a237b2672e74c774fa1af83e26b6c46
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-05-20 10:36:36 +0200

    DEV-1179: fix issue barplot HRD

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/data/HrDeficiency.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/data/HrDeficiency.java
@@ -2,7 +2,9 @@ package com.hartwig.hmftools.patientreporter.cfreport.data;
 
 public final class HrDeficiency {
 
-    public static final double RANGE_MIN = 0;
+    public static final double RANGE_MIN_BARPLOT_NO_UNDERSHOOT = 0;
+    public static final double RANGE_MIN_BARPLOT = 0.1;
+    public static final double RANGE_MIN = 0.0001;
     public static final double RANGE_MAX = 1;
 
     private HrDeficiency() {
