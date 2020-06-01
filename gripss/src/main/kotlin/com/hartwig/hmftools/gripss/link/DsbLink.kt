c037ce058d3ff7986e42bd3d511d4a05d7747960
Jonathan Baber <jonbaber@gmail.com>
2020-06-01 10:17:19 +1000

    Speed up dsb linking

--- gripss/src/main/kotlin/com/hartwig/hmftools/gripss/link/DsbLink.kt
+++ gripss/src/main/kotlin/com/hartwig/hmftools/gripss/link/DsbLink.kt
@@ -5,19 +5,23 @@ import com.hartwig.hmftools.gripss.store.LinkStore
 import com.hartwig.hmftools.gripss.store.VariantStore
 import java.util.*
 import kotlin.collections.HashMap
+import kotlin.math.max
 
-class DsbLink(private val duplicates: Set<String>, private val variantStore: VariantStore, private val assemblyLinkStore: LinkStore) {
+class DsbLink(private val duplicates: Set<String>, private val variants: List<StructuralVariantContext>, private val assemblyLinkStore: LinkStore) {
 
     companion object {
         const val MAX_DSB_DISTANCE = 30
+        const val MAX_FIND_DISTANCE = 1000
 
         operator fun invoke(variantStore: VariantStore, assemblyLinkStore: LinkStore, duplicates: Set<String>): LinkStore {
 
             val linkByVariant = HashMap<String, Link>()
-            val dsbLink = DsbLink(duplicates, variantStore, assemblyLinkStore)
-            for (variant in variantStore.selectAll()) {
+            val variants = variantStore.selectAll()
+            val dsbLink = DsbLink(duplicates, variants, assemblyLinkStore)
+            for (index in variants.indices) {
+                val variant = variants[index]
                 if (!linkByVariant.containsKey(variant.vcfId) && !duplicates.contains(variant.vcfId)) {
-                    val links = dsbLink.dsbLinks(linkByVariant.size / 2 + 1, variant)
+                    val links = dsbLink.dsbLinks(linkByVariant.size / 2 + 1, index)
                     links.forEach { x -> linkByVariant[x.vcfId] = x }
                 }
             }
@@ -25,29 +29,58 @@ class DsbLink(private val duplicates: Set<String>, private val variantStore: Var
         }
     }
 
-    fun dsbLinks(i: Int, variant: StructuralVariantContext): List<Link> {
-        val nearby = variantStore.selectDoubleStrandedBreaks(variant)
+    fun dsbLinks(linkId: Int, variantIndex: Int): List<Link> {
+        val variant = variants[variantIndex]
+        val nearby = findNearbyIndices(variantIndex)
         if (nearby.size == 1) {
-            val other = nearby[0]
-            val linkIsSymmetric = variantStore.selectDoubleStrandedBreaks(other).size == 1
+            val otherIndex = nearby[0]
+            val linkIsSymmetric = findNearbyIndices(otherIndex).size == 1
             if (!linkIsSymmetric) {
                 return Collections.emptyList()
             }
 
-            val alreadyLinked = assemblyLinkStore.linkedVariants(variant.vcfId).filter { x -> x.otherVcfId == other.vcfId }.isNotEmpty()
+            val other = variants[otherIndex]
+            val alreadyLinked = assemblyLinkStore.linkedVariants(variant.vcfId).any { x -> x.otherVcfId == other.vcfId }
             if (alreadyLinked) {
                 return Collections.emptyList()
             }
 
-            return listOf(Link("dsb$i", Pair(variant, other)), Link("dsb$i", Pair(other, variant)))
+            return listOf(Link("dsb$linkId", Pair(variant, other)), Link("dsb$linkId", Pair(other, variant)))
         }
 
         return Collections.emptyList()
     }
 
-    private fun VariantStore.selectDoubleStrandedBreaks(variant: StructuralVariantContext): List<StructuralVariantContext> {
-        val dsbFilter = { other: StructuralVariantContext -> other.orientation != variant.orientation && !duplicates.contains(other.vcfId) }
-        return selectOthersNearby(variant, MAX_DSB_DISTANCE, dsbFilter)
-    }
+    private fun findNearbyIndices(i: Int): List<Int> {
+        val variant = variants[i]
+        val result = mutableListOf<Int>()
 
+        val idFilter = { other: StructuralVariantContext -> variant.vcfId != other.vcfId && variant.mateId?.equals(other.vcfId) != true }
+        val overlapFilter = { other: StructuralVariantContext -> other.minStart <= variant.maxStart + MAX_DSB_DISTANCE && other.maxStart >= variant.minStart - MAX_DSB_DISTANCE }
+        val orientationFilter = {other: StructuralVariantContext -> other.orientation != variant.orientation}
+        val duplicateFilter = {other: StructuralVariantContext -> !duplicates.contains(other.vcfId)}
+        val allFilters = {other: StructuralVariantContext -> idFilter(other) && overlapFilter(other) && orientationFilter(other) && duplicateFilter(other)}
+
+        // Look forwards
+        for (j in i + 1 until variants.size) {
+            val other = variants[j]
+            if (other.minStart > variant.maxStart + MAX_FIND_DISTANCE) {
+                break
+            } else if (allFilters(other)) {
+                result.add(j)
+            }
+        }
+
+        // Look backwards
+        for (j in max(0, i - 1) downTo 0) {
+            val other = variants[j]
+            if (other.maxStart < variant.minStart - MAX_FIND_DISTANCE) {
+                break
+            } else if (allFilters(other)) {
+                result.add(j)
+            }
+        }
+
+        return result
+    }
 }
--- gripss/src/test/kotlin/com/hartwig/hmftools/gripss/link/DsbLinkTest.kt
+++ gripss/src/test/kotlin/com/hartwig/hmftools/gripss/link/DsbLinkTest.kt
@@ -25,6 +25,23 @@ class DsbLinkTest {
     }
 
     @Test
+    fun testDsbLinkOneLargeCIPOSAfterInbetweenNonMatching() {
+        val variant1 = createVariant(110, "id1", "T", "T[2:100[", "mate1", "asm1", "325").toSv()
+        val variant2 = createVariant(220, "id2", "T", "[3:100[T", "mate2", "asm2", "325").toSv()
+        val variant3 = createVariant(420, "id2", "T", "]2:100]T", "mate2", "asm2", "325", Pair(-400, 0)).toSv()
+        val variants = listOf(variant1, variant2, variant3)
+        val duplicates = setOf<String>()
+
+        val variantStore = VariantStore(variants)
+        val assemblyStore = AssemblyLink(variants)
+
+        val dsbLinks = DsbLink(variantStore, assemblyStore, duplicates)
+        assertEquals("dsb1", dsbLinks[variant1.vcfId])
+        assertEquals("dsb1", dsbLinks[variant3.vcfId])
+    }
+
+
+    @Test
     fun testIgnoreAlreadyAssembled() {
         val variant1 = createVariant(110, "id1", "T", "T[2:100[", "mate1", "asm1", "325").toSv()
         val variant2 = createVariant(120, "id2", "T", "]2:100]T", "mate2", "asm1", "325").toSv()
@@ -75,6 +92,11 @@ class DsbLinkTest {
 
     private fun createVariant(pos: Int, vcfId: String, ref: String, alt: String, mate: String?, beid: String, beidl: String): VariantContext {
         val line = "1\t${pos}\t${vcfId}\t${ref}\t${alt}\t100\tPASS\tMATEID=${mate};BEID=${beid};BEIDL=${beidl};AS=10\tGT:BVF:VF:REF:REFPAIR\t./.:1:1:1:1\t./.:10:10:1:1"
+        return VariantContextTestFactory.decode(line);
+    }
+
+    private fun createVariant(pos: Int, vcfId: String, ref: String, alt: String, mate: String?, beid: String, beidl: String, cipos: Pair<Int, Int>): VariantContext {
+        val line = "1\t${pos}\t${vcfId}\t${ref}\t${alt}\t100\tPASS\tMATEID=${mate};BEID=${beid};BEIDL=${beidl};CIPOS=${cipos.first},${cipos.second};AS=10\tGT:BVF:VF:REF:REFPAIR\t./.:1:1:1:1\t./.:10:10:1:1"
         return VariantContextTestFactory.decode(line);
     }
 
