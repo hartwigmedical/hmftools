b2b3a6f8d8fc5ec443e021eea2b2f57ccda2011b
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-17 10:43:51 +0200

    DEV-798: Decapitalize key strings in protein annotations

--- vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/datamodel/Feature.java
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/datamodel/Feature.java
@@ -2,6 +2,8 @@ package com.hartwig.hmftools.vicc.datamodel;
 
 import java.util.List;
 
+import com.hartwig.hmftools.vicc.util.ProteinAnnotationExtractor;
+
 import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,21 +15,7 @@ public abstract class Feature {
     @NotNull
     @Value.Derived
     public String proteinAnnotation() {
-        String trimmedName = name().trim();
-        // Many KBs include the gene in the feature name in some form (eg "EGFR E709K" or "EGFR:E709K").
-        // Other KBs put the coding info behind the protein annotation ("V130L (c.388G>C)" rather than the gene in front of it)
-        String proteinAnnotation;
-        if (trimmedName.contains(" ")) {
-            String[] trimmedParts = trimmedName.split(" ");
-            proteinAnnotation = trimmedParts[1].contains("(c.") ? trimmedParts[0] : trimmedParts[1];
-        } else if (trimmedName.contains(":")) {
-            proteinAnnotation = trimmedName.split(":")[1];
-        } else {
-            proteinAnnotation = trimmedName;
-        }
-
-        // Some KBs include "p." in front of the protein annotation
-        return proteinAnnotation.startsWith("p.") ? proteinAnnotation.substring(2) : proteinAnnotation;
+        return ProteinAnnotationExtractor.toProteinAnnotation(name());
     }
 
     @NotNull
---
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/util/ProteinAnnotationExtractor.java
@@ -0,0 +1,44 @@
+package com.hartwig.hmftools.vicc.util;
+
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class ProteinAnnotationExtractor {
+
+    private static final Set<String> CAPITALIZED_STRINGS_TO_UNCAPITALIZE = Sets.newHashSet("DELINS", "DEL", "INS", "FS");
+
+    private ProteinAnnotationExtractor() {
+    }
+
+    @NotNull
+    public static String toProteinAnnotation(@NotNull String featureName) {
+        String trimmedName = featureName.trim();
+        // Many KBs include the gene in the feature name in some form (eg "EGFR E709K" or "EGFR:E709K").
+        // Other KBs put the coding info behind the protein annotation ("V130L (c.388G>C)" rather than the gene in front of it)
+        String proteinAnnotation;
+        if (trimmedName.contains(" ")) {
+            String[] trimmedParts = trimmedName.split(" ");
+            proteinAnnotation = trimmedParts[1].contains("(c.") ? trimmedParts[0] : trimmedParts[1];
+        } else if (trimmedName.contains(":")) {
+            proteinAnnotation = trimmedName.split(":")[1];
+        } else {
+            proteinAnnotation = trimmedName;
+        }
+
+        // Some KBs include "p." in front of the protein annotation
+        proteinAnnotation = proteinAnnotation.startsWith("p.") ? proteinAnnotation.substring(2) : proteinAnnotation;
+
+        // Some KBs use DEL/INS/FS rather than del/ins/fs
+        for (String stringToLookFor : CAPITALIZED_STRINGS_TO_UNCAPITALIZE) {
+            int position = proteinAnnotation.indexOf(stringToLookFor);
+            if (position > 0 && Character.isDigit(proteinAnnotation.charAt(position - 1))) {
+                proteinAnnotation = proteinAnnotation.substring(0, position) + stringToLookFor.toLowerCase() + proteinAnnotation.substring(
+                        position + stringToLookFor.length());
+            }
+        }
+        return proteinAnnotation;
+    }
+}
