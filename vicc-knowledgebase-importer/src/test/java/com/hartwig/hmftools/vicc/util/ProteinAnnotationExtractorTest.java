eeb89f2c27a4b6377c5c39489280e642d2cbc49d
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-17 11:06:33 +0200

    DEV-798: Add test

--- vicc-knowledgebase-importer/src/test/java/com/hartwig/hmftools/vicc/util/ProteinAnnotationExtractorTest.java
+++ vicc-knowledgebase-importer/src/test/java/com/hartwig/hmftools/vicc/util/ProteinAnnotationExtractorTest.java
@@ -13,6 +13,7 @@ public class ProteinAnnotationExtractorTest {
         assertEquals("E709K", ProteinAnnotationExtractor.toProteinAnnotation("EGFR:E709K"));
         assertEquals("E709K", ProteinAnnotationExtractor.toProteinAnnotation("EGFR:p.E709K"));
         assertEquals("E709K", ProteinAnnotationExtractor.toProteinAnnotation("EGFR p.E709K"));
+        assertEquals("E709K", ProteinAnnotationExtractor.toProteinAnnotation("E709K (c.2100A>c)"));
 
         assertEquals("V560del", ProteinAnnotationExtractor.toProteinAnnotation("KIT:p.V560DEL"));
         assertEquals("V560fs", ProteinAnnotationExtractor.toProteinAnnotation("KIT:p.V560FS"));
