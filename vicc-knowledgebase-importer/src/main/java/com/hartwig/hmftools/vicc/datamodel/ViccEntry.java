7565deb872e8ee0020f5262c5465495b397dc5a5
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-17 11:03:28 +0200

    DEV-798: Refactor transcript resolving

--- vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/datamodel/ViccEntry.java
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/datamodel/ViccEntry.java
@@ -2,11 +2,8 @@ package com.hartwig.hmftools.vicc.datamodel;
 
 import java.util.List;
 
-import com.hartwig.hmftools.vicc.datamodel.cgi.Cgi;
-import com.hartwig.hmftools.vicc.datamodel.civic.Civic;
-import com.hartwig.hmftools.vicc.datamodel.oncokb.OncoKb;
+import com.hartwig.hmftools.vicc.util.TranscriptExtractor;
 
-import org.apache.logging.log4j.util.Strings;
 import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -45,32 +42,7 @@ public abstract class ViccEntry {
     @Nullable
     @Value.Derived
     public String transcriptId() {
-        switch (source()) {
-            case ONCOKB: {
-                String transcriptId = ((OncoKb) kbSpecificObject()).transcriptID();
-                return !transcriptId.isEmpty() ? transcriptId : null;
-            }
-            case CIVIC: {
-                String transcriptId = ((Civic) kbSpecificObject()).coordinates().representativeTranscript();
-                if (transcriptId == null || transcriptId.isEmpty()) {
-                    return null;
-                }
-                int versionSeparator = transcriptId.indexOf(".");
-                String transcript = versionSeparator >= 0 ? transcriptId.substring(0, versionSeparator) : transcriptId;
-                // CIVIC occasionally just contains "ENST" or "ENST0000" as transcript. Need to filter that out.
-                return transcript.length() == 15 ? transcript : null;
-            }
-            case CGI: {
-                List<String> transcripts = ((Cgi) kbSpecificObject()).transcripts();
-                // Even though a vicc entry could have multiple different transcripts, in practice every vicc entry with more than one
-                // transcript only has one distinct transcript.
-                String transcript = !transcripts.isEmpty() ? transcripts.get(0) : Strings.EMPTY;
-                return !transcript.isEmpty() ? transcript : null;
-            }
-            case JAX:
-            default:
-                return null;
-        }
+        return TranscriptExtractor.extractTranscriptId(this);
     }
 }
 
---
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/util/TranscriptExtractor.java
@@ -0,0 +1,48 @@
+package com.hartwig.hmftools.vicc.util;
+
+import java.util.List;
+
+import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
+import com.hartwig.hmftools.vicc.datamodel.cgi.Cgi;
+import com.hartwig.hmftools.vicc.datamodel.civic.Civic;
+import com.hartwig.hmftools.vicc.datamodel.oncokb.OncoKb;
+
+import org.apache.logging.log4j.util.Strings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class TranscriptExtractor {
+
+    private TranscriptExtractor() {
+    }
+
+    @Nullable
+    public static String extractTranscriptId(@NotNull ViccEntry viccEntry) {
+        switch (viccEntry.source()) {
+            case ONCOKB: {
+                String transcriptId = ((OncoKb) viccEntry.kbSpecificObject()).transcriptID();
+                return !transcriptId.isEmpty() ? transcriptId : null;
+            }
+            case CIVIC: {
+                String transcriptId = ((Civic) viccEntry.kbSpecificObject()).coordinates().representativeTranscript();
+                if (transcriptId == null || transcriptId.isEmpty()) {
+                    return null;
+                }
+                int versionSeparator = transcriptId.indexOf(".");
+                String transcript = versionSeparator >= 0 ? transcriptId.substring(0, versionSeparator) : transcriptId;
+                // CIVIC occasionally just contains "ENST" or "ENST0000" as transcript. Need to filter that out.
+                return transcript.length() == 15 ? transcript : null;
+            }
+            case CGI: {
+                List<String> transcripts = ((Cgi) viccEntry.kbSpecificObject()).transcripts();
+                // Even though a vicc entry could have multiple different transcripts, in practice every vicc entry with more than one
+                // transcript only has one distinct transcript.
+                String transcript = !transcripts.isEmpty() ? transcripts.get(0) : Strings.EMPTY;
+                return !transcript.isEmpty() ? transcript : null;
+            }
+            case JAX:
+            default:
+                return null;
+        }
+    }
+}
