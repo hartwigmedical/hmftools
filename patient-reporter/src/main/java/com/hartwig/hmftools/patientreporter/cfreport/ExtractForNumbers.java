89dc9ef3ab02c162f618cdf6375e680a26d013fc
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-19 08:50:48 +0200

    DEV-1333: Another simplification

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/ExtractForNumbers.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/cfreport/ExtractForNumbers.java
@@ -16,12 +16,7 @@ public final class ExtractForNumbers {
     }
 
     @NotNull
-    public static String extractSuccessfulForNumbers(double purity, boolean hasReliablePurity) {
-        return successForNumber(purity, hasReliablePurity);
-    }
-
-    @NotNull
-    private static String successForNumber(double purity, boolean hasReliablePurity) {
+    public static String successForNumber(double purity, boolean hasReliablePurity) {
         return (hasReliablePurity && purity > 0.195) ? ForNumber.FOR_080.display() : ForNumber.FOR_103.display();
     }
 }
