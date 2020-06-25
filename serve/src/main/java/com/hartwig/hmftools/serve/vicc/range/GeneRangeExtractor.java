8d47ed66a1b9d14fb18cc20c1173c1e9d63632b5
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-06-25 12:04:43 +0200

    DEV-1112: extract codons from jax

--- serve/src/main/java/com/hartwig/hmftools/serve/vicc/range/GeneRangeExtractor.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/vicc/range/GeneRangeExtractor.java
@@ -9,39 +9,26 @@ import com.hartwig.hmftools.vicc.datamodel.Feature;
 import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
 import com.hartwig.hmftools.vicc.datamodel.ViccSource;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class GeneRangeExtractor {
+    private static final Logger LOGGER = LogManager.getLogger(GeneRangeExtractor.class);
 
     private static final Set<String> GENE_EXON = Sets.newHashSet("exon");
-    private static final Set<String> GENE_MULTIPLE_CODONS = Sets.newHashSet("V600X ",
-            "H1047X ",
-            "V600E/K ",
-            "Q61X ",
-            "G12X ",
-            "H1047X ",
-            "G13X ",
-            "E709X ",
-            "D835X ",
-            "G719X ",
-            "D842X ",
-            "R132X ");
+    private static final Set<String> GENE_MULTIPLE_CODONS = Sets.newHashSet("nonsense", "V600E/K");
 
     @NotNull
     public Map<Feature, String> extractGeneRanges(@NotNull ViccEntry viccEntry) {
         Map<Feature, String> geneRangesPerFeature = Maps.newHashMap();
         for (Feature feature : viccEntry.features()) {
-            String event = feature.name();
-            if (viccEntry.source() == ViccSource.JAX) {
-                String[] extractEvent = event.split(" ", 2);
-                if (extractEvent.length == 2) {
-                    event = extractEvent[1];
-                }
-            }
 
-            if (GENE_EXON.contains(event.toLowerCase())) {
+            if (GENE_EXON.contains(feature.name().toLowerCase())) {
                 geneRangesPerFeature.put(feature, feature.name());
-            } else if (GENE_MULTIPLE_CODONS.contains(event)) {
+            } else if (GENE_MULTIPLE_CODONS.contains(feature.biomarkerType()) && feature.proteinAnnotation()
+                    .substring(feature.proteinAnnotation().length() - 1)
+                    .equals("X") || GENE_MULTIPLE_CODONS.contains(feature.proteinAnnotation())) {
                 //TODO: possible using transvar for the codons
                 geneRangesPerFeature.put(feature, feature.name());
             }
