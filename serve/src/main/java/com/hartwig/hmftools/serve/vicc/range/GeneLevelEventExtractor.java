cea3d0734ec4dc4c47e1571dfee63afdfd6c8649
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-06-25 10:12:30 +0200

    DEV-1112: start with gene/range implementation for jax

--- serve/src/main/java/com/hartwig/hmftools/serve/vicc/range/GeneLevelEventExtractor.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/vicc/range/GeneLevelEventExtractor.java
@@ -9,28 +9,35 @@ import com.hartwig.hmftools.vicc.datamodel.Feature;
 import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
 import com.hartwig.hmftools.vicc.datamodel.ViccSource;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class GeneLevelEventExtractor {
+    private static final Logger LOGGER = LogManager.getLogger(GeneLevelEventExtractor.class);
 
-    private static final Set<String> ONCOKB_GENE_ACTIVATION =
-            Sets.newHashSet("Gain-of-function Mutations");
+    private static final Set<String> GENE_ACTIVATION = Sets.newHashSet("Gain-of-function Mutations", " act mut");
 
-    private static final Set<String> ONCOKB_GENE_INACTIVATION =
-            Sets.newHashSet("Truncating Mutations");
+    private static final Set<String> GENE_INACTIVATION = Sets.newHashSet("Truncating Mutations", " inact mut");
 
     @NotNull
     public Map<Feature, String> extractKnownGeneLevelEvents(@NotNull ViccEntry viccEntry) {
         Map<Feature, String> geneLevelEventsPerFeature = Maps.newHashMap();
-        if (viccEntry.source() == ViccSource.ONCOKB) {
-            for (Feature feature : viccEntry.features()) {
-                if (ONCOKB_GENE_ACTIVATION.contains(feature.name())) {
-                    geneLevelEventsPerFeature.put(feature, "gain of " + feature.geneSymbol());
-                } else if (ONCOKB_GENE_INACTIVATION.contains(feature.name())) {
-                    geneLevelEventsPerFeature.put(feature, "loss of " + feature.geneSymbol());
+        for (Feature feature : viccEntry.features()) {
+            String event = feature.name();
+            if (viccEntry.source() == ViccSource.JAX) {
+                String [] extractEvent = event.split(" ", 2);
+                if (extractEvent.length == 2) {
+                    event = extractEvent[1];
                 }
             }
+            if (GENE_ACTIVATION.contains(event)) {
+                geneLevelEventsPerFeature.put(feature, "gain of " + feature.geneSymbol());
+            } else if (GENE_INACTIVATION.contains(event)) {
+                geneLevelEventsPerFeature.put(feature, "loss of " + feature.geneSymbol());
+            }
         }
+
         return geneLevelEventsPerFeature;
     }
 }
--- serve/src/main/java/com/hartwig/hmftools/serve/vicc/range/GeneRangeExtractor.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/vicc/range/GeneRangeExtractor.java
@@ -1,8 +1,10 @@
 package com.hartwig.hmftools.serve.vicc.range;
 
 import java.util.Map;
+import java.util.Set;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.hartwig.hmftools.vicc.datamodel.Feature;
 import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
 import com.hartwig.hmftools.vicc.datamodel.ViccSource;
@@ -11,16 +13,40 @@ import org.jetbrains.annotations.NotNull;
 
 public class GeneRangeExtractor {
 
+    private static final Set<String> GENE_EXON = Sets.newHashSet("exon");
+    private static final Set<String> GENE_MULTIPLE_CODONS = Sets.newHashSet("V600X ",
+            "H1047X ",
+            "V600E/K ",
+            "Q61X ",
+            "G12X ",
+            "H1047X ",
+            "G13X ",
+            "E709X ",
+            "D835X ",
+            "G719X ",
+            "D842X ",
+            "R132X ");
+
     @NotNull
     public Map<Feature, String> extractGeneRanges(@NotNull ViccEntry viccEntry) {
         Map<Feature, String> geneRangesPerFeature = Maps.newHashMap();
-        if (viccEntry.source() == ViccSource.ONCOKB) {
-            for (Feature feature : viccEntry.features()) {
-                if (feature.name().toLowerCase().contains("exon")) {
-                    geneRangesPerFeature.put(feature, feature.name());
+        for (Feature feature : viccEntry.features()) {
+            String event = feature.name();
+            if (viccEntry.source() == ViccSource.JAX) {
+                String[] extractEvent = event.split(" ", 2);
+                if (extractEvent.length == 2) {
+                    event = extractEvent[1];
                 }
             }
+
+            if (GENE_EXON.contains(event.toLowerCase())) {
+                geneRangesPerFeature.put(feature, feature.name());
+            } else if (GENE_MULTIPLE_CODONS.contains(event)) {
+                //TODO: possible using transvar for the codons
+                geneRangesPerFeature.put(feature, feature.name());
+            }
         }
+
         return geneRangesPerFeature;
     }
 }
