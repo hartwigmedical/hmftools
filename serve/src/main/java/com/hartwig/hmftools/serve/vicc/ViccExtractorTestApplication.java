7e70f0e672a034bf3edf68bc18f0a5ad58a7aa98
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-30 15:31:46 +0200

    Formatting

--- serve/src/main/java/com/hartwig/hmftools/serve/vicc/ViccExtractorTestApplication.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/vicc/ViccExtractorTestApplication.java
@@ -194,9 +194,7 @@ public class ViccExtractorTestApplication {
         LOGGER.info("No genomic events derived for {} features.", featuresWithoutGenomicEvents.size());
         for (Feature feature : featuresWithoutGenomicEvents) {
             if (!FeatureIgnoreUtil.canIgnore(feature)) {
-                LOGGER.debug(" No genomic events derived from '{}' in '{}'",
-                        feature.name(),
-                        feature.geneSymbol());
+                LOGGER.debug(" No genomic events derived from '{}' in '{}'", feature.name(), feature.geneSymbol());
                 LOGGER.debug("  {}", feature);
             }
         }
