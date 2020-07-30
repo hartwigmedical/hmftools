243405253c9a38b33ad73552c6eaaf65f94f9cff
Jonathan Baber <jonbaber@gmail.com>
2020-07-30 15:48:12 +1000

    add lrs and lps to somatic variant

--- hmf-common/src/main/java/com/hartwig/hmftools/common/sage/SageMetaData.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/sage/SageMetaData.java
@@ -10,14 +10,20 @@ public class SageMetaData {
 
     public static final String TIER = "TIER";
     public static final String PHASED_INFRAME_INDEL = "PII";
+    public final static String LOCAL_PHASE_SET = "LPS";
+    public final static String LOCAL_REALIGN_SET = "LRS";
 
     private static final String TIER_DESCRIPTION = "Tier: [HOTSPOT, PANEL, HIGH_CONFIDENCE, LOW_CONFIDENCE]";
     public static final String PHASED_INFRAME_INDEL_DESCRIPTION = "Phased inframe indel identifier";
+    private final static String PHASE_DESCRIPTION = "Local Phase Set";
+    private final static String REALIGN_DESCRIPTION = "Local Realignment Set";
 
     @NotNull
     public static VCFHeader addSageMetaData(@NotNull final VCFHeader header) {
         header.addMetaDataLine(new VCFInfoHeaderLine(TIER, 1, VCFHeaderLineType.String, TIER_DESCRIPTION));
         header.addMetaDataLine(new VCFInfoHeaderLine(PHASED_INFRAME_INDEL, 1, VCFHeaderLineType.Integer, PHASED_INFRAME_INDEL_DESCRIPTION));
+        header.addMetaDataLine(new VCFInfoHeaderLine(LOCAL_PHASE_SET, 1, VCFHeaderLineType.Integer, PHASE_DESCRIPTION));
+        header.addMetaDataLine(new VCFInfoHeaderLine(LOCAL_REALIGN_SET, 1, VCFHeaderLineType.Integer, REALIGN_DESCRIPTION));
         return header;
     }
 }
