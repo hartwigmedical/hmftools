83c9108d7357ce7da5f1368583504c2fbd1477b1
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-08-04 10:48:52 +0200

    DEV-1416: refactor

--- serve/src/main/java/com/hartwig/hmftools/serve/vicc/fusion/FusionExtractor.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/vicc/fusion/FusionExtractor.java
@@ -21,8 +21,6 @@ public class FusionExtractor {
     private final Set<String> uniqueFusionsPair = Sets.newHashSet();
     @NotNull
     private final Set<String> uniqueFusionsPromiscuous = Sets.newHashSet();
-    private static final String FUSION_PAIR = "fusion pair";
-    private static final String FUSION_PROMISCUOUS = "fusion promiscuous";
     private static final Set<String> SEARCH_FUSION_PAIRS = Sets.newHashSet("Fusion", "Disruptive Inframe Deletion", "Gene Fusion", "EGFRvII");
     private static final Set<String> SEARCH_FUSION_PROMISCUOUS =
             Sets.newHashSet("REARRANGEMENT", "Fusions", "fusion", "rearrange", "Transcript Fusion");
