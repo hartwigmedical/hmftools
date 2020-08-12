19dd47cd08f7a21fa9f1fdac755a90085930db43
Jonathan Baber <jonbaber@gmail.com>
2020-08-12 15:54:33 +1000

    Add new PADDLE module

---
+++ paddle/src/main/kotlin/com/hartwig/hmftools/paddle/PaddleDndsApplication.kt
@@ -0,0 +1,42 @@
+package com.hartwig.hmftools.paddle
+
+import com.hartwig.hmftools.paddle.dnds.DndsCv
+import com.hartwig.hmftools.paddle.dnds.DndsMutation
+import org.apache.logging.log4j.LogManager
+
+fun main(args: Array<String>) {
+
+    try {
+        PaddleDndsApplication().use { x -> x.run() }
+    } catch (e: Exception) {
+        println(e)
+    }
+}
+
+class PaddleDndsApplication : AutoCloseable, Runnable {
+
+    companion object {
+        val logger = LogManager.getLogger(this::class.java)
+    }
+
+    private val startTime = System.currentTimeMillis()
+
+    override fun run() {
+
+        val mutationsFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/DndsMutations.tsv"
+        val dndsCVFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/HmfRefCDSCv.tsv"
+
+        logger.info("Loading mutations: $mutationsFile")
+        val dndsMutations = DndsMutation.fromFile(mutationsFile)
+
+        logger.info("Loading dNdScv values: $dndsCVFile")
+        val dndsCv = DndsCv.fromFile(dndsCVFile)
+
+        println("Sdf")
+
+    }
+
+    override fun close() {
+        logger.info("Finished in ${(System.currentTimeMillis() - startTime) / 1000} seconds")
+    }
+}
---
+++ paddle/src/main/kotlin/com/hartwig/hmftools/paddle/dnds/DndsImpact.kt
@@ -0,0 +1,5 @@
+package com.hartwig.hmftools.paddle.dnds
+
+enum class DndsImpact {
+    SYNONYMOUS, MISSENSE, NONSENSE, SPLICE, UNKNOWN;
+}
