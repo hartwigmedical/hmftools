909f1b13918ac50e69300b7b95ded6d879e38001
Jonathan Baber <jonbaber@gmail.com>
2020-08-17 15:49:19 +1000

    automatically determine split for biallelic

--- paddle/src/main/kotlin/com/hartwig/hmftools/paddle/PaddleDndsApplication.kt
+++ paddle/src/main/kotlin/com/hartwig/hmftools/paddle/PaddleDndsApplication.kt
@@ -1,6 +1,5 @@
 package com.hartwig.hmftools.paddle
 
-import com.hartwig.hmftools.common.drivercatalog.dnds.DndsDriverGeneLikelihoodSupplier
 import com.hartwig.hmftools.paddle.cohort.TumorMutationalLoad
 import com.hartwig.hmftools.paddle.cohort.TumorMutationalLoadSample
 import com.hartwig.hmftools.paddle.dnds.DndsCvGene
@@ -26,57 +25,74 @@ class PaddleDndsApplication : AutoCloseable, Runnable {
 
     private val startTime = System.currentTimeMillis()
 
-    override fun run() {
+    fun splitDnds(biallelicFile: String, nonBiallelicFile: String): Pair<Map<Gene, DndsCvGene>, Map<Gene, DndsCvGene>> {
+        val dndsCvBiallelic = DndsCvGene.fromFileNoIndel(biallelicFile).associateBy { x -> x.gene }
+        val dndsCvNonBiallelic = DndsCvGene.fromFileNoIndel(nonBiallelicFile).associateBy { x -> x.gene }
 
+        val splitGenes = mutableSetOf<String>()
+        for (gene in dndsCvBiallelic.keys) {
+            val biallelic = dndsCvBiallelic[gene]!!
+            val nonBiallelic = dndsCvNonBiallelic[gene]!!
+            if (biallelic.missense.wCv > nonBiallelic.missense.wCv) {
+                splitGenes.add(gene)
+            }
+        }
+
+        return Pair(dndsCvBiallelic.filter {  it.key in splitGenes }, dndsCvNonBiallelic.filter {  it.key in splitGenes })
+    }
+
+    override fun run() {
         val cohortFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/HmfTMB.tsv"
         val dndsCVFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/HmfRefCDSCv.tsv"
+        val dndsBiallelicCVFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/HmfRefCDSCvBiallelic.tsv"
+        val dndsNonBiallelicCVFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/HmfRefCDSCvNonBiallelic.tsv"
         val mutationsFile = "/Users/jon/hmf/repos/hmftools/paddle/src/main/resources/DndsMutations.tsv"
+
+        logger.info("Loading dNdScv values: $dndsCVFile")
+        val dndsCv = DndsCvGene.fromFile(dndsCVFile).associateBy { x -> x.gene }
+        val (dndsCvBiallelic, dndsCvNonBiallelic) = splitDnds(dndsBiallelicCVFile, dndsNonBiallelicCVFile)
+        val tsSplitGenes = dndsCvBiallelic.keys
+
+//        logger.info("Loading old data")
+//        val genePanel = DriverGenePanelFactory().create()
+//        val oncoGenes = genePanel.oncoGenes()
+//        val tsgGenes = genePanel.tsGenes()
 
         logger.info("Loading cohort: $cohortFile")
         val (cohortSize, cohortLoad) = loadCohort(cohortFile)
 
         logger.info("Loading mutations: $mutationsFile")
-        val dndsMutations = DndsMutation.fromFile(mutationsFile).filter { it.gene == "ACVR1" }
-//        val dndsMutations = DndsMutation.fromFile(mutationsFile).filter { x -> x.impact == Impact.MISSENSE || x.impact == Impact.INFRAME || x.impact == Impact.SYNONYMOUS}
-//        val dndsMutations = DndsMutation.fromFile(mutationsFile).filter { x -> x.impact == Impact.MISSENSE || x.impact == Impact.INFRAME }
-//        val other = dndsMutations.filter { x -> x.impact != Impact.MISSENSE && x.impact != Impact.INFRAME }.sortedBy { x -> x.sample }
-
-
-        logger.info("Loading dNdScv values: $dndsCVFile")
-        val dndsCv = DndsCvGene.fromFile(dndsCVFile).associateBy { x -> x.gene }
+        val dndsMutations = DndsMutation.fromFile(mutationsFile)
+//        val oncoMutations = dndsMutations.filter { it.gene in oncoGenes }
+//        val tsgMutations = dndsMutations.filter { it.gene in tsgGenes }
+        val tsgBiallelicMutations = dndsMutations.filter { it.gene in tsSplitGenes && it.biallelic }
+        val tsgNonBiallelicMutations = dndsMutations.filter { it.gene in tsSplitGenes && !it.biallelic  }
 
         logger.info("Calculating gene mutation summary")
         val oncoGeneMutations = MutationsGene.oncoGeneMutations(dndsMutations).associateBy { x -> x.gene }
         val tsgGeneMutations = MutationsGene.tsgGeneMutations(dndsMutations).associateBy { x -> x.gene }
+        val tsgBiallelicGeneMutations = MutationsGene.tsgGeneMutations(tsgBiallelicMutations).associateBy { x -> x.gene }
+        val tsgNonBiallelicGeneMutations = MutationsGene.tsgGeneMutations(tsgNonBiallelicMutations).associateBy { x -> x.gene }
 
-        // Log any missing genes from dNdS
-        val dndsGenes = dndsCv.keys
-        val missingDndsGenes = mutableSetOf<String>()
-        missingDndsGenes.addAll(tsgGeneMutations.keys.subtract(dndsGenes))
-        missingDndsGenes.addAll(oncoGeneMutations.keys.subtract(dndsGenes))
-        for (gene in missingDndsGenes) {
-            logger.warn("Dnds values missing for gene $gene")
-        }
-
-        val oncoGenes = DndsDriverGeneLikelihoodSupplier.oncoLikelihood().keys
-        val file = "/Users/jon/hmf/repos/hmftools/hmf-common/src/main/resources/dnds/DndsDriverLikelihoodOnco.AR.tsv"
-        val oncoLikelihood = LikelihoodGene(cohortSize, cohortLoad.totalLoad, dndsCv, oncoGeneMutations)
-        LikelihoodGene.writeFile(file, oncoLikelihood.values.filter { oncoGenes.contains(it.gene) })
-
-//        val tsgBiallelicLikelihood = LikelihoodGene(cohortSize, cohortLoad.biallelicLoad, dndsCv, tsgGeneMutations)
-//        val tsgNonBiallelicLikelihood = LikelihoodGene(cohortSize, cohortLoad.nonBiallelicLoad, dndsCv, tsgGeneMutations)
-
-
-        for (oncoGeneMutation in oncoLikelihood) {
-            println(oncoGeneMutation)
-        }
-
-        //
-
-//        val tsgLikelihood = LikelihoodGene(cohortSize, cohortLoad.totalLoad, dndsCv, tsgGeneMutations)
-//        for (tsgGeneMutation in tsgLikelihood) {
-//            println(tsgGeneMutation)
+//        // Log any missing genes from dNdS
+//        val dndsGenes = dndsCv.keys
+//        val missingDndsGenes = mutableSetOf<String>()
+//        missingDndsGenes.addAll(tsgGeneMutations.keys.subtract(dndsGenes))
+//        missingDndsGenes.addAll(oncoGeneMutations.keys.subtract(dndsGenes))
+//        for (gene in missingDndsGenes) {
+//            logger.warn("Dnds values missing for gene $gene")
 //        }
+
+        val oncoLikelihood = LikelihoodGene(cohortSize, cohortLoad.totalLoad, dndsCv, oncoGeneMutations)
+        val tsgLikelihood = LikelihoodGene(cohortSize, cohortLoad.totalLoad, dndsCv, tsgGeneMutations)
+        val tsgBiallelicLikelihood = LikelihoodGene(cohortSize, cohortLoad.biallelicLoad, dndsCvBiallelic, tsgBiallelicGeneMutations)
+        val tsgNoneBiallelicLikelihood = LikelihoodGene(cohortSize, cohortLoad.nonBiallelicLoad, dndsCvNonBiallelic, tsgNonBiallelicGeneMutations)
+
+
+        LikelihoodGene.writeFile("/Users/jon/hmf/repos/hmftools/hmf-common/src/main/resources/dnds/AllDndsDriverLikelihoodOnco.tsv", oncoLikelihood.values)
+        LikelihoodGene.writeFile("/Users/jon/hmf/repos/hmftools/hmf-common/src/main/resources/dnds/AllDndsDriverLikelihoodTsg.tsv", tsgLikelihood.values)
+        LikelihoodGene.writeFile("/Users/jon/hmf/repos/hmftools/hmf-common/src/main/resources/dnds/AllDndsDriverLikelihoodTsgBiallelic.tsv", tsgBiallelicLikelihood.values)
+        LikelihoodGene.writeFile("/Users/jon/hmf/repos/hmftools/hmf-common/src/main/resources/dnds/AllDndsDriverLikelihoodTsgNonBiallelic.tsv", tsgNoneBiallelicLikelihood.values)
 
     }
 
