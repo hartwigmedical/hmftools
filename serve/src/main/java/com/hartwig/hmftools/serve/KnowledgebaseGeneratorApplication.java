93ae137239eadd6575b9087a91bda3ebc5c78e62
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-08-27 15:52:16 +0200

    refactor SERVE

--- serve/src/main/java/com/hartwig/hmftools/serve/KnowledgebaseGeneratorApplication.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/KnowledgebaseGeneratorApplication.java
@@ -3,24 +3,17 @@ package com.hartwig.hmftools.serve;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.hartwig.hmftools.iclusion.data.IclusionTrial;
 import com.hartwig.hmftools.iclusion.io.IclusionTrialFile;
 import com.hartwig.hmftools.serve.compassionateuse.CompassionateUseProgram;
 import com.hartwig.hmftools.serve.compassionateuse.CompassionateUseProgramFile;
-import com.hartwig.hmftools.serve.output.GeneratingOutputFiles;
 import com.hartwig.hmftools.serve.vicc.copynumber.ActionableAmplificationDeletion;
 import com.hartwig.hmftools.serve.vicc.copynumber.KnownAmplificationDeletion;
-import com.hartwig.hmftools.serve.vicc.eventtype.DetermineEventOfGenomicMutation;
-import com.hartwig.hmftools.serve.vicc.eventtype.EventType;
-import com.hartwig.hmftools.serve.vicc.eventtype.EventTypeAnalyzer;
 import com.hartwig.hmftools.serve.vicc.fusion.KnownFusions;
 import com.hartwig.hmftools.serve.vicc.hotspot.HotspotExtractor;
 import com.hartwig.hmftools.serve.vicc.signatures.Signatures;
@@ -79,10 +72,7 @@ public class KnowledgebaseGeneratorApplication {
         HotspotExtractor hotspotExtractor =
                 HotspotExtractor.transvarWithRefGenome(refGenomeVersion, cmd.getOptionValue(REF_GENOME_FASTA_FILE));
 
-        ImmutableAllGenomicEvents.Builder genomicEventsBuilder = ImmutableAllGenomicEvents.builder();
-
         //Lists of known genomic events
-        List<EventType> allEventType = Lists.newArrayList();
         List<KnownAmplificationDeletion> listKnownAmplification = Lists.newArrayList();
         List<KnownAmplificationDeletion> listKnownDeletion = Lists.newArrayList();
         List<String> listKnownVariants = Lists.newArrayList();
@@ -94,180 +84,6 @@ public class KnowledgebaseGeneratorApplication {
         //Lists of actionable genomic events
         List<ActionableAmplificationDeletion> listActionableDeletion = Lists.newArrayList();
         List<ActionableAmplificationDeletion> listActionableAmplification = Lists.newArrayList();
-
-        LOGGER.info("Analyzing all VICC entries");
-        for (ViccEntry viccEntry : viccEntries) {
-
-            List<EventType> eventType = EventTypeAnalyzer.determineEventTypes(viccEntry);
-
-            for (EventType type : eventType) {
-                allEventType.add(type);
-
-                // Generating known events
-                //TODO: map every genomic event to one objecxt
-                //TODO: if combined event use single event for determine known events
-
-                for (Map.Entry<String, List<String>> entryDB : type.eventMap().entrySet()) {
-                    for (String event : entryDB.getValue()) {
-                        listKnownAmplification.add(DetermineEventOfGenomicMutation.checkKnownAmplification(viccEntry,
-                                entryDB.getKey(),
-                                event));
-                        listKnownDeletion.add(DetermineEventOfGenomicMutation.checkKnownDeletion(viccEntry, entryDB.getKey(), event));
-                        DetermineEventOfGenomicMutation.checkVariants(viccEntry, entryDB.getKey(), event);
-                        DetermineEventOfGenomicMutation.checkRange(viccEntry, entryDB.getKey(), event);
-                        listKnownFusionPairs.add(DetermineEventOfGenomicMutation.checkFusionsPairs(viccEntry, entryDB.getKey(), event));
-                        listKnownFusionPromiscuous.add(DetermineEventOfGenomicMutation.checkFusionPromiscuous(viccEntry,
-                                entryDB.getKey(),
-                                event));
-                        listSignatures.add(DetermineEventOfGenomicMutation.checkSignatures(viccEntry, event));
-
-                        // Generating actionable event
-                        listActionableDeletion.add(DetermineEventOfGenomicMutation.checkActionableDeletion(viccEntry,
-                                entryDB.getKey(),
-                                event));
-                        listActionableAmplification.add(DetermineEventOfGenomicMutation.checkActionableAmplification(viccEntry,
-                                entryDB.getKey(),
-                                event));
-                    }
-                }
-            }
-        }
-        AllGenomicEvents allGenomicEvents = genomicEventsBuilder.build();
-
-        List<KnownAmplificationDeletion> listAmpsFilter = Lists.newArrayList();
-        List<KnownAmplificationDeletion> listDelsFIlter = Lists.newArrayList();
-        Set<String> uniqueAmps = Sets.newHashSet();
-        Set<String> uniqueDels = Sets.newHashSet();
-        for (KnownAmplificationDeletion amps : listKnownAmplification) {
-            if (!amps.eventType().isEmpty()) {
-                listAmpsFilter.add(amps);
-                uniqueAmps.add(amps.gene());
-            }
-        }
-
-        List<String> sortedUniqueAmps = Lists.newArrayList(uniqueAmps);
-        Collections.sort(sortedUniqueAmps);
-
-        for (KnownAmplificationDeletion dels : listKnownDeletion) {
-            if (!dels.eventType().isEmpty()) {
-                listDelsFIlter.add(dels);
-                uniqueDels.add(dels.gene());
-            }
-        }
-        List<String> sortedUniqueDels = Lists.newArrayList(uniqueDels);
-        Collections.sort(sortedUniqueDels);
-
-        List<Signatures> listSignaturesFilter = Lists.newArrayList();
-        for (Signatures signatures : listSignatures) {
-            if (!signatures.eventType().isEmpty()) {
-                listSignaturesFilter.add(signatures);
-            }
-        }
-
-        Set<String> uniqueKnownFusionPairs = Sets.newHashSet();
-        List<KnownFusions> listKnownFusionsPairsFilter = Lists.newArrayList();
-        List<String> promiscusThree = Lists.newArrayList();
-        List<String> promiscuosFive = Lists.newArrayList();
-
-        for (KnownFusions knownPairFusions : listKnownFusionPairs) {
-            if (!knownPairFusions.eventType().isEmpty()) {
-                listKnownFusionsPairsFilter.add(knownPairFusions);
-                uniqueKnownFusionPairs.add(knownPairFusions.gene());
-
-                if (knownPairFusions.gene().equals("TRAC-NKX2-1")) {
-                    promiscuosFive.add("TRAC");
-                    promiscusThree.add("NKX2-1");
-                } else if (knownPairFusions.gene().contains("-")) {
-                    promiscuosFive.add(knownPairFusions.gene().split("-")[0]);
-                    promiscusThree.add(knownPairFusions.gene().split("-")[1]);
-                }
-            }
-        }
-
-        List<String> sortedUniqueKnownFusionPairs = Lists.newArrayList(uniqueKnownFusionPairs);
-        Collections.sort(sortedUniqueKnownFusionPairs);
-
-        Map<String, Integer> countsPromiscuousFive = Maps.newHashMap();
-        for (String five : promiscuosFive) {
-            if (countsPromiscuousFive.containsKey(five)) {
-                int count = countsPromiscuousFive.get(five) + 1;
-                countsPromiscuousFive.put(five, count);
-            } else {
-                countsPromiscuousFive.put(five, 1);
-            }
-        }
-
-        Set<String> promiscuousFiveGenes = Sets.newHashSet();
-        for (KnownFusions five : listKnownFusionPromiscuous) {
-            if (!five.eventType().isEmpty()) {
-                if (countsPromiscuousFive.containsKey(five.gene())) {
-                    if (countsPromiscuousFive.get(five.gene()) >= 3 || countsPromiscuousFive.get(five.gene()) >= 1) {
-                        promiscuousFiveGenes.add(five.gene());
-                    }
-                }
-            }
-        }
-
-        Map<String, Integer> countsPromiscuousThree = Maps.newHashMap();
-        for (String three : promiscusThree) {
-            if (countsPromiscuousThree.containsKey(three)) {
-                int count = countsPromiscuousThree.get(three) + 1;
-                countsPromiscuousThree.put(three, count);
-            } else {
-                countsPromiscuousThree.put(three, 1);
-            }
-        }
-
-        Set<String> promiscuousThreeGenes = Sets.newHashSet();
-        for (KnownFusions three : listKnownFusionPromiscuous) {
-            if (!three.eventType().isEmpty()) {
-                if (countsPromiscuousThree.containsKey(three.gene())) {
-                    if (countsPromiscuousThree.get(three.gene()) >= 3 || countsPromiscuousThree.get(three.gene()) >= 1) {
-                        promiscuousThreeGenes.add(three.gene());
-                    }
-                }
-            }
-        }
-
-        List<ActionableAmplificationDeletion> listFilterActionableAmplifications = Lists.newArrayList();
-
-        // If drug info/tumor location is known then variant is an actionable variant
-        for (ActionableAmplificationDeletion actionableAmplification : listActionableAmplification) {
-            if (actionableAmplification.level() != null && actionableAmplification.drug() != null
-                    && actionableAmplification.drugType() != null && actionableAmplification.direction() != null
-                    && actionableAmplification.sourceLink() != null && actionableAmplification.cancerType() != null) {
-                listFilterActionableAmplifications.add(actionableAmplification);
-            }
-        }
-
-        List<ActionableAmplificationDeletion> listFilterActionableDeletion = Lists.newArrayList();
-
-        for (ActionableAmplificationDeletion actionableDeletion : listActionableDeletion) {
-            if (actionableDeletion.level() != null && actionableDeletion.drug() != null && actionableDeletion.drugType() != null
-                    && actionableDeletion.direction() != null && actionableDeletion.sourceLink() != null
-                    && actionableDeletion.cancerType() != null) {
-                listFilterActionableDeletion.add(actionableDeletion);
-            }
-        }
-
-        AllGenomicEvents finalAllGenomicEvents = ImmutableAllGenomicEvents.builder()
-                .eventType(allEventType)
-                .knownAmplifications(listAmpsFilter)
-                .uniqueAmplification(sortedUniqueAmps)
-                .knownDeletions(listDelsFIlter)
-                .uniqueDeletions(sortedUniqueDels)
-                .actionableAmplification(listFilterActionableAmplifications)
-                .actionableDeletion(listFilterActionableDeletion)
-                .signatures(listSignaturesFilter)
-                .knownFusionPairs(listKnownFusionsPairsFilter)
-                .uniqueKnownFusionPairs(sortedUniqueKnownFusionPairs)
-                .knownFusionPromiscuousThree(promiscuousThreeGenes)
-                .knownFusionPromiscuousFive(promiscuousFiveGenes)
-                .build();
-
-        // Create all output files from knowledgebase with data
-        LOGGER.info("Generating output files");
-        GeneratingOutputFiles.generatingOutputFiles(cmd.getOptionValue(OUTPUT_DIR), finalAllGenomicEvents);
     }
 
     private static void readIclusionTrials(@NotNull String iClusionTrialTsv) throws IOException {
