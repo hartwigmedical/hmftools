c0ef608d203fd59638e0d528c4d372f979b4e1e8
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-08-28 14:07:05 +0200

    DEV-1416: add unmapped events to file

--- serve/src/main/java/com/hartwig/hmftools/serve/vicc/ViccExtractorTestApplication.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/vicc/ViccExtractorTestApplication.java
@@ -250,6 +250,8 @@ public class ViccExtractorTestApplication {
         for (Feature feature : featuresWithoutGenomicEvents) {
             if (!FeatureIgnoreUtil.canIgnore(feature)) {
                 LOGGER.debug(" No genomic events derived from '{}' in '{}'", feature.name(), feature.geneSymbol());
+                writer.write("Unmapped_event" + "\t" + feature.geneSymbol() + "\t" + feature.name() + "\t" + feature.biomarkerType() + "\t"
+                        + feature + "\n");
             } else {
                 writer.write("Ignore_event" + "\t" + feature.geneSymbol() + "\t" + feature.name() + "\t" + feature.biomarkerType() + "\t"
                         + feature + "\n");
