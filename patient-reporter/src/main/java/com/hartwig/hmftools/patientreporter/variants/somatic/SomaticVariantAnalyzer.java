34937245f64ac34f0d9f93a771e871d4ce5a4ad8
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-09-18 10:28:11 +0200

    DEV-1472: Remove logica for determine reportable somatic variant

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/somatic/SomaticVariantAnalyzer.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/variants/somatic/SomaticVariantAnalyzer.java
@@ -21,15 +21,6 @@ import org.jetbrains.annotations.NotNull;
 
 public final class SomaticVariantAnalyzer {
 
-//    private static final Logger LOGGER = LogManager.getLogger(SomaticVariantAnalyzer.class);
-//
-//    private static final List<CodingEffect> TSG_CODING_EFFECTS_TO_REPORT =
-//            Lists.newArrayList(CodingEffect.NONSENSE_OR_FRAMESHIFT, CodingEffect.MISSENSE, CodingEffect.SPLICE);
-//
-//    private static final Set<String> ONCO_GENES_WITH_SPLICE_EFFECTS = Sets.newHashSet("MET", "JAK2");
-//
-//    private static final List<CodingEffect> ONCO_CODING_EFFECTS_TO_REPORT = Lists.newArrayList(CodingEffect.MISSENSE);
-
     private SomaticVariantAnalyzer() {
     }
 
@@ -42,52 +33,7 @@ public final class SomaticVariantAnalyzer {
         variants.forEach(drivers::add);
         List<DriverCatalog> driverCatalog = drivers.build(exomeGeneCopyNumbers);
 
-//        // Check that we miss no driver genes.
-//        for (DriverCatalog driver : driverCatalog) {
-//            boolean reported = false;
-//            for (SomaticVariant variant : variantsToReport) {
-//                if (variant.gene().equals(driver.gene())) {
-//                    reported = true;
-//                }
-//            }
-//            if (!reported) {
-//                LOGGER.warn("Driver gene '{}' not added to reported somatic variants!", driver.gene());
-//            }
-//        }
-
         return ImmutableSomaticVariantAnalysis.of(variantsToReport, driverCatalog);
     }
 
-//    @NotNull
-//    private static Predicate<SomaticVariant> includeFilter(@NotNull DriverGenePanel driverGenePanel) {
-//        return variant -> {
-//            if (variant.isFiltered()) {
-//                return false;
-//            }
-//
-//            if (driverGenePanel.oncoGenes().contains(variant.gene()) || driverGenePanel.tsGenes().contains(variant.gene())) {
-//                if (variant.isHotspot()) {
-//                    return true;
-//                }
-//
-//                DriverCategory category = driverGenePanel.category(variant.gene());
-//                if (category == null) {
-//                    throw new IllegalStateException("Driver category not known for driver gene: " + variant.gene());
-//                }
-//
-//                CodingEffect effect = variant.canonicalCodingEffect();
-//                if (category == DriverCategory.TSG) {
-//                    return TSG_CODING_EFFECTS_TO_REPORT.contains(effect);
-//                } else {
-//                    assert category == DriverCategory.ONCO;
-//                    if (ONCO_GENES_WITH_SPLICE_EFFECTS.contains(variant.gene())) {
-//                        return ONCO_CODING_EFFECTS_TO_REPORT.contains(effect) || effect == CodingEffect.SPLICE;
-//                    } else {
-//                        return ONCO_CODING_EFFECTS_TO_REPORT.contains(effect);
-//                    }
-//                }
-//            }
-//            return false;
-//        };
-//    }
 }
