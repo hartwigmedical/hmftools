81b634f48238f13d9d379a859fb8dd5e1119eaec
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-09-21 10:59:51 +0200

    DEV-1472: Bump version ahead of release

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReporterApplication.java
@@ -33,7 +33,7 @@ public class PatientReporterApplication {
     public static final String VERSION = PatientReporterApplication.class.getPackage().getImplementationVersion();
 
     // Uncomment this line when generating an example report using PDFWriterTest
-    //                public static final String VERSION = "7.14";
+    //                public static final String VERSION = "7.15s";
 
     public static void main(@NotNull String[] args) throws IOException {
         Options options = PatientReporterConfig.createOptions();
