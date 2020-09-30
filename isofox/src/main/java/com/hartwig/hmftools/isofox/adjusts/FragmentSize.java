476f84a58abf41fe01e224b2bf3cf7a42299cfd9
Charles Shale <shalecharles@gmail.com>
2020-10-01 09:47:38 +1000

    Isofox: added new class

---
+++ isofox/src/main/java/com/hartwig/hmftools/isofox/adjusts/FragmentSize.java
@@ -0,0 +1,13 @@
+package com.hartwig.hmftools.isofox.adjusts;
+
+public class FragmentSize
+{
+    public final int Length;
+    public int Frequency;
+
+    public FragmentSize(final int length, final int frequency)
+    {
+        Length = length;
+        Frequency = frequency;
+    }
+}
