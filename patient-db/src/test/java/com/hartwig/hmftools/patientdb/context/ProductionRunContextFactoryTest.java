21ce8709532e91017bbca383bd126838921c5d37
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-09-30 15:30:48 +0200

    Remove old and obsolete code

--- patient-db/src/test/java/com/hartwig/hmftools/patientdb/context/ProductionRunContextFactoryTest.java
+++ patient-db/src/test/java/com/hartwig/hmftools/patientdb/context/ProductionRunContextFactoryTest.java
@@ -3,6 +3,7 @@ package com.hartwig.hmftools.patientdb.context;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
 
 import com.google.common.io.Resources;
 import com.hartwig.hmftools.common.utils.io.exception.MalformedFileException;
@@ -14,13 +15,13 @@ public class ProductionRunContextFactoryTest {
     private static final String RESOURCE_DIR = Resources.getResource("context").getPath();
 
     @Test
-    public void picksMetadataWhenAvailable() throws MalformedFileException {
+    public void picksMetadataWhenAvailable() throws IOException {
         String runDirectory = RESOURCE_DIR + File.separator + "RunDirSomatic";
         assertNotNull(ProductionRunContextFactory.fromRunDirectory(runDirectory));
     }
 
     @Test(expected = MalformedFileException.class)
-    public void throwExceptionWhenNoMetaData() throws MalformedFileException {
+    public void throwExceptionWhenNoMetaData() throws IOException {
         String runDirectory = RESOURCE_DIR + File.separator + "DoesNotExist";
         assertNotNull(ProductionRunContextFactory.fromRunDirectory(runDirectory));
     }
