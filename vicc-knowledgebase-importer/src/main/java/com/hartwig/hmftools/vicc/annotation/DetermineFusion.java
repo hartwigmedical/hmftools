58153646e918204a456754e96c6ca03b712c21b7
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-10-07 14:59:25 +0200

    DEV-1416: improve mapping of exons

--- vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/annotation/DetermineFusion.java
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/annotation/DetermineFusion.java
@@ -32,11 +32,7 @@ final class DetermineFusion {
     private static FusionEvent extractKeyFusion(@NotNull String feature, @Nullable String biomarkerType, @Nullable String provenanceRule,
             @NotNull String proteinAnnotation) {
         if (!FeatureTypeExtractor.IGNORE.contains(feature)) { // Extract internal fusion
-            if (FeatureTypeExtractor.INTERNAL_FUSION.contains(proteinAnnotation) && proteinAnnotation.contains("[a-zA-Z]+")) {
-                return FusionEvent.FUSION_PAIR;
-            } else if (feature.contains("DELETION") && FeatureTypeExtractor.INTERNAL_FUSION.contains(biomarkerType)) {
-                return FusionEvent.FUSION_PAIR;
-            } else if (feature.contains("DELETION") && FeatureTypeExtractor.INTERNAL_FUSION.contains(provenanceRule)) {
+            if (FeatureTypeExtractor.INTERNAL_FUSION.contains(proteinAnnotation) || proteinAnnotation.contains("[a-zA-Z]+")) {
                 return FusionEvent.FUSION_PAIR;
             } else if (FeatureTypeExtractor.SEARCH_FUSION_PAIRS.contains(proteinAnnotation)) {
                 return FusionEvent.FUSION_PAIR;
@@ -59,15 +55,9 @@ final class DetermineFusion {
                 if (FeatureTypeExtractor.SEARCH_FUSION_PAIRS.contains(biomarkerType)) {
                     return FusionEvent.FUSION_PAIR;
                 }
-            } else if (feature.toLowerCase().contains("exon") && feature.toLowerCase().contains("deletion") && feature.contains("-")
-                    || feature.contains("&")) {// Extract internal fusion
-                return FusionEvent.FUSION_PAIR;
             } else {
                 return FusionEvent.UNKNOWN;
             }
-        }
-
-        //TODO: check why this is needed??
-        return FusionEvent.UNKNOWN;
+        } return FusionEvent.UNKNOWN;
     }
 }
--- vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/annotation/FeatureTypeExtractor.java
+++ vicc-knowledgebase-importer/src/main/java/com/hartwig/hmftools/vicc/annotation/FeatureTypeExtractor.java
@@ -46,7 +46,7 @@ public final class FeatureTypeExtractor {
     public static final Set<String> IGNORE = Sets.newHashSet("3' EXON DELETION");
 
     public static final Set<String> INTERNAL_FUSION =
-            Sets.newHashSet("(Partial", "Inframe Deletion", "is_deletion", "EGFRvIII", "EGFRvV", "EGFRvII", "ITD");
+            Sets.newHashSet("(Partial", "is_deletion", "EGFRvIII", "EGFRvV", "EGFRvII", "ITD");
 
     public static final Set<String> GENE_LEVEL = Sets.newHashSet("Gain-of-function Mutations",
             "Gain-of-Function",
@@ -141,7 +141,7 @@ public final class FeatureTypeExtractor {
         } else if (DetermineFusion.isFusion(featureName, biomarkerType, provenanceRule, proteinAnnotation) && !featureName.contains(
                 "p61BRAF")) {
             return FeatureType.FUSION_PAIR;
-        } else if (FeatureTypeExtractor.GENE_EXON.contains(event) && featureName.contains("-")) {
+        } else if (FeatureTypeExtractor.GENE_EXON.contains(event) && featureName.contains("-") && featureName.contains("DELETION")) {
             return FeatureType.FUSION_PAIR;
         } else if (DetermineFusion.isFusionPromiscuous(featureName, biomarkerType, provenanceRule, proteinAnnotation)) {
             return FeatureType.FUSION_PROMISCUOUS;
