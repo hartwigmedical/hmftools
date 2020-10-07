300045e45878b5723e06a6ab9984fbe316ce37e3
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-10-07 14:59:52 +0200

    Add NONSENSE_OR_FRAMESHIFT mutation filter

--- serve/src/main/java/com/hartwig/hmftools/serve/actionability/range/MutationTypeFilter.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/actionability/range/MutationTypeFilter.java
@@ -3,6 +3,7 @@ package com.hartwig.hmftools.serve.actionability.range;
 public enum MutationTypeFilter {
     INFRAME_DELETION,
     INFRAME_INSERTION,
+    NONSENSE_OR_FRAMESHIFT,
     MISSENSE,
     SPLICE,
     ANY
