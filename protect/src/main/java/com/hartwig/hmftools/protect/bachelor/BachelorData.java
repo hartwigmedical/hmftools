f488c196dc58877ad1352d688a17e52b935c9974
Jonathan Baber <jonbaber@gmail.com>
2020-10-21 08:52:16 +1000

    bachelor data

---
+++ protect/src/main/java/com/hartwig/hmftools/protect/bachelor/BachelorData.java
@@ -0,0 +1,16 @@
+package com.hartwig.hmftools.protect.bachelor;
+
+import java.util.List;
+
+import com.hartwig.hmftools.protect.variants.ReportableVariant;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public interface BachelorData {
+    @NotNull
+    List<ReportableVariant> germlineVariants();
+}
---
+++ protect/src/main/java/com/hartwig/hmftools/protect/bachelor/BachelorDataLoader.java
@@ -0,0 +1,46 @@
+package com.hartwig.hmftools.protect.bachelor;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Lists;
+import com.hartwig.hmftools.common.chord.ChordStatus;
+import com.hartwig.hmftools.common.variant.germline.ReportableGermlineVariant;
+import com.hartwig.hmftools.common.variant.germline.ReportableGermlineVariantFile;
+import com.hartwig.hmftools.protect.purple.PurpleData;
+import com.hartwig.hmftools.protect.variants.ReportableVariant;
+import com.hartwig.hmftools.protect.variants.ReportableVariantFactory;
+import com.hartwig.hmftools.protect.variants.germline.DriverGermlineVariant;
+import com.hartwig.hmftools.protect.variants.germline.FilterGermlineVariants;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+public class BachelorDataLoader {
+
+    private static final Logger LOGGER = LogManager.getLogger(BachelorDataLoader.class);
+
+    public static BachelorData load(@NotNull PurpleData purpleData, @NotNull String bachelorTsv, @NotNull ChordStatus chordStatus)
+            throws IOException {
+        List<ReportableVariant> result = Lists.newArrayList();
+
+        List<ReportableGermlineVariant> variants = ReportableGermlineVariantFile.read(bachelorTsv);
+        Set<String> allowedGenes = variants.stream().map(ReportableGermlineVariant::gene).collect(Collectors.toSet());
+
+        List<DriverGermlineVariant> driverGermlineVariants = FilterGermlineVariants.filterGermlineVariantsForReporting(variants,
+                allowedGenes,
+                purpleData.geneCopyNumbers(),
+                purpleData.somaticVariants().stream().map(ReportableVariant::gene).collect(Collectors.toSet()),
+                chordStatus);
+
+        result.addAll(ReportableVariantFactory.reportableGermlineVariants(driverGermlineVariants));
+
+        LOGGER.info("Loaded {} germline variants from {}", result.size(), bachelorTsv);
+        return ImmutableBachelorData.builder().addAllGermlineVariants(result).build();
+
+    }
+
+}
