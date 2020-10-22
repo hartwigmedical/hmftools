d25c016649309829715c3dbda3865dd372074c64
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-10-22 13:55:49 +0200

    DEV-1575: add test

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/diseaseOntology/diseaseOntology.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/diseaseOntology/diseaseOntology.java
@@ -1,13 +1,59 @@
 package com.hartwig.hmftools.patientdb.diseaseOntology;
 
 
+import java.io.File;
+import java.util.logging.Level;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.MissingImportHandlingStrategy;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.InferenceType;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
-public class diseaseOntology {
-    private static final Logger LOGGER = LogManager.getLogger(diseaseOntology.class);
+public final class DiseaseOntology {
+    private static final Logger LOGGER = LogManager.getLogger(DiseaseOntology.class);
 
     private static final IRI DISEASE_IRI = IRI.create("http://purl.obolibrary.org/obo/DOID_4");
+
+    public static void readDoid(@NotNull String doidFile) throws OWLOntologyCreationException {
+        OWLOntology ontology = createOntology(doidFile);
+        OWLReasoner reasoner = createReasoner(ontology);
+        OWLClass a = createDiseaseMapping(ontology, reasoner);
+    }
+
+    private static OWLOntology createOntology(@NotNull String doidFile) throws OWLOntologyCreationException {
+        java.util.logging.Logger logger = java.util.logging.Logger.getLogger("org.obolibrary.oboformat.parser.OBOFormatParser");
+        logger.setLevel(Level.SEVERE);
+        OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
+        OWLOntologyLoaderConfiguration config = new OWLOntologyLoaderConfiguration().setFollowRedirects(true)
+                .setMissingImportHandlingStrategy(MissingImportHandlingStrategy.SILENT);
+
+        return ontologyManager.loadOntologyFromOntologyDocument(new FileDocumentSource(new File(doidFile)), config);
+    }
+
+    private static OWLReasoner createReasoner(@NotNull OWLOntology ontology) {
+        OWLReasoner reasoner = new Reasoner.ReasonerFactory().createReasoner(ontology);
+        reasoner.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+        return reasoner;
+    }
+
+    private static OWLClass createDiseaseMapping(@NotNull OWLOntology ontology, @NotNull OWLReasoner reasoner) {
+
+        OWLClass diseaseClass = ontology.getOWLOntologyManager().getOWLDataFactory().getOWLClass(DISEASE_IRI);
+        return diseaseClass;
+        //        val diseases = setOf(diseaseClass) + subClasses(diseaseClass, reasoner)
+        //        val synonyms = diseases.flatMap { disease -> getSynonyms(disease, ontology).map { Pair(it, disease) } }
+        //        return diseases.associateBy { getLabel(it, ontology) } + synonyms.toMap()
+    }
 
 }
--- patient-db/src/test/java/com/hartwig/hmftools/patientdb/diseaseOntology/DiseaseOntologyTest.java
+++ patient-db/src/test/java/com/hartwig/hmftools/patientdb/diseaseOntology/DiseaseOntologyTest.java
@@ -1,7 +1,20 @@
 package com.hartwig.hmftools.patientdb.diseaseOntology;
 
-import static org.junit.Assert.*;
+
+import com.google.common.io.Resources;
+
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public class DiseaseOntologyTest {
+
+    public static final String DOID_FILE = Resources.getResource("diseaseOntology/doid.owl").getPath();
+
+    public static void canLoadDoidFile() throws OWLOntologyCreationException {
+        try {
+            DiseaseOntology.readDoid(DOID_FILE);
+        } catch (OWLOntologyCreationException e) {
+            throw new OWLOntologyCreationException("Could not load doid file!");
+        }
+    }
 
 }
