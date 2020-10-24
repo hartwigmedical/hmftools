6198477eb46f75c5b298ff307af2a39e8f12c274
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-10-24 16:34:59 +0200

    Remove support for reading v1 CHORD output

--- hmf-common/src/test/java/com/hartwig/hmftools/common/chord/ChordFileReaderTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/chord/ChordFileReaderTest.java
@@ -11,42 +11,24 @@ import org.junit.Test;
 
 public class ChordFileReaderTest {
 
-    private static final String V1_CHORD_FILE = Resources.getResource("chord/v1/sample_chord_prediction.txt").getPath();
-    private static final String V2_CHORD_FILE = Resources.getResource("chord/v2/sample_chord_prediction.txt").getPath();
+    private static final String CHORD_FILE = Resources.getResource("chord/sample_chord_prediction.txt").getPath();
     private static final double EPSILON = 1.0E-10;
 
-    private static final double V1_BRCA1 = 0.230;
-    private static final double V1_BRCA2 = 0.400;
-    private static final double V1_HRD = 0.630;
-
-    private static final double V2_BRCA1 = 0.1;
-    private static final double V2_BRCA2 = 0.2;
-    private static final double V2_HRD = 0.3;
-    private static final ChordStatus V2_HR_STATUS = ChordStatus.HR_PROFICIENT;
-    private static final String V2_HRD_TYPE = "none";
+    private static final double BRCA1 = 0.1;
+    private static final double BRCA2 = 0.2;
+    private static final double HRD = 0.3;
+    private static final ChordStatus HR_STATUS = ChordStatus.HR_PROFICIENT;
+    private static final String HRD_TYPE = "none";
 
     @Test
-    public void canLoadV1ChordFile() throws IOException {
-        ChordAnalysis chordAnalysis = ChordFileReader.read(V1_CHORD_FILE);
+    public void canLoadChordFile() throws IOException {
+        ChordAnalysis chordAnalysis = ChordFileReader.read(CHORD_FILE);
 
-        assertEquals(V1_BRCA1, chordAnalysis.BRCA1Value(), EPSILON);
-        assertEquals(V1_BRCA2, chordAnalysis.BRCA2Value(), EPSILON);
-        assertEquals(V1_HRD, chordAnalysis.hrdValue(), EPSILON);
-        assertEquals(ChordStatus.UNKNOWN, chordAnalysis.hrStatus());
-        assertEquals(ChordFileReader.V1_NA, chordAnalysis.hrdType());
-        assertEquals(ChordFileReader.V1_NA, chordAnalysis.remarksHrStatus());
-        assertEquals(ChordFileReader.V1_NA, chordAnalysis.remarksHrdType());
-    }
-
-    @Test
-    public void canLoadV2ChordFile() throws IOException {
-        ChordAnalysis chordAnalysis = ChordFileReader.read(V2_CHORD_FILE);
-
-        assertEquals(V2_BRCA1, chordAnalysis.BRCA1Value(), EPSILON);
-        assertEquals(V2_BRCA2, chordAnalysis.BRCA2Value(), EPSILON);
-        assertEquals(V2_HRD, chordAnalysis.hrdValue(), EPSILON);
-        assertEquals(V2_HR_STATUS, chordAnalysis.hrStatus());
-        assertEquals(V2_HRD_TYPE, chordAnalysis.hrdType());
+        assertEquals(BRCA1, chordAnalysis.BRCA1Value(), EPSILON);
+        assertEquals(BRCA2, chordAnalysis.BRCA2Value(), EPSILON);
+        assertEquals(HRD, chordAnalysis.hrdValue(), EPSILON);
+        assertEquals(HR_STATUS, chordAnalysis.hrStatus());
+        assertEquals(HRD_TYPE, chordAnalysis.hrdType());
         assertEquals(Strings.EMPTY, chordAnalysis.remarksHrStatus());
         assertEquals(Strings.EMPTY, chordAnalysis.remarksHrdType());
     }
