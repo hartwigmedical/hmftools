831af03f8fb540176d255ffba33fc4ede9dd603c
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-10-26 16:38:53 +0100

    Extend main class

--- hmf-common/src/main/java/com/hartwig/hmftools/common/doid/Doid.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/doid/Doid.java
@@ -11,9 +11,19 @@ import org.jetbrains.annotations.Nullable;
              passAnnotations = { NotNull.class, Nullable.class })
 public abstract class Doid {
 
+    // TODO rename to DoidNode
     @NotNull
     public abstract List<DoidEntry> entries();
 
     @NotNull
     public abstract DoidEdges edges();
+
+    @NotNull
+    public abstract String id();
+
+    // TODO Is this a list of strings?
+    @NotNull
+    public abstract List<String> equivalentNodsSets();
+
+    // TODO Add all other fields of a graphElement.
 }
