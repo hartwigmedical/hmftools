b259564a08ac540b2b93cf51d230f5c4cb28c2a5
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-11-04 13:15:56 +0100

    DEV-1399: Remove filtering of hotspots on fusions

--- serve/src/main/java/com/hartwig/hmftools/serve/sources/vicc/filter/FilterFactory.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/vicc/filter/FilterFactory.java
@@ -156,23 +156,6 @@ final class FilterFactory {
 
         // exon 12 does not exist in canonical transcript of NPM1
         FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "NPM1", "EXON 12 MUTATION"));
-
-        // Hotspots on fusion genes are not supported (COMBINED events)
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ABL1", "BCR-ABL E255K"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ABL1", "BCR-ABL F317L"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ABL1", "BCR-ABL F486S"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ABL1", "BCR-ABL G398R"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ABL1", "BCR-ABL T334I"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ALK", "EML4-ALK L1152R"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "PDGFRA", "FIP1L1-PDGFRA T674I"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "NTRK1", "LMNA-NTRK1 G595R AND G667C"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "NTRK3", "ETV6-NTRK3 G623R"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "PML", "PML-RARA A216V"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "PML", "PML-RARA L218P"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ROS1", "CD74-ROS1 G2032R"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ROS1", "CD74-ROS1 G2101A"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ROS1", "CD74-ROS1 L2026M"));
-        FEATURE_KEYS_TO_FILTER.add(new FilterKey(ViccSource.CIVIC, "ROS1", "CD74-ROS1 L2155S"));
     }
 
     private FilterFactory() {
