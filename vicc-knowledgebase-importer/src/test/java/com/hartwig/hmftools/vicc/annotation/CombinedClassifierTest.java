b25a815046f150e65c8016d6427607a87f5e0b75
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-11-06 15:04:23 +0100

    DEV-1416: Improve combined classifier

--- vicc-knowledgebase-importer/src/test/java/com/hartwig/hmftools/vicc/annotation/CombinedClassifierTest.java
+++ vicc-knowledgebase-importer/src/test/java/com/hartwig/hmftools/vicc/annotation/CombinedClassifierTest.java
@@ -13,6 +13,8 @@ public class CombinedClassifierTest {
         assertTrue(CombinedClassifier.isCombinedEvent("EGFR inframe deletion (L747),inframe insertion (P753PS)", "EGFR"));
         assertTrue(CombinedClassifier.isCombinedEvent("KIT inframe deletion (416-422),inframe insertion (416-422)", "KIT"));
         assertTrue(CombinedClassifier.isCombinedEvent("CSF3R frameshift variant (D771),frameshift variant (S783)", "CSF3R"));
+        assertTrue(CombinedClassifier.isCombinedEvent("NOTCH1 splice donor variant (2245-2536),splice acceptor variant (2245-2536),"
+                + "stop gained (2245-2536),stop lost (2245-2536),frameshift variant (2245-2536)", "NOTCH1"));
 
         assertTrue(CombinedClassifier.isCombinedEvent("BCR-ABL F486S", "ABL"));
         assertTrue(CombinedClassifier.isCombinedEvent("NPM1-ALK  amp", "ALK"));
