0247d0c48b27d5d8b060872046d1ba31e3a7279a
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-11-17 12:19:03 +0100

    Move code generation to abstract classes

--- serve/src/main/java/com/hartwig/hmftools/serve/fusion/FusionPair.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/fusion/FusionPair.java
@@ -1,29 +1,25 @@
 package com.hartwig.hmftools.serve.fusion;
 
-import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Value.Immutable
-@Value.Style(allParameters = true,
-             passAnnotations = { NotNull.class, Nullable.class })
 public interface FusionPair {
 
     @NotNull
-    public abstract String geneUp();
+    String geneUp();
 
     @Nullable
-    public abstract Integer minExonUp();
+    Integer minExonUp();
 
     @Nullable
-    public abstract Integer maxExonUp();
+    Integer maxExonUp();
 
     @NotNull
-    public abstract String geneDown();
+    String geneDown();
 
     @Nullable
-    public abstract Integer minExonDown();
+    Integer minExonDown();
 
     @Nullable
-    public abstract Integer maxExonDown();
+    Integer maxExonDown();
 }
--- serve/src/main/java/com/hartwig/hmftools/serve/fusion/KnownFusionPair.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/fusion/KnownFusionPair.java
@@ -1,5 +1,12 @@
 package com.hartwig.hmftools.serve.fusion;
 
-public abstract class KnownFusionPair implements FusionPair{
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(allParameters = true,
+             passAnnotations = { NotNull.class, Nullable.class })
+public abstract class KnownFusionPair implements FusionPair {
 
 }
