ea263f63dbc0686cdf4c84dedcf6fa034084245d
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-11-17 10:53:32 +0100

    Typo

--- serve/src/main/java/com/hartwig/hmftools/serve/actionability/signature/SignatureName.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/actionability/signature/SignatureName.java
@@ -3,6 +3,6 @@ package com.hartwig.hmftools.serve.actionability.signature;
 public enum SignatureName {
     MICROSATELLITE_UNSTABLE,
     HIGH_TUMOR_MUTATIONAL_LOAD,
-    HOMOLOGOUS_RECOMBINATION_DEFICIENY,
+    HOMOLOGOUS_RECOMBINATION_DEFICIENCY,
     UNKNOWN
 }
