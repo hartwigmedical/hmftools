537d177bf36e5f674d1a57c0dab922cf441c9e38
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-11-24 17:13:59 +0100

    DEV-1149: Refactor filter and curation, add test for mutation interpretation

---
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/iclusion/IclusionReader.java
@@ -0,0 +1,59 @@
+package com.hartwig.hmftools.serve.sources.iclusion;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.hartwig.hmftools.iclusion.data.IclusionTrial;
+import com.hartwig.hmftools.iclusion.io.IclusionTrialFile;
+import com.hartwig.hmftools.serve.sources.iclusion.curation.IclusionCurator;
+import com.hartwig.hmftools.serve.sources.iclusion.filter.IclusionFilter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+public final class IclusionReader {
+
+    private static final Logger LOGGER = LogManager.getLogger(IclusionReader.class);
+
+    private IclusionReader() {
+    }
+
+    @NotNull
+    public static List<IclusionTrial> readAndCurate(@NotNull String iClusionTrialTsv) throws IOException {
+        LOGGER.info("Reading iClusion trial TSV from '{}'", iClusionTrialTsv);
+        List<IclusionTrial> trials = IclusionTrialFile.read(iClusionTrialTsv);
+        LOGGER.info(" Read {} trials from {}", trials.size(), iClusionTrialTsv);
+
+        return filter(curate(trials));
+    }
+
+    @NotNull
+    private static List<IclusionTrial> curate(@NotNull List<IclusionTrial> trials) {
+        IclusionCurator curator = new IclusionCurator();
+
+        LOGGER.info("Curating {} iClusion trials", trials.size());
+        List<IclusionTrial> curatedTrials = curator.run(trials);
+        LOGGER.info(" Finished iClusion curation. {} curated trials remaining. {} trials have been removed",
+                curatedTrials.size(),
+                trials.size() - curatedTrials.size());
+
+        curator.reportUnusedCurationEntries();
+
+        return curatedTrials;
+    }
+
+    @NotNull
+    private static List<IclusionTrial> filter(@NotNull List<IclusionTrial> trials) {
+        IclusionFilter filter = new IclusionFilter();
+
+        LOGGER.info("Filtering {} iClusion entries", trials.size());
+        List<IclusionTrial> filteredTrials = filter.run(trials);
+        LOGGER.info("  Finished iClusion filtering. {} trials remaining. {} entries have been removed",
+                filteredTrials.size(),
+                trials.size() - filteredTrials.size());
+        filter.reportUnusedFilterEntries();
+
+        return filteredTrials;
+    }
+}
--- serve/src/main/java/com/hartwig/hmftools/serve/sources/iclusion/tools/IclusionExtractorTestApp.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/iclusion/tools/IclusionExtractorTestApp.java
@@ -10,14 +10,13 @@ import java.util.List;
 import com.hartwig.hmftools.common.drivercatalog.panel.DriverGene;
 import com.hartwig.hmftools.common.drivercatalog.panel.DriverGeneFile;
 import com.hartwig.hmftools.iclusion.data.IclusionTrial;
-import com.hartwig.hmftools.iclusion.io.IclusionTrialFile;
 import com.hartwig.hmftools.serve.RefGenomeVersion;
 import com.hartwig.hmftools.serve.hotspot.ProteinResolver;
 import com.hartwig.hmftools.serve.hotspot.ProteinResolverFactory;
 import com.hartwig.hmftools.serve.sources.ExtractionOutput;
 import com.hartwig.hmftools.serve.sources.iclusion.IclusionExtractor;
-import com.hartwig.hmftools.serve.sources.iclusion.curation.IclusionCurator;
-import com.hartwig.hmftools.serve.sources.iclusion.filter.IclusionFilter;
+import com.hartwig.hmftools.serve.sources.iclusion.IclusionReader;
+import com.hartwig.hmftools.serve.sources.iclusion.IclusionUtil;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -58,26 +57,20 @@ public class IclusionExtractorTestApp {
             Files.createDirectory(outputPath);
         }
 
-        LOGGER.debug("Configured '{}' as the iclusion trial TSV path", iclusionTrialTsv);
+        String iclusionMutationTsv = outputDir + "/iclusionMutations.tsv";
+
+        LOGGER.debug("Configured '{}' as the iClusion trial TSV path", iclusionTrialTsv);
         LOGGER.debug("Configured '{}' as the driver gene TSV path", driverGeneTsvPath);
+        LOGGER.debug("Configured '{}' as the iClusion mutation TSV path", iclusionMutationTsv);
 
         List<DriverGene> driverGenes = DriverGeneFile.read(driverGeneTsvPath);
         LOGGER.debug(" Read {} driver genes from {}", driverGenes.size(), driverGeneTsvPath);
 
-        List<IclusionTrial> trials = IclusionTrialFile.read(iclusionTrialTsv);
-        LOGGER.info(" Read {} trials from {}", trials.size(), iclusionTrialTsv);
+        List<IclusionTrial> trials = IclusionReader.readAndCurate(iclusionTrialTsv);
 
-        IclusionFilter filter = new IclusionFilter();
-        List<IclusionTrial> filteredTrials = filter.run(trials);
-        LOGGER.info("  {} trials remaining after filtering", filteredTrials.size());
-        filter.reportUnusedFilterEntries();
-
-        LOGGER.info(" Starting iClusion trial curation");
-        IclusionCurator curator = new IclusionCurator();
-        List<IclusionTrial> curatedTrials = curator.run(filteredTrials);
-        curator.reportUnusedCurationEntries();
-
-        ExtractionOutput output = new IclusionExtractor().extractFromIclusionTrials(curatedTrials);
+        ExtractionOutput output = new IclusionExtractor().extractFromIclusionTrials(trials);
         LOGGER.info("Generated {}", output);
+
+        IclusionUtil.writeIclusionMutationTypes(iclusionMutationTsv, trials);
     }
 }
