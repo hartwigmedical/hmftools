f2283f7dc43e50f8586612119706c2c038f3bd4d
Charles Shale <shalecharles@gmail.com>
2020-11-25 12:47:44 +1100

    Isofox: cache somatic variants, adjust DEL position match, filter out non-PASS variants

--- isofox/src/main/java/com/hartwig/hmftools/isofox/novel/cohort/SpliceVariantMatcher.java
+++ isofox/src/main/java/com/hartwig/hmftools/isofox/novel/cohort/SpliceVariantMatcher.java
@@ -12,6 +12,7 @@ import static com.hartwig.hmftools.common.utils.io.FileWriterUtils.createFieldsI
 import static com.hartwig.hmftools.common.utils.sv.StartEndIterator.SE_END;
 import static com.hartwig.hmftools.common.utils.sv.StartEndIterator.SE_START;
 import static com.hartwig.hmftools.common.utils.sv.SvRegion.positionWithin;
+import static com.hartwig.hmftools.common.variant.structural.StructuralVariantFactory.PASS;
 import static com.hartwig.hmftools.isofox.IsofoxConfig.ISF_LOGGER;
 import static com.hartwig.hmftools.isofox.cohort.CohortAnalysisType.ALT_SPLICE_JUNCTION;
 import static com.hartwig.hmftools.isofox.cohort.CohortConfig.formSampleFilenames;
@@ -64,15 +65,19 @@ public class SpliceVariantMatcher
     private Map<String,List<SpliceVariant>> mSampleSpliceVariants;
 
     private BufferedWriter mWriter;
+    private BufferedWriter mSomaticWriter;
     private final Map<String,Integer> mFieldsMap;
 
     private final Map<String,EnsemblGeneData> mGeneDataMap;
     private final AltSjFilter mAltSjFilter;
 
     private final Map<String,Integer> mCohortAltSJs;
+    private final boolean mWriteSomaticVariants;
 
     private static final String SOMATIC_VARIANT_FILE = "somatic_variant_file";
     private static final String COHORT_ALT_SJ_FILE = "cohort_alt_sj_file";
+    private static final String INCLUDE_ALL_TRANSCRIPTS = "include_all_transcripts";
+    private static final String WRITE_SOMATIC_VARIANTS = "write_somatic_variants";
 
     public SpliceVariantMatcher(final CohortConfig config, final CommandLine cmd)
     {
@@ -81,8 +86,11 @@ public class SpliceVariantMatcher
         mGeneDataMap = Maps.newHashMap();
         mCohortAltSJs = Maps.newHashMap();
 
+        boolean allTranscripts = cmd.hasOption(INCLUDE_ALL_TRANSCRIPTS);
+        mWriteSomaticVariants = cmd.hasOption(WRITE_SOMATIC_VARIANTS);
+
         mGeneTransCache = new EnsemblDataCache(mConfig.EnsemblDataCache, RefGenomeVersion.HG37);
-        mGeneTransCache.setRequiredData(true, false, false, false);
+        mGeneTransCache.setRequiredData(true, false, false, !allTranscripts);
         mGeneTransCache.load(false);
         mSampleSpliceVariants = Maps.newHashMap();
 
@@ -116,6 +124,8 @@ public class SpliceVariantMatcher
     {
         options.addOption(SOMATIC_VARIANT_FILE, true, "File with somatic variants potentially affecting splicing");
         options.addOption(COHORT_ALT_SJ_FILE, true, "Cohort frequency for alt SJs");
+        options.addOption(WRITE_SOMATIC_VARIANTS, false, "Write out somatic variants for subsequent non-DB loading");
+        options.addOption(INCLUDE_ALL_TRANSCRIPTS, false, "Consider all transcripts, not just canonical (default: false)");
     }
 
     public void processAltSpliceJunctions()
@@ -139,8 +149,8 @@ public class SpliceVariantMatcher
         ISF_LOGGER.info("splice variant matching complete");
 
         closeBufferedWriter(mWriter);
+        closeBufferedWriter(mSomaticWriter);
     }
-
 
     public void evaluateSpliceVariants(final String sampleId, final List<AltSpliceJunction> altSpliceJunctions)
     {
@@ -156,6 +166,9 @@ public class SpliceVariantMatcher
                 sampleId, spliceVariants.size());
 
         spliceVariants.forEach(x -> evaluateSpliceVariant(sampleId, altSpliceJunctions, x));
+
+        if(mWriteSomaticVariants)
+            spliceVariants.forEach(x -> writeSomaticVariant(sampleId, x));
     }
 
     private final List<SpliceVariant> getSomaticVariants(final String sampleId)
@@ -173,9 +186,11 @@ public class SpliceVariantMatcher
             if(!mGeneDataMap.isEmpty() && !mGeneDataMap.containsKey(variant.gene()))
                 continue;
 
+            if(variant.filter() != "PASS")
+
             spliceVariants.add(new SpliceVariant(
                     variant.gene(), variant.chromosome(), (int)variant.position(), variant.type(),variant.ref(), variant.alt(),
-                    variant.worstEffect(), variant.canonicalHgvsCodingImpact(), variant.trinucleotideContext()));
+                    variant.canonicalEffect(), variant.canonicalHgvsCodingImpact(), variant.trinucleotideContext()));
         }
 
         return spliceVariants;
@@ -216,7 +231,7 @@ public class SpliceVariantMatcher
         if(delLength <= 0)
             return false;
 
-        if(altSJ.SpliceJunction[SE_START] == variant.Position && altSJ.SpliceJunction[SE_END] == variant.Position + delLength)
+        if(altSJ.SpliceJunction[SE_START] == variant.Position && altSJ.SpliceJunction[SE_END] == variant.Position + delLength + 1)
             return true;
 
         return false;
@@ -239,6 +254,10 @@ public class SpliceVariantMatcher
         final List<TranscriptData> transDataList = mGeneTransCache.getTranscripts(geneData.GeneId);
 
         boolean isWithinSpliceRegion = false;
+
+        AcceptorDonorType closestAcceptorDonorType = AcceptorDonorType.NONE;
+        int closestExonDistance = 0;
+        String closestTransStr = "";
 
         for(final TranscriptData transData : transDataList)
         {
@@ -286,6 +305,13 @@ public class SpliceVariantMatcher
 
                 isWithinSpliceRegion = true;
 
+                if(closestAcceptorDonorType == NONE || abs(exonDistance) < abs(closestExonDistance))
+                {
+                    closestAcceptorDonorType = acceptorDonorType;
+                    closestExonDistance = exonDistance;
+                    closestTransStr = String.format("%s;%d", transData.TransName, exon.ExonRank);
+                }
+
                 final ExonData prevExon = i > 0 ? transData.exons().get(i - 1) : null;
                 final ExonData nextExon = i < transData.exons().size() - 1 ? transData.exons().get(i + 1) : null;
 
@@ -327,7 +353,8 @@ public class SpliceVariantMatcher
         if(matchedAltSJs.isEmpty() && isWithinSpliceRegion)
         {
             writeMatchData(
-                    sampleId, variant, geneData, null, SpliceVariantMatchType.NONE, AcceptorDonorType.NONE, -1, "");
+                    sampleId, variant, geneData, null, SpliceVariantMatchType.NONE,
+                    closestAcceptorDonorType, closestExonDistance, closestTransStr);
         }
 
         return matchedAltSJs;
@@ -382,6 +409,15 @@ public class SpliceVariantMatcher
             mWriter.write(",AsjCohortCount,AsjTransData");
 
             mWriter.newLine();
+
+            if(mWriteSomaticVariants)
+            {
+                final String somVarFileName = mConfig.formCohortFilename("somatic_variants.csv");
+                mSomaticWriter = createBufferedWriter(somVarFileName, false);
+
+                mSomaticWriter.write("SampleId,GeneName,Chromosome,Position,Type,CodingEffect,Ref,Alt,HgvsImpact,TriNucContext");
+                mSomaticWriter.newLine();
+            }
         }
         catch(IOException e)
         {
@@ -413,14 +449,33 @@ public class SpliceVariantMatcher
             else
             {
                 mWriter.write(String.format(",%s,%s,%s,%d,%d,%d,%d,%d,%s",
-                        UNKNOWN, AltSpliceJunctionContext.UNKNOWN, AltSpliceJunctionContext.UNKNOWN, -1, -1, 0, 0, 0, ""));
+                        UNKNOWN, AltSpliceJunctionContext.UNKNOWN, AltSpliceJunctionContext.UNKNOWN, -1, -1, 0, 0, 0, 0, ""));
             }
 
             mWriter.newLine();
         }
         catch(IOException e)
         {
-            ISF_LOGGER.error("failed to write splice variant file: {}", e.toString());
+            ISF_LOGGER.error("failed to write splice variant matching file: {}", e.toString());
+        }
+    }
+
+    private void writeSomaticVariant(final String sampleId, final SpliceVariant variant)
+    {
+        if(mSomaticWriter == null)
+            return;
+
+        try
+        {
+            mSomaticWriter.write(String.format("%s,%s,%s,%d,%s,%s,%s,%s,%s,%s",
+                    sampleId, variant.GeneName, variant.Chromosome, variant.Position, variant.Type,
+                    variant.CodingEffect, variant.Ref, variant.Alt, variant.HgvsCodingImpact, variant.TriNucContext));
+
+            mSomaticWriter.newLine();
+        }
+        catch(IOException e)
+        {
+            ISF_LOGGER.error("failed to write splice variant data: {}", e.toString());
         }
     }
 
@@ -429,8 +484,8 @@ public class SpliceVariantMatcher
         if(mCohortAltSJs.isEmpty())
             return 0;
 
-        final String altSjKey = String.format("%s;%s;%s;%s",
-            altSJ.type(), altSJ.Chromosome, altSJ.SpliceJunction[SE_START], altSJ.SpliceJunction[SE_END]);
+        final String altSjKey = String.format("%s;%s;%s",
+            altSJ.Chromosome, altSJ.SpliceJunction[SE_START], altSJ.SpliceJunction[SE_END]); // altSJ.type(),
 
         Integer cohortCount = mCohortAltSJs.get(altSjKey);
         return cohortCount != null ? cohortCount : 0;
@@ -455,7 +510,7 @@ public class SpliceVariantMatcher
 
             // GeneId,CancerType,SampleCount,Chromosome,Type,SjStart,SjEnd
             int sampleCountIndex = fieldsMap.get("SampleCount");
-            int typeIndex = fieldsMap.get("Type");
+            // int typeIndex = fieldsMap.get("Type");
             int chromosomeIndex = fieldsMap.get("Chromosome");
             int sjStartPosIndex = fieldsMap.get("SjStart");
             int sjEndPosIndex = fieldsMap.get("SjEnd");
@@ -464,13 +519,15 @@ public class SpliceVariantMatcher
             {
                 final String[] items = data.split(DELIMITER);
 
-                final String altSjKey = String.format("%s;%s;%s;%s",
-                        items[typeIndex], items[chromosomeIndex], items[sjStartPosIndex], items[sjEndPosIndex]);
+                final String altSjKey = String.format("%s;%s;%s",
+                        items[chromosomeIndex], items[sjStartPosIndex], items[sjEndPosIndex]);
 
                 final int sampleCount = Integer.parseInt(items[sampleCountIndex]);
 
                 mCohortAltSJs.put(altSjKey, sampleCount);
             }
+
+            ISF_LOGGER.info("loaded {} cohort alt-SJ records", mCohortAltSJs.size());
         }
         catch(IOException e)
         {
