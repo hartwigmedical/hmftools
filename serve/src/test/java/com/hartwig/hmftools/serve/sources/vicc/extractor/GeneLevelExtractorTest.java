1739a286e3715424c3cf78dcac60c46acff2de1a
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-11-26 15:04:38 +0100

    DEV-1416: Add test for extract gene level

--- serve/src/test/java/com/hartwig/hmftools/serve/sources/vicc/extractor/GeneLevelExtractorTest.java
+++ serve/src/test/java/com/hartwig/hmftools/serve/sources/vicc/extractor/GeneLevelExtractorTest.java
@@ -6,13 +6,21 @@ import static com.hartwig.hmftools.common.drivercatalog.DriverCategory.TSG;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.hartwig.hmftools.common.drivercatalog.panel.DriverGene;
 import com.hartwig.hmftools.common.drivercatalog.panel.ImmutableDriverGene;
+import com.hartwig.hmftools.common.genome.genepanel.HmfGenePanelSupplier;
 import com.hartwig.hmftools.serve.actionability.gene.GeneLevelEvent;
+import com.hartwig.hmftools.serve.sources.vicc.ViccTestFactory;
+import com.hartwig.hmftools.serve.sources.vicc.annotation.GeneLevelAnnotation;
+import com.hartwig.hmftools.serve.sources.vicc.annotation.ImmutableGeneLevelAnnotation;
 import com.hartwig.hmftools.vicc.datamodel.Feature;
 import com.hartwig.hmftools.vicc.datamodel.ImmutableFeature;
+import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
+import com.hartwig.hmftools.vicc.datamodel.ViccSource;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.Ignore;
@@ -21,19 +29,99 @@ import org.junit.Test;
 public class GeneLevelExtractorTest {
 
     @Test
-    @Ignore
-    public void canExtractGeneLevelEvent() {
-        //TODO improve test
-        Feature feature = ImmutableFeature.builder().geneSymbol("a").name("a").build();
+    public void canExtractGeneLevelEventONCO() {
+        Map<Feature, GeneLevelAnnotation> geneLevelEventsPerFeature = Maps.newHashMap();
 
-        assertEquals(GeneLevelExtractor.extractGeneLevelEvent(feature, createDriverGeneONCO()), GeneLevelEvent.ACTIVATION);
-        assertEquals(GeneLevelExtractor.extractGeneLevelEvent(feature, createDriverGeneTSG()), GeneLevelEvent.INACTIVATION);
+        GeneLevelExtractor geneLevelExtractor = new GeneLevelExtractor(HmfGenePanelSupplier.allGenesMap37(), createDriverGenes());
+        ViccEntry viccEntry = ViccTestFactory.testViccEntryWithSourceAndKbObject(ViccSource.ONCOKB,
+                "any",
+                "KIT",
+                "KIT  positive",
+                "description",
+                "chromosome",
+                "pos");
+
+        assertEquals(GeneLevelExtractor.extractGeneLevelEvent(viccEntry.features().get(0), createDriverGenes()), GeneLevelEvent.ACTIVATION);
+
+        geneLevelEventsPerFeature.put(viccEntry.features().get(0),
+                ImmutableGeneLevelAnnotation.builder()
+                        .gene("KIT")
+                        .event(GeneLevelEvent.ACTIVATION)
+                        .build());
+        assertEquals(geneLevelEventsPerFeature, geneLevelExtractor.extractGeneLevelEvents(viccEntry));
+    }
+
+    @Test
+    public void canExtractGeneLevelEventTSG() {
+        Map<Feature, GeneLevelAnnotation> geneLevelEventsPerFeature = Maps.newHashMap();
+
+        GeneLevelExtractor geneLevelExtractor = new GeneLevelExtractor(HmfGenePanelSupplier.allGenesMap37(), createDriverGenes());
+        ViccEntry viccEntry = ViccTestFactory.testViccEntryWithSourceAndKbObject(ViccSource.ONCOKB,
+                "any",
+                "TP53",
+                "TP53  negative",
+                "description",
+                "chromosome",
+                "pos");
+
+        assertEquals(GeneLevelExtractor.extractGeneLevelEvent(viccEntry.features().get(0), createDriverGenes()), GeneLevelEvent.INACTIVATION);
+
+        geneLevelEventsPerFeature.put(viccEntry.features().get(0),
+                ImmutableGeneLevelAnnotation.builder()
+                        .gene("TP53")
+                        .event(GeneLevelEvent.INACTIVATION)
+                        .build());
+        assertEquals(geneLevelEventsPerFeature, geneLevelExtractor.extractGeneLevelEvents(viccEntry));
+    }
+
+    @Test
+    public void canExtractGeneLevelEventGeneral() {
+        Map<Feature, GeneLevelAnnotation> geneLevelEventsPerFeature = Maps.newHashMap();
+
+        GeneLevelExtractor geneLevelExtractor = new GeneLevelExtractor(HmfGenePanelSupplier.allGenesMap37(), createDriverGenes());
+        ViccEntry viccEntry = ViccTestFactory.testViccEntryWithSourceAndKbObject(ViccSource.ONCOKB,
+                "any",
+                "SRSF2",
+                "Truncating Mutations",
+                "description",
+                "chromosome",
+                "pos");
+
+        assertEquals(GeneLevelExtractor.extractGeneLevelEvent(viccEntry.features().get(0), createDriverGenes()), GeneLevelEvent.INACTIVATION);
+
+        geneLevelEventsPerFeature.put(viccEntry.features().get(0),
+                ImmutableGeneLevelAnnotation.builder()
+                        .gene("SRSF2")
+                        .event(GeneLevelEvent.INACTIVATION)
+                        .build());
+        assertEquals(geneLevelEventsPerFeature, geneLevelExtractor.extractGeneLevelEvents(viccEntry));
+    }
+
+    @Test
+    public void canExtractGeneLevelEventFusion() {
+        Map<Feature, GeneLevelAnnotation> geneLevelEventsPerFeature = Maps.newHashMap();
+
+        GeneLevelExtractor geneLevelExtractor = new GeneLevelExtractor(HmfGenePanelSupplier.allGenesMap37(), createDriverGenes());
+        ViccEntry viccEntry = ViccTestFactory.testViccEntryWithSourceAndKbObject(ViccSource.ONCOKB,
+                "any",
+                "NTRK3",
+                "NTRK3 fusion",
+                "description",
+                "chromosome",
+                "pos");
+
+        geneLevelEventsPerFeature.put(viccEntry.features().get(0),
+                ImmutableGeneLevelAnnotation.builder()
+                        .gene("NTRK3")
+                        .event(GeneLevelEvent.FUSION)
+                        .build());
+        assertEquals(geneLevelEventsPerFeature, geneLevelExtractor.extractGeneLevelEvents(viccEntry));
     }
 
     @NotNull
-    private static List<DriverGene> createDriverGeneTSG() {
-        return Lists.newArrayList(ImmutableDriverGene.builder()
-                .gene("a")
+    private static List<DriverGene> createDriverGenes() {
+        DriverGene driverGeneTsg = ImmutableDriverGene.builder()
+                .gene("SRSF2")
                 .reportMissenseAndInframe(true)
                 .reportNonsenseAndFrameshift(true)
                 .reportSplice(true)
@@ -44,13 +132,10 @@ public class GeneLevelExtractorTest {
                 .reportGermlineVariant(false)
                 .reportGermlineHotspot(false)
                 .likelihoodType(TSG)
-                .build());
-    }
+                .build();
 
-    @NotNull
-    private static List<DriverGene> createDriverGeneONCO() {
-        return Lists.newArrayList(ImmutableDriverGene.builder()
-                .gene("a")
+        DriverGene driverGeneOnco = ImmutableDriverGene.builder()
+                .gene("KIT")
                 .reportMissenseAndInframe(true)
                 .reportNonsenseAndFrameshift(true)
                 .reportSplice(true)
@@ -61,6 +146,7 @@ public class GeneLevelExtractorTest {
                 .reportGermlineVariant(false)
                 .reportGermlineHotspot(false)
                 .likelihoodType(ONCO)
-                .build());
+                .build();
+        return Lists.newArrayList(driverGeneTsg, driverGeneOnco);
     }
 }
