92f8215af9e1223bb31390db7cccea0b2726dcd3
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-11-26 11:39:06 +0100

    DEV-1416: Add test for extract signatures

--- serve/src/test/java/com/hartwig/hmftools/serve/sources/vicc/extractor/SignaturesExtractorTest.java
+++ serve/src/test/java/com/hartwig/hmftools/serve/sources/vicc/extractor/SignaturesExtractorTest.java
@@ -3,15 +3,55 @@ package com.hartwig.hmftools.serve.sources.vicc.extractor;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
 import com.hartwig.hmftools.serve.actionability.signature.SignatureName;
+import com.hartwig.hmftools.serve.sources.vicc.ViccTestFactory;
+import com.hartwig.hmftools.vicc.datamodel.Feature;
+import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
+import com.hartwig.hmftools.vicc.datamodel.ViccSource;
 
 import org.junit.Test;
 
 public class SignaturesExtractorTest {
 
     @Test
-    public void canExtractSignatureName() {
-        assertEquals(SignatureName.MICROSATELLITE_UNSTABLE, SignaturesExtractor.extractSignatureName("Microsatellite Instability-High"));
-        assertNull(SignaturesExtractor.extractSignatureName("abc"));
+    public void canExtractSignatureNameUnknown() {
+        SignaturesExtractor signaturesExtractor = new SignaturesExtractor();
+        ViccEntry viccEntry = ViccTestFactory.testViccEntryWithSourceAndKbObject(ViccSource.CIVIC,
+                "any",
+                "Other Biomarkers",
+                "Microsatellite Instability-High");
+        Feature feature = viccEntry.features().get(0);
+        SignatureName signatureName = SignaturesExtractor.extractSignatureName(feature
+                .name());
+
+        //extract signature name
+        assertEquals(SignatureName.MICROSATELLITE_UNSTABLE, signatureName);
+
+        Map<Feature, SignatureName> signaturesPerFeature = Maps.newHashMap();
+        signaturesPerFeature.put(feature, signatureName);
+
+        assertEquals(signaturesPerFeature, signaturesExtractor.extractSignatures(viccEntry));
+    }
+
+    @Test
+    public void canExtractSignatureUnknown() {
+        SignaturesExtractor signaturesExtractor = new SignaturesExtractor();
+        ViccEntry viccEntry = ViccTestFactory.testViccEntryWithSourceAndKbObject(ViccSource.CIVIC,
+                "any",
+                "Other Biomarkers",
+                "Tum");
+        Feature feature = viccEntry.features().get(0);
+        SignatureName signatureName = SignaturesExtractor.extractSignatureName(feature
+                .name());
+
+        //extract signature name
+        assertNull(signatureName);
+
+        Map<Feature, SignatureName> signaturesPerFeature = Maps.newHashMap();
+
+        assertEquals(signaturesPerFeature, signaturesExtractor.extractSignatures(viccEntry));
     }
 }
