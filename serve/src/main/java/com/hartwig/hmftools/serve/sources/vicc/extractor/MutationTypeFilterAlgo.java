3f886e7b388d2febe1568eeeddb542fac11eee50
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-12-08 13:24:03 +0100

    DEV-1112: Simplify rules for determining mutation type filter

--- serve/src/main/java/com/hartwig/hmftools/serve/sources/vicc/extractor/MutationTypeFilterAlgo.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/vicc/extractor/MutationTypeFilterAlgo.java
@@ -7,6 +7,7 @@ import com.hartwig.hmftools.common.drivercatalog.panel.DriverGene;
 import com.hartwig.hmftools.serve.actionability.range.MutationTypeFilter;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class MutationTypeFilterAlgo {
 
@@ -19,43 +20,39 @@ public class MutationTypeFilterAlgo {
 
     @NotNull
     public MutationTypeFilter determine(@NotNull String gene, @NotNull String event) {
-        String lowerCaseEvent = event.toLowerCase();
-        String extractSpecificInfoOfEvent = lowerCaseEvent.substring(lowerCaseEvent.lastIndexOf(" ") + 1);
-        MutationTypeFilter filter;
-        if (lowerCaseEvent.contains("skipping mutation") || lowerCaseEvent.contains("splice site insertion")) {
-            filter = MutationTypeFilter.SPLICE;
-        } else if (extractSpecificInfoOfEvent.equals("deletions") || extractSpecificInfoOfEvent.equals("deletion") || lowerCaseEvent.contains(
-                "partial deletion of exons")) {
-            filter = MutationTypeFilter.MISSENSE_INFRAME_DELETION;
-        } else if (extractSpecificInfoOfEvent.equals("insertions") || extractSpecificInfoOfEvent.equals("insertion")) {
-            filter = MutationTypeFilter.MISSENSE_INFRAME_INSERTION;
-        } else if (extractSpecificInfoOfEvent.equals("deletion/insertion") || extractSpecificInfoOfEvent.equals("insertions/deletions")) {
-            filter = MutationTypeFilter.MISSENSE_INFRAME_ANY;
-        } else if (extractSpecificInfoOfEvent.equals("frameshift")) {
-            filter = MutationTypeFilter.NONSENSE_OR_FRAMESHIFT;
+        String formattedEvent = event.toLowerCase();
+
+        if (formattedEvent.contains("skipping") || formattedEvent.contains("splice")) {
+            return MutationTypeFilter.SPLICE;
+        } else if (formattedEvent.contains("deletion/insertion") || formattedEvent.contains("insertions/deletions")) {
+            return MutationTypeFilter.MISSENSE_INFRAME_ANY;
+        } else if (formattedEvent.contains("deletion")) {
+            return MutationTypeFilter.MISSENSE_INFRAME_DELETION;
+        } else if (formattedEvent.contains("insertion")) {
+            return MutationTypeFilter.MISSENSE_INFRAME_INSERTION;
+        } else if (formattedEvent.contains("frameshift")) {
+            return MutationTypeFilter.NONSENSE_OR_FRAMESHIFT;
         } else {
-            filter = MutationTypeFilter.UNKNOWN;
-        }
-
-        for (DriverGene driverGene : driverGenes) {
-            if (driverGene.gene().equals(gene)) {
+            DriverGene driverGene = findByGene(gene);
+            if (driverGene != null) {
                 if (driverGene.likelihoodType() == DriverCategory.ONCO) {
-                    if (filter == MutationTypeFilter.UNKNOWN) {
-                        return MutationTypeFilter.MISSENSE_ANY;
-                    } else {
-                        return filter;
-                    }
+                    return MutationTypeFilter.MISSENSE_ANY;
                 } else if (driverGene.likelihoodType() == DriverCategory.TSG) {
-                    if (filter == MutationTypeFilter.UNKNOWN) {
-                        return MutationTypeFilter.ANY;
-
-                    } else {
-                        return filter;
-                    }
+                    return MutationTypeFilter.ANY;
                 }
             }
         }
 
-        return filter;
+        return MutationTypeFilter.UNKNOWN;
+    }
+
+    @Nullable
+    private DriverGene findByGene(@NotNull String gene) {
+        for (DriverGene driverGene : driverGenes) {
+            if (driverGene.gene().equals(gene)) {
+                return driverGene;
+            }
+        }
+        return null;
     }
 }
