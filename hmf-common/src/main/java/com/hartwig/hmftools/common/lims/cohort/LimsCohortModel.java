881a165a57c89c872ef4a3e29224ee5191485695
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-14 17:30:24 +0100

    DEV-1621: more config

--- hmf-common/src/main/java/com/hartwig/hmftools/common/lims/cohort/LimsCohortModel.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/lims/cohort/LimsCohortModel.java
@@ -2,6 +2,8 @@ package com.hartwig.hmftools.common.lims.cohort;
 
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.immutables.value.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,16 +16,20 @@ public abstract class LimsCohortModel {
     @NotNull
     abstract Map<String, LimsCohortConfigData> limsCohortMap();
 
+    private static final Logger LOGGER = LogManager.getLogger(LimsCohortModel.class);
+
     @Nullable
     public LimsCohortConfigData queryCohortData(@Nullable String cohortString) {
         if (cohortString == null) {
             return null;
+        } else {
+            LimsCohortConfigData cohortConfigData = limsCohortMap().get(cohortString);
+            if (cohortConfigData == null) {
+                LOGGER.info("No cohort map is present for cohortString {}", cohortString);
+                return null;
+            } else {
+                return cohortConfigData;
+            }
         }
-        LimsCohortConfigData configData = limsCohortMap().get(cohortString);
-        return configData;
-
-
     }
-
-
 }
