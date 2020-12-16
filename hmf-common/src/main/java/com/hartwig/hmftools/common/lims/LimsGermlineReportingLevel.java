fcc093a0250932e6121a415a4b53063f141fad43
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-16 09:48:02 +0100

    DEV-1621: more improvements

--- hmf-common/src/main/java/com/hartwig/hmftools/common/lims/LimsGermlineReportingLevel.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/lims/LimsGermlineReportingLevel.java
@@ -2,9 +2,6 @@ package com.hartwig.hmftools.common.lims;
 
 import com.hartwig.hmftools.common.lims.cohort.LimsCohortConfig;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 import org.jetbrains.annotations.NotNull;
 
 public enum LimsGermlineReportingLevel {
@@ -12,12 +9,10 @@ public enum LimsGermlineReportingLevel {
     REPORT_WITHOUT_NOTIFICATION,
     NO_REPORTING;
 
-    private static final Logger LOGGER = LogManager.getLogger(LimsGermlineReportingLevel.class);
-
     @NotNull
     static LimsGermlineReportingLevel fromLimsInputs(boolean reportGermlineVariants, @NotNull String germlineReportingLevelString,
             @NotNull String sampleId, @NotNull LimsCohortConfig cohort) {
-        if (reportGermlineVariants && !cohort.cohortId().equals(Strings.EMPTY)) {
+        if (reportGermlineVariants && !cohort.cohortId().isEmpty()) {
             // Cases "geen toevalsbevindingen: familie mag deze/wel niet opvragen" have been merged
             // into a single category "geen toevalsbevindingen" per feb 1st 2020
             if (cohort.reportGermline() || cohort.reportGermlineFlag()) {
