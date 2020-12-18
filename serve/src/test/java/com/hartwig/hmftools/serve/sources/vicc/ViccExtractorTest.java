6184aa794d8846ed7eac78af6e2a8750e50658be
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-18 16:10:09 +0100

    DEV-1706: fix test

--- serve/src/test/java/com/hartwig/hmftools/serve/sources/vicc/ViccExtractorTest.java
+++ serve/src/test/java/com/hartwig/hmftools/serve/sources/vicc/ViccExtractorTest.java
@@ -15,6 +15,8 @@ import com.hartwig.hmftools.common.genome.region.HmfTranscriptRegion;
 import com.hartwig.hmftools.common.serve.classification.EventClassifierConfig;
 import com.hartwig.hmftools.common.variant.hotspot.ImmutableVariantHotspotImpl;
 import com.hartwig.hmftools.common.variant.hotspot.VariantHotspot;
+import com.hartwig.hmftools.serve.checkertool.CheckCodonRanges;
+import com.hartwig.hmftools.serve.checkertool.CheckExons;
 import com.hartwig.hmftools.serve.curation.DoidLookupTestFactory;
 import com.hartwig.hmftools.serve.extraction.ExtractionResult;
 import com.hartwig.hmftools.serve.extraction.hotspot.ProteinResolver;
@@ -52,7 +54,10 @@ public class ViccExtractorTest {
         entries.add(ViccTestFactory.testEntryWithGeneEventAndAssociation("ALK", "EML4-ALK Fusion", association));
         entries.add(ViccTestFactory.testEntryWithGeneEventAndAssociation("-", "Microsatellite Instability-High", association));
 
-        ExtractionResult result = extractor.extract(entries);
+        CheckExons checkExons = new CheckExons();
+        CheckCodonRanges checkCodonRanges = new CheckCodonRanges();
+
+        ExtractionResult result = extractor.extract(entries, checkExons, checkCodonRanges);
         assertEquals(1, result.knownHotspots().size());
         assertEquals(1, result.knownCopyNumbers().size());
         assertEquals(1, result.knownFusionPairs().size());
