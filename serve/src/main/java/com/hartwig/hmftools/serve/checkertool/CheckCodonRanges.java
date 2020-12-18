f407d1689ba0adf3f6935ec20f8976ab2b231e43
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-18 16:16:53 +0100

    DEV-1706: put event for checker in map

--- serve/src/main/java/com/hartwig/hmftools/serve/checkertool/CheckCodonRanges.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/checkertool/CheckCodonRanges.java
@@ -1,5 +1,9 @@
 package com.hartwig.hmftools.serve.checkertool;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +17,8 @@ public class CheckCodonRanges {
 
     public void run(@NotNull String event, @NotNull String gene) {
         LOGGER.info("Event check codon ranges {} of gene {}", event, gene);
+        Map<String, String> exonMap = Maps.newHashMap();
+        exonMap.put(gene, event);
 
     }
 }
--- serve/src/main/java/com/hartwig/hmftools/serve/checkertool/CheckExons.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/checkertool/CheckExons.java
@@ -1,5 +1,9 @@
 package com.hartwig.hmftools.serve.checkertool;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +17,8 @@ public class CheckExons {
 
     public void run(@NotNull String event, @NotNull String geneSymbol) {
         LOGGER.info("Event check exons {} of gene {}", event, geneSymbol);
+        Map<String, String> exonMap = Maps.newHashMap();
+        exonMap.put(geneSymbol, event);
 
     }
 }
