65b64be450886caf8b387b2a209ba1615a9d3385
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-18 16:07:47 +0100

    DEV-1706: refactor

--- serve/src/main/java/com/hartwig/hmftools/serve/checkertool/CheckCodonRanges.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/checkertool/CheckCodonRanges.java
@@ -1,7 +1,5 @@
 package com.hartwig.hmftools.serve.checkertool;
 
-import com.hartwig.hmftools.serve.extraction.ExtractionResult;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
