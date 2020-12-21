36e4f06654952d2d427c8ba686860d3d264f2759
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-12-21 12:43:24 +0100

    Mark comment as TODO

--- hmf-common/src/main/java/com/hartwig/hmftools/common/ecrf/reader/XMLPatientReader.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/ecrf/reader/XMLPatientReader.java
@@ -46,7 +46,7 @@ public final class XMLPatientReader extends EcrfReader {
     private static final String ITEM_OID_ATTRIBUTE = "ItemOID";
     private static final String ITEM_VALUE_ATTRIBUTE = "Value";
 
-    // See DEV-491, DEV-1304, DEV-1724
+    // TODO See DEV-491, DEV-1304, DEV-1724
     private static final List DROPDOWN_ITEMS_TO_IGNORE_FOR_WARNING = Lists.newArrayList("FLD.DDELINV",
             "FLD.DDELINST",
             "FLD.CS.CSRTYPE",
