32be5d7801685c13c73a0648e7d9e63d15c1aca0
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-30 18:01:54 +0100

    DEV-1706: start with reading known exon file

--- serve/src/main/java/com/hartwig/hmftools/serve/extraction/exon/KnownExonFile.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/extraction/exon/KnownExonFile.java
@@ -1,13 +1,21 @@
 package com.hartwig.hmftools.serve.extraction.exon;
 
+import static com.hartwig.hmftools.serve.actionability.util.ActionableFileFunctions.FIELD_DELIMITER;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Set;
 import java.util.StringJoiner;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.hartwig.hmftools.common.serve.Knowledgebase;
+import com.hartwig.hmftools.serve.actionability.hotspot.ActionableHotspot;
+import com.hartwig.hmftools.serve.actionability.hotspot.ImmutableActionableHotspot;
+import com.hartwig.hmftools.serve.actionability.util.ActionableFileFunctions;
+import com.hartwig.hmftools.serve.extraction.util.MutationTypeFilter;
 import com.hartwig.hmftools.serve.util.RefGenomeVersion;
 
 import org.jetbrains.annotations.NotNull;
@@ -23,6 +31,39 @@ public final class KnownExonFile {
     @NotNull
     public static String knownExonTsvPath(@NotNull String outputDir, @NotNull RefGenomeVersion refGenomeVersion) {
         return refGenomeVersion.addVersionToFilePath(outputDir + File.separator + KNOWN_EXON_TSV);
+    }
+
+    public static List<KnownExon> read(@NotNull String file) throws IOException {
+        List<String> lines = Files.readAllLines(new File(file).toPath());
+
+        return fromLines(lines.subList(1, lines.size()));
+    }
+
+    @NotNull
+    private static List<KnownExon> fromLines(@NotNull List<String> lines) {
+        List<KnownExon> exons = Lists.newArrayList();
+        for (String line : lines) {
+            exons.add(fromLine(line));
+        }
+        return exons;
+    }
+
+    @NotNull
+    private static KnownExon fromLine(@NotNull String line) {
+        String[] values = line.split(FIELD_DELIMITER);
+
+        return ImmutableKnownExon.builder()
+                .annotation(ImmutableExonAnnotation.builder()
+                        .gene(values[0])
+                        .chromosome(values[1])
+                        .start(Long.parseLong(values[2]))
+                        .end(Long.parseLong(values[3]))
+                        .mutationType(MutationTypeFilter.valueOf(values[4]))
+                        .exonEnsemblId(values[5])
+                        .exonIndex(Integer.parseInt(values[6]))
+                        .build())
+                .sources(Sets.newHashSet(Knowledgebase.DOCM)) //TODO fix
+                .build();
     }
 
     public static void write(@NotNull String exonTsv, @NotNull Iterable<KnownExon> exons) throws IOException {
--- serve/src/main/java/com/hartwig/hmftools/serve/extraction/exon/tools/ExonChecker.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/extraction/exon/tools/ExonChecker.java
@@ -1,15 +1,32 @@
 package com.hartwig.hmftools.serve.extraction.exon.tools;
 
 import java.io.IOException;
+import java.util.List;
 
+import com.hartwig.hmftools.serve.extraction.exon.KnownExon;
+import com.hartwig.hmftools.serve.extraction.exon.KnownExonFile;
+
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class ExonChecker {
     private static final Logger LOGGER = LogManager.getLogger(ExonChecker.class);
+    private static final boolean LOG_DEBUG = true;
 
     public static void main(String[] args) throws IOException {
-        LOGGER.info("Running SERVE checker on version {}");
+        LOGGER.info("Running SERVE exon checker");
+
+        if (LOG_DEBUG) {
+            Configurator.setRootLevel(Level.DEBUG);
+        }
+
+        String knownExonsTsv = System.getProperty("user.home") + "/hmf/tmp/serve/KnownExons.SERVE.37.tsv";
+        List<KnownExon> exons = KnownExonFile.read(knownExonsTsv);
+
+        LOGGER.info("The size of the file is {}", exons.size());
+        LOGGER.info(exons);
 
         LOGGER.info("Checking exons");
 
