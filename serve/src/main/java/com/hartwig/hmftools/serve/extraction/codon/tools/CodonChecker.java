aaf35894643fa4b0f514928d11091eb56af010a9
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-31 11:23:15 +0100

    DEV-1706: start with logica of codon checker SERVE

--- serve/src/main/java/com/hartwig/hmftools/serve/extraction/codon/tools/CodonChecker.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/extraction/codon/tools/CodonChecker.java
@@ -2,7 +2,9 @@ package com.hartwig.hmftools.serve.extraction.codon.tools;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
+import com.google.common.collect.Lists;
 import com.hartwig.hmftools.serve.extraction.codon.KnownCodon;
 import com.hartwig.hmftools.serve.extraction.codon.KnownCodonFile;
 
@@ -10,6 +12,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.jetbrains.annotations.Nullable;
 
 public class CodonChecker {
     private static final Logger LOGGER = LogManager.getLogger(CodonChecker.class);
@@ -22,18 +25,71 @@ public class CodonChecker {
             Configurator.setRootLevel(Level.DEBUG);
         }
 
+        Random random = new Random();
         String knownCodonsTsv = System.getProperty("user.home") + "/hmf/tmp/serve/KnownCodons.SERVE.37.tsv";
         List<KnownCodon> codons = KnownCodonFile.read(knownCodonsTsv);
 
         LOGGER.info("The size of the file is {}", codons.size());
 
-        for (KnownCodon codon: codons) {
-            LOGGER.info(codon);
+        Integer codonIndex = null;
+        String chromosome = null;
+        Long start = null;
+        Long end = null;
+        List<String> bases = Lists.newArrayList();
+        List<Long> genomicPositions = Lists.newArrayList();
+        String randomAltBase = null;
+        for (KnownCodon codon : codons) {
+            start = codon.annotation().start();
+            end = codon.annotation().end();
+            Long bewtweenNumber = end > start ? start + 1 : start -1;
+            genomicPositions = Lists.newArrayList(start, bewtweenNumber, end);
+            chromosome = codon.annotation().chromosome();
+            codonIndex = codon.annotation().codonIndex();
+
+            for (Long genomicPosition : genomicPositions) {
+                String extractRefBaseOfPosition = extractRefBaseOfGenomicPosition(chromosome, genomicPosition);
+
+                if (extractRefBaseOfPosition.equals("A")) {
+                    bases = Lists.newArrayList("C", "T", "G");
+                    randomAltBase = bases.get(random.nextInt(3));
+                } else if (extractRefBaseOfPosition.equals("C")) {
+                    bases = Lists.newArrayList("A", "T", "G");
+                    randomAltBase = bases.get(random.nextInt(3));
+                } else if (extractRefBaseOfPosition.equals("T")) {
+                    bases = Lists.newArrayList("C", "A", "G");
+                    randomAltBase = bases.get(random.nextInt(3));
+                } else if (extractRefBaseOfPosition.equals("G")) {
+                    bases = Lists.newArrayList("C", "T", "A");
+                    randomAltBase = bases.get(random.nextInt(3));
+                }
+
+                Integer annotatedVariantCodonIndex =
+                        extactAnnotationVariantCodonIndex(extractRefBaseOfPosition, randomAltBase, chromosome, genomicPosition);
+
+                if (!codonIndex.equals(annotatedVariantCodonIndex)) {
+                    LOGGER.warn("Condon index of SERVE {} are not equals for annotated codon index {}",
+                            codonIndex,
+                            annotatedVariantCodonIndex);
+                }
+            }
         }
+
         LOGGER.info("Checking codons");
 
         LOGGER.info("Done!");
 
+    }
+
+    private static String extractRefBaseOfGenomicPosition(@Nullable String chromosome, Long genomicPosition) {
+        String genomicPositionBase = chromosome + ":" + genomicPosition + "-" + genomicPosition;
+        //TODO extract base with genomic posiiton
+        return genomicPositionBase;
+    }
+
+    private static Integer extactAnnotationVariantCodonIndex(@Nullable String extractRefBaseOfPosition, @Nullable String randomAltBase,
+            @Nullable String chromosome, Long position) {
+        //TODO extract codon annotation
+        return 1;
     }
 
 }
