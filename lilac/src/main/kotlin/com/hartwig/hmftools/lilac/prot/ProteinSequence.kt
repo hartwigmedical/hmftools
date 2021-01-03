fdc9bd3fcd2ca9d561cbf1a530601efeac0b2101
Jonathan Baber <jonbaber@gmail.com>
2021-01-04 09:41:51 +1000

    rolling kmers

--- lilac/src/main/kotlin/com/hartwig/hmftools/lilac/prot/ProteinSequence.kt
+++ lilac/src/main/kotlin/com/hartwig/hmftools/lilac/prot/ProteinSequence.kt
@@ -1,7 +1,7 @@
 package com.hartwig.hmftools.lilac.prot
 
+import com.hartwig.hmftools.ext.rollingKmers
 import com.hartwig.hmftools.lilac.hla.HlaAllele
-import java.util.*
 
 data class ProteinSequence(val contig: String, val protein: String) {
     val allele: HlaAllele by lazy { HlaAllele(contig) }
@@ -14,7 +14,7 @@ data class ProteinSequence(val contig: String, val protein: String) {
         return ProteinSequence(contig, protein + more)
     }
 
-    private fun exonicProteins(exonicBoundaries: List<Int>): List<String> {
+    fun exonicProteins(exonicBoundaries: List<Int>): List<String> {
         if (exonicBoundaries.isEmpty()) {
             return listOf(fullProteinSequence)
         }
@@ -36,11 +36,11 @@ data class ProteinSequence(val contig: String, val protein: String) {
             result.add(protein.substring(previousBoundary + 1).removeSpecialCharacters())
         }
 
-        return result
+        return result.filter { it.isNotEmpty() }
     }
 
     fun allKmers(length: Int): Set<String> {
-        return fixedLengthRollingKmers(length, fullProteinSequence).toSet()
+        return fullProteinSequence.rollingKmers(length).toSet()
     }
 
     fun exonicKmers(kmerLength: Int, exonicBoundaries: List<Int>): Set<String> {
@@ -49,29 +49,13 @@ data class ProteinSequence(val contig: String, val protein: String) {
 
     fun exonicKmers(minKmerLength: Int, maxKmerLength: Int, exonicBoundaries: List<Int>): Set<String> {
         return exonicProteins(exonicBoundaries)
-                .flatMap { variableLengthRollingKmers(minKmerLength, maxKmerLength, it) }
+                .flatMap { it.rollingKmers(minKmerLength, maxKmerLength) }
                 .toSet()
 //                .groupBy { it }
 //                .filter { it.value.size == 1 }
 //                .keys
     }
 
-    private fun fixedLengthRollingKmers(kmerSize: Int, sequence: String): List<String> {
-        val result = mutableListOf<String>()
-        for (i in 0..sequence.length - kmerSize) {
-            result.add(sequence.substring(i, i + kmerSize))
-        }
-
-        return result
-    }
-
-    private fun variableLengthRollingKmers(minKmerLength: Int, maxKmerLength: Int, sequence: String): List<String> {
-        if (sequence.length < minKmerLength) {
-            return Collections.emptyList()
-        }
-
-        return fixedLengthRollingKmers(maxKmerLength.coerceAtMost(sequence.length), sequence);
-    }
 
     private fun String.removeSpecialCharacters(): String {
         return this.replace("*", "").replace(".", "");
