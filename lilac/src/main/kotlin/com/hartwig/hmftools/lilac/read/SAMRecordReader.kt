16244a43d63342548bf68cf4f319b714ae0b34dd
Jonathan Baber <jonbaber@gmail.com>
2021-01-21 15:35:27 +1000

    look at aligned blocks

--- lilac/src/main/kotlin/com/hartwig/hmftools/lilac/read/SAMRecordReader.kt
+++ lilac/src/main/kotlin/com/hartwig/hmftools/lilac/read/SAMRecordReader.kt
@@ -2,7 +2,6 @@ package com.hartwig.hmftools.lilac.read
 
 import com.hartwig.hmftools.common.genome.bed.NamedBed
 import com.hartwig.hmftools.common.genome.region.*
-import com.hartwig.hmftools.lilac.ext.containsIndel
 import com.hartwig.hmftools.lilac.sam.SamSlicer
 import htsjdk.samtools.SAMRecord
 import htsjdk.samtools.SamReaderFactory
@@ -49,15 +48,20 @@ class SAMRecordReader(maxDistance: Int, private val transcripts: List<HmfTranscr
 
             val consumer = Consumer<SAMRecord> { samRecord ->
 
-                if (samRecord.inRangeOfCodingTranscripts() &&  !samRecord.containsIndel()) {
-                    if (reverseStrand) {
-                        result.add(realignReverseStrand(gene, hlaCodingRegionOffset, region, samRecord))
-                    } else {
-                        result.add(realignForwardStrand(gene, hlaCodingRegionOffset, region, samRecord))
+                if (samRecord.bothEndsinRangeOfCodingTranscripts()) {
+                    for (alignmentBlock in samRecord.alignmentBlocks) {
+                        val alignmentStart = alignmentBlock.referenceStart
+                        val alignmentEnd = alignmentStart + alignmentBlock.length - 1
+                        if (alignmentStart <= region.end() && alignmentEnd >= region.start()) {
+                            if (reverseStrand) {
+                                result.add(realignReverseStrand(gene, hlaCodingRegionOffset, region, alignmentStart, alignmentEnd, samRecord))
+                            } else {
+                                result.add(realignForwardStrand(gene, hlaCodingRegionOffset, region, alignmentStart, alignmentEnd, samRecord))
+                            }
+                        }
                     }
                 }
             }
-
 
             slicer.slice(region, samReader, consumer)
         }
@@ -65,12 +69,9 @@ class SAMRecordReader(maxDistance: Int, private val transcripts: List<HmfTranscr
     }
 
 
-    private fun realignForwardStrand(gene: String, hlaExonOffset: Int, region: GenomeRegion, samRecord: SAMRecord): SAMRecordRead {
+    private fun realignForwardStrand(gene: String, hlaExonOffset: Int, region: GenomeRegion, alignmentStart: Int, alignmentEnd: Int, samRecord: SAMRecord): SAMRecordRead {
         val hlaExonStartPosition = region.start().toInt()
         val hlaExonEndPosition = region.end().toInt()
-
-        val alignmentStart = samRecord.alignmentStart
-        val alignmentEnd = samRecord.alignmentEnd
 
         val hlaStart = max(alignmentStart, hlaExonStartPosition)
         val hlaEnd = min(alignmentEnd, hlaExonEndPosition)
@@ -82,12 +83,9 @@ class SAMRecordReader(maxDistance: Int, private val transcripts: List<HmfTranscr
         return SAMRecordRead(gene, hlaStartIndex, readIndex, length, false, samRecord)
     }
 
-    private fun realignReverseStrand(gene: String, hlaExonOffset: Int, region: GenomeRegion, samRecord: SAMRecord): SAMRecordRead {
+    private fun realignReverseStrand(gene: String, hlaExonOffset: Int, region: GenomeRegion, alignmentStart: Int, alignmentEnd: Int, samRecord: SAMRecord): SAMRecordRead {
         val hlaExonStartPosition = region.end().toInt()
         val hlaExonEndPosition = region.start().toInt()
-
-        val alignmentStart = samRecord.alignmentStart
-        val alignmentEnd = samRecord.alignmentEnd
 
         val hlaStart = min(alignmentEnd, hlaExonStartPosition)
         val hlaEnd = max(alignmentStart, hlaExonEndPosition)
@@ -99,9 +97,9 @@ class SAMRecordReader(maxDistance: Int, private val transcripts: List<HmfTranscr
         return SAMRecordRead(gene, hlaStartIndex, readIndex, length, true, samRecord)
     }
 
-    private fun SAMRecord.inRangeOfCodingTranscripts(): Boolean {
-        val thisInRange = codingRegions.any {it.chromosome() == this.contig && this.alignmentStart >= it.start() && this.alignmentStart <= it.end()}
-        val mateInRange = codingRegions.any {it.chromosome() == this.contig && this.mateAlignmentStart >= it.start() && this.mateAlignmentStart <= it.end()}
+    private fun SAMRecord.bothEndsinRangeOfCodingTranscripts(): Boolean {
+        val thisInRange = codingRegions.any { it.chromosome() == this.contig && this.alignmentStart >= it.start() && this.alignmentStart <= it.end() }
+        val mateInRange = codingRegions.any { it.chromosome() == this.contig && this.mateAlignmentStart >= it.start() && this.mateAlignmentStart <= it.end() }
 
         return thisInRange && mateInRange
     }
