65d609cb9a668a7967c1280744ffeb7100ba376e
Jonathan Baber <jonbaber@gmail.com>
2021-02-02 09:42:24 +1000

    clean up

--- lilac/src/main/kotlin/com/hartwig/hmftools/lilac/evidence/PhasedEvidenceValidation.kt
+++ lilac/src/main/kotlin/com/hartwig/hmftools/lilac/evidence/PhasedEvidenceValidation.kt
@@ -5,10 +5,10 @@ import org.apache.logging.log4j.LogManager
 
 object PhasedEvidenceValidation {
 
-        val logger = LogManager.getLogger(this::class.java)
+    val logger = LogManager.getLogger(this::class.java)
 
     fun validateExpected(gene: String, evidence: List<PhasedEvidence>, candidates: List<HlaSequence>) {
-        val expectedSequences = candidates.filter { it.allele.gene  == gene}
+        val expectedSequences = candidates.filter { it.allele.gene == gene }
         for (sequence in expectedSequences) {
             for (phasedEvidence in evidence) {
                 if (!sequence.consistentWith(phasedEvidence)) {
