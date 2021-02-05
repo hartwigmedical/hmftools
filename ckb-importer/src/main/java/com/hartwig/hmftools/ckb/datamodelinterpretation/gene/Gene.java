8326c44a4f2a1ac3f08e80bd7191b18e4fe49a1b
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-02-05 10:22:45 +0100

    DEV-1694: add gene + variant interpretation datamodel

---
+++ ckb-importer/src/main/java/com/hartwig/hmftools/ckb/datamodelinterpretation/gene/Gene.java
@@ -0,0 +1,48 @@
+package com.hartwig.hmftools.ckb.datamodelinterpretation.gene;
+
+import java.util.Date;
+import java.util.List;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public abstract class Gene {
+
+    public abstract int id();
+
+    @NotNull
+    public abstract String geneSymbol();
+
+    @NotNull
+    public abstract List<String> terms();
+
+    @NotNull
+    public abstract String entrezId();
+
+    @NotNull
+    public abstract List<String> synonyms();
+
+    @NotNull
+    public abstract String chromosome();
+
+    @NotNull
+    public abstract String mapLocation();
+
+    @NotNull
+    public abstract List<GeneDescription> geneDescriptions();
+
+    @NotNull
+    public abstract String canonicalTranscript();
+
+    @NotNull
+    public abstract String geneRole();
+
+    @NotNull
+    public abstract Date createDate();
+
+    @NotNull
+    public abstract Date updateDate();
+}
---
+++ ckb-importer/src/main/java/com/hartwig/hmftools/ckb/datamodelinterpretation/gene/Reference.java
@@ -0,0 +1,22 @@
+package com.hartwig.hmftools.ckb.datamodelinterpretation.gene;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public abstract class Reference {
+
+    public abstract int id();
+
+    @NotNull
+    public abstract String pubMedId();
+
+    @NotNull
+    public abstract String title();
+
+    @NotNull
+    public abstract String url();
+
+}
