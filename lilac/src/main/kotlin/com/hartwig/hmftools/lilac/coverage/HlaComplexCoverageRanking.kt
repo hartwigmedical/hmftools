0347414ee03fd20f529b08a57ea05a512941769e
Jonathan Baber <jonbaber@gmail.com>
2021-02-16 09:23:49 +1000

    better formatting

--- lilac/src/main/kotlin/com/hartwig/hmftools/lilac/coverage/HlaComplexCoverageRanking.kt
+++ lilac/src/main/kotlin/com/hartwig/hmftools/lilac/coverage/HlaComplexCoverageRanking.kt
@@ -2,14 +2,14 @@ package com.hartwig.hmftools.lilac.coverage
 
 import kotlin.math.min
 
-
-class HlaComplexCoverageRanking(private val maxDistanceFromTopScore: Int = 2) {
-
-    // Complexes within 2 fragments of the highest aligned fragment count are considered as possible solutions.
-    // These are then ranked by:
-    // - first prioritising solutions with the lowest number of wildcard matches,
-    // - then choosing solutions with the most homozygous alleles,
-    // - then choosing the solution with the highest allele population frequency.
+/**
+ * Complexes within [maxDistanceFromTopScore] fragments of the highest aligned fragment count are considered as possible solutions.
+ * These are then ranked by:
+ * - first prioritising solutions with the lowest number of wildcard matches,
+ * - then choosing solutions with the most homozygous alleles,
+ * - then choosing the solution with the highest allele population frequency.
+ */
+class HlaComplexCoverageRanking(private val maxDistanceFromTopScore: Int = 10) {
 
     fun candidateRanking(complexes: List<HlaComplexCoverage>): List<HlaComplexCoverage> {
         require(complexes.isNotEmpty())
@@ -20,7 +20,7 @@ class HlaComplexCoverageRanking(private val maxDistanceFromTopScore: Int = 2) {
                 .sortedWith(Comparator { x, y -> compare(x, y) })
     }
 
-    fun compare(o1: HlaComplexCoverage, o2: HlaComplexCoverage): Int {
+    private fun compare(o1: HlaComplexCoverage, o2: HlaComplexCoverage): Int {
         val wildCoverageCompare = o1.wildCoverage.compareTo(o2.wildCoverage)
         if (wildCoverageCompare != 0) {
             return wildCoverageCompare
