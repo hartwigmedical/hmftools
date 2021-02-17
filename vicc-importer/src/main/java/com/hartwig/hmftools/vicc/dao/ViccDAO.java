95c903f9d038f9dd32f2e453b094200367aedbd9
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-02-17 13:44:00 +0100

    Use correct logger in ViccDAO

--- vicc-importer/src/main/java/com/hartwig/hmftools/vicc/dao/ViccDAO.java
+++ vicc-importer/src/main/java/com/hartwig/hmftools/vicc/dao/ViccDAO.java
@@ -31,7 +31,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.hartwig.hmftools.vicc.ViccJsonSQLImporter;
 import com.hartwig.hmftools.vicc.datamodel.Association;
 import com.hartwig.hmftools.vicc.datamodel.EnvironmentalContext;
 import com.hartwig.hmftools.vicc.datamodel.Evidence;
@@ -71,7 +70,7 @@ import org.jooq.impl.DSL;
 
 public class ViccDAO {
 
-    private static final Logger LOGGER = LogManager.getLogger(ViccJsonSQLImporter.class);
+    private static final Logger LOGGER = LogManager.getLogger(ViccDAO.class);
 
     private static final String DEV_CATALOG = "vicc_test";
 
