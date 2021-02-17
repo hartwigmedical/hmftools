d695247b84b87636478b224f4cc12b3f81b80258
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-02-17 15:01:55 +0100

    fix date converter test CKB

--- ckb-importer/src/test/java/com/hartwig/hmftools/ckb/util/DateConverterTest.java
+++ ckb-importer/src/test/java/com/hartwig/hmftools/ckb/util/DateConverterTest.java
@@ -2,7 +2,7 @@ package com.hartwig.hmftools.ckb.util;
 
 import static org.junit.Assert.assertEquals;
 
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import org.junit.Test;
@@ -12,7 +12,7 @@ public class DateConverterTest {
     @Test
     public void canConvertString() {
         String date = "10/02/2020";
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH);
         assertEquals(date, format.format(DateConverter.toDate(date)));
     }
 }
