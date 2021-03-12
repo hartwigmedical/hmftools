7a7e6426d3dc0c3c23ce036d91ad09f18ef11792
Jonathan Baber <jonbaber@gmail.com>
2021-03-12 10:02:06 +1000

    Do not load MT SV

---
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/variant/filter/HumanChromosomeFilter.java
@@ -0,0 +1,14 @@
+package com.hartwig.hmftools.common.variant.filter;
+
+import com.hartwig.hmftools.common.genome.chromosome.HumanChromosome;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.filter.VariantContextFilter;
+
+public class HumanChromosomeFilter implements VariantContextFilter {
+
+    @Override
+    public boolean test(final VariantContext record) {
+        return HumanChromosome.contains(record.getContig());
+    }
+}
