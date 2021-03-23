4b10cd6bb85b2021fad13d923b161e848bef5c23
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-03-23 19:01:12 +0100

    DEV-1694: move large dels to complex events

--- ckb-importer/src/main/java/com/hartwig/hmftools/ckb/classification/CKBClassificationConfig.java
+++ ckb-importer/src/main/java/com/hartwig/hmftools/ckb/classification/CKBClassificationConfig.java
@@ -36,7 +36,7 @@ public class CKBClassificationConfig {
     private static final Map<String, Set<String>> COMBINED_EVENTS_PER_GENE = combinedEventsPerGene();
     private static final Map<String, Set<String>> COMPLEX_EVENTS_PER_GENE = complexEventsPerGene();
 
-    private CKBClassificationConfig(){
+    private CKBClassificationConfig() {
     }
 
     @NotNull
@@ -220,6 +220,129 @@ public class CKBClassificationConfig {
     @NotNull
     private static Map<String, Set<String>> complexEventsPerGene() {
         Map<String, Set<String>> map = Maps.newHashMap();
+
+        Set<String> arSet = Sets.newHashSet("V7_splice");
+        map.put("AR", arSet);
+
+        Set<String> atmSet = Sets.newHashSet("G2718_K2756del", "N2326_K2363del", "R2506_N2543del");
+        map.put("ATM", atmSet);
+
+        Set<String> axin2Set = Sets.newHashSet("E745_S762del", "S217_E823del");
+        map.put("AXIN2", axin2Set);
+
+        Set<String> brafSet = Sets.newHashSet("A81_D380del",
+                "A81_M438del",
+                "G203_G393del",
+                "V169_D380del",
+                "V169_G327del",
+                "V47_D380del",
+                "V47_G327del",
+                "V47_G393del",
+                "V47_M438del");
+        map.put("BRAF", brafSet);
+
+        Set<String> brca1Set = Sets.newHashSet("E427_S713del", "S377_N417del");
+        map.put("BRCA1", brca1Set);
+
+        Set<String> brca2Set = Sets.newHashSet("S917_H1918del");
+        map.put("BRCA2", brca2Set);
+
+        Set<String> brd4Set = Sets.newHashSet("Q762_P780del");
+        map.put("BRD4", brd4Set);
+
+        Set<String> btkSet = Sets.newHashSet("P204_Y263del");
+        map.put("BTK", btkSet);
+
+        Set<String> cblSet = Sets.newHashSet("E366_K382del");
+        map.put("CBL", cblSet);
+
+        Set<String> chek2Set = Sets.newHashSet("D265_H282del", "E107_K197del");
+        map.put("CHEK2", chek2Set);
+
+        Set<String> cic2Set = Sets.newHashSet("R1464_M1519del", "W24_V311del");
+        map.put("CIC", cic2Set);
+
+        Set<String> ctnna1Set = Sets.newHashSet("V36_Q196del");
+        map.put("CTNNA1", ctnna1Set);
+
+        Set<String> ctnnb1Set = Sets.newHashSet("A21_A149del",
+                "A21_A152del",
+                "A5_A80del",
+                "A5_Q143del",
+                "H24_Y142del",
+                "L10_N141del",
+                "M12_D144del",
+                "M8_L132del",
+                "P16_K133del",
+                "T3_A126del",
+                "V22_A97del",
+                "V22_D145del",
+                "V22_G38del",
+                "V22_Y64del");
+        map.put("CTNNB1", ctnnb1Set);
+
+        Set<String> egfrSet = Sets.newHashSet("G696_P1033dup", "G983_G1054del", "T34_A289del", "V1010_D1152del", "V30_R297delinsG");
+        map.put("EGFR", egfrSet);
+
+        Set<String> erbb3Set = Sets.newHashSet("E928fs*16");
+        map.put("ERBB3", erbb3Set);
+
+        Set<String> fancfSet = Sets.newHashSet("L285_V302del");
+        map.put("FANCF", fancfSet);
+
+        Set<String> frs2Set = Sets.newHashSet("F251_Q270del", "P232_G272del", "P232_R295del");
+        map.put("FRS2", frs2Set);
+
+        Set<String> gata1Set = Sets.newHashSet("D65_C228del");
+        map.put("GATA1", gata1Set);
+
+        Set<String> jak1Set = Sets.newHashSet("E966_K989del");
+        map.put("JAK1", jak1Set);
+
+        Set<String> kitSet = Sets.newHashSet("E554_D572delinsA",
+                "M552_D572del",
+                "V555_I571del",
+                "V555_L576del",
+                "V555_P573del",
+                "V556_L576del",
+                "V556_T574del",
+                "V560_L576del",
+                "V560_Y578del");
+        map.put("KIT", kitSet);
+
+        Set<String> lrp1bSet = Sets.newHashSet("D155_I197del");
+        map.put("LRP1B", lrp1bSet);
+
+        Set<String> mlh1Set = Sets.newHashSet("E633_E663del", "P578_E632del");
+        map.put("MLH1", mlh1Set);
+
+        Set<String> notch1Set = Sets.newHashSet("S2499_F2554del", "V1110_S1723del");
+        map.put("NOTCH1", notch1Set);
+
+        Set<String> ntrk1Set = Sets.newHashSet("V3_splice");
+        map.put("NTRK1", ntrk1Set);
+
+        Set<String> pdgfraSet = Sets.newHashSet("C456_R481del", "Y375_K455del");
+        map.put("PDGFRA", pdgfraSet);
+
+        Set<String> pik3r1Set = Sets.newHashSet("I442_Y462del");
+        map.put("PIK3R1", pik3r1Set);
+
+        Set<String> plcg2Set = Sets.newHashSet("A686_W806del", "W646_R685del");
+        map.put("PLCG2", plcg2Set);
+
+        Set<String> ptk2Set = Sets.newHashSet("E956_E982del");
+        map.put("PTK2", ptk2Set);
+
+        Set<String> rb1Set = Sets.newHashSet("T738_R775del");
+        map.put("RB1", rb1Set);
+
+        Set<String> stk11Set = Sets.newHashSet("E98_G155del");
+        map.put("STK11", stk11Set);
+
+        Set<String> znf703Set = Sets.newHashSet("K295_S327del");
+        map.put("ZNF703", znf703Set);
+
         return map;
     }
 }
