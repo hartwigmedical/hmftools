baf2e54e248f1a6ace3fbfce99a6eacec9ae5642
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-03-30 11:50:00 +0200

    DEV-1694: implement logica for cancertype doid

--- serve/src/main/java/com/hartwig/hmftools/serve/sources/ckb/ActionableEvidenceFactory.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/ckb/ActionableEvidenceFactory.java
@@ -1,9 +1,11 @@
 package com.hartwig.hmftools.serve.sources.ckb;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.hartwig.hmftools.ckb.datamodel.CkbEntry;
 import com.hartwig.hmftools.ckb.datamodel.evidence.Evidence;
@@ -18,7 +20,6 @@ import com.hartwig.hmftools.serve.sources.ckb.curation.EvidenceLevelCurator;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 import org.immutables.value.internal.$guava$.annotations.$VisibleForTesting;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -81,7 +82,10 @@ class ActionableEvidenceFactory {
                 EvidenceLevel level = resolveLevel(evidence.ampCapAscoEvidenceLevel());
                 EvidenceDirection direction = resolveDirection(evidence.responseType());
                 String cancerType = evidence.indication().name();
+                String doid = evidence.indication().termId();
                 Set<String> urls = Sets.newHashSet();
+
+                Map<String, Set<String>> cancerTypeToDoidsMap = buildCancerTypeToDoidsMap(cancerType, extractDoid(doid));
 
                 for (Reference reference : evidence.references()) {
                     if (reference.url() != null) {
@@ -89,21 +93,77 @@ class ActionableEvidenceFactory {
                     }
                 }
 
-                level = evidenceLevelCurator.curate(Knowledgebase.CKB, entry.variants().get(0).gene().geneSymbol(), therapyName, level, direction);
+                level = evidenceLevelCurator.curate(Knowledgebase.CKB,
+                        entry.variants().get(0).gene().geneSymbol(),
+                        therapyName,
+                        level,
+                        direction);
                 List<List<String>> drugLists = drugCurator.curate(Knowledgebase.CKB, level, therapyName);
 
                 if (therapyName != null && level != null && direction != null) {
                     ImmutableActionableEvidence.Builder builder =
                             ImmutableActionableEvidence.builder().source(Knowledgebase.CKB).level(level).direction(direction).urls(urls);
-                    for (List<String> drugList : drugLists) {
-                        actionableEvents.add(builder.cancerType(cancerType).doid(Strings.EMPTY).treatment(drugList.toString()).build());
+                    for (Map.Entry<String, Set<String>> cancerTypeEntry : cancerTypeToDoidsMap.entrySet()) {
+                        for (String doidSet : cancerTypeEntry.getValue()) {
+                            for (List<String> drugList : drugLists) {
+                                actionableEvents.add(builder.cancerType(cancerType).doid(doidSet).treatment(drugList.toString()).build());
+                            }
+                        }
                     }
                 }
             }
-
         }
 
         return actionableEvents;
+    }
+
+    @Nullable
+    @VisibleForTesting
+    static String extractDoid(@Nullable String doidString) {
+        if (doidString == null) {
+            return null;
+        }
+
+        String[] parts = doidString.split(":");
+        if (parts.length == 2) {
+            if (parts[0].equalsIgnoreCase("doid")) {
+                return parts[2];
+            } else if (parts[0].equalsIgnoreCase("jax")) {
+                return doidString;
+            } else {
+                return null;
+            }
+        } else {
+            LOGGER.warn("Unexpected Doid string: '{}'", doidString);
+            return null;
+        }
+    }
+
+    @NotNull
+    private Set<String> lookupDoids(@NotNull String cancerType) {
+        Set<String> doids = missingDoidLookup.lookupDoidsForCancerType(cancerType);
+        if (doids != null) {
+            return doids;
+        } else {
+            LOGGER.warn("Could not resolve doids for VICC cancer type '{}'", cancerType);
+            return Sets.newHashSet();
+        }
+    }
+
+    @NotNull
+    private Map<String, Set<String>> buildCancerTypeToDoidsMap(@Nullable String cancerType, @Nullable String doid) {
+        Map<String, Set<String>> cancerTypeToDoidsMap = Maps.newHashMap();
+        if (cancerType != null) {
+            if (doid != null) {
+                if (doid.split(";")[0].equalsIgnoreCase("jax")) {
+                    cancerTypeToDoidsMap.put(cancerType, Sets.newHashSet("0050686"));
+                } else {
+                    cancerTypeToDoidsMap.put(cancerType, Sets.newHashSet(doid));
+
+                }
+            }
+        }
+        return cancerTypeToDoidsMap;
     }
 
     public void evaluateCuration() {
