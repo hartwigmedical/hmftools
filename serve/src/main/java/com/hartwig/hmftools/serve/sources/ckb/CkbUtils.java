4ce85ac222e1f87807cd4de8d746c8028992360e
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-03-31 09:22:25 +0200

    DEV-1694: Improve CKB test app

--- serve/src/main/java/com/hartwig/hmftools/serve/sources/ckb/CkbUtils.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/ckb/CkbUtils.java
@@ -1,7 +1,20 @@
 package com.hartwig.hmftools.serve.sources.ckb;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Set;
+import java.util.StringJoiner;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.hartwig.hmftools.ckb.datamodel.CkbEntry;
 import com.hartwig.hmftools.common.serve.classification.EventType;
 import com.hartwig.hmftools.serve.extraction.ExtractionResult;
+import com.hartwig.hmftools.serve.sources.vicc.ViccUtil;
+import com.hartwig.hmftools.vicc.datamodel.Feature;
+import com.hartwig.hmftools.vicc.datamodel.ViccEntry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,8 +23,29 @@ import org.jetbrains.annotations.NotNull;
 public class CkbUtils {
 
     private static final Logger LOGGER = LogManager.getLogger(CkbUtils.class);
+    private static final String FIELD_DELIMITER = "\t";
 
-    public static void printExtractionResults(@NotNull ExtractionResult result, @NotNull EventType type) {
+    public static void printExtractionResults(@NotNull ExtractionResult result) {
+        LOGGER.info("Analysis performed on CKB extraction result");
+        LOGGER.info(" {} actionable hotspot records generated", result.actionableHotspots().size());
+        LOGGER.info(" {} actionable range records generated", result.actionableRanges().size());
+        LOGGER.info(" {} actionable gene records generated", result.actionableGenes().size());
+        LOGGER.info(" {} actionable fusion records generated", result.actionableFusions().size());
+        LOGGER.info(" {} actionable tumor characteristics records generated", result.actionableCharacteristics().size());
+    }
+
+    public static void writeEventsToTsv(@NotNull String eventTsv, List<CkbEntry> filteredAndcurateCkbEntries) throws IOException {
+        List<String> lines = Lists.newArrayList();
+        String header = new StringJoiner(FIELD_DELIMITER).add("profileName").add("gene").add("event").add("type").toString();
+        lines.add(header);
+
+        for (CkbEntry entry : filteredAndcurateCkbEntries) {
+            lines.add(new StringJoiner(FIELD_DELIMITER).add(entry.profileName()).add(entry.variants().get(0).gene().geneSymbol())
+                    .add(entry.variants().get(0).variant()).add(entry.type().toString()).toString());
+        }
+
+       LOGGER.info("Writing {} unique CKB features to {}", lines.size() - 1, eventTsv);
+        Files.write(new File(eventTsv).toPath(), lines);
     }
 }
 
--- serve/src/main/java/com/hartwig/hmftools/serve/sources/ckb/tools/CkbExtractorTestApp.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/sources/ckb/tools/CkbExtractorTestApp.java
@@ -1,15 +1,9 @@
 package com.hartwig.hmftools.serve.sources.ckb.tools;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 
-import javax.lang.model.element.ElementVisitor;
-
-import com.google.common.collect.Lists;
 import com.hartwig.hmftools.ckb.JsonDatabaseToCkbEntryConverter;
 import com.hartwig.hmftools.ckb.classification.CkbClassificationConfig;
 import com.hartwig.hmftools.ckb.datamodel.CkbEntry;
@@ -20,12 +14,8 @@ import com.hartwig.hmftools.common.drivercatalog.panel.DriverGeneFile;
 import com.hartwig.hmftools.common.fusion.KnownFusionCache;
 import com.hartwig.hmftools.common.genome.genepanel.HmfGenePanelSupplier;
 import com.hartwig.hmftools.common.genome.region.HmfTranscriptRegion;
-import com.hartwig.hmftools.common.serve.Knowledgebase;
 import com.hartwig.hmftools.common.serve.RefGenomeVersion;
-import com.hartwig.hmftools.common.serve.classification.EventClassifier;
 import com.hartwig.hmftools.common.serve.classification.EventClassifierConfig;
-import com.hartwig.hmftools.common.serve.classification.EventClassifierFactory;
-import com.hartwig.hmftools.common.serve.classification.EventType;
 import com.hartwig.hmftools.serve.curation.DoidLookup;
 import com.hartwig.hmftools.serve.curation.DoidLookupFactory;
 import com.hartwig.hmftools.serve.extraction.ExtractionResult;
@@ -38,68 +28,25 @@ import com.hartwig.hmftools.serve.sources.ckb.CkbExtractor;
 import com.hartwig.hmftools.serve.sources.ckb.CkbExtractorFactory;
 import com.hartwig.hmftools.serve.sources.ckb.CkbReader;
 import com.hartwig.hmftools.serve.sources.ckb.CkbUtils;
-import com.hartwig.hmftools.serve.sources.iclusion.IclusionExtractor;
-import com.hartwig.hmftools.serve.sources.iclusion.IclusionExtractorFactory;
-import com.hartwig.hmftools.serve.sources.iclusion.IclusionUtil;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 
 public class CkbExtractorTestApp {
 
     private static final Logger LOGGER = LogManager.getLogger(CkbExtractorTestApp.class);
-    private static final String FIELD_DELIMITER = "\t";
 
     public static void main(String[] args) throws IOException {
         String ckbDir = "/data/common/dbs/ckb/210326_flex_dump";
-        String outputDir = System.getProperty("user.home") + "/tmp";
+        String outputDir = System.getProperty("user.home") + "/tmp/serve_ckb";
+        String eventsTsv = outputDir + "/CkbEvents.tsv";
 
+        // Read and curate CKB datamodel
         CkbJsonDatabase ckbJsonDatabase = CkbJsonReader.read(ckbDir);
         List<CkbEntry> allCkbEntries = JsonDatabaseToCkbEntryConverter.convert(ckbJsonDatabase);
         List<CkbEntry> filteredAndcurateCkbEntries = CkbReader.filterAndCurateRelevantEntries(allCkbEntries);
 
-        EventClassifierConfig config = CkbClassificationConfig.build();
-        EventClassifier classifier = EventClassifierFactory.buildClassifier(config);
-
-        // TODO 1. Make sure every entry has correct event type.
-
-        List<String> lines = Lists.newArrayList();
-        String header = new StringJoiner(FIELD_DELIMITER).add("gene").add("event").add("type").toString();
-        lines.add(header);
-
-        EventType type = EventType.UNKNOWN;
-
-        for (CkbEntry entry : filteredAndcurateCkbEntries) {
-            String gene = entry.variants().get(0).gene().geneSymbol();
-
-            String profileName;
-            if (entry.variants().size() > 1) {
-                type = EventType.COMBINED;
-                profileName = entry.profileName();
-            } else {
-                if (entry.variants().get(0).variant().equals("fusion") && entry.variants().get(0).impact() != null && entry.variants()
-                        .get(0)
-                        .impact()
-                        .equals("fusion")) {
-                    profileName = "fusion promisuous";
-                } else if (entry.variants().get(0).impact() != null && entry.variants().get(0).impact().equals("fusion")) {
-                    profileName = entry.variants().get(0).variant().replaceAll("\\s+","") + " fusion";
-                } else if (entry.variants().get(0).variant().contains("exon")) {
-                    profileName = entry.variants().get(0).variant().replace("exon", "exon ");
-                }
-                else {
-                    profileName = entry.variants().get(0).variant() ;
-                }
-
-                type = classifier.determineType(gene, profileName);
-            }
-
-            lines.add(new StringJoiner(FIELD_DELIMITER).add(gene).add(profileName).add(type.toString()).toString());
-        }
-        Files.write(new File("/data/common/dbs/serve/pilot_output/events.tsv").toPath(), lines);
-
-        // TODO 2. Make sure every event is extracted correctly using EventExtractor
+        // Read required data
         String driverGeneTsvPath = "/data/common/dbs/driver_gene_panel/DriverGenePanel.hg38.tsv";
         String knownFusionFilePath = "/data/common/dbs/fusions/known_fusion_data.csv";
         String missingDoidMappingTsv = "/data/common/dbs/serve/curation/missing_doids_mapping.tsv";
@@ -107,8 +54,6 @@ public class CkbExtractorTestApp {
         RefGenomeVersion refGenomeVersion = RefGenomeVersion.V38;
         Map<String, HmfTranscriptRegion> allGenesMap = HmfGenePanelSupplier.allGenesMap38();
         ProteinResolver proteinResolver = ProteinResolverFactory.dummy();
-
-
         DoidLookup doidLookup = DoidLookupFactory.buildFromConfigTsv(missingDoidMappingTsv);
         List<DriverGene> driverGenes = DriverGeneFile.read(driverGeneTsvPath);
         LOGGER.debug(" Read {} driver genes from {}", driverGenes.size(), driverGeneTsvPath);
@@ -126,16 +71,17 @@ public class CkbExtractorTestApp {
                 .proteinResolver(proteinResolver)
                 .build();
 
+        // TODO 1. Make sure every entry has correct event type.
+
+        CkbUtils.writeEventsToTsv(eventsTsv, filteredAndcurateCkbEntries);
+
+        // TODO 2. Make sure every event is extracted correctly using EventExtractor
+        EventClassifierConfig config = CkbClassificationConfig.build();
         CkbExtractor extractor = CkbExtractorFactory.buildCkbExtractor(config, refGenomeResource, doidLookup);
 
         // TODO 3. Create ActionableEvents for all relevant entries.
         ExtractionResult result = extractor.extract(filteredAndcurateCkbEntries);
-
-       // CkbUtils.printExtractionResults(result, type);
-
-
-        // IclusionUtil.printIclusionResult(result);
-     //   IclusionUtil.writeIclusionMutationTypes(iclusionMutationTsv, trials);
+        CkbUtils.printExtractionResults(result);
 
         new ExtractionResultWriter(outputDir, refGenomeVersion).write(result);
 
