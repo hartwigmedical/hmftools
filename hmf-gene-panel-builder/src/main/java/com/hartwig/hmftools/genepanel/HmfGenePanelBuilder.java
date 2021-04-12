68337e064afe9c4be4c85f1c984e943dd6973ee6
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-12 14:58:18 +0200

    DEV-1905: Patch gene panel to v38 in the builder itself

--- hmf-gene-panel-builder/src/main/java/com/hartwig/hmftools/genepanel/HmfGenePanelBuilder.java
+++ hmf-gene-panel-builder/src/main/java/com/hartwig/hmftools/genepanel/HmfGenePanelBuilder.java
@@ -1,24 +1,26 @@
 package com.hartwig.hmftools.genepanel;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Set;
 
-import com.google.common.collect.Sets;
 import com.google.common.io.Resources;
+import com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -33,9 +35,7 @@ import org.jooq.tools.StringUtils;
 public class HmfGenePanelBuilder {
 
     private static final Logger LOGGER = LogManager.getLogger(HmfGenePanelBuilder.class);
-    private static final Set<String> VERSIONS = Sets.newHashSet("37", "38");
 
-    private static final String ENSEMBL_VERSION = "ensembl";
     private static final String GENE_TSV = "gene_tsv";
     private static final String REFSEQ_TO_ENSEMBL_TSV = "refseq_to_ensembl_tsv";
 
@@ -47,41 +47,62 @@ public class HmfGenePanelBuilder {
         final Options options = createOptions();
         final CommandLine cmd = new DefaultParser().parse(options, args);
 
-        final String ensemblVersion = cmd.getOptionValue(ENSEMBL_VERSION);
+        final RefGenomeVersion refGenomeVersion = RefGenomeVersion.from(cmd.getOptionValue(RefGenomeVersion.REF_GENOME_VERSION));
         final String geneTsv = cmd.getOptionValue(GENE_TSV);
         final String refseqToEnsemblTsv = cmd.getOptionValue(REFSEQ_TO_ENSEMBL_TSV);
 
-        if (!VERSIONS.contains(ensemblVersion) || geneTsv == null || refseqToEnsemblTsv == null) {
+        if (geneTsv == null || refseqToEnsemblTsv == null) {
             final HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("HmfGenePanelBuilder", options);
             System.exit(1);
         }
 
-        final String database = ensemblVersion.equals("37") ? ENSEMBLDB_URL_37 : ENSEMBLDB_URL_38;
+        final String database = refGenomeVersion.is37() ? ENSEMBLDB_URL_37 : ENSEMBLDB_URL_38;
 
-        LOGGER.info("Running hmf-gene-panel-builder with version {}", ensemblVersion);
+        LOGGER.info("Running hmf-gene-panel-builder with version {}", refGenomeVersion);
         final DSLContext context = connectEnsemblDb(database);
         LOGGER.info(" Connected to {}", database);
 
+        generateGenes(context, geneTsv, refGenomeVersion);
+        generateRefSeqToEnsembl(context, refseqToEnsemblTsv);
+
+        LOGGER.info("Complete");
+    }
+
+    private static void generateGenes(@NotNull final DSLContext context, @NotNull final String outputTsv,
+            @NotNull final RefGenomeVersion refGenomeVersion) throws IOException {
         final Result<Record> geneResult = context.fetch(read(Resources.getResource("sql/ensembl_gene_query.sql")));
         LOGGER.info(" Gene query returned {} entries", geneResult.size());
 
-        writeAsTsv(geneTsv, geneResult);
-        LOGGER.info(" Written gene output to {}", geneTsv);
+        writeAsTsv(outputTsv, geneResult);
+        LOGGER.info(" Written gene output to {}", outputTsv);
 
+        if (refGenomeVersion == RefGenomeVersion.V38) {
+            LOGGER.info("  Patching genes to v38 in {}", outputTsv);
+            final List<String> genes = Files.readAllLines(new File(outputTsv).toPath());
+            final List<String> updatedGenes = Lists.newArrayList();
+
+            // First column is the chromosome, and we need to prefix with "chr" to patch the genes to v38 ref genome
+            for (String gene : genes) {
+                updatedGenes.add("chr" + gene);
+            }
+            Files.write(new File(outputTsv).toPath(), updatedGenes);
+            LOGGER.info("  Finished patching genes to v38 in {}", outputTsv);
+        }
+    }
+
+    private static void generateRefSeqToEnsembl(@NotNull final DSLContext context, @NotNull final String outputTsv) throws IOException {
         final Result<Record> refseqToEnsemblResult = context.fetch(read(Resources.getResource("sql/refseq_to_ensembl_transcripts.sql")));
         LOGGER.info(" RefSeq to Ensembl query returned {} entries", refseqToEnsemblResult.size());
 
-        writeAsTsv(refseqToEnsemblTsv, refseqToEnsemblResult);
-        LOGGER.info(" Written RefSeq to Ensembl output to {}", refseqToEnsemblTsv);
-
-        LOGGER.info("Complete");
+        writeAsTsv(outputTsv, refseqToEnsemblResult);
+        LOGGER.info(" Written RefSeq to Ensembl output to {}", outputTsv);
     }
 
     @NotNull
     private static Options createOptions() {
         final Options options = new Options();
-        options.addOption(ENSEMBL_VERSION, true, "Ensembl version to use. Must be either 37 or 38.");
+        options.addOption(RefGenomeVersion.REF_GENOME_VERSION, true, "Ref genome version to generate files for");
         options.addOption(GENE_TSV, true, "Path towards the gene tsv output file.");
         options.addOption(REFSEQ_TO_ENSEMBL_TSV, true, "Path towards the refseq to ensembl tsv output file.");
         return options;
