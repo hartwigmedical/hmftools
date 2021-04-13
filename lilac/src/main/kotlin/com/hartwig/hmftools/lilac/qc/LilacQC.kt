419556467351934c9128c7533c8b2870d0f4c213
Jonathan Baber <jonbaber@gmail.com>
2021-04-14 07:51:19 +1000

    Support multiple QC Status

--- lilac/src/main/kotlin/com/hartwig/hmftools/lilac/qc/LilacQC.kt
+++ lilac/src/main/kotlin/com/hartwig/hmftools/lilac/qc/LilacQC.kt
@@ -3,30 +3,34 @@ package com.hartwig.hmftools.lilac.qc
 import java.io.File
 
 
-data class LilacQC(val status: LilacQCStatus, val aminoAcidQC: AminoAcidQC, val bamQC: BamQC, val coverageQC: CoverageQC, val haplotypeQC: HaplotypeQC, val somaticVariantQC: SomaticVariantQC) {
+data class LilacQC(val status: Set<LilacQCStatus>, val aminoAcidQC: AminoAcidQC, val bamQC: BamQC, val coverageQC: CoverageQC, val haplotypeQC: HaplotypeQC, val somaticVariantQC: SomaticVariantQC) {
 
     companion object {
         fun create(aminoAcidQC: AminoAcidQC, bamQC: BamQC, coverageQC: CoverageQC, haplotypeQC: HaplotypeQC, somaticVariantQC: SomaticVariantQC): LilacQC {
-            var status = LilacQCStatus.PASS
+            val status = mutableSetOf<LilacQCStatus>()
 
             if (haplotypeQC.unusedHaplotypes > 0) {
-                status = LilacQCStatus.WARN_UNMATCHED_HAPLOTYPE
+                status.add(LilacQCStatus.WARN_UNMATCHED_HAPLOTYPE)
             }
 
             if (coverageQC.aTypes == 0 || coverageQC.bTypes == 0 || coverageQC.cTypes == 0 ) {
-                status = LilacQCStatus.WARN_UNMATCHED_TYPE
+                status.add(LilacQCStatus.WARN_UNMATCHED_TYPE)
             }
 
             if (bamQC.discardedIndelFragments > 0) {
-                status = LilacQCStatus.WARN_UNMATCHED_INDEL
+                status.add(LilacQCStatus.WARN_UNMATCHED_INDEL)
             }
 
             if (somaticVariantQC.unmatchedVariants()) {
-                status = LilacQCStatus.WARN_UNMATCHED_SOMATIC_VARIANT
+                status.add(LilacQCStatus.WARN_UNMATCHED_SOMATIC_VARIANT)
             }
 
             if (coverageQC.percentWildcard > 0) {
-                status = LilacQCStatus.WARN_WILDCARD_MATCH
+                status.add(LilacQCStatus.WARN_WILDCARD_MATCH)
+            }
+
+            if (status.isEmpty()) {
+                status.add(LilacQCStatus.PASS)
             }
 
             return LilacQC(status, aminoAcidQC, bamQC, coverageQC, haplotypeQC, somaticVariantQC)
@@ -38,7 +42,7 @@ data class LilacQC(val status: LilacQCStatus, val aminoAcidQC: AminoAcidQC, val 
     }
 
     fun body(): List<String> {
-        return listOf(status.toString()) + aminoAcidQC.body() + bamQC.body() + coverageQC.body() + haplotypeQC.body() + somaticVariantQC.body()
+        return listOf(status.joinToString(",")) + aminoAcidQC.body() + bamQC.body() + coverageQC.body() + haplotypeQC.body() + somaticVariantQC.body()
     }
 
     fun writefile(fileName: String) {
