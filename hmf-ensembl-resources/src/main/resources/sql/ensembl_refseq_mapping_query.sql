8dd41f40cd4af160498e0b74174e7492354f2b2e
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-04-13 12:18:55 +0200

    DEV-1694: update ensembl query

--- hmf-ensembl-resources/src/main/resources/sql/ensembl_refseq_mapping_query.sql
+++ hmf-ensembl-resources/src/main/resources/sql/ensembl_refseq_mapping_query.sql
@@ -9,5 +9,5 @@ from xref
 inner join object_xref on xref.xref_id=object_xref.xref_id
 inner join transcript on object_xref.ensembl_id=transcript.transcript_id
 inner join gene on transcript.gene_id=gene.gene_id
-left join xref as entrez_xref on (entrez_xref.xref_id=object_xref.xref_id and entrez_xref.external_db_id = 1801);
+left join xref as entrez_xref on entrez_xref.xref_id=object_xref.xref_id where entrez_xref.external_db_id = 1801;
 
