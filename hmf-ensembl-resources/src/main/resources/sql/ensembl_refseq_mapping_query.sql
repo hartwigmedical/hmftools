013d9690378180e3bd91ae3160f484788c40bc0e
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-04-13 12:20:44 +0200

    DEV-1694: uase only refseqId, geneId, transcriptId

--- hmf-ensembl-resources/src/main/resources/sql/ensembl_refseq_mapping_query.sql
+++ hmf-ensembl-resources/src/main/resources/sql/ensembl_refseq_mapping_query.sql
@@ -1,10 +1,7 @@
 select
+    xref.dbprimary_acc as refSeqId,
     gene.stable_id as geneId,
-    transcript.stable_id as transcriptId,
-    xref.display_label,
-    xref.dbprimary_acc,
-    transcript.seq_region_start,
-    transcript.seq_region_end
+    transcript.stable_id as transcriptId
 from xref
 inner join object_xref on xref.xref_id=object_xref.xref_id
 inner join transcript on object_xref.ensembl_id=transcript.transcript_id
