62779cb13c8eadf3bb28bb800fc1c005ee791ad3
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-19 13:10:48 +0200

    DEV-1943: Remove reference to hpc from paddle

--- paddle/src/main/kotlin/com/hartwig/hmftools/paddle/cohort/CohortSample.kt
+++ paddle/src/main/kotlin/com/hartwig/hmftools/paddle/cohort/CohortSample.kt
@@ -3,7 +3,7 @@ package com.hartwig.hmftools.paddle.cohort
 import java.io.File
 import java.nio.file.Files
 
-data class HighestPuritySample(val sampleId: String, val purity: Double) {
+data class CohortSample(val sampleId: String, val purity: Double) {
 
     override fun toString(): String {
         return "$sampleId\t$purity"
@@ -11,13 +11,13 @@ data class HighestPuritySample(val sampleId: String, val purity: Double) {
 
     companion object {
 
-        fun readFile(file: String): List<HighestPuritySample> {
+        fun readFile(file: String): List<CohortSample> {
             return Files.readAllLines(File(file).toPath()).drop(1).map { fromString(it) }
         }
 
-        private fun fromString(line: String): HighestPuritySample {
+        private fun fromString(line: String): CohortSample {
             val lineArray = line.split("\t")
-            return HighestPuritySample(lineArray[0], lineArray[1].toDouble())
+            return CohortSample(lineArray[0], lineArray[1].toDouble())
         }
     }
 }
