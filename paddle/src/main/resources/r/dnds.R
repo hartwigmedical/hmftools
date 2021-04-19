b9babe1e0e4f87161f324b5ea799871c438246ba
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-19 11:02:29 +0200

    DEV-1943: Make R script independent from hardcoded paths

--- paddle/src/main/resources/r/dnds.R
+++ paddle/src/main/resources/r/dnds.R
@@ -1,5 +1,15 @@
 
-path = "/data/experiments/kodu_paddle_test/test_cohort/somatics"
+args <- commandArgs(trailing = T)
+
+work_dir <- args[1]
+
+somatics_input_dir <- paste0(work_dir, "/somatics")
+mutation_load_input_file <- paste0(work_dir, "/mutationalLoad.tsv")
+
+all_somatics_output_file <- paste0(work_dir, "/allSomatics.RData")
+ref_cdscv_output_rdata <- paste0(work_dir, "/HmfRefCDSCv.RData")
+ref_cdscv_output_tsv <- paste0(work_dir, "/HmfRefCDSCv.tsv")
+dnds_mutations_output_tsv <- paste0(work_dir, "/DndsMutations.tsv")
 
 library(GenomicRanges)
 library(dplyr)
@@ -17,9 +27,9 @@ data("covariates_hg19", package="dndscv")
 refdb="/data/common/dbs/dnds/HmfRefCDS.RData"
 load(refdb)
 
-summary = read.table(file = "/data/experiments/kodu_paddle_test/test_cohort/mutationalLoad.tsv", header = T)
+summary = read.table(file = mutation_load_input_file, header = T)
 
-files = list.files(path, full.names = T, pattern = "exonic.somatics.tsv")
+files = list.files(somatics_input_dir, full.names = T, pattern = "exonic.somatics.tsv")
 allSomatics = data.frame(stringsAsFactors = F)
 for (i in 1:length(files)) {
   file = files[i]
@@ -34,7 +44,7 @@ for (i in 1:length(files)) {
 rm(somatics)
 rm(exonicSomatics)
 
-save(allSomatics, file ="/data/experiments/kodu_paddle_test/test_cohort/allSomatics.RData")
+save(allSomatics, file = all_somatics_output_file)
 
 ############### Dnds Results
 newgenes = sapply(RefCDS, function(x) x$gene_name) # New list of genes
@@ -45,8 +55,8 @@ kc = newgenes[oldgenes %in% known_cancergenes]
 
 output = dndscv(allSomatics, refdb=refdb, kc=kc, cv=cv, stop_loss_is_nonsense = TRUE)
 HmfRefCDSCv <- output$sel_cv
-save(HmfRefCDSCv, file ="/data/experiments/kodu_paddle_test/test_cohort/HmfRefCDSCv.RData")
-write.table(HmfRefCDSCv, file ="/data/experiments/kodu_paddle_test/test_cohort/HmfRefCDSCv.tsv", quote = F, row.names = F, sep = "\t")
+save(HmfRefCDSCv, file = ref_cdscv_output_rdata)
+write.table(HmfRefCDSCv, file = ref_cdscv_output_tsv, quote = F, row.names = F, sep = "\t")
 
 ############### Dnds Mutations
 unfilteredSomatics = allSomatics %>% select(sample, chr = chromosome, pos = position, ref, alt, worstCodingEffect, canonicalCodingEffect, repeatCount, biallelic, hotspot)
@@ -55,4 +65,4 @@ dndsUnfilteredAnnotatedMutations = unfilteredOutput$annotmuts
 
 names(dndsUnfilteredAnnotatedMutations) <- c("sample", "chr","pos", "ref", "mut", "worstCodingEffect", "canonicalCodingEffect", "repeatCount", "biallelic", "hotspot", "geneind", "gene", "ref_cod", "mut_cod", "strand", "ref3_cod", "mut3_cod", "aachange", "ntchange","impact",  "pid")
 dndsUnfilteredAnnotatedMutations = dndsUnfilteredAnnotatedMutations %>% select(sample, chr, pos, ref, mut, worstCodingEffect, canonicalCodingEffect, repeatCount, biallelic, hotspot, gene, impact)
-write.table(dndsUnfilteredAnnotatedMutations, file ="/data/experiments/kodu_paddle_test/test_cohort/DndsMutations.tsv", quote = F, row.names = F, sep = "\t")
+write.table(dndsUnfilteredAnnotatedMutations, file = dnds_mutations_output_tsv, quote = F, row.names = F, sep = "\t")
