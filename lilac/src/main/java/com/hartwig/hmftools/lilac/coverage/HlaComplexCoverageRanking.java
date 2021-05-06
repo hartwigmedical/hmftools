7a7a2477debad4d983862f8fc4f4a545f78ffb06
Charles Shale <shalecharles@gmail.com>
2021-05-07 09:12:08 +1000

    Lilac: complex ranking

--- lilac/src/main/java/com/hartwig/hmftools/lilac/coverage/HlaComplexCoverageRanking.java
+++ lilac/src/main/java/com/hartwig/hmftools/lilac/coverage/HlaComplexCoverageRanking.java
@@ -1,14 +1,26 @@
 package com.hartwig.hmftools.lilac.coverage;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import static java.lang.Math.min;
+
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.hartwig.hmftools.lilac.hla.HlaAllele;
 
-public class HlaComplexCoverageRanking
+/**
+ * Complexes within [maxDistanceFromTopScore] fragments of the highest aligned fragment count are considered as possible solutions.
+ * These are then ranked by:
+ * - first prioritising solutions with the fewest alleles with wildcard matches,
+ * - then choosing solutions with the most homozygous alleles,
+ * - then choosing solution with the most common alleles,
+ * - then choosing solution with the least recovered alleles
+ * - finally choosing the solution with the lowest number.
+ */
+
+public class HlaComplexCoverageRanking implements Comparator<HlaComplexCoverage>
 {
     private final int mMaxDistanceFromTopScore;
     private final List<HlaAllele> mCommon;
@@ -16,8 +28,7 @@ public class HlaComplexCoverageRanking
     private final List<HlaAllele> mFavourites;
 
     public HlaComplexCoverageRanking(
-            int maxDistanceFromTopScore, final List<HlaAllele> common, final List<HlaAllele> recovered,
-            final List<HlaAllele> favourites)
+            int maxDistanceFromTopScore, final List<HlaAllele> common, final List<HlaAllele> recovered, final List<HlaAllele> favourites)
     {
         mMaxDistanceFromTopScore = maxDistanceFromTopScore;
         mCommon = common;
@@ -25,254 +36,71 @@ public class HlaComplexCoverageRanking
         mFavourites = favourites;
     }
 
-    public static List<HlaComplexCoverage> candidateRanking(final List<HlaComplexCoverage> complexes)
+    public List<HlaComplexCoverage> candidateRanking(final List<HlaComplexCoverage> complexes)
     {
-        // TODO
-        return Lists.newArrayList();
-        
-        /*
-        int topScore;
-        HlaComplexCoverage it;
-        Iterable $receiver$iv$iv;
-        Iterable $receiver$iv;
-        boolean bl;
-        Intrinsics.checkParameterIsNotNull(complexes, (String) "complexes");
-        Collection collection = complexes;
-        boolean bl2 = bl = !collection.isEmpty();
-        if(!bl)
-        {
-            String string = "Failed requirement.";
-            throw (Throwable) new IllegalArgumentException(string.toString());
-        }
-        Iterable iterable = $receiver$iv = (Iterable) complexes;
-        Collection destination$iv$iv = new ArrayList(CollectionsKt.collectionSizeOrDefault((Iterable) $receiver$iv, (int) 10));
-        for(Object item$iv$iv : $receiver$iv$iv)
-        {
-            HlaComplexCoverage
-                    hlaComplexCoverage = (HlaComplexCoverage) item$iv$iv;
-            Collection collection2 = destination$iv$iv;
-            boolean bl3 = false;
-            Integer n = it.getTotalCoverage();
-            collection2.add(n);
-        }
-        Comparable comparable = CollectionsKt.max((Iterable) ((List) destination$iv$iv));
-        if(comparable == null)
-        {
-            Intrinsics.throwNpe();
-        }
-        if((topScore = ((Number) ((Object) comparable)).intValue()) == 0)
-        {
-            return CollectionsKt.emptyList();
-        }
-        $receiver$iv = complexes;
-        $receiver$iv$iv = $receiver$iv;
-        destination$iv$iv = new ArrayList();
-        for(Object element$iv$iv : $receiver$iv$iv)
-        {
-            it = (HlaComplexCoverage) element$iv$iv;
-            boolean bl4 = false;
-            if(!(it.getTotalCoverage() >= topScore - mMaxDistanceFromTopScore))
-            {
-                continue;
-            }
-            destination$iv$iv.add(element$iv$iv);
-        }
-        return CollectionsKt.sortedWith((Iterable) ((List) destination$iv$iv), (Comparator) new Comparator<HlaComplexCoverage>(this)
-         */
+        int topScore = complexes.stream().mapToInt(x -> x.TotalCoverage).max().orElse(0);
+        if (topScore == 0)
+            return Lists.newArrayList();
+
+        List<HlaComplexCoverage> results = complexes.stream()
+                .filter(x -> x.TotalCoverage >= topScore - mMaxDistanceFromTopScore).collect(Collectors.toList());
+
+        Collections.sort(results, this);
+        return results;
     }
 
-    private static int compare(HlaComplexCoverage o1, HlaComplexCoverage o2)
+    public int compare(final HlaComplexCoverage o1, final HlaComplexCoverage o2)
     {
-        return 1;
-        
-        /*
-        int o2RecoveredCount;
-        int o2CommonCount;
-        int favouritesCount = Intrinsics.compare((int) favourites(o1), (int) favourites(o2));
-        if(favouritesCount != 0)
-        {
+        int favouritesCount = favourites(o1) - favourites(o2);
+        if (favouritesCount != 0)
             return -favouritesCount;
-        }
-        int wildcardCount = Intrinsics.compare((int) wildcardCount(o1), (int) wildcardCount(o2));
-        if(wildcardCount != 0)
-        {
+
+        int wildcardCount = wildcardCount(o1) - wildcardCount(o2);
+        if (wildcardCount != 0)
             return wildcardCount;
-        }
-        int homozygousCompare = Intrinsics.compare((int) o1.homozygousAlleles(), (int) o2.homozygousAlleles());
-        if(homozygousCompare != 0)
-        {
+
+        int homozygousCompare = o1.homozygousAlleles() - o2.homozygousAlleles();
+        if (homozygousCompare != 0)
             return -homozygousCompare;
-        }
-        int o1CommonCount = commonCount(o1);
-        int commonCountCompare = Intrinsics.compare((int) o1CommonCount, (int) (o2CommonCount = commonCount(o2)));
-        if(commonCountCompare != 0)
-        {
+
+        int commonCountCompare = commonCount(o1) - commonCount(o2);
+        if (commonCountCompare != 0)
             return -commonCountCompare;
-        }
-        int o1RecoveredCount = recoveredCount(o1);
-        int recoveredCountCompare = Intrinsics.compare((int) o1RecoveredCount, (int) (o2RecoveredCount = recoveredCount(o2)));
-        if(recoveredCountCompare != 0)
-        {
+
+        int recoveredCountCompare = recoveredCount(o1) - recoveredCount(o2);
+        if (recoveredCountCompare != 0)
             return recoveredCountCompare;
-        }
-        int n = 0;
-        int n2 = o1.getAlleleCoverage().size();
-        int n3 = o2.getAlleleCoverage().size();
-        int n4 = Math.min(n2, n3);
-        while(n < n4)
+
+        for (int i = 0; i < min(o1.getAlleleCoverage().size(), o2.getAlleleCoverage().size()); ++i)
         {
-            HlaAllele o2Allele;
-            void i;
-            HlaAllele o1Allele = o1.getAlleleCoverage().get((int) i).getAllele();
-            int alleleCompare = o1Allele.compareTo(o2Allele = o2.getAlleleCoverage().get((int) i).getAllele());
-            if(alleleCompare != 0)
-            {
+            HlaAllele o1Allele = o1.getAlleleCoverage().get(i).Allele;
+            HlaAllele o2Allele = o2.getAlleleCoverage().get(i).Allele;
+
+            int alleleCompare = o1Allele.compareTo(o2Allele);
+            if (alleleCompare != 0)
                 return alleleCompare;
-            }
-            ++i;
         }
-        throw (Throwable) new UnsupportedOperationException("Should not be able to make it to here");
-        
-         */
+
+        return 0;
     }
 
-    private static int favourites(final HlaComplexCoverage coverage)
+    private int favourites(final HlaComplexCoverage coverage)
     {
-        return 1;
-        
-        /*
-        HlaAllele it;
-        Iterable $receiver$iv$iv;
-        Iterable $receiver$iv;
-        Iterable iterable = $receiver$iv = (Iterable) coverage.getAlleleCoverage();
-        Collection destination$iv$iv = new ArrayList(CollectionsKt.collectionSizeOrDefault((Iterable) $receiver$iv, (int) 10));
-        for(Object item$iv$iv : $receiver$iv$iv)
-        {
-            HlaAlleleCoverage
-                    hlaAlleleCoverage = (HlaAlleleCoverage) item$iv$iv;
-            Collection collection = destination$iv$iv;
-            boolean bl = false;
-            HlaAllele hlaAllele = ((HlaAlleleCoverage) ((Object) it)).getAllele();
-            collection.add(hlaAllele);
-        }
-        $receiver$iv = (List) destination$iv$iv;
-        $receiver$iv$iv = $receiver$iv;
-        destination$iv$iv = new ArrayList();
-        for(Object element$iv$iv : $receiver$iv$iv)
-        {
-            it = (HlaAllele) element$iv$iv;
-            boolean bl = false;
-            if(!mFavourites.contains(it))
-            {
-                continue;
-            }
-            destination$iv$iv.add(element$iv$iv);
-        }
-        Collection collection = (List) destination$iv$iv;
-        return collection.size();
-        
-         */
+        return (int)coverage.getAlleleCoverage().stream().filter(x -> mFavourites.contains(x.Allele)).count();
     }
 
-    private static int wildcardCount(HlaComplexCoverage coverage)
+    private static int wildcardCount(final HlaComplexCoverage coverage)
     {
-        return 1;
-        // TODO
-        
-        /*
-        void $receiver$iv$iv;
-        Iterable $receiver$iv;
-        Iterable iterable = $receiver$iv = (Iterable) coverage.getAlleleCoverage();
-        Collection destination$iv$iv = new ArrayList();
-        for(Object element$iv$iv : $receiver$iv$iv)
-        {
-            HlaAlleleCoverage it = (HlaAlleleCoverage) element$iv$iv;
-            boolean bl = false;
-            if(!(it.getWildCoverage() > 0.0))
-            {
-                continue;
-            }
-            destination$iv$iv.add(element$iv$iv);
-        }
-        Collection collection = (List) destination$iv$iv;
-        return collection.size();
-        
-         */
+        return (int)coverage.getAlleleCoverage().stream().filter(x -> x.WildCoverage > 0).count();
     }
 
-    private final int commonCount(HlaComplexCoverage coverage)
+    private final int commonCount(final HlaComplexCoverage coverage)
     {
-        return 1;
-        
-        /*
-        HlaAllele it;
-        Iterable $receiver$iv$iv;
-        Iterable $receiver$iv;
-        Iterable iterable = $receiver$iv = (Iterable) coverage.getAlleleCoverage();
-        Collection destination$iv$iv = new ArrayList(CollectionsKt.collectionSizeOrDefault((Iterable) $receiver$iv, (int) 10));
-        for(Object item$iv$iv : $receiver$iv$iv)
-        {
-            HlaAlleleCoverage
-                    hlaAlleleCoverage = (HlaAlleleCoverage) item$iv$iv;
-            Collection collection = destination$iv$iv;
-            boolean bl = false;
-            HlaAllele hlaAllele = ((HlaAlleleCoverage) ((Object) it)).getAllele();
-            collection.add(hlaAllele);
-        }
-        $receiver$iv = (List) destination$iv$iv;
-        $receiver$iv$iv = $receiver$iv;
-        destination$iv$iv = new ArrayList();
-        for(Object element$iv$iv : $receiver$iv$iv)
-        {
-            it = (HlaAllele) element$iv$iv;
-            boolean bl = false;
-            if(!mCommon.contains(it))
-            {
-                continue;
-            }
-            destination$iv$iv.add(element$iv$iv);
-        }
-        Collection collection = (List) destination$iv$iv;
-        return collection.size();
-        
-         */
+        return (int)coverage.getAlleleCoverage().stream().filter(x -> mCommon.contains(x.Allele)).count();
     }
 
-    private final int recoveredCount(HlaComplexCoverage coverage)
+    private final int recoveredCount(final HlaComplexCoverage coverage)
     {
-        return 1;
-        
-        /*
-        HlaAllele it;
-        Iterable $receiver$iv$iv;
-        Iterable $receiver$iv;
-        Iterable iterable = $receiver$iv = (Iterable) coverage.getAlleleCoverage();
-        Collection destination$iv$iv = new ArrayList(CollectionsKt.collectionSizeOrDefault((Iterable) $receiver$iv, (int) 10));
-        for(Object item$iv$iv : $receiver$iv$iv)
-        {
-            HlaAlleleCoverage
-                    hlaAlleleCoverage = (HlaAlleleCoverage) item$iv$iv;
-            Collection collection = destination$iv$iv;
-            boolean bl = false;
-            HlaAllele hlaAllele = ((HlaAlleleCoverage) ((Object) it)).getAllele();
-            collection.add(hlaAllele);
-        }
-        $receiver$iv = (List) destination$iv$iv;
-        $receiver$iv$iv = $receiver$iv;
-        destination$iv$iv = new ArrayList();
-        for(Object element$iv$iv : $receiver$iv$iv)
-        {
-            it = (HlaAllele) element$iv$iv;
-            boolean bl = false;
-            if(!mRecovered.contains(it))
-            {
-                continue;
-            }
-            destination$iv$iv.add(element$iv$iv);
-        }
-        Collection collection = (List) destination$iv$iv;
-        return collection.size();
-        
-         */
+        return (int)coverage.getAlleleCoverage().stream().filter(x -> mRecovered.contains(x.Allele)).count();
     }
 }
