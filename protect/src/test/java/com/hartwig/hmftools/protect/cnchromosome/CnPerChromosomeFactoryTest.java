1d7fc4ef74f1bccd40ab6aeb30d39b9153e8fbf4
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-05-17 13:59:46 +0200

    DEV-1832: add test to ignore

--- protect/src/test/java/com/hartwig/hmftools/protect/cnchromosome/CnPerChromosomeFactoryTest.java
+++ protect/src/test/java/com/hartwig/hmftools/protect/cnchromosome/CnPerChromosomeFactoryTest.java
@@ -5,13 +5,15 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 
 import org.jetbrains.annotations.NotNull;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CnPerChromosomeFactoryTest {
 
     @Test
+    @Ignore
     public void extarctCopyNumberperChromosomeArm() throws IOException {
-        CnPerChromosomeFactory.fromPurpleSomaticCopynumberTsv(System.getProperty("user.home") + "/hmf/tmp/test.purple.cnv.somatic.tsv");
+        CnPerChromosomeFactory.fromPurpleSomaticCopynumberTsv(System.getProperty("user.home") + "/hmf/tmp/COREDB010053T.purple.cnv.somatic.tsv");
 
     }
 
