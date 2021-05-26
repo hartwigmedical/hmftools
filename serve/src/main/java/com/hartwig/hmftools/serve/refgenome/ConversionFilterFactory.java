eff11fe8baa004c0df71ff63287971b54b9f38f0
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-05-26 14:19:28 +0200

    DEV-1976: Formatting

--- serve/src/main/java/com/hartwig/hmftools/serve/refgenome/ConversionFilterFactory.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/refgenome/ConversionFilterFactory.java
@@ -16,8 +16,8 @@ final class ConversionFilterFactory {
         // These genes lie on a part of the ref genome that has been flipped.
         // As a result these genes flipped strand between ref genome versions and are filtered for ref genome conversion
         GENES_TO_EXCLUDE_FOR_CONVERSION.add("MAGEA1");
-        GENES_TO_EXCLUDE_FOR_CONVERSION.add("PDE4DIP");
         GENES_TO_EXCLUDE_FOR_CONVERSION.add("NCOA4");
+        GENES_TO_EXCLUDE_FOR_CONVERSION.add("PDE4DIP");
     }
 
     private ConversionFilterFactory() {
