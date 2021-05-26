dda8a7b340f70bbb6dc65eab378b2ac54d4c0490
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-05-26 14:44:52 +0200

    DEV-1976: Do some fiddling with transvar gene mapping

--- serve/src/main/java/com/hartwig/hmftools/serve/transvar/TransvarCurator.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/transvar/TransvarCurator.java
@@ -1,8 +1,10 @@
 package com.hartwig.hmftools.serve.transvar;
 
 import java.util.Map;
+import java.util.Set;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.hartwig.hmftools.common.genome.refgenome.GeneNameMapping;
 import com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion;
 
@@ -13,11 +15,17 @@ import org.jetbrains.annotations.NotNull;
 class TransvarCurator {
 
     private static final Logger LOGGER = LogManager.getLogger(TransvarCurator.class);
+
     private static final Map<String, String> PROTEIN_ANNOTATION_MAPPING = Maps.newHashMap();
+    private static final Set<String> GENES_FOR_WHICH_TO_SKIP_38_MAPPING = Sets.newHashSet();
+    private static final Map<String, String> ADDITIONAL_GENE_MAPPING_38_TO_37 = Maps.newHashMap();
 
     static {
         // Transvar can't interpret start-lost so we map it to another mutation.
         PROTEIN_ANNOTATION_MAPPING.put("M1?", "M1I");
+
+        ADDITIONAL_GENE_MAPPING_38_TO_37.put("EPOP", "C17orf96");
+        GENES_FOR_WHICH_TO_SKIP_38_MAPPING.add("CCDC186");
     }
 
     @NotNull
@@ -34,7 +42,13 @@ class TransvarCurator {
         // Somehow can't manage to configure transvar to use typical v38 gene names.
         String v37Gene = gene;
         if (refGenomeVersion == RefGenomeVersion.V38) {
-            if (geneNameMapping.isValidV38Gene(gene)) {
+            if (ADDITIONAL_GENE_MAPPING_38_TO_37.containsKey(gene)) {
+                v37Gene = ADDITIONAL_GENE_MAPPING_38_TO_37.get(gene);
+                LOGGER.debug("Manually mapping gene '{}' to '{}' for transvar", gene, v37Gene);
+            } else if (GENES_FOR_WHICH_TO_SKIP_38_MAPPING.contains(gene)) {
+                v37Gene = gene;
+                LOGGER.debug("Skipping mapping gene '{}' for transvar", gene);
+            } else if (geneNameMapping.isValidV38Gene(gene)) {
                 v37Gene = geneNameMapping.v37Gene(gene);
             } else {
                 LOGGER.warn("Could not map gene '{}' to v37 for transvar!", gene);
