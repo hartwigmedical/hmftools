7c4d99515911b7e618e32603b3ae17116cedbbe7
Charles Shale <shalecharles@gmail.com>
2021-07-01 19:13:40 +1000

    GeneUtils: write header for gene panel

--- gene-utils/src/main/java/com/hartwig/hmftools/geneutils/ensembl/GenerateGenePanelRefSeq.java
+++ gene-utils/src/main/java/com/hartwig/hmftools/geneutils/ensembl/GenerateGenePanelRefSeq.java
@@ -17,9 +17,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 
 import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
+import com.hartwig.hmftools.common.ensemblcache.EnsemblGeneData;
 import com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion;
 
 import org.apache.commons.cli.CommandLine;
@@ -33,8 +36,9 @@ import org.jooq.Result;
 
 import org.jooq.CSVFormat;
 import org.jooq.tools.StringUtils;
+import org.jooq.types.UInteger;
 
-public class HmfEnsemblResourceBuilder
+public class GenerateGenePanelRefSeq
 {
     private static final String ENSEMBL_DB_URL_37 = "jdbc:mysql://ensembldb.ensembl.org:3337/homo_sapiens_core_89_37";
     private static final String ENSEMBL_DB_URL_38 = "jdbc:mysql://ensembldb.ensembl.org:3306/homo_sapiens_core_89_38";
@@ -49,8 +53,8 @@ public class HmfEnsemblResourceBuilder
 
         RefGenomeVersion refGenomeVersion = RefGenomeVersion.from(cmd.getOptionValue(RefGenomeVersion.REF_GENOME_VERSION));
         String outputDir = parseOutputDir(cmd);
-        String geneTsv = String.format("%sall_genes_%s.tsv", outputDir, refGenomeVersion);
-        String refseqMappingTsv = String.format("%sref_seq_%s.tsv", outputDir, refGenomeVersion);
+        String geneTsv = String.format("%sall_genes.%s.tsv", outputDir, refGenomeVersion.identifier());
+        String refseqMappingTsv = String.format("%sref_seq.%s.tsv", outputDir, refGenomeVersion.identifier());
 
         if(geneTsv == null || refseqMappingTsv == null)
         {
@@ -59,39 +63,98 @@ public class HmfEnsemblResourceBuilder
             System.exit(1);
         }
 
-        GU_LOGGER.info("writing Ensembl version({}) gene panel and ref-seq data files", refGenomeVersion);
+        GU_LOGGER.info("writing Ensembl gene panel and ref-seq data files, ref-genome-version({})", refGenomeVersion);
 
         DSLContext context = createEnsemblDbConnection(cmd);
-        // GU_LOGGER.info(" Connected to {}", database);
 
         generateGenes(context, geneTsv, refGenomeVersion);
         generateRefSeqMapping(context, refseqMappingTsv);
 
-        GU_LOGGER.info("Complete");
+        GU_LOGGER.info("complete");
     }
 
     private static void generateGenes(
             final DSLContext context, final String outputTsv, final RefGenomeVersion refGenomeVersion) throws IOException
     {
-        final Result<Record> geneResult = context.fetch(read(Resources.getResource("sql/ensembl_gene_query.sql")));
-        GU_LOGGER.debug("gene query returned {} entries", geneResult.size());
+        final Result<Record> results = context.fetch(read(Resources.getResource("sql/ensembl_gene_query.sql")));
+        GU_LOGGER.debug("gene query returned {} entries", results.size());
 
-        writeAsTsv(outputTsv, geneResult);
-        GU_LOGGER.info("written gene output to {}", outputTsv);
+        writeAsTsv(outputTsv, results);
+        GU_LOGGER.info("written gene panel output to {}", outputTsv);
 
-        // LOL..
-        if(refGenomeVersion.is38())
-        {
-            final List<String> genes = Files.readAllLines(new File(outputTsv).toPath());
-            final List<String> updatedGenes = Lists.newArrayList();
-
-            // First column is the chromosome, and we need to prefix with "chr" to patch the genes to v38 ref genome
-            for(String gene : genes)
+        /*
+            if(refGenomeVersion.is38())
             {
-                updatedGenes.add("chr" + gene);
+                final List<String> genes = Files.readAllLines(new File(outputTsv).toPath());
+                final List<String> updatedGenes = Lists.newArrayList();
+
+                // First column is the chromosome, and we need to prefix with "chr" to patch the genes to v38 ref genome
+                for(String gene : genes)
+                {
+                    updatedGenes.add("chr" + gene);
+                }
+                Files.write(new File(outputTsv).toPath(), updatedGenes);
             }
-            Files.write(new File(outputTsv).toPath(), updatedGenes);
+         */
+
+        // Chromosome	GeneStart	GeneEnd	GeneId	GeneName	EntrezId	ChrBand
+        // TranscriptId	TranscriptVersion	TranscriptStart	TranscriptEnd
+        // ExonId	ExonStart	ExonEnd	Strand	CodingStart	CodingEnd
+
+        try
+        {
+            BufferedWriter writer = createBufferedWriter(outputTsv, false);
+
+            StringJoiner header = new StringJoiner("\t");
+            header.add("Chromosome").add("GeneStart").add("GeneEnd").add("GeneId").add("GeneName");
+            header.add("EntrezId").add("ChrBand");
+            header.add("TranscriptId").add("TransVersion").add("TransStart").add("TransEnd");
+            header.add("ExonId").add("ExonStart").add("ExonEnd");
+            header.add("Strand").add("CodingStart").add("CodingEnd");
+            writer.write(header.toString());
+            writer.newLine();
+
+            for(final Record record : results)
+            {
+                StringJoiner sj = new StringJoiner("\t");
+                sj.add((String)record.get("chromosome"));
+                sj.add(String.valueOf(record.get("gene_start")));
+                sj.add(String.valueOf(record.get("gene_end")));
+                sj.add((String)record.get("gene_id"));
+                sj.add((String)record.get("gene_name"));
+
+                Object entrezId = record.get("entrezId");
+                sj.add(entrezId != null ? entrezId.toString() : "");
+                sj.add((String)record.get("chromosome_band"));
+
+                sj.add((String)record.get("transcript_id"));
+                sj.add(String.valueOf(record.get("transcript_version")));
+                sj.add(String.valueOf(record.get("transcript_start")));
+                sj.add(String.valueOf(record.get("transcript_end")));
+
+                sj.add((String)record.get("exon_id"));
+                sj.add(String.valueOf(record.get("exon_start")));
+                sj.add(String.valueOf(record.get("exon_end")));
+
+                sj.add(String.valueOf(record.get("strand")));
+
+                Object codingStart = record.get("coding_start");
+                sj.add(codingStart != null ? codingStart.toString() : "");
+
+                Object codingEnd = record.get("coding_end");
+                sj.add(codingEnd != null ? codingEnd.toString() : "");
+
+                writer.write(sj.toString());
+                writer.newLine();
+            }
+
+            writer.close();
         }
+        catch (final IOException e)
+        {
+            GU_LOGGER.error("error writing Ensembl gene data file: {}", e.toString());
+        }
+
     }
 
     private static void generateRefSeqMapping(final DSLContext context, final String outputTsv) throws IOException
