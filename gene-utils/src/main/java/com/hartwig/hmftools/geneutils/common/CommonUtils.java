487af9353835ad4f9c7b23eb8ac6bd72160ef2fe
Charles Shale <shalecharles@gmail.com>
2021-07-01 11:41:48 +1000

    Adding new module for common gene reference data routines

---
+++ gene-utils/src/main/java/com/hartwig/hmftools/geneutils/common/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.hartwig.hmftools.geneutils.common;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CommonUtils
+{
+    public static final Logger GU_LOGGER = LogManager.getLogger(CommonUtils.class);
+
+}
