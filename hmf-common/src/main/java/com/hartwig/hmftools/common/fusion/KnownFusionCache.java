2143991b97766357995a4b261eb3992424dd04df
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-08-20 12:40:15 +0200

    Make file public

--- hmf-common/src/main/java/com/hartwig/hmftools/common/fusion/KnownFusionCache.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/fusion/KnownFusionCache.java
@@ -236,7 +236,7 @@ public class KnownFusionCache
             mHighImpactPromiscuousData.add(data);
     }
 
-    private boolean loadFile(final String filename)
+    public boolean loadFile(final String filename)
     {
         if (!Files.exists(Paths.get(filename)))
         {
