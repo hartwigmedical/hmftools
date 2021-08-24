1523e598ff0b7ad200751537511b5e57ec476dd5
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-08-24 12:39:40 +0200

    CR-212: add system user for report

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReport.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/PatientReport.java
@@ -16,6 +16,10 @@ public interface PatientReport {
             return "Lieke Schoenmaker";
         } else if (systemUser.equals("korneel") || systemUser.equals("korneelduyvesteyn")) {
             return "Korneel Duyvesteyn";
+        } else if (systemUser.equals("sandra") || systemUser.equals("sandravandenbroek")) {
+            return "Sandra van den Broek";
+        } else if (systemUser.equals("root")) {
+            return "trained IT employee and trained clinical molecular biologist (KMBP)";
         } else {
             return systemUser;
         }
