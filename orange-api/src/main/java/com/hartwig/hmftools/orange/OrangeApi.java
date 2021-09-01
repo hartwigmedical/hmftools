329cd2fa95e4cea72dbb2a79a06732c9aff4ad9a
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-09-01 15:47:20 +0200

    DEV-2219: Remove reading the json inside the API

--- orange-api/src/main/java/com/hartwig/hmftools/orange/OrangeApi.java
+++ orange-api/src/main/java/com/hartwig/hmftools/orange/OrangeApi.java
@@ -3,10 +3,6 @@ package com.hartwig.hmftools.orange;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 import com.google.gson.GsonBuilder;
 
@@ -23,24 +19,5 @@ public final class OrangeApi {
 
         writer.write(json);
         writer.close();
-    }
-
-    @NotNull
-    public static OrangeReport read(@NotNull String jsonFile) {
-        String json = readLineByLine(jsonFile);
-        return new GsonBuilder().serializeNulls().serializeSpecialFloatingPointValues().create().fromJson(json, OrangeReport.class);
-    }
-
-    @NotNull
-    private static String readLineByLine(String filePath) {
-        StringBuilder contentBuilder = new StringBuilder();
-
-        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return contentBuilder.toString();
     }
 }
