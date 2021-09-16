a0fed20ec9385d16a93b573bd6df01ec25b766c9
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-09-16 13:36:09 +0200

    Bunch of minor changes to virus interpreter

--- virus-interpreter/src/main/java/com/hartwig/hmftools/virusinterpreter/VirusInterpreterAlgo.java
+++ virus-interpreter/src/main/java/com/hartwig/hmftools/virusinterpreter/VirusInterpreterAlgo.java
@@ -34,10 +34,6 @@ public class VirusInterpreterAlgo {
         this.coveragesAnalysis = coveragesAnalysis;
     }
 
-    public boolean isPurpleQcPass(@NotNull Set<PurpleQCStatus> purpleQCStatus) {
-        return !purpleQCStatus.contains(PurpleQCStatus.FAIL_NO_TUMOR) && !purpleQCStatus.contains(PurpleQCStatus.FAIL_CONTAMINATION);
-    }
-
     @NotNull
     public List<AnnotatedVirus> analyze(@NotNull List<VirusBreakend> virusBreakends, @NotNull PurityContext purityContext) {
         List<AnnotatedVirus> annotatedViruses = Lists.newArrayList();
@@ -53,22 +49,23 @@ public class VirusInterpreterAlgo {
                     .interpretation(interpretation)
                     .percentageCovered(virusBreakend.coverage())
                     .meanCoverage(virusBreakend.meanDepth())
-                    .expectedClonalCoverage(isPurpleQcPass(purityContext.qc().status()) ? coveragesAnalysis.expectedClonalCoverage() : null)
-                    .reported(report(virusBreakend, coveragesAnalysis.expectedClonalCoverage(), purityContext.qc().status())).build());
+                    .expectedClonalCoverage(isPurpleQCPass(purityContext.qc().status()) ? coveragesAnalysis.expectedClonalCoverage() : null)
+                    .reported(report(virusBreakend, coveragesAnalysis.expectedClonalCoverage(), purityContext.qc().status()))
+                    .build());
         }
 
         return annotatedViruses;
     }
 
     @VisibleForTesting
-    public boolean report(@NotNull VirusBreakend virusBreakend, double expectedClonalCoverage, @NotNull Set<PurpleQCStatus> purpleQCStatuses) {
+    boolean report(@NotNull VirusBreakend virusBreakend, double expectedClonalCoverage, @NotNull Set<PurpleQCStatus> purpleQCStatuses) {
         boolean reported = false;
         if (virusReportingDbModel.hasInterpretation(virusBreakend.taxidSpecies())) {
             Integer minimalCoveragePercentage = determineMinimalCoverageVirus(virusBreakend.integrations(), virusBreakend.taxidSpecies());
             double viralPercentageCovered = virusBreakend.coverage();
             double viralCoverage = virusBreakend.meanDepth();
 
-            if (isPurpleQcPass(purpleQCStatuses)) {
+            if (isPurpleQCPass(purpleQCStatuses)) {
                 if (minimalCoveragePercentage != null) {
                     if (viralPercentageCovered > minimalCoveragePercentage && viralCoverage > expectedClonalCoverage) {
                         reported = true;
@@ -88,11 +85,17 @@ public class VirusInterpreterAlgo {
     }
 
     @Nullable
-    public Integer determineMinimalCoverageVirus(int integrations, int taxidSpecies) {
+    @VisibleForTesting
+    Integer determineMinimalCoverageVirus(int integrations, int taxidSpecies) {
         if (integrations >= 1) {
             return virusReportingDbModel.integratedMinimalCoverage(taxidSpecies);
         } else {
             return virusReportingDbModel.nonIntegratedMinimalCoverage(taxidSpecies);
         }
+    }
+
+    @VisibleForTesting
+    static boolean isPurpleQCPass(@NotNull Set<PurpleQCStatus> purpleQCStatus) {
+        return !purpleQCStatus.contains(PurpleQCStatus.FAIL_NO_TUMOR) && !purpleQCStatus.contains(PurpleQCStatus.FAIL_CONTAMINATION);
     }
 }
