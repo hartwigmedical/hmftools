d11606c1a5f2726904e9d15645d7a28c21319ce6
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-10-08 16:41:01 +0200

    DEV-2223: extend baseline table

--- patient-db/src/main/resources/patches/patientdb/patientdb3.62_to_3.63_migration.sql
+++ patient-db/src/main/resources/patches/patientdb/patientdb3.62_to_3.63_migration.sql
@@ -2,3 +2,12 @@ DROP TABLE rna;
 
 ALTER TABLE rnaStatistics
     ADD COLUMN qcStatus varchar(50) NOT NULL AFTER sampleId;
+
+ALTER TABLE baseline
+    ADD COLUMN informedConsentDate varchar(255) AFTER pifVersion;
+
+ALTER TABLE baseline
+    ADD COLUMN pifVersion BOOLEAN AFTER inHMFDatabase;
+
+ALTER TABLE baseline
+    ADD COLUMN inHMFDatabase BOOLEAN AFTER outsideEU;
