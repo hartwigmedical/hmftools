ef0205a7cf37fd4c3b1184a363ada8c81b2e9600
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-10-11 14:28:42 +0200

    DEV-2223: update migration sql queries

--- patient-db/src/main/resources/patches/patientdb/patientdb3.62_to_3.63_migration.sql
+++ patient-db/src/main/resources/patches/patientdb/patientdb3.62_to_3.63_migration.sql
@@ -4,10 +4,10 @@ ALTER TABLE rnaStatistics
     ADD COLUMN qcStatus varchar(50) NOT NULL AFTER sampleId;
 
 ALTER TABLE baseline
-    ADD COLUMN informedConsentDate varchar(255) AFTER pifVersion;
+    ADD COLUMN pifVersion varchar(255) AFTER informedConsentDate;
 
 ALTER TABLE baseline
-    ADD COLUMN pifVersion BOOLEAN AFTER inHMFDatabase;
+    ADD COLUMN inHMFDatabase BOOLEAN AFTER pifVersion;
 
 ALTER TABLE baseline
-    ADD COLUMN inHMFDatabase BOOLEAN AFTER outsideEU;
+    ADD COLUMN outsideEU BOOLEAN AFTER inHMFDatabase;
