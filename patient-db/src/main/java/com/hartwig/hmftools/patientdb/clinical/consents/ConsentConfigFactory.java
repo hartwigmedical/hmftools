7f6f2e7c2671158f43e65865b5dd4d0523007bae
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-10-11 14:43:30 +0200

    DEV-2223: refactor

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/clinical/consents/ConsentConfigFactory.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/clinical/consents/ConsentConfigFactory.java
@@ -3,7 +3,6 @@ package com.hartwig.hmftools.patientdb.clinical.consents;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
