0c8ef8a0f11a0cb1bb7ed5ee55b0d4c087d81aa0
Charles Shale <shalecharles@gmail.com>
2021-10-12 18:35:36 +1100

    Purple: can function again without SV VCFs

--- purple/src/main/java/com/hartwig/hmftools/purple/config/SampleDataFiles.java
+++ purple/src/main/java/com/hartwig/hmftools/purple/config/SampleDataFiles.java
@@ -60,7 +60,7 @@ public class SampleDataFiles
             CobaltDirectory = cmd.getOptionValue(COBALT);
         }
 
-        SvVcfFile = getFilename(cmd, STRUCTURAL_VARIANTS, SampleDataDir, sampleId, ".gripss.somatic.filtered.vcf.gz", true);
+        SvVcfFile = getFilename(cmd, STRUCTURAL_VARIANTS, SampleDataDir, sampleId, ".gripss.somatic.filtered.vcf.gz", false);
         RecoveredSvVcfFile = getFilename(cmd, STRUCTURAL_VARIANT_RECOVERY, SampleDataDir, sampleId, ".gripss.somatic.vcf.gz", false);
         SomaticVcfFile = getFilename(cmd, SOMATIC_VARIANTS, SampleDataDir, sampleId, ".sage.somatic.filtered.vcf.gz", false);
         GermlineVcfFile = getFilename(cmd, GERMLINE_VARIANTS, SampleDataDir, sampleId, ".sage.germline.filtered.vcf.gz", false);
