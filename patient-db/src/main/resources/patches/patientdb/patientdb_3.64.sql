7ecc6e0288562eb0f9b621a3bccd196dd1d57581
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-12-15 17:33:41 +0100

    Fix issue and remove duplications

--- patient-db/src/main/resources/patches/patientdb/patientdb_3.64.sql
+++ patient-db/src/main/resources/patches/patientdb/patientdb_3.64.sql
@@ -1,43 +1,4 @@
 ####
-# RNA and Isofox
-- rna table no longer used, replaced by rnaStatistics
-
-DROP TABLE rna;
-
-ALTER TABLE rnaStatistics
-    ADD COLUMN qcStatus varchar(50) NOT NULL AFTER sampleId;
-
-####
-# Baseline changes
-ALTER TABLE baseline
-    ADD COLUMN pifVersion varchar(255) AFTER informedConsentDate;
-
-ALTER TABLE baseline
-    ADD COLUMN inHMFDatabase BOOLEAN AFTER pifVersion;
-
-ALTER TABLE baseline
-    ADD COLUMN outsideEU BOOLEAN AFTER inHMFDatabase;
-
-####
-# Added unique composite keys to some tables to avoid duplications
-ALTER TABLE copyNumber
-	ADD UNIQUE KEY (sampleId, chromosome, start, end);
-
-ALTER TABLE geneCopyNumber
-	MODIFY COLUMN sampleId VARCHAR(31);
-ALTER TABLE geneCopyNumber
-	MODIFY COLUMN geneCopyNumber VARCHAR(31);
-ALTER TABLE geneCopyNumber
-	ADD UNIQUE KEY (sampleId, chromosome, gene, transcriptId);
-
-ALTER TABLE somaticVariant
-	MODIFY COLUMN sampleId VARCHAR(31);
-ALTER TABLE somaticVariant
-	MODIFY COLUMN chromosome VARCHAR(31);
-ALTER TABLE somaticVariant
-	ADD UNIQUE KEY (sampleId, chromosome, position, ref, alt);
-
-####
 # Linx v1.17
 # - removal of old viral insertion table
 # - remove unused replication timing fields
