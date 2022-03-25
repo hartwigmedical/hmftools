ed5bde732e02148015e552208a6eb6191a656170
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-03-25 16:11:47 +0100

    sort on tree

--- ckb-importer/src/main/resources/database/patches/ckb_importer-1.4.sql
+++ ckb-importer/src/main/resources/database/patches/ckb_importer-1.4.sql
@@ -1,14 +1,5 @@
 ## Adding relevant drug classes of therapies
 
-DROP TABLE IF EXISTS treatmentApproachEvidence;
-CREATE TABLE treatmentApproachEvidence
-(   evidenceId int NOT NULL,
-    treatmentApproachEvidenceId int NOT NULL,
-    PRIMARY KEY (evidenceId, treatmentApproachEvidenceId),
-    FOREIGN KEY (evidenceId) REFERENCES evidence(id),
-    FOREIGN KEY (treatmentApproachEvidenceId) REFERENCES treatmentApproach(id)
-);
-
 DROP TABLE IF EXISTS treatmentApproach;
 CREATE TABLE treatmentApproach
 (   id int NOT NULL AUTO_INCREMENT,
@@ -16,6 +7,15 @@ CREATE TABLE treatmentApproach
     createDate DATE NOT NULL,
     updateDate DATE,
     PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS treatmentApproachEvidence;
+CREATE TABLE treatmentApproachEvidence
+(   evidenceId int NOT NULL,
+    treatmentApproachEvidenceId int NOT NULL,
+    PRIMARY KEY (evidenceId, treatmentApproachEvidenceId),
+    FOREIGN KEY (evidenceId) REFERENCES evidence(id),
+    FOREIGN KEY (treatmentApproachEvidenceId) REFERENCES treatmentApproach(id)
 );
 
 DROP TABLE IF EXISTS treatmentApproachDrugClass;
