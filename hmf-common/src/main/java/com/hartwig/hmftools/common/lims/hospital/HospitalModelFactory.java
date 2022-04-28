14a61f45dbc0b5ef6be5ff64a99b950c72b3a4d2
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-04-28 14:07:57 +0200

    DEV-2605: requester name

--- hmf-common/src/main/java/com/hartwig/hmftools/common/lims/hospital/HospitalModelFactory.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/lims/hospital/HospitalModelFactory.java
@@ -164,7 +164,7 @@ public final class HospitalModelFactory {
                 String requesterEmail = parts.length > 2 ? parts[HOSPITAL_PERSONS_REQUESTER_EMAIL_COLUMN] : null;
 
                 HospitalPersons hospitalPersons = ImmutableHospitalPersons.builder()
-                        .hospitalPI(cohort.startsWith("COREDB") || cohort.startsWith("GENAYA")
+                        .hospitalPI(requesterName != null
                                 ? requesterName
                                 : parts[HOSPITAL_PERSONS_PI_COLUMN])
                         .requesterName(requesterName)
