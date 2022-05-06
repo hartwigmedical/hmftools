fa4c11c3fe3d536bd18b96266fc9431c297b1c9c
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-05-06 15:17:12 +0200

    fix bug of evidence extraction

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/actionability/ClinicalTrialFactory.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/actionability/ClinicalTrialFactory.java
@@ -1,8 +1,11 @@
 package com.hartwig.hmftools.patientreporter.actionability;
 
 import java.util.List;
+import java.util.Set;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.hartwig.hmftools.common.protect.ImmutableProtectEvidence;
 import com.hartwig.hmftools.common.protect.ProtectEvidence;
 import com.hartwig.hmftools.common.protect.ProtectSource;
 import com.hartwig.hmftools.common.serve.Knowledgebase;
@@ -18,10 +21,15 @@ public final class ClinicalTrialFactory {
     public static List<ProtectEvidence> extractOnLabelTrials(@NotNull List<ProtectEvidence> evidenceItems) {
         List<ProtectEvidence> trials = Lists.newArrayList();
         for (ProtectEvidence evidence : evidenceItems) {
+            Set<ProtectSource> protectSources = Sets.newHashSet();
             for (ProtectSource protectSource: evidence.protectSources()) {
                 if (protectSource.source() == Knowledgebase.ICLUSION && evidence.onLabel()) {
-                    trials.add(evidence);
+                    protectSources.add(protectSource);
                 }
+            }
+
+            if (protectSources.size() >= 1) {
+                trials.add(ImmutableProtectEvidence.builder().from(evidence).protectSources(protectSources).build());
             }
 
         }
