4b24f5aa569039e205eaa39b1efaeaa52ba67bbf
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-05-17 17:10:53 +0200

    Start with adding summon tools

---
+++ summon/src/test/java/com/hartwig/hmftools/summon/actionability/ActionabilityFileReaderTest.java
@@ -0,0 +1,7 @@
+package com.hartwig.hmftools.summon.actionability;
+
+import junit.framework.TestCase;
+
+public class ActionabilityFileReaderTest extends TestCase {
+
+}
---
+++ summon/src/test/java/com/hartwig/hmftools/summon/conclusion/ConclusionAlgoTest.java
@@ -0,0 +1,7 @@
+package com.hartwig.hmftools.summon.conclusion;
+
+import junit.framework.TestCase;
+
+public class ConclusionAlgoTest extends TestCase {
+
+}
