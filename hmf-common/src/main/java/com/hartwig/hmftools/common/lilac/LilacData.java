d7e3b0c96261af5e9ec2b01c44bfa3b79b95ae4b
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-06-02 13:44:06 +0200

    Add datamodel for LILAC ingestion

---
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/lilac/LilacData.java
@@ -0,0 +1,18 @@
+package com.hartwig.hmftools.common.lilac;
+
+import java.util.List;
+
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public abstract class LilacData {
+
+    @NotNull
+    public abstract String qc();
+
+    @NotNull
+    public abstract List<LilacRecord> records();
+}
