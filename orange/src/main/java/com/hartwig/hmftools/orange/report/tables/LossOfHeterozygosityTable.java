95ec91eb13d0cf7637a3776ab585247d4e4b1d47
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-06-02 11:46:25 +0200

    Fix sorting and add MACN to LOH table

--- orange/src/main/java/com/hartwig/hmftools/orange/report/tables/LossOfHeterozygosityTable.java
+++ orange/src/main/java/com/hartwig/hmftools/orange/report/tables/LossOfHeterozygosityTable.java
@@ -1,11 +1,10 @@
 package com.hartwig.hmftools.orange.report.tables;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.hartwig.hmftools.common.genome.region.TranscriptRegion;
 import com.hartwig.hmftools.common.purple.gene.GeneCopyNumber;
+import com.hartwig.hmftools.orange.report.interpretation.Chromosomes;
 import com.hartwig.hmftools.orange.report.util.Cells;
 import com.hartwig.hmftools.orange.report.util.Tables;
 import com.itextpdf.layout.element.Cell;
@@ -26,22 +25,36 @@ public final class LossOfHeterozygosityTable {
         }
 
         Table table = Tables.createContent(width,
-                new float[] { 1, 1, 1, 3 },
-                new Cell[] { Cells.createHeader("Location"), Cells.createHeader("Gene"), Cells.createHeader("Tumor CN"),
-                        Cells.createHeader(Strings.EMPTY) });
+                new float[] { 1, 1, 1, 1, 3 },
+                new Cell[] { Cells.createHeader("Location"), Cells.createHeader("Gene"), Cells.createHeader("Tumor MACN"),
+                        Cells.createHeader("Tumor CN"), Cells.createHeader(Strings.EMPTY) });
 
         for (GeneCopyNumber lohGene : sort(lohGenes)) {
             table.addCell(Cells.createContent(lohGene.chromosome() + lohGene.chromosomeBand()));
             table.addCell(Cells.createContent(lohGene.geneName()));
-            table.addCell(Cells.createContent(String.valueOf(Math.round(Math.max(0, lohGene.minCopyNumber())))));
+            table.addCell(Cells.createContent(String.valueOf(round(lohGene.minMinorAlleleCopyNumber()))));
+            table.addCell(Cells.createContent(String.valueOf(round(lohGene.minCopyNumber()))));
             table.addCell(Cells.createContent(Strings.EMPTY));
         }
 
         return Tables.createWrapping(table, title);
     }
 
+    private static long round(double copyNumber) {
+        return Math.round(Math.max(0, copyNumber));
+    }
+
     @NotNull
     private static List<GeneCopyNumber> sort(@NotNull List<GeneCopyNumber> lohGenes) {
-        return lohGenes.stream().sorted(Comparator.comparing(TranscriptRegion::geneName)).collect(Collectors.toList());
+        return lohGenes.stream().sorted((geneCopyNumber1, geneCopyNumber2) -> {
+            String location1 = Chromosomes.zeroPrefixed(geneCopyNumber1.chromosome() + geneCopyNumber1.chromosomeBand());
+            String location2 = Chromosomes.zeroPrefixed(geneCopyNumber2.chromosome() + geneCopyNumber2.chromosomeBand());
+
+            if (location1.equals(location2)) {
+                return geneCopyNumber1.geneName().compareTo(geneCopyNumber2.geneName());
+            } else {
+                return location1.compareTo(location2);
+            }
+        }).collect(Collectors.toList());
     }
 }
