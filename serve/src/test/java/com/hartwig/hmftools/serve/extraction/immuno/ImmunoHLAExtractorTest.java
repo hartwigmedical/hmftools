257259c8cccc9f0aa95c970385a1fd72cce9ddc1
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-06-13 17:00:59 +0200

    Add warn message when HLA type is incorrect length

--- serve/src/test/java/com/hartwig/hmftools/serve/extraction/immuno/ImmunoHLAExtractorTest.java
+++ serve/src/test/java/com/hartwig/hmftools/serve/extraction/immuno/ImmunoHLAExtractorTest.java
@@ -11,8 +11,14 @@ public class ImmunoHLAExtractorTest {
     @Test
     public void canExtractImmunoHLA() {
         ImmunoHLAExtractor immunoHLAExtractor = new ImmunoHLAExtractor();
-        ImmunoHLA immunohLA = immunoHLAExtractor.extract(EventType.IMMUNO_HLA, "A*20");
 
-        assertEquals("A*20", immunohLA.immunoHLA());
+        ImmunoHLA immunohLACorrect = immunoHLAExtractor.extract(EventType.IMMUNO_HLA, "A*20");
+        assertEquals("A*20", immunohLACorrect.immunoHLA());
+
+        ImmunoHLA immunohLALess = immunoHLAExtractor.extract(EventType.IMMUNO_HLA, "A*2");
+        assertEquals("A*2", immunohLALess.immunoHLA());
+
+        ImmunoHLA immunohLAMore = immunoHLAExtractor.extract(EventType.IMMUNO_HLA, "HLA-A*021:054");
+        assertEquals("HLA-A*021:054", immunohLAMore.immunoHLA());
     }
 }
