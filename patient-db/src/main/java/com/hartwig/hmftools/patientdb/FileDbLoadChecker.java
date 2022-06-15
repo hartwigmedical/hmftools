88ce232ff6954c4d1e5b4100e37097da7ebc215f
Charles Shale <shalecharles@gmail.com>
2022-06-16 06:12:20 +1000

    File-DB load checker (FDLC): multi-thread, check all purple files

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/FileDbLoadChecker.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/FileDbLoadChecker.java
@@ -1,6 +1,10 @@
 package com.hartwig.hmftools.patientdb;
 
+import static java.lang.Math.min;
+
+import static com.hartwig.hmftools.common.drivercatalog.DriverType.DRIVERS_LINX_SOMATIC;
 import static com.hartwig.hmftools.common.purple.PurpleCommon.PURPLE_SOMATIC_VCF_SUFFIX;
+import static com.hartwig.hmftools.common.purple.PurpleCommon.PURPLE_SV_VCF_SUFFIX;
 import static com.hartwig.hmftools.common.utils.ConfigUtils.setLogLevel;
 import static com.hartwig.hmftools.common.utils.FileWriterUtils.checkAddDirSeparator;
 import static com.hartwig.hmftools.common.utils.FileWriterUtils.closeBufferedWriter;
@@ -15,10 +19,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.drivercatalog.DriverCatalog;
 import com.hartwig.hmftools.common.drivercatalog.DriverCatalogFile;
+import com.hartwig.hmftools.common.purple.copynumber.PurpleCopyNumber;
+import com.hartwig.hmftools.common.purple.copynumber.PurpleCopyNumberFile;
+import com.hartwig.hmftools.common.purple.gene.GeneCopyNumber;
+import com.hartwig.hmftools.common.purple.gene.GeneCopyNumberFile;
+import com.hartwig.hmftools.common.purple.purity.FittedPurity;
+import com.hartwig.hmftools.common.purple.purity.FittedPurityRangeFile;
+import com.hartwig.hmftools.common.sv.StructuralVariant;
+import com.hartwig.hmftools.common.sv.StructuralVariantFileLoader;
 import com.hartwig.hmftools.common.sv.linx.LinxBreakend;
 import com.hartwig.hmftools.common.sv.linx.LinxCluster;
 import com.hartwig.hmftools.common.sv.linx.LinxDriver;
@@ -27,8 +41,10 @@ import com.hartwig.hmftools.common.sv.linx.LinxLink;
 import com.hartwig.hmftools.common.sv.linx.LinxSvAnnotation;
 import com.hartwig.hmftools.common.utils.ConfigUtils;
 import com.hartwig.hmftools.common.utils.FileWriterUtils;
+import com.hartwig.hmftools.common.utils.TaskExecutor;
 import com.hartwig.hmftools.common.variant.SomaticVariant;
 import com.hartwig.hmftools.common.variant.SomaticVariantFactory;
+import com.hartwig.hmftools.common.variant.filter.AlwaysPassFilter;
 import com.hartwig.hmftools.patientdb.dao.DatabaseAccess;
 
 import org.apache.commons.cli.CommandLine;
@@ -48,11 +64,13 @@ public class FileDbLoadChecker
     private final String mLinxDir;
     private final List<String> mSampleIds;
     private final DatabaseAccess mDbAccess;
+    private final int mThreads;
 
     private static final String SAMPLE = "sample";
     private static final String SAMPLE_ID_FILE = "sample_id_file";
     private static final String PURPLE_DIR = "purple_dir";
     private static final String LINX_DIR = "linx_dir";
+    private static final String THREADS = "threads";
 
     private static final Logger LOGGER = LogManager.getLogger(LoadPurpleSomaticVariants.class);
 
@@ -77,6 +95,7 @@ public class FileDbLoadChecker
 
         mPurpleDir = cmd.hasOption(PURPLE_DIR) ? checkAddDirSeparator(cmd.getOptionValue(PURPLE_DIR)) : null;
         mLinxDir = cmd.hasOption(LINX_DIR) ? checkAddDirSeparator(cmd.getOptionValue(LINX_DIR)) : null;
+        mThreads = Integer.parseInt(cmd.getOptionValue(THREADS, "1"));
     }
 
     public void run()
@@ -92,130 +111,30 @@ public class FileDbLoadChecker
 
         LOGGER.info("processing {} samples", mSampleIds.size());
 
-        int processed = 0;
+        List<SampleCheckerTask> sampleTasks = Lists.newArrayList();
+
+        for(int i = 0; i < min(mSampleIds.size(), mThreads); ++i)
+        {
+            sampleTasks.add(new SampleCheckerTask(i));
+        }
+
+        int taskIndex = 0;
         for(String sampleId : mSampleIds)
         {
-            LOGGER.debug("processing sample({})", sampleId);
+            if(taskIndex >= sampleTasks.size())
+                taskIndex = 0;
 
-            ++processed;
+            sampleTasks.get(taskIndex).getSampleIds().add(sampleId);
 
-            if(mPurpleDir != null)
-            {
-                String purpleDir = mPurpleDir.replaceAll("\\*", sampleId);
-                checkPurpleFiles(sampleId, purpleDir);
-            }
-
-            if(mLinxDir != null)
-            {
-                String linxDir = mLinxDir.replaceAll("\\*", sampleId);
-                checkLinxFiles(sampleId, linxDir);
-            }
-
-            if(processed > 0 && (processed % 100) == 0)
-            {
-                LOGGER.info("processed {} samples", processed);
-            }
+            ++taskIndex;
         }
+
+        final List<Callable> callableList = sampleTasks.stream().collect(Collectors.toList());
+        TaskExecutor.executeTasks(callableList, mThreads);
 
         closeBufferedWriter(mWriter);
 
         LOGGER.info("File DB loading check complete");
-    }
-
-    private void checkPurpleFiles(final String sampleId, final String purpleDir)
-    {
-        if(!Files.exists(Paths.get(purpleDir)))
-        {
-            LOGGER.error("invalid Purple data directory({})", purpleDir);
-            return;
-        }
-
-        final String somaticVcf = purpleDir + sampleId + PURPLE_SOMATIC_VCF_SUFFIX;
-
-        if(!Files.exists(Paths.get(somaticVcf)))
-        {
-            LOGGER.warn("somatic VCF({}) missing");
-            return;
-        }
-
-        SomaticVariantFactory somaticVariantFactory = new SomaticVariantFactory();
-
-        try
-        {
-            List<SomaticVariant> somaticVariants = somaticVariantFactory.fromVCFFile(sampleId, somaticVcf);
-
-            checkFileVsDatabase(sampleId, "SomaticVariant", somaticVariants.size(), "somaticVariant");
-        }
-        catch(Exception e)
-        {
-            LOGGER.error("failed to read sample({}) file: {}", sampleId, e.toString());
-        }
-    }
-
-    private void checkLinxFiles(final String sampleId, final String linxDir)
-    {
-        if(!Files.exists(Paths.get(linxDir)))
-        {
-            LOGGER.error("invalid Linx data directory({})", linxDir);
-            return;
-        }
-
-        try
-        {
-            final String svAnnotationFile = LinxSvAnnotation.generateFilename(linxDir, sampleId);
-            final String svClusterFile = LinxCluster.generateFilename(linxDir, sampleId);
-            final String svLinkFile = LinxLink.generateFilename(linxDir, sampleId);
-            final String svBreakendFile = LinxBreakend.generateFilename(linxDir, sampleId);
-            final String svFusionFile = LinxFusion.generateFilename(linxDir, sampleId);
-            final String svDriverFile = LinxDriver.generateFilename(linxDir, sampleId);
-            final String driverCatalogFile = LinxDriver.generateCatalogFilename(linxDir, sampleId, true);
-
-            List<String> requiredFiles = Lists.newArrayList(
-                    svAnnotationFile, svClusterFile, svLinkFile, svBreakendFile, svFusionFile, svDriverFile, driverCatalogFile);
-
-            if(requiredFiles.stream().noneMatch(x -> Files.exists(Paths.get(x))))
-            {
-                LOGGER.info("skipping somatic data - no files present");
-                return;
-            }
-
-            if(hasMissingFiles(requiredFiles, "somatic"))
-                System.exit(1);
-
-            List<LinxSvAnnotation> svAnnotations = LinxSvAnnotation.read(svAnnotationFile);
-            List<LinxCluster> clusters = LinxCluster.read(svClusterFile);
-            List<LinxLink> links = LinxLink.read(svLinkFile);
-            List<LinxBreakend> breakends = LinxBreakend.read(svBreakendFile);
-            List<LinxFusion> fusions = LinxFusion.read(svFusionFile);
-            List<LinxDriver> drivers = LinxDriver.read(svDriverFile);
-            List<DriverCatalog> driverCatalog = DriverCatalogFile.read(driverCatalogFile);
-
-            checkFileVsDatabase(sampleId, "SvAnnotation", svAnnotations.size(), "svAnnotation");
-            checkFileVsDatabase(sampleId, "SvBreakend", breakends.size(), "svBreakend");
-            checkFileVsDatabase(sampleId, "SvFusion", fusions.size(), "svFusion");
-            checkFileVsDatabase(sampleId, "DriverCatalog", driverCatalog.size(), "driverCatalog");
-            checkFileVsDatabase(sampleId, "SvDriver", drivers.size(), "svDriver");
-            checkFileVsDatabase(sampleId, "SvCluster", clusters.size(), "svCluster");
-            checkFileVsDatabase(sampleId, "SvLinx", links.size(), "svLink");
-        }
-        catch(Exception e)
-        {
-            LOGGER.error("failed to read sample({}) file: {}", sampleId, e.toString());
-        }
-    }
-
-    private void checkFileVsDatabase(final String sampleId, final String fileType, int fileCount, final String tableName)
-    {
-        int dbCount = getDbVariantCount(sampleId, mDbAccess, tableName);
-
-        if(dbCount == fileCount)
-        {
-            LOGGER.trace("sample({}) table({}) count({}) match", sampleId, tableName, fileCount);
-            return;
-        }
-
-        LOGGER.trace("sample({}) table({}) file({}) DB({}) mismatch", sampleId, tableName, fileCount, dbCount);
-        writeDiff(sampleId, fileType, fileCount, dbCount);
     }
 
     private BufferedWriter initialiseOutputFile(final String outputDir)
@@ -240,7 +159,7 @@ public class FileDbLoadChecker
         }
     }
 
-    private void writeDiff(final String sampleId, final String fileType, int fileCount, int dbCount)
+    private synchronized void writeDiff(final String sampleId, final String fileType, int fileCount, int dbCount)
     {
         if(mWriter == null)
             return;
@@ -257,23 +176,191 @@ public class FileDbLoadChecker
         }
     }
 
-    private static int getDbVariantCount(final String sampleId, final DatabaseAccess dbAccess, final String tableName)
+    public class SampleCheckerTask implements Callable
     {
-        // String queryStr = String.format("select count(*) from %s where sampleId = '%s'", tableName, sampleId);
-        // dbAccess.context().query(queryStr).execute();
+        private final int mTaskId;
+        private final List<String> mSampleIds;
 
-        Result<Record1<Integer>> result = dbAccess.context()
-                .selectCount()
-                .from(tableName)
-                .where(String.format("sampleId = '%s'", sampleId))
-                .fetch();
-
-        for(Record record : result)
+        public SampleCheckerTask(int taskId)
         {
-            return Integer.parseInt(record.getValue(0).toString());
+            mTaskId = taskId;
+            mSampleIds = Lists.newArrayList();
         }
 
-        return 0;
+        public List<String> getSampleIds() { return mSampleIds; }
+
+        @Override
+        public Long call()
+        {
+            for(int i = 0; i < mSampleIds.size(); ++i)
+            {
+                String sampleId = mSampleIds.get(i);
+
+                processSample(sampleId);
+
+                if(i > 0 && (i % 100) == 0)
+                {
+                    LOGGER.info("{}: processed {} samples", mTaskId, i);
+                }
+            }
+
+            LOGGER.info("{}: tasks complete for {} samples", mTaskId, mSampleIds.size());
+            return (long)0;
+        }
+
+        private void processSample(final String sampleId)
+        {
+            LOGGER.debug("processing sample({})", sampleId);
+
+            if(mPurpleDir != null)
+            {
+                String purpleDir = mPurpleDir.replaceAll("\\*", sampleId);
+                checkPurpleFiles(sampleId, purpleDir);
+            }
+
+            if(mLinxDir != null)
+            {
+                String linxDir = mLinxDir.replaceAll("\\*", sampleId);
+                checkLinxFiles(sampleId, linxDir);
+            }
+        }
+
+        private void checkPurpleFiles(final String sampleId, final String purpleDir)
+        {
+            if(!Files.exists(Paths.get(purpleDir)))
+            {
+                LOGGER.error("invalid Purple data directory({})", purpleDir);
+                return;
+            }
+
+            final String somaticVcf = purpleDir + sampleId + PURPLE_SOMATIC_VCF_SUFFIX;
+
+            if(!Files.exists(Paths.get(somaticVcf)))
+            {
+                LOGGER.warn("somatic VCF({}) missing");
+                return;
+            }
+
+            SomaticVariantFactory somaticVariantFactory = new SomaticVariantFactory();
+
+            try
+            {
+                List<SomaticVariant> somaticVariants = somaticVariantFactory.fromVCFFile(sampleId, somaticVcf);
+                checkFileVsDatabase(sampleId, "SomaticVariant", somaticVariants.size(), "somaticVariant");
+
+                final String geneCopyNumberFile = GeneCopyNumberFile.generateFilenameForReading(purpleDir, sampleId);
+                final String copyNumberFile = PurpleCopyNumberFile.generateFilenameForReading(purpleDir, sampleId);
+                final String svVcf = purpleDir + sampleId + PURPLE_SV_VCF_SUFFIX;
+
+                List<GeneCopyNumber> geneCopyNumbers = GeneCopyNumberFile.read(geneCopyNumberFile);
+                List<PurpleCopyNumber> copyNumbers = PurpleCopyNumberFile.read(copyNumberFile);
+                List<StructuralVariant> svs = StructuralVariantFileLoader.fromFile(svVcf, new AlwaysPassFilter());
+
+                checkFileVsDatabase(sampleId, "GeneCopyNumber", geneCopyNumbers.size(), "geneCopyNumber");
+                checkFileVsDatabase(sampleId, "CopyNumber", copyNumbers.size(), "copyNumber");
+                checkFileVsDatabase(sampleId, "StructuralVariant", svs.size(), "structuralVariant");
+            }
+            catch(Exception e)
+            {
+                LOGGER.error("failed to read sample({}) file: {}", sampleId, e.toString());
+            }
+        }
+
+        private void checkLinxFiles(final String sampleId, final String linxDir)
+        {
+            if(!Files.exists(Paths.get(linxDir)))
+            {
+                LOGGER.error("invalid Linx data directory({})", linxDir);
+                return;
+            }
+
+            try
+            {
+                final String svAnnotationFile = LinxSvAnnotation.generateFilename(linxDir, sampleId);
+                final String svClusterFile = LinxCluster.generateFilename(linxDir, sampleId);
+                final String svLinkFile = LinxLink.generateFilename(linxDir, sampleId);
+                final String svBreakendFile = LinxBreakend.generateFilename(linxDir, sampleId);
+                final String svFusionFile = LinxFusion.generateFilename(linxDir, sampleId);
+                final String svDriverFile = LinxDriver.generateFilename(linxDir, sampleId);
+                final String driverCatalogFile = LinxDriver.generateCatalogFilename(linxDir, sampleId, true);
+
+                List<String> requiredFiles = Lists.newArrayList(
+                        svAnnotationFile, svClusterFile, svLinkFile, svBreakendFile, svFusionFile, svDriverFile, driverCatalogFile);
+
+                if(requiredFiles.stream().noneMatch(x -> Files.exists(Paths.get(x))))
+                {
+                    LOGGER.info("skipping somatic data - no files present");
+                    return;
+                }
+
+                if(hasMissingFiles(requiredFiles, "somatic"))
+                    System.exit(1);
+
+                List<LinxSvAnnotation> svAnnotations = LinxSvAnnotation.read(svAnnotationFile);
+                List<LinxCluster> clusters = LinxCluster.read(svClusterFile);
+                List<LinxLink> links = LinxLink.read(svLinkFile);
+                List<LinxBreakend> breakends = LinxBreakend.read(svBreakendFile);
+                List<LinxFusion> fusions = LinxFusion.read(svFusionFile);
+                List<LinxDriver> drivers = LinxDriver.read(svDriverFile);
+
+                checkFileVsDatabase(sampleId, "SvAnnotation", svAnnotations.size(), "svAnnotation");
+                checkFileVsDatabase(sampleId, "SvBreakend", breakends.size(), "svBreakend");
+                checkFileVsDatabase(sampleId, "SvFusion", fusions.size(), "svFusion");
+                checkFileVsDatabase(sampleId, "SvDriver", drivers.size(), "svDriver");
+                checkFileVsDatabase(sampleId, "SvCluster", clusters.size(), "svCluster");
+                checkFileVsDatabase(sampleId, "SvLink", links.size(), "svLink");
+
+                List<DriverCatalog> driverCatalog = DriverCatalogFile.read(driverCatalogFile);
+                List<DriverCatalog> dbDrivers = mDbAccess.readDriverCatalog(sampleId);
+
+                int fileDriverCount = (int)driverCatalog.stream().filter(x -> DRIVERS_LINX_SOMATIC.contains(x.driver())).count();
+                int dbDriverCount = (int)dbDrivers.stream().filter(x -> DRIVERS_LINX_SOMATIC.contains(x.driver())).count();
+
+                if(dbDriverCount != fileDriverCount)
+                {
+                    LOGGER.trace("sample({}) driverCatalog file({}) DB({}) mismatch", sampleId, fileDriverCount, dbDriverCount);
+                    writeDiff(sampleId, "DriverCatalog", fileDriverCount, dbDriverCount);
+                }
+                else
+                {
+                    LOGGER.trace("sample({}) driverCatalog({}) count({}) match", sampleId, fileDriverCount);
+                }
+            }
+            catch(Exception e)
+            {
+                LOGGER.error("failed to read sample({}) file: {}", sampleId, e.toString());
+            }
+        }
+
+        private void checkFileVsDatabase(final String sampleId, final String fileType, int fileCount, final String tableName)
+        {
+            int dbCount = getDbVariantCount(sampleId, mDbAccess, tableName);
+
+            if(dbCount == fileCount)
+            {
+                LOGGER.trace("sample({}) table({}) count({}) match", sampleId, tableName, fileCount);
+                return;
+            }
+
+            LOGGER.trace("sample({}) table({}) file({}) DB({}) mismatch", sampleId, tableName, fileCount, dbCount);
+            writeDiff(sampleId, fileType, fileCount, dbCount);
+        }
+
+        private int getDbVariantCount(final String sampleId, final DatabaseAccess dbAccess, final String tableName)
+        {
+            Result<Record1<Integer>> result = dbAccess.context()
+                    .selectCount()
+                    .from(tableName)
+                    .where(String.format("sampleId = '%s'", sampleId))
+                    .fetch();
+
+            for(Record record : result)
+            {
+                return Integer.parseInt(record.getValue(0).toString());
+            }
+
+            return 0;
+        }
     }
 
     public static void main(@NotNull String[] args)
@@ -305,6 +392,7 @@ public class FileDbLoadChecker
         options.addOption(SAMPLE_ID_FILE, true, "CSV with SampleId");
         options.addOption(PURPLE_DIR, true, "Sample Purple directory");
         options.addOption(LINX_DIR, true, "Sample Linx directory");
+        options.addOption(THREADS, true, "Thread count");
         addDatabaseCmdLineArgs(options);
         ConfigUtils.addLoggingOptions(options);
         FileWriterUtils.addOutputOptions(options);
