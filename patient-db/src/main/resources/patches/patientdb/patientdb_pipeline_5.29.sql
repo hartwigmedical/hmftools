7265cc2e62f84973affac4fb7e957fc3e5539930
Charles Shale <shalecharles@gmail.com>
2022-07-09 06:22:47 +1000

    Patient-DB: removed Purple update to next pipeline release (5.30)

--- patient-db/src/main/resources/patches/patientdb/patientdb_pipeline_5.29.sql
+++ patient-db/src/main/resources/patches/patientdb/patientdb_pipeline_5.29.sql
@@ -2,11 +2,6 @@
 # SQL updates for Pipeline release 5.29
 # NOTE: only add updates to this script if the tools impacted by them will be released with this pipeline release
 
-# Purple
-
-ALTER TABLE germlineDeletion
-    ADD COLUMN chromosomeBand VARCHAR(20) NOT NULL after chromosome;
-
 # Protect
 
 ALTER TABLE protect
