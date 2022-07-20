e30ce6429f45f4249656b9326d184067a50f61fd
Charles Shale <shalecharles@gmail.com>
2022-07-20 14:09:52 +1000

    Cuppa: added missing file

---
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/cuppa/DataTypes.java
@@ -0,0 +1,8 @@
+package com.hartwig.hmftools.common.cuppa;
+
+public final class DataTypes
+{
+    public static final String DATA_TYPE_DNA_COMBINED = "DNA_COMBINED";
+    public static final String DATA_TYPE_RNA_COMBINED = "RNA_COMBINED";
+    public static final String DATA_TYPE_COMBINED = "COMBINED";
+}
