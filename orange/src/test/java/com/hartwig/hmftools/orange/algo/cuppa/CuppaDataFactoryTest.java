be231bb2dfe40f80eed7ccd8cb3119727e6cc04a
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-07-26 11:36:32 +0200

    Add cuppa data factory test on test file

--- orange/src/test/java/com/hartwig/hmftools/orange/algo/cuppa/CuppaDataFactoryTest.java
+++ orange/src/test/java/com/hartwig/hmftools/orange/algo/cuppa/CuppaDataFactoryTest.java
@@ -5,10 +5,13 @@ import static com.hartwig.hmftools.common.cuppa.CategoryType.COMBINED;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.io.Resources;
 import com.hartwig.hmftools.common.cuppa.CuppaDataFile;
 import com.hartwig.hmftools.common.cuppa.DataTypes;
 import com.hartwig.hmftools.common.cuppa.ResultType;
@@ -18,6 +21,36 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 public class CuppaDataFactoryTest {
+
+    private static final String CUPPA_TEST_CSV = Resources.getResource("test_run/cuppa/tumor_sample.cup.data.csv").getPath();
+
+    private static final double EPSILON = 1.0E-10;
+
+    @Test
+    public void canCreateCorrectDataForTestRun() throws IOException {
+        CuppaData cuppaData = CuppaDataFactory.create(CuppaDataFile.read(CUPPA_TEST_CSV));
+
+        assertEquals(36, cuppaData.predictions().size());
+        assertEquals(0.996, findByCancerType(cuppaData.predictions(), "Melanoma").likelihood(), EPSILON);
+        assertEquals(0.000131, findByCancerType(cuppaData.predictions(), "Pancreas").likelihood(), EPSILON);
+        assertEquals(0.000146, findByCancerType(cuppaData.predictions(), "Lung: Non-small Cell").likelihood(), EPSILON);
+
+        assertEquals(3, cuppaData.simpleDups32To200B());
+        assertEquals(8, cuppaData.maxComplexSize());
+        assertEquals(0, cuppaData.telomericSGLs());
+        assertEquals(4, cuppaData.LINECount());
+    }
+
+    @NotNull
+    private static CuppaPrediction findByCancerType(@NotNull List<CuppaPrediction> predictions, @NotNull String cancerType) {
+        for (CuppaPrediction prediction : predictions) {
+            if (prediction.cancerType().equals(cancerType)) {
+                return prediction;
+            }
+        }
+
+        throw new IllegalStateException("Could not find prediction for cancer type '" + cancerType + "'");
+    }
 
     @Test
     public void respectOrderingOfCombinedDataTypes() {
