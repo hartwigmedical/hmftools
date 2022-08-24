33fb580600fe6e2fb29602d141d8008806a786cc
Charles Shale <shalecharles@gmail.com>
2022-08-24 13:28:21 +1000

    SvPrep: high-depth for hg38

---
+++ sv-prep/src/main/java/com/hartwig/hmftools/svprep/reads/ReadIdTrimmer.java
@@ -0,0 +1,42 @@
+package com.hartwig.hmftools.svprep.reads;
+
+public class ReadIdTrimmer
+{
+    private final boolean mEnabled;
+    private int mReadIdTrimIndex;
+
+    public ReadIdTrimmer(boolean enabled)
+    {
+        mEnabled = enabled;
+        mReadIdTrimIndex = -1;
+    }
+
+    public String trim(final String readId)
+    {
+        if(!mEnabled)
+            return readId;
+
+        if(mReadIdTrimIndex < 0)
+            mReadIdTrimIndex = findReadIdTrimIndex(readId);
+
+        return mReadIdTrimIndex > 0 ? readId.substring(mReadIdTrimIndex) : readId;
+    }
+
+    private static final char READ_ID_DELIM = ':';
+
+    private static int findReadIdTrimIndex(final String readId)
+    {
+        // expected IDs: A00260:251:HLYGFDSXY:1:1673:32280:4946
+        int delimCount = 0;
+        for(int i = 0; i < readId.length(); ++i)
+        {
+            if(readId.charAt(i) == READ_ID_DELIM)
+                ++delimCount;
+
+            if(delimCount == 3)
+                return i + 1;
+        }
+
+        return -1;
+    }
+}
