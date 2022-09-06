10956262b298804431a562b3243c5556b233200e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-09-06 09:38:39 +0200

    Fix compile issue due to changes in nucleotides

--- serve/src/main/java/com/hartwig/hmftools/serve/transvar/TransvarInterpreter.java
+++ serve/src/main/java/com/hartwig/hmftools/serve/transvar/TransvarInterpreter.java
@@ -361,7 +361,7 @@ class TransvarInterpreter {
                     refSequence(record.chromosome(), posPriorToCodon + 1 + strandCorrection, posPriorToCodon + 1 + strandCorrection);
             String refBase2 =
                     refSequence(record.chromosome(), posPriorToCodon + 2 + strandCorrection, posPriorToCodon + 2 + strandCorrection);
-            for (String base : Nucleotides.DNA_BASE_LIST) {
+            for (char base : Nucleotides.DNA_BASES) {
                 String newRefCodon;
                 if (i == 0) {
                     newRefCodon = base + refBase1 + refBase2;
