0be456505fdf1fc7b911188ef82a040bb0393c5b
David Koetsier <david.f.koetsier@gmail.com>
2022-10-11 10:17:28 +0200

    DEV-2919: Implement basic liftover

--- peach/src/main/java/com/hartwig/hmftools/peach/PeachApplication.java
+++ peach/src/main/java/com/hartwig/hmftools/peach/PeachApplication.java
@@ -5,17 +5,72 @@ import static com.hartwig.hmftools.common.utils.ConfigUtils.setLogLevel;
 import org.apache.commons.cli.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
 import static com.hartwig.hmftools.peach.PeachUtils.PCH_LOGGER;
 
 public class PeachApplication {
-    private final PeachConfig mConfig;
-    public PeachApplication(final PeachConfig config)
+    @NotNull
+    private final PeachConfig config;
+    public PeachApplication(@NotNull final PeachConfig config)
     {
-        mConfig = config;
+        this.config = config;
     }
 
     public void run(){
+        if(!config.isValid())
+        {
+            PCH_LOGGER.error("invalid config, exiting");
+            System.exit(1);
+        }
         PCH_LOGGER.info("running PEACH");
+
+        String v38Vcf;
+        if (config.doLiftOver)
+        {
+            PCH_LOGGER.info("do lift over");
+            v38Vcf = getExtendedVcfFileName(config.vcfFile, "38");
+            String rejectVcf = getExtendedVcfFileName(config.vcfFile, "reject");
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java",
+                    "-jar",
+                    config.picardJar,
+                    "--CHAIN",
+                    config.chainFile,
+                    "--INPUT",
+                    config.vcfFile,
+                    "--OUTPUT",
+                    v38Vcf,
+                    "--REFERENCE_SEQUENCE",
+                    config.targetRefGenome,
+                    "--REJECT",
+                    rejectVcf
+            );
+            try {
+                Process process = pb.start();
+                int exitCode = process.waitFor();
+                PCH_LOGGER.info("Picard exit code: {}", exitCode);
+            }
+            catch(IOException e){
+                PCH_LOGGER.error("Picard LiftoverVcf failed: ");
+                e.printStackTrace();
+                System.exit(1);
+            }
+            catch(InterruptedException e){
+                PCH_LOGGER.error("Picard LiftoverVcf was interrupted");
+                System.exit(1);
+            }
+        }
+
+        PCH_LOGGER.info("finished running PEACH");
+    }
+
+    private String getExtendedVcfFileName(String originalFileName, String extension){
+        String[] fileItems = originalFileName.split("/");
+        String filename = fileItems[fileItems.length - 1];
+        int extensionIndex = filename.indexOf(".vcf");
+        return config.outputDir + filename.substring(0, extensionIndex) + "." + extension + filename.substring(extensionIndex);
     }
 
     public static void main(String[] args) {
--- peach/src/main/java/com/hartwig/hmftools/peach/PeachConfig.java
+++ peach/src/main/java/com/hartwig/hmftools/peach/PeachConfig.java
@@ -3,13 +3,63 @@ package com.hartwig.hmftools.peach;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 
+import static com.hartwig.hmftools.common.utils.FileWriterUtils.addOutputDir;
+import static com.hartwig.hmftools.common.utils.FileWriterUtils.parseOutputDir;
+
 public class PeachConfig {
+    public final String vcfFile;
+    public final String outputDir;
+
+    public final boolean doLiftOver;
+    public final String chainFile;
+    public final String liftOverBed;
+    public final String picardJar;
+    public final String targetRefGenome;
+
+    private static final String VCF_FILE = "vcf_file";
+
+    private static final String DO_LIFT_OVER = "do_lift_over";
+    private static final String CHAIN_FILE = "chain_file";
+    private static final String LIFT_OVER_BED = "lift_over_bed";
+    private static final String PICARD = "picard_jar";
+    private static final String TARGET_REF_GENOME = "target_ref_genome";
+
     public PeachConfig(final CommandLine cmd)
     {
+        vcfFile = cmd.getOptionValue(VCF_FILE);
+
+        doLiftOver = cmd.hasOption(DO_LIFT_OVER);
+        chainFile = cmd.getOptionValue(CHAIN_FILE);
+        liftOverBed = cmd.getOptionValue(LIFT_OVER_BED);
+        picardJar = cmd.getOptionValue(PICARD);
+        targetRefGenome = cmd.getOptionValue(TARGET_REF_GENOME);
+
+        outputDir = parseOutputDir(cmd);
     }
+
+    public boolean isValid()
+    {
+        if(vcfFile == null || outputDir == null)
+            return false;
+
+        if (doLiftOver)
+            return !(liftOverBed == null || picardJar == null || chainFile == null || targetRefGenome == null);
+
+        return true;
+    }
+
     public static Options createOptions()
     {
         final Options options = new Options();
+        options.addOption(VCF_FILE, true, "VCF input file");
+
+        options.addOption(DO_LIFT_OVER, false, "Do liftover to 38");
+        options.addOption(CHAIN_FILE, true, "USCS chain file for liftover, if liftover is needed");
+        options.addOption(LIFT_OVER_BED, true, "BED file specifying ranges that need to be lifted over to 38, if liftover is needed");
+        options.addOption(PICARD, true, "Picard JAR for liftover, if liftover is needed");
+        options.addOption(TARGET_REF_GENOME, true, "Target reference FASTA file for liftover, if liftover is needed");
+
+        addOutputDir(options);
         return options;
     }
 }
