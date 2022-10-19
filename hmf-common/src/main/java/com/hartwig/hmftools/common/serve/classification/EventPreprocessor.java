5ad4f31d8ef349d39b58071e4a871d4fd8e51b67
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-10-19 20:01:35 +0200

    Remove unused code

--- hmf-common/src/main/java/com/hartwig/hmftools/common/serve/classification/EventPreprocessor.java
+++
@@ -1,9 +0,0 @@
-package com.hartwig.hmftools.common.serve.classification;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface EventPreprocessor {
-
-    @NotNull
-    String apply(@NotNull String event);
-}
--- hmf-common/src/main/java/com/hartwig/hmftools/common/serve/datamodel/fusion/ExonicDelDupType.java
+++
@@ -1,6 +0,0 @@
-package com.hartwig.hmftools.common.serve.datamodel.fusion;
-
-public enum ExonicDelDupType {
-    PARTIAL_EXONIC_DELETION,
-    FULL_EXONIC_DELETION
-}
