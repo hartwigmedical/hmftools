dac3b854f7ff47dcc564b5e6cf5d7155a1d0c874
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-10-26 11:37:03 +0200

    Solve coding issue non-canonical transcript

--- hmf-common/src/test/java/com/hartwig/hmftools/common/protect/ProtectEventGeneratorTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/protect/ProtectEventGeneratorTest.java
@@ -49,14 +49,10 @@ public class ProtectEventGeneratorTest {
         Variant base = SomaticVariantTestFactory.builder().canonicalEffect("some effect").build();
         assertEquals("some effect", EventGenerator.variantEvent(base));
 
-        String upstreamEffect = EventGenerator.UPSTREAM_GENE_VARIANT;
-        Variant upstream = ImmutableSomaticVariantImpl.builder().from(base).canonicalEffect(upstreamEffect).build();
-        assertEquals("upstream", EventGenerator.variantEvent(upstream));
-
-        Variant coding = ImmutableSomaticVariantImpl.builder().from(upstream).canonicalHgvsCodingImpact("coding impact").build();
+        Variant coding = ImmutableSomaticVariantImpl.builder().from(base).canonicalHgvsCodingImpact("coding impact").build();
         assertEquals("coding impact", EventGenerator.variantEvent(coding));
 
-        Variant protein = ImmutableSomaticVariantImpl.builder().from(coding).canonicalHgvsProteinImpact("protein impact").build();
+        Variant protein = ImmutableSomaticVariantImpl.builder().from(base).canonicalHgvsProteinImpact("protein impact").build();
         assertEquals("protein impact", EventGenerator.variantEvent(protein));
     }
 
