1a8a019d5f007ad1e46344a9479132972226632d
liekeschoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-12-23 09:43:09 +0100

    OPR-85: Sort protect urls

--- hmf-common/src/main/java/com/hartwig/hmftools/common/protect/KnowledgebaseSourceComparator.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/protect/KnowledgebaseSourceComparator.java
@@ -1,8 +1,10 @@
 package com.hartwig.hmftools.common.protect;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class KnowledgebaseSourceComparator implements Comparator<KnowledgebaseSource> {
 
@@ -24,6 +26,23 @@ public class KnowledgebaseSourceComparator implements Comparator<KnowledgebaseSo
             return evidenceTypeCompare;
         }
 
+        int rangerankCompare = compareInteger(source1.rangeRank(), source2.rangeRank());
+        if (rangerankCompare != 0) {
+            return rangerankCompare;
+        }
+
         return 0;
+    }
+
+    private static int compareInteger(@Nullable Integer integer1, @Nullable Integer integer2) {
+        if (Objects.equals(integer1, integer2)) {
+            return 0;
+        } else if (integer1 == null) {
+            return -1;
+        } else if (integer2 == null) {
+            return 1;
+        } else {
+            return integer1.compareTo(integer2);
+        }
     }
 }
