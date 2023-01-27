8e1c882e8f11b361a8dd64b307d28a02aaab2827
liekeschoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-01-27 15:46:06 +0100

    Update cuppa likelihood string

--- patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/xml/XMLFactory.java
+++ patient-reporter/src/main/java/com/hartwig/hmftools/patientreporter/xml/XMLFactory.java
@@ -77,8 +77,8 @@ public class XMLFactory {
         } else if (report.molecularTissueOriginReporting().interpretLikelihood() == null) {
             cupAnalyse = report.molecularTissueOriginReporting().interpretCancerType();
         } else {
-            cupAnalyse = report.molecularTissueOriginReporting().interpretCancerType() + " (" + report.molecularTissueOriginReporting()
-                    .interpretLikelihood() + ")";
+            String cuppaLikelihood = DataUtil.formatPercentageDigit(report.molecularTissueOriginReporting().interpretLikelihood());
+            cupAnalyse = report.molecularTissueOriginReporting().interpretCancerType() + " " + cuppaLikelihood;
         }
         xml.add(ImmutableKeyXML.builder()
                 .keyPath("WgsCupAnalyse")
