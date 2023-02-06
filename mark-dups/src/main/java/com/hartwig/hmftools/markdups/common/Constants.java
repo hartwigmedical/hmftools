b1bfbc19b1ff5d51d9e3efc441646d1b8e0cddf4
Charles Shale <shalecharles@gmail.com>
2023-02-07 09:38:55 +1100

    MarkDups: default buffer size 1K->500 bases

--- mark-dups/src/main/java/com/hartwig/hmftools/markdups/common/Constants.java
+++ mark-dups/src/main/java/com/hartwig/hmftools/markdups/common/Constants.java
@@ -4,7 +4,7 @@ public class Constants
 {
     // BAM processing
     public static final int DEFAULT_PARTITION_SIZE = 1000000;
-    public static final int DEFAULT_POS_BUFFER_SIZE = 1000;
+    public static final int DEFAULT_POS_BUFFER_SIZE = 500;
 
     // UMIs
     public static final int MAX_UMI_BASE_DIFF = 1;
