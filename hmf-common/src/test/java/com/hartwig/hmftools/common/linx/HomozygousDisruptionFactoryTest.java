597f2ced5129942ac8fb840d375b6d0f57fae7d0
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-02-14 13:19:01 +0100

    Add loading of germline homozygous disruptions to linx data loading

--- hmf-common/src/test/java/com/hartwig/hmftools/common/linx/HomozygousDisruptionFactoryTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/linx/HomozygousDisruptionFactoryTest.java
@@ -1,6 +1,7 @@
 package com.hartwig.hmftools.common.linx;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,12 +12,14 @@ import org.junit.Test;
 
 public class HomozygousDisruptionFactoryTest {
 
-    private static final String LINX_DRIVERS_CATALOG_TSV = Resources.getResource("linx/sample.linx.driver.catalog.tsv").getPath();
+    private static final String SOMATIC_DRIVERS_CATALOG_TSV = Resources.getResource("linx/sample.linx.driver.catalog.tsv").getPath();
+    private static final String GERMLINE_DRIVERS_CATALOG_TSV = Resources.getResource("linx/sample.linx.germline.driver.catalog.tsv").getPath();
 
     @Test
-    public void canExtractHomozygousDisruptions() throws IOException {
+    public void canExtractSomaticHomozygousDisruptions() throws IOException
+    {
         List<HomozygousDisruption> homozygousDisruptions =
-                HomozygousDisruptionFactory.extractFromLinxDriverCatalogTsv(LINX_DRIVERS_CATALOG_TSV);
+                HomozygousDisruptionFactory.extractSomaticFromLinxDriverCatalogTsv(SOMATIC_DRIVERS_CATALOG_TSV);
 
         assertEquals(1, homozygousDisruptions.size());
 
@@ -24,5 +27,21 @@ public class HomozygousDisruptionFactoryTest {
         assertEquals("9", homozygousDisruption1.chromosome());
         assertEquals("p23-p24.1", homozygousDisruption1.chromosomeBand());
         assertEquals("PTPRD", homozygousDisruption1.gene());
+    }
+
+    @Test
+    public void canExtractGermlineHomozygousDisruptions() throws IOException
+    {
+        List<HomozygousDisruption> homozygousDisruptions =
+                HomozygousDisruptionFactory.extractGermlineFromLinxDriverCatalogTsv(GERMLINE_DRIVERS_CATALOG_TSV);
+
+        assertEquals(1, homozygousDisruptions.size());
+
+        HomozygousDisruption homozygousDisruption1 = homozygousDisruptions.get(0);
+        assertEquals("13", homozygousDisruption1.chromosome());
+        assertEquals("q13.1", homozygousDisruption1.chromosomeBand());
+        assertEquals("BRCA2", homozygousDisruption1.gene());
+        assertEquals("ENST00000544455", homozygousDisruption1.transcript());
+        assertTrue(homozygousDisruption1.isCanonical());
     }
 }
