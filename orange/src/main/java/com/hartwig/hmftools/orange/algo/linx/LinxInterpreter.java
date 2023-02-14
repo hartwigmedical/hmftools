04e070fce6081289fe5a5b1ebcafef2a9aafe1aa
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-02-14 13:28:28 +0100

    Pass through germline homozygous disruptions and remove filtering of breakends

--- orange/src/main/java/com/hartwig/hmftools/orange/algo/linx/LinxInterpreter.java
+++ orange/src/main/java/com/hartwig/hmftools/orange/algo/linx/LinxInterpreter.java
@@ -2,22 +2,15 @@ package com.hartwig.hmftools.orange.algo.linx;
 
 import java.util.List;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.drivercatalog.panel.DriverGene;
 import com.hartwig.hmftools.common.fusion.KnownFusionCache;
-import com.hartwig.hmftools.common.linx.HomozygousDisruption;
-import com.hartwig.hmftools.common.linx.ImmutableHomozygousDisruption;
 import com.hartwig.hmftools.common.linx.LinxBreakend;
 import com.hartwig.hmftools.common.linx.LinxData;
 import com.hartwig.hmftools.common.linx.LinxFusion;
-import com.hartwig.hmftools.common.sv.StructuralVariantType;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class LinxInterpreter {
 
@@ -48,20 +41,6 @@ public class LinxInterpreter {
         LOGGER.info(" Found an additional {} suspect somatic breakends that are potentially interesting",
                 additionalSuspectSomaticBreakends.size());
 
-        List<LinxBreakend> reportableGermlineBreakends = filterForPresenceInTumor(linx.reportableGermlineBreakends());
-        if (reportableGermlineBreakends != null) {
-            LOGGER.info(" Resolved {} reportable germline breakends from {} disruptive breakends",
-                    reportableGermlineBreakends.size(),
-                    linx.reportableGermlineBreakends().size());
-        }
-
-        List<HomozygousDisruption> germlineHomozygousDisruptions = resolveHomozygousDisruptions(reportableGermlineBreakends);
-        if (germlineHomozygousDisruptions != null) {
-            LOGGER.info(" Resolved {} germline homozygous disruptions from {} breakends",
-                    germlineHomozygousDisruptions.size(),
-                    reportableGermlineBreakends.size());
-        }
-
         return ImmutableLinxInterpretedData.builder()
                 .allSomaticStructuralVariants(linx.allSomaticStructuralVariants())
                 .allSomaticFusions(linx.allSomaticFusions())
@@ -73,57 +52,8 @@ public class LinxInterpreter {
                 .somaticHomozygousDisruptions(linx.somaticHomozygousDisruptions())
                 .allGermlineStructuralVariants(linx.allGermlineStructuralVariants())
                 .allGermlineBreakends(linx.allGermlineBreakends())
-                .reportableGermlineBreakends(reportableGermlineBreakends)
-                .germlineHomozygousDisruptions(germlineHomozygousDisruptions)
+                .reportableGermlineBreakends(linx.reportableGermlineBreakends())
+                .germlineHomozygousDisruptions(linx.germlineHomozygousDisruptions())
                 .build();
-    }
-
-    @Nullable
-    @VisibleForTesting
-    static List<LinxBreakend> filterForPresenceInTumor(@Nullable List<LinxBreakend> breakends) {
-        if (breakends == null) {
-            return null;
-        }
-
-        List<LinxBreakend> filtered = Lists.newArrayList();
-        for (LinxBreakend breakend : breakends) {
-            if (breakend.junctionCopyNumber() > 0.1) {
-                filtered.add(breakend);
-            }
-        }
-
-        return filtered;
-    }
-
-    @Nullable
-    @VisibleForTesting
-    static List<HomozygousDisruption> resolveHomozygousDisruptions(@Nullable List<LinxBreakend> breakends) {
-        if (breakends == null) {
-            return null;
-        }
-
-        List<HomozygousDisruption> homozygousDisruptions = Lists.newArrayList();
-        for (Pair<LinxBreakend, LinxBreakend> breakendPair : BreakendUtil.createPairsPerSvId(breakends)) {
-            LinxBreakend first = breakendPair.getLeft();
-            LinxBreakend second = breakendPair.getRight();
-
-            boolean bothReported = first.reportedDisruption() && second.reportedDisruption();
-            boolean bothDup = first.type() == StructuralVariantType.DUP && second.type() == StructuralVariantType.DUP;
-            boolean bothSameGene = first.gene().equals(second.gene());
-            boolean bothSameTranscript = first.transcriptId().equals(second.transcriptId());
-            boolean bothDisruptRemainingCopies = first.junctionCopyNumber() >= first.undisruptedCopyNumber()
-                    && second.junctionCopyNumber() >= second.undisruptedCopyNumber();
-
-            if (bothReported && bothDup && bothSameGene && bothSameTranscript && bothDisruptRemainingCopies) {
-                homozygousDisruptions.add(ImmutableHomozygousDisruption.builder()
-                        .chromosome(first.chromosome())
-                        .chromosomeBand(first.chrBand())
-                        .gene(first.gene())
-                        .transcript(first.transcriptId())
-                        .isCanonical(first.canonical())
-                        .build());
-            }
-        }
-        return homozygousDisruptions;
     }
 }
--- orange/src/test/java/com/hartwig/hmftools/orange/algo/linx/LinxInterpreterTest.java
+++ orange/src/test/java/com/hartwig/hmftools/orange/algo/linx/LinxInterpreterTest.java
@@ -1,19 +1,10 @@
 package com.hartwig.hmftools.orange.algo.linx;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.fusion.KnownFusionCache;
-import com.hartwig.hmftools.common.linx.HomozygousDisruption;
 import com.hartwig.hmftools.common.linx.ImmutableLinxData;
-import com.hartwig.hmftools.common.linx.LinxBreakend;
-import com.hartwig.hmftools.common.linx.LinxTestFactory;
-import com.hartwig.hmftools.common.sv.StructuralVariantType;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
@@ -24,58 +15,6 @@ public class LinxInterpreterTest {
     public void canInterpretMinimalLinxData() {
         LinxInterpreter interpreter = createTestInterpreter();
         assertNotNull(interpreter.interpret(ImmutableLinxData.builder().build()));
-    }
-
-    @Test
-    public void canFilterReportableGermlineBreakends() {
-        LinxBreakend present = LinxTestFactory.breakendBuilder().junctionCopyNumber(1D).build();
-        LinxBreakend absent = LinxTestFactory.breakendBuilder().junctionCopyNumber(0D).build();
-
-        assertNull(LinxInterpreter.filterForPresenceInTumor(null));
-        assertEquals(1, LinxInterpreter.filterForPresenceInTumor(Lists.newArrayList(present)).size());
-        assertEquals(0, LinxInterpreter.filterForPresenceInTumor(Lists.newArrayList(absent)).size());
-    }
-
-    @Test
-    public void canResolveHomozygousDisruptions() {
-        LinxBreakend match = LinxTestFactory.breakendBuilder()
-                .svId(1)
-                .reportedDisruption(true)
-                .junctionCopyNumber(2)
-                .undisruptedCopyNumber(1)
-                .type(StructuralVariantType.DUP)
-                .build();
-
-        LinxBreakend noPair = LinxTestFactory.breakendBuilder().svId(2).build();
-
-        LinxBreakend notAHomPair = LinxTestFactory.breakendBuilder()
-                .svId(1)
-                .reportedDisruption(true)
-                .junctionCopyNumber(1)
-                .undisruptedCopyNumber(2)
-                .type(StructuralVariantType.DUP)
-                .build();
-
-        LinxBreakend homPair = LinxTestFactory.breakendBuilder()
-                .svId(1)
-                .reportedDisruption(true)
-                .junctionCopyNumber(2)
-                .undisruptedCopyNumber(2)
-                .type(StructuralVariantType.DUP)
-                .build();
-
-        assertNull(LinxInterpreter.resolveHomozygousDisruptions(null));
-        assertTrue(LinxInterpreter.resolveHomozygousDisruptions(Lists.newArrayList(match, noPair)).isEmpty());
-        assertTrue(LinxInterpreter.resolveHomozygousDisruptions(Lists.newArrayList(match, notAHomPair)).isEmpty());
-
-        List<HomozygousDisruption> homozygousDisruptions = LinxInterpreter.resolveHomozygousDisruptions(Lists.newArrayList(match, homPair));
-        assertEquals(1, homozygousDisruptions.size());
-        HomozygousDisruption homozygousDisruption = homozygousDisruptions.get(0);
-        assertEquals(match.chromosome(), homozygousDisruption.chromosome());
-        assertEquals(match.chrBand(), homozygousDisruption.chromosomeBand());
-        assertEquals(match.gene(), homozygousDisruption.gene());
-        assertEquals(match.transcriptId(), homozygousDisruption.transcript());
-        assertEquals(match.canonical(), homozygousDisruption.isCanonical());
     }
 
     @NotNull
