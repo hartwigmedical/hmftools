b831d2f9f393b864be42d3285d12c515b9a87fec
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-04-18 14:20:18 +0200

    Formatting

--- orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/purple/PurpleQC.java
+++ orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/purple/PurpleQC.java
@@ -11,6 +11,7 @@ import org.jetbrains.annotations.Nullable;
 @Value.Immutable
 @Value.Style(passAnnotations = { NotNull.class, Nullable.class })
 public interface PurpleQC {
+
     @NotNull
     Set<PurpleQCStatus> status();
 
