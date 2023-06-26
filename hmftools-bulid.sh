2c3dde38d800cea0e73d12e32ea4aaa725c7541c
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-06-26 10:23:42 +0200

    DEV-3317 finish build script

--- hmftools-bulid.sh
+++ hmftools-bulid.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+# Verify input
 if [ $# -ne 1 ]; then
   echo "Invalid arguments. Usage: hmftools-build semver-version"
   exit 1
 fi
 
 SEMVER_VERSION="$1"
-
+# The regex pattern is <module_name>-<semver>
 SEMVER_REGEX="^([a-zA-Z0-9_]+)-([0-9]+)\.([0-9]+)\.([0-9]+)(-(alpha|beta)\.[0-9]+)?$"
 if ! [[ "$SEMVER_VERSION" =~ $SEMVER_REGEX ]]; then
   echo "Invalid semver version: $SEMVER_VERSION"
@@ -18,7 +19,17 @@ fi
 BUILD_MODULE=${BASH_REMATCH[1]}
 SEMVER="${SEMVER_VERSION/${BASH_REMATCH[1]}-}"
 
-echo $BUILD_MODULE
-echo $SEMVER
+# Check if this module exists
+if ! [[ -d "$BUILD_MODULE" ]]; then
+  echo "Invalid semver version: module $BUILD_MODULE does not exist"
+  exit 1
+fi
 
-mvn -f ./${BUILD_MODULE}/pom.xml versions:set -DnewVersion=${SEMVER}
+# Version the appropriate files
+mvn -f "./${BUILD_MODULE}/pom.xml" versions:set -DnewVersion=${SEMVER}
+
+# Step 2: compile, package, release
+mvn -f "./${BUILD_MODULE}/pom.xml" deploy -B
+
+
+echo "done!"
