36f5e6a025722d6619ab003687bb4fe2980b3b12
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-06-26 12:39:05 +0200

    DEV-3317 Add main class in test module

---
+++ test-hmftools/src/main/java/com/hartwig/Main.java
@@ -0,0 +1,9 @@
+package com.hartwig;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+    public final String test = "test string";
+}
