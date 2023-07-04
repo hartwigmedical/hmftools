5060b2c7e2dfc61f64a2b97bfc07e1fbf0b256a9
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-07-04 09:42:25 +0200

    DEV-3317 Delete unneeded test project

--- test-hmftools/src/main/java/com/hartwig/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hartwig;
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-    }
-    public final String test = "test string";
-}
