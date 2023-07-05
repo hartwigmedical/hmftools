7bcec6dd28c301de4086d888138c0a4cae086393
Charles Shale <shalecharles@gmail.com>
2023-07-06 06:18:33 +1000

    Common: added Sage germline dir config

--- hmf-common/src/main/java/com/hartwig/hmftools/common/utils/config/CommonConfig.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/utils/config/CommonConfig.java
@@ -68,6 +68,9 @@ public final class CommonConfig
     public static final String SAGE_DIR_CFG = toolDirectory("sage");
     public static final String SAGE_DIR_DESC = toolDirectoryDesc("Sage");
 
+    public static final String SAGE_GERMLINE_DIR_CFG = toolDirectory("sage_germline");
+    public static final String SAGE_GERMLINE_DIR_DESC = toolDirectoryDesc("Sage germline");
+
     public static final String SIGS_DIR_CFG = toolDirectory("sigs");
     public static final String SIGS_DIR_DESC = toolDirectoryDesc("Signatures");
 
