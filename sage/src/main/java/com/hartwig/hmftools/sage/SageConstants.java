a760957a25c97059b1bd9be437453b98d186c0b0
Charles Shale <shalecharles@gmail.com>
2023-07-10 21:46:00 +1000

    Sage: lowered hotspot min average base qual from 22 to 18 as intended

--- sage/src/main/java/com/hartwig/hmftools/sage/SageConstants.java
+++ sage/src/main/java/com/hartwig/hmftools/sage/SageConstants.java
@@ -55,7 +55,7 @@ public class SageConstants
     public static final int HOTSPOT_MIN_RAW_ALT_BASE_QUAL = 150;
 
     public static final int DEFAULT_MIN_AVG_BASE_QUALITY = 28;
-    public static final int DEFAULT_MIN_AVG_BASE_QUALITY_HOTSPOT = 22;
+    public static final int DEFAULT_MIN_AVG_BASE_QUALITY_HOTSPOT = 18;
 
     public static final SoftFilterConfig DEFAULT_HOTSPOT_FILTER = new SoftFilterConfig(
             "hotspot", 70, 0.005,
