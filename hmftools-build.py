c524cb92f4b1588cd1c497bd56316bd7d1ea633f
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-07-10 10:25:44 +0200

    DEV-3374 refactor class

--- hmftools-build.py
+++ hmftools-build.py
@@ -3,8 +3,39 @@ import subprocess
 import sys
 import xml.etree.ElementTree as ET
 
+### Classes
 
-def extract_hmftools_dependencies(dependencies):
+class Maven:
+    def __init__(self, pom_path):
+        self.pom_path = pom_path
+    
+    def set_property(self, property, value):
+        subprocess.run(['mvn', '-f', self.pom_path, 'versions:set-property',
+                     '-DgenerateBackupPoms=false', f'-Dproperty={property}', f'-DnewVersion={value}'], check=True)
+    
+    def set_version(self, version):
+        subprocess.run(['mvn', '-f', self.pom_path, 'versions:set',
+                        '-DgenerateBackupPoms=false', f'-DnewVersion={version}'], check=True)
+        
+    def install(self, non_recursive=False):
+        if non_recursive:
+            subprocess.run(['mvn', '-N', '-f', self.pom_path, 'clean', 'install'], check=True)
+        else:
+            subprocess.run(['mvn', '-f', self.pom_path, 'clean', 'install'], check=True)
+
+    def deploy(self, non_recursive=False):
+        if non_recursive:
+            subprocess.run(['mvn', '-N', '-f', self.pom_path, 'deploy', '-B'], check=True)
+        else:
+            subprocess.run(['mvn', '-f', self.pom_path, 'deploy', '-B'], check=True)
+
+
+### Helper methods
+
+def extract_hmftools_dependencies(pom_path):
+    parsed_module_pom = ET.parse(pom_path)
+    namespace = {'ns': 'http://maven.apache.org/POM/4.0.0'}
+    dependencies = parsed_module_pom.root().findall('.//ns:dependencies/ns:dependency', namespace)
     hmftools_dependencies = set()
     for dep in dependencies:
         group_id = dep.find('ns:groupId', namespace).text
@@ -14,12 +45,11 @@ def extract_hmftools_dependencies(dependencies):
     return hmftools_dependencies
 
 
-
-
+### Script
 
 # Check if a semver version is included as argument.
 if len(sys.argv) != 2:
-    print(f'Invalid arguments. Usage: {sys.argv[0]} semver-version')
+    print(f'Invalid arguments. Usage: {sys.argv[0]} <semver-version>')
     quit()
 
 tag = sys.argv[1]
@@ -36,41 +66,29 @@ module = match.group(1)
 version = match.group(2)
 
 # parse all the hmftools modules the project depends on from the pom.xml
-parsed_module_pom = ET.parse(f'{module}/pom.xml')
-root = parsed_module_pom.getroot()
-namespace = {'ns': 'http://maven.apache.org/POM/4.0.0'}
-dependencies = root.findall('.//ns:dependencies/ns:dependency', namespace)
-hmftools_dependencies = extract_hmftools_dependencies(dependencies)
+hmftools_dependencies = extract_hmftools_dependencies(f'{module}/pom.xml')
 
+parent_pom = Maven('pom.xml')
+module_pom = Maven(f'{module}/pom.xml')
+dependencies_pom = [Maven(f'{hmf_dep}/pom.xml') for hmf_dep in hmftools_dependencies]
 
-# Set version of module in parent pom.
-subprocess.run(
-    ['mvn', '-f', 'pom.xml', 'versions:set-property', '-DgenerateBackupPoms=false', f'-Dproperty={module}.version',
-     f'-DnewVersion={version}'],
-    check=True)
-
-# Set version of hmftools dependencies in parent pom.xml
+# Set versions in appropriate poms
+# For the module we are targetting, we will use only the version part of the semver tag
+# For all dependencies, we will use the entire semver tag 
+parent_pom.set_property(f'{module}.version', version)
 for hmf_dep in hmftools_dependencies:
-    subprocess.run(['mvn', '-f', 'pom.xml', 'versions:set-property', '-DgenerateBackupPoms=false',
-                    f'-Dproperty={hmf_dep}.version', f'-DnewVersion={tag}'], check=True)
-
-# Set version of parent pom
-subprocess.run(['mvn', '-f', 'pom.xml', 'versions:set',
-                '-DgenerateBackupPoms=false', f'-DnewVersion={tag}'], check=True)
+    parent_pom.set_property(f'{hmf_dep}.version', tag)
+parent_pom.set_version(tag)
+module_pom.set_version(version)
 
 # Build all submodules to check for build errors
-subprocess.run(['mvn', '-N', '-f', '"pom.xml"',
-                'clean', 'install'], check=True)
-for hmf_dep in hmftools_dependencies:
-    subprocess.run(
-        ['mvn', '-f', f'{hmf_dep}/pom.xml', 'clean', 'install'], check=True)
-subprocess.run(
-    ['mvn', '-f', f'{module}/pom.xml', 'clean', 'install'], check=True)
+parent_pom.install(non_recursive=True)
+for dependency_pom in dependencies_pom:
+    dependency_pom.install()
+module_pom.install()
 
 # Deploy if no errors
-subprocess.run(['mvn', '-N', '-f', 'pom.xml', 'deploy', '-B'], check=True)
-for hmf_dep in hmftools_dependencies:
-    subprocess.run(
-        ['mvn', '-f', f'{hmf_dep}/pom.xml', 'deploy', '-B'], check=True)
-
-subprocess.run(['mvn', '-f', f'{module}/pom.xml', 'deploy', '-B'], check=True)
+parent_pom.deploy(non_recursive=True)
+for dependency_pom in dependencies_pom:
+    dependency_pom.deploy()
+module_pom.deploy()
