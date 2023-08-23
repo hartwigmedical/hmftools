f9a15b1b3227704d4876caa5c745934fe8dfc3ed
James Venning <james.venning@imc.com>
2023-08-24 07:28:59 +1000

    Refactor Strings.reverseString
    Previous implementation was n^2

--- hmf-common/src/main/java/com/hartwig/hmftools/common/utils/Strings.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/utils/Strings.java
@@ -22,14 +22,7 @@ public final class Strings
 
     public static String reverseString(final String str)
     {
-        String reverse = "";
-
-        for(int i = str.length() - 1; i >= 0; --i)
-        {
-            reverse += str.charAt(i);
-        }
-
-        return reverse;
+        return new StringBuilder(str).reverse().toString();
     }
 
 }
