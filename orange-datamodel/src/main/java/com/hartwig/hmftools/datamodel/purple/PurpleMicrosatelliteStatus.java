b077b900da32ae8fa83028f0bd0d3376b624e7bb
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-08-23 14:32:50 +0200

    Annotation enums of orange datamodel

--- orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/purple/PurpleMicrosatelliteStatus.java
+++ orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/purple/PurpleMicrosatelliteStatus.java
@@ -1,7 +1,22 @@
 package com.hartwig.hmftools.datamodel.purple;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum PurpleMicrosatelliteStatus {
-    MSI,
-    MSS,
-    UNKNOWN
+    MSI("Unstable"),
+    MSS("Stable"),
+    UNKNOWN("Unknown");
+
+    private final String display;
+
+    PurpleMicrosatelliteStatus(final String display)
+    {
+        this.display = display;
+    }
+
+    @NotNull
+    public String display()
+    {
+        return display;
+    }
 }
