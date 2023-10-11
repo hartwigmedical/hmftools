0b459ea344ef879437f99021fa9022db421d1629
Matthew Cooper <matthew.cooper@pobox.com>
2023-10-11 13:50:51 +1100

    mark-dups: Bug fix.

--- mark-dups/src/main/java/com/hartwig/hmftools/sieve/bedstats/BedStatsConsumer.java
+++ mark-dups/src/main/java/com/hartwig/hmftools/sieve/bedstats/BedStatsConsumer.java
@@ -62,6 +62,10 @@ public class BedStatsConsumer implements Callable
         {
             ++mRegionCounter;
             mBaseDepth = new int[mCurrentRegion.baseLength()];
+            for(int i = 0; i < mBaseDepth.length; i++)
+            {
+                mBaseDepth[i] = 0;
+            }
 
             mJobPerfCounter.start();
             mBamSlicer.slice(mSamReader, mCurrentRegion, this::processSamRecord);
