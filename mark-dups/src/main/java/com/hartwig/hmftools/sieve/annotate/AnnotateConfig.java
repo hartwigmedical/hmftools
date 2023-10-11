14429d5b0326f1f00e770ea1046290da082ab93c
Matthew Cooper <matthew.cooper@pobox.com>
2023-10-11 11:35:11 +1100

    Post rebase fixes

--- mark-dups/src/main/java/com/hartwig/hmftools/sieve/annotate/AnnotateConfig.java
+++ mark-dups/src/main/java/com/hartwig/hmftools/sieve/annotate/AnnotateConfig.java
@@ -7,12 +7,15 @@ import static com.hartwig.hmftools.common.utils.TaskExecutor.parseThreads;
 
 import com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion;
 import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
+import com.hartwig.hmftools.sieve.count.CountConfig;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AnnotateConfig
 {
+    public static final Logger MD_LOGGER = LogManager.getLogger(AnnotateConfig.class);
+
     private static final String BAM_FILE = "bam_file";
     private static final String HIGH_DEPTH_FILE = "high_depth_file";
     private static final String OUTPUT_FILE = "output_file";
