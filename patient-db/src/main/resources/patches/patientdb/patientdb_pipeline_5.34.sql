173b6e24b15b19f6dfe6796eedbd854f562d0f57
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-10-16 14:26:05 +0200

    ACTIN-341: Add patch

---
+++ patient-db/src/main/resources/patches/patientdb/patientdb_pipeline_5.34.sql
@@ -0,0 +1,36 @@
+####
+# SQL updates for Pipeline release 5.34
+# NOTE: only add updates to this script if the tools impacted by them will be released with this pipeline release
+
+DROP TABLE IF EXISTS svBreakend;
+CREATE TABLE svBreakend
+(   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    modified DATETIME NOT NULL,
+    sampleId VARCHAR(50) NOT NULL,
+    svId INT NOT NULL,
+    startBreakend TINYINT(1) NOT NULL,
+    gene VARCHAR(50) NOT NULL,
+    transcriptId VARCHAR(128) NOT NULL,
+    canonicalTranscript TINYINT(1) NOT NULL,
+    geneOrientation VARCHAR(20) NOT NULL,
+    disruptive TINYINT(1) NOT NULL,
+    reportedDisruption TINYINT(1) NOT NULL,
+    undisruptedCopyNumber DOUBLE PRECISION,
+    regionType VARCHAR(20) NOT NULL,
+    codingContext VARCHAR(20),
+    biotype VARCHAR(255),
+    exonicBasePhase TINYINT,
+    nextSpliceExonRank TINYINT UNSIGNED,
+    nextSpliceExonPhase TINYINT,
+    nextSpliceDistance INT,
+    totalExonCount SMALLINT NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    chromosome VARCHAR(20) NOT NULL,
+    chrBand VARCHAR(20) NOT NULL,
+    orientation SMALLINT NOT NULL,
+    strand SMALLINT NOT NULL,
+    exonUp SMALLINT NOT NULL,
+    exonDown SMALLINT NOT NULL,
+    junctionCopyNumber DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (id)
+);
