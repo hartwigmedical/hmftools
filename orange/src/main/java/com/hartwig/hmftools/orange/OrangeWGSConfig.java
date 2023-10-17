458da5c76c7f32e5862625a2b6106d7bdb1d5570
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2023-10-17 14:43:37 -0400

    ACTIN-352: WGS & Ref shared config

--- orange/src/main/java/com/hartwig/hmftools/orange/OrangeWGSConfig.java
+++ orange/src/main/java/com/hartwig/hmftools/orange/OrangeWGSConfig.java
@@ -6,6 +6,8 @@ import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.FLAGS
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.LINX_GERMLINE_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.METRICS_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.PEACH_DIR;
+import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.SAGE_GERMLINE_DIR;
+import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.SAGE_SOMATIC_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.SIGS_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.VIRUS_INTERPRETER_DIR;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.CHORD_DIR_CFG;
@@ -13,6 +15,8 @@ import static com.hartwig.hmftools.common.utils.config.CommonConfig.CUPPA_DIR_CF
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.LINX_GERMLINE_DIR_CFG;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.PEACH_DIR_CFG;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.PIPELINE_SAMPLE_ROOT_DIR;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAGE_DIR_CFG;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAGE_GERMLINE_DIR_CFG;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAMPLE_DATA_DIR_CFG;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.SIGS_DIR_CFG;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.VIRUS_DIR_CFG;
@@ -28,6 +32,7 @@ import static com.hartwig.hmftools.orange.util.Config.optionalFileIfExists;
 
 import com.hartwig.hmftools.common.chord.ChordDataFile;
 import com.hartwig.hmftools.common.cuppa.CuppaDataFile;
+import com.hartwig.hmftools.common.sage.SageCommon;
 import com.hartwig.hmftools.common.sigs.SignatureAllocationFile;
 import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
 import com.hartwig.hmftools.common.virus.AnnotatedVirusFile;
@@ -40,6 +45,12 @@ import org.jetbrains.annotations.Nullable;
 @Value.Style(passAnnotations = { NotNull.class, Nullable.class })
 public interface OrangeWGSConfig
 {
+    @NotNull
+    String sageGermlineGeneCoverageTsv();
+
+    @NotNull
+    String sageSomaticRefSampleBQRPlot();
+
     @NotNull
     String linxGermlineDataDirectory();
 
@@ -90,6 +101,16 @@ public interface OrangeWGSConfig
         // TODO share these from elsewhere?
         String pipelineSampleRootDir = checkAddDirSeparator(configBuilder.getValue(PIPELINE_SAMPLE_ROOT_DIR));
         String sampleDataDir = checkAddDirSeparator(configBuilder.getValue(SAMPLE_DATA_DIR_CFG));
+        String sageSomaticDir = getToolDirectory(configBuilder, pipelineSampleRootDir, sampleDataDir, SAGE_DIR_CFG, SAGE_SOMATIC_DIR);
+
+        // TODO sageGermline should be here or in WGS?
+        String sageGermlineDir = getToolDirectory(
+                configBuilder, pipelineSampleRootDir, sampleDataDir, SAGE_GERMLINE_DIR_CFG, SAGE_GERMLINE_DIR);
+
+        builder.sageGermlineGeneCoverageTsv(fileIfExists(SageCommon.generateGeneCoverageFilename(sageGermlineDir, refSampleId)));
+
+        // TODO why are somatic dir and ref sample id together here?
+        builder.sageSomaticRefSampleBQRPlot(fileIfExists(SageCommon.generateBqrPlotFilename(sageSomaticDir, refSampleId)));
 
         String linxGermlineDir = getToolDirectory(
                 configBuilder, pipelineSampleRootDir, sampleDataDir, LINX_GERMLINE_DIR_CFG, LINX_GERMLINE_DIR);
