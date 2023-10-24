9d11f4378354856efa7b3d880ea72dab60f2b72e
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-10-24 12:23:12 +0200

    ACTIN-357: Add somatic drivers to linx orange record

---
+++ orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/linx/LinxDriver.java
@@ -0,0 +1,18 @@
+package com.hartwig.hmftools.datamodel.linx;
+
+import org.immutables.gson.Gson;
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Gson.TypeAdapters
+@Value.Immutable
+@Value.Style(passAnnotations = { NotNull.class, Nullable.class })
+public interface LinxDriver
+{
+    @NotNull
+    String gene();
+
+    @NotNull
+    LinxDriverType type();
+}
