9abcafe59e689ce42a5eeb14b4db24f8754b7eb0
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2023-11-15 13:25:21 +1100

    cobalt: added tests

--- cobalt/src/test/java/com/hartwig/hmftools/cobalt/count/BamReadCounterTest.java
+++ cobalt/src/test/java/com/hartwig/hmftools/cobalt/count/BamReadCounterTest.java
@@ -1,6 +1,11 @@
 package com.hartwig.hmftools.cobalt.count;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 
 import static htsjdk.samtools.util.SequenceUtil.A;
@@ -17,44 +22,56 @@ public class BamReadCounterTest
     @Test
     public void testAccumulateAlignmentBlock()
     {
-        String CHROMOSOME = "chr1";
+        final String CHROMOSOME = "chr1";
 
-        ReadDepthAccumulator readDepthCounter = mock(ReadDepthAccumulator.class);
-        byte[] bases = new byte[210];
+        final ReadDepthAccumulator readDepthAccumulator = mock(ReadDepthAccumulator.class);
+        final byte[] readBases = new byte[210];
 
-        for(int i = 0; i < bases.length; ++i)
+        // create some base patterns
+        for(int i = 0; i < readBases.length; ++i)
         {
             switch(i % 7)
             {
                 case 0:
                 case 3:
-                    bases[i] = A; break;
-                case 1: bases[i] = G; break;
+                    readBases[i] = A; break;
+                case 1: readBases[i] = G; break;
                 case 2:
                 case 5:
-                    bases[i] = C; break;
-                default: bases[i] = T; break;
+                    readBases[i] = C; break;
+                default: readBases[i] = T; break;
             }
         }
+        
+        final ChrBaseRegion region = new ChrBaseRegion(CHROMOSOME, 1001, 2000);
 
-        BamReadCounter.accumulateAlignmentBlock(new ChrBaseRegion(CHROMOSOME, 1001, 2000), readDepthCounter,
-                11, 1101, 200, bases);
-
+        BamReadCounter.accumulateAlignmentBlock(region, readDepthAccumulator,
+                11, 1101, 200, readBases);
         // check that it invoked the correct parameters
-        verify(readDepthCounter).addReadAlignmentToCounts(CHROMOSOME, 1101, 200, bases, 10);
+        verify(readDepthAccumulator).addReadAlignmentToCounts(CHROMOSOME, 1101, 200, readBases, 10);
 
         // now test that first 100 bases are before this region
-        BamReadCounter.accumulateAlignmentBlock(new ChrBaseRegion(CHROMOSOME, 1001, 2000), readDepthCounter,
-                11, 901, 200, bases);
-
+        BamReadCounter.accumulateAlignmentBlock(region, readDepthAccumulator,
+                11, 901, 200, readBases);
         // check that it invoked the correct parameters
-        verify(readDepthCounter).addReadAlignmentToCounts(CHROMOSOME, 1001, 100, bases, 110);
+        verify(readDepthAccumulator).addReadAlignmentToCounts(CHROMOSOME, 1001, 100, readBases, 110);
 
         // now test that last 100 bases are after this region
-        BamReadCounter.accumulateAlignmentBlock(new ChrBaseRegion(CHROMOSOME, 1001, 2000), readDepthCounter,
-                11, 1901, 200, bases);
-
+        BamReadCounter.accumulateAlignmentBlock(region, readDepthAccumulator,
+                11, 1901, 200, readBases);
         // check that it invoked the correct parameters
-        verify(readDepthCounter).addReadAlignmentToCounts(CHROMOSOME, 1901, 100, bases, 10);
+        verify(readDepthAccumulator).addReadAlignmentToCounts(CHROMOSOME, 1901, 100, readBases, 10);
+
+        reset(readDepthAccumulator);
+
+        // test a block that is before the region, check that no call to addReadAlignmentToCounts has been invoked
+        BamReadCounter.accumulateAlignmentBlock(region, readDepthAccumulator,
+                11, 500, 200, readBases);
+        verify(readDepthAccumulator, never()).addReadAlignmentToCounts(anyString(), anyInt(), anyInt(), any(byte[].class), anyInt());
+
+        // test a block that is after the region, check that no call to addReadAlignmentToCounts has been invoked
+        BamReadCounter.accumulateAlignmentBlock(region, readDepthAccumulator,
+                11, 2500, 200, readBases);
+        verify(readDepthAccumulator, never()).addReadAlignmentToCounts(anyString(), anyInt(), anyInt(), any(byte[].class), anyInt());
     }
 }
