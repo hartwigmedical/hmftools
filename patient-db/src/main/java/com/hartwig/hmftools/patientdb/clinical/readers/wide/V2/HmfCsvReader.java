e8edbe373f462df11c681f25117a42bd59fb96de
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-11-17 13:40:04 +0100

    INFRA-66 add HmfCsvreader tests

--- patient-db/src/main/java/com/hartwig/hmftools/patientdb/clinical/readers/wide/V2/HmfCsvReader.java
+++ patient-db/src/main/java/com/hartwig/hmftools/patientdb/clinical/readers/wide/V2/HmfCsvReader.java
@@ -1,9 +1,11 @@
 package com.hartwig.hmftools.patientdb.clinical.readers.wide.V2;
 
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jooq.tools.csv.CSVReader;
 
@@ -15,17 +17,18 @@ public final class HmfCsvReader
 
     public static List<CsvEntry> read(String pathToCsv) throws IOException
     {
-        List<CsvEntry> result = new ArrayList<>();
-        CSVReader csvReader = new CSVReader(new FileReader(pathToCsv));
-        String[] headers = csvReader.readNext();
-
-        while(csvReader.hasNext())
-        {
-            String[] values = csvReader.readNext();
-            CsvEntry entry = new CsvEntry(headers, values);
-            result.add(entry);
+        InputStream file = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathToCsv);
+        if (file == null) {
+            throw new FileNotFoundException(String.format("File '%s' not found.", pathToCsv));
         }
-        return result;
+        return read(file);
+    }
+
+    public static List<CsvEntry> read(InputStream inputStream) throws IOException
+    {
+        CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream));
+        String[] headers = csvReader.readNext();
+        return csvReader.readAll().stream().map(values -> new CsvEntry(headers, values)).collect(Collectors.toList());
     }
 
 }
---
+++ patient-db/src/test/java/com/hartwig/hmftools/patientdb/clinical/readers/wide/V2/HmfCsvReaderTest.java
@@ -0,0 +1,47 @@
+package com.hartwig.hmftools.patientdb.clinical.readers.wide.V2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+public class HmfCsvReaderTest
+{
+    private static final String DUMMY_CSV_PATH  = "readers/dummy_csv_file.csv";
+
+    @Test
+    public void canReadDummyFile() throws IOException
+    {
+        List<CsvEntry> entries = HmfCsvReader.read(DUMMY_CSV_PATH);
+
+        assertEquals(2, entries.size());
+
+        var firstEntry = entries.get(0);
+        var firstEntryValue0 = firstEntry.get("header1").orElseThrow();
+        var firstEntryValue1 = firstEntry.get("header2").orElseThrow();
+        var firstEntryValue2 = firstEntry.get("header3").orElseThrow();
+
+        assertEquals("0", firstEntryValue0);
+        assertEquals("foo", firstEntryValue1);
+        assertEquals("bar", firstEntryValue2);
+
+        var secondEntry = entries.get(1);
+        var secondEntryValue0 = secondEntry.get("header1").orElseThrow();
+        var secondEntryValue1 = secondEntry.get("header2").orElseThrow();
+        var secondEntryValue2 = secondEntry.get("header3").orElseThrow();
+
+        assertEquals("1", secondEntryValue0);
+        assertEquals("baz", secondEntryValue1);
+        assertEquals("qux", secondEntryValue2);
+    }
+
+    @Test
+    public void nonExistentFileThrows() throws IOException
+    {
+        assertThrows(FileNotFoundException.class, () -> HmfCsvReader.read("NonExistentCsvPath.csv"));
+    }
+}
