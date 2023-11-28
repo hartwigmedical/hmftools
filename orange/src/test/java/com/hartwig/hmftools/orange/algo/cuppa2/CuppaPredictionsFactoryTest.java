4191ca2f2e32f2a6564b642221428617c08e2802
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2023-11-28 15:02:37 +1100

    AUS-204: CUPPA_VIS_DATA_TSV now refers to the relative resource path

--- orange/src/test/java/com/hartwig/hmftools/orange/algo/cuppa2/CuppaPredictionsFactoryTest.java
+++ orange/src/test/java/com/hartwig/hmftools/orange/algo/cuppa2/CuppaPredictionsFactoryTest.java
@@ -1,6 +1,7 @@
 package com.hartwig.hmftools.orange.algo.cuppa2;
 import java.io.IOException;
 
+import com.google.common.io.Resources;
 import com.hartwig.hmftools.common.cuppa2.CuppaPredictions;
 import com.hartwig.hmftools.datamodel.cuppa2.ImmutableCuppaPredictions;
 import com.hartwig.hmftools.datamodel.cuppa2.ImmutableFeatureContributionEntry;
@@ -13,7 +14,7 @@ public class CuppaPredictionsFactoryTest
 {
     public CuppaPredictionsFactoryTest() throws IOException {}
 
-    private static final String CUPPA_VIS_DATA_TSV = "/Users/lnguyen/Hartwig/hartwigmedical/hmftools/hmf-common/src/test/resources/cuppa/cuppa_vis_data.tsv";
+    private static final String CUPPA_VIS_DATA_TSV = Resources.getResource("cuppa/cuppa_vis_data.tsv").getPath();
     CuppaPredictions cuppaPredictions = CuppaPredictions.fromTsv(CUPPA_VIS_DATA_TSV);
 
     @Test
