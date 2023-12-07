10d718745b45b1156dc4d9dfa28068577e033af5
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2023-12-07 18:30:08 +1100

    AUS-204: Added setMaxHeight(430) so that the cuppa vis image does not overflow to the next page. Renamed variable cuppaSummaryImage to cuppaVisPlotImage
    (cherry picked from commit 9bfa0cfb41dfdc86bad1e2163acb73d2eda51545)

--- orange/src/main/java/com/hartwig/hmftools/orange/report/chapters/CohortComparisonChapter.java
+++ orange/src/main/java/com/hartwig/hmftools/orange/report/chapters/CohortComparisonChapter.java
@@ -62,9 +62,10 @@ public class CohortComparisonChapter implements ReportChapter
 
     private void addCuppaVisPlot(@NotNull Document document)
     {
-        Image cuppaSummaryImage = Images.build(plotPathResolver.resolve(report.plots().cuppa2VisPlot()));
-        cuppaSummaryImage.setMaxWidth(740);
-        cuppaSummaryImage.setHorizontalAlignment(HorizontalAlignment.CENTER);
-        document.add(cuppaSummaryImage);
+        Image cuppaVisPlotImage = Images.build(plotPathResolver.resolve(report.plots().cuppa2VisPlot()));
+        cuppaVisPlotImage.setMaxWidth(740);
+        cuppaVisPlotImage.setMaxHeight(430);
+        cuppaVisPlotImage.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        document.add(cuppaVisPlotImage);
     }
 }
