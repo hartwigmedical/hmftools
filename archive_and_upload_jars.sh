f75603a769083ae34ebdd2f5e31e81ba91cf8772
Ned Leitch <ned.leitch@gmail.com>
2023-12-20 13:58:32 -0500

    DEV-3720 Move archival logic to script
    Believe shell quoting issues are causing problems with the logic when it
    is in `cloudbuild.yaml`.

---
+++ archive_and_upload_jars.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+[[ $# -ne 2 ]] && echo "Provide working directory and version string" && exit 1
+
+REMOTE="gs://pipeline5-jars/${1}.tar"
+gsutil stat ${REMOTE} 2>&1 >/dev/null
+[[ $? -eq 0 ]] && echo "ERROR: ${REMOTE} already exists" && exit 1
+
+tar -C ${1} --exclude='*-dependencies.jar' --exclude='*hmf-common*' --exclude='*-shaded.jar' -cvf ${1}/${2}.tar $(find ${1} -name '*.jar')
+gsutil cp target/${2}.tar ${REMOTE}
