26d567c8b02cc7e770f7422bcb97f6a836afb6bc
Ned Leitch <ned.leitch@gmail.com>
2023-12-20 14:48:47 -0500

    DEV-3720 Remove redundant -C for now

--- archive_and_upload_jars.sh
+++ archive_and_upload_jars.sh
@@ -8,5 +8,5 @@ REMOTE="gs://pipeline5-jars/${2}.tar"
 gsutil stat ${REMOTE} 2>&1 >/dev/null
 [[ $? -eq 0 ]] && echo "ERROR: ${REMOTE} already exists" && exit 1
 
-tar -C ${1} --exclude='*-dependencies.jar' --exclude='*hmf-common*' --exclude='*-shaded.jar' -cvf ${1}/${2}.tar $(find ${1} -name '*.jar')
+tar --exclude='*-dependencies.jar' --exclude='*hmf-common*' --exclude='*-shaded.jar' -cvf ${1}/${2}.tar $(find ${1} -name '*.jar')
 gsutil cp ${1}/${2}.tar ${REMOTE}
