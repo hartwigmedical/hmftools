84fd586a2cc07541c2a9d844b53e6fdcf69b8812
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2023-12-22 18:06:31 -0500

    ACTIN-46: minimal arg handling

--- crest/src/main/java/com/hartwig/hmftools/crest/CrestApplication.java
+++ crest/src/main/java/com/hartwig/hmftools/crest/CrestApplication.java
@@ -1,28 +1,76 @@
 package com.hartwig.hmftools.crest;
 
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.PURPLE_DIR_CFG;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.PURPLE_DIR_DESC;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAMPLE;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAMPLE_DESC;
+
 import static htsjdk.tribble.AbstractFeatureReader.getFeatureReader;
 
 import java.io.IOException;
 
+import com.hartwig.hmftools.common.purple.PurpleCommon;
+import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
+import com.hartwig.hmftools.common.utils.version.VersionInfo;
 import com.hartwig.hmftools.common.variant.AllelicDepth;
 import com.hartwig.hmftools.common.variant.VariantContextDecorator;
 import com.hartwig.hmftools.common.variant.VariantType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import htsjdk.tribble.AbstractFeatureReader;
 import htsjdk.tribble.readers.LineIterator;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFCodec;
 
-// Proof of concept - check if wts sample is from same patient
-// as wgs sample.
 public class CrestApplication
 {
+    private static final String RNA_SAMPLE = "rna";
+
+    private static final Logger LOGGER = LogManager.getLogger(CrestApplication.class);
 
     public static void main(String[] args) throws IOException
     {
-        String rnaAnnotatedGermlineVcf = args[0];
-        String rnaSample = args[1];
-        check(rnaAnnotatedGermlineVcf, rnaSample);
+        logVersion();
+
+        ConfigBuilder configBuilder = new ConfigBuilder();
+        addConfig(configBuilder);
+
+        if(!configBuilder.parseCommandLine(args))
+        {
+            configBuilder.logInvalidDetails();
+            System.exit(1);
+        }
+
+        try
+        {
+            String purpleDir = configBuilder.getValue(PURPLE_DIR_CFG);
+            String sampleId = configBuilder.getValue(SAMPLE);
+            String rnaSample = configBuilder.getValue(RNA_SAMPLE);
+            final String rnaAnnotatedGermlineVcf = PurpleCommon.purpleGermlineVcfFile(purpleDir, sampleId);
+            check(rnaAnnotatedGermlineVcf, rnaSample);
+        }
+        catch(Exception e)
+        {
+            LOGGER.error("Crest failed: {}", e.toString());
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void addConfig(final ConfigBuilder configBuilder)
+    {
+        configBuilder.addConfigItem(SAMPLE, SAMPLE_DESC);
+        configBuilder.addConfigItem(RNA_SAMPLE, "RNA sample ID");
+        configBuilder.addConfigItem(PURPLE_DIR_CFG, true, PURPLE_DIR_DESC);
+        //        ConfigUtils.addLoggingOptions(configBuilder);
+    }
+
+    public static void logVersion()
+    {
+        final VersionInfo version = new VersionInfo("crest.version");
+        LOGGER.info("Cest version: {}", version.version());
     }
 
     public static double check(String germlineVcf, String rnaSample) throws IOException
@@ -51,7 +99,7 @@ public class CrestApplication
             }
         }
         double fraction = total > 0 ? supported * 1D / total : 0D;
-        System.out.println("Supported: " + supported + " Total: " + total + " Fraction: " + fraction);
+        LOGGER.info("Supported: " + supported + " Total: " + total + " Fraction: " + fraction);
         return fraction;
     }
 }
