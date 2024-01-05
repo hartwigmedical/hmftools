3c5e6225f5dcba4a547ab625a9f77f853736f979
Charles Shale <shalecharles@gmail.com>
2024-01-05 13:06:48 +1100

    Gripss: add run time log

--- gripss/src/main/java/com/hartwig/hmftools/gripss/GripssApplication.java
+++ gripss/src/main/java/com/hartwig/hmftools/gripss/GripssApplication.java
@@ -4,6 +4,7 @@ import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource.REF_G
 import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource.loadRefGenome;
 import static com.hartwig.hmftools.common.gripss.RepeatMaskAnnotations.REPEAT_MASK_FILE;
 import static com.hartwig.hmftools.common.sv.SvVcfTags.BEALN;
+import static com.hartwig.hmftools.common.utils.PerformanceCounter.runTimeMinsStr;
 import static com.hartwig.hmftools.common.utils.sv.StartEndIterator.SE_END;
 import static com.hartwig.hmftools.common.utils.sv.StartEndIterator.SE_START;
 import static com.hartwig.hmftools.common.utils.sv.StartEndIterator.switchIndex;
@@ -115,9 +116,11 @@ public class GripssApplication
             System.exit(1);
         }
 
+        long startTimeMs = System.currentTimeMillis();
+
         processVcf(mConfig.VcfFile);
 
-        GR_LOGGER.info("Gripss run complete");
+        GR_LOGGER.info("Gripss complete, mins({})", runTimeMinsStr(startTimeMs));
     }
 
     private void processVcf(final String vcfFile)
