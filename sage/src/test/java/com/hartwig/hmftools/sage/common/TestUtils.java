665111ef8cf0004bf8459ece7c4bd188c98877a9
Matthew Cooper <matthew.cooper@pobox.com>
2024-01-09 15:44:03 +1100

    Sage: Adding TestUtil method for dumping vis files and some minor cleanup.

--- sage/src/test/java/com/hartwig/hmftools/sage/common/TestUtils.java
+++ sage/src/test/java/com/hartwig/hmftools/sage/common/TestUtils.java
@@ -1,5 +1,7 @@
 package com.hartwig.hmftools.sage.common;
 
+import static java.util.Map.entry;
+
 import static com.hartwig.hmftools.common.samtools.SamRecordUtils.MATE_CIGAR_ATTRIBUTE;
 import static com.hartwig.hmftools.common.samtools.SamRecordUtils.NUM_MUTATONS_ATTRIBUTE;
 import static com.hartwig.hmftools.common.test.GeneTestUtils.CHR_1;
@@ -7,18 +9,33 @@ import static com.hartwig.hmftools.common.test.MockRefGenome.generateRandomBases
 import static com.hartwig.hmftools.sage.SageConstants.DEFAULT_READ_CONTEXT_FLANK_SIZE;
 import static com.hartwig.hmftools.sage.SageConstants.MIN_CORE_DISTANCE;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 import com.google.common.collect.Lists;
+import com.hartwig.hmftools.common.gene.TranscriptData;
+import com.hartwig.hmftools.common.genome.bed.NamedBed;
 import com.hartwig.hmftools.common.genome.chromosome.HumanChromosome;
+import com.hartwig.hmftools.common.region.BaseRegion;
+import com.hartwig.hmftools.common.region.ChrBaseRegion;
+import com.hartwig.hmftools.common.test.MockRefGenome;
+import com.hartwig.hmftools.common.variant.hotspot.VariantHotspot;
+import com.hartwig.hmftools.sage.SageCallConfig;
 import com.hartwig.hmftools.sage.SageConfig;
-import com.hartwig.hmftools.sage.candidate.Candidate;
-import com.hartwig.hmftools.sage.evidence.ReadContextCounter;
-import com.hartwig.hmftools.sage.quality.QualityCalculator;
+import com.hartwig.hmftools.sage.bqr.BqrRecord;
 import com.hartwig.hmftools.sage.bqr.BqrRecordMap;
-
-import org.jetbrains.annotations.NotNull;
+import com.hartwig.hmftools.sage.candidate.Candidate;
+import com.hartwig.hmftools.sage.coverage.Coverage;
+import com.hartwig.hmftools.sage.evidence.FragmentLengths;
+import com.hartwig.hmftools.sage.evidence.ReadContextCounter;
+import com.hartwig.hmftools.sage.phase.PhaseSetCounter;
+import com.hartwig.hmftools.sage.pipeline.RegionResults;
+import com.hartwig.hmftools.sage.pipeline.RegionTask;
+import com.hartwig.hmftools.sage.quality.QualityCalculator;
+import com.hartwig.hmftools.sage.vcf.VcfWriter;
 
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SAMRecordSetBuilder;
@@ -212,5 +229,38 @@ public class TestUtils
         return record;
     }
 
+    public static void dumpVariantVis(final String sampleId, final List<SAMRecord> reads,
+            final ChrBaseRegion region, final MockRefGenome refGenome, final String fullVisOutputDir)
+    {
+        int taskId = 0;
 
+        SamSlicerInterface samSlicer = (final Consumer<SAMRecord> consumer) -> reads.forEach(consumer);
+        SamSlicerFactory samSlicerFactory = new SamSlicerFactory();
+        samSlicerFactory.addSamSlicer(sampleId, samSlicer);
+
+        VcfWriter vcfWriter = null;
+        RegionResults results = new RegionResults(vcfWriter);
+
+        SageCallConfig config = new SageCallConfig(fullVisOutputDir);
+        config.TumorIds.add(sampleId);
+
+        List<BqrRecord> bqrRecords = Lists.newArrayList();
+        Map<String, BqrRecordMap> qualityRecalibrationMap = Map.ofEntries(
+                entry(sampleId, new BqrRecordMap(bqrRecords))
+        );
+
+        Collection<NamedBed> panel = Lists.newArrayList();
+        Coverage coverage = new Coverage(config.TumorIds, panel, config.Common);
+
+        List<VariantHotspot> hotspots = Lists.newArrayList();
+        List<BaseRegion> panelRegions = Lists.newArrayList();
+        List<BaseRegion> highConfidenceRegions = Lists.newArrayList();
+        List<TranscriptData> transcripts = null;
+        PhaseSetCounter phaseSetCounter = null;
+        FragmentLengths fragmentLengths = null;
+
+        RegionTask regionTask = new RegionTask(taskId, region, results, config, refGenome, hotspots, panelRegions, transcripts,
+                highConfidenceRegions, qualityRecalibrationMap, phaseSetCounter, coverage, samSlicerFactory, fragmentLengths);
+        regionTask.run();
+    }
 }
