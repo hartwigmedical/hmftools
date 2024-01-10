1a5ba0f01770a43833998b930a1939c6ef8dc7f7
Charles Shale <shalecharles@gmail.com>
2024-01-10 14:28:10 +1100

    Purple: updated HRD cut-offs after final panel validation

--- purple/src/main/java/com/hartwig/hmftools/purple/hrd/HrdDetection.java
+++ purple/src/main/java/com/hartwig/hmftools/purple/hrd/HrdDetection.java
@@ -47,9 +47,9 @@ public class HrdDetection
     private static final int DEFAULT_SEGMENT_BREAK_MIN_LENGTH = 100_000;
     private static final int DEFAULT_SEGMENT_BREAK_LENGTH = 3_000_000;
 
-    private static final int HRD_STATUS_LOH_CUTOFF = 8;
+    private static final int HRD_STATUS_LOH_CUTOFF = 7;
     private static final int HRD_STATUS_IMBALANCE_CUTOFF = 2;
-    private static final int HRD_STATUS_SEGMENT_BREAKS_CUTOFF = 29;
+    private static final int HRD_STATUS_SEGMENT_BREAKS_CUTOFF = 24;
 
     private static final double MAX_COPY_NUM_DIFF = 0.5;
     private static final double MAX_COPY_NUM_DIFF_PERC = 0.2;
