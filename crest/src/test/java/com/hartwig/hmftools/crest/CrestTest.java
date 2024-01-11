7a9451c0c365a0466f640ba200517509aeaaf1d7
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-01-11 16:40:51 +0100

    ACTIN-46: do not write check files in tests
    validate check directly

--- crest/src/test/java/com/hartwig/hmftools/crest/CrestTest.java
+++ crest/src/test/java/com/hartwig/hmftools/crest/CrestTest.java
@@ -1,17 +1,14 @@
 package com.hartwig.hmftools.crest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.IOException;
 
 import com.google.common.io.Resources;
 
-import org.jetbrains.annotations.NotNull;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 public class CrestTest
 {
@@ -24,33 +21,22 @@ public class CrestTest
 
     private static final double EPSILON = 1E-8;
 
-    @Rule
-    public TemporaryFolder tempFolder = new TemporaryFolder();
-
     @Test
     public void shouldFlagMismatchedSample() throws IOException
     {
-        String expectedFile = tempFolder.getRoot().getPath() + File.separator + "tumor_sample.CrestCheckFailed";
-        cleanCheckFile(expectedFile);
 
-        CrestAlgo crestAlgo = new CrestAlgo(PURPLE_DIR, tempFolder.getRoot().getPath(), WGS_SAMPLE, RNA_SAMPLE,
-                10, 1, 0.9, false);
-        crestAlgo.run();
-        validateCheckFile(expectedFile);
+        CrestAlgo crestAlgo = new CrestAlgo(PURPLE_DIR, null, WGS_SAMPLE, RNA_SAMPLE,
+                10, 1, 0.9, true);
+        assertFalse(crestAlgo.crestCheck(VCF_FILE));
     }
 
     @Test
     public void shouldAcceptMatchedSample() throws IOException
     {
-        String expectedFile = tempFolder.getRoot().getPath() + File.separator + "tumor_sample.CrestCheckSucceeded";
-        cleanCheckFile(expectedFile);
-
         // rather than cook up another test file, just lower the threshold
-        CrestAlgo crestAlgo = new CrestAlgo(PURPLE_DIR, tempFolder.getRoot().getPath(), WGS_SAMPLE, RNA_SAMPLE,
-                10, 1, 0.3, false);
-        crestAlgo.run();
-
-        validateCheckFile(expectedFile);
+        CrestAlgo crestAlgo = new CrestAlgo(PURPLE_DIR, null, WGS_SAMPLE, RNA_SAMPLE,
+                10, 1, 0.3, true);
+        assertTrue(crestAlgo.crestCheck(VCF_FILE));
     }
 
     @Test(expected = RuntimeException.class)
@@ -77,24 +63,5 @@ public class CrestTest
                 10, 1, 0.9, true);
         double x = crestAlgo.computeRnaSupportRatio(MINIMAL_VCF_FILE);
         assertEquals(0.5, x, EPSILON);
-    }
-
-    private void cleanCheckFile(@NotNull String filename)
-    {
-        File expectedFile = new File(filename);
-        if(expectedFile.exists())
-        {
-            System.out.println("Deleting " + expectedFile.getAbsolutePath());
-            expectedFile.delete();
-        }
-    }
-
-    private void validateCheckFile(@NotNull String filename)
-    {
-        File expectedFile = new File(filename);
-        if(!expectedFile.exists())
-        {
-            fail("Expected " + expectedFile.getAbsolutePath() + " to exist but file not found");
-        }
     }
 }
