307ed219973b876d0ade743afbec36a26a19ae4d
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-01-11 16:32:39 +1100

    Cuppa: Updated tests

--- cuppa/src/main/python/pycuppa/cuppa/classifier/test/test_cuppa_classifier.py
+++ cuppa/src/main/python/pycuppa/cuppa/classifier/test/test_cuppa_classifier.py
@@ -23,8 +23,24 @@ class TestCuppaClassifier:
 
 class TestMissingFeaturesHandler:
 
-    def test_one_sample_without_rna_data(self):
-        #X = FeatureLoaderNew(MockInputData.path_tsv_new_format_colo, verbose=True).load()
+    def test_missing_rna_features_are_filled_with_zeroes(self):
+        X = pd.DataFrame(
+            [[1, 1, 1, 1]],
+            columns=["gen_pos.feat_1", "snv96.feat_1", "event.feat_1", "sig.feat_1"],
+            index=["sample_1"]
+        )
+
+        present_features = ["gen_pos.feat_1", "snv96.feat_1", "event.feat_1", "sig.feat_1"]
+        missing_features = ["gen_pos.feat_2", "snv96.feat_2", "event.feat_2", "sig.feat_2"]
+
+        required_features = present_features + missing_features
+
+        handler = MissingFeaturesHandler(X, required_features=required_features)
+        X_filled = handler.fill_missing()
+
+        assert all(X_filled[missing_features].iloc[0] == 0)
+
+    def test_missing_rna_features_are_filled_with_na(self):
         X = pd.DataFrame(
             [[1, 1, 1, 1]],
             columns=["gen_pos.feat_1", "snv96.feat_1", "event.feat_1", "sig.feat_1"],
@@ -32,10 +48,10 @@ class TestMissingFeaturesHandler:
         )
 
         required_dna_features = [
-            "gen_pos.feat_1",   "gen_pos.feat_2",
-            "snv96.feat_1",     "snv96.feat_2",
-            "event.feat_1",     "event.feat_2",
-            "sig.feat_1",       "sig.feat_2",
+            "gen_pos.feat_1",
+            "snv96.feat_1",
+            "event.feat_1",
+            "sig.feat_1",
         ]
 
         required_rna_features = [
@@ -45,12 +61,6 @@ class TestMissingFeaturesHandler:
 
         required_features = required_dna_features + required_rna_features
 
-        self = MissingFeaturesHandler(X, required_features=required_features)
-
-        self.fill_missing()
-        self.X
-
-        self.required_features
-
-
-        pass
+        handler = MissingFeaturesHandler(X, required_features=required_features)
+        X_filled = handler.fill_missing()
+        assert X_filled[required_rna_features].isna().iloc[0].all()
