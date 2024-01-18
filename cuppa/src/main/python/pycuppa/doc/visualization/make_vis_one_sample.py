bb71da145d4f6238c9c6f2a6ca41d2bda239f334
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-01-18 14:55:47 +1100

    Cuppa: Updated script to make visualization png for docs

--- cuppa/src/main/python/pycuppa/doc/visualization/make_vis_one_sample.py
+++ cuppa/src/main/python/pycuppa/doc/visualization/make_vis_one_sample.py
@@ -1,13 +1,10 @@
 import importlib_resources as impresources
+from cuppa.tests.mock_data import MockCvOutput
 
-import importlib
-from cuppa.misc.mock_data import MockCvOutput
-
-DOC_DIR = importlib.resources.files("doc")
+DOC_DIR = impresources.files("doc")
 
 MockCvOutput.predictions_for_vis.plot(
     sample_id="Breast_TNBC",
-    vis_data_out_path=DOC_DIR/"visualization/cuppa_vis_data.tsv",
     plot_path=DOC_DIR/"visualization/cuppa_vis.png"
 )
 
