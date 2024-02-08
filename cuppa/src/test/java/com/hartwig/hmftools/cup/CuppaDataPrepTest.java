27e8dec84ab15d7845194b4cae4846fca8b69e8d
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-02-08 21:02:55 +1100

    AUS-223: Added test and test data for one sample feature extraction

---
+++ cuppa/src/test/java/com/hartwig/hmftools/cup/CuppaDataPrepTest.java
@@ -0,0 +1,42 @@
+package com.hartwig.hmftools.cup;
+import static com.hartwig.hmftools.cup.common.CupConstants.APP_NAME;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import com.google.common.io.Resources;
+import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
+import com.hartwig.hmftools.cup.prep.CuppaDataPrep;
+import com.hartwig.hmftools.cup.prep.PrepConfig;
+
+import org.junit.Test;
+
+public class CuppaDataPrepTest
+{
+    @Test
+    public void canExtractFeaturesFromOneSample()
+    {
+        ConfigBuilder configBuilder = new ConfigBuilder(APP_NAME);
+        PrepConfig.registerConfig(configBuilder);
+
+        String SAMPLE_DATA_DIR = Resources.getResource("pipeline_output/prostate_sample/").getPath();
+
+        String[] args = {
+                "-sample", "prostate_sample",
+                "-categories", "DNA",
+                "-ref_genome_version", "V37",
+                "-sample_data_dir", SAMPLE_DATA_DIR,
+                "-output_dir", "/Users/lnguyen/Desktop/test_output/"
+        };
+
+        configBuilder.checkAndParseCommandLine(args);
+
+        CuppaDataPrep cuppaDataPrep = new CuppaDataPrep(configBuilder);
+        cuppaDataPrep.run();
+
+        File outputFile = new File(cuppaDataPrep.mSampleDataWriter.getOutputPathSingleSample());
+        assertTrue(outputFile.exists());
+        //outputFile.delete();
+    }
+}
