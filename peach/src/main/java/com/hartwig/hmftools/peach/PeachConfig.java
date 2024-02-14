a9e7e8903c07c872ec69105124f371a4ef126cb5
David Koetsier <david.f.koetsier@gmail.com>
2024-02-14 10:42:59 +0100

    DEV-2919: Add arguments for drug functionality

--- peach/src/main/java/com/hartwig/hmftools/peach/PeachConfig.java
+++ peach/src/main/java/com/hartwig/hmftools/peach/PeachConfig.java
@@ -11,16 +11,22 @@ public class PeachConfig
     public final String vcfFile;
     public final String sampleName;
     public final String haplotypesFile;
+    public final String drugsFile;
+    public final String functionalityFile;
     public final String outputDir;
     private static final String VCF_FILE = "vcf_file";
     private static final String SAMPLE_NAME = "sample_name";
     private static final String HAPLOTYPES_FILE = "haplotypes_file";
+    private static final String DRUGS_FILE = "drugs_file";
+    private static final String FUNCTIONALITY_FILE = "functionality_file";
 
     public PeachConfig(final ConfigBuilder configBuilder)
     {
         vcfFile = configBuilder.getValue(VCF_FILE);
         haplotypesFile = configBuilder.getValue(HAPLOTYPES_FILE);
         sampleName = configBuilder.getValue(SAMPLE_NAME);
+        drugsFile = configBuilder.hasValue(DRUGS_FILE) ? configBuilder.getValue(DRUGS_FILE) : null;
+        functionalityFile = configBuilder.hasValue(FUNCTIONALITY_FILE) ? configBuilder.getValue(FUNCTIONALITY_FILE) : null;
 
         outputDir = parseOutputDir(configBuilder);
     }
@@ -35,7 +41,8 @@ public class PeachConfig
         configBuilder.addPath(VCF_FILE, true, "VCF input file");
         configBuilder.addPath(HAPLOTYPES_FILE, true, "Haplotype config file");
         configBuilder.addConfigItem(SAMPLE_NAME, true, "Name of sample in VCF to call haplotypes for");
-
+        configBuilder.addPath(DRUGS_FILE, false, "Config file of relevant drugs");
+        configBuilder.addPath(FUNCTIONALITY_FILE, false, "Config file for haplotype functionality");
         addOutputDir(configBuilder);
         addLoggingOptions(configBuilder);
     }
--- peach/src/test/java/com/hartwig/hmftools/peach/PeachConfigTest.java
+++ peach/src/test/java/com/hartwig/hmftools/peach/PeachConfigTest.java
@@ -7,39 +7,85 @@ import static org.junit.Assert.assertTrue;
 
 import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 public class PeachConfigTest
 {
     @Test
-    public void testCommandLineArguments()
+    public void testMinimalCommandLineArguments()
     {
         String vcfFile = getTestResourcePath("variants.vcf.gz");
         String haplotypesFile = getTestResourcePath("haplotypes.complicated.37.tsv");
         String sampleName = "FAKENAME";
         String outputDir = "/path/to/output";
-        ConfigBuilder configBuilder = new ConfigBuilder("Peach");
-        PeachConfig.addOptions(configBuilder);
+
         String[] args = {
-                "-vcf_file", vcfFile, 
+                "-vcf_file", vcfFile,
                 "-haplotypes_file", haplotypesFile,
-                "-sample_name", sampleName, 
+                "-sample_name", sampleName,
                 "-output_dir", outputDir
         };
-        configBuilder.checkAndParseCommandLine(args);
-        PeachConfig config = new PeachConfig(configBuilder);
-        
+
+        PeachConfig config = constructPeachConfigFromArgs(args);
+
         assertEquals(vcfFile, config.vcfFile);
         assertEquals(haplotypesFile, config.haplotypesFile);
         assertEquals(sampleName, config.sampleName);
-        assertEquals("/path/to/output/", config.outputDir);
-        
+        assertEquals(outputDir + "/", config.outputDir);
+
         assertTrue(config.isValid());
-        
+
         assertEquals("/path/to/output/FAKENAME.peach.events.tsv", config.getEventsOutputPath());
         assertEquals("/path/to/output/FAKENAME.peach.gene.events.tsv", config.getEventsPerGeneOutputPath());
         assertEquals("/path/to/output/FAKENAME.peach.haplotypes.all.tsv", config.getAllHaplotypeCombinationsOutputPath());
         assertEquals("/path/to/output/FAKENAME.peach.haplotypes.best.tsv", config.getBestHaplotypeCombinationsOutputPath());
         assertEquals("/path/to/output/FAKENAME.peach.qc.tsv", config.getQcStatusOutputPath());
+    }
+
+    @Test
+    public void testMaximalCommandLineArguments()
+    {
+        String vcfFile = getTestResourcePath("variants.vcf.gz");
+        String haplotypesFile = getTestResourcePath("haplotypes.complicated.37.tsv");
+        String drugsFile = getTestResourcePath("drugs.tsv");
+        String functionalityFile = getTestResourcePath("functionality.tsv");
+        String sampleName = "FAKENAME";
+        String outputDir = "/path/to/output";
+
+        String[] args = {
+                "-vcf_file", vcfFile,
+                "-haplotypes_file", haplotypesFile,
+                "-sample_name", sampleName,
+                "-output_dir", outputDir,
+                "-drugs_file", drugsFile,
+                "-functionality_file", functionalityFile
+        };
+
+        PeachConfig config = constructPeachConfigFromArgs(args);
+
+        assertEquals(vcfFile, config.vcfFile);
+        assertEquals(haplotypesFile, config.haplotypesFile);
+        assertEquals(sampleName, config.sampleName);
+        assertEquals(outputDir + "/", config.outputDir);
+        assertEquals(drugsFile, config.drugsFile);
+        assertEquals(functionalityFile, config.functionalityFile);
+
+        assertTrue(config.isValid());
+
+        assertEquals("/path/to/output/FAKENAME.peach.events.tsv", config.getEventsOutputPath());
+        assertEquals("/path/to/output/FAKENAME.peach.gene.events.tsv", config.getEventsPerGeneOutputPath());
+        assertEquals("/path/to/output/FAKENAME.peach.haplotypes.all.tsv", config.getAllHaplotypeCombinationsOutputPath());
+        assertEquals("/path/to/output/FAKENAME.peach.haplotypes.best.tsv", config.getBestHaplotypeCombinationsOutputPath());
+        assertEquals("/path/to/output/FAKENAME.peach.qc.tsv", config.getQcStatusOutputPath());
+    }
+
+    @NotNull
+    private static PeachConfig constructPeachConfigFromArgs(final String[] args)
+    {
+        ConfigBuilder configBuilder = new ConfigBuilder("Peach");
+        PeachConfig.addOptions(configBuilder);
+        configBuilder.checkAndParseCommandLine(args);
+        return new PeachConfig(configBuilder);
     }
 }
