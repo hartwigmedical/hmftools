f5fadba3223ede369835d6a38543684c66899f8f
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-02-19 09:54:56 +1100

    Cuppa: Readme now uses full COLO829v003T sample for the example visualization

--- cuppa/src/main/python/pycuppa/doc/visualization/make_vis_one_sample.py
+++ cuppa/src/main/python/pycuppa/doc/visualization/make_vis_one_sample.py
@@ -1,10 +1,23 @@
+from cuppa.sample_data.cuppa_features import FeatureLoaderNew
+from cuppa.classifier.cuppa_classifier import CuppaClassifier
+from cuppa.constants import DEFAULT_CUPPA_CLASSIFIER_PATH
+from cuppa.visualization.visualization import CuppaVisDataBuilder, CuppaVisPlotter
+
 import importlib_resources as impresources
-from cuppa.tests.mock_data import MockVisData
+INPUT_PATH = impresources.files("cuppa") / "resources/mock_data/input_data/new_format/COLO829v003T.cuppa_data.tsv.gz"
+PLOT_PATH = impresources.files("doc") / "visualization/COLO829v003T.cuppa.vis.png"
 
-DOC_DIR = impresources.files("doc")
 
-MockVisData.predictions.plot(
-    sample_id="Breast_TNBC",
-    plot_path=DOC_DIR/"visualization/cuppa_vis.png"
-)
+loader = FeatureLoaderNew(str(INPUT_PATH), sample_id="COLO829v003T")
+features = loader.load()
 
+classifier = CuppaClassifier.from_file(DEFAULT_CUPPA_CLASSIFIER_PATH)
+features = classifier.fill_missing_cols(features)
+
+predictions = classifier.predict(features)
+
+builder = CuppaVisDataBuilder(predictions)
+vis_data = builder.build()
+
+plotter = CuppaVisPlotter(vis_data, plot_path=str(PLOT_PATH))
+plotter.plot()
