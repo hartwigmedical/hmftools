74e12bfb19f0af2b65e10b07286ee96e76b9763c
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2024-04-08 12:32:19 +1000

    purple: improve circos plots: 1. change colour of indels to allow distinguishing indel types. 2. make link sizes dependent on the size of SV. 3. make cobalt ratio points proportional to the log of number of windows.

--- hmf-common/src/main/java/com/hartwig/hmftools/common/circos/CircosINDELWriter.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/circos/CircosINDELWriter.java
@@ -1,15 +1,11 @@
 package com.hartwig.hmftools.common.circos;
 
-import java.io.File;
+import static com.hartwig.hmftools.common.circos.CircosFileWriter.writeCircosFile;
+
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Collection;
-import java.util.function.Function;
 
-import com.google.common.collect.Lists;
 import com.hartwig.hmftools.common.variant.VariantContextDecorator;
-
-import org.jetbrains.annotations.NotNull;
 
 public final class CircosINDELWriter
 {
@@ -19,31 +15,35 @@ public final class CircosINDELWriter
         writeCircosFile(filePath, values, CircosINDELWriter::transformPosition);
     }
 
-    private static <T> void writeCircosFile(final String filePath, Collection<T> values, Function<T, String> toStringFunction) throws IOException
-    {
-        final Collection<String> lines = Lists.newArrayList();
-        lines.add(header());
-        values.stream().map(toStringFunction).forEach(lines::add);
-        Files.write(new File(filePath).toPath(), lines);
-    }
-
-    private static String header()
-    {
-        return "#chromosome\tstart\tend\tvalue";
-    }
-
     private static String transformPosition(final VariantContextDecorator position)
     {
         return CircosFileWriter.transformPosition(position, CircosINDELWriter::color);
     }
 
+    //    DEL REPEAT>=4  ORANGE
+    //    DEL MH>2 RED
+    //    OTHER DEL YELLOW
+    //    INS  GREEN
     private static String color(final VariantContextDecorator variant)
     {
         if(variant.alt().length() > variant.ref().length())
         {
-            return "vdyellow";
+            // insertion
+            return "green";
         }
 
-        return "red";
+        if(variant.repeatCount() >= 4)
+        {
+            // del repeat >= 4
+            return "orange";
+        }
+
+        if(!variant.microhomology().isEmpty())
+        {
+            // microhomology
+            return "vdred";
+        }
+
+        return "vdyellow";
     }
 }
--- hmf-common/src/main/java/com/hartwig/hmftools/common/circos/CircosLinkWriter.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/circos/CircosLinkWriter.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import com.hartwig.hmftools.common.sv.StructuralVariant;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class CircosLinkWriter
 {
@@ -31,14 +32,25 @@ public final class CircosLinkWriter
         // SGLs are filtered out at this stage
         assert startChromosome != null && endChromosome != null;
 
-        return new StringJoiner("\t").add(CircosFileWriter.circosContig(startChromosome))
+        String bezierRadiusVal = bezierRadius(variant);
+
+        StringJoiner stringJoiner = new StringJoiner("\t").add(CircosFileWriter.circosContig(startChromosome))
                 .add(String.valueOf(variant.position(true)))
                 .add(String.valueOf(variant.position(true)))
                 .add(CircosFileWriter.circosContig(endChromosome))
                 .add(String.valueOf(variant.position(false)))
-                .add(String.valueOf(variant.position(false)))
-                .add("color=" + color(variant))
-                .toString();
+                .add(String.valueOf(variant.position(false)));
+
+        if(bezierRadiusVal == null)
+        {
+            stringJoiner.add("color=" + color(variant));
+        }
+        else
+        {
+            stringJoiner.add("color=" + color(variant) + ",bezier_radius=" + bezierRadiusVal);
+        }
+
+        return stringJoiner.toString();
     }
 
     @NotNull
@@ -59,5 +71,49 @@ public final class CircosLinkWriter
             default:
                 return "purple";
         }
+    }
+
+    // calculate the bezier radius of the link.
+    // it is proportional to the log10 size of the SV if it is within same chromosome
+    @Nullable
+    static String bezierRadius(final StructuralVariant variant)
+    {
+        String startChromosome = variant.chromosome(true);
+        String endChromosome = variant.chromosome(false);
+
+        // SGLs are filtered out at this stage
+        if (startChromosome == null || !startChromosome.equals(endChromosome))
+        {
+            return null;
+        }
+
+        int length;
+
+        Integer start = variant.position(true);
+        Integer end = variant.position(false);
+
+        if(start != null && end != null)
+        {
+            // end is inclusive
+            length = Math.max(end - start + 1, 1);
+        }
+        else
+        {
+            return null;
+        }
+
+        // between 0.1 to 0.35
+        // which is scaled by log scale
+        // we set 10m to be max, which is 1e7
+        // the bez radius would be 0.1 when length is 10m, and 0.35 when length is 1
+
+        double maxBezRadius = 0.35; // this value is the same as in the circos.template link section (radius)
+        double minBezRadius = 0.1; // this value is the same as in the circos.template link section (bezier radius)
+        double log10Max = 7.0;
+        double r = Math.min(Math.log10(length), log10Max);
+
+        r = maxBezRadius - r / log10Max * (maxBezRadius - minBezRadius);
+
+        return String.format("%.3fr", r);
     }
 }
