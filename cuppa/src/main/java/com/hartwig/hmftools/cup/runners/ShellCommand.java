d7249ac3e7ed6506056d6baad2bdfb5bac6846bc
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-05-01 12:54:50 +1000

    Cuppa: ShellCommand now captures stdout using a separate thread

--- cuppa/src/main/java/com/hartwig/hmftools/cup/runners/ShellCommand.java
+++ cuppa/src/main/java/com/hartwig/hmftools/cup/runners/ShellCommand.java
@@ -3,9 +3,11 @@ package com.hartwig.hmftools.cup.runners;
 import static com.hartwig.hmftools.cup.CuppaConfig.CUP_LOGGER;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.Nullable;
@@ -17,24 +19,52 @@ public abstract class ShellCommand
     @Nullable
     private Level mLogLevel = Level.INFO;
     private boolean mShowCommand = false;
+    private long mTimeout = Long.MAX_VALUE;
+    private TimeUnit mTimeoutUnit = TimeUnit.SECONDS;
 
-    private int mExitCode = 0;
+    private int mExitCode;
     private List<String> mStdout;
+    private boolean mComplete = false;
+
+    private static final String THREAD_NAME = "bash";
 
     public ShellCommand(ProcessBuilder processBuilder)
     {
         mProcessBuilder = processBuilder;
     }
 
+    private void checkComplete()
+    {
+        if(mComplete)
+            throw new IllegalStateException("Cannot set options or run a command that is already complete");
+    }
+
     public ShellCommand logLevel(@Nullable Level logLevel)
     {
+        checkComplete();
         mLogLevel = logLevel;
         return this;
     }
 
     public ShellCommand showCommand()
     {
+        checkComplete();
         mShowCommand = true;
+        return this;
+    }
+
+    public ShellCommand timeout(long timeout, TimeUnit unit)
+    {
+        checkComplete();
+        mTimeout = timeout;
+        mTimeoutUnit = unit;
+        return this;
+    }
+
+    public ShellCommand timeout(long timeout)
+    {
+        checkComplete();
+        mTimeout = timeout;
         return this;
     }
 
@@ -42,42 +72,66 @@ public abstract class ShellCommand
 
     public ShellCommand run()
     {
+        checkComplete();
+
         try
         {
             if(mShowCommand)
-            {
                 CUP_LOGGER.info("Running command: " + this);
-            }
 
             Process process = mProcessBuilder
                     .redirectErrorStream(true) // Merge stdout and stderr
                     .start();
 
-            mStdout = new ArrayList<>();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while( (line = reader.readLine()) != null )
-            {
-                mStdout.add(line);
-                if(mLogLevel != null)
-                {
-                    CUP_LOGGER.log(mLogLevel, line);
-                }
-            }
+            mStdout = captureStdout(process, mLogLevel);
 
-            mExitCode = process.waitFor();
+            mComplete = process.waitFor(mTimeout, mTimeoutUnit);
+            process.destroy();
+
+            mExitCode = process.exitValue();
             if(mExitCode > 0)
-            {
                 throw new RuntimeException();
-            }
         }
-        catch(Exception e)
+        catch(InterruptedException | IllegalThreadStateException e)
         {
-            CUP_LOGGER.error("command({}) failed with exit code {}: {}", mProcessBuilder.command(), String.valueOf(mExitCode), e);
+            CUP_LOGGER.error("Failed to run command({}) due to time out after {} {}", mProcessBuilder.command(), mTimeout, mTimeoutUnit.toString().toLowerCase());
+            System.exit(mExitCode);
+        }
+        catch(IOException | RuntimeException e)
+        {
+
+            CUP_LOGGER.error("Failed to run command({}) with exit code({})", mProcessBuilder.command(), String.valueOf(mExitCode));
             System.exit(mExitCode);
         }
 
         return this;
+    }
+
+    private static List<String> captureStdout(Process process, Level logLevel)
+    {
+        List<String> stdout = new ArrayList<>();
+
+        // Use a separate thread to avoid reader blocking the main thread (which would cause the process to never finish)
+        Thread thread = new Thread(() -> {
+            try
+            {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+                String line;
+                while( (line = reader.readLine()) != null )
+                {
+                    stdout.add(line);
+                    if(logLevel != null)
+                        CUP_LOGGER.log(logLevel, line);
+                }
+            }
+            catch(IOException ignore){}
+        });
+
+        thread.setName(THREAD_NAME);
+        thread.start();
+
+        return stdout;
     }
 
     public List<String> getStdout(){ return mStdout; }
