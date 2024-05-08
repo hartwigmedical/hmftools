665e255492dac673149c936dc0b5104d5997659e
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-05-08 15:45:02 +1000

    Cuppa: Added constructors to BashCommand

--- cuppa/src/main/java/com/hartwig/hmftools/cup/cli/BashCommand.java
+++ cuppa/src/main/java/com/hartwig/hmftools/cup/cli/BashCommand.java
@@ -1,5 +1,7 @@
 package com.hartwig.hmftools.cup.cli;
 
+import java.util.StringJoiner;
+
 public class BashCommand extends ShellCommand
 {
     private final String mCommand;
@@ -8,6 +10,21 @@ public class BashCommand extends ShellCommand
     {
         super(new ProcessBuilder("bash", "-c", command));
         mCommand = command;
+    }
+
+    private static String formCommand(String[] args)
+    {
+        StringJoiner joiner = new StringJoiner(" ");
+
+        for(String arg : args)
+            joiner.add(arg);
+
+        return joiner.toString();
+    }
+
+    public BashCommand(String... args)
+    {
+        this(formCommand(args));
     }
 
     @Override
