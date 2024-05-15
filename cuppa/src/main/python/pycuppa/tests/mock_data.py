a14efac3c2a77b70c7a4f8a4150f66637013b8ee
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-05-15 16:27:37 +1000

    Cuppa: In mock_data.py, removed uses of cache_class_property and added type hints to class attributes

--- cuppa/src/main/python/pycuppa/tests/mock_data.py
+++ cuppa/src/main/python/pycuppa/tests/mock_data.py
@@ -5,20 +5,13 @@ from cuppa.constants import MOCK_DATA_DIR
 from cuppa.classifier.cuppa_classifier import CuppaClassifier
 from cuppa.classifier.cuppa_prediction import CuppaPrediction, CuppaPredSummary
 from cuppa.performance.performance_stats import PerformanceStats
-from cuppa.misc.cached_class_property import cached_class_property
 from cuppa.visualization.visualization import CuppaVisData
-
-"""
-@cached_class_property is used throughout this module so that data is: 
-- only loaded when actually needed (lazy loading)
-- immutable 
-"""
 
 
 class MockInputData:
-    path_tsv_new_format = os.path.join(MOCK_DATA_DIR, "input_data/new_format/COLO829v003T.cuppa_data.tsv.gz")
-    dir_old_format = os.path.join(MOCK_DATA_DIR, "input_data/old_format/")
-    dir_new_format = os.path.join(MOCK_DATA_DIR, "input_data/new_format/cohort/")
+    path_tsv_new_format: str = os.path.join(MOCK_DATA_DIR, "input_data/new_format/COLO829v003T.cuppa_data.tsv.gz")
+    dir_old_format: str = os.path.join(MOCK_DATA_DIR, "input_data/old_format/")
+    dir_new_format: str = os.path.join(MOCK_DATA_DIR, "input_data/new_format/cohort/")
 
 
 class MockTrainingData:
@@ -27,21 +20,10 @@ class MockTrainingData:
     path_metadata = os.path.join(MOCK_DATA_DIR, "training_data/metadata.tsv")
     path_fusion_overrides = os.path.join(MOCK_DATA_DIR, "training_data/fusion_overrides.tsv")
 
-    @cached_class_property
-    def X(self) -> pd.DataFrame:
-        return pd.read_table(self.path_features, index_col=0)
-
-    @cached_class_property
-    def metadata(self) -> pd.DataFrame:
-        return pd.read_table(self.path_metadata, index_col=0)
-
-    @cached_class_property
-    def y(self) -> pd.Series:
-        return self.metadata["CancerSubtype"]
-
-    @cached_class_property
-    def y_split(self) -> pd.Series:
-        return self.y + "__" + self.metadata["RnaReadLength"].astype(str)
+    X: pd.DataFrame = pd.read_table(path_features, index_col=0)
+    metadata: pd.DataFrame = pd.read_table(path_metadata, index_col=0)
+    y: pd.Series = metadata["CancerSubtype"]
+    y_split: pd.Series = y + "__" + metadata["RnaReadLength"].astype(str)
 
 
 def _get_probs_per_clf(predictions: CuppaPrediction) -> dict[str, pd.DataFrame]:
@@ -63,51 +45,30 @@ def _get_probs_per_clf(predictions: CuppaPrediction) -> dict[str, pd.DataFrame]:
 
 class MockCvOutput:
 
-    path_predictions = os.path.join(MOCK_DATA_DIR, "cv_output/predictions.tsv.gz")
-    path_pred_summ = os.path.join(MOCK_DATA_DIR, "cv_output/pred_summ.tsv")
-    path_performance = os.path.join(MOCK_DATA_DIR, "cv_output/performance.tsv")
+    path_predictions: str = os.path.join(MOCK_DATA_DIR, "cv_output/predictions.tsv.gz")
+    path_pred_summ: str = os.path.join(MOCK_DATA_DIR, "cv_output/pred_summ.tsv")
+    path_performance: str = os.path.join(MOCK_DATA_DIR, "cv_output/performance.tsv")
 
-    @cached_class_property
-    def predictions(self) -> CuppaPrediction:
-        return CuppaPrediction.from_tsv(self.path_predictions)
-
-    @cached_class_property
-    def pred_summ(self) -> CuppaPredSummary:
-        return CuppaPredSummary.from_tsv(self.path_pred_summ)
-
-    @cached_class_property
-    def performance(self) -> pd.DataFrame:
-        return PerformanceStats.from_tsv(self.path_performance)
-
-    @cached_class_property
-    def probs_per_clf(self) -> dict[str, pd.DataFrame]:
-        return _get_probs_per_clf(self.predictions)
+    predictions = CuppaPrediction.from_tsv(path_predictions)
+    pred_summ = CuppaPredSummary.from_tsv(path_pred_summ)
+    performance = PerformanceStats.from_tsv(path_performance)
+    probs_per_clf: dict[str, pd.DataFrame] = _get_probs_per_clf(predictions)
 
 
 class MockCuppaClassifier:
 
-    path_classifier = os.path.join(MOCK_DATA_DIR, "final_model/cuppa_classifier.pickle.gz")
-    path_predictions = os.path.join(MOCK_DATA_DIR, "final_model/predictions.tsv.gz")
+    path_classifier: str = os.path.join(MOCK_DATA_DIR, "final_model/cuppa_classifier.pickle.gz")
+    path_predictions: str = os.path.join(MOCK_DATA_DIR, "final_model/predictions.tsv.gz")
 
-    @cached_class_property
-    def cuppa_classifier(self) -> CuppaClassifier:
-        cuppa_classifier = CuppaClassifier.from_file(self.path_classifier)
-        cuppa_classifier.cv_performance = MockCvOutput.performance
-        return cuppa_classifier
+    cuppa_classifier: CuppaClassifier = CuppaClassifier.from_file(path_classifier)
+    cuppa_classifier.cv_performance = MockCvOutput.performance
 
-    @cached_class_property
-    def predictions(self) -> CuppaPrediction:
-        return CuppaPrediction.from_tsv(self.path_predictions)
-
-    @cached_class_property
-    def probs_per_clf(self) -> dict[str, pd.DataFrame]:
-        return _get_probs_per_clf(self.predictions)
+    predictions: CuppaPrediction = CuppaPrediction.from_tsv(path_predictions)
+    probs_per_clf: dict[str, pd.DataFrame] = _get_probs_per_clf(predictions)
 
 
 class MockVisData:
 
-    path_vis_data = os.path.join(MOCK_DATA_DIR, "visualization/COLO829v003T.cuppa.vis_data.with_dummy_rna_preds.tsv")
+    path_vis_data: str = os.path.join(MOCK_DATA_DIR, "visualization/COLO829v003T.cuppa.vis_data.with_dummy_rna_preds.tsv")
+    vis_data: CuppaVisData = CuppaVisData.from_tsv(path_vis_data)
 
-    @cached_class_property
-    def vis_data(self) -> CuppaVisData:
-        return CuppaVisData.from_tsv(self.path_vis_data)
