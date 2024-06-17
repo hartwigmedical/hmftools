1bee14ae823a835e03720c16b3dbc1c40d43b288
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-06-17 12:35:08 +1000

    Cuppa: Removed unused method DataSource.fromCategory()

--- cuppa/src/main/java/com/hartwig/hmftools/cup/prep/DataSource.java
+++ cuppa/src/main/java/com/hartwig/hmftools/cup/prep/DataSource.java
@@ -4,16 +4,4 @@ public enum DataSource
 {
     DNA,
     RNA;
-
-    public static DataSource fromCategory(final CategoryType categoryType)
-    {
-        switch(categoryType)
-        {
-            case GENE_EXP:
-            case ALT_SJ:
-                return RNA;
-            default:
-                return DNA;
-        }
-    }
 }
