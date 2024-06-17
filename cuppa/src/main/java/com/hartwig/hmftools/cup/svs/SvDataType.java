79f7f7213324c06d3e73442a5c3a32506f7186e9
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-06-17 10:47:35 +1000

    Cuppa: Moved SvDataType to svs package and removed unused SvDataType methods

---
+++ cuppa/src/main/java/com/hartwig/hmftools/cup/svs/SvDataType.java
@@ -0,0 +1,11 @@
+package com.hartwig.hmftools.cup.svs;
+
+public enum SvDataType
+{
+    LINE,
+    SIMPLE_DEL_20KB_1MB,
+    SIMPLE_DUP_32B_200B,
+    SIMPLE_DUP_100KB_5MB,
+    MAX_COMPLEX_SIZE,
+    TELOMERIC_SGL;
+}
