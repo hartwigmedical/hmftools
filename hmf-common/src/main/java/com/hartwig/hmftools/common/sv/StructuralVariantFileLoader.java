0f921f5cc2d274fa08f172d1509f2fb9a52a2e65
Charles Shale <shalecharles@gmail.com>
2024-06-26 12:46:15 +1000

    Linx: use Gridss VCF file loader for now

--- hmf-common/src/main/java/com/hartwig/hmftools/common/sv/StructuralVariantFileLoader.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/sv/StructuralVariantFileLoader.java
@@ -3,6 +3,8 @@ package com.hartwig.hmftools.common.sv;
 import java.io.IOException;
 import java.util.List;
 
+import com.hartwig.hmftools.common.sv.gridss.GridssSvFactory;
+
 import htsjdk.tribble.AbstractFeatureReader;
 import htsjdk.tribble.readers.LineIterator;
 import htsjdk.variant.variantcontext.VariantContext;
@@ -14,6 +16,19 @@ public final class StructuralVariantFileLoader
     public static List<StructuralVariant> fromFile(final String vcfFileLocation, final VariantContextFilter filter) throws IOException
     {
         final StructuralVariantFactory factory = StructuralVariantFactory.build(filter);
+
+        try(final AbstractFeatureReader<VariantContext, LineIterator> reader = AbstractFeatureReader.getFeatureReader(vcfFileLocation,
+                new VCFCodec(), false))
+        {
+            reader.iterator().forEach(factory::addVariantContext);
+        }
+
+        return factory.results();
+    }
+
+    public static List<StructuralVariant> fromGridssFile(final String vcfFileLocation, final VariantContextFilter filter) throws IOException
+    {
+        final GridssSvFactory factory = GridssSvFactory.build(filter);
 
         try(final AbstractFeatureReader<VariantContext, LineIterator> reader = AbstractFeatureReader.getFeatureReader(vcfFileLocation,
                 new VCFCodec(), false))
