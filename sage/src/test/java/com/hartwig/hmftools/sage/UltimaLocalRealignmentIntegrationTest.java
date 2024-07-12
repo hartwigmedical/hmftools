2e853e040ca9b3da9c57e859c5980c6cfe8aa40f
Matthew Cooper <matthew.cooper@pobox.com>
2024-07-12 11:00:25 +1000

    WIP: Now sketch handles dels of any length.

--- sage/src/test/java/com/hartwig/hmftools/sage/UltimaLocalRealignmentIntegrationTest.java
+++ sage/src/test/java/com/hartwig/hmftools/sage/UltimaLocalRealignmentIntegrationTest.java
@@ -190,4 +190,212 @@ public class UltimaLocalRealignmentIntegrationTest
 
         assertTrue(UltimaLocalRealigner.isVariantExplained(indelReadContext));
     }
+
+    // TODO: Rename
+    @Test
+    public void testMultiBaseHomopolymerContractionInMiddle()
+    {
+        Random random = new Random(0);
+
+        String initRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+        String middleRefBases = "ACTCGGCCTCCCCCGGAGGTGCCGGA";
+        String finalRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+
+        String refBases = initRefBases + middleRefBases + finalRefBases;
+        MockRefGenome refGenome = new MockRefGenome(true);
+        refGenome.RefGenomeMap.put(CHROMOSOME, refBases);
+        refGenome.ChromosomeLengths.put(CHROMOSOME, refBases.length());
+        ChrBaseRegion region = new ChrBaseRegion(CHROMOSOME, 1, refBases.length());
+
+        MutatedBasesBuilder mutatedBasesBuilder = new MutatedBasesBuilder(refBases);
+        // TODO: change this back once we get read base expansion working.
+        //            mutatedBasesBuilder.delBases(initRefBases.length() + 10, 1);
+        mutatedBasesBuilder.delBases(initRefBases.length() + 11, 3);
+        mutatedBasesBuilder.mutateBase(initRefBases.length() + 15, 'T');
+        MutatedBases mutatedBases = mutatedBasesBuilder.build();
+
+        int leftMiddleMutIndex = mutatedBases.leftMutIndexFromRefPos(initRefBases.length());
+        int rightMiddleMutIndex = mutatedBases.rightMutIndexFromRefPos(refBases.length() - finalRefBases.length() + 1);
+        int centreMiddleMutIndex = (leftMiddleMutIndex + rightMiddleMutIndex)/2;
+        int mutStartIndex = centreMiddleMutIndex - READ_LENGTH/2;
+
+        SAMRecord read = mutatedBases.getUnpairedRead("READ_001", CHROMOSOME, mutStartIndex, READ_LENGTH, true);
+
+        List<SAMRecord> reads = Lists.newArrayList();
+        while(reads.size() < READ_DEPTH)
+        {
+            // TODO: Different read name for each read. Other tests as well.
+            reads.add(read);
+        }
+
+        SageConfig config = new SageConfig(false, SequencingType.ULTIMA, null);
+        List<Candidate> initialCandidates = findInitialCandidates(config, region, refGenome, reads);
+
+        assertEquals(2, initialCandidates.size());
+
+        List<Candidate> indelCandidates = initialCandidates.stream().filter(x -> x.variant().isIndel()).collect(Collectors.toList());
+        List<Candidate> snvCandidates = initialCandidates.stream().filter(x -> !x.variant().isIndel()).collect(Collectors.toList());
+
+        assertEquals(1, indelCandidates.size());
+        assertEquals(1, snvCandidates.size());
+
+        Candidate indelCandidate = indelCandidates.get(0);
+        VariantReadContext indelReadContext = indelCandidate.readContext();
+
+        assertTrue(UltimaLocalRealigner.isVariantExplained(indelReadContext));
+    }
+
+    // TODO: Rename
+    @Test
+    public void testMultiBaseHomopolymerContractionAtBoundary()
+    {
+        Random random = new Random(0);
+
+        String initRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+        String middleRefBases = "ACTCGGCCTCCCCCGGAGGTGCCGGA";
+        String finalRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+
+        String refBases = initRefBases + middleRefBases + finalRefBases;
+        MockRefGenome refGenome = new MockRefGenome(true);
+        refGenome.RefGenomeMap.put(CHROMOSOME, refBases);
+        refGenome.ChromosomeLengths.put(CHROMOSOME, refBases.length());
+        ChrBaseRegion region = new ChrBaseRegion(CHROMOSOME, 1, refBases.length());
+
+        MutatedBasesBuilder mutatedBasesBuilder = new MutatedBasesBuilder(refBases);
+        mutatedBasesBuilder.delBases(initRefBases.length() + 10, 3);
+        mutatedBasesBuilder.mutateBase(initRefBases.length() + 15, 'T');
+        MutatedBases mutatedBases = mutatedBasesBuilder.build();
+
+        int leftMiddleMutIndex = mutatedBases.leftMutIndexFromRefPos(initRefBases.length());
+        int rightMiddleMutIndex = mutatedBases.rightMutIndexFromRefPos(refBases.length() - finalRefBases.length() + 1);
+        int centreMiddleMutIndex = (leftMiddleMutIndex + rightMiddleMutIndex)/2;
+        int mutStartIndex = centreMiddleMutIndex - READ_LENGTH/2;
+
+        SAMRecord read = mutatedBases.getUnpairedRead("READ_001", CHROMOSOME, mutStartIndex, READ_LENGTH, true);
+
+        List<SAMRecord> reads = Lists.newArrayList();
+        while(reads.size() < READ_DEPTH)
+        {
+            // TODO: Different read name for each read. Other tests as well.
+            reads.add(read);
+        }
+
+        SageConfig config = new SageConfig(false, SequencingType.ULTIMA, null);
+        List<Candidate> initialCandidates = findInitialCandidates(config, region, refGenome, reads);
+
+        assertEquals(2, initialCandidates.size());
+
+        List<Candidate> indelCandidates = initialCandidates.stream().filter(x -> x.variant().isIndel()).collect(Collectors.toList());
+        List<Candidate> snvCandidates = initialCandidates.stream().filter(x -> !x.variant().isIndel()).collect(Collectors.toList());
+
+        assertEquals(1, indelCandidates.size());
+        assertEquals(1, snvCandidates.size());
+
+        Candidate indelCandidate = indelCandidates.get(0);
+        VariantReadContext indelReadContext = indelCandidate.readContext();
+
+        assertTrue(UltimaLocalRealigner.isVariantExplained(indelReadContext));
+    }
+
+    // TODO: Rename
+    @Test
+    public void testSingleHomopolymerDeletion()
+    {
+        Random random = new Random(0);
+
+        String initRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+        String middleRefBases = "ACTCGGCCTCCCGGAGGTGCCGGA";
+        String finalRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+
+        String refBases = initRefBases + middleRefBases + finalRefBases;
+        MockRefGenome refGenome = new MockRefGenome(true);
+        refGenome.RefGenomeMap.put(CHROMOSOME, refBases);
+        refGenome.ChromosomeLengths.put(CHROMOSOME, refBases.length());
+        ChrBaseRegion region = new ChrBaseRegion(CHROMOSOME, 1, refBases.length());
+
+        MutatedBasesBuilder mutatedBasesBuilder = new MutatedBasesBuilder(refBases);
+        mutatedBasesBuilder.delBases(initRefBases.length() + 10, 3);
+        mutatedBasesBuilder.mutateBase(initRefBases.length() + 13, 'T');
+        MutatedBases mutatedBases = mutatedBasesBuilder.build();
+
+        int leftMiddleMutIndex = mutatedBases.leftMutIndexFromRefPos(initRefBases.length());
+        int rightMiddleMutIndex = mutatedBases.rightMutIndexFromRefPos(refBases.length() - finalRefBases.length() + 1);
+        int centreMiddleMutIndex = (leftMiddleMutIndex + rightMiddleMutIndex)/2;
+        int mutStartIndex = centreMiddleMutIndex - READ_LENGTH/2;
+
+        SAMRecord read = mutatedBases.getUnpairedRead("READ_001", CHROMOSOME, mutStartIndex, READ_LENGTH, true);
+
+        List<SAMRecord> reads = Lists.newArrayList();
+        while(reads.size() < READ_DEPTH)
+        {
+            reads.add(read);
+        }
+
+        SageConfig config = new SageConfig(false, SequencingType.ULTIMA, null);
+        List<Candidate> initialCandidates = findInitialCandidates(config, region, refGenome, reads);
+
+        assertEquals(2, initialCandidates.size());
+
+        List<Candidate> indelCandidates = initialCandidates.stream().filter(x -> x.variant().isIndel()).collect(Collectors.toList());
+        List<Candidate> snvCandidates = initialCandidates.stream().filter(x -> !x.variant().isIndel()).collect(Collectors.toList());
+
+        assertEquals(1, indelCandidates.size());
+        assertEquals(1, snvCandidates.size());
+
+        Candidate indelCandidate = indelCandidates.get(0);
+        VariantReadContext indelReadContext = indelCandidate.readContext();
+
+        assertTrue(UltimaLocalRealigner.isVariantExplained(indelReadContext));
+    }
+
+    // TODO: Rename
+    @Test
+    public void testMultipleHomopolymerDeletion()
+    {
+        Random random = new Random(0);
+
+        String initRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+        String middleRefBases = "ACTCGGCCTCCCTAAGGAGGTGCCGGA";
+        String finalRefBases = "A".repeat(RANDOM_BASES_PADDING_LENGTH);
+
+        String refBases = initRefBases + middleRefBases + finalRefBases;
+        MockRefGenome refGenome = new MockRefGenome(true);
+        refGenome.RefGenomeMap.put(CHROMOSOME, refBases);
+        refGenome.ChromosomeLengths.put(CHROMOSOME, refBases.length());
+        ChrBaseRegion region = new ChrBaseRegion(CHROMOSOME, 1, refBases.length());
+
+        MutatedBasesBuilder mutatedBasesBuilder = new MutatedBasesBuilder(refBases);
+        mutatedBasesBuilder.delBases(initRefBases.length() + 10, 6);
+        mutatedBasesBuilder.mutateBase(initRefBases.length() + 16, 'T');
+        MutatedBases mutatedBases = mutatedBasesBuilder.build();
+
+        int leftMiddleMutIndex = mutatedBases.leftMutIndexFromRefPos(initRefBases.length());
+        int rightMiddleMutIndex = mutatedBases.rightMutIndexFromRefPos(refBases.length() - finalRefBases.length() + 1);
+        int centreMiddleMutIndex = (leftMiddleMutIndex + rightMiddleMutIndex)/2;
+        int mutStartIndex = centreMiddleMutIndex - READ_LENGTH/2;
+
+        SAMRecord read = mutatedBases.getUnpairedRead("READ_001", CHROMOSOME, mutStartIndex, READ_LENGTH, true);
+
+        List<SAMRecord> reads = Lists.newArrayList();
+        while(reads.size() < READ_DEPTH)
+        {
+            reads.add(read);
+        }
+
+        SageConfig config = new SageConfig(false, SequencingType.ULTIMA, null);
+        List<Candidate> initialCandidates = findInitialCandidates(config, region, refGenome, reads);
+
+        assertEquals(2, initialCandidates.size());
+
+        List<Candidate> indelCandidates = initialCandidates.stream().filter(x -> x.variant().isIndel()).collect(Collectors.toList());
+        List<Candidate> snvCandidates = initialCandidates.stream().filter(x -> !x.variant().isIndel()).collect(Collectors.toList());
+
+        assertEquals(1, indelCandidates.size());
+        assertEquals(1, snvCandidates.size());
+
+        Candidate indelCandidate = indelCandidates.get(0);
+        VariantReadContext indelReadContext = indelCandidate.readContext();
+
+        assertTrue(UltimaLocalRealigner.isVariantExplained(indelReadContext));
+    }
 }
