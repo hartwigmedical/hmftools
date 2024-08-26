b64577a7b3e66300233de5b0974c2d928e920fb3
David Koetsier <david.f.koetsier@gmail.com>
2024-08-26 15:20:16 +0200

    Compar: DEV-4061: Compare transcripts and JCN for fusions

--- compar/src/main/java/com/hartwig/hmftools/compar/linx/FusionData.java
+++ compar/src/main/java/com/hartwig/hmftools/compar/linx/FusionData.java
@@ -23,12 +23,15 @@ public class FusionData implements ComparableItem
     protected static final String FLD_REPORTED_TYPE = "ReportedType";
     protected static final String FLD_PHASED = "Phased";
     protected static final String FLD_LIKELIHOOD = "likelihood";
+    protected static final String FLD_TRANSCRIPT_UP = "fusedTranscriptUp";
     protected static final String FLD_EXON_UP = "fusedExonUp";
+    protected static final String FLD_TRANSCRIPT_DOWN = "fusedTranscriptDown";
     protected static final String FLD_EXON_DOWN = "fusedExonDown";
     protected static final String FLD_CHAIN_LINKS = "chainLinks";
     protected static final String FLD_CHAIN_TERM = "chainTerminated";
     protected static final String FLD_DOMAINS_KEPT = "domainsKept";
     protected static final String FLD_DOMAINS_LOST = "domainsLost";
+    protected static final String FLD_JUNCTION_COPY_NUMBER = "junctionCopyNumber";
 
     public FusionData(final LinxFusion fusion, final String geneMappedName)
     {
@@ -53,12 +56,15 @@ public class FusionData implements ComparableItem
         values.add(String.format("%s", Fusion.reportedType()));
         values.add(String.format("%s", Fusion.phased()));
         values.add(String.format("%s", Fusion.likelihood()));
+        values.add(String.format("%s", Fusion.geneTranscriptStart()));
         values.add(String.format("%d", Fusion.fusedExonUp()));
+        values.add(String.format("%s", Fusion.geneTranscriptEnd()));
         values.add(String.format("%d", Fusion.fusedExonDown()));
         values.add(String.format("%d", Fusion.chainLinks()));
         values.add(String.format("%s", Fusion.chainTerminated()));
         values.add(String.format("%s", Fusion.domainsKept()));
         values.add(String.format("%s", Fusion.domainsLost()));
+        values.add(String.format("%.2f", Fusion.junctionCopyNumber()));
         return values;
     }
 
@@ -84,12 +90,15 @@ public class FusionData implements ComparableItem
         checkDiff(diffs, FLD_REPORTED_TYPE, Fusion.reportedType(), otherFusion.Fusion.reportedType());
         checkDiff(diffs, FLD_PHASED, Fusion.phased().toString(), otherFusion.Fusion.phased().toString());
         checkDiff(diffs, FLD_LIKELIHOOD, Fusion.likelihood().toString(), otherFusion.Fusion.likelihood().toString());
+        checkDiff(diffs, FLD_TRANSCRIPT_UP, Fusion.geneTranscriptStart(), otherFusion.Fusion.geneTranscriptStart());
         checkDiff(diffs, FLD_EXON_UP, Fusion.fusedExonUp(), otherFusion.Fusion.fusedExonUp());
+        checkDiff(diffs, FLD_TRANSCRIPT_DOWN, Fusion.geneTranscriptEnd(), otherFusion.Fusion.geneTranscriptEnd());
         checkDiff(diffs, FLD_EXON_DOWN, Fusion.fusedExonDown(), otherFusion.Fusion.fusedExonDown());
         checkDiff(diffs, FLD_CHAIN_LINKS, Fusion.chainLinks(), otherFusion.Fusion.chainLinks());
         checkDiff(diffs, FLD_CHAIN_TERM, Fusion.chainTerminated(), otherFusion.Fusion.chainTerminated());
         checkDiff(diffs, FLD_DOMAINS_KEPT, Fusion.domainsKept(), otherFusion.Fusion.domainsKept());
         checkDiff(diffs, FLD_DOMAINS_LOST, Fusion.domainsLost(), otherFusion.Fusion.domainsLost());
+        checkDiff(diffs, FLD_JUNCTION_COPY_NUMBER, Fusion.junctionCopyNumber(), otherFusion.Fusion.junctionCopyNumber(), thresholds);
 
         return !diffs.isEmpty() ? new Mismatch(this, other, VALUE, diffs) : null;
     }
