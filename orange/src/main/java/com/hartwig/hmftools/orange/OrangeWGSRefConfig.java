8f2ae2b3f383f6908960a374931adc82369eeaa3
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-09-27 13:17:19 +1000

    Orange: Fixed wrong peach genotypes tsv suffix

--- orange/src/main/java/com/hartwig/hmftools/orange/OrangeWGSRefConfig.java
+++ orange/src/main/java/com/hartwig/hmftools/orange/OrangeWGSRefConfig.java
@@ -2,7 +2,6 @@ package com.hartwig.hmftools.orange;
 
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.CHORD_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.CUPPA_DIR;
-import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.FLAGSTAT_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.LINX_GERMLINE_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.METRICS_DIR;
 import static com.hartwig.hmftools.common.pipeline.PipelineToolDirectories.PEACH_DIR;
@@ -149,7 +148,7 @@ public interface OrangeWGSRefConfig
             String peachGenotypeTsv = optionalPath(PeachGenotypeFile.generateFileName(peachDir, refSampleId));
             if (peachGenotypeTsv == null)
             {
-                peachGenotypeTsv = mandatoryPath(peachDir + File.separator + tumorSampleId + ".peach.genotype.tsv");
+                peachGenotypeTsv = mandatoryPath(PeachGenotypeFile.generateFileName(peachDir, tumorSampleId));
             }
             builder.peachGenotypeTsv(peachGenotypeTsv);
 
