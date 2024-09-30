511c06b420bcf557bc98becae7f9db136a8eb805
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-10-01 08:22:22 +1000

    Chord: Added config

---
+++ chord/src/main/java/com/hartwig/hmftools/chord/ChordConfig.java
@@ -0,0 +1,124 @@
+package com.hartwig.hmftools.chord;
+
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion.REF_GENOME_VERSION;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion.REF_GENOME_VERSION_CFG_DESC;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion.V37;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.PURPLE_DIR_CFG;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.PURPLE_DIR_DESC;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAMPLE;
+import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAMPLE_DESC;
+import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.OUTPUT_ID;
+import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.parseOutputDir;
+
+import java.util.List;
+
+import com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion;
+import com.hartwig.hmftools.common.purple.PurpleCommon;
+import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
+import com.hartwig.hmftools.common.utils.config.ConfigUtils;
+import com.hartwig.hmftools.common.utils.file.FileWriterUtils;
+
+
+public class ChordConfig
+{
+    public final List<String> SampleIds;
+
+    public final String PurpleDir;
+
+    public final RefGenomeVersion RefGenVersion;
+
+    public final String OutputDir;
+    public final String OutputId;
+
+    private static final String SAMPLE_IDS_DELIM = ",";
+
+    public ChordConfig(final ConfigBuilder configBuilder)
+    {
+        SampleIds = parseSampleIds(configBuilder.getValue(SAMPLE));
+
+        PurpleDir = configBuilder.getValue(PURPLE_DIR_CFG);
+
+        RefGenVersion = RefGenomeVersion.from(configBuilder);
+
+        OutputDir = parseOutputDir(configBuilder);
+        OutputId = configBuilder.getValue(OUTPUT_ID);
+    }
+
+    private List<String> parseSampleIds(String sampleIdsString)
+    {
+        List<String> sampleIds = List.of(sampleIdsString.split(SAMPLE_IDS_DELIM));
+
+        if(sampleIds.size() > 1)
+        {
+            throw new UnsupportedOperationException("Running CHORD in multi-sample mode is not yet implemented");
+        }
+
+        return sampleIds;
+    }
+
+    public String purpleSomaticVcfFile(String sampleId) { return PurpleCommon.purpleSomaticVcfFile(PurpleDir, sampleId); }
+
+    public String purpleSvVcfFile(String sampleId) { return PurpleCommon.purpleSomaticSvFile(PurpleDir, sampleId); }
+
+    public static void registerConfig(final ConfigBuilder configBuilder)
+    {
+        configBuilder.addConfigItem(SAMPLE, true, SAMPLE_DESC);
+
+        configBuilder.addPath(PURPLE_DIR_CFG, false, PURPLE_DIR_DESC);
+
+        configBuilder.addConfigItem(REF_GENOME_VERSION, false, REF_GENOME_VERSION_CFG_DESC, V37.toString());
+
+        FileWriterUtils.addOutputOptions(configBuilder);
+        ConfigUtils.addLoggingOptions(configBuilder);
+    }
+
+    public ChordConfig(List<String> sampleIds, String purpleDir, RefGenomeVersion refGenomeVersion, String outputDir, String outputId)
+    {
+        SampleIds = sampleIds;
+        PurpleDir = purpleDir;
+        RefGenVersion = refGenomeVersion;
+        OutputDir = outputDir;
+        OutputId = outputId;
+    }
+
+    public static class Builder
+    {
+        private List<String> SampleIds;
+        private String PurpleDir;
+        private RefGenomeVersion RefGenVersion = V37;
+        private String OutputDir;
+        private String OutputId = "";
+
+        public Builder sampleIds(List<String> sampleIds)
+        {
+            SampleIds = sampleIds;
+            return this;
+        }
+
+        public Builder purpleDir(String purpleDir)
+        {
+            PurpleDir = purpleDir;
+            return this;
+        }
+
+        public Builder refGenomeVersion(RefGenomeVersion refGenomeVersion)
+        {
+            RefGenVersion = refGenomeVersion;
+            return this;
+        }
+
+        public Builder outputDir(String outputDir)
+        {
+            OutputDir = outputDir;
+            return this;
+        }
+
+        public Builder outputId(String outputId)
+        {
+            OutputId = outputId;
+            return this;
+        }
+
+        public ChordConfig build(){ return new ChordConfig(SampleIds, PurpleDir, RefGenVersion, OutputDir, OutputId); }
+    }
+}
