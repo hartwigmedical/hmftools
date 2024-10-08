d8c8ffe03e7bcd0f76c034f702629d541d7cc306
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-10-08 13:24:00 -0400

    ACTIN-346: reuse converter

--- orange/src/main/java/com/hartwig/hmftools/orange/algo/linx/LinxBreakendConverter.java
+++ orange/src/main/java/com/hartwig/hmftools/orange/algo/linx/LinxBreakendConverter.java
@@ -9,8 +9,6 @@ import com.hartwig.hmftools.common.gene.GeneData;
 import com.hartwig.hmftools.common.linx.LinxSvAnnotation;
 import com.hartwig.hmftools.common.sv.StructuralVariant;
 import com.hartwig.hmftools.common.sv.StructuralVariantType;
-import com.hartwig.hmftools.datamodel.gene.TranscriptCodingType;
-import com.hartwig.hmftools.datamodel.gene.TranscriptRegionType;
 import com.hartwig.hmftools.datamodel.linx.ImmutableLinxBreakend;
 import com.hartwig.hmftools.datamodel.linx.LinxBreakend;
 import com.hartwig.hmftools.datamodel.linx.LinxBreakendType;
@@ -75,32 +73,15 @@ public class LinxBreakendConverter
             return LinxConversion.convert(linxBreakend);
         }
 
-        StructuralVariantType type = sv.type();
-
         double junctionCopyNumber = junctionCopyNumber(linxBreakend, sv, svAnnotation);
-
         GeneData geneData = ensemblDataCache.getGeneDataByName(linxBreakend.gene());
         String chrBand = geneData != null ? geneData.KaryotypeBand : "";
 
         return ImmutableLinxBreakend.builder()
-                .id(linxBreakend.id())
-                .svId(linxBreakend.svId())
-                .gene(linxBreakend.gene())
-                .transcript(linxBreakend.transcriptId())
-                .isCanonical(linxBreakend.canonical())
-                .geneOrientation(linxBreakend.geneOrientation())
-                .isCanonical(linxBreakend.canonical())
-                .disruptive(linxBreakend.disruptive())
-                .reported(linxBreakend.reportedDisruption())
-                .undisruptedCopyNumber(linxBreakend.undisruptedCopyNumber())
-                .regionType(TranscriptRegionType.valueOf(linxBreakend.regionType().name()))
-                .codingType(TranscriptCodingType.valueOf(linxBreakend.codingType().name()))
-                .nextSpliceExonRank(linxBreakend.nextSpliceExonRank())
-                .exonUp(linxBreakend.exonUp())
-                .exonDown(linxBreakend.exonDown())
+                .from(LinxConversion.convert(linxBreakend))
                 .chromosome(chrom)
                 .chromosomeBand(chrBand)
-                .type(LinxBreakendType.valueOf(type.name()))
+                .type(LinxBreakendType.valueOf(sv.type().name()))
                 .orientation(orientation)
                 .junctionCopyNumber(junctionCopyNumber)
                 .build();
