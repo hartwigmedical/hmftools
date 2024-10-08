dc6079436603b8cab4186726d921598c516518ff
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-10-08 14:02:23 +1100

    Chord: SnvPrep now outputs details file

--- chord/src/main/java/com/hartwig/hmftools/chord/snv/SnvPrep.java
+++ chord/src/main/java/com/hartwig/hmftools/chord/snv/SnvPrep.java
@@ -2,6 +2,8 @@ package com.hartwig.hmftools.chord.snv;
 
 import static com.hartwig.hmftools.chord.ChordConstants.CHORD_LOGGER;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -13,7 +15,6 @@ import com.hartwig.hmftools.chord.common.MutTypeCount;
 import com.hartwig.hmftools.chord.common.SmallVariant;
 import com.hartwig.hmftools.chord.common.VcfFile;
 import com.hartwig.hmftools.common.sigs.SnvSigUtils;
-import com.hartwig.hmftools.common.variant.SageVcfTags;
 
 import htsjdk.variant.variantcontext.VariantContext;
 
@@ -21,103 +22,111 @@ public class SnvPrep
 {
     private final ChordConfig mConfig;
 
+    List<SnvDetails> mSnvDetailsList = new ArrayList<>();
+
+    private static final String SNV_DETAILS_FILE_SUFFIX = ".chord.snv.details.tsv";
+
     public SnvPrep(ChordConfig config)
     {
         mConfig = config;
     }
 
-    private List<VariantContext> loadVariants(String sampleId) throws NoSuchFileException
+    private List<SmallVariant> loadVariants(String sampleId) throws NoSuchFileException
     {
         VcfFile vcfFile = new VcfFile(mConfig.purpleSomaticVcfFile(sampleId), mConfig.IncludeNonPass);
-        return vcfFile.loadVariants();
-    }
 
-    private static Map<String,Integer> countTriNucContexts(List<VariantContext> variantContexts)
-    {
-        // Get bin names
-        // Returns: C>A_ACA -> 0, C>A_ACC -> 1, C>A_ACG -> 2, etc. We only need the name (i.e. key)
-        Map<String,Integer> triNucNameIndexMap = new LinkedHashMap<>();
-        SnvSigUtils.populateBucketMap(triNucNameIndexMap);
+        List<VariantContext> variants = vcfFile.loadVariants();
 
-        // Initialize count vector
-        Map<String,Integer> triNucNameCountsMap = new LinkedHashMap<>();
-        triNucNameIndexMap.keySet().forEach(i -> triNucNameCountsMap.put(i, 0));
-
-        // Count trinucleotide contexts
-        int snvCount = 0;
-        for(VariantContext variantContext : variantContexts)
+        List<SmallVariant> snvs = new ArrayList<>();
+        for(VariantContext variantContext : variants)
         {
             SmallVariant smallVariant = new SmallVariant(variantContext);
 
             if(!smallVariant.isSnv())
                 continue;
 
-            String triNucSequence = smallVariant.Context.getAttributeAsString(SageVcfTags.TRINUCLEOTIDE_CONTEXT, null);
-            String triNucContext = SnvSigUtils.variantContext(smallVariant.RefBases, smallVariant.AltBases, triNucSequence);
-
-            // CHORD_LOGGER.trace("{}:{}:{}:{} {}",
-            //       variantContext.getContig(), variantContext.getStart(), refSeq, altSeq, renameTriNucBin(triNucContext));
-
-            triNucNameCountsMap.compute(triNucContext, (k,v) -> v + 1);
-
-            snvCount++;
+            snvs.add(smallVariant);
         }
 
-        CHORD_LOGGER.debug("Counted trinucleotide contexts for {} SNVs", snvCount);
+        return snvs;
+    }
+
+    private static Map<String, Integer> initializeCounts()
+    {
+        // Get bin names
+        // Returns: C>A_ACA -> 0, C>A_ACC -> 1, C>A_ACG -> 2, etc. We only need the name (i.e. key)
+        Map<String,Integer> triNucNameIndexMap = new LinkedHashMap<>();
+        SnvSigUtils.populateBucketMap(triNucNameIndexMap);
+
+        // Initialize counts
+        Map<String,Integer> triNucNameCountsMap = new LinkedHashMap<>();
+        triNucNameIndexMap.keySet().forEach(i -> triNucNameCountsMap.put(i, 0));
 
         return triNucNameCountsMap;
-    }
-
-    private static String renameTriNucBin(String bucketName)
-    {
-        // Convert e.g. "C>A_ACA" to "A[C>A]A"
-        // The latter is the format required by the CHORD random forest model
-
-        String[] bucketNameSplit = bucketName.split("_");
-
-        String substitutionType = bucketNameSplit[0];
-        char[] triNucSequence = bucketNameSplit[1].toCharArray();
-
-        return String.format("%s[%s]%s", triNucSequence[0], substitutionType, triNucSequence[2]);
-    }
-
-    private static List<MutTypeCount> makeCountsList(Map<String,Integer> triNucCounts)
-    {
-        List<MutTypeCount> counts = new ArrayList<>();
-
-        for(String binName : triNucCounts.keySet())
-        {
-            String newBinName = renameTriNucBin(binName);
-            int count = triNucCounts.get(binName);
-
-            MutTypeCount mutTypeCount = new MutTypeCount(newBinName, count);
-
-            CHORD_LOGGER.trace(mutTypeCount);
-
-            counts.add(new MutTypeCount(newBinName, count));
-        }
-
-        return counts;
     }
 
     public List<MutTypeCount> extractSampleData(String sampleId)
     {
         try
         {
-            CHORD_LOGGER.info("Counting SNV 96 trinucleotide contexts");
+            CHORD_LOGGER.info("Extracting SNV 96 trinucleotide contexts");
 
-            List<VariantContext> variantContexts = loadVariants(sampleId);
-            Map<String,Integer> triNucCountsMap = countTriNucContexts(variantContexts);
-            List<MutTypeCount> triNucCountsList = makeCountsList(triNucCountsMap);
+            List<SmallVariant> snvs = loadVariants(sampleId);
+            CHORD_LOGGER.debug("Loaded {} SNVs", snvs.size());
+
+            CHORD_LOGGER.debug("Initializing counts");
+            Map<String,Integer> triNucNameCountsMap = initializeCounts();
+
+            CHORD_LOGGER.debug("Populating counts");
+            for(SmallVariant snv : snvs)
+            {
+                SnvDetails snvDetails = SnvDetails.from(sampleId, snv);
+
+                if(mConfig.WriteDetailedFiles)
+                    mSnvDetailsList.add(snvDetails);
+
+                triNucNameCountsMap.compute(snvDetails.mTriNucContext, (k,v) -> v + 1);
+            }
+
+            if(mConfig.WriteDetailedFiles)
+            {
+                String snvDetailsPath = mConfig.OutputDir + "/" + sampleId + SNV_DETAILS_FILE_SUFFIX;
+                CHORD_LOGGER.info("Writing SNV details to: {}", snvDetailsPath);
+                writeDetails(snvDetailsPath, mSnvDetailsList);
+            }
+
+            List<MutTypeCount> triNucCountsList = new ArrayList<>();
+
+            for(String binName : triNucNameCountsMap.keySet())
+            {
+                String newBinName = SnvDetails.renameTriNucBin(binName);
+                int count = triNucNameCountsMap.get(binName);
+
+                MutTypeCount mutTypeCount = new MutTypeCount(newBinName, count);
+
+                CHORD_LOGGER.trace(mutTypeCount);
+
+                triNucCountsList.add(mutTypeCount);
+            }
 
             return triNucCountsList;
         }
         catch(Exception e)
         {
-            CHORD_LOGGER.error("sample({}) failed to count SNV trinucleotide contexts:", sampleId);
+            CHORD_LOGGER.error("sample({}) failed to count SNV trinucleotide contexts", sampleId);
             e.printStackTrace();
             System.exit(1);
             return null;
         }
+    }
+
+    private static void writeDetails(String path, List<SnvDetails> snvDetailsList) throws IOException
+    {
+        BufferedWriter writer = SnvDetails.initializeWriter(path);
+
+        for(SnvDetails snvDetails : snvDetailsList)
+            snvDetails.writeLine(writer);
+
+        writer.close();
     }
 }
