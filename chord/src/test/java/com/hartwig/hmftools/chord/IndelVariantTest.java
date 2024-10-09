e11af19598e00ca4116bf59b61418e2f647a6edd
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-10-09 11:39:31 +1100

    Chord: IndelVariantTest no longer depends on the HG37 ref genome

--- chord/src/test/java/com/hartwig/hmftools/chord/IndelVariantTest.java
+++ chord/src/test/java/com/hartwig/hmftools/chord/IndelVariantTest.java
@@ -2,41 +2,23 @@ package com.hartwig.hmftools.chord;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.common.io.Resources;
 import com.hartwig.hmftools.chord.indel.IndelVariant;
 import com.hartwig.hmftools.chord.common.SmallVariant;
 import com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class IndelVariantTest
 {
-    private static final RefGenomeSource REF_GENOME = RefGenomeSource.loadRefGenome("/Users/lnguyen/Hartwig/hartwigmedical/resources/ref_genomes/GRCh37/Homo_sapiens.GRCh37.GATK.illumina.fasta");
+    private static final String REF_GENOME_FILE = Resources.getResource("fasta/indel_regions.fasta").getPath();
+    private static final RefGenomeSource REF_GENOME = RefGenomeSource.loadRefGenome(REF_GENOME_FILE);
 
-    @Ignore // TODO: Convert to test using dummy fasta file
-    @Test
-    public void canGetInsertionFlankSequences()
-    {
-        // HG37
-        IndelVariant insertion = new IndelVariant(new SmallVariant("chr17", 79329838, "G", "GGGTGT"));
-
-        String leftFlank = insertion.getFlankingRefBasesLeft(REF_GENOME, 1);
-        String rightFlank = insertion.getFlankingRefBasesRight(REF_GENOME, 3);
-
-        System.out.println("Left flank: " + leftFlank);
-        System.out.println("Right flank: " + rightFlank);
-
-        assertEquals(insertion.mIndelLength, 5);
-        assertEquals("GGTCG", leftFlank);
-        assertEquals("GGTGTGGTGGCTCAC", rightFlank);
-    }
-
-    @Ignore // TODO: Convert to test using dummy fasta file
     @Test
     public void canGetDeletionFlankSequences()
     {
-        // HG37
-        IndelVariant deletion = new IndelVariant(new SmallVariant("chr1", 222493574, "CAA", "C"));
+        // Based on deletion at chr:222493574 in COLO829v003T
+        IndelVariant deletion = new IndelVariant(new SmallVariant("chr1", 21, "CAA", "C"));
 
         String leftFlank = deletion.getFlankingRefBasesLeft(REF_GENOME, 1);
         String rightFlank = deletion.getFlankingRefBasesRight(REF_GENOME, 3);
@@ -47,6 +29,23 @@ public class IndelVariantTest
         assertEquals(deletion.mIndelLength, 2);
         assertEquals("CC", leftFlank);
         assertEquals("AAGTCT", rightFlank);
+    }
+
+    @Test
+    public void canGetInsertionFlankSequences()
+    {
+        // Based on insertion at chr17:79329838 in COLO829v003T
+        IndelVariant insertion = new IndelVariant(new SmallVariant("chr17", 21, "G", "GGGTGT"));
+
+        String leftFlank = insertion.getFlankingRefBasesLeft(REF_GENOME, 1);
+        String rightFlank = insertion.getFlankingRefBasesRight(REF_GENOME, 3);
+
+        System.out.println("Left flank: " + leftFlank);
+        System.out.println("Right flank: " + rightFlank);
+
+        assertEquals(insertion.mIndelLength, 5);
+        assertEquals("GGTCG", leftFlank);
+        assertEquals("GGTGTGGTGGCTCAC", rightFlank);
     }
 
     @Test
