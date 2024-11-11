ceea4258942b1bb078a456df3aeac11cb26481ea
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2024-11-11 14:51:11 +1100

    Chord: Remove embedded mutSigExtractor and CHORD R packages

--- chord/src/main/R/CHORD/paper/extract_features/HMF_DR010_DR047/extract_sigs/merge_sig_vectors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-base_dir=/hpc/cog_bioinf/cuppen/project_data/Luan_projects/datasets/HMF_DR010_DR047/
-R_SCRIPT=$base_dir/scripts/extract_sigs/merge_sig_vectors.R
-
-guixr load-profile ~/.guix-profile --<<EOF
-Rscript $R_SCRIPT
-EOF
--- chord/src/main/R/CHORD/paper/extract_features/PCAWG_2020/extract_sigs/merge_sig_vectors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-guixr load-profile ~/.guix-profile --<<EOF
-Rscript /hpc/cuppen/projects/P0013_WGS_patterns_Diagn/datasets/processed/PCAWG_2020/scripts/extract_sigs/merge_sig_vectors.R
-EOF
