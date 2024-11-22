747042e098cbbac7a8f2e6c34a61665e4002a963
Charles Shale <shalecharles@gmail.com>
2024-11-22 16:41:31 +1100

    Esvee: moved BWA constants into common

--- esvee/src/main/java/com/hartwig/hmftools/esvee/assembly/alignment/BwaAligner.java
+++ esvee/src/main/java/com/hartwig/hmftools/esvee/assembly/alignment/BwaAligner.java
@@ -3,7 +3,6 @@ package com.hartwig.hmftools.esvee.assembly.alignment;
 import static com.hartwig.hmftools.esvee.assembly.AssemblyConfig.SV_LOGGER;
 import static com.hartwig.hmftools.esvee.common.SvConstants.MIN_INDEL_LENGTH;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
@@ -12,7 +11,6 @@ import java.util.List;
 import org.broadinstitute.hellbender.utils.bwa.BwaMemAligner;
 import org.broadinstitute.hellbender.utils.bwa.BwaMemAlignment;
 import org.broadinstitute.hellbender.utils.bwa.BwaMemIndex;
-import org.jetbrains.annotations.Nullable;
 
 public class BwaAligner implements Aligner
 {
@@ -47,55 +45,6 @@ public class BwaAligner implements Aligner
         {
             mAligner = null;
         }
-    }
-
-    private static final String LIBBWA_PATH = "LIBBWA_PATH"; // as expected by the BWA library
-    private static final String LIBBWA_PREFIX = "libbwa.";
-
-    private static final String MAC_OS = "Mac";
-    private static final String MAC_ARCH = "aarch64";
-    private static final String MAC_BWA_LIB = "libbwa.Darwin.dylib";
-
-    public static void loadAlignerLibrary(@Nullable final String bwaLibraryPath)
-    {
-        if(System.getProperty(LIBBWA_PATH) != null)
-            return;
-
-        if(bwaLibraryPath != null)
-        {
-            System.setProperty(LIBBWA_PATH, new File(bwaLibraryPath).getAbsolutePath());
-            return;
-        }
-
-        String osName = System.getProperty("os.name");
-        String osLibExtension = osExtension(osName);
-        String osArchitecture = System.getProperty("os.arch");
-
-        String candidateBWAPath = null;
-
-        if(osName.contains(MAC_OS) && osArchitecture.equals(MAC_ARCH))
-        {
-            candidateBWAPath = MAC_BWA_LIB;
-        }
-        else
-        {
-            candidateBWAPath = LIBBWA_PREFIX + osArchitecture + osLibExtension;
-        }
-
-        if(Files.exists(Paths.get(candidateBWAPath)))
-        {
-            System.setProperty(LIBBWA_PATH, new File(candidateBWAPath).getAbsolutePath());
-        }
-    }
-
-    private static String osExtension(final String osName)
-    {
-        if(osName.contains("Mac"))
-            return ".dylib";
-        else if(osName.contains("Win"))
-            return ".dll";
-        else
-            return ".so";
     }
 
     @Override
