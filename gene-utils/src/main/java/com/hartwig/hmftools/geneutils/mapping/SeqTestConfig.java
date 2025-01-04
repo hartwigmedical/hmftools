6062e963d9d47c32001414dd6da8b1a99c1ce4a1
Charles Shale <shalecharles@gmail.com>
2025-01-05 08:16:35 +1100

    GeneUtils: refactored sequence tester

---
+++ gene-utils/src/main/java/com/hartwig/hmftools/geneutils/mapping/SeqTestConfig.java
@@ -0,0 +1,59 @@
+package com.hartwig.hmftools.geneutils.mapping;
+
+import static com.hartwig.hmftools.common.blastn.BlastnRunner.registerBlastn;
+import static com.hartwig.hmftools.common.bwa.BwaUtils.BWA_LIB_PATH;
+import static com.hartwig.hmftools.common.bwa.BwaUtils.BWA_LIB_PATH_DESC;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource.REF_GENOME;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource.addRefGenomeFile;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource.deriveRefGenomeVersion;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource.loadRefGenome;
+import static com.hartwig.hmftools.common.utils.TaskExecutor.addThreadOptions;
+import static com.hartwig.hmftools.common.utils.TaskExecutor.parseThreads;
+import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.OUTPUT_ID;
+import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.addOutputOptions;
+import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.parseOutputDir;
+
+import com.hartwig.hmftools.common.genome.refgenome.RefGenomeSource;
+import com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion;
+import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
+import com.hartwig.hmftools.common.utils.config.ConfigUtils;
+
+public class SeqTestConfig
+{
+    public final String InputFile;
+    public final String OutputDir;
+    public final String OutputId;
+
+    public final int Threads;
+
+    public final RefGenomeSource RefGenome;
+    public final RefGenomeVersion RefGenomeVersion;
+
+    private static final String INPUT_FILE = "input_file";
+
+    public SeqTestConfig(final ConfigBuilder configBuilder)
+    {
+        String refGenomeFile = configBuilder.getValue(REF_GENOME);
+        RefGenome = loadRefGenome(refGenomeFile);
+        RefGenomeVersion = deriveRefGenomeVersion(RefGenome);
+
+        InputFile = configBuilder.getValue(INPUT_FILE);
+        OutputDir = parseOutputDir(configBuilder);
+        OutputId = configBuilder.getValue(OUTPUT_ID);
+        Threads = parseThreads(configBuilder);
+    }
+
+    public static void registerConfig(final ConfigBuilder configBuilder)
+    {
+        configBuilder.addPath(INPUT_FILE, true, "Input regions and sequences");
+
+        configBuilder.addPath(BWA_LIB_PATH, false, BWA_LIB_PATH_DESC);
+        registerBlastn(configBuilder, false);
+
+        addOutputOptions(configBuilder, false);
+        ConfigUtils.addLoggingOptions(configBuilder);
+        addThreadOptions(configBuilder);
+        addRefGenomeFile(configBuilder, true);
+
+    }
+}
