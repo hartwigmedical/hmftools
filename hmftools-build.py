b873195cf3976223826fefc13c2470299610563d
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2025-01-11 10:40:44 +1100

    upgrade google cloud artifactregistry-maven-wagon

--- hmftools-build.py
+++ hmftools-build.py
@@ -29,7 +29,7 @@ from argparse import ArgumentParser
 import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout,
-                    format='%(asctime)s %(levelname)5s - %(message)s',
+                    format='%(asctime)s [%(levelname)5s] - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
 logger.setLevel(logging.DEBUG)
