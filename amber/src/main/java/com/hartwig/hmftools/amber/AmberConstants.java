bb2fefeb1f5a56b297ac92ef55dc781ac9ecc786
Charles Shale <shalecharles@gmail.com>
2025-01-30 11:33:44 +1100

    Amber: modified contamination check conditions

--- amber/src/main/java/com/hartwig/hmftools/amber/AmberConstants.java
+++ amber/src/main/java/com/hartwig/hmftools/amber/AmberConstants.java
@@ -5,7 +5,6 @@ public class AmberConstants
     public static final String APP_NAME = "Amber";
 
     public static final int DEFAULT_MIN_BASE_QUALITY = 13;
-    public static final int DEFAULT_MIN_PARTITION = 10000;
     public static final int DEFAULT_MIN_MAPPING_QUALITY = 50;
     public static final int DEFAULT_TUMOR_ONLY_MIN_SUPPORT = 2;
     public static final double DEFAULT_TUMOR_ONLY_MIN_VAF = 0.05;
@@ -19,7 +18,11 @@ public class AmberConstants
     public static final double DEFAULT_MAX_HET_AF_PERCENTAGE = 0.65;
 
     public static final int MIN_NORMAL_READ_DEPTH = 7;
-    public static final long MIN_THREE_PLUS_READS = 2000;
+
+    public static final int THREE_PLUS_READS_MIN = 10;
+    public static final double THREE_PLUS_READS_SITE_PERC = 0.03;
+    public static final double TWO_PLUS_READS_SITE_PERC = 0.002;
+    public static final double TWO_PLUS_READS_VAF_MIN = 0.05;
 
     public static final int HOMOZYGOUS_REGION_MIN_SIZE = 500_000;
     public static final int HOMOZYGOUS_REGION_MIN_SNP_LOCI_COUNT = 50;
@@ -29,15 +32,8 @@ public class AmberConstants
     public static final int HOMOZYGOUS_REGION_LONG_SIZE = 3_000_000;
     public static final int UNIPARENTAL_DISOMY_MIN_LENGTH = 10_000_000;
 
-    // BAM/CRAM slicing optimisations
-    public static final int BAM_REGION_GROUP_MAX = 150000;
-    public static final int CRAM_REGION_GROUP_MAX = 10000;
-
     public static final int CRAM_MIN_GAP_START = 10000;
     public static final int BAM_MIN_GAP_START = 4000;
-
-    public static final int CRAM_MIN_GAP_INCREMENT = 1000;
-    public static final int BAM_MIN_GAP_INCREMENT = 200;
 
     public static final int TARGET_REGION_SITE_BUFFER = 300;
 
