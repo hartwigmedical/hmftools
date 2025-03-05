22a9f155a0da0da57de88dc3d49feb52bd771b3d
Charles Shale <shalecharles@gmail.com>
2025-03-06 08:58:14 +1100

    GeneUtils: tidied up ref-seq transcript mapping

--- gene-utils/src/main/java/com/hartwig/hmftools/geneutils/ensembl/GenerateRefSeq.java
+++ gene-utils/src/main/java/com/hartwig/hmftools/geneutils/ensembl/GenerateRefSeq.java
@@ -1,9 +1,7 @@
 package com.hartwig.hmftools.geneutils.ensembl;
 
-import static com.hartwig.hmftools.common.ensemblcache.EnsemblDataCache.addEnsemblDir;
 import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion.REF_GENOME_VERSION;
 import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion.REF_GENOME_VERSION_CFG_DESC;
-import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.addOutputDir;
 import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.parseOutputDir;
 import static com.hartwig.hmftools.geneutils.common.CommonUtils.APP_NAME;
 import static com.hartwig.hmftools.geneutils.common.CommonUtils.GU_LOGGER;
@@ -26,6 +24,8 @@ import org.jooq.Result;
 
 public class GenerateRefSeq
 {
+    private static final String OUTPUT_FILE = "output_file";
+
     public static void main(String[] args) throws ParseException, IOException, SQLException
     {
         ConfigBuilder configBuilder = new ConfigBuilder(APP_NAME);
@@ -33,14 +33,14 @@ public class GenerateRefSeq
         configBuilder.addConfigItem(REF_GENOME_VERSION, true, REF_GENOME_VERSION_CFG_DESC);
         EnsemblDAO.addCmdLineArgs(configBuilder);
         ConfigUtils.addLoggingOptions(configBuilder);
-        addOutputDir(configBuilder);
+
+        configBuilder.addConfigItem(OUTPUT_FILE, true, "Output file for RefSeq transcript mappings");
 
         configBuilder.checkAndParseCommandLine(args);
 
         RefGenomeVersion refGenomeVersion = RefGenomeVersion.from(configBuilder);
-        String outputDir = parseOutputDir(configBuilder);
 
-        GU_LOGGER.info("writing Ensembl gene panel and ref-seq data files, ref-genome-version({})", refGenomeVersion);
+        GU_LOGGER.info("writing Ensembl RefSeq transcript mapping, ref-genome-version({})", refGenomeVersion);
 
         DSLContext context = createEnsemblDbConnection(configBuilder);
 
@@ -49,16 +49,16 @@ public class GenerateRefSeq
 
         GU_LOGGER.debug("database connection established");
 
-        String reqSeqFile = String.format("%sref_seq.%s.tsv", outputDir, refGenomeVersion.identifier());
-        generateRefSeqMapping(context, reqSeqFile);
+        String reqSeqOutputFile = configBuilder.getValue(OUTPUT_FILE);
+        generateRefSeqMapping(context, reqSeqOutputFile);
 
-        GU_LOGGER.info("complete");
+        GU_LOGGER.info("ReqSeq transcript mapping complete");
     }
 
     private static void generateRefSeqMapping(final DSLContext context, final String outputFile)
     {
         final Result<Record> refseqMappingResult = context.fetch(readQueryString(Resources.getResource("ensembl_sql/ensembl_refseq_mapping.sql")));
-        GU_LOGGER.info("RefSeq mapping query returned {} entries", refseqMappingResult.size());
+        GU_LOGGER.debug("RefSeq mapping query returned {} entries", refseqMappingResult.size());
 
         writeRecordsAsTsv(outputFile, refseqMappingResult);
         GU_LOGGER.info("written RefSeq mapping output to {}", outputFile);
