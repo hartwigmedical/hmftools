a641129a1b212a24690affe338f5a4669b0a04c1
Matthew Cooper <matthew.cooper@pobox.com>
2025-03-27 13:39:26 +1100

    TODO: Add todo.

--- redux/src/main/java/com/hartwig/hmftools/redux/common/DuplicateGroupCollapser.java
+++ redux/src/main/java/com/hartwig/hmftools/redux/common/DuplicateGroupCollapser.java
@@ -24,6 +24,7 @@ import org.jetbrains.annotations.Nullable;
 
 import htsjdk.samtools.SAMRecord;
 
+// TODO: run SBX and ultima bams
 @FunctionalInterface
 public interface DuplicateGroupCollapser
 {
