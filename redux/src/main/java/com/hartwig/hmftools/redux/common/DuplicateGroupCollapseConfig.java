a597b724c777a4aef5152d570ead2464166d91b1
Matthew Cooper <matthew.cooper@pobox.com>
2025-03-31 14:47:56 +1100

    Redux: Fix Ultima duplicate group collapsing so that chain collapsing no longer occurs.

--- redux/src/main/java/com/hartwig/hmftools/redux/common/DuplicateGroupCollapseConfig.java
+++ redux/src/main/java/com/hartwig/hmftools/redux/common/DuplicateGroupCollapseConfig.java
@@ -7,36 +7,34 @@ import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
 public class DuplicateGroupCollapseConfig
 {
     public final SequencingType Sequencing;
-    public final boolean UMIEnabled;
     public final int SbxMaxDuplicateDistance;
 
     private static final String SBX_MAX_DUPLICATE_DISTANCE = "sbx_max_dup_dist";
 
     private static final String SBX_MAX_DUPLICATE_DISTANCE_DESC = "Max distance between the end of SBX fragments to be declared duplicates";
 
-    public DuplicateGroupCollapseConfig(final SequencingType sequencing, boolean umiEnabled, final int sbxMaxDuplicateDistance)
+    public DuplicateGroupCollapseConfig(final SequencingType sequencing, final int sbxMaxDuplicateDistance)
     {
         Sequencing = sequencing;
-        UMIEnabled = umiEnabled;
         SbxMaxDuplicateDistance = sbxMaxDuplicateDistance;
     }
 
     @VisibleForTesting
     public DuplicateGroupCollapseConfig(final SequencingType sequencing)
     {
-        this(sequencing, false, 0);
+        this(sequencing, 0);
     }
 
     public static void addConfig(final ConfigBuilder configBuilder)
     {
-        // sequencing and umi config is already defined in ReduxConfig
+        // sequencing is already defined in ReduxConfig
 
         configBuilder.addInteger(SBX_MAX_DUPLICATE_DISTANCE, SBX_MAX_DUPLICATE_DISTANCE_DESC, 0);
     }
 
-    public static DuplicateGroupCollapseConfig from(final SequencingType sequencing, boolean umiEnabled, final ConfigBuilder configBuilder)
+    public static DuplicateGroupCollapseConfig from(final SequencingType sequencing, final ConfigBuilder configBuilder)
     {
         int sbxMaxDuplicateDistance = configBuilder.getInteger(SBX_MAX_DUPLICATE_DISTANCE);
-        return new DuplicateGroupCollapseConfig(sequencing, umiEnabled, sbxMaxDuplicateDistance);
+        return new DuplicateGroupCollapseConfig(sequencing, sbxMaxDuplicateDistance);
     }
 }
