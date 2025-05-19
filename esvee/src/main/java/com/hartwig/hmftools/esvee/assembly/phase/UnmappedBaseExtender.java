1d012c50516f4f21d79e4d9592935963c12a3234
Charles Shale <shalecharles@gmail.com>
2025-05-19 19:36:54 +1000

    Esvee: removed subsequence match repeat check from unmapped ext base extender

--- esvee/src/main/java/com/hartwig/hmftools/esvee/assembly/phase/UnmappedBaseExtender.java
+++ esvee/src/main/java/com/hartwig/hmftools/esvee/assembly/phase/UnmappedBaseExtender.java
@@ -595,16 +595,9 @@ public class UnmappedBaseExtender
         {
             String readSeqBases = new String(readBases, readIndex, subsequenceLength);
             int extBaseMatchIndex = mExtensionBases.indexOf(readSeqBases);
-            boolean hasMultipleSubsequenceMatches = false;
 
             while(extBaseMatchIndex >= 0)
             {
-                // rule out this read if it can be added at more than 1 location
-                if(hasMultipleSubsequenceMatches)
-                    return null;
-
-                hasMultipleSubsequenceMatches = true;
-
                 if(readBaseQuals == null)
                     readBaseQuals = reverseBases ? reverseArray(read.getBaseQuality()) : read.getBaseQuality();
 
