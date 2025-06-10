fbe99736bc643bdf5bc8e58241bf0abc16f8670d
Reece Jones <r.jones@hartwigmedicalfoundation.nl>
2025-06-10 17:41:13 +1000

    GeneUtils: initial genome mappability skeleton code adjustment

--- gene-utils/src/main/java/com/hartwig/hmftools/geneutils/utils/GenomeMappabilityFinder.java
+++ gene-utils/src/main/java/com/hartwig/hmftools/geneutils/utils/GenomeMappabilityFinder.java
@@ -18,7 +18,6 @@ import static com.hartwig.hmftools.common.utils.file.CommonFields.FLD_CHROMOSOME
 import static com.hartwig.hmftools.common.utils.file.CommonFields.FLD_POSITION_END;
 import static com.hartwig.hmftools.common.utils.file.CommonFields.FLD_POSITION_START;
 import static com.hartwig.hmftools.common.utils.file.FileDelimiters.TSV_DELIM;
-import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.addOutputOptions;
 import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.closeBufferedWriter;
 import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.createBufferedWriter;
 import static com.hartwig.hmftools.geneutils.common.CommonUtils.APP_NAME;
@@ -50,52 +49,61 @@ import org.jetbrains.annotations.NotNull;
 
 public class GenomeMappabilityFinder
 {
-    private final String mOutputFile;
-
-    private final BufferedWriter mWriter;
     private final RefGenomeSource mRefGenome;
     private final RefGenomeVersion mRefGenVersion;
-
-    private final int mBaseWindowLength;
-    private final int mBaseBufferLength;
-
-    private final int mPartitionSize;
-    private final int mThreads;
+    // Genome region filtering.
     private final SpecificRegions mSpecificChrRegions;
+
+    // Genome is partitioned in windows of this length, each of which are given a score.
+    private final int mBaseWindowLength;
+    // Each window is spaced this far apart. < mBaseWindowLength implies overlapping windows.
+    private final int mBaseWindowSpacing;
+
+    // Performance tuning parameters.
+    // How many windows to align with BWA-MEM at once.
+    private final int mBatchSize;
+    private final int mThreads;
 
     private final BwaMemAligner mAligner;
 
-    // config items
-    private static final String CFG_OUTPUT_FILE = "output_file";
-    protected static final String CFG_BASE_WINDOW_LENGTH = "window_length";
-    protected static final String CFG_BASE_BUFFER_LENGTH = "buffer_length";
-    protected static final String CFG_PARTITION_SIZE = "partition_size";
+    private final BufferedWriter mOutputWriter;
 
-    private static final int DEFAULT_BASE_LENGTH = 20;
-    private static final int DEFAULT_BUFFER_LENGTH = 40;
+    private static final String CFG_OUTPUT_FILE = "output_file";
+
+    private static final String CFG_BASE_WINDOW_LENGTH = "window_length";
+    private static final int DEFAULT_WINDOW_LENGTH = 40;
+
+    private static final String CFG_BASE_WINDOW_SPACING = "window_spacing";
+    private static final int DEFAULT_WINDOW_SPACING = 40;
+
+    private static final String CFG_BATCH_SIZE = "batch_size";
+    private static final int DEFAULT_BATCH_SIZE = 100000;
+
+    private static final String FLD_RISK_SCORE = "risk_score";
+    private static final String FLD_QUALITY_SCORE = "quality_score";
 
     public GenomeMappabilityFinder(final ConfigBuilder configBuilder)
     {
-        mOutputFile = configBuilder.getValue(CFG_OUTPUT_FILE);
-        mWriter = initialiseWriter();
-
         mRefGenome = loadRefGenome(configBuilder.getValue(REF_GENOME));
         mRefGenVersion = deriveRefGenomeVersion(mRefGenome);
 
-        mPartitionSize = configBuilder.getInteger(CFG_PARTITION_SIZE);
-        mBaseWindowLength = configBuilder.getInteger(CFG_BASE_WINDOW_LENGTH);
-        mBaseBufferLength = configBuilder.getInteger(CFG_BASE_BUFFER_LENGTH);
-
-        mThreads = parseThreads(configBuilder);
         mSpecificChrRegions = SpecificRegions.from(configBuilder);
+
+        mBaseWindowLength = configBuilder.getInteger(CFG_BASE_WINDOW_LENGTH);
+        mBaseWindowSpacing = configBuilder.getInteger(CFG_BASE_WINDOW_SPACING);
+
+        mBatchSize = configBuilder.getInteger(CFG_BATCH_SIZE);
+        mThreads = parseThreads(configBuilder);
 
         String refGenomeImageFile = configBuilder.getValue(REF_GENOME) + ".img";
 
         loadAlignerLibrary(configBuilder.getValue(BWA_LIB_PATH));
         mAligner = initialiseBwaAligner(refGenomeImageFile);
+
+        mOutputWriter = initialiseOutputWriter(configBuilder.getValue(CFG_OUTPUT_FILE));
     }
 
-    private BwaMemAligner initialiseBwaAligner(final String refGenomeImageFile)
+    private static BwaMemAligner initialiseBwaAligner(final String refGenomeImageFile)
     {
         if(refGenomeImageFile.isEmpty() || !Files.exists(Paths.get(refGenomeImageFile)))
             System.exit(1);
@@ -105,7 +113,7 @@ public class GenomeMappabilityFinder
             BwaMemIndex index = new BwaMemIndex(refGenomeImageFile);
             BwaMemAligner aligner = new BwaMemAligner(index);
 
-            // adjust parameters as required
+            // TODO: adjust parameters as required
 
             GU_LOGGER.debug("BWA align options: ");
             GU_LOGGER.debug("  BWA Bandwidth: {}", aligner.getBandwidthOption());
@@ -123,8 +131,6 @@ public class GenomeMappabilityFinder
             GU_LOGGER.debug("  BWA MaxXAHits: {}", aligner.getMaxXAHitsOption());
             GU_LOGGER.debug("  BWA MaxXAHitsAlt: {}", aligner.getMaxXAHitsAltOption());
 
-            // aligner.setMismatchPenaltyOption(5);
-
             return aligner;
         }
         catch(Exception e)
@@ -133,16 +139,32 @@ public class GenomeMappabilityFinder
             return null;
         }
     }
+
     public void run()
     {
         GU_LOGGER.info("analysing blacklist regions");
 
         long startTimeMs = System.currentTimeMillis();
 
+        List<ChrBaseRegion> regions = getBaseRegions();
+
+        // TODO
+        List<RegionTask> regionTasks = regions.stream().map(x -> new RegionTask(x)).collect(Collectors.toList());
+        List<Callable> callableList = regionTasks.stream().collect(Collectors.toList());
+        if(!TaskExecutor.executeTasks(callableList, mThreads))
+            System.exit(1);
+
+        closeBufferedWriter(mOutputWriter);
+
+        GU_LOGGER.info("Genome mappability analysis complete, mins({})", runTimeMinsStr(startTimeMs));
+    }
+
+    private List<ChrBaseRegion> getBaseRegions() {
         RefGenomeCoordinates coordinates = mRefGenVersion.is37() ? RefGenomeCoordinates.COORDS_37 : RefGenomeCoordinates.COORDS_38;
 
-        List<ChrBaseRegion> regions = Lists.newArrayList();
+        // TODO
 
+        List<ChrBaseRegion> regions = Lists.newArrayList();
         for(HumanChromosome chromosome : HumanChromosome.values())
         {
             String chrStr = mRefGenVersion.versionedChromosome(chromosome.toString());
@@ -160,18 +182,10 @@ public class GenomeMappabilityFinder
                 regions.add(region);
             }
         }
-
-        List<RegionTask> regionTasks = regions.stream().map(x -> new RegionTask(x)).collect(Collectors.toList());
-
-        List<Callable> callableList = regionTasks.stream().collect(Collectors.toList());
-        if(!TaskExecutor.executeTasks(callableList, mThreads))
-            System.exit(1);
-
-        closeBufferedWriter(mWriter);
-
-        GU_LOGGER.info("Genome mappability analysis complete, mins({})", runTimeMinsStr(startTimeMs));
+        return regions;
     }
 
+    // TODO
     private class RegionTask implements Callable
     {
         private final ChrBaseRegion mRegion;
@@ -208,18 +222,15 @@ public class GenomeMappabilityFinder
         }
     }
 
-    private BufferedWriter initialiseWriter()
+    private BufferedWriter initialiseOutputWriter(String path)
     {
         try
         {
-            BufferedWriter writer = createBufferedWriter(mOutputFile, false);
+            BufferedWriter writer = createBufferedWriter(path, false);
 
             StringJoiner sj = new StringJoiner(TSV_DELIM);
-
-            sj.add(FLD_CHROMOSOME).add(FLD_POSITION_START).add(FLD_POSITION_END);
-
-            // sj.add("ScoreInfo") etc;
-
+            sj.add(FLD_CHROMOSOME).add(FLD_POSITION_START).add(FLD_POSITION_END)
+                    .add(FLD_RISK_SCORE).add(FLD_QUALITY_SCORE);
             writer.write(sj.toString());
             writer.newLine();
 
@@ -232,18 +243,17 @@ public class GenomeMappabilityFinder
         }
     }
 
-    private synchronized void writeWindowData(
-            final String chromosome, final int positionStart, final int positionEnd)
+    private void writeWindowData(
+            final String chromosome, final int positionStart, final int positionEnd,
+            final double riskScore, final double qualityScore)
     {
         try
         {
             StringJoiner sj = new StringJoiner(TSV_DELIM);
-
-            sj.add(chromosome).add(String.valueOf(positionStart)).add(String.valueOf(positionEnd));
-
-            mWriter.write(sj.toString());
-            mWriter.newLine();
-
+            sj.add(chromosome).add(String.valueOf(positionStart)).add(String.valueOf(positionEnd))
+                    .add(String.valueOf(riskScore)).add(String.valueOf(qualityScore));
+            mOutputWriter.write(sj.toString());
+            mOutputWriter.newLine();
         }
         catch(IOException e)
         {
@@ -256,17 +266,17 @@ public class GenomeMappabilityFinder
         ConfigBuilder configBuilder = new ConfigBuilder(APP_NAME);
 
         addRefGenomeFile(configBuilder, true);
+        addSpecificChromosomesRegionsConfig(configBuilder);
+
+        configBuilder.addInteger(CFG_BASE_WINDOW_LENGTH, "Base window length for analysis", DEFAULT_WINDOW_LENGTH);
+        configBuilder.addInteger(CFG_BASE_WINDOW_SPACING, "Offset through the genome of each base window", DEFAULT_WINDOW_SPACING);
+
+        configBuilder.addInteger(CFG_BATCH_SIZE, "Number of windows to align simultaneously", DEFAULT_BATCH_SIZE);
+        addThreadOptions(configBuilder);
 
         configBuilder.addConfigItem(CFG_OUTPUT_FILE, true, "Output filename");
-        configBuilder.addInteger(CFG_PARTITION_SIZE, "Read length", 1_000_000);
 
-        configBuilder.addInteger(CFG_BASE_WINDOW_LENGTH, "Base window length for analysis", DEFAULT_BASE_LENGTH);
-        configBuilder.addInteger(CFG_BASE_BUFFER_LENGTH, "Base window length for analysis", DEFAULT_BUFFER_LENGTH);
-
-        addOutputOptions(configBuilder);
         addLoggingOptions(configBuilder);
-        addThreadOptions(configBuilder);
-        addSpecificChromosomesRegionsConfig(configBuilder);
 
         configBuilder.checkAndParseCommandLine(args);
 
