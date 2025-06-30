b9185cbc1996b5a799b22dd228a83fb38cdbac73
David Koetsier <david.f.koetsier@gmail.com>
2025-06-30 10:10:57 +0200

    Cuppa: DEV-4187: Fix failing SV loading test

--- cuppa/src/test/java/com/hartwig/hmftools/cup/CategoryPrepTest.java
+++ cuppa/src/test/java/com/hartwig/hmftools/cup/CategoryPrepTest.java
@@ -64,13 +64,13 @@ public class CategoryPrepTest
 
         // Check one value of each type
         HashMap<String, String> dataItemsMap = makeDataItemsMap(dataItems);
-        assertEquals(dataItemsMap.get("C>T_TCC"), "2");
-        assertEquals(dataItemsMap.get("snv_count"), "8");
-        assertEquals(dataItemsMap.get("SIG_7_UV"), "6.4");
+        assertEquals("2", dataItemsMap.get("C>T_TCC"));
+        assertEquals("8", dataItemsMap.get("snv_count"));
+        assertEquals("6.4", dataItemsMap.get("SIG_7_UV"));
 
-        assertEquals(dataItemsMap.get("1_500000"), "1");
-        assertEquals(dataItemsMap.get("1_1000000"), "1");
-        assertEquals(dataItemsMap.get("7_55000000"), "1");
+        assertEquals("1", dataItemsMap.get("1_500000"));
+        assertEquals("1", dataItemsMap.get("1_1000000"));
+        assertEquals("1", dataItemsMap.get("7_55000000"));
     }
 
     @Test
@@ -88,13 +88,13 @@ public class CategoryPrepTest
 
         // Check one value of each type
         HashMap<String, String> dataItemsMap = makeDataItemsMap(dataItems);
-        assertEquals(dataItemsMap.get("C>T_TCC"), "2");
-        assertEquals(dataItemsMap.get("snv_count"), "8");
-        assertEquals(dataItemsMap.get("SIG_7_UV"), "6.4");
+        assertEquals("2", dataItemsMap.get("C>T_TCC"));
+        assertEquals("8", dataItemsMap.get("snv_count"));
+        assertEquals("6.4", dataItemsMap.get("SIG_7_UV"));
 
-        assertEquals(dataItemsMap.get("chr1_500000"), "1");
-        assertEquals(dataItemsMap.get("chr1_1000000"), "1");
-        assertEquals(dataItemsMap.get("chr7_55000000"), "1");
+        assertEquals("1", dataItemsMap.get("chr1_500000"));
+        assertEquals("1", dataItemsMap.get("chr1_1000000"));
+        assertEquals("1", dataItemsMap.get("chr7_55000000"));
     }
 
     @Test
@@ -104,12 +104,12 @@ public class CategoryPrepTest
         List<DataItem> dataItems = prep.extractSampleData(selectedSampleId);
 
         assertEquals(6, dataItems.size());
-        assertEquals(dataItems.get(0), new DataItem(DataSource.DNA, ItemType.SV_COUNT, "LINE", "3"));
-        assertEquals(dataItems.get(1), new DataItem(DataSource.DNA, ItemType.SV_COUNT, "SIMPLE_DEL_20KB_1MB", "20"));
-        assertEquals(dataItems.get(2), new DataItem(DataSource.DNA, ItemType.SV_COUNT, "SIMPLE_DUP_32B_200B", "3"));
-        assertEquals(dataItems.get(3), new DataItem(DataSource.DNA, ItemType.SV_COUNT, "SIMPLE_DUP_100KB_5MB", "2"));
-        assertEquals(dataItems.get(4), new DataItem(DataSource.DNA, ItemType.SV_COUNT, "MAX_COMPLEX_SIZE", "8"));
-        assertEquals(dataItems.get(5), new DataItem(DataSource.DNA, ItemType.SV_COUNT, "TELOMERIC_SGL", "0"));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SV_COUNT, "LINE", "3"), dataItems.get(0));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SV_COUNT, "SIMPLE_DEL_20KB_1MB", "25"), dataItems.get(1));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SV_COUNT, "SIMPLE_DUP_32B_200B", "4"), dataItems.get(2));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SV_COUNT, "SIMPLE_DUP_100KB_5MB", "2"), dataItems.get(3));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SV_COUNT, "MAX_COMPLEX_SIZE", "8"), dataItems.get(4));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SV_COUNT, "TELOMERIC_SGL", "0"), dataItems.get(5));
     }
 
     @Test
@@ -119,9 +119,9 @@ public class CategoryPrepTest
         List<DataItem> dataItems = prep.extractSampleData(selectedSampleId);
 
         assertEquals(3, dataItems.size());
-        assertEquals(dataItems.get(0), new DataItem(DataSource.DNA, ItemType.SAMPLE_TRAIT, "is_male", "1"));
-        assertEquals(dataItems.get(1), new DataItem(DataSource.DNA, ItemType.TUMOR_MUTATIONAL_BURDEN, "indels_per_mb", "0.1207"));
-        assertEquals(dataItems.get(2), new DataItem(DataSource.DNA, ItemType.SAMPLE_TRAIT, "whole_genome_duplication", "1"));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SAMPLE_TRAIT, "is_male", "1"), dataItems.get(0));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.TUMOR_MUTATIONAL_BURDEN, "indels_per_mb", "0.1207"), dataItems.get(1));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.SAMPLE_TRAIT, "whole_genome_duplication", "1"), dataItems.get(2));
     }
 
     @Test
@@ -131,14 +131,14 @@ public class CategoryPrepTest
         List<DataItem> dataItems = prep.extractSampleData(selectedSampleId);
 
         assertEquals(8, dataItems.size());
-        assertEquals(dataItems.get(0), new DataItem(DataSource.DNA, ItemType.DRIVER, "BRAF.mut", "1.0000"));
-        assertEquals(dataItems.get(1), new DataItem(DataSource.DNA, ItemType.DRIVER, "MYC.amp", "1.0000"));
-        assertEquals(dataItems.get(2), new DataItem(DataSource.DNA, ItemType.DRIVER, "CDKN2A.mut", "1.0000"));
-        assertEquals(dataItems.get(3), new DataItem(DataSource.DNA, ItemType.DRIVER, "SFTPB.indel", "1.0000"));
-        assertEquals(dataItems.get(4), new DataItem(DataSource.DNA, ItemType.FUSION, "TMPRSS2_ERG", "1.0000"));
-        assertEquals(dataItems.get(5), new DataItem(DataSource.DNA, ItemType.FUSION, "BRAF_PROM3", "1.0000"));
-        assertEquals(dataItems.get(6), new DataItem(DataSource.DNA, ItemType.FUSION, "FGFR2_PROM5", "1.0000"));
-        assertEquals(dataItems.get(7), new DataItem(DataSource.DNA, ItemType.VIRUS, "MERKEL", "1.0000"));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.DRIVER, "BRAF.mut", "1.0000"), dataItems.get(0));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.DRIVER, "MYC.amp", "1.0000"), dataItems.get(1));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.DRIVER, "CDKN2A.mut", "1.0000"), dataItems.get(2));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.DRIVER, "SFTPB.indel", "1.0000"), dataItems.get(3));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.FUSION, "TMPRSS2_ERG", "1.0000"), dataItems.get(4));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.FUSION, "BRAF_PROM3", "1.0000"), dataItems.get(5));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.FUSION, "FGFR2_PROM5", "1.0000"), dataItems.get(6));
+        assertEquals(new DataItem(DataSource.DNA, ItemType.VIRUS, "MERKEL", "1.0000"), dataItems.get(7));
     }
 
     @Test
@@ -148,8 +148,8 @@ public class CategoryPrepTest
         List<DataItem> dataItems = prep.extractSampleData(selectedSampleId);
 
         assertEquals(2, dataItems.size());
-        assertEquals(dataItems.get(0), new DataItem(DataSource.RNA, ItemType.EXPRESSION, "BRAF", "3.434e+00"));
-        assertEquals(dataItems.get(1), new DataItem(DataSource.RNA, ItemType.EXPRESSION, "TP53", "3.870e+00"));
+        assertEquals(new DataItem(DataSource.RNA, ItemType.EXPRESSION, "BRAF", "3.434e+00"), dataItems.get(0));
+        assertEquals(new DataItem(DataSource.RNA, ItemType.EXPRESSION, "TP53", "3.870e+00"), dataItems.get(1));
     }
 
     @Test
@@ -159,9 +159,9 @@ public class CategoryPrepTest
         List<DataItem> dataItems = prep.extractSampleData(selectedSampleId);
 
         assertEquals(4, dataItems.size());
-        assertEquals(dataItems.get(0), new DataItem(DataSource.RNA, ItemType.ALT_SJ, "7;140426316;140439612", "2"));
-        assertEquals(dataItems.get(1), new DataItem(DataSource.RNA, ItemType.ALT_SJ, "10;89623836;89623905", "1"));
-        assertEquals(dataItems.get(2), new DataItem(DataSource.RNA, ItemType.ALT_SJ, "13;32901736;32901958", "1"));
-        assertEquals(dataItems.get(3), new DataItem(DataSource.RNA, ItemType.ALT_SJ, "20;46474688;46476232", "1"));
+        assertEquals(new DataItem(DataSource.RNA, ItemType.ALT_SJ, "7;140426316;140439612", "2"), dataItems.get(0));
+        assertEquals(new DataItem(DataSource.RNA, ItemType.ALT_SJ, "10;89623836;89623905", "1"), dataItems.get(1));
+        assertEquals(new DataItem(DataSource.RNA, ItemType.ALT_SJ, "13;32901736;32901958", "1"), dataItems.get(2));
+        assertEquals(new DataItem(DataSource.RNA, ItemType.ALT_SJ, "20;46474688;46476232", "1"), dataItems.get(3));
     }
 }
