720dd5c0d960e5495b40fc8282d53e03fdbd6cbe
Reece Jones <r.jones@hartwigmedicalfoundation.nl>
2025-08-07 17:06:51 +1000

    PanelBuilder: more unit tests

--- panel-builder/src/test/java/com/hartwig/hmftools/panelbuilder/VariantProbeBuilderTest.java
+++ panel-builder/src/test/java/com/hartwig/hmftools/panelbuilder/VariantProbeBuilderTest.java
@@ -1,8 +1,10 @@
 package com.hartwig.hmftools.panelbuilder;
 
 import static com.hartwig.hmftools.common.genome.region.Orientation.ORIENT_FWD;
+import static com.hartwig.hmftools.common.genome.region.Orientation.ORIENT_REV;
 import static com.hartwig.hmftools.panelbuilder.samplevariants.VariantProbeBuilder.buildMutationProbe;
 import static com.hartwig.hmftools.panelbuilder.samplevariants.VariantProbeBuilder.buildSglProbe;
+import static com.hartwig.hmftools.panelbuilder.samplevariants.VariantProbeBuilder.buildSvProbe;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,13 +19,16 @@ import org.junit.Test;
 public class VariantProbeBuilderTest
 {
     private static final String CHR = "1";
+    private static final String CHR2 = "2";
 
     private final MockRefGenome mRefGenome;
 
     public VariantProbeBuilderTest()
     {
         mRefGenome = new MockRefGenome(true);
-        mRefGenome.RefGenomeMap = Map.of(CHR, "A".repeat(200));
+        mRefGenome.RefGenomeMap = Map.of(
+                CHR, "A".repeat(200),
+                CHR2, "C".repeat(300));
     }
 
     @Test
@@ -74,5 +79,18 @@ public class VariantProbeBuilderTest
         assertEquals(expected, actual);
     }
 
-    // TODO: more tests
+    @Test
+    public void testBuildSvProbe()
+    {
+        VariantProbeData actual = buildSvProbe(
+                CHR, 101, ORIENT_FWD,
+                CHR2, 201, ORIENT_REV,
+                "GGT", 11, mRefGenome);
+        VariantProbeData expected = new VariantProbeData(
+                "AAAAGGTCCCC",
+                new ChrBaseRegion(CHR, 98, 101),
+                "GGT",
+                new ChrBaseRegion(CHR2, 201, 204));
+        assertEquals(expected, actual);
+    }
 }
