52e5d6a906f5ba96bc47fd4fed7a6f58ef857e11
Reece Jones <r.jones@hartwigmedicalfoundation.nl>
2025-08-12 21:05:05 +1000

    PanelBuilder: started MSI sites probes

---
+++ panel-builder/src/main/java/com/hartwig/hmftools/panelbuilder/MsiSites.java
@@ -0,0 +1,83 @@
+package com.hartwig.hmftools.panelbuilder;
+
+import static java.util.Objects.requireNonNull;
+
+import static com.hartwig.hmftools.common.utils.file.CommonFields.FLD_CHROMOSOME;
+import static com.hartwig.hmftools.common.utils.file.CommonFields.FLD_POSITION;
+import static com.hartwig.hmftools.panelbuilder.PanelBuilderConstants.MSI_QUALITY_MIN;
+
+import java.util.List;
+
+import com.hartwig.hmftools.common.region.BasePosition;
+import com.hartwig.hmftools.common.utils.file.DelimFileReader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// TODO: doc
+public class MsiSites
+{
+    // TODO
+    private static final ProbeSelectCriteria PROBE_CRITERIA = new ProbeSelectCriteria(
+            new ProbeEvaluator.Criteria(MSI_QUALITY_MIN, , ),
+            new ProbeSelector.Strategy.BestGc());
+
+    private static final Logger LOGGER = LogManager.getLogger(MsiSites.class);
+
+    public static void generateProbes(final String msiSitesFile, final ProbeGenerator probeGenerator, PanelData panelData)
+    {
+        LOGGER.info("Generating MSI probes");
+
+        List<BasePosition> msiSites = loadMsiSitesFile(msiSitesFile);
+
+        ProbeGenerationResult result = generateProbes(msiSites, probeGenerator, panelData);
+        // Don't bother checking overlaps between MSI probes because there are few sites which are far apart.
+        panelData.addResult(result);
+
+        LOGGER.info("Done generating MSI probes");
+    }
+
+    private static List<BasePosition> loadMsiSitesFile(final String path)
+    {
+        LOGGER.debug("Loading MSI sites file: {}", path);
+
+        try(DelimFileReader reader = new DelimFileReader(path))
+        {
+            int chrIndex = requireNonNull(reader.getColumnIndex(FLD_CHROMOSOME));
+            int posIndex = requireNonNull(reader.getColumnIndex(FLD_POSITION));
+
+            List<BasePosition> sites = reader.stream().map(row ->
+            {
+                String chromosome = row.get(chrIndex);
+                int position = row.getInt(posIndex);
+                return new BasePosition(chromosome, position);
+            }).toList();
+
+            LOGGER.info("Loaded {} MSI sites from {}", sites.size(), path);
+            return sites;
+        }
+    }
+
+    private static ProbeGenerationResult generateProbes(final List<BasePosition> msiSites, final ProbeGenerator probeGenerator,
+            final PanelCoverage coverage)
+    {
+        return msiSites.stream()
+                .map(msiSite -> generateProbe(msiSite, probeGenerator, coverage))
+                .reduce(new ProbeGenerationResult(), ProbeGenerationResult::add);
+    }
+
+    private static ProbeGenerationResult generateProbe(final BasePosition msiSite, final ProbeGenerator probeGenerator,
+            final PanelCoverage coverage)
+    {
+        //         TODO: is this the right methodology?
+        // TODO: how big is the MSI site? can't have probe edge on position?
+        TargetMetadata metadata = createTargetMetadata(msiSite);
+        return probeGenerator.coverPosition(msiSite, metadata, PROBE_CRITERIA, coverage);
+    }
+
+    private static TargetMetadata createTargetMetadata(final BasePosition msiSite)
+    {
+        String extraInfo = msiSite.toString();
+        return new TargetMetadata(TargetMetadata.Type.MSI, extraInfo);
+    }
+}
