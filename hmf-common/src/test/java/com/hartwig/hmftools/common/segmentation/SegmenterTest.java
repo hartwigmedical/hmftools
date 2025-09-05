c434fd41e724ef6ec8a4a5ee71b4b3f58dfef12f
Tim Lavers <laverstim@gmail.com>
2025-09-05 14:56:45 +1000

    Cobalt: move methods that are only for testing to test classes.

--- hmf-common/src/test/java/com/hartwig/hmftools/common/segmentation/SegmenterTest.java
+++ hmf-common/src/test/java/com/hartwig/hmftools/common/segmentation/SegmenterTest.java
@@ -28,39 +28,9 @@ public class SegmenterTest extends SegmentationTestBase
         Segmenter segmenter = new Segmenter(data, gamma, true);
         double segmentCost = segmenter.segmentPenalty;
         double leastCostByFastSearch = segmenter.leastCostSegmentation.cost(segmentCost);
-        double leastCostByExhaustiveSearch = segmenter.cheapestSegmentationByExhaustiveSearch().cost(segmentCost);
+        ExhaustiveSearchSegmenter exhaustiveSearchSegmenter = new ExhaustiveSearchSegmenter(data, gamma, true);
+        double leastCostByExhaustiveSearch = exhaustiveSearchSegmenter.cheapestSegmentationByExhaustiveSearch().cost(segmentCost);
         assertEquals(leastCostByFastSearch, leastCostByExhaustiveSearch, 0.001);
-    }
-
-    @Test
-    public void allPossibleSegmentations()
-    {
-        Set<Segmentation> segmentations1 = new Segmenter(d(1)).allPossibleSegmentations();
-        assertEquals(1, segmentations1.size());
-        assertTrue(segmentations1.contains(segmentation(d(1))));
-
-        Set<Segmentation> segmentations2 = new Segmenter(d(1, 1)).allPossibleSegmentations();
-        assertEquals(2, segmentations2.size());
-        assertTrue(segmentations2.contains(segmentation(d(1), d(1))));
-        assertTrue(segmentations2.contains(segmentation(d(1, 1))));
-
-        Set<Segmentation> segmentations3 = new Segmenter(d(1, 2, 3)).allPossibleSegmentations();
-        assertEquals(4, segmentations3.size());
-        assertTrue(segmentations3.contains(segmentation(d(1), d(2), d(3))));
-        assertTrue(segmentations3.contains(segmentation(d(1, 2), d(3))));
-        assertTrue(segmentations3.contains(segmentation(d(1), d(2, 3))));
-        assertTrue(segmentations3.contains(segmentation(d(1, 2, 3))));
-
-        Set<Segmentation> segmentations4 = new Segmenter(d(1, 2, 3, 1)).allPossibleSegmentations();
-        assertEquals(8, segmentations4.size());
-        assertTrue(segmentations4.contains(segmentation(d(1), d(2), d(3), d(1))));
-        assertTrue(segmentations4.contains(segmentation(d(1, 2), d(3), d(1))));
-        assertTrue(segmentations4.contains(segmentation(d(1), d(2, 3), d(1))));
-        assertTrue(segmentations4.contains(segmentation(d(1), d(2), d(3, 1))));
-        assertTrue(segmentations4.contains(segmentation(d(1, 2), d(3, 1))));
-        assertTrue(segmentations4.contains(segmentation(d(1), d(2, 3, 1))));
-        assertTrue(segmentations4.contains(segmentation(d(1, 2, 3), d(1))));
-        assertTrue(segmentations4.contains(segmentation(d(1, 2, 3, 1))));
     }
 
     @Test
@@ -119,34 +89,67 @@ public class SegmenterTest extends SegmentationTestBase
         );
     }
 
+    // Tests for ExhaustiveSearchSegmenter so that we know it can be trusted in tests.
     @Test
-    public void pcfExact()
+    public void exhaustiveSearchSegmenterTest()
+    {
+        Set<Segmentation> segmentations1 = new ExhaustiveSearchSegmenter(d(1)).allPossibleSegmentations();
+        assertEquals(1, segmentations1.size());
+        assertTrue(segmentations1.contains(segmentation(d(1))));
+
+        Set<Segmentation> segmentations2 = new ExhaustiveSearchSegmenter(d(1, 1)).allPossibleSegmentations();
+        assertEquals(2, segmentations2.size());
+        assertTrue(segmentations2.contains(segmentation(d(1), d(1))));
+        assertTrue(segmentations2.contains(segmentation(d(1, 1))));
+
+        Set<Segmentation> segmentations3 = new ExhaustiveSearchSegmenter(d(1, 2, 3)).allPossibleSegmentations();
+        assertEquals(4, segmentations3.size());
+        assertTrue(segmentations3.contains(segmentation(d(1), d(2), d(3))));
+        assertTrue(segmentations3.contains(segmentation(d(1, 2), d(3))));
+        assertTrue(segmentations3.contains(segmentation(d(1), d(2, 3))));
+        assertTrue(segmentations3.contains(segmentation(d(1, 2, 3))));
+
+        Set<Segmentation> segmentations4 = new ExhaustiveSearchSegmenter(d(1, 2, 3, 1)).allPossibleSegmentations();
+        assertEquals(8, segmentations4.size());
+        assertTrue(segmentations4.contains(segmentation(d(1), d(2), d(3), d(1))));
+        assertTrue(segmentations4.contains(segmentation(d(1, 2), d(3), d(1))));
+        assertTrue(segmentations4.contains(segmentation(d(1), d(2, 3), d(1))));
+        assertTrue(segmentations4.contains(segmentation(d(1), d(2), d(3, 1))));
+        assertTrue(segmentations4.contains(segmentation(d(1, 2), d(3, 1))));
+        assertTrue(segmentations4.contains(segmentation(d(1), d(2, 3, 1))));
+        assertTrue(segmentations4.contains(segmentation(d(1, 2, 3), d(1))));
+        assertTrue(segmentations4.contains(segmentation(d(1, 2, 3, 1))));
+    }
+
+    @Test
+    public void exhaustiveSearchCheapestTest()
     {
         assertEquals(
                 new PiecewiseConstantFit(new int[] { 1 }, new int[] { 0 }, d(1.0)),
-                new Segmenter(d(1.0), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
+                new ExhaustiveSearchSegmenter(d(1.0), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
         );
         assertEquals(
                 new PiecewiseConstantFit(new int[] { 2 }, new int[] { 0 }, d(1.0)),
-                new Segmenter(d(1.0, 1.0), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
+                new ExhaustiveSearchSegmenter(d(1.0, 1.0), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
         );
         assertEquals(
                 new PiecewiseConstantFit(new int[] { 3 }, new int[] { 0 }, d(1.0)),
-                new Segmenter(d(1.0, 1.0, 1.0), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
+                new ExhaustiveSearchSegmenter(d(1.0, 1.0, 1.0), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
         );
 
         assertEquals(
                 new PiecewiseConstantFit(new int[] { 3, 3 }, new int[] { 0, 3 }, d(2.0, 12.0)),
-                new Segmenter(d(2, 3, 1, 12, 13, 11), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
+                new ExhaustiveSearchSegmenter(d(2, 3, 1, 12, 13, 11), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
         );
 
         assertEquals(
                 new PiecewiseConstantFit(new int[] { 3, 3, 3 }, new int[] { 0, 3, 6 }, d(2.0, 12.0, 2.0)),
-                new Segmenter(d(2, 3, 1, 12, 13, 11, 2, 3, 1), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
+                new ExhaustiveSearchSegmenter(d(2, 3, 1, 12, 13, 11, 2, 3, 1), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
         );
         assertEquals(
                 new PiecewiseConstantFit(new int[] { 3, 3, 3 }, new int[] { 0, 3, 6 }, d(2.0, 12.0, 22.0)),
-                new Segmenter(d(2, 3, 1, 12, 13, 11, 22, 23, 21), 50.0, false).cheapestSegmentationByExhaustiveSearch().pcf()
+                new ExhaustiveSearchSegmenter(d(2, 3, 1, 12, 13, 11, 22, 23, 21), 50.0, false).cheapestSegmentationByExhaustiveSearch()
+                        .pcf()
         );
     }
 }
