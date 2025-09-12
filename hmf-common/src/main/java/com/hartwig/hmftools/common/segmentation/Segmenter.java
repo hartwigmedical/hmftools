39ef7368bf26a47c5ba672684d64a0602987c1c8
Tim Lavers <laverstim@gmail.com>
2025-09-12 10:34:13 +1000

    Common: performance improvement for (as yet unused) Segmenter.

--- hmf-common/src/main/java/com/hartwig/hmftools/common/segmentation/Segmenter.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/segmentation/Segmenter.java
@@ -2,17 +2,13 @@ package com.hartwig.hmftools.common.segmentation;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Given a list of doubles and a segmentation penalty this class finds the least cost
  * segmentation of the doubles using a dynamic programming algorithm described in
  * "Copynumber: Eﬃcient algorithms for single- and multi-track copy number segmentation"
  * Nilsen et al. BMC Genomics 2012,13:591 <a href="http://www.biomedcentral.com/1471-2164/13/591">...</a>
- * <p>
- * A solution by exhaustive search is provided too, to assist in testing.
  */
 public class Segmenter
 {
@@ -28,33 +24,38 @@ public class Segmenter
         // and s in [0, ..., e]. For any such pair, the least cost is:
         // leastCost(s,e) = leastCostOfSegmentingTo(s-1) + segmentPenalty + intervalCost(s,e)
         // The least cost segmentation corresponds to the leastCostOfSegmentingTo(y.size - 1)
-        Map<Integer, Double> leastCostEndingAt = new HashMap<>();
-        leastCostEndingAt.put(-1, 0.0);
+        List<Double> leastCostEndingJustBefore = new ArrayList<>(y.length);
+        leastCostEndingJustBefore.add(0, 0.0);
         // We record the segment endpoints so that we can recover the least cost segmentation itself.
         List<Integer> leastCostSegmentEndpoints = new ArrayList<>(); // there's only one possible segment of length 1
         segmentPenalty = new Gamma(y, gamma, normalise).getSegmentPenalty();
-        List<Double> partialSumsFromIndexToCurrentEnd = new ArrayList<>();
+
+        // Precompute cumulative sums for efficient partial sum calculation.
+        double[] cumulativeSums = new double[y.length];
+        if (y.length > 0) {
+            cumulativeSums[0] = y[0];
+            for (int i = 1; i < y.length; i++) {
+                cumulativeSums[i] = cumulativeSums[i - 1] + y[i];
+            }
+        }
+
         for(int end = 0; end < y.length; end++)
         {
-            for(int i = 0; i < partialSumsFromIndexToCurrentEnd.size(); i++)
-            {
-                partialSumsFromIndexToCurrentEnd.set(i, partialSumsFromIndexToCurrentEnd.get(i) + y[end]);
-            }
-            partialSumsFromIndexToCurrentEnd.add(y[end]);
             double minCost = Double.MAX_VALUE;
             int endOfPreviousSegmentForLeastCost = 0;
             for(int start = 0; start <= end; start++)
             {
-                double partialSum = partialSumsFromIndexToCurrentEnd.get(start);
+                // Calculate partial sum efficiently using precomputed cumulative sums
+                double partialSum = start == 0 ? cumulativeSums[end] : cumulativeSums[end] - cumulativeSums[start - 1];
                 double segmentCost = 1 * partialSum * partialSum / ((start - end - 1.0f));
-                double cost = leastCostEndingAt.get(start - 1) + segmentPenalty + segmentCost;
+                double cost = leastCostEndingJustBefore.get(start) + segmentPenalty + segmentCost;
                 if(cost < minCost)
                 {
                     minCost = cost;
                     endOfPreviousSegmentForLeastCost = start - 1;
                 }
             }
-            leastCostEndingAt.put(end, minCost);
+            leastCostEndingJustBefore.add(end + 1, minCost);
             leastCostSegmentEndpoints.add(endOfPreviousSegmentForLeastCost);
         }
         List<Integer> segmentEndpoints = new ArrayList<>();
