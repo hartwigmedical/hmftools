eff4ce1ff9e3ff4bd9d2677644d569ff49639003
Tim Lavers <laverstim@gmail.com>
2025-09-17 14:03:39 +1000

    Cobalt: batch-mode tester needs to invoke Cobalt with ref genome version.

--- cobalt/src/test/java/com/hartwig/hmftools/cobalt/e2e/CobaltBatchLocal.java
+++ cobalt/src/test/java/com/hartwig/hmftools/cobalt/e2e/CobaltBatchLocal.java
@@ -5,24 +5,25 @@ import static com.hartwig.hmftools.cobalt.CobaltConfig.TARGET_REGION_NORM_FILE;
 import static com.hartwig.hmftools.cobalt.utils.CobaltOutputsComparison.COMPARISON_VALUES_DIR;
 import static com.hartwig.hmftools.cobalt.utils.CobaltOutputsComparison.ORIGINAL_VALUES_DIR;
 import static com.hartwig.hmftools.common.genome.gc.GCProfileFactory.GC_PROFILE;
+import static com.hartwig.hmftools.common.genome.refgenome.RefGenomeVersion.REF_GENOME_VERSION;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.SAMPLE;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.TUMOR;
 import static com.hartwig.hmftools.common.utils.config.CommonConfig.TUMOR_BAM;
 import static com.hartwig.hmftools.common.utils.file.FileWriterUtils.OUTPUT_DIR;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import com.hartwig.hmftools.cobalt.CobaltApplication;
 import com.hartwig.hmftools.cobalt.utils.CobaltOutputsComparison;
 
 import org.junit.Ignore;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.ArrayList;
-import java.util.List;
 
 @Ignore
 public class CobaltBatchLocal
@@ -36,14 +37,19 @@ public class CobaltBatchLocal
     @Test
     public void run() throws Exception
     {
+        //        List<String> samples = List.of("Sample_13927535");
         List<String> samples = List.of("Sample_13927535", "Sample_15307684", "Sample_15846501");
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         List<Future<?>> futures = new ArrayList<>();
         samples.forEach(sample -> addExecutorTaskForSample(futures, executorService, sample));
-        for (Future<?> future : futures) {
-            try {
+        for(Future<?> future : futures)
+        {
+            try
+            {
                 future.get();
-            } catch (Exception e) {
+            }
+            catch(Exception e)
+            {
                 executorService.shutdownNow();
                 throw e;
             }
@@ -56,21 +62,24 @@ public class CobaltBatchLocal
 
     private void addExecutorTaskForSample(List<Future<?>> futures, ExecutorService executorService, String sample)
     {
-        futures.add(executorService.submit(() -> {
-            try {
+        futures.add(executorService.submit(() ->
+        {
+            try
+            {
                 runCobalt(sample);
-            } catch (Exception e) {
+            }
+            catch(Exception e)
+            {
                 throw new RuntimeException(e);
             }
         }));
-
     }
 
     private void runCobalt(String sample)
     {
         String bamFile = bamBase + sample + ".bam";
         String outputDir = cobaltOutputBase + sample;
-        String[] args = new String[12];
+        String[] args = new String[14];
         int index = 0;
         args[index++] = String.format("-%s", TUMOR);
         args[index++] = String.format("%s", sample);
@@ -80,6 +89,8 @@ public class CobaltBatchLocal
         args[index++] = String.format("%s", gcProfile);
         args[index++] = String.format("-%s", PCF_GAMMA);
         args[index++] = String.format("%d", 50);
+        args[index++] = String.format("-%s", REF_GENOME_VERSION);
+        args[index++] = String.format("%s", "37");
         args[index++] = String.format("-%s", OUTPUT_DIR);
         args[index++] = String.format("%s", outputDir);
         args[index++] = String.format("-%s", TARGET_REGION_NORM_FILE);
