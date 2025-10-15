e8913f543a878ad0478c29df48162222fa0996be
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2025-10-16 09:39:52 +1100

    Qsee: Add sample by feature matrix

---
+++ qsee/src/main/java/feature/SampleFeatureMatrix.java
@@ -0,0 +1,118 @@
+package feature;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class SampleFeatureMatrix
+{
+    private final Map<String, Double[]> mFeatureValuesMap;
+    private final List<String> mSampleIds;
+
+    // There is no concurrent implementation of LinkedHashMap.
+    // Therefore, store feature keys (= Map keys) in a list to store the insertion order of features.
+    private final List<String> mFeatureKeys = new ArrayList<>();
+
+    public SampleFeatureMatrix(Map<String, Double[]> featureValuesMap, List<String> sampleIds)
+    {
+        mFeatureValuesMap = featureValuesMap;
+        mSampleIds = sampleIds;
+
+        validateInput();
+
+        mFeatureKeys.addAll(featureValuesMap.keySet());
+    }
+
+    private void validateInput()
+    {
+        if(mSampleIds.isEmpty())
+        {
+            throw new IllegalArgumentException("Sample IDs must not be empty");
+        }
+
+        Set<String> uniqueSampleIds = new HashSet<>(mSampleIds);
+        int nonUniqueSampleIdCount = mSampleIds.size() - uniqueSampleIds.size();
+
+        if(nonUniqueSampleIdCount > 0)
+        {
+            throw new IllegalArgumentException(String.format("Sample IDs must be unique. Found %s non-unique sample IDs",
+                    nonUniqueSampleIdCount));
+        }
+
+        if(!mFeatureValuesMap.isEmpty())
+        {
+            String firstKey = mFeatureValuesMap.keySet().stream().findFirst().get();
+            int expectedNumSamples = mFeatureValuesMap.get(firstKey).length;
+
+            for(String key : mFeatureValuesMap.keySet())
+            {
+                if(mFeatureValuesMap.get(key).length != expectedNumSamples)
+                {
+                    throw new IllegalArgumentException(String.format("All feature columns must have the same length. Expected %s but got %s for key '%s'",
+                            expectedNumSamples, mFeatureValuesMap.get(key).length, key));
+                }
+            }
+        }
+    }
+
+    public List<String> getSampleIds(){ return mSampleIds; }
+
+    public List<String> getFeatureKeys(){ return mFeatureKeys; }
+
+    public Double[][] getValues(boolean samplesAsRows)
+    {
+        return samplesAsRows ? getSampleByFeatureMatrix() : getFeatureBySampleMatrix();
+    }
+
+    private Double[][] getSampleByFeatureMatrix()
+    {
+        Double[][] matrix = new Double[mSampleIds.size()][mFeatureKeys.size()];
+
+        for(int i = 0; i < mSampleIds.size(); i++)
+        {
+            for(int j = 0; j < mFeatureKeys.size(); j++)
+            {
+                matrix[i][j] = mFeatureValuesMap.get(mFeatureKeys.get(j))[i];
+            }
+        }
+
+        return matrix;
+    }
+
+    private Double[][] getFeatureBySampleMatrix()
+    {
+        Double[][] matrix = new Double[mFeatureKeys.size()][mSampleIds.size()];
+
+        for(int i = 0; i < mFeatureKeys.size(); i++)
+        {
+            matrix[i] = mFeatureValuesMap.get(mFeatureKeys.get(i));
+        }
+
+        return matrix;
+    }
+
+    public synchronized void addSampleFeatures(String sampleId, List<FeatureValue<Double>> features)
+    {
+        if(!mSampleIds.contains(sampleId))
+        {
+            mSampleIds.add(sampleId);
+        }
+
+        int sampleIndex = mSampleIds.indexOf(sampleId);
+
+        for(FeatureValue<Double> feature : features)
+        {
+            String key = feature.mKey;
+
+            if(!mFeatureKeys.contains(key))
+            {
+                mFeatureKeys.add(key);
+                mFeatureValuesMap.put(key, new Double[mSampleIds.size()]);
+            }
+
+            mFeatureValuesMap.get(key)[sampleIndex] = feature.mValue;
+        }
+    }
+}
---
+++ qsee/src/test/java/feature/SampleFeatureMatrixTest.java
@@ -0,0 +1,47 @@
+package feature;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SampleFeatureMatrixTest
+{
+    @Test
+    public void canConstructMatrixIncrementally()
+    {
+        List<FeatureValue<Double>> sample1Features = List.of(
+                new FeatureValue<>("feature1", 1.1, null),
+                new FeatureValue<>("feature2", 1.2, null)
+        );
+
+        List<FeatureValue<Double>> sample2Features = List.of(
+                new FeatureValue<>("feature1", 2.1, null),
+                new FeatureValue<>("feature2", 2.2, null)
+        );
+
+        List<FeatureValue<Double>> sample3Features = List.of(
+                new FeatureValue<>("feature1", 3.1, null),
+                new FeatureValue<>("feature2", 3.2, null),
+                new FeatureValue<>("feature3", 3.3, null)
+        );
+
+        SampleFeatureMatrix matrix = new SampleFeatureMatrix(new HashMap<>(), List.of("sample1", "sample2", "sample3"));
+        matrix.addSampleFeatures("sample1", sample1Features);
+        matrix.addSampleFeatures("sample2", sample2Features);
+        matrix.addSampleFeatures("sample3", sample3Features);
+
+        assertEquals(List.of("sample1", "sample2", "sample3"), matrix.getSampleIds());
+        assertEquals(List.of("feature1", "feature2", "feature3"), matrix.getFeatureKeys());
+
+        Double[][] expectedFeatureValues = {
+                {1.1, 1.2, null},
+                {2.1, 2.2, null},
+                {3.1, 3.2, 3.3},
+        };
+        Double[][] actualFeatureValues = matrix.getValues(true);
+        assertEquals(expectedFeatureValues, actualFeatureValues);
+    }
+}
