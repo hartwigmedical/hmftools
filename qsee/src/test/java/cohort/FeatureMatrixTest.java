d88ad2e142f7495552755d09a18134391c5d4b44
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2025-10-22 15:24:18 +1100

    Qsee: Add test for adding rows to FeatureMatrix concurrently

--- qsee/src/test/java/cohort/FeatureMatrixTest.java
+++ qsee/src/test/java/cohort/FeatureMatrixTest.java
@@ -1,9 +1,13 @@
 package cohort;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
@@ -45,13 +49,81 @@ public class FeatureMatrixTest
         assertEquals(List.of("sample1", "sample2", "sample3", "sample4"), matrix.getRowIds());
         assertEquals(List.of("feature1", "feature2", "feature3"), matrix.getFeatureKeys());
 
-        double[][] expectedFeatureValues = {
+        double[][] expectedValues = {
                 { 1.1, 1.2, Double.NaN },
                 { 2.1, 2.2, Double.NaN },
                 { 3.1, 3.2, 3.3 },
                 { 4.1, 4.2, 4.3 },
         };
-        double[][] actualFeatureValues = matrix.getValues();
-        assertEquals(expectedFeatureValues, actualFeatureValues);
+        double[][] actualValues = matrix.getValues();
+        assertArrayEquals(expectedValues, actualValues);
+    }
+
+    @Test
+    public void canAddRowsConcurrently() throws InterruptedException
+    {
+        int NUM_SAMPLE_THREADS = 100;
+        int NUM_FEATURES = 5;
+
+        FeatureMatrix matrix = new FeatureMatrix(new ConcurrentHashMap<>(), NUM_SAMPLE_THREADS);
+
+        // Create workers
+        List<Thread> threads = new ArrayList<>();
+        for(int sampleIndex = 0; sampleIndex < NUM_SAMPLE_THREADS; sampleIndex++)
+        {
+            Thread thread = createAddRowThread(matrix, sampleIndex, NUM_FEATURES);
+            thread.start();
+            threads.add(thread);
+        }
+
+        for(Thread thread : threads)
+        {
+            thread.join();
+        }
+
+        // Check result
+        List<String> expectedSampleIds = IntStream.range(0, NUM_SAMPLE_THREADS).mapToObj(x -> formTestSampleId(x)).toList();
+        assertEquals(expectedSampleIds, matrix.getRowIds());
+
+        List<String> expectedFeatureKeys = IntStream.range(0, NUM_FEATURES).mapToObj(x -> formTestFeatureKey(x)).toList();
+        assertEquals(expectedFeatureKeys, matrix.getFeatureKeys());
+
+        double[][] expectedValues = createExpectedValues(NUM_SAMPLE_THREADS, NUM_FEATURES);
+        double[][] actualValues = matrix.getValues();
+        assertArrayEquals(expectedValues, actualValues);
+    }
+
+    private static String formTestSampleId(int sampleIndex) { return String.format("sample%d", sampleIndex); }
+    private static String formTestFeatureKey(int featureIndex) { return String.format("feature%d", featureIndex); }
+    private static double formTestFeatureValue(int sampleIndex, int featureIndex) { return sampleIndex + 0.1*featureIndex; }
+
+    private Thread createAddRowThread(FeatureMatrix matrix, int sampleIndex, int numFeatures)
+    {
+        String sampleId = formTestSampleId(sampleIndex);
+
+        List<FeatureValue> features = new ArrayList<>();
+        for(int featureIndex = 0; featureIndex < numFeatures; ++featureIndex)
+        {
+            String featureKey = formTestFeatureKey(featureIndex);
+            double featureValue = formTestFeatureValue(sampleIndex, featureIndex);
+            features.add(new FeatureValue(featureKey, featureValue));
+        }
+
+        return new Thread(() -> matrix.addRow(sampleId, features));
+    }
+
+    private double[][] createExpectedValues(int numSamples, int numFeatures)
+    {
+        double[][] expectedValues = new double[numSamples][numFeatures];
+
+        for(int sampleIndex = 0; sampleIndex < numSamples; sampleIndex++)
+        {
+            for(int featureIndex = 0; featureIndex < numFeatures; featureIndex++)
+            {
+                expectedValues[sampleIndex][featureIndex] = formTestFeatureValue(sampleIndex, featureIndex);
+            }
+        }
+
+        return expectedValues;
     }
 }
