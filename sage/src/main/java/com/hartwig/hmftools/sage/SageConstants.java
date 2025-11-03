82bb9d6d45ce879e43a8d7c458ab95c203c8017e
Charles Shale <shalecharles@gmail.com>
2025-11-03 11:28:34 +1100

    Sage: minor change to hotspot jitter threshold

--- sage/src/main/java/com/hartwig/hmftools/sage/SageConstants.java
+++ sage/src/main/java/com/hartwig/hmftools/sage/SageConstants.java
@@ -56,7 +56,7 @@ public class SageConstants
 
     public static final double MSI_JITTER_MIN_RATIO = 2;
     public static final double MSI_JITTER_MIN_RATIO_HOTSPOT = 1.5;
-    public static final double MSI_JITTER_RATIO_HOTSPOT_VAF = 0.025;
+    public static final double MSI_JITTER_RATIO_HOTSPOT_VAF = 0.03;
 
     public static final int INDEL_UNCERTAIN_BASE_REPEAT_MIN = 3;
 
