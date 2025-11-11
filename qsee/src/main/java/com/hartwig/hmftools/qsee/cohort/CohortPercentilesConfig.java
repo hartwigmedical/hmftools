ddd150cc9c5b64b2503cd194c360471608b89727
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2025-11-12 09:34:02 +1100

    Qsee: Add CohortPercentilesConfig

---
+++ qsee/src/main/java/com/hartwig/hmftools/qsee/cohort/CohortPercentilesConfig.java
@@ -0,0 +1,7 @@
+package com.hartwig.hmftools.qsee.cohort;
+
+public class CohortPercentilesConfig
+{
+    public static final String COHORT_PERCENTILES_FILE = "cohort_percentiles_file";
+    public static final String COHORT_PERCENTILES_FILE_DESC = "Path to the cohort percentiles file";
+}
--- qsee/src/main/java/com/hartwig/hmftools/qsee/vis/VisPrepConfig.java
+++ qsee/src/main/java/com/hartwig/hmftools/qsee/vis/VisPrepConfig.java
@@ -1,5 +1,8 @@
 package com.hartwig.hmftools.qsee.vis;
 
+import static com.hartwig.hmftools.qsee.cohort.CohortPercentilesConfig.COHORT_PERCENTILES_FILE;
+import static com.hartwig.hmftools.qsee.cohort.CohortPercentilesConfig.COHORT_PERCENTILES_FILE_DESC;
+
 import com.hartwig.hmftools.common.utils.config.ConfigBuilder;
 import com.hartwig.hmftools.qsee.prep.CommonPrepConfig;
 
@@ -7,9 +10,6 @@ public class VisPrepConfig
 {
     public final CommonPrepConfig CommonPrep;
     public final String CohortPercentilesFile;
-
-    public static final String COHORT_PERCENTILES_FILE = "cohort_percentiles_file";
-    public static final String COHORT_PERCENTILES_FILE_DESC = "Path to the cohort percentiles file";
 
     public VisPrepConfig(final ConfigBuilder configBuilder)
     {
