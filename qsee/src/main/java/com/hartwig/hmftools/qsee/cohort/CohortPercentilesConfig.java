cd5fd729256e45192fd6daee76604d6c85c9955c
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2025-11-13 10:22:18 +1100

    Qsee: Move CohortPercentileConfig variables to CohortPercentilesFile

--- qsee/src/main/java/com/hartwig/hmftools/qsee/cohort/CohortPercentilesConfig.java
+++
@@ -1,7 +0,0 @@
-package com.hartwig.hmftools.qsee.cohort;
-
-public class CohortPercentilesConfig
-{
-    public static final String COHORT_PERCENTILES_FILE = "cohort_percentiles_file";
-    public static final String COHORT_PERCENTILES_FILE_DESC = "Path to the cohort percentiles file";
-}
