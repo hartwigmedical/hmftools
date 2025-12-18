7bf8bbfe24245bfaf695a1810e343393d6546dc3
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2025-12-18 15:00:10 +0100

    Class was in wrong package

--- finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/Driver.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/Driver.java
@@ -1,19 +1,18 @@
-package com.hartwig.hmftools.datamodel.driver;
+package com.hartwig.hmftools.datamodel.finding;
 
-import com.hartwig.hmftools.datamodel.finding.Finding;
+import com.hartwig.hmftools.datamodel.driver.DriverInterpretation;
+import com.hartwig.hmftools.datamodel.driver.DriverSource;
+import com.hartwig.hmftools.datamodel.driver.ReportedStatus;
 
 import org.jetbrains.annotations.NotNull;
 
 public interface Driver extends Finding
 {
-    @NotNull
-    DriverSource driverSource();
+    @NotNull DriverSource driverSource();
 
-    @NotNull
-    ReportedStatus reportedStatus();
+    @NotNull ReportedStatus reportedStatus();
 
-    @NotNull
-    DriverInterpretation driverInterpretation();
+    @NotNull DriverInterpretation driverInterpretation();
 
     default boolean isReportable()
     {
