95e7058a2dc6b1fbc9036c33526d43f554f39fa9
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2025-12-19 01:00:10 +1100

    findings: Add finding and finding-datamodel

---
+++ finding/src/main/java/com/hartwig/hmftools/datamodel/finding/Drivers.java
@@ -0,0 +1,48 @@
+package com.hartwig.hmftools.datamodel.finding;
+
+import java.util.List;
+import java.util.Set;
+
+import com.hartwig.hmftools.datamodel.purple.PurpleDriver;
+import com.hartwig.hmftools.datamodel.purple.PurpleDriverType;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+final class Drivers
+{
+    private static final Set<PurpleDriverType> MUTATION_DRIVER_TYPES =
+            Set.of(PurpleDriverType.MUTATION, PurpleDriverType.GERMLINE_MUTATION);
+
+    @NotNull
+    public static List<PurpleDriver> nonCanonicalMutationEntries(@NotNull List<PurpleDriver> drivers)
+    {
+        List<PurpleDriver> nonCanonicalVariantEntries = new java.util.ArrayList<>();
+        for(PurpleDriver driver : drivers)
+        {
+            if(MUTATION_DRIVER_TYPES.contains(driver.type()) && !driver.isCanonical())
+            {
+                nonCanonicalVariantEntries.add(driver);
+            }
+        }
+        return nonCanonicalVariantEntries;
+    }
+
+    @Nullable
+    public static PurpleDriver canonicalMutationEntryForGene(@NotNull List<PurpleDriver> drivers, @NotNull String geneToFind)
+    {
+        PurpleDriver highest = null;
+        for(PurpleDriver driver : drivers)
+        {
+            if(MUTATION_DRIVER_TYPES.contains(driver.type()) && driver.gene().equals(geneToFind) && driver.isCanonical())
+            {
+                if(highest == null || driver.driverLikelihood() > highest.driverLikelihood())
+                {
+                    highest = driver;
+                }
+            }
+        }
+
+        return highest;
+    }
+}
