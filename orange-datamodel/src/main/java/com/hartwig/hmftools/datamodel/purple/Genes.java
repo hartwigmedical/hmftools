506cb34a50ff121290cb41eb2cc968c420229df9
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-01-07 02:32:22 +1100

    Moved Gene lists to separate class in orange-datamodel

---
+++ orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/purple/Genes.java
@@ -0,0 +1,9 @@
+package com.hartwig.hmftools.datamodel.purple;
+
+import java.util.Set;
+
+public class Genes {
+
+    public static final Set<String> HRD_GENES = Set.of("BRCA1", "BRCA2", "PALB2", "RAD51C", "RAD51B");
+    public static final Set<String> MSI_GENES = Set.of("MSH6", "MSH2", "MLH1", "PMS2", "EPCAM");
+}
