6c6b181911e1c8774c1193eacb1d8cb542ad9ef5
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-01-09 10:30:18 +0100

    Added utils to read/write FindingRecords from/to Json, and to convert and Orange json to a findings.json
    Also added a version to FindingRecord

---
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/FindingsJson.java
@@ -0,0 +1,63 @@
+package com.hartwig.hmftools.datamodel.finding;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.ServiceLoader;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapterFactory;
+import com.hartwig.hmftools.datamodel.LocalDateAdapter;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FindingsJson {
+
+    @Nullable
+    private static FindingsJson instance;
+
+    private final Gson gson;
+
+    @NotNull
+    public static FindingsJson getInstance() {
+        if (instance == null) {
+            instance = new FindingsJson();
+        }
+        return instance;
+    }
+
+    private FindingsJson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        for (TypeAdapterFactory factory : ServiceLoader.load(TypeAdapterFactory.class)) {
+            gsonBuilder.registerTypeAdapterFactory(factory);
+        }
+
+        gson = gsonBuilder.serializeNulls().serializeSpecialFloatingPointValues().setPrettyPrinting()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .create();
+    }
+
+    @NotNull
+    public FindingRecord read(@NotNull Path orangeJsonFilePath) throws IOException {
+        return read(Files.newBufferedReader(orangeJsonFilePath));
+    }
+
+    @NotNull
+    public FindingRecord read(@NotNull Reader reader) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(reader)) {
+            return gson.fromJson(bufferedReader, FindingRecord.class);
+        }
+    }
+
+    public void write(@NotNull FindingRecord findingRecord, @NotNull Path outputFilePath) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(outputFilePath)) {
+            gson.toJson(findingRecord, FindingRecord.class, writer);
+        }
+    }
+}
