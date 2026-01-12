eb73156a3ab9f6430bf2517b169fe4dcdf1b0e07
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2026-01-12 11:44:57 +0700

    Patient-db: Add unit test for loading driver gene panel

---
+++ patient-db/src/test/java/com/hartwig/hmftools/patientdb/LoadDriverGenePanelTest.java
@@ -0,0 +1,61 @@
+package com.hartwig.hmftools.patientdb;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import com.hartwig.hmftools.common.driver.DriverCategory;
+import com.hartwig.hmftools.common.driver.panel.DriverGene;
+import com.hartwig.hmftools.common.driver.panel.DriverGeneGermlineReporting;
+import com.hartwig.hmftools.common.driver.panel.ImmutableDriverGene;
+import com.hartwig.hmftools.patientdb.database.hmfpatients.Tables;
+import com.hartwig.hmftools.patientdb.database.hmfpatients.tables.records.DrivergenepanelRecord;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class LoadDriverGenePanelTest extends DatabaseAutoSetup
+{
+    @Test
+    public void canLoadDriverGenePanel()
+    {
+        List<DriverGene> driverGenes = List.of(
+                createTestGene("BRCA1"),
+                createTestGene("BRCA2")
+        );
+
+        DB_ACCESS.writeGenePanel(driverGenes);
+
+        List<DrivergenepanelRecord> driverGeneRecords = DB_ACCESS.context()
+                .select().from(Tables.DRIVERGENEPANEL).fetch()
+                .stream().map(r -> r.into(DrivergenepanelRecord.class))
+                .toList();
+
+        assertEquals(2, driverGeneRecords.size());
+    }
+
+    private static DriverGene createTestGene(String geneName)
+    {
+        return ImmutableDriverGene.builder()
+                .gene(geneName)
+                .reportMissenseAndInframe(false)
+                .reportNonsenseAndFrameshift(false)
+                .reportSplice(false)
+                .reportDeletion(false)
+                .reportHetDeletion(false)
+                .hetDeletionThreshold(0)
+                .reportDisruption(false)
+                .reportAmplification(false)
+                .amplificationRatio(0)
+                .reportSomaticHotspot(false)
+                .reportGermlineVariant(DriverGeneGermlineReporting.NONE)
+                .reportGermlineHotspot(DriverGeneGermlineReporting.NONE)
+                .likelihoodType(DriverCategory.TSG)
+                .reportGermlineDeletion(DriverGeneGermlineReporting.NONE)
+                .reportGermlineDisruption(DriverGeneGermlineReporting.NONE)
+                .additionalReportedTranscripts(List.of())
+                .reportPGX(false)
+                .build();
+    }
+}
