0e54337f33cca63dd6475704055877eb0dc80cd9
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-01-12 16:20:23 +0100

    Always sort on finding key

--- finding/src/main/java/com/hartwig/hmftools/datamodel/finding/HlaAlleleFactory.java
+++ finding/src/main/java/com/hartwig/hmftools/datamodel/finding/HlaAlleleFactory.java
@@ -1,7 +1,6 @@
 package com.hartwig.hmftools.datamodel.finding;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -84,7 +83,7 @@ public class HlaAlleleFactory
             }
         }
 
-        hlaAlleles.sort(Comparator.comparing(HlaAllele::gene).thenComparing(HlaAllele::allele));
+        hlaAlleles.sort(HlaAllele.COMPARATOR);
         return hlaAlleles;
     }
 
