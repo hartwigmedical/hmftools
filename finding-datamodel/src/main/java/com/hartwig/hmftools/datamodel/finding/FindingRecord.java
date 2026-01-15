1f590903363f4c032197a54aefec016a22772b16
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-01-15 15:18:24 +0100

    Moved meta properties to record

--- finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/FindingRecord.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/FindingRecord.java
@@ -1,19 +1,12 @@
 package com.hartwig.hmftools.datamodel.finding;
 
-import com.hartwig.hmftools.datamodel.orange.ExperimentType;
-import com.hartwig.hmftools.datamodel.orange.OrangeRefGenomeVersion;
-
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 
 @RecordBuilder
 public record FindingRecord(
-        @NotNull ExperimentType experimentType,
-        @Nullable String pipelineVersion,
-        @NotNull String version,
-        @NotNull OrangeRefGenomeVersion refGenomeVersion,
+        @NotNull MetaProperties metaProperties,
         @NotNull PurityPloidyFit purityPloidyFit,
         @NotNull DriverFindingList<SmallVariant> smallVariants,
         @NotNull DriverFindingList<GainDeletion> gainDeletions,
