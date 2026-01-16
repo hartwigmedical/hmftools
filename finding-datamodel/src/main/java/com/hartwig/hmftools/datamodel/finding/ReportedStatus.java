1fb9c2c95969d569e77901c1ef15a6c37677547f
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-01-16 14:09:16 +1100

    finding-datamodel: move ReportedStatus into finding-datamodel. Add CANDIDATE reported status

---
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/ReportedStatus.java
@@ -0,0 +1,10 @@
+package com.hartwig.hmftools.datamodel.finding;
+
+public enum ReportedStatus
+{
+    // must be listed this way to allow comparison
+    NON_DRIVER_GENE,
+    NOT_REPORTED,
+    CANDIDATE,
+    REPORTED,
+}
---
+++ finding/src/main/java/com/hartwig/hmftools/datamodel/finding/DriverUtil.java
@@ -0,0 +1,25 @@
+package com.hartwig.hmftools.datamodel.finding;
+
+import com.hartwig.hmftools.datamodel.driver.DriverInterpretation;
+
+final class DriverUtil
+{
+    static ReportedStatus reportedStatus(boolean isDriverGene, boolean isReportable, DriverInterpretation driverInterpretation)
+    {
+        if(!isDriverGene)
+        {
+            return ReportedStatus.NON_DRIVER_GENE;
+        }
+        if(isReportable)
+        {
+            if(driverInterpretation == DriverInterpretation.LOW)
+                return ReportedStatus.CANDIDATE;
+            else
+                return ReportedStatus.REPORTED;
+        }
+        else
+        {
+            return ReportedStatus.NOT_REPORTED;
+        }
+    }
+}
