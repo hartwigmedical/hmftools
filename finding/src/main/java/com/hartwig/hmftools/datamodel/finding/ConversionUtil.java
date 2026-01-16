d0055bd124494cf0a66e7ca43cffb0770698d5e5
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-01-16 10:11:52 +0100

    Refactored

--- finding/src/main/java/com/hartwig/hmftools/datamodel/finding/ConversionUtil.java
+++ finding/src/main/java/com/hartwig/hmftools/datamodel/finding/ConversionUtil.java
@@ -3,20 +3,15 @@ package com.hartwig.hmftools.datamodel.finding;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import com.hartwig.hmftools.datamodel.OrangeJson;
-import com.hartwig.hmftools.datamodel.orange.OrangeRecord;
-
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ConversionUtil
 {
-
     public static void orangeJsonToFindingsJson(@NotNull Path findingsJson, @NotNull Path orangeJson, @Nullable Path clinicalTranscriptsTsv,
             @Nullable Path driverGeneTsv) throws IOException
     {
-        OrangeRecord orangeRecord = OrangeJson.getInstance().read(orangeJson.toString());
-        FindingRecord findingRecord = FindingRecordFactory.fromOrangeRecord(orangeRecord, clinicalTranscriptsTsv, driverGeneTsv);
+        FindingRecord findingRecord = FindingRecordFactory.fromOrangeJsonWithTranscriptFile(orangeJson, clinicalTranscriptsTsv, driverGeneTsv);
         FindingsJson.getInstance().write(findingRecord, findingsJson);
     }
 }
