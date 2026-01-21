b3f2161f74f3bb10306df194021c15ae95468cd2
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2026-01-21 15:32:10 +1100

    Qsee: Add DiscordantFragFreqPrep

---
+++ qsee/src/main/java/com/hartwig/hmftools/qsee/prep/discordant/DiscordantFragGroup.java
@@ -0,0 +1,37 @@
+package com.hartwig.hmftools.qsee.prep.discordant;
+
+import com.hartwig.hmftools.common.sv.DiscordantFragType;
+
+public enum DiscordantFragGroup
+{
+    DEL_DUP_SHORT("Del/Dup 1K-5K"),
+    DEL_DUP_MEDIUM("Del/Dup 5K-100K"),
+    DEL_DUP_LONG("Del/Dup >100K"),
+    INV_SHORT("Inv <=1K"),
+    INV_MEDIUM("Inv 1K-100K"),
+    INV_LONG("Inv >100K"),
+    TRANSLOCATION("Translocation");
+
+    public final String mName;
+
+    DiscordantFragGroup(String name)
+    {
+        mName = name;
+    }
+
+    public String getName(){ return mName; }
+
+    public static DiscordantFragGroup fromType(DiscordantFragType type)
+    {
+        return switch(type)
+        {
+            case Del1To5K, Dup1To5K -> DiscordantFragGroup.DEL_DUP_SHORT;
+            case Del5To100K, Dup5To100K -> DiscordantFragGroup.DEL_DUP_MEDIUM;
+            case DelGt100K, DupGt100K -> DiscordantFragGroup.DEL_DUP_LONG;
+            case InvLt1K -> DiscordantFragGroup.INV_SHORT;
+            case Inv1To5K, Inv5To100K -> DiscordantFragGroup.INV_MEDIUM;
+            case InvGt100K -> DiscordantFragGroup.INV_LONG;
+            case Translocation -> DiscordantFragGroup.TRANSLOCATION;
+        };
+    }
+}
---
+++ qsee/src/test/java/com/hartwig/hmftools/qsee/prep/category/DiscordantFragFreqPrepTest.java
@@ -0,0 +1,29 @@
+package com.hartwig.hmftools.qsee.prep.category;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import com.hartwig.hmftools.common.sv.DiscordantFragType;
+import com.hartwig.hmftools.common.sv.EsveeDiscordantStats;
+import com.hartwig.hmftools.qsee.prep.discordant.DiscordantFragGroup;
+
+import org.junit.Test;
+
+public class DiscordantFragFreqPrepTest
+{
+    @Test
+    public void canCalcDiscordantProportions()
+    {
+        long[] typeCounts = new long[DiscordantFragType.values().length];
+
+        typeCounts[DiscordantFragType.Del1To5K.ordinal()] = 5;
+        typeCounts[DiscordantFragType.Dup1To5K.ordinal()] = 5;
+
+        EsveeDiscordantStats discordantStats = new EsveeDiscordantStats(100, Integer.MIN_VALUE, typeCounts);
+        Map<DiscordantFragGroup, Double> discPropPerGroup = DiscordantFragFreqPrep.calcDiscordantProportions(discordantStats);
+
+        assertEquals(0.1, discPropPerGroup.get(DiscordantFragGroup.DEL_DUP_SHORT), 0.001);
+    }
+}
