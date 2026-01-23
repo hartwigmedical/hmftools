bad2ad5eeb848ecf754be14bfc73c29cae042c41
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-01-23 16:03:11 +1100

    finding: add allSmallVariants / allGainDeletions / allDisruptions convenience methods

--- finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/FindingRecord.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/FindingRecord.java
@@ -1,5 +1,8 @@
 package com.hartwig.hmftools.datamodel.finding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.soabase.recordbuilder.core.RecordBuilder;
 import jakarta.validation.constraints.NotNull;
 
@@ -22,5 +25,57 @@ public record FindingRecord(
         @NotNull FindingItem<PredictedTumorOrigin> predictedTumorOrigin,
         @NotNull FindingItem<HomologousRecombination> homologousRecombination)
 {
+    @NotNull
+    List<SmallVariant> allSmallVariants()
+    {
+        return mergeDriverFindings(somaticSmallVariants, germlineSmallVariants);
+    }
 
+    @NotNull
+    List<GainDeletion> allGainDeletions()
+    {
+        return mergeDriverFindings(somaticGainDeletions, germlineGainDeletions);
+    }
+
+    @NotNull
+    List<Disruption> allDisruptions()
+    {
+        return mergeDriverFindings(somaticDisruptions, germlineDisruptions);
+    }
+
+    @NotNull
+    List<SmallVariant> allReportableSmallVariants()
+    {
+        return mergeReportedDriverFindings(somaticSmallVariants, germlineSmallVariants);
+    }
+
+    @NotNull
+    List<GainDeletion> allReportableGainDeletions()
+    {
+        return mergeReportedDriverFindings(somaticGainDeletions, germlineGainDeletions);
+    }
+
+    @NotNull
+    List<Disruption> allReportableDisruptions()
+    {
+        return mergeReportedDriverFindings(somaticDisruptions, germlineDisruptions);
+    }
+
+    @NotNull
+    private static <T extends Driver> List<T> mergeDriverFindings(DriverFindingList<T> somatic, DriverFindingList<T> germline)
+    {
+        List<T> all = new ArrayList<>(somatic.findings());
+        all.addAll(germline.findings());
+        all.sort(T.COMPARATOR);
+        return all;
+    }
+
+    @NotNull
+    private static <T extends Driver> List<T> mergeReportedDriverFindings(DriverFindingList<T> somatic, DriverFindingList<T> germline)
+    {
+        List<T> all = new ArrayList<>(somatic.reportedOnly().findings());
+        all.addAll(germline.reportedOnly().findings());
+        all.sort(T.COMPARATOR);
+        return all;
+    }
 }
