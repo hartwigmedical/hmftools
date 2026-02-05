34b10edd213d6023bd0bec891e702324f3e3caaf
Reece Jones <r.jones@hartwigmedicalfoundation.nl>
2026-02-05 16:01:45 +1100

    Cider: Unit tests

--- cider/src/test/java/com/hartwig/hmftools/cider/SomaticHypermutationTest.kt
+++ cider/src/test/java/com/hartwig/hmftools/cider/SomaticHypermutationTest.kt
@@ -4,6 +4,7 @@ import com.hartwig.hmftools.cider.annotation.ImgtSequenceFile
 import com.hartwig.hmftools.cider.genes.VJ
 import com.hartwig.hmftools.common.bam.CigarUtils.cigarElementsFromStr
 import com.hartwig.hmftools.common.genome.region.Strand
+import htsjdk.samtools.util.SequenceUtil.reverseComplement
 import kotlin.test.*
 
 class SomaticHypermutationTest {
@@ -41,11 +42,13 @@ class SomaticHypermutationTest {
 
         val anchorBoundary = 20
         val queryRange = 2 until 47
+        val queryAlignRange = 1 until 21
         val imgtRange = 5 until 25
+        val refAlignRange = 3 until 23
         val alignment = Alignment(
-            layoutSeq.substring(queryRange), 1 until 21,
-            "test", 3 until 23, Strand.FORWARD,
-            20 * 4, 25, cigarElementsFromStr("1S20M24S"),
+            layoutSeq.substring(queryRange), queryAlignRange,
+            "test", refAlignRange, Strand.FORWARD,
+            0, 27, cigarElementsFromStr("1S20M24S"),
             imgtSeq.length)
         val actual = compareVJRegionToImgt(
             layoutSeq,
@@ -61,18 +64,106 @@ class SomaticHypermutationTest {
     @Test
     fun testCompareVJRegionToImgtVReverse()
     {
-        // TODO
+        //   sections:   |---V----||-anchor-||--CDR3--||-anchor-||---J----|
+        //   query:        |-------------------------------------------|
+        //   alignment:     |------------------|
+        val layoutSeq = "GGGTTGGGGGCAAAAACAAACCCCCCCCCCAAAAAAAAAATTTTTTTTTT"
+        //   IMGT             |------------------|
+        //   ref:        -----                    -----
+        //   alignment:     |------------------|
+        var imgtSeq =   "AAAAAGGGGGAAAAAAAAAACCCCCAAAAA"
+        //   compare:         |-------------|
+
+        imgtSeq = reverseComplement(imgtSeq)
+
+        val anchorBoundary = 20
+        val queryRange = 2 until 47
+        val queryAlignRange = 1 until 21
+        val imgtRange = imgtSeq.length - 25 until imgtSeq.length - 5
+        val refAlignRange = imgtSeq.length - 23 until imgtSeq.length - 3
+        val alignment = Alignment(
+            layoutSeq.substring(queryRange), queryAlignRange,
+            "test", refAlignRange, Strand.REVERSE,
+            0, 27, cigarElementsFromStr("24S20M1S"),
+            imgtSeq.length)
+        val actual = compareVJRegionToImgt(
+            layoutSeq,
+            VJ.V,
+            anchorBoundary,
+            ImgtSequenceFile.Sequence("test", "1", imgtSeq, imgtRange),
+            queryRange,
+            alignment)
+        val expected = ShmGeneComparison(15, 15, 100.0 * 13 / 15, 0)
+        assertEquals(expected, actual)
     }
 
     @Test
     fun testCompareVJRegionToImgtJForward()
     {
-        // TODO
+        //   sections:   |---V----||-anchor-||--CDR3--||-anchor-||---J----|
+        //   query:        |--------------------------------------------|
+        //   alignment:                             |------------------|
+        val layoutSeq = "GGGGGGGGGGAAAAAAAAAACCCCCCCTCTCAAAAAAAAGTTTTTTTTTT"
+        //   IMGT                                 |------------------|
+        //   ref:                            -----                    -----
+        //   alignment:                             |------------------|
+        val imgtSeq =                       "AAAAACCCCCAAAAAAAAAATTTTTAAAAA"
+        //   compare:                                  |-------------|
+
+        val anchorBoundary = 30
+        val queryRange = 2 until 48
+        val queryAlignRange = 25 until 45
+        val imgtRange = 5 until 25
+        val refAlignRange = 7 until 27
+        val alignment = Alignment(
+            layoutSeq.substring(queryRange), queryAlignRange,
+            "test", refAlignRange, Strand.FORWARD,
+            0, 27, cigarElementsFromStr("25S20M1S"),
+            imgtSeq.length)
+        val actual = compareVJRegionToImgt(
+            layoutSeq,
+            VJ.J,
+            anchorBoundary,
+            ImgtSequenceFile.Sequence("test", "1", imgtSeq, imgtRange),
+            queryRange,
+            alignment)
+        val expected = ShmGeneComparison(15, 15, 100.0 * 13 / 15, 0)
+        assertEquals(expected, actual)
     }
 
     @Test
     fun testCompareVJRegionToImgtJReverse()
     {
-        // TODO
+        //   sections:   |---V----||-anchor-||--CDR3--||-anchor-||---J----|
+        //   query:        |--------------------------------------------|
+        //   alignment:                             |------------------|
+        val layoutSeq = "GGGGGGGGGGAAAAAAAAAACCCCCCCTCTCAAAAAAAAGTTTTTTTTTT"
+        //   IMGT                                 |------------------|
+        //   ref:                            -----                    -----
+        //   alignment:                             |------------------|
+        var imgtSeq =                       "AAAAACCCCCAAAAAAAAAATTTTTAAAAA"
+        //   compare:                                  |-------------|
+
+        imgtSeq = reverseComplement(imgtSeq)
+
+        val anchorBoundary = 30
+        val queryRange = 2 until 48
+        val queryAlignRange = 25 until 45
+        val imgtRange = imgtSeq.length - 25 until imgtSeq.length - 5
+        val refAlignRange = imgtSeq.length - 27 until imgtSeq.length - 7
+        val alignment = Alignment(
+            layoutSeq.substring(queryRange), queryAlignRange,
+            "test", refAlignRange, Strand.REVERSE,
+            0, 27, cigarElementsFromStr("1S20M25S"),
+            imgtSeq.length)
+        val actual = compareVJRegionToImgt(
+            layoutSeq,
+            VJ.J,
+            anchorBoundary,
+            ImgtSequenceFile.Sequence("test", "1", imgtSeq, imgtRange),
+            queryRange,
+            alignment)
+        val expected = ShmGeneComparison(15, 15, 100.0 * 13 / 15, 0)
+        assertEquals(expected, actual)
     }
 }
