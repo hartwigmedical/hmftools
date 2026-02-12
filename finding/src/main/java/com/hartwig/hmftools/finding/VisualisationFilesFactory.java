d4e55ce3c334959abff51d9f0c7e5400f603a3d4
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-02-12 11:46:08 +1100

    findings: FindingApplication store the visualisation paths in the datamodel

--- finding/src/main/java/com/hartwig/hmftools/finding/VisualisationFilesFactory.java
+++ finding/src/main/java/com/hartwig/hmftools/finding/VisualisationFilesFactory.java
@@ -1,19 +1,11 @@
 package com.hartwig.hmftools.finding;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
-import com.hartwig.hmftools.common.utils.file.FileWriterUtils;
 import com.hartwig.hmftools.datamodel.finding.VisualisationFiles;
 import com.hartwig.hmftools.datamodel.finding.VisualisationFilesBuilder;
+import com.hartwig.hmftools.datamodel.orange.OrangePlots;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,63 +14,32 @@ import org.jspecify.annotations.Nullable;
 public class VisualisationFilesFactory
 {
     private static final Logger LOGGER = LogManager.getLogger(VisualisationFilesFactory.class);
-    private final String visDir;
 
-    public VisualisationFilesFactory(String visDir)
+    private VisualisationFilesFactory()
     {
-        this.visDir = visDir;
-        LOGGER.info("Creating visualisation directory: {}", visDir);
-        FileWriterUtils.checkCreateOutputDir(visDir);
     }
 
-    public VisualisationFiles create(
-            final String pipelineOutputDir,
-            final String tumorSampleId,
-            @Nullable final String referenceSampleId,
-            boolean hasCuppa)
+    public static VisualisationFiles create(OrangePlots orangePlots)
     {
         LOGGER.info("Loading plots");
 
-        List<String> linxDriverPlots = new ArrayList<>();
-        Path linxPlotDir = Paths.get(pipelineOutputDir + File.separator + "linx/somatic_plots/reportable/");
-        if (Files.exists(linxPlotDir) && Files.isDirectory(linxPlotDir))
-        {
-            try
-            {
-                try (Stream<Path> stream = Files.list(linxPlotDir))
-                {
-                    stream.forEach(p -> {
-                        linxDriverPlots.add(processVisualisation(p.toString()));
-                    });
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to list LINX plot directory: " + linxPlotDir, e);
-            }
+        List<String> linxDriverPlots = orangePlots.linxDriverPlots().stream()
+                .map(VisualisationFilesFactory::processFilePath)
+                .toList();
 
-            LOGGER.info(" Loaded {} linx plots from {}", linxDriverPlots.size(), linxPlotDir);
-        }
+        String referenceBqrPlot = processNullableFilePath(orangePlots.sageReferenceBQRPlot());
 
-        LOGGER.info("Loading plots");
+        String tumorBqrPlot = processFilePath(orangePlots.sageTumorBQRPlot());
 
-        String referenceBqrPlot = referenceSampleId == null
-                ? null
-                : processVisualisation(
-                        String.format("%s/redux/%s.redux.bqr.png", pipelineOutputDir, referenceSampleId));
+        String purpleInputPlot = processFilePath(orangePlots.purpleInputPlot());
+        String purpleFinalCircosPlot = processFilePath(orangePlots.purpleFinalCircosPlot());
+        String purpleClonalityPlot = processFilePath(orangePlots.purpleClonalityPlot());
+        String purpleCopyNumberPlot = processFilePath(orangePlots.purpleCopyNumberPlot());
+        String purpleVariantCopyNumberPlot = processFilePath(orangePlots.purpleVariantCopyNumberPlot());
+        String purplePurityRangePlot = processFilePath(orangePlots.purplePurityRangePlot());
+        String purpleKataegisPlot = processFilePath(orangePlots.purpleKataegisPlot());
 
-        String tumorBqrPlot = processVisualisation(String.format("%s/redux/%s.redux.bqr.png", pipelineOutputDir, tumorSampleId));
-
-        String purplePlotBasePath = String.format("%s/purple/plot/%s", pipelineOutputDir, tumorSampleId);
-        String purpleInputPlot = processVisualisation(purplePlotBasePath + ".input.png");
-        String purpleFinalCircosPlot = processVisualisation(purplePlotBasePath + ".circos.png");
-        String purpleClonalityPlot = processVisualisation(purplePlotBasePath + ".somatic.clonality.png");
-        String purpleCopyNumberPlot = processVisualisation(purplePlotBasePath + ".copynumber.png");
-        String purpleVariantCopyNumberPlot = processVisualisation(purplePlotBasePath + ".somatic.png");
-        String purplePurityRangePlot = processVisualisation(purplePlotBasePath + ".purity.range.png");
-        String purpleKataegisPlot = processVisualisation(purplePlotBasePath + ".somatic.rainfall.png");
-
-        String cuppaSummaryPlot = hasCuppa ?
-                processVisualisation(String.format("%s/cuppa/%s.cuppa.vis.png", pipelineOutputDir, tumorSampleId))
-                : null;
+        String cuppaSummaryPlot = processNullableFilePath(orangePlots.cuppaSummaryPlot());
 
         return VisualisationFilesBuilder.builder()
                 .referenceBqrPlot(referenceBqrPlot)
@@ -90,36 +51,24 @@ public class VisualisationFilesFactory
                 .purpleVariantCopyNumberPlot(purpleVariantCopyNumberPlot)
                 .purplePurityRangePlot(purplePurityRangePlot)
                 .purpleKataegisPlot(purpleKataegisPlot)
-                .qseePlot(processVisualisation(String.format("%s/qsee/%s.qsee.vis.report.pdf", pipelineOutputDir, tumorSampleId)))
+                .qseePlot(null)
                 .linxDriverPlots(linxDriverPlots)
                 .cuppaSummaryPlot(cuppaSummaryPlot)
                 .build();
     }
 
-    public String processVisualisation(final Path path)
+    @Nullable
+    private static String processNullableFilePath(@Nullable final String path)
     {
-        if (!path.toFile().exists())
+        if(path != null)
         {
-            throw new IllegalArgumentException("Required plot file does not exist: " + path);
+            return processFilePath(path);
         }
-        
-        // copy the file to the plot directory
-        try
-        {
-            LOGGER.info("Copying plot file: {}", path.getFileName());
-            Path targetPath = Path.of(visDir + "/" + path.getFileName());
-            Files.copy(path, targetPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-        catch (IOException e)
-        {
-            throw new UncheckedIOException(e);
-        }
-
-        return path.getFileName().toString();
+        return null;
     }
 
-    public String processVisualisation(final String path)
+    private static String processFilePath(final String path)
     {
-        return processVisualisation(Path.of(path));
+        return Path.of(path).getFileName().toString();
     }
 }
