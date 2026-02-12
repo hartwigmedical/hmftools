77e5acf8bcadbc5b7bc7035c3aaab81ff0f081f4
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-02-12 21:49:34 +1100

    findings: add CurationApplier which can apply curations to creates a new findings record

--- finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/CurationRecord.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/datamodel/finding/CurationRecord.java
@@ -1,19 +1,6 @@
 package com.hartwig.hmftools.datamodel.finding;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.ServiceLoader;
-
-import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapterFactory;
-import com.hartwig.hmftools.datamodel.LocalDateAdapter;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 import jakarta.validation.constraints.NotNull;
@@ -23,43 +10,9 @@ public record CurationRecord(
         @NotNull List<DriverCuration> driverCurations
 )
 {
-
-    @NotNull
-    public static CurationRecord read(@NotNull Path findingsJsonFilePath) throws IOException
+    @SuppressWarnings("unused")
+    public static JsonReadWriter<CurationRecord> jsonReadWriter()
     {
-        try(BufferedReader bufferedReader = Files.newBufferedReader(findingsJsonFilePath))
-        {
-            return gson().fromJson(bufferedReader, CurationRecord.class);
-        }
-    }
-
-    @NotNull
-    public static CurationRecord read(@NotNull InputStream stream) throws IOException
-    {
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream)))
-        {
-            return gson().fromJson(bufferedReader, CurationRecord.class);
-        }
-    }
-
-    public void write(@NotNull CurationRecord curationRecord, @NotNull Path outputFilePath) throws IOException
-    {
-        try(BufferedWriter writer = Files.newBufferedWriter(outputFilePath))
-        {
-            gson().toJson(curationRecord, CurationRecord.class, writer);
-        }
-    }
-
-    private static com.google.gson.Gson gson()
-    {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        for(TypeAdapterFactory factory : ServiceLoader.load(TypeAdapterFactory.class))
-        {
-            gsonBuilder.registerTypeAdapterFactory(factory);
-        }
-
-        return gsonBuilder.serializeNulls().serializeSpecialFloatingPointValues().setPrettyPrinting()
-                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-                .create();
+        return new JsonReadWriter<>(CurationRecord.class);
     }
 }
