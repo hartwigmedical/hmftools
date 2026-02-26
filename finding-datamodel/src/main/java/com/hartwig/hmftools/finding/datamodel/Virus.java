d0662cb46a07f5278188db261e0bb99ad05637d2
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-02-26 16:54:05 +1100

    finding-datamodel: rename package to com.hartwig.hmftools.finding.datamodel

---
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/Virus.java
@@ -0,0 +1,58 @@
+package com.hartwig.hmftools.finding.datamodel;
+
+import com.hartwig.hmftools.datamodel.driver.DriverInterpretation;
+import com.hartwig.hmftools.datamodel.driver.DriverSource;
+import com.hartwig.hmftools.datamodel.virus.VirusBreakendQCStatus;
+import com.hartwig.hmftools.datamodel.virus.VirusInterpretation;
+
+import org.jspecify.annotations.Nullable;
+
+import io.soabase.recordbuilder.core.RecordBuilder;
+import jakarta.validation.constraints.NotNull;
+
+@RecordBuilder
+public record Virus(
+        @NotNull DriverFields driver,
+        @NotNull String name,
+        @NotNull VirusBreakendQCStatus qcStatus,
+        int integrations,
+        @Nullable VirusInterpretation interpretation,
+        double percentageCovered,
+        double meanCoverage,
+        @Nullable Double expectedClonalCoverage
+) implements Driver
+{
+    @NotNull
+    @Override
+    public String findingKey()
+    {
+        return driver.findingKey();
+    }
+
+    @NotNull
+    @Override
+    public DriverSource driverSource()
+    {
+        return driver.driverSource();
+    }
+
+    @NotNull
+    @Override
+    public ReportedStatus reportedStatus()
+    {
+        return driver.reportedStatus();
+    }
+
+    @NotNull
+    @Override
+    public DriverInterpretation driverInterpretation()
+    {
+        return driver.driverInterpretation();
+    }
+
+    @Override
+    public double driverLikelihood()
+    {
+        return driver.driverLikelihood();
+    }
+}
