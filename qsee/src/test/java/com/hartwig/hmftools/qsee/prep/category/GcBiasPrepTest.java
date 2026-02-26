bc74e26d810ac2fbd96951edf00331a89dd5f375
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2026-02-27 09:17:00 +1100

    Qsee: Fix failing unit tests due to recent change to field delimiter

--- qsee/src/test/java/com/hartwig/hmftools/qsee/prep/category/GcBiasPrepTest.java
+++ qsee/src/test/java/com/hartwig/hmftools/qsee/prep/category/GcBiasPrepTest.java
@@ -27,11 +27,11 @@ public class GcBiasPrepTest
         Feature actualFeature;
 
         actualFeature = features.get(0);
-        assertEquals("GCBucket=25", actualFeature.key().name());
+        assertEquals("GCBucket:25", actualFeature.key().name());
         assertEquals(1.0, actualFeature.value(), 0.001);
 
         actualFeature = features.get(1);
-        assertEquals("GCBucket=26", actualFeature.key().name());
+        assertEquals("GCBucket:26", actualFeature.key().name());
         assertEquals(1.05, actualFeature.value(), 0.001);
     }
 }
