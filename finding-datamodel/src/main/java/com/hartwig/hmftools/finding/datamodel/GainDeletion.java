1c2ce0b0b0e3e37b6f826c3231d3df8eae95ee5f
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-03-02 13:59:36 +1100

    finding-datamodel: improve gain deletion datamodel

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/GainDeletion.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/GainDeletion.java
@@ -2,35 +2,51 @@ package com.hartwig.hmftools.finding.datamodel;
 
 import com.hartwig.hmftools.datamodel.driver.DriverInterpretation;
 import com.hartwig.hmftools.datamodel.driver.DriverSource;
-import com.hartwig.hmftools.datamodel.purple.CopyNumberInterpretation;
+
+import org.jspecify.annotations.Nullable;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 import jakarta.validation.constraints.NotNull;
 
 @RecordBuilder
+@RecordBuilder.Options(nullableAnnotationClass = "org.jspecify.annotations.Nullable", defaultNotNull = true)
 public record GainDeletion(
         @NotNull DriverFields driver,
-        @NotNull Type type,
-        @NotNull CopyNumberInterpretation interpretation,
         @NotNull String gene,
         @NotNull String chromosome,
         @NotNull String chromosomeBand,
         @NotNull String transcript,
         boolean isCanonical,
+        @NotNull Type somaticType,
+        @Nullable Type germlineType,
+        @NotNull GeneExtent geneExtent,
+        @Nullable ExonRange exonRange, // null if exon range info not available
         double tumorMinCopies,
         double tumorMaxCopies,
         double tumorMinMinorAlleleCopies,
-        double chromosomeArmCopies) implements Driver
+        double chromosomeArmCopies,
+        @Nullable Double germlineMinCopyNumber) implements Driver
 {
     public enum Type
     {
-        GERMLINE_DEL_HOM_IN_TUMOR,
-        GERMLINE_DEL_HET_IN_TUMOR,
-        GERMLINE_GAIN,
-        SOMATIC_GAIN,
-        SOMATIC_DEL,
-        SOMATIC_LOH
+        GAIN,
+        HOM_DEL,
+        HET_DEL,
+        CN_NEUTRAL_LOH,
+        NONE
     }
+
+    public enum GeneExtent
+    {
+        WHOLE_GENE,
+        PARTIAL_GENE
+    }
+
+    public record ExonRange(
+        @Nullable Integer exonStart, // null if starts before first exon
+        @Nullable Integer exonEnd    // null if ends after last exon
+    )
+    {}
 
     @NotNull
     @Override
@@ -64,5 +80,11 @@ public record GainDeletion(
     public double driverLikelihood()
     {
         return driver.driverLikelihood();
+    }
+
+    public boolean isLossOfHeterozygosity()
+    {
+        return (somaticType() == Type.CN_NEUTRAL_LOH) ||
+               (somaticType() == Type.HET_DEL && tumorMinMinorAlleleCopies() < 0.5);
     }
 }
