ff7838988e36f2bedffc56ad70d7ecbb031c912b
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-03-02 13:59:36 +1100

    finding-datamodel: fix up the gain deletion data model

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/GainDeletion.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/GainDeletion.java
@@ -2,7 +2,8 @@ package com.hartwig.hmftools.finding.datamodel;
 
 import com.hartwig.hmftools.datamodel.driver.DriverInterpretation;
 import com.hartwig.hmftools.datamodel.driver.DriverSource;
-import com.hartwig.hmftools.datamodel.purple.CopyNumberInterpretation;
+
+import org.jspecify.annotations.Nullable;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 import jakarta.validation.constraints.NotNull;
@@ -10,27 +11,40 @@ import jakarta.validation.constraints.NotNull;
 @RecordBuilder
 public record GainDeletion(
         @NotNull DriverFields driver,
-        @NotNull Type type,
-        @NotNull CopyNumberInterpretation interpretation,
         @NotNull String gene,
         @NotNull String chromosome,
         @NotNull String chromosomeBand,
         @NotNull String transcript,
         boolean isCanonical,
+        @Nullable Type germlineType,
+        @NotNull Type somaticType,
+        @NotNull GeneExtent geneExtent,
+        @Nullable ExonRange exonRange, // null if exon range info not available
         double tumorMinCopies,
         double tumorMaxCopies,
         double tumorMinMinorAlleleCopies,
-        double chromosomeArmCopies) implements Driver
+        double chromosomeArmCopies,
+        @Nullable Double germlineMinCopyNumber) implements Driver
 {
     public enum Type
     {
-        GERMLINE_DEL_HOM_IN_TUMOR,
-        GERMLINE_DEL_HET_IN_TUMOR,
-        GERMLINE_GAIN,
-        SOMATIC_GAIN,
-        SOMATIC_DEL,
-        SOMATIC_LOH
+        GAIN,
+        DEL,
+        HEL_DEL,
+        CN_NEUTRAL_LOH
     }
+
+    public enum GeneExtent
+    {
+        WHOLE_GENE,
+        PARTIAL_GENE
+    }
+
+    public record ExonRange(
+            @Nullable Integer exonStart, // null if starts before first exon
+            @Nullable Integer exonEnd    // null if ends after last exon
+    )
+    {}
 
     @NotNull
     @Override
