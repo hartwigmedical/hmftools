1c7cab735c28f015babedfe66713c0f946bd44ff
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-03-03 12:51:44 +1100

    finding: fix package name and GainDeletion conversion

--- finding/src/main/java/com/hartwig/hmftools/finding/GainDeletionFactory.java
+++ finding/src/main/java/com/hartwig/hmftools/finding/GainDeletionFactory.java
@@ -6,24 +6,32 @@ import java.util.Objects;
 
 import com.hartwig.hmftools.datamodel.driver.DriverInterpretation;
 import com.hartwig.hmftools.datamodel.driver.DriverSource;
-import com.hartwig.hmftools.datamodel.finding.*;
 import com.hartwig.hmftools.datamodel.orange.OrangeRefGenomeVersion;
+import com.hartwig.hmftools.datamodel.purple.GermlineAmpDelFields;
 import com.hartwig.hmftools.datamodel.purple.PurpleDriver;
+import com.hartwig.hmftools.datamodel.purple.PurpleDriverType;
 import com.hartwig.hmftools.datamodel.purple.PurpleGainDeletion;
 import com.hartwig.hmftools.datamodel.purple.PurpleGeneCopyNumber;
 import com.hartwig.hmftools.datamodel.purple.PurpleGermlineStatus;
 import com.hartwig.hmftools.datamodel.purple.PurpleRecord;
+import com.hartwig.hmftools.finding.datamodel.DriverFieldsBuilder;
+import com.hartwig.hmftools.finding.datamodel.DriverFindingList;
+import com.hartwig.hmftools.finding.datamodel.DriverFindingListBuilder;
+import com.hartwig.hmftools.finding.datamodel.FindingsStatus;
+import com.hartwig.hmftools.finding.datamodel.GainDeletion;
+import com.hartwig.hmftools.finding.datamodel.GainDeletionBuilder;
+
+import org.jspecify.annotations.Nullable;
 
 final class GainDeletionFactory
 {
-
     public static DriverFindingList<GainDeletion> somaticGainDeletionFindings(
             OrangeRefGenomeVersion orangeRefGenomeVersion,
             FindingsStatus findingsStatus,
             PurpleRecord purple)
     {
         ChromosomeArmCopyNumberMap cnPerChromosome =
-                ChromosomeArmCopyNumberMap.create(purple.somaticCopyNumbers(), orangeRefGenomeVersion);
+                com.hartwig.hmftools.finding.ChromosomeArmCopyNumberMap.create(purple.somaticCopyNumbers(), orangeRefGenomeVersion);
 
         List<GainDeletion> gainDeletions = somaticDriverGainDels(purple.somaticGainsDels(), cnPerChromosome);
 
@@ -90,35 +98,18 @@ final class GainDeletionFactory
             ChromosomeArmCopyNumberMap cnPerChromosome)
     {
         PurpleDriver driver = purpleGainDeletion.driver();
+        GermlineAmpDelFields germlineAmpDelFields = purpleGainDeletion.germlineAmpDelFields();
+        PurpleGermlineStatus somaticStatus = germlineAmpDelFields != null ? germlineAmpDelFields.somaticStatus() : null;
+        PurpleGermlineStatus germlineStatus = germlineAmpDelFields != null ? germlineAmpDelFields.germlineStatus() : null;
 
-        GainDeletion.Type type = switch(purpleGainDeletion.driver().type())
-        {
-            case AMP, PARTIAL_AMP -> GainDeletion.Type.SOMATIC_GAIN;
-            case DEL, HET_DEL -> GainDeletion.Type.SOMATIC_DEL;
-            case LOH -> GainDeletion.Type.SOMATIC_LOH;
-            case GERMLINE_DELETION -> {
-                if(purpleGainDeletion.germlineAmpDelFields().somaticStatus() == PurpleGermlineStatus.HOM_DELETION)
-                {
-                    yield GainDeletion.Type.GERMLINE_DEL_HOM_IN_TUMOR;
-                }
-                else if(purpleGainDeletion.germlineAmpDelFields().somaticStatus() == PurpleGermlineStatus.HET_DELETION)
-                {
-                    yield GainDeletion.Type.GERMLINE_DEL_HET_IN_TUMOR;
-                }
-                else
-                {
-                    throw new IllegalStateException("Unexpected germline status: " + purpleGainDeletion.germlineAmpDelFields().somaticStatus());
-                }
-            }
-            case GERMLINE_AMP -> GainDeletion.Type.GERMLINE_GAIN;
-            default -> throw new IllegalStateException("Unexpected driver type: " + purpleGainDeletion.driver().type());
-        };
+        GainDeletion.Type somaticType = somaticGainDelType(driver.type(), somaticStatus);
+        GainDeletion.Type germlineType = germlineStatus != null ? germlineGainDelType(driver.type(), germlineStatus) : null;
 
         return GainDeletionBuilder.builder()
                 .driver(DriverFieldsBuilder.builder()
                         .findingKey(FindingKeys.gainDeletion(sourceSample,
                                 purpleGainDeletion.gene(),
-                                purpleGainDeletion.interpretation(),
+                                driver.type(),
                                 driver.isCanonical(),
                                 purpleGainDeletion.transcript()))
                         .driverSource(sourceSample)
@@ -127,18 +118,54 @@ final class GainDeletionFactory
                         .driverLikelihood(driver.driverLikelihood())
                         .build()
                 )
-                .type(type)
                 .chromosome(purpleGainDeletion.chromosome())
                 .chromosomeBand(purpleGainDeletion.chromosomeBand())
                 .gene(purpleGainDeletion.gene())
                 .transcript(purpleGainDeletion.transcript())
                 .isCanonical(driver.isCanonical())
-                .interpretation(purpleGainDeletion.interpretation())
-                .tumorMinCopies(purpleGainDeletion.minCopies())
-                .tumorMaxCopies(purpleGainDeletion.maxCopies())
+                .somaticType(somaticType)
+                .germlineType(germlineType)
+                .geneExtent(interpretGeneExtent(purpleGainDeletion.exonRange()))
+                .tumorMinCopies(purpleGainDeletion.minCopyNumber())
+                .tumorMaxCopies(purpleGainDeletion.maxCopyNumber())
                 .tumorMinMinorAlleleCopies(purpleGainDeletion.minMinorAlleleCopies())
                 .chromosomeArmCopies(cnPerChromosome.chromosomeArmCopyNumber(
                         purpleGainDeletion.chromosome(), purpleGainDeletion.chromosomeBand()))
                 .build();
+    }
+
+    private static GainDeletion.GeneExtent interpretGeneExtent(String exonRange)
+    {
+        return exonRange.equals("FULL") ? GainDeletion.GeneExtent.WHOLE_GENE : GainDeletion.GeneExtent.PARTIAL_GENE;
+    }
+
+    private static GainDeletion.Type germlineGainDelType(PurpleDriverType purpleDriverType, PurpleGermlineStatus purpleGermlineStatus)
+    {
+        return switch (purpleDriverType) {
+            case GERMLINE_DELETION -> switch (purpleGermlineStatus) {
+                case HOM_DELETION -> GainDeletion.Type.HOM_DEL;
+                case HET_DELETION -> GainDeletion.Type.HET_DEL;
+                default -> throw new IllegalStateException("Unexpected germline status: " + purpleGermlineStatus.name());
+            };
+            case GERMLINE_AMP -> GainDeletion.Type.GAIN;
+            default -> throw new IllegalStateException("Unexpected driver type: " + purpleDriverType.name());
+        };
+    }
+
+    private static GainDeletion.Type somaticGainDelType(PurpleDriverType purpleDriverType, @Nullable PurpleGermlineStatus somaticStatus)
+    {
+        return switch (purpleDriverType) {
+            case AMP, PARTIAL_AMP -> GainDeletion.Type.GAIN;
+            case DEL -> GainDeletion.Type.HOM_DEL;
+            case HET_DEL -> GainDeletion.Type.HET_DEL;
+            case LOH -> GainDeletion.Type.CN_NEUTRAL_LOH;
+            case GERMLINE_DELETION, GERMLINE_AMP -> switch (Objects.requireNonNull(somaticStatus)) {
+                case HOM_DELETION -> GainDeletion.Type.HOM_DEL;
+                case HET_DELETION -> GainDeletion.Type.HET_DEL;
+                case AMPLIFICATION -> GainDeletion.Type.GAIN;
+                case NOISE, DIPLOID, UNKNOWN -> GainDeletion.Type.NONE;
+            };
+            default -> throw new IllegalStateException("Unexpected driver type: " + purpleDriverType.name());
+        };
     }
 }
