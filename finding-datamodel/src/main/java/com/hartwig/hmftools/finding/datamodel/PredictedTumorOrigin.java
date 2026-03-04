bf9d9d805bba32183d0482e0baf36a19ab042970
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-03-04 17:31:14 +1100

    Made fields nullable

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/PredictedTumorOrigin.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/PredictedTumorOrigin.java
@@ -1,5 +1,7 @@
 package com.hartwig.hmftools.finding.datamodel;
 
+import org.jspecify.annotations.Nullable;
+
 import jakarta.validation.constraints.NotNull;
 
 @RecordBuilder
@@ -8,11 +10,11 @@ public record PredictedTumorOrigin(
         @NotNull CuppaMode mode,
         @NotNull String cancerType,
         double likelihood,
-        Double snvPairwiseClassifier,
-        Double genomicPositionClassifier,
-        Double featureClassifier,
-        Double altSjCohortClassifier,
-        Double expressionPairwiseClassifier
+        @Nullable Double snvPairwiseClassifier,
+        @Nullable Double genomicPositionClassifier,
+        @Nullable Double featureClassifier,
+        @Nullable Double altSjCohortClassifier,
+        @Nullable Double expressionPairwiseClassifier
 ) implements Finding
 {
     public enum CuppaMode
