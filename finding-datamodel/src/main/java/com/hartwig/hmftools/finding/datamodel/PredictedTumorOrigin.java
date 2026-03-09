d19dd6c00ac84a87a11e388fee51dd61a3dfca2d
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-03-10 10:34:54 +1100

    finding-datamodel: match with the backport version

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/PredictedTumorOrigin.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/PredictedTumorOrigin.java
@@ -1,12 +1,25 @@
 package com.hartwig.hmftools.finding.datamodel;
 
+import org.jspecify.annotations.Nullable;
+
 import jakarta.validation.constraints.NotNull;
 
 @RecordBuilder
 public record PredictedTumorOrigin(
         @NotNull String findingKey,
+        @NotNull CuppaMode mode,
         @NotNull String cancerType,
-        double likelihood
+        double likelihood,
+        @Nullable Double snvPairwiseClassifier,
+        @Nullable Double genomicPositionClassifier,
+        @Nullable Double featureClassifier,
+        @Nullable Double altSjCohortClassifier,
+        @Nullable Double expressionPairwiseClassifier
 ) implements Finding
 {
+    public enum CuppaMode
+    {
+        WGS,
+        WGTS
+    }
 }
