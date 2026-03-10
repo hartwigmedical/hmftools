afeaa6fc8b84e7156d1781fb442555615f9d5cb5
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-03-10 11:16:29 +1100

    finding-datamodel: further align with backport branch

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/MetaProperties.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/MetaProperties.java
@@ -1,5 +1,7 @@
 package com.hartwig.hmftools.finding.datamodel;
 
+import java.time.LocalDate;
+
 import org.jspecify.annotations.Nullable;
 
 import jakarta.validation.constraints.NotNull;
@@ -9,7 +11,8 @@ public record MetaProperties(
         @NotNull SequencingScope sequencingScope,
         @Nullable String pipelineVersion,
         @NotNull String version,
-        @NotNull RefGenomeVersion refGenomeVersion
-)
+        @NotNull RefGenomeVersion refGenomeVersion,
+        @NotNull String sampleId,
+        @NotNull LocalDate samplingDate)
 {
 }
