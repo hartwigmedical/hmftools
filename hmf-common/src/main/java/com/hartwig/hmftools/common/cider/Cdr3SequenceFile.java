bdc4cb2e2c69567c177e06905e4e5702a1b74cc0
David Koetsier <david.f.koetsier@gmail.com>
2026-03-18 09:20:47 +0100

    Common: INFRA-2409: Make new Cider format readable

--- hmf-common/src/main/java/com/hartwig/hmftools/common/cider/Cdr3SequenceFile.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/cider/Cdr3SequenceFile.java
@@ -2,6 +2,7 @@ package com.hartwig.hmftools.common.cider;
 
 import java.io.File;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import com.hartwig.hmftools.common.utils.file.DelimFileReader;
@@ -14,13 +15,14 @@ public class Cdr3SequenceFile
         cdr3AA,
         locus,
         filter,
-        blastnStatus,
+        alignmentStatus,
         minHighQualBaseReads,
         assignedReads,
         inFrame,
         containsStop
     }
 
+    private static final String OLD_BLASTN_STATUS_COLUMN_NAME = "blastnStatus";
     private static final String FILE_EXTENSION = ".cider.vdj.tsv.gz";
 
     public static String generateFilename(String basePath, String sample)
@@ -32,17 +34,33 @@ public class Cdr3SequenceFile
     {
         try (DelimFileReader reader = new DelimFileReader(filename))
         {
-            return reader.stream().map(row -> ImmutableCdr3Sequence.builder()
-                        .cdr3Seq(row.get(Column.cdr3Seq))
-                        .cdr3AA(row.get(Column.cdr3AA))
-                        .locus(row.get(Column.locus))
-                        .filter(row.get(Column.filter))
-                        .blastnStatus(row.get(Column.blastnStatus))
-                        .minHighQualBaseReads(row.getInt(Column.minHighQualBaseReads))
-                        .assignedReads(row.getInt(Column.assignedReads))
-                        .inFrame(Boolean.parseBoolean(row.get(Column.inFrame)))
-                        .containsStop(Boolean.parseBoolean(row.get(Column.containsStop)))
-                    .build()).collect(Collectors.toList());
+            return reader.stream().map(row -> readRow(row)).collect(Collectors.toList());
         }
+    }
+
+    private static ImmutableCdr3Sequence readRow(final DelimFileReader.Row row)
+    {
+        String blastnStatus;
+        try
+        {
+            // Try old name first such that error message refers to new name if both fail
+            blastnStatus = row.get(OLD_BLASTN_STATUS_COLUMN_NAME);
+        }
+        catch(NoSuchElementException exception)
+        {
+            blastnStatus = row.get(Column.alignmentStatus);
+        }
+
+        return ImmutableCdr3Sequence.builder()
+                .cdr3Seq(row.get(Column.cdr3Seq))
+                .cdr3AA(row.get(Column.cdr3AA))
+                .locus(row.get(Column.locus))
+                .filter(row.get(Column.filter))
+                .blastnStatus(blastnStatus)
+                .minHighQualBaseReads(row.getInt(Column.minHighQualBaseReads))
+                .assignedReads(row.getInt(Column.assignedReads))
+                .inFrame(Boolean.parseBoolean(row.get(Column.inFrame)))
+                .containsStop(Boolean.parseBoolean(row.get(Column.containsStop)))
+                .build();
     }
 }
