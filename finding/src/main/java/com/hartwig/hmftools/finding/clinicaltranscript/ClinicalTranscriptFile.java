486a2cbc110f52e3f117f65fa036ed5470e2a7ac
Hong Wing Lee <h.lee@hartwigmedicalfoundation.nl>
2026-03-21 10:32:36 +1100

    finding: use CSV reader from hmf-common

--- finding/src/main/java/com/hartwig/hmftools/finding/clinicaltranscript/ClinicalTranscriptFile.java
+++ finding/src/main/java/com/hartwig/hmftools/finding/clinicaltranscript/ClinicalTranscriptFile.java
@@ -3,63 +3,38 @@ package com.hartwig.hmftools.finding.clinicaltranscript;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.hartwig.hmftools.common.utils.file.DelimFileReader;
 import com.hartwig.hmftools.datamodel.orange.OrangeRefGenomeVersion;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public final class ClinicalTranscriptFile
 {
+    private enum Column
+    {
+        Gene,
+        Transcript
+    }
 
     private ClinicalTranscriptFile()
     {
     }
 
-    private static final Logger LOGGER = LogManager.getLogger(ClinicalTranscriptFile.class);
-
-    private static final String SEPARATOR = "\t";
-
     @NotNull
     public static ClinicalTranscriptsModel buildFromTsv(@NotNull OrangeRefGenomeVersion orangeRefGenomeVersion,
             @NotNull Path path) throws IOException
     {
-
         if(orangeRefGenomeVersion == OrangeRefGenomeVersion.V37)
         {
-            Map<String, String> clinicalTranscriptEntries = new HashMap<>();
-            List<String> lines = Files.readAllLines(path);
-            String columnNameGene = "Gene";
-            String columnNameTranscript = "Transcript";
-
-            final Map<String, Integer> fieldIndexMap = CsvFileReader.getHeadersToDelimiter(lines.get(0), SEPARATOR);
-            if(!fieldIndexMap.containsKey(columnNameGene) || !fieldIndexMap.containsKey(columnNameTranscript))
+            try (DelimFileReader reader = new DelimFileReader(Files.newBufferedReader(path)))
             {
-                throw new IllegalArgumentException(
-                        "Wrong column names " + fieldIndexMap.keySet() + " are present in clinical transcripts tsv!");
+                Map<String, String> clinicalTranscriptEntries = reader.stream()
+                        .collect(Collectors.toMap(row -> row.get(Column.Gene), row -> row.get(Column.Transcript)));
+                return new ClinicalTranscriptsModel(clinicalTranscriptEntries);
             }
-
-            // Skip header
-            for(String line : lines.subList(1, lines.size()))
-            {
-                final String[] parts = line.split(SEPARATOR, -1);
-
-                if(parts.length == 2)
-                {
-                    String geneName = parts[fieldIndexMap.get(columnNameGene)];
-                    String clinicalTranscript = parts[fieldIndexMap.get(columnNameTranscript)];
-                    clinicalTranscriptEntries.put(geneName, clinicalTranscript);
-                }
-                else
-                {
-                    LOGGER.warn("Suspicious line detected in clinical transcripts tsv: {}", line);
-                }
-            }
-            return new ClinicalTranscriptsModel(clinicalTranscriptEntries);
         }
         return ClinicalTranscriptsModel.emptyModel();
     }
