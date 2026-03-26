0a7df2c1a479ce4dec9d16f5fb579c166cb6f516
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-03-26 11:27:33 +0100

    finding: Different thresholds for WGS vs targeted

--- finding/src/main/java/com/hartwig/hmftools/finding/ThresholdValueFactory.java
+++ finding/src/main/java/com/hartwig/hmftools/finding/ThresholdValueFactory.java
@@ -5,6 +5,7 @@ import static com.hartwig.hmftools.common.purple.MicrosatelliteStatus.MSI_THRESH
 import static com.hartwig.hmftools.common.purple.TumorMutationalStatus.TMB_THRESHOLD;
 import static com.hartwig.hmftools.common.purple.TumorMutationalStatus.TML_THRESHOLD;
 
+import com.hartwig.hmftools.datamodel.orange.ExperimentType;
 import com.hartwig.hmftools.finding.datamodel.ThresholdValue;
 
 import org.jetbrains.annotations.NotNull;
@@ -12,11 +13,13 @@ import org.jetbrains.annotations.NotNull;
 class ThresholdValueFactory
 {
     private static final double HRD_THRESHOLD = 0.5;
+    // TODO: Is this defined elsewhere?
+    private static final double TARGETED_MIN_PURITY = 0.1;
 
     @NotNull
-    static ThresholdValue purityValue(double value)
+    static ThresholdValue purityValue(double value, ExperimentType experimentType)
     {
-        return new ThresholdValue(value, MIN_PURITY);
+        return new ThresholdValue(value, experimentType == ExperimentType.WHOLE_GENOME ? MIN_PURITY : TARGETED_MIN_PURITY);
     }
 
     @NotNull
