a31682723f2d4952033d43f1a5960f274b5edd4c
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-04-30 12:00:54 +0200

    Rolled back unnecessarily introduced class.

--- orange-datamodel/src/main/java/com/hartwig/hmftools/datamodel/driver/ReportedStatus.java
+++
@@ -1,9 +0,0 @@
-package com.hartwig.hmftools.datamodel.driver;
-
-public enum ReportedStatus
-{
-    // must be listed this way to allow comparison
-    NON_DRIVER_GENE,
-    NOT_REPORTED,
-    REPORTED,
-}
