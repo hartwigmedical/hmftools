3127f9776776f70244ec6cfff7bc6898d4520169
Arno de Quaasteniet <a.dequaasteniet@hartwigmedicalfoundation.nl>
2026-05-05 16:35:40 +0200

    Removed hasLowPurity, removed merging of finding lists.

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/FindingRecord.java
+++ finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/FindingRecord.java
@@ -1,18 +1,12 @@
 package com.hartwig.hmftools.finding.datamodel;
 
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.stream.Stream;
 
 import com.hartwig.hmftools.finding.datamodel.driver.Driver;
 import com.hartwig.hmftools.finding.datamodel.driver.DriverFindingList;
-import com.hartwig.hmftools.finding.datamodel.driver.DriverFindingListBuilder;
 import com.hartwig.hmftools.finding.datamodel.finding.FindingItem;
 import com.hartwig.hmftools.finding.datamodel.finding.FindingList;
-import com.hartwig.hmftools.finding.datamodel.finding.FindingStatus;
-import com.hartwig.hmftools.finding.datamodel.finding.FindingStatusBuilder;
-import com.hartwig.hmftools.finding.datamodel.finding.IFindingList;
 
 import jakarta.validation.constraints.NotNull;
 
@@ -41,38 +35,20 @@ public record FindingRecord(
 {
     public static final String VERSION = FindingRecord.class.getPackage().getImplementationVersion();
 
-    public boolean hasLowPurity()
-    {
-        List<IFindingList<?>> lists =
-                List.of(somaticSmallVariants, somaticGainDeletions, somaticDisruptions, fusions, viruses, chromosomeArmCopyNumbers, hlaAlleles, pharmacoGenotypes);
-        for(IFindingList<?> list : lists)
-        {
-            if(list.status().errors().contains(FindingStatus.Issue.LOW_PURITY))
-            {
-                return true;
-            }
-            if(list.status().warnings().contains(FindingStatus.Issue.LOW_PURITY))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
     @NotNull
-    public DriverFindingList<SmallVariant> allSmallVariants()
+    public List<SmallVariant> allSmallVariants()
     {
         return mergeDriverFindings(somaticSmallVariants, germlineSmallVariants);
     }
 
     @NotNull
-    public DriverFindingList<GainDeletion> allGainDeletions()
+    public List<GainDeletion> allGainDeletions()
     {
         return mergeDriverFindings(somaticGainDeletions, germlineGainDeletions);
     }
 
     @NotNull
-    public DriverFindingList<Disruption> allDisruptions()
+    public List<Disruption> allDisruptions()
     {
         return mergeDriverFindings(somaticDisruptions, germlineDisruptions);
     }
@@ -80,27 +56,31 @@ public record FindingRecord(
     @NotNull
     public List<SmallVariant> allReportableSmallVariants()
     {
-        return allSmallVariants().reportedOnly().findings();
+        return mergeReportedDriverFindings(somaticSmallVariants, germlineSmallVariants);
     }
 
     @NotNull
     public List<GainDeletion> allReportableGainDeletions()
     {
-        return allGainDeletions().reportedOnly().findings();
+        return mergeReportedDriverFindings(somaticGainDeletions, germlineGainDeletions);
     }
 
     @NotNull
     public List<Disruption> allReportableDisruptions()
     {
-        return allDisruptions().reportedOnly().findings();
+        return mergeReportedDriverFindings(somaticDisruptions, germlineDisruptions);
     }
 
-    private static <T extends Driver> DriverFindingList<T> mergeDriverFindings(DriverFindingList<T> somatic, DriverFindingList<T> germline)
+    @NotNull
+    private static <T extends Driver> List<T> mergeDriverFindings(DriverFindingList<T> somatic, DriverFindingList<T> germline)
     {
-        List<T> merged = mergeAndSort(somatic.findings(), germline.findings());
-        return DriverFindingListBuilder.<T>builder()
-                .status(mergeFindingStatus(somatic.status(), germline.status()))
-                .findings(merged).build();
+        return mergeAndSort(somatic.findings(), germline.findings());
+    }
+
+    @NotNull
+    private static <T extends Driver> List<T> mergeReportedDriverFindings(DriverFindingList<T> somatic, DriverFindingList<T> germline)
+    {
+        return mergeAndSort(somatic.reportedOnly().findings(), germline.reportedOnly().findings());
     }
 
     @NotNull
@@ -109,53 +89,5 @@ public record FindingRecord(
         return Stream.concat(list1.stream(), list2.stream())
                 .sorted(T.COMPARATOR)
                 .toList();
-    }
-
-    static FindingStatus mergeFindingStatus(FindingStatus somaticStatus, FindingStatus germlineStatus)
-    {
-
-        SortedSet<FindingStatus.Issue> errors = new TreeSet<>();
-        SortedSet<FindingStatus.Issue> warnings = new TreeSet<>();
-        warnings.addAll(somaticStatus.warnings());
-        warnings.addAll(germlineStatus.warnings());
-        FindingStatus.Status status = switch(somaticStatus.status())
-        {
-            case OK -> switch(germlineStatus.status())
-            {
-                case OK:
-                    yield FindingStatus.Status.OK;
-                case NOT_RELIABLE, NOT_AVAILABLE:
-                    warnings.addAll(germlineStatus.errors());
-                    yield FindingStatus.Status.OK;
-            };
-            case NOT_RELIABLE -> switch(germlineStatus.status())
-            {
-                case OK:
-                    warnings.addAll(somaticStatus.errors());
-                    yield FindingStatus.Status.OK;
-                case NOT_RELIABLE, NOT_AVAILABLE:
-                    yield FindingStatus.Status.NOT_RELIABLE;
-            };
-            case NOT_AVAILABLE -> switch(germlineStatus.status())
-            {
-                case OK:
-                    warnings.addAll(somaticStatus.errors());
-                    yield FindingStatus.Status.OK;
-                case NOT_RELIABLE:
-                    yield FindingStatus.Status.NOT_RELIABLE;
-                case NOT_AVAILABLE:
-                    yield FindingStatus.Status.NOT_AVAILABLE;
-            };
-        };
-        if(status != FindingStatus.Status.OK)
-        {
-            errors.addAll(somaticStatus.errors());
-            errors.addAll(germlineStatus.errors());
-        }
-        return FindingStatusBuilder.builder()
-                .status(status)
-                .errors(errors)
-                .warnings(warnings)
-                .build();
     }
 }
