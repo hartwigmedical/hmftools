385d231ee2e4c084999efa055386182276215890
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2026-05-06 11:27:26 +0200

    OPR-213: set reportable status to candidate for events with CN <0.1

--- finding/src/main/java/com/hartwig/hmftools/finding/ConversionUtil.java
+++ finding/src/main/java/com/hartwig/hmftools/finding/ConversionUtil.java
@@ -7,8 +7,8 @@ import java.util.List;
 import com.hartwig.hmftools.common.purple.Gender;
 import com.hartwig.hmftools.datamodel.orange.OrangeRecord;
 import com.hartwig.hmftools.finding.datamodel.FindingRecord;
-import com.hartwig.hmftools.finding.datamodel.FindingsJson;
 import com.hartwig.hmftools.finding.util.CandidateToReportableConverter;
+import com.hartwig.hmftools.finding.util.CopyNumberConverter;
 import com.hartwig.hmftools.finding.util.ErrorConverter;
 import com.hartwig.hmftools.finding.util.FindingRecordConverterUtil;
 import com.hartwig.hmftools.finding.util.GainDeletionsFilterConverter;
@@ -20,23 +20,20 @@ import com.hartwig.hmftools.finding.util.ReportedOnlyConverter;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
-public class ConversionUtil
-{
+public class ConversionUtil {
     public static FindingRecord orangeRecordToFindingRecord(OrangeRecord orangeRecord, @Nullable Path clinicalTranscriptsTsv,
-            @Nullable Path driverGeneTsv, @Nullable Gender gender) throws IOException
-    {
+            @Nullable Path driverGeneTsv, @Nullable Gender gender) throws IOException {
         return convert(FindingRecordFactory.fromOrangeRecord(orangeRecord, clinicalTranscriptsTsv, driverGeneTsv, gender));
     }
 
-    private static FindingRecord convert(FindingRecord findingRecord)
-    {
+    private static FindingRecord convert(FindingRecord findingRecord) {
         return FindingRecordConverterUtil.listConverter(List.of(ErrorConverter::convert,
-                        LowPurityConverter::convert,
-                        PTOConverter::convert,
-                        GainDeletionsFilterConverter::convert,
-                        NoGermlineConverter::convert,
-                        CandidateToReportableConverter::convert,
-                        ReportedOnlyConverter::convert))
-                .apply(findingRecord);
+                LowPurityConverter::convert,
+                PTOConverter::convert,
+                GainDeletionsFilterConverter::convert,
+                NoGermlineConverter::convert,
+                CandidateToReportableConverter::convert,
+                ReportedOnlyConverter::convert,
+                CopyNumberConverter::convert)).apply(findingRecord);
     }
 }
