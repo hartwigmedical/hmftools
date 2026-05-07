a8d7517bfd0e892689b7320b0114c9b839dd8973
shiv-hartwig <s.savani@hartwigmedicalfoundation.nl>
2026-05-07 14:05:39 +1000

    first commit

---
+++ redux/src/test/java/com/hartwig/hmftools/redux/splice/ContigSidecarTest.java
@@ -0,0 +1,54 @@
+package com.hartwig.hmftools.redux.splice;
+
+import static com.hartwig.hmftools.common.test.GeneTestUtils.CHR_1;
+import static com.hartwig.hmftools.common.test.GeneTestUtils.CHR_2;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import com.hartwig.hmftools.common.region.BaseRegion;
+
+import org.junit.Test;
+
+public class ContigSidecarTest
+{
+    @Test
+    public void testRoundTrip() throws IOException
+    {
+        File tempFile = Files.createTempFile("contig_sidecar_", ".tsv").toFile();
+        tempFile.deleteOnExit();
+
+        List<ContigEntry> entries = List.of(
+                new ContigEntry(
+                        "ensENSG0000001_GENEA_ENST0000001", "ENSG0000001", "GENEA", "ENST0000001", CHR_1,
+                        List.of(new BaseRegion(100, 199), new BaseRegion(300, 399))),
+                new ContigEntry(
+                        "ensENSG0000002_GENEB_ENST0000002", "ENSG0000002", "GENEB", "ENST0000002", CHR_2,
+                        List.of(new BaseRegion(1000, 1050))));
+
+        ContigSidecar.write(tempFile.getAbsolutePath(), entries);
+
+        List<ContigEntry> readBack = ContigSidecar.read(tempFile.getAbsolutePath());
+
+        assertEquals(2, readBack.size());
+
+        ContigEntry first = readBack.get(0);
+        assertEquals("ensENSG0000001_GENEA_ENST0000001", first.ContigName);
+        assertEquals("ENSG0000001", first.GeneId);
+        assertEquals("GENEA", first.GeneName);
+        assertEquals("ENST0000001", first.TransName);
+        assertEquals(CHR_1, first.Chromosome);
+        assertEquals(2, first.ExonSpans.size());
+        assertEquals(new BaseRegion(100, 199), first.ExonSpans.get(0));
+        assertEquals(new BaseRegion(300, 399), first.ExonSpans.get(1));
+
+        ContigEntry second = readBack.get(1);
+        assertEquals("ENST0000002", second.TransName);
+        assertEquals(1, second.ExonSpans.size());
+        assertEquals(new BaseRegion(1000, 1050), second.ExonSpans.get(0));
+    }
+}
