f292edd581861da29c88ba0e93be92143e5fec9b
Hong Wing Lee <4666150+hongwingl@users.noreply.github.com>
2026-07-24 12:45:37 +1000

    Findings: moved to separate repo (AUS439)
    - remove modules finding and finding-datamodel
    - orange-datamodel: small fixes to the test orange json

--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/SequencingScope.java
+++
@@ -1,7 +0,0 @@
-package com.hartwig.hmftools.finding.datamodel;
-
-public enum SequencingScope
-{
-    TARGETED,
-    WHOLE_GENOME
-}
--- finding-datamodel/src/main/java/com/hartwig/hmftools/finding/datamodel/VisualisationFile.java
+++
@@ -1,9 +0,0 @@
-package com.hartwig.hmftools.finding.datamodel;
-
-import jakarta.validation.constraints.NotNull;
-
-public record VisualisationFile(
-        @NotNull String fileName
-)
-{
-}
