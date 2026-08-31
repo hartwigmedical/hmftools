b4ec59dd437a29a25fb6458afd3061f23fab4c31
Charles Shale <shalecharles@gmail.com>
2026-08-31 13:59:06 +1000

    Esvee: caller raised inv short fragment thresholds: min AF 0.05 -> 0.1, fragment ratio 50 -> 100wq

--- esvee/src/main/java/com/hartwig/hmftools/esvee/caller/FilterConstants.java
+++ esvee/src/main/java/com/hartwig/hmftools/esvee/caller/FilterConstants.java
@@ -86,8 +86,8 @@ public class FilterConstants
     public static final int INV_SHORT_RATE_HIGHER = 400;
 
     public static final int INV_SHORT_FRAGMENT_LENGTH = 300;
-    public static final double INV_SHORT_FRAGMENT_MIN_AF = 0.05;
-    public static final int INV_SHORT_FRAGMENT_AF_RATIO = 50;
+    public static final double INV_SHORT_FRAGMENT_MIN_AF = 0.1;
+    public static final int INV_SHORT_FRAGMENT_AF_RATIO = 100;
 
     public static final int PRIME_MAX_PERMITTED_RANGE = 50;
     public static final int PRIME_MAX_BASE_FACTOR = 10;
